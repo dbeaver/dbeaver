5339116d72d1e519b16902fa9f72c8742052112f
jurgen <jurgen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2010-04-13 21:08:12 +0000

    Grid refactoring + fixes

---
+++ org.jkiss.dbeaver/core/src/org/jkiss/dbeaver/ui/controls/grid/scroll/NullScrollBar.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright (c) 2010, Serge Rieder and others. All Rights Reserved.
+ */
+
+package org.jkiss.dbeaver.ui.controls.grid.scroll;
+
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.widgets.Event;
+
+/**
+ * A null-op scrollbar proxy.  Used when the grid is not showing scrollbars.
+ *
+ * @author chris.gross@us.ibm.com
+ * @since 2.0.0
+ */
+public class NullScrollBar implements IGridScrollBar
+{
+
+    /** 
+     * {@inheritDoc}
+     */
+    public boolean getVisible()
+    {
+        return false;
+    }
+
+    /** 
+     * {@inheritDoc}
+     */
+    public void setVisible(boolean visible)
+    {
+    }
+
+    /** 
+     * {@inheritDoc}
+     */
+    public int getSelection()
+    {
+        return 0;
+    }
+
+    /** 
+     * {@inheritDoc}
+     */
+    public void setSelection(int selection)
+    {
+    }
+
+    /** 
+     * {@inheritDoc}
+     */
+    public void setValues(int selection, int min, int max, int thumb, int increment,
+                          int pageIncrement)
+    {
+    }
+
+    /** 
+     * {@inheritDoc}
+     */
+    public void handleMouseWheel(Event e)
+    {
+    }
+
+    /** 
+     * {@inheritDoc}
+     */
+    public void setMinimum(int min)
+    {
+    }
+
+    /** 
+     * {@inheritDoc}
+     */
+    public int getMinimum()
+    {
+        return 0;
+    }
+
+    /** 
+     * {@inheritDoc}
+     */
+    public void setMaximum(int max)
+    {
+    }
+
+    /** 
+     * {@inheritDoc}
+     */
+    public int getMaximum()
+    {
+        return 0;
+    }
+
+    /** 
+     * {@inheritDoc}
+     */
+    public void setThumb(int thumb)
+    {
+    }
+
+    /** 
+     * {@inheritDoc}
+     */
+    public int getThumb()
+    {
+        return 0;
+    }
+
+    /** 
+     * {@inheritDoc}
+     */
+    public void setIncrement(int increment)
+    {
+    }
+
+    /** 
+     * {@inheritDoc}
+     */
+    public int getIncrement()
+    {
+        return 0;
+    }
+
+    /** 
+     * {@inheritDoc}
+     */
+    public void setPageIncrement(int page)
+    {
+    }
+
+    /** 
+     * {@inheritDoc}
+     */
+    public int getPageIncrement()
+    {
+        return 0;
+    }
+
+    /** 
+     * {@inheritDoc}
+     */
+    public void addSelectionListener(SelectionListener listener)
+    { 
+    }
+
+    /** 
+     * {@inheritDoc}
+     */
+    public void removeSelectionListener(SelectionListener listener)
+    {
+   
+    }
+
+}
