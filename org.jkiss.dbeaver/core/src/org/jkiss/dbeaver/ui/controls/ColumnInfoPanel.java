b0ca09304a22408c1b300dea10ab07aadff75624
jurgen <jurgen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2010-04-25 20:12:56 +0000

    Value edit

--- org.jkiss.dbeaver/core/src/org/jkiss/dbeaver/ui/controls/ColumnInfoPanel.java
+++ org.jkiss.dbeaver/core/src/org/jkiss/dbeaver/ui/controls/ColumnInfoPanel.java
@@ -5,12 +5,17 @@
 package org.jkiss.dbeaver.ui.controls;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.TableCursor;
+import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
 import org.jkiss.dbeaver.model.data.DBDValueController;
 import org.jkiss.dbeaver.model.dbc.DBCColumnMetaData;
 
@@ -75,20 +80,61 @@ public class ColumnInfoPanel extends Composite {
             idGroup.setLayout(new GridLayout(2, false));
             idGroup.setText("Key info");
 
+            Label label = new Label(idGroup, SWT.NONE);
+            label.setText("Key: ");
+
+            Text text = new Text(idGroup, SWT.BORDER | SWT.READ_ONLY);
+            text.setText(valueController.getValueLocator().getUniqueKey().getName());
+            gd = new GridData(GridData.FILL_HORIZONTAL);
+            gd.minimumWidth = 50;
+            text.setLayoutData(gd);
+
+            label = new Label(idGroup, SWT.NONE);
+            label.setText("Columns: ");
+            gd = new GridData(GridData.FILL_HORIZONTAL);
+            gd.horizontalSpan = 2;
+            label.setLayoutData(gd);
+
+            Table keyColumnTable = new Table(idGroup, SWT.BORDER| SWT.FULL_SELECTION);
+            //keyColumnTable.setHeaderVisible(true);
+            keyColumnTable.setLinesVisible(true);
+            TableCursor cursor = new TableCursor(keyColumnTable, SWT.NONE);
+
+            TableColumn nameColumn = new TableColumn(keyColumnTable, SWT.LEFT);
+            nameColumn.setText("Name");
+
+            TableColumn valueColumn = new TableColumn(keyColumnTable, SWT.LEFT);
+            valueColumn.setText("Value");
+
+            int maxNameWidth = 0, maxValueWidth = 0;
+            GC gc = new GC(this);
+            gc.setFont(keyColumnTable.getFont());
             java.util.List<? extends DBCColumnMetaData> keyColumns = valueController.getValueLocator().getKeyColumns();
             for (DBCColumnMetaData keyColumn : keyColumns) {
-                Label label = new Label(idGroup, SWT.NONE);
-                label.setText(keyColumn.getColumnName() + ":");
 
-                Text text = new Text(idGroup, SWT.BORDER | SWT.READ_ONLY);
-                gd = new GridData(GridData.FILL_HORIZONTAL);
-                gd.minimumWidth = 50;
-                text.setLayoutData(gd);
+                TableItem keyItem = new TableItem(keyColumnTable, SWT.NONE);
+                String columnName = keyColumn.getColumnName();
+                keyItem.setText(0, columnName);
+
                 Object keyValue = valueController.getColumnValue(keyColumn);
                 String strValue = keyValue == null ? "[NULL" : keyValue.toString();
-                text.setText(strValue);
+                keyItem.setText(1, strValue);
+
+                maxNameWidth = Math.max(maxNameWidth, gc.stringExtent(columnName).x);
+                maxValueWidth = Math.max(maxValueWidth, gc.stringExtent(strValue).x);
             }
 
+            int maxColumnWidth = 300;
+            nameColumn.pack();
+            nameColumn.setWidth(Math.min(maxColumnWidth, nameColumn.getWidth() + 20));
+            valueColumn.pack();
+            valueColumn.setWidth(Math.min(maxColumnWidth, valueColumn.getWidth() + 20));
+
+            gd = new GridData(GridData.FILL_BOTH);
+            gd.minimumWidth = 100;// + maxNameWidth + maxValueWidth;
+            gd.widthHint = nameColumn.getWidth() + valueColumn.getWidth();
+            gd.horizontalSpan = 2;
+            keyColumnTable.setLayoutData(gd);
         }
         int extraGroupsNum = createInfoGroups(this, valueController);
 
