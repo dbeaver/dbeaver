d3f6f2b968143c50631a958e4dbb846dfee25dbf
jurgen <jurgen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2010-07-09 11:37:25 +0000

    About box
    Former-commit-id: 9076e6016bcf8e6a45d9007e22d1dd2107978a77

--- org.jkiss.dbeaver/core/src/org/jkiss/dbeaver/ui/dialogs/misc/AboutBoxDialog.java
+++ org.jkiss.dbeaver/core/src/org/jkiss/dbeaver/ui/dialogs/misc/AboutBoxDialog.java
@@ -7,17 +7,19 @@ package org.jkiss.dbeaver.ui.dialogs.misc;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.resource.JFaceColors;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.*;
 import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.BusyIndicator;
+import org.eclipse.swt.program.Program;
+import org.eclipse.swt.events.*;
 import org.eclipse.swt.graphics.Color;
-import org.eclipse.osgi.util.NLS;
-import org.eclipse.ui.internal.WorkbenchMessages;
+import org.eclipse.swt.graphics.Font;
 import org.eclipse.ui.internal.IWorkbenchHelpContextIds;
+import org.eclipse.ui.internal.about.InstallationDialog;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.jkiss.dbeaver.ui.DBIcon;
 
 /**
@@ -25,10 +27,20 @@ import org.jkiss.dbeaver.ui.DBIcon;
  */
 public class AboutBoxDialog extends Dialog
 {
+    private static final String STR_SITE_URL = "<a>http://dbeaver.jkiss.org</a>";
+    private static final String STR_EMAIL_URL = "<a>serge@jkiss.org</a>";
+    private final Font TITLE_FONT;
 
     public AboutBoxDialog(Shell shell)
     {
         super(shell);
+        TITLE_FONT = new Font(shell.getDisplay(), "SansSerif", 20, SWT.NORMAL);
+    }
+
+    @Override
+    public boolean close() {
+        TITLE_FONT.dispose();
+        return super.close();
     }
 
     protected void configureShell(Shell newShell) {
@@ -46,34 +58,95 @@ public class AboutBoxDialog extends Dialog
     protected Control createDialogArea(Composite parent)
     {
         Color background = JFaceColors.getBannerBackground(parent.getDisplay());
-        Color foreground = JFaceColors.getBannerForeground(parent.getDisplay());
+        //Color foreground = JFaceColors.getBannerForeground(parent.getDisplay());
         parent.setBackground(background);
-        parent.setForeground(foreground);
 
-        Label imageLabel = new Label(parent, SWT.NONE);
+        Composite group = new Composite(parent, SWT.NONE);
+        group.setBackground(background);
+        GridLayout layout = new GridLayout(1, false);
+        layout.marginHeight = 20;
+        layout.marginWidth = 20;
+        group.setLayout(layout);
+
+        GridData gd;
+
+        Label titleLabel = new Label(group, SWT.NONE);
+        titleLabel.setBackground(background);
+        titleLabel.setFont(TITLE_FONT);
+        titleLabel.setText("Universal Database Manager");
+        gd = new GridData(GridData.FILL_HORIZONTAL);
+        gd.horizontalAlignment = GridData.CENTER;
+        titleLabel.setLayoutData(gd);
+        titleLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseDoubleClick(MouseEvent e) {
+                BusyIndicator.showWhile(getShell().getDisplay(), new Runnable() {
+                    public void run() {
+                        IWorkbenchWindow workbenchWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+                        InstallationDialog dialog = new InstallationDialog(getShell(), workbenchWindow);
+                        dialog.setModalParent(AboutBoxDialog.this);
+                        dialog.open();
+                    }
+                });
+            }
+        });
+        
+        Label imageLabel = new Label(group, SWT.NONE);
         imageLabel.setBackground(background);
-        //imageLabel.setForeground(foreground);
 
-        GridData data = new GridData();
-        data.verticalAlignment = GridData.BEGINNING;
-        data.horizontalAlignment = GridData.CENTER;
-        data.grabExcessHorizontalSpace = true;
-        data.horizontalIndent = 20;
-        data.verticalIndent = 20;
-        imageLabel.setLayoutData(data);
+        gd = new GridData();
+        gd.verticalAlignment = GridData.BEGINNING;
+        gd.horizontalAlignment = GridData.CENTER;
+        gd.grabExcessHorizontalSpace = true;
+        imageLabel.setLayoutData(gd);
         imageLabel.setImage(DBIcon.ABOUT.getImage());
+
+        Label authorLabel = new Label(group, SWT.NONE);
+        authorLabel.setBackground(background);
+        authorLabel.setText("Authored By Serge Rieder, 2010");
+        gd = new GridData(GridData.FILL_HORIZONTAL);
+        gd.horizontalAlignment = GridData.CENTER;
+        authorLabel.setLayoutData(gd);
+
+        Link siteLink = new Link(group, SWT.NONE);
+        siteLink.setText(STR_SITE_URL);
+        siteLink.setBackground(background);
+        siteLink.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent e) {
+                Program.launch(e.text);
+            }
+        });
+        gd = new GridData();
+        gd.horizontalAlignment = GridData.CENTER;
+        siteLink.setLayoutData(gd);
+
+        Link emailLink = new Link(group, SWT.NONE);
+        emailLink.setText(STR_EMAIL_URL);
+        emailLink.setBackground(background);
+        emailLink.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent e) {
+                Program.launch("mailto:" + e.text);
+            }
+        });
+        gd = new GridData();
+        gd.horizontalAlignment = GridData.CENTER;
+        emailLink.setLayoutData(gd);
 
         return parent;
     }
 
     @Override
     protected void createButtonsForButtonBar(Composite parent) {
-        parent.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-        createButton(
+        GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+        gd.horizontalAlignment = GridData.CENTER; 
+        parent.setLayoutData(gd);
+        parent.setBackground(JFaceColors.getBannerBackground(parent.getDisplay()));
+        Button button = createButton(
             parent,
-            IDialogConstants.OK_ID, 
+            IDialogConstants.OK_ID,
             IDialogConstants.OK_LABEL,
             true);
+        button.setFocus();
     }
 
 }
