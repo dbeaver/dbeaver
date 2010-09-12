07893dc95eb898b6dbb586e2db788314995cd084
jurgen <jurgen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2010-09-12 19:36:17 +0000

    JDBC driver class finder (driver edit dialog)
    Former-commit-id: a46659b8bc5a1aef073d38970e9ea48a4067140e

--- org.jkiss.dbeaver/core/src/org/jkiss/dbeaver/ui/dialogs/driver/EditDriverDialog.java
+++ org.jkiss.dbeaver/core/src/org/jkiss/dbeaver/ui/dialogs/driver/EditDriverDialog.java
@@ -5,15 +5,23 @@
 package org.jkiss.dbeaver.ui.dialogs.driver;
 
 import net.sf.jkiss.utils.CommonUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.ListViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.*;
+import org.eclipse.swt.widgets.List;
 import org.jkiss.dbeaver.DBException;
 import org.jkiss.dbeaver.registry.DataSourceProviderDescriptor;
 import org.jkiss.dbeaver.registry.DriverDescriptor;
@@ -22,12 +30,22 @@ import org.jkiss.dbeaver.ui.UIUtils;
 import org.jkiss.dbeaver.utils.DBeaverUtils;
 
 import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.*;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 /**
  * EditDriverDialog
  */
 public class EditDriverDialog extends Dialog
 {
+    static final Log log = LogFactory.getLog(EditDriverDialog.class);
+
     private DataSourceProviderDescriptor provider;
     private DriverDescriptor driver;
     private String curFolder = null;
@@ -35,6 +53,8 @@ public class EditDriverDialog extends Dialog
     private Button deleteButton;
     private Button upButton;
     private Button downButton;
+    private Combo classListCombo;
+    private Button findClassButton;
     private Text driverNameText;
     private Text driverDescText;
     private Text driverClassText;
@@ -129,7 +149,7 @@ public class EditDriverDialog extends Dialog
                 }
             });
         }
-        {
+        if (!isReadOnly) {
             Composite libsGroup = new Composite(group, SWT.BORDER);
             libsGroup.setLayout(new GridLayout(2, false));
             gd = new GridData(GridData.FILL_BOTH);
@@ -141,23 +161,79 @@ public class EditDriverDialog extends Dialog
             libsLabel.setLayoutData(gd);
 
             //ListViewer list = new ListViewer(libsGroup, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL | SWT.H_SCROLL);
-            libList = new ListViewer(libsGroup, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL | SWT.H_SCROLL);
-            //libsTable.setLinesVisible (true);
-            //libsTable.setHeaderVisible (true);
-            libList.getControl().setLayoutData(new GridData(GridData.FILL_BOTH));
-            libList.getControl().addListener(SWT.Selection, new Listener()
             {
-                public void handleEvent(Event event)
-                {
-                    changeLibSelection();
-                }
-            });
+                Composite libsListGroup = new Composite(libsGroup, SWT.TOP);
+                libsListGroup.setLayoutData(new GridData(GridData.FILL_BOTH));
+                GridLayout layout = new GridLayout(1, false);
+                layout.marginHeight = 0;
+                layout.marginWidth = 0;
+                libsListGroup.setLayout(layout);
+                //gd = new GridData(GridData.FILL_HORIZONTAL);
 
-            for (DriverLibraryDescriptor lib : driver.getLibraries()) {
-                if (lib.isDisabled()) {
-                    continue;
+                // Additional libraries list
+                libList = new ListViewer(libsListGroup, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL | SWT.H_SCROLL);
+                //libsTable.setLinesVisible (true);
+                //libsTable.setHeaderVisible (true);
+                libList.getControl().setLayoutData(new GridData(GridData.FILL_BOTH));
+                libList.getControl().addListener(SWT.Selection, new Listener()
+                {
+                    public void handleEvent(Event event)
+                    {
+                        changeLibSelection();
+                    }
+                });
+
+                for (DriverLibraryDescriptor lib : driver.getLibraries()) {
+                    if (lib.isDisabled()) {
+                        continue;
+                    }
+                    libList.getList().add(lib.getLibraryFile().getPath());
                 }
-                libList.getList().add(lib.getLibraryFile().getPath());
+                
+                // Find driver class
+
+                Composite findClassGroup = new Composite(libsListGroup, SWT.TOP);
+                findClassGroup.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+                layout = new GridLayout(3, false);
+                layout.marginHeight = 0;
+                layout.marginWidth = 0;
+                findClassGroup.setLayout(layout);
+
+                UIUtils.createControlLabel(findClassGroup, "Driver class");
+                classListCombo = new Combo(findClassGroup, SWT.DROP_DOWN | SWT.READ_ONLY);
+                classListCombo.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+                classListCombo.addSelectionListener(new SelectionAdapter() {
+                    @Override
+                    public void widgetSelected(SelectionEvent e) {
+                        int selIndex = classListCombo.getSelectionIndex();
+                        if (selIndex >= 0) {
+                            driverClassText.setText(classListCombo.getItem(selIndex));
+                        }
+                    }
+                });
+                findClassButton = new Button(findClassGroup, SWT.PUSH);
+                findClassButton.setText("Find Class");
+                findClassButton.addListener(SWT.Selection, new Listener()
+                {
+                    public void handleEvent(Event event)
+                    {
+                        try {
+                            ClassFindJob classFinder = new ClassFindJob(libList.getList().getItems());
+                            new ProgressMonitorDialog(getShell()).run(true, true, classFinder);
+
+                            if (classListCombo != null && !classListCombo.isDisposed()) {
+                                java.util.List<String> clasNames = classFinder.getDriverClassNames();
+                                classListCombo.setItems(clasNames.toArray(new String[clasNames.size()]));
+                                classListCombo.setListVisible(true);
+                            }
+
+                        } catch (InvocationTargetException e) {
+                            log.error(e.getTargetException());
+                        } catch (InterruptedException e) {
+                            log.error(e);
+                        }
+                    }
+                });
             }
 
             Composite libsControlGroup = new Composite(libsGroup, SWT.TOP);
@@ -184,12 +260,8 @@ public class EditDriverDialog extends Dialog
                             File folderFile = new File(curFolder);
                             for (String fileName : fileNames) {
                                 libList.getList().add(new File(folderFile, fileName).getPath());
-/*
-                                TableItem item = new TableItem (libsTable, SWT.NONE);
-                                    item.setText(
-                                        new File(folderFile, fileName).getPath());
-*/
                             }
+                            changeLibContent();
                         }
                     }
                 }
@@ -209,6 +281,7 @@ public class EditDriverDialog extends Dialog
                     if (selected != null) {
                         curFolder = fd.getFilterPath();
                         libList.getList().add(selected);
+                        changeLibContent();
                     }
                 }
             });
@@ -221,6 +294,7 @@ public class EditDriverDialog extends Dialog
                 public void handleEvent(Event event)
                 {
                     libList.getList().remove(libList.getList().getSelectionIndices());
+                    changeLibContent();
                 }
             });
             deleteButton.setEnabled(false);
@@ -272,8 +346,27 @@ public class EditDriverDialog extends Dialog
                     cpDialog.open();
                 }
             });
+
+            changeLibContent();
         }
+
         return group;
+    }
+
+    @Override
+    protected void createButtonsForButtonBar(Composite parent) {
+        if (provider.isDriversManagable()) {
+            createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, true);
+            createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, false);
+        } else {
+            createButton(parent, IDialogConstants.OK_ID, IDialogConstants.CLOSE_LABEL, true);
+        }
+    }
+
+    private void changeLibContent()
+    {
+        List list = libList.getList();
+        findClassButton.setEnabled(list.getItemCount() > 0);
     }
 
     private void changeLibSelection()
@@ -286,13 +379,20 @@ public class EditDriverDialog extends Dialog
 
     private void onChangeProperty()
     {
-        getButton(IDialogConstants.OK_ID).setEnabled(
-            !CommonUtils.isEmpty(driverNameText.getText()) &&
-            !CommonUtils.isEmpty(driverClassText.getText()));
+        if (provider.isDriversManagable()) {
+            getButton(IDialogConstants.OK_ID).setEnabled(
+                !CommonUtils.isEmpty(driverNameText.getText()) &&
+                !CommonUtils.isEmpty(driverClassText.getText()));
+        }
     }
 
     protected void okPressed()
     {
+        if (!provider.isDriversManagable()) {
+            super.okPressed();
+            return;
+        }
+
         // Check props
         Integer portNumber = null;
         if (!CommonUtils.isEmpty(driverPortText.getText())) {
@@ -322,7 +422,7 @@ public class EditDriverDialog extends Dialog
         for (File libFile : libFiles) {
             boolean exists = false;
             for (DriverLibraryDescriptor lib : driver.getLibraries()) {
-                if (lib.getLibraryFile().equals(libFile)) {
+                if (!lib.isDisabled() && lib.getLibraryFile().equals(libFile)) {
                     exists = true;
                     break;
                 }
@@ -355,6 +455,86 @@ public class EditDriverDialog extends Dialog
         }
 
         super.okPressed();
+    }
+
+    private class ClassFindJob implements IRunnableWithProgress {
+
+        private String[] fileNames;
+        private java.util.List<String> driverClassNames = new ArrayList<String>();
+
+        private ClassFindJob(String[] fileNames) {
+            this.fileNames = fileNames;
+        }
+
+        public java.util.List<String> getDriverClassNames() {
+            return driverClassNames;
+        }
+
+        public void run(IProgressMonitor monitor)
+            throws InvocationTargetException, InterruptedException
+        {
+            findDriverClasses(monitor);
+        }
+
+        private void findDriverClasses(IProgressMonitor monitor)
+        {
+            java.util.List<File> libFiles = new ArrayList<File>();
+            java.util.List<URL> libURLs = new ArrayList<URL>();
+            for (String libFileName : fileNames) {
+                File libFile = new File(libFileName);
+                if (libFile.exists() && !libFile.isDirectory()) {
+                    libFiles.add(libFile);
+                    try {
+                        libURLs.add(libFile.toURI().toURL());
+                    } catch (MalformedURLException e) {
+                        log.debug(e);
+                    }
+                }
+            }
+            ClassLoader findCL = new URLClassLoader(libURLs.toArray(new URL[libURLs.size()]));
+
+            for (File libFile : libFiles) {
+                if (monitor.isCanceled()) {
+                    break;
+                }
+                findDriverClasses(monitor, findCL, libFile);
+            }
+        }
+
+        private void findDriverClasses(IProgressMonitor monitor, ClassLoader findCL, File libFile)
+        {
+            try {
+                JarFile currentFile = new JarFile(libFile, false);
+                monitor.beginTask(libFile.getName(), currentFile.size());
+
+                for (Enumeration e = currentFile.entries(); e.hasMoreElements(); ) {
+                    {
+                        if (monitor.isCanceled()) {
+                            break;
+                        }
+                        JarEntry current = (JarEntry) e.nextElement();
+                        String fileName = current.getName();
+                        if (fileName.endsWith(".class")) {
+                            String className = fileName.replaceAll("/", ".").replace(".class", "");
+                            monitor.subTask(className);
+                            try {
+                                Class<?> aClass = Class.forName(className, false, findCL);
+                                if (java.sql.Driver.class.isAssignableFrom(aClass)) {
+                                    driverClassNames.add(aClass.getName());
+                                }
+                            } catch (Throwable e1) {
+                                // do nothing
+                            }
+                            monitor.worked(1);
+                        }
+                    }
+                }
+                monitor.done();
+            } catch (IOException e) {
+                log.debug(e);
+            }
+        }
+
     }
 
 }
