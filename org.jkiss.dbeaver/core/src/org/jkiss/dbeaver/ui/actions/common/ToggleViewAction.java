00dbf9dbd868d0e856b6abdfbd2136e34ae4f541
jurgen <jurgen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2010-09-20 20:46:54 +0000

    Action refactoring
    Former-commit-id: a3cc5ee1b7a95b994609f653eb4542badbe4c51f

--- org.jkiss.dbeaver/core/src/org/jkiss/dbeaver/ui/actions/common/ToggleViewAction.java
+++ org.jkiss.dbeaver/core/src/org/jkiss/dbeaver/ui/actions/common/ToggleViewAction.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2010, Serge Rieder and others. All Rights Reserved.
  */
 
-package org.jkiss.dbeaver.ui.actions;
+package org.jkiss.dbeaver.ui.actions.common;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
---
+++ org.jkiss.dbeaver/core/src/org/jkiss/dbeaver/ui/actions/navigator/SetActiveObjectAction.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2010, Serge Rieder and others. All Rights Reserved.
+ */
+
+package org.jkiss.dbeaver.ui.actions.navigator;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IActionDelegate;
+import org.jkiss.dbeaver.DBException;
+import org.jkiss.dbeaver.core.DBeaverCore;
+import org.jkiss.dbeaver.model.navigator.DBNNode;
+import org.jkiss.dbeaver.model.struct.DBSEntitySelector;
+import org.jkiss.dbeaver.model.DBUtils;
+import org.jkiss.dbeaver.model.runtime.DBRRunnableWithProgress;
+import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
+import org.jkiss.dbeaver.utils.ViewUtils;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class SetActiveObjectAction implements IActionDelegate
+{
+    private ISelection selection;
+
+    public void run(IAction action)
+    {
+        if (selection instanceof IStructuredSelection) {
+            final DBNNode selectedNode = ViewUtils.getSelectedNode((IStructuredSelection) selection);
+            if (selectedNode != null) {
+                final DBSEntitySelector activeContainer = DBUtils.queryParentInterface(
+                    DBSEntitySelector.class, selectedNode.getObject());
+                DBeaverCore.getInstance().runAndWait(new DBRRunnableWithProgress() {
+                    public void run(DBRProgressMonitor monitor)
+                        throws InvocationTargetException, InterruptedException
+                    {
+                        try {
+                            activeContainer.setActiveChild(monitor, selectedNode.getObject());
+                        }
+                        catch (DBException e) {
+                            throw new InvocationTargetException(e);
+                        }
+                    }
+                });
+            }
+        }
+    }
+
+    public void selectionChanged(IAction action, ISelection selection)
+    {
+        this.selection = selection;
+    }
+
+}
