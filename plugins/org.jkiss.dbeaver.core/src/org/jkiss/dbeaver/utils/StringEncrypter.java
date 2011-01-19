9825e8c4a31d35192a7aa6876996fe8d51ffa088
jurgen <jurgen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2011-01-19 15:27:44 +0000

    Password encryption fix. Use simple XOR instead of complex DES encryption
    Former-commit-id: f1a8b9be1a5a6094dc0cd064469d74a733c93a82

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/utils/StringEncrypter.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/utils/StringEncrypter.java
@@ -6,35 +6,32 @@ package org.jkiss.dbeaver.utils;
 
 import net.sf.jkiss.utils.Base64;
 
-import javax.crypto.Cipher;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.DESKeySpec;
-import javax.crypto.spec.SecretKeySpec;
 import java.io.UnsupportedEncodingException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 /**
  * Encryption util
  */
 public class StringEncrypter {
 
-    public static final String SCHEME_DES = "DES";
+    //public static final String SCHEME_DES = "DES";
 
     private static final String CHARSET = "UTF8";
 
-    private DESKeySpec keySpec;
-    private Cipher cipher;
+    private String encryptionKey;
+    //private DESKeySpec keySpec;
+    //private Cipher cipher;
 
     public StringEncrypter(String encryptionKey) throws EncryptionException
     {
+        this.encryptionKey = encryptionKey;
         if (encryptionKey == null) {
             throw new IllegalArgumentException("encryption key was null");
         }
         if (encryptionKey.trim().length() < 24) {
             throw new IllegalArgumentException("encryption key was less than 24 characters");
         }
+/*
         try {
             byte[] keyAsBytes = encryptionKey.getBytes(CHARSET);
 
@@ -52,6 +49,7 @@ public class StringEncrypter {
         } catch (NoSuchPaddingException e) {
             throw new EncryptionException(e);
         }
+*/
 
     }
 
@@ -62,14 +60,37 @@ public class StringEncrypter {
         }
 
         try {
+            byte[] stringBytes = unencryptedString.getBytes(CHARSET);
+            byte[] plainBytes = Arrays.copyOf(stringBytes, stringBytes.length + 2);
+            plainBytes[plainBytes.length - 2] = 0;
+            plainBytes[plainBytes.length - 1] = -127;
+            xorStringByKey(plainBytes);
+            return Base64.encode(plainBytes);
+/*
             SecretKey key = makeSecretKey();//keyFactory.generateSecret(keySpec);
             cipher.init(Cipher.ENCRYPT_MODE, key);
             byte[] cleartext = unencryptedString.getBytes(CHARSET);
             byte[] ciphertext = cipher.doFinal(cleartext);
 
             return Base64.encode(ciphertext);
+*/
         } catch (Exception e) {
             throw new EncryptionException(e);
+        }
+    }
+
+    private void xorStringByKey(byte[] plainBytes) throws UnsupportedEncodingException
+    {
+        byte[] keyBytes = encryptionKey.getBytes(CHARSET);
+
+        int keyOffset = 0;
+        for (int i = 0; i < plainBytes.length; i++) {
+            byte keyChar = keyBytes[keyOffset];
+            keyOffset++;
+            if (keyOffset >= keyBytes.length) {
+                keyOffset = 0;
+            }
+            plainBytes[i] ^= keyChar;
         }
     }
 
@@ -80,27 +101,44 @@ public class StringEncrypter {
         }
 
         try {
+            byte[] cleartext = Base64.decode(encryptedString);
+            xorStringByKey(cleartext);
+            if (cleartext[cleartext.length - 2] != 0 || cleartext[cleartext.length - 1] != -127) {
+                throw new EncryptionException("Invalid encrypted string");
+            }
+            return new String(cleartext, 0, cleartext.length - 2, CHARSET);
+/*
             SecretKey key = makeSecretKey();
-            cipher.init(Cipher.DECRYPT_MODE, key/*, SecureRandom.getInstance("SHA1PRNG")*/);
+            cipher.init(Cipher.DECRYPT_MODE, key*/
+/*, SecureRandom.getInstance("SHA1PRNG")*//*
+);
             byte[] cleartext = Base64.decode(encryptedString);
             byte[] ciphertext = cipher.doFinal(cleartext);
 
             return new String(ciphertext, CHARSET);
+*/
         } catch (Exception e) {
             throw new EncryptionException(e);
         }
     }
 
+/*
     private SecretKey makeSecretKey()
     {
         return new SecretKeySpec(keySpec.getKey(), "DES");
     }
+*/
 
     @SuppressWarnings("serial")
     public static class EncryptionException extends Exception {
         public EncryptionException(Throwable t)
         {
             super(t);
+        }
+
+        public EncryptionException(String message)
+        {
+            super(message);
         }
     }
 
