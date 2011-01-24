3a4dc0453b43b58e12f9dc3dac876b743acf592e
jurgen <jurgen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2011-01-24 10:14:31 +0000

    Import config plugin fix
    Former-commit-id: 6e38563fb7560f0d3677c56acfbef4902b077164

--- plugins/org.jkiss.dbeaver.import-config/src/org/jkiss/dbeaver/ext/import_config/importWizards/ImportWizard.java
+++ plugins/org.jkiss.dbeaver.import-config/src/org/jkiss/dbeaver/ext/import_config/importWizards/ImportWizard.java
@@ -3,7 +3,6 @@
  */
 package org.jkiss.dbeaver.ext.import_config.importWizards;
 
-import org.eclipse.core.resources.IFile;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.IImportWizard;
@@ -21,9 +20,6 @@ public class ImportWizard extends Wizard implements IImportWizard {
 	 * @see org.eclipse.jface.wizard.Wizard#performFinish()
 	 */
 	public boolean performFinish() {
-		IFile file = mainPage.createNewFile();
-        if (file == null)
-            return false;
         return true;
 	}
 	 
@@ -33,7 +29,7 @@ public class ImportWizard extends Wizard implements IImportWizard {
 	public void init(IWorkbench workbench, IStructuredSelection selection) {
 		setWindowTitle("File Import Wizard"); //NON-NLS-1
 		setNeedsProgressMonitor(true);
-		mainPage = new ImportWizardPage("Import File",selection); //NON-NLS-1
+		mainPage = new ImportWizardPage("Import File"); //NON-NLS-1
 	}
 	
 	/* (non-Javadoc)
