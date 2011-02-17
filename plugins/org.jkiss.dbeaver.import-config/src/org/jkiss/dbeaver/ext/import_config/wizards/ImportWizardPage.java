d23b4491dcd1efefbf3cc28787ef0717bec46632
jurgen <jurgen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2011-02-17 22:42:08 +0000

    Import config
    Former-commit-id: 6753c9e7ea82df689fee11d5fcb7b10685830190

--- plugins/org.jkiss.dbeaver.import-config/src/org/jkiss/dbeaver/ext/import_config/wizards/ImportWizardPage.java
+++ plugins/org.jkiss.dbeaver.import-config/src/org/jkiss/dbeaver/ext/import_config/wizards/ImportWizardPage.java
@@ -1,7 +1,8 @@
 /*
  * Copyright (c) 2011, Serge Rieder and others. All Rights Reserved.
  */
-package org.jkiss.dbeaver.ext.import_config.importWizards;
+
+package org.jkiss.dbeaver.ext.import_config.wizards;
 
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.widgets.Composite;
