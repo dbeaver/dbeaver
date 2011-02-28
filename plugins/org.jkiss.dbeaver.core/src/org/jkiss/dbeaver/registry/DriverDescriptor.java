95cbe8211585db33ca1880ed64269e833dca3e9b
jurgen <jurgen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2011-02-28 16:22:33 +0000

    Driver load fix
    Former-commit-id: 5af316ecdd8ffda22ada72ca1f82cf6e4835ca12

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/registry/DriverDescriptor.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/registry/DriverDescriptor.java
@@ -576,7 +576,7 @@ public class DriverDescriptor extends AbstractDescriptor implements DBPDriver
             }
             URL url;
             try {
-                url = new URL("file:" + library.getLibraryFile().getAbsolutePath());
+                url = new URL("file:/" + library.getLibraryFile().getAbsolutePath());
             } catch (MalformedURLException e) {
                 log.error(e);
                 continue;
