8fc2bd1ac63a6f8dc64694c4897fa1fcebae918a
jurgen <jurgen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2011-03-17 22:09:14 +0000

    DND support in search view
    Former-commit-id: 11154c7c8dc52e574a1d0ff379309a545510efa9

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/views/search/SearchObjectsView.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/views/search/SearchObjectsView.java
@@ -28,6 +28,7 @@ import org.jkiss.dbeaver.ui.UIUtils;
 import org.jkiss.dbeaver.ui.controls.itemlist.NodeListControl;
 import org.jkiss.dbeaver.ui.views.navigator.database.DatabaseNavigatorTree;
 import org.jkiss.dbeaver.ui.views.navigator.database.load.TreeLoadNode;
+import org.jkiss.dbeaver.utils.ViewUtils;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.*;
@@ -311,6 +312,7 @@ public class SearchObjectsView extends ViewPart {
             gd.heightHint = 500;
             itemList.setLayoutData(gd);
             getSite().setSelectionProvider(itemList.getSelectionProvider());
+            ViewUtils.addDragAndDropSupport(itemList.getItemsViewer());
             //itemList.addFocusListener(new ItemsFocusListener());
         }
 
