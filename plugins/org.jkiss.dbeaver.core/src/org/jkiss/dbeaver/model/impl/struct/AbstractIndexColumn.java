3ac437b690c4d8cf63151f45da2590df2c9bf9e6
jurgen <jurgen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2011-03-23 13:47:56 +0000

    Tree navigator in object list control Inline fk/pk/index columns
    Former-commit-id: b0cc5386001b632c7dda4a8a58a98c6782079d27

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/model/impl/struct/AbstractIndexColumn.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/model/impl/struct/AbstractIndexColumn.java
@@ -4,16 +4,29 @@
 
 package org.jkiss.dbeaver.model.impl.struct;
 
+import org.eclipse.swt.graphics.Image;
+import org.jkiss.dbeaver.ext.ui.IObjectImageProvider;
 import org.jkiss.dbeaver.model.struct.DBSIndexColumn;
+import org.jkiss.dbeaver.model.struct.DBSTableColumn;
 
 /**
  * AbstractIndexColumn
  */
-public abstract class AbstractIndexColumn implements DBSIndexColumn
+public abstract class AbstractIndexColumn implements DBSIndexColumn, IObjectImageProvider
 {
 
     public boolean isPersisted()
     {
         return true;
     }
+
+    public Image getObjectImage()
+    {
+        DBSTableColumn tableColumn = getTableColumn();
+        if (tableColumn instanceof IObjectImageProvider) {
+            return ((IObjectImageProvider)tableColumn).getObjectImage();
+        }
+        return null;
+    }
+
 }
