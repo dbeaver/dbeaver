be319455356b963461d278d577f80e3af9fe462c
jurgen <jurgen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2011-03-24 14:12:49 +0000

    DS description
    Former-commit-id: 33004a2bbe31f142db239a2609166e5e44ec35dc

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/model/DBPDataSource.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/model/DBPDataSource.java
@@ -25,6 +25,12 @@ public interface DBPDataSource extends DBPObject
      */
     DBSDataSourceContainer getContainer();
 
+    /**
+     * Datasource information/options
+     * Info SHOULD be read at datasource initialization stage and should be cached and available
+     * at the moment of invocation of this function.
+     * @return datasource info.
+     */
     DBPDataSourceInfo getInfo();
 
     /**
@@ -38,16 +44,18 @@ public interface DBPDataSource extends DBPObject
     /**
      * Opens new execution context
      * @param monitor progress monitor
-     * @param purpose
-     *@param task task description  @return execution context
+     * @param purpose context purpose
+     * @param task task description
+     * @return execution context
      */
     DBCExecutionContext openContext(DBRProgressMonitor monitor, DBCExecutionPurpose purpose, String task);
 
     /**
      * Opens new isolated execution context.
      * @param monitor progress monitor
-     * @param purpose
-     *@param task task description  @return execution context
+     * @param purpose context purpose
+     * @param task task description
+     * @return execution context
      */
     DBCExecutionContext openIsolatedContext(DBRProgressMonitor monitor, DBCExecutionPurpose purpose, String task);
 
@@ -55,19 +63,20 @@ public interface DBPDataSource extends DBPObject
      * checks connection is alive and reconnects if needed.
      *
      * @throws org.jkiss.dbeaver.DBException on any error
-     * @param monitor
+     * @param monitor progress monitor
      */
     void invalidateConnection(DBRProgressMonitor monitor) throws DBException;
 
     /**
-     * Reads base metadata from remote database or do any neccessary initialization routines.
-     * @throws DBException on any DB error  @param monitor progress monitor
+     * Reads base metadata from remote database or do any necessarily initialization routines.
+     * @throws DBException on any DB error
+     * @param monitor progress monitor
      */
     void initialize(DBRProgressMonitor monitor) throws DBException;
 
     /**
      * Closes datasource
-     * @param monitor
+     * @param monitor progress monitor
      */
     void close(DBRProgressMonitor monitor);
 
