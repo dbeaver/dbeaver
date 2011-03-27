ff94b68bab8957d69ba214cf1967e2101a274af3
jurgen <jurgen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2011-03-27 20:51:29 +0000

    DB objects rename
    Former-commit-id: edb913aaf427a4e18c988222b95028bca7b23c97

--- plugins/org.jkiss.dbeaver.mysql/src/org/jkiss/dbeaver/ext/mysql/runtime/MySQLCatalogManager.java
+++ plugins/org.jkiss.dbeaver.mysql/src/org/jkiss/dbeaver/ext/mysql/runtime/MySQLCatalogManager.java
@@ -11,9 +11,8 @@ import org.jkiss.dbeaver.ext.IDatabasePersistAction;
 import org.jkiss.dbeaver.ext.mysql.model.MySQLCatalog;
 import org.jkiss.dbeaver.ext.mysql.model.MySQLDataSource;
 import org.jkiss.dbeaver.model.DBPEvent;
-import org.jkiss.dbeaver.model.edit.DBECommandReflector;
-import org.jkiss.dbeaver.model.edit.DBEObjectRenamer;
 import org.jkiss.dbeaver.model.edit.DBEObjectMaker;
+import org.jkiss.dbeaver.model.edit.DBEObjectRenamer;
 import org.jkiss.dbeaver.model.impl.edit.AbstractDatabasePersistAction;
 import org.jkiss.dbeaver.model.impl.edit.DBECommandImpl;
 import org.jkiss.dbeaver.model.impl.jdbc.edit.DBEObjectCommanderJDBC;
@@ -116,6 +115,79 @@ public class MySQLCatalogManager extends DBEObjectCommanderJDBC<MySQLCatalog> im
                 new DBPEvent(DBPEvent.Action.OBJECT_UPDATE, object));
         }
     }
+
+    /*
+http://www.artfulsoftware.com/infotree/queries.php#112
+Rename Database
+It's sometimes necessary to rename a database. MySQL 5.0 has no command for it. Simply bringing down the server to rename a database directory is not safe. MySQL 5.1.7 introduced a RENAME DATABASE command, but the command left several unchanged database objects behind, and was found to lose data, so it was dropped in 5.1.23.
+
+It seems a natural for a stored procedure using dynamic (prepared) statements. PREPARE supports CREATE | RENAME TABLE. As precautions:
+
+    Before calling the sproc, the new database must have been created.
+    The procedure refuses to rename the mysql database.
+    The old database is left behind, minus what was moved.
+
+
+DROP PROCEDURE IF EXISTS RenameDatabase;
+DELIMITER go
+CREATE PROCEDURE RenameDatabase( oldname CHAR (64), newname CHAR(64) )
+BEGIN
+  DECLARE version CHAR(32);
+  DECLARE sname CHAR(64) DEFAULT NULL;
+  DECLARE rows INT DEFAULT 1;
+  DECLARE changed INT DEFAULT 0;
+  IF STRCMP( oldname, 'mysql' ) <> 0 THEN
+    REPEAT
+      SELECT table_name INTO sname
+      FROM information_schema.tables AS t
+      WHERE t.table_type='BASE TABLE' AND t.table_schema = oldname
+      LIMIT 1;
+      SET rows = FOUND_ROWS();
+      IF rows = 1 THEN
+        SET @scmd = CONCAT( 'RENAME TABLE `', oldname, '`.`', sname,
+                            '` TO `', newname, '`.`', sname, '`' );
+        PREPARE cmd FROM @scmd;
+        EXECUTE cmd;
+        DEALLOCATE PREPARE cmd;
+        SET changed = 1;
+      END IF;
+    UNTIL rows = 0 END REPEAT;
+    IF changed > 0 THEN
+      SET @scmd = CONCAT( "UPDATE mysql.db SET Db = '",
+                          newname,
+                          "' WHERE Db = '", oldname, "'" );
+      PREPARE cmd FROM @scmd;
+      EXECUTE cmd;
+      DROP PREPARE cmd;
+      SET @scmd = CONCAT( "UPDATE mysql.proc SET Db = '",
+                          newname,
+                          "' WHERE Db = '", oldname, "'" );
+      PREPARE cmd FROM @scmd;
+      EXECUTE cmd;
+      DROP PREPARE cmd;
+      SELECT version() INTO version;
+      IF version >= '5.1.7' THEN
+        SET @scmd = CONCAT( "UPDATE mysql.event SET db = '",
+                            newname,
+                            "' WHERE db = '", oldname, "'" );
+        PREPARE cmd FROM @scmd;
+        EXECUTE cmd;
+        DROP PREPARE cmd;
+      END IF;
+      SET @scmd = CONCAT( "UPDATE mysql.columns_priv SET Db = '",
+                          newname,
+                          "' WHERE Db = '", oldname, "'" );
+      PREPARE cmd FROM @scmd;
+      EXECUTE cmd;
+      DROP PREPARE cmd;
+      FLUSH PRIVILEGES;
+    END IF;
+  END IF;
+END;
+go
+DELIMITER ;
+
+     */
 
 }
 
