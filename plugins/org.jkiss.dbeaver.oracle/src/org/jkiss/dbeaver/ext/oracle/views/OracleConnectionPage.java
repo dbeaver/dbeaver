0314c30db862b07d2bd3a66836021a69b6cfc326
ejf <ejf@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2011-09-20 20:10:33 +0000

    multiple Oracle homes are available again
    Former-commit-id: 4c3b824b3a38e7387744fb15afd6a673f06097c9

--- plugins/org.jkiss.dbeaver.oracle/src/org/jkiss/dbeaver/ext/oracle/views/OracleConnectionPage.java
+++ plugins/org.jkiss.dbeaver.oracle/src/org/jkiss/dbeaver/ext/oracle/views/OracleConnectionPage.java
@@ -208,16 +208,10 @@ public class OracleConnectionPage extends DialogPage implements IDataSourceConne
         Label label = UIUtils.createControlLabel(selectorContainer, "  Oracle Home");
         label.setFont(UIUtils.makeBoldFont(label.getFont()));
         oraHomeCombo = new Combo(selectorContainer, SWT.DROP_DOWN);
-/* only one ORA HOME (from PATH) is used for now
         for (OracleHomeDescriptor home : OCIUtils.oraHomes) {
             oraHomeCombo.add(home.getOraHome());
         }
         oraHomeCombo.add(BROWSE);
-*/
-        if (!OCIUtils.oraHomes.isEmpty()) {
-            oraHomeCombo.setText(OCIUtils.oraHomes.get(0).getOraHome());
-        }
-        oraHomeCombo.setEnabled(false);
         GridData gd = new GridData(GridData.FILL_HORIZONTAL);
         gd.grabExcessHorizontalSpace = true;
         oraHomeCombo.setLayoutData(gd);
@@ -433,7 +427,6 @@ public class OracleConnectionPage extends DialogPage implements IDataSourceConne
             Map<Object,Object> connectionProperties = connectionInfo.getProperties();
 
             if (isOCI) {
-/* only one ORA HOME (from PATH) is used for now
                 Object oraHome = connectionProperties.get(OracleConstants.PROP_ORA_HOME);
                 if (oraHome != null) {
                     oraHomeCombo.setText(oraHome.toString());
@@ -443,7 +436,6 @@ public class OracleConnectionPage extends DialogPage implements IDataSourceConne
                         oraHomeCombo.setText(OCIUtils.oraHomes.get(0).getOraHome());
                     }
                 }
-*/
             }
 
             final Object conTypeProperty = connectionProperties.get(OracleConstants.PROP_CONNECTION_TYPE);
