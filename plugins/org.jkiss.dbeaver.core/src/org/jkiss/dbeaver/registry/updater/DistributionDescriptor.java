f5261771e34c087de74e7d5af7b540d2a4b061ea
jurgen <jurgen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2011-10-11 19:08:46 +0000

    Updater registry
    Former-commit-id: 8a02d46ee3acd66befff357fcf686fe58d8b59e9

---
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/registry/updater/DistributionDescriptor.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2011, Serge Rieder and others. All Rights Reserved.
+ */
+
+package org.jkiss.dbeaver.registry.updater;
+
+import org.jkiss.dbeaver.registry.OSDescriptor;
+
+/**
+ * Update distribution descriptor
+ */
+public class DistributionDescriptor {
+
+    private OSDescriptor os;
+    private String type;
+
+}
---
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/registry/updater/UpdateSiteDescriptor.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2011, Serge Rieder and others. All Rights Reserved.
+ */
+
+package org.jkiss.dbeaver.registry.updater;
+
+/**
+ * Update site descriptor
+ */
+public class UpdateSiteDescriptor {
+
+    private String siteName;
+    private String baseURL;
+    private int priority;
+
+}
