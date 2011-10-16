89e12b7f40e75df99740398dca5e792bb4ea0f98
ejf <ejf@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2011-10-16 22:09:29 +0000

    Babel based internationalization algorithm performance is improved.
    Former-commit-id: a03e3d98a9c6b953bba6587f016b7b515b0cf802

--- tools/src/org/jkiss/tools/ant/babel/BabelRepackerTask.java
+++ tools/src/org/jkiss/tools/ant/babel/BabelRepackerTask.java
@@ -13,7 +13,12 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
@@ -57,58 +62,67 @@ public class BabelRepackerTask extends Task
         Target owningTarget = this.getOwningTarget();
         Hashtable properties = prj.getProperties();
 */
+        Map<File, List<File>> pluginJarMap = new HashMap<File, List<File>>(); // plugin jar -> list of Babel jars (for different locales)
+        System.out.println("Collecting an information about jars for localization.");
+
         for (String locale : locales.split(",")) {
-            processLocale(locale);
-        }
-    }
-
-    private void processLocale(String locale)
-    {
-        String[] babelJars = null;
-        final String nlSuffix = "nl_" + locale;
-        File babelDir = new File(babelLocation + "/eclipse/plugins");
-        if (babelDir.exists() && babelDir.isDirectory()) {
-            babelJars = babelDir.list(new FilenameFilter()
-            {
-                public boolean accept(File dir, String name)
+            String[] babelJars = null;
+            final String nlSuffix = "nl_" + locale;
+            File babelDir = new File(babelLocation + "/eclipse/plugins");
+            if (babelDir.exists() && babelDir.isDirectory()) {
+                babelJars = babelDir.list(new FilenameFilter()
                 {
-                    return name.endsWith(".jar") && name.contains(nlSuffix);
-                }
-            });
-        }
-        else {
-            throw new BuildException("Babel directory isn't found or it doesn't contain eclipse/plugins subfolder.");
-        }
+                    public boolean accept(File dir, String name)
+                    {
+                        return name.endsWith(".jar") && name.contains(nlSuffix);
+                    }
+                });
+            }
+            else {
+                throw new BuildException("Babel directory isn't found or it doesn't contain eclipse/plugins subfolder.");
+            }
 
-        if (babelJars != null && babelJars.length > 0) {
+            if (babelJars != null && babelJars.length > 0) {
 
-            File pluginsDir = new File(eclipseDirectory + "/plugins");
-            if (pluginsDir.exists() && pluginsDir.isDirectory()) {
-                String[] plugins = pluginsDir.list(JARS_FILTER);
-                for (String plugin : plugins) {
-                    File pluginFile = new File(pluginsDir, plugin);
-                    String pluginFileName = pluginFile.getName();
-                    for (String babelJar : babelJars) {
-                        String pluginName = babelJar.substring(0, babelJar.indexOf(nlSuffix) - 1);
-                        if (pluginFileName.startsWith(pluginName) && Character.isDigit(pluginFileName.charAt(pluginName.length() + 1))) {
-                            appendZip(new File(babelDir, babelJar), pluginFile, PROPERTIES_FILTER);
-                            break;
+                File pluginsDir = new File(eclipseDirectory + "/plugins");
+                if (pluginsDir.exists() && pluginsDir.isDirectory()) {
+                    String[] plugins = pluginsDir.list(JARS_FILTER);
+                    for (String plugin : plugins) {
+                        File pluginFile = new File(pluginsDir, plugin);
+                        String pluginFileName = pluginFile.getName();
+                        for (String babelJar : babelJars) {
+                            String pluginName = babelJar.substring(0, babelJar.indexOf(nlSuffix) - 1);
+                            if (pluginFileName.startsWith(pluginName) && Character.isDigit(pluginFileName.charAt(pluginName.length() + 1))) {
+                                if (pluginJarMap.get(pluginFile) == null) {
+                                    pluginJarMap.put(pluginFile, new ArrayList<File>());
+                                }
+                                pluginJarMap.get(pluginFile).add(new File(babelDir, babelJar));
+                                break;
+                            }
                         }
                     }
                 }
+                else {
+                    throw new BuildException("Eclipse plugins directory isn't found.");
+                }
             }
             else {
-                throw new BuildException("Eclipse plugins directory isn't found.");
+                throw new BuildException("No jars found in Babel.");
             }
         }
-        else {
-            throw new BuildException("No jars found in Babel.");
+
+        Set<File> eclipsePlugins = pluginJarMap.keySet();
+        int k = 1;
+        int kplugins = eclipsePlugins.size();
+        for (File pluginFile : eclipsePlugins) {
+            System.out.println("Plugin " + pluginFile.getName() + " [" + k++ + "/" + kplugins + "] is being localized.");
+            localizePlugin(pluginJarMap.get(pluginFile), pluginFile, PROPERTIES_FILTER);
         }
     }
 
-    public static void appendZip(File sourceZipFile, File destZipFile, FilenameFilter filenameFilter)
+    public static void localizePlugin(List<File> babelFiles, File pluginFile, FilenameFilter filenameFilter)
     {
-        if (!sourceZipFile.exists() || !destZipFile.exists()) {
+        if (!pluginFile.exists()) {
             return;
         }
         // get a temp file
@@ -121,16 +135,16 @@ public class BabelRepackerTask extends Task
         // delete it, otherwise you cannot rename your existing zip to it.
         tempFile.delete();
 
-        boolean renameOk = destZipFile.renameTo(tempFile);
+        boolean renameOk = pluginFile.renameTo(tempFile);
         if (!renameOk) {
-            throw new BuildException("could not rename the file " + destZipFile.getAbsolutePath() + " to " + tempFile.getAbsolutePath());
+            throw new BuildException("could not rename the file " + pluginFile.getAbsolutePath() + " to " + tempFile.getAbsolutePath());
         }
 
         ZipOutputStream out = null;
         try {
-            out = new ZipOutputStream(new FileOutputStream(destZipFile));
+            out = new ZipOutputStream(new FileOutputStream(pluginFile));
         } catch (FileNotFoundException e) {
-            System.out.println("Can't create an output stream for destination zip file " + destZipFile.getAbsolutePath());
+            System.out.println("Can't create an output stream for destination zip file " + pluginFile.getAbsolutePath());
             return;
         }
 
@@ -139,7 +153,7 @@ public class BabelRepackerTask extends Task
         try {
             destZip = new ZipFile(tempFile);
         } catch (IOException e) {
-            System.out.println("A problem with processing destination zip file " + destZipFile.getAbsolutePath());
+            System.out.println("A problem with processing destination zip file " + pluginFile.getAbsolutePath());
             return;
         }
         Enumeration<? extends ZipEntry> destEntries = destZip.entries();
@@ -158,37 +172,39 @@ public class BabelRepackerTask extends Task
                 out.closeEntry();
                 bis.close();
             } catch (IOException e) {
-                System.out.println("Can't copy " + entryName + " from temporary file to " + destZipFile.getAbsolutePath());
+                System.out.println("Can't copy " + entryName + " from temporary file to " + pluginFile.getAbsolutePath());
                 break;
             }
         }
 
-        // append source file entries
-        ZipFile sourceZip = null;
-        try {
-            sourceZip = new ZipFile(sourceZipFile);
-        } catch (IOException e) {
-            System.out.println("A problem with processing source zip file " + sourceZipFile.getAbsolutePath());
-        }
-        Enumeration<? extends ZipEntry> srcEntries = sourceZip.entries();
-        while (srcEntries.hasMoreElements()) {
-            ZipEntry entry = (ZipEntry) srcEntries.nextElement();
-            String entryName = entry.getName();
-            if (filenameFilter.accept(null, entryName)) {
-                //System.out.println("entryName = " + entryName);
-                ZipEntry newEntry = new ZipEntry(entryName);
-                try {
-                    out.putNextEntry(newEntry);
-
-                    BufferedInputStream bis = new BufferedInputStream(sourceZip.getInputStream(entry));
-                    while (bis.available() > 0) {
-                        out.write(bis.read());
+        for (File sourceZipFile : babelFiles) {
+            // append source file entries
+            ZipFile sourceZip = null;
+            try {
+                sourceZip = new ZipFile(sourceZipFile);
+            } catch (IOException e) {
+                System.out.println("A problem with processing source zip file " + sourceZipFile.getAbsolutePath());
+            }
+            Enumeration<? extends ZipEntry> srcEntries = sourceZip.entries();
+            while (srcEntries.hasMoreElements()) {
+                ZipEntry entry = (ZipEntry) srcEntries.nextElement();
+                String entryName = entry.getName();
+                if (filenameFilter.accept(null, entryName)) {
+                    //System.out.println("entryName = " + entryName);
+                    ZipEntry newEntry = new ZipEntry(entryName);
+                    try {
+                        out.putNextEntry(newEntry);
+    
+                        BufferedInputStream bis = new BufferedInputStream(sourceZip.getInputStream(entry));
+                        while (bis.available() > 0) {
+                            out.write(bis.read());
+                        }
+                        out.closeEntry();
+                        bis.close();
+                    } catch (IOException e) {
+                        System.out.println("Can't copy " + entryName + " from " + sourceZipFile.getAbsolutePath() + " to " + pluginFile.getAbsolutePath());
+                        break;
                     }
-                    out.closeEntry();
-                    bis.close();
-                } catch (IOException e) {
-                    System.out.println("Can't copy " + entryName + " from " + sourceZipFile.getAbsolutePath() + " to " + destZipFile.getAbsolutePath());
-                    break;
                 }
             }
         }
@@ -197,7 +213,7 @@ public class BabelRepackerTask extends Task
         try {
             out.close();
         } catch (IOException e) {
-            System.out.println("Can't close output stream for destination " + destZipFile.getAbsolutePath());
+            System.out.println("Can't close output stream for destination " + pluginFile.getAbsolutePath());
         }
         tempFile.delete();
     }
