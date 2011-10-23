4871f58c2891c5b6d7ce3b5c29b469c74360d8fa
jurgen <jurgen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2011-10-23 18:32:18 +0000

    Version generator utility
    Former-commit-id: 9562487a05497ddd6c2ffc8a5bcd78248ee273fc

--- tools/version-updater/src/org/jkiss/tools/ant/versiongen/GenerateVersionTask.java
+++ tools/version-updater/src/org/jkiss/tools/ant/versiongen/GenerateVersionTask.java
@@ -2,10 +2,17 @@
  * Copyright (c) 2011, Serge Rieder and others. All Rights Reserved.
  */
 
-package org.jkiss.tools.ant.versiongen;
+package org.jkiss.tools.ant.productman;
 
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
+import org.jkiss.utils.IOUtils;
+import org.jkiss.utils.xml.XMLBuilder;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Generates version descriptor from product descriptor
@@ -16,9 +23,34 @@ public class GenerateVersionTask extends Task
     private String targetDirectory;
     private String productDescriptor;
     private String versionNumber;
+    private String configs;
+    private String locales;
 
     public void execute() throws BuildException
     {
+        File dir = new File(targetDirectory);
+        if (!dir.exists()) {
+            if (!dir.mkdirs()) {
+                throw new BuildException("Can't create target directory '" + dir.getAbsolutePath() + "'");
+            }
+        }
+        File versionFile = new File(dir, "version.xml");
+        try {
+            FileOutputStream out = new FileOutputStream(versionFile);
+            try {
+                XMLBuilder versionXML = new XMLBuilder(out, "UTF-8");
+                generateVersionInfo(versionXML);
+            } finally {
+                IOUtils.close(out);
+            }
+        } catch (IOException e) {
+            throw new BuildException("IO error", e);
+        }
+    }
+
+    private void generateVersionInfo(XMLBuilder xml)
+    {
+
     }
 
     public void setProductDescriptor(String msg)
@@ -34,5 +66,15 @@ public class GenerateVersionTask extends Task
     public void setVersionNumber(String versionNumber)
     {
         this.versionNumber = versionNumber;
+    }
+
+    public void setConfigs(String configs)
+    {
+        this.configs = configs;
+    }
+
+    public void setLocales(String locales)
+    {
+        this.locales = locales;
     }
 }
---
+++ tools/version-updater/src/org/jkiss/tools/ant/versiongen/VMProductDescriptor.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2011, Serge Rieder and others. All Rights Reserved.
+ */
+
+package org.jkiss.tools.ant.productman;
+
+/**
+ * Product descriptor
+ */
+public class VMProductDescriptor {
+}
