a04bbe1f4821bca4f8b2622a216e6aa3bfb408dd
jurgen <jurgen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2011-10-24 18:12:35 +0000

    Babel task fix
    Former-commit-id: e0bf49f3404dd237e660547c1d5b48a068f44292

--- tools/babel-packer/src/org/jkiss/tools/ant/babel/BabelRepackerTask.java
+++ tools/babel-packer/src/org/jkiss/tools/ant/babel/BabelRepackerTask.java
@@ -126,96 +126,94 @@ public class BabelRepackerTask extends Task
             return;
         }
         // get a temp file
-        File tempFile = null;
+        File tempFile = new File(pluginFile.getName() + ".tmp");
+
         try {
-            tempFile = File.createTempFile("tmp", ".zip");
-        } catch (IOException e) {
-            System.out.println("Can't create a temp file.");
-        }
-        // delete it, otherwise you cannot rename your existing zip to it.
-        tempFile.delete();
-
-        boolean renameOk = pluginFile.renameTo(tempFile);
-        if (!renameOk) {
-            throw new BuildException("could not rename the file " + pluginFile.getAbsolutePath() + " to " + tempFile.getAbsolutePath());
-        }
-
-        ZipOutputStream out = null;
-        try {
-            out = new ZipOutputStream(new FileOutputStream(pluginFile));
-        } catch (FileNotFoundException e) {
-            System.out.println("Can't create an output stream for destination zip file " + pluginFile.getAbsolutePath());
-            return;
-        }
-
-        // append dest file own entries
-        ZipFile destZip = null;
-        try {
-            destZip = new ZipFile(tempFile);
-        } catch (IOException e) {
-            System.out.println("A problem with processing destination zip file " + pluginFile.getAbsolutePath());
-            return;
-        }
-        Enumeration<? extends ZipEntry> destEntries = destZip.entries();
-        while (destEntries.hasMoreElements()) {
-            ZipEntry entry = (ZipEntry) destEntries.nextElement();
-            //System.out.println(entry.getName());
-            String entryName = entry.getName();
-            ZipEntry newEntry = new ZipEntry(entryName);
-            try {
-                out.putNextEntry(newEntry);
-
-                BufferedInputStream bis = new BufferedInputStream(destZip.getInputStream(entry));
-                while (bis.available() > 0) {
-                    out.write(bis.read());
-                }
-                out.closeEntry();
-                bis.close();
-            } catch (IOException e) {
-                System.out.println("Can't copy " + entryName + " from temporary file to " + pluginFile.getAbsolutePath());
-                break;
+            boolean renameOk = pluginFile.renameTo(tempFile);
+            if (!renameOk) {
+                throw new BuildException("could not rename the file " + pluginFile.getAbsolutePath() + " to " + tempFile.getAbsolutePath());
             }
-        }
 
-        for (File sourceZipFile : babelFiles) {
-            // append source file entries
-            ZipFile sourceZip = null;
+            ZipOutputStream out;
             try {
-                sourceZip = new ZipFile(sourceZipFile);
-            } catch (IOException e) {
-                System.out.println("A problem with processing source zip file " + sourceZipFile.getAbsolutePath());
+                out = new ZipOutputStream(new FileOutputStream(pluginFile));
+            } catch (FileNotFoundException e) {
+                System.out.println("Can't create an output stream for destination zip file " + pluginFile.getAbsolutePath());
+                return;
             }
-            Enumeration<? extends ZipEntry> srcEntries = sourceZip.entries();
-            while (srcEntries.hasMoreElements()) {
-                ZipEntry entry = (ZipEntry) srcEntries.nextElement();
+
+            // append dest file own entries
+            ZipFile destZip;
+            try {
+                destZip = new ZipFile(tempFile);
+            } catch (IOException e) {
+                System.out.println("A problem with processing destination zip file " + pluginFile.getAbsolutePath());
+                return;
+            }
+            Enumeration<? extends ZipEntry> destEntries = destZip.entries();
+            while (destEntries.hasMoreElements()) {
+                ZipEntry entry = (ZipEntry) destEntries.nextElement();
+                //System.out.println(entry.getName());
                 String entryName = entry.getName();
-                if (filenameFilter.accept(null, entryName)) {
-                    //System.out.println("entryName = " + entryName);
-                    ZipEntry newEntry = new ZipEntry(entryName);
-                    try {
-                        out.putNextEntry(newEntry);
-    
-                        BufferedInputStream bis = new BufferedInputStream(sourceZip.getInputStream(entry));
-                        while (bis.available() > 0) {
-                            out.write(bis.read());
+                ZipEntry newEntry = new ZipEntry(entryName);
+                try {
+                    out.putNextEntry(newEntry);
+
+                    BufferedInputStream bis = new BufferedInputStream(destZip.getInputStream(entry));
+                    while (bis.available() > 0) {
+                        out.write(bis.read());
+                    }
+                    out.closeEntry();
+                    bis.close();
+                } catch (IOException e) {
+                    System.out.println("Can't copy " + entryName + " from temporary file to " + pluginFile.getAbsolutePath());
+                    break;
+                }
+            }
+
+            for (File sourceZipFile : babelFiles) {
+                // append source file entries
+                ZipFile sourceZip = null;
+                try {
+                    sourceZip = new ZipFile(sourceZipFile);
+                } catch (IOException e) {
+                    System.out.println("A problem with processing source zip file " + sourceZipFile.getAbsolutePath());
+                }
+                Enumeration<? extends ZipEntry> srcEntries = sourceZip.entries();
+                while (srcEntries.hasMoreElements()) {
+                    ZipEntry entry = (ZipEntry) srcEntries.nextElement();
+                    String entryName = entry.getName();
+                    if (filenameFilter.accept(null, entryName)) {
+                        //System.out.println("entryName = " + entryName);
+                        ZipEntry newEntry = new ZipEntry(entryName);
+                        try {
+                            out.putNextEntry(newEntry);
+
+                            BufferedInputStream bis = new BufferedInputStream(sourceZip.getInputStream(entry));
+                            while (bis.available() > 0) {
+                                out.write(bis.read());
+                            }
+                            out.closeEntry();
+                            bis.close();
+                        } catch (IOException e) {
+                            System.out.println("Can't copy " + entryName + " from " + sourceZipFile.getAbsolutePath() + " to " + pluginFile.getAbsolutePath());
+                            break;
                         }
-                        out.closeEntry();
-                        bis.close();
-                    } catch (IOException e) {
-                        System.out.println("Can't copy " + entryName + " from " + sourceZipFile.getAbsolutePath() + " to " + pluginFile.getAbsolutePath());
-                        break;
                     }
                 }
             }
-        }
 
-        // Complete the ZIP file
-        try {
-            out.close();
-        } catch (IOException e) {
-            System.out.println("Can't close output stream for destination " + pluginFile.getAbsolutePath());
+            // Complete the ZIP file
+            try {
+                out.close();
+            } catch (IOException e) {
+                System.out.println("Can't close output stream for destination " + pluginFile.getAbsolutePath());
+            }
+        } finally {
+            if (!tempFile.delete()) {
+                System.out.println("Can't delete temp file " + tempFile.getAbsolutePath());
+            }
         }
-        tempFile.delete();
     }
 
     public void setBabelLocation(String msg)
