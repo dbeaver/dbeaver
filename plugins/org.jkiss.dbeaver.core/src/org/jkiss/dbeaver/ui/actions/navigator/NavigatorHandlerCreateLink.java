57e3bc86bfec40abef9b211e04f26ecabcbb7063
jurgen <jurgen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2011-12-22 17:58:32 +0000

    External folders link support
    Former-commit-id: 04d894e8dc341c148ae3e67f0008faa54d79e7b1

---
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/actions/navigator/NavigatorHandlerCreateLink.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2011, Serge Rieder and others. All Rights Reserved.
+ */
+
+package org.jkiss.dbeaver.ui.actions.navigator;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.osgi.util.NLS;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.DirectoryDialog;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.jkiss.dbeaver.DBException;
+import org.jkiss.dbeaver.core.CoreMessages;
+import org.jkiss.dbeaver.model.navigator.DBNResource;
+import org.jkiss.dbeaver.ui.UIUtils;
+import org.jkiss.dbeaver.ui.dialogs.EnterNameDialog;
+import org.jkiss.dbeaver.ui.editors.entity.FolderEditor;
+import org.jkiss.dbeaver.utils.ContentUtils;
+import org.jkiss.utils.CommonUtils;
+
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+
+public class NavigatorHandlerCreateLink extends NavigatorHandlerObjectBase {
+
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        final ISelection selection = HandlerUtil.getCurrentSelection(event);
+
+        if (selection instanceof IStructuredSelection) {
+            final IStructuredSelection structSelection = (IStructuredSelection)selection;
+            Object element = structSelection.getFirstElement();
+            if (!(element instanceof DBNResource)) {
+                return null;
+            }
+            final IResource resource = ((DBNResource) element).getResource();
+            if (resource instanceof IFolder) {
+                final IWorkbenchWindow workbenchWindow = HandlerUtil.getActiveWorkbenchWindow(event);
+                DirectoryDialog dialog = new DirectoryDialog(workbenchWindow.getShell(), SWT.NONE);
+                String folder = dialog.open();
+                if (folder != null) {
+                    createLink(workbenchWindow, (IFolder)resource, folder);
+                }
+            }
+        }
+        return null;
+    }
+
+    private void createLink(IWorkbenchWindow workbenchWindow, IFolder folder, String fsFolder)
+    {
+        final File externalFolder = new File(fsFolder);
+        try {
+            final IFolder linkedFolder = folder.getFolder(externalFolder.getName());
+            workbenchWindow.run(true, true, new IRunnableWithProgress() {
+                public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException
+                {
+                    try {
+                        linkedFolder.createLink(externalFolder.toURI(), IResource.NONE, monitor);
+                    } catch (CoreException e) {
+                        throw new InvocationTargetException(e);
+                    }
+                }
+            });
+        } catch (InvocationTargetException e) {
+            UIUtils.showErrorDialog(workbenchWindow.getShell(), "Create link", "Can't create link", e);
+        } catch (InterruptedException e) {
+            // skip
+        }
+    }
+
+}
