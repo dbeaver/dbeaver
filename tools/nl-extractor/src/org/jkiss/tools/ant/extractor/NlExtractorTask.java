249f4ddb208092a5f2e648332646dd50018dad57
ejf <ejf@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2012-06-13 08:57:41 +0000

    Resources properties files extractor for localization. Locales input list support is implemented.
    Former-commit-id: 30d31b8afe36c7acb028af637c016ceeaa95cd30

--- tools/nl-extractor/src/org/jkiss/tools/ant/extractor/NlExtractorTask.java
+++ tools/nl-extractor/src/org/jkiss/tools/ant/extractor/NlExtractorTask.java
@@ -21,9 +21,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.StringTokenizer;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
@@ -33,22 +35,48 @@ import java.util.zip.ZipOutputStream;
  */
 public class NlExtractorTask extends Task
 {
-    public static final int PROPERTIES_EXT_LENGTH = 11;
-
+    public static final String PROPERTIES_EXT = ".properties";
     public static final FilenameFilter PROPERTIES_FILTER = new FilenameFilter()
     {
         @Override
         public boolean accept(File dir, String name)
         {
-            return name.endsWith(".properties");
+            return name.endsWith(PROPERTIES_EXT);
         }
     };
 
+    /**
+     * The project base folder.
+     */
     private String baseLocation;
+    /**
+     * Extractor target location.
+     */
     private String targetLocation;
+    /**
+     * Required locales (for example, "fr, de, it"). Can be null. 
+     * English locale is always extracted.
+     */
     private String locales;
+
     private File baseDir;
     private File targetDir;
+    private Set<String> localesSet = new HashSet<String>();
+
+    public void setTargetLocation(String msg)
+    {
+        this.targetLocation = msg;
+    }
+
+    public void setBaseLocation(String baseLocation)
+    {
+        this.baseLocation = baseLocation;
+    }
+
+    public void setLocales(String locales)
+    {
+        this.locales = locales;
+    }
 
     // The method executing the task
     @Override
@@ -66,9 +94,15 @@ public class NlExtractorTask extends Task
 */
         System.out.println("baseLocation = " + baseLocation);
         System.out.println("targetLocation = " + targetLocation);
+        System.out.println("locales = " + locales);
 
         baseDir = new File(baseLocation);
         targetDir = new File(targetLocation);
+        StringTokenizer st = new StringTokenizer(locales, ", ");
+        while (st.hasMoreTokens()) {
+            localesSet.add(st.nextToken());
+        }
+        //System.out.println(localesSet);
         try {
             processDirectory (baseDir);
         } catch (IOException e) {
@@ -91,54 +125,61 @@ public class NlExtractorTask extends Task
             else {
                 String filename = file.getName();
                 if (PROPERTIES_FILTER.accept(null, filename)) {
-                    String name = (file.getName()).substring(0, filename.length() - PROPERTIES_EXT_LENGTH);
-                    if (new File(dir, name + "_ru.properties").exists()) {
+                    String basePropertiesName = (file.getName()).substring(0, filename.length() - PROPERTIES_EXT.length());
+                    if (new File(dir, basePropertiesName + "_ru.properties").exists()) { // russian localization exists
                         String relativePath = baseDir.toURI().relativize(file.toURI()).toString();
                         //System.out.println(relativePath);
                         String relativeDir = relativePath.substring(0, relativePath.lastIndexOf("/"));
                         //System.out.println(relativeDir);
                         File target = new File(targetDir, relativeDir);
                         target.mkdirs();
-                        for (File properties : dir.listFiles(PROPERTIES_FILTER)) {
-                            if (properties.getName().startsWith(name)) {
-                                //System.out.println(name);
-                                copyFile(properties, target);
-                            }
+                        for (File propertiesFile : dir.listFiles(new LocalPropertiesFilenameFilter(basePropertiesName))) {
+                            copyFile(propertiesFile, target);
                         }
                     }
                 }
             }
         }
-
     }
 
-    private static void copyFile(File source , File target) throws IOException
+    private static void copyFile(File source, File target) throws IOException
     {
-            InputStream in = new FileInputStream(source);
-            OutputStream out = new FileOutputStream(target+"/"+source.getName());
+        InputStream in = new FileInputStream(source);
+        OutputStream out = new FileOutputStream(target + "/" + source.getName());
 
-            // Copy the bits from input stream to output stream
-            byte[] buf = new byte[1024];
-            int len;
-            while ((len = in.read(buf)) > 0) {
-                out.write(buf, 0, len);
+        // Copy the bits from input stream to output stream
+        byte[] buf = new byte[1024];
+        int len;
+        while ((len = in.read(buf)) > 0) {
+            out.write(buf, 0, len);
+        }
+        in.close();
+        out.close();
+    }
+
+    private class LocalPropertiesFilenameFilter implements FilenameFilter
+    {
+        private String basePropertiesName;
+
+        private LocalPropertiesFilenameFilter(String basePropertiesName)
+        {
+            this.basePropertiesName = basePropertiesName;
+        }
+
+        @Override
+        public boolean accept(File dir, String name)
+        {
+            return name.equalsIgnoreCase(basePropertiesName + PROPERTIES_EXT) ||
+                    (name.startsWith(basePropertiesName) && isPropertiesAccepted(name));
+        }
+
+        private boolean isPropertiesAccepted(String fileName) {
+            for (String locale : localesSet) {
+                if (fileName.contains("_" + locale)) {
+                    return true;
+                }
             }
-            in.close();
-            out.close();
-      }
-
-    public void setTargetLocation(String msg)
-    {
-        this.targetLocation = msg;
-    }
-
-    public void setBaseLocation(String baseLocation)
-    {
-        this.baseLocation = baseLocation;
-    }
-
-    public void setLocales(String locales)
-    {
-        this.locales = locales;
+            return false;
+        }
     }
 }
