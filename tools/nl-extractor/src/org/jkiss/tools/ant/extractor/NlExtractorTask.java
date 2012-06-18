1672e710fe4e7b58d43fa673931a9bef90b1d69e
ejf <ejf@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2012-06-18 12:33:15 +0000

    Resources properties files extractor for localization. Encoding support.
    Former-commit-id: 6c92d6fdaed641589de8a9b7900cc9edec34fd2c

--- tools/nl-extractor/src/org/jkiss/tools/ant/extractor/NlExtractorTask.java
+++ tools/nl-extractor/src/org/jkiss/tools/ant/extractor/NlExtractorTask.java
@@ -8,6 +8,8 @@ import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -15,7 +17,9 @@ import java.io.FileOutputStream;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -58,6 +62,7 @@ public class NlExtractorTask extends Task
      * English locale is always extracted.
      */
     private String locales;
+    private String encoding;
 
     private File baseDir;
     private File targetDir;
@@ -76,6 +81,11 @@ public class NlExtractorTask extends Task
     public void setLocales(String locales)
     {
         this.locales = locales;
+    }
+
+    public void setEncoding(String encoding)
+    {
+        this.encoding = encoding;
     }
 
     // The method executing the task
@@ -131,10 +141,10 @@ public class NlExtractorTask extends Task
                         //System.out.println(relativePath);
                         String relativeDir = relativePath.substring(0, relativePath.lastIndexOf("/"));
                         //System.out.println(relativeDir);
-                        File target = new File(targetDir, relativeDir);
-                        target.mkdirs();
+                        File targetBase = new File(targetDir, relativeDir);
+                        targetBase.mkdirs();
                         for (File propertiesFile : dir.listFiles(new LocalPropertiesFilenameFilter(basePropertiesName))) {
-                            copyFile(propertiesFile, target);
+                            copyFile(propertiesFile, new File(targetBase, propertiesFile.getName()), encoding);
                         }
                     }
                 }
@@ -142,19 +152,90 @@ public class NlExtractorTask extends Task
         }
     }
 
-    private static void copyFile(File source, File target) throws IOException
+    private static void copyFile(File source, File target, String encoding) throws IOException
     {
-        InputStream in = new FileInputStream(source);
-        OutputStream out = new FileOutputStream(target + "/" + source.getName());
-
-        // Copy the bits from input stream to output stream
-        byte[] buf = new byte[1024];
-        int len;
-        while ((len = in.read(buf)) > 0) {
-            out.write(buf, 0, len);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(source)));
+        OutputStreamWriter out;
+        boolean doEncode = encoding != null && encoding.length() > 0;
+        if (doEncode) {
+            out = new OutputStreamWriter(new FileOutputStream(target), encoding);
         }
-        in.close();
-        out.close();
+        else {
+            out = new OutputStreamWriter(new FileOutputStream(target));
+        }
+        BufferedWriter writer = new BufferedWriter(out);
+        String line = null;
+        char[] convtBuf = new char[1024];
+        while ((line=reader.readLine()) != null) {
+            if (doEncode) {
+                line = loadConvert(line.toCharArray(), 0, line.length(), convtBuf);
+            }
+            writer.write(line);
+            writer.newLine();   // Write system dependent end of line.
+        }
+        reader.close();  // Close to unlock.
+        writer.close();  // Close to unlock and flush to disk.
+    }
+
+    /**
+     * Converts encoded &#92;uxxxx to unicode chars
+     * and changes special saved chars to their original forms
+     * 
+     * Got from the java.util.Properties class.
+     */
+    private static String loadConvert (char[] in, int off, int len, char[] convtBuf) {
+        if (convtBuf.length < len) {
+            int newLen = len * 2;
+            if (newLen < 0) {
+                newLen = Integer.MAX_VALUE;
+            }
+            convtBuf = new char[newLen];
+        }
+        char aChar;
+        char[] out = convtBuf;
+        int outLen = 0;
+        int end = off + len;
+
+        while (off < end) {
+            aChar = in[off++];
+            if (aChar == '\\') {
+                aChar = in[off++];
+                if(aChar == 'u') {
+                    // Read the xxxx
+                    int value=0;
+                    for (int i=0; i<4; i++) {
+                        aChar = in[off++];
+                        switch (aChar) {
+                            case '0': case '1': case '2': case '3': case '4':
+                            case '5': case '6': case '7': case '8': case '9':
+                                value = (value << 4) + aChar - '0';
+                                break;
+                            case 'a': case 'b': case 'c':
+                            case 'd': case 'e': case 'f':
+                                value = (value << 4) + 10 + aChar - 'a';
+                                break;
+                            case 'A': case 'B': case 'C':
+                            case 'D': case 'E': case 'F':
+                                value = (value << 4) + 10 + aChar - 'A';
+                                break;
+                            default:
+                                throw new IllegalArgumentException(
+                                        "Malformed \\uxxxx encoding.");
+                        }
+                    }
+                    out[outLen++] = (char)value;
+                } else {
+                    if (aChar == 't') aChar = '\t';
+                    else if (aChar == 'r') aChar = '\r';
+                    else if (aChar == 'n') aChar = '\n';
+                    else if (aChar == 'f') aChar = '\f';
+                    out[outLen++] = aChar;
+                }
+            } else {
+                out[outLen++] = (char)aChar;
+            }
+        }
+        return new String (out, 0, outLen);
     }
 
     private class LocalPropertiesFilenameFilter implements FilenameFilter
