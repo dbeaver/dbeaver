13bfc075fa9ec07b854bb313001a84511e4b9639
jurgen <jurgen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2012-10-17 20:17:25 +0000

    Plugin configuration
    Former-commit-id: 830e967e5d9720defae4f5f7d7b239534432cf36

---
+++ branches/juno-migration/plugins/org.jkiss.dbeaver.core.eclipse/src/org/jkiss/dbeaver/core/eclipse/preferences/EPPDataFormat.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2010-2012 Serge Rieder serge@jkiss.org
+ * Copyright (C) 2011-2012 Eugene Fradkin eugene.fradkin@gmail.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.jkiss.dbeaver.core.eclipse.preferences;
+
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.*;
+import org.jkiss.dbeaver.core.CoreMessages;
+import org.jkiss.dbeaver.core.DBeaverCore;
+import org.jkiss.dbeaver.model.data.DBDDataFormatter;
+import org.jkiss.dbeaver.model.data.DBDDataFormatterProfile;
+import org.jkiss.dbeaver.registry.DataFormatterDescriptor;
+import org.jkiss.dbeaver.registry.DataFormatterRegistry;
+import org.jkiss.dbeaver.registry.DataSourceDescriptor;
+import org.jkiss.dbeaver.runtime.RuntimeUtils;
+import org.jkiss.dbeaver.ui.UIUtils;
+import org.jkiss.dbeaver.ui.controls.LocaleSelectorControl;
+import org.jkiss.dbeaver.ui.dialogs.DataFormatProfilesEditDialog;
+import org.jkiss.dbeaver.ui.preferences.PrefPageDataFormat;
+import org.jkiss.dbeaver.ui.preferences.TargetPrefPage;
+import org.jkiss.dbeaver.ui.properties.PropertySourceCustom;
+import org.jkiss.dbeaver.ui.properties.PropertyTreeViewer;
+
+import java.util.*;
+import java.util.List;
+
+/**
+ * EPPDataFormat
+ */
+public class EPPDataFormat extends PrefPageDataFormat
+{
+
+}
