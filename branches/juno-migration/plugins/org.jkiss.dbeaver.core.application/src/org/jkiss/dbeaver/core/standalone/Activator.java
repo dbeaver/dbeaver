c14d4ee42c2dc5581d762ed4ffa35561a24ca946
jurgen <jurgen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2012-10-17 06:09:31 +0000

    Application plugin
    Former-commit-id: 00118d4577437cebb3f19c6ae4373c8ea003d4ec

---
+++ branches/juno-migration/plugins/org.jkiss.dbeaver.core.application/src/org/jkiss/dbeaver/core/standalone/Activator.java
@@ -0,0 +1,30 @@
+package org.jkiss.dbeaver.core.standalone;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	}
+
+}
