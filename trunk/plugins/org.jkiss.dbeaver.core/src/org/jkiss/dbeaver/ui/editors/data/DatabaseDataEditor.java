daa04b8cad97f008b0f470c54ede7696d886e574
jurgen <jurgen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2012-10-30 14:16:51 +0000

    RSV toolbar handlers
    Former-commit-id: 12dc41adfaa27fc1ee463843d1a0d4fab4ee2d0c

--- trunk/plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/editors/data/DatabaseDataEditor.java
+++ trunk/plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/editors/data/DatabaseDataEditor.java
@@ -19,7 +19,6 @@
 package org.jkiss.dbeaver.ui.editors.data;
 
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IEditorPart;
 import org.jkiss.dbeaver.model.struct.DBSDataContainer;
@@ -53,6 +52,7 @@ public class DatabaseDataEditor extends AbstractDatabaseObjectEditor<DBSDataCont
             resultSetView = new ResultSetViewer(parent, getSite(), this);
             resultSetView.addListener(this);
             parent.layout();
+            resultSetView.getControl().setFocus();
         }
 
         if (!loaded) {
