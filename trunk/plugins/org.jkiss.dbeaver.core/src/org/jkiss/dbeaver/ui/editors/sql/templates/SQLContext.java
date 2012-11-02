6227528da675eb3c4755d4d425bcbc89239fd07a
jurgen <jurgen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2012-11-02 14:46:27 +0000

    Templates support
    Former-commit-id: 67a4d766e5c7290718df80656c6de9dd247e3f70

--- trunk/plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/editors/sql/templates/SQLContext.java
+++ trunk/plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/editors/sql/templates/SQLContext.java
@@ -4,21 +4,31 @@ import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.Position;
 import org.eclipse.jface.text.templates.DocumentTemplateContext;
 import org.eclipse.jface.text.templates.TemplateContextType;
+import org.jkiss.dbeaver.ext.IDataSourceProvider;
+import org.jkiss.dbeaver.model.DBPDataSource;
 import org.jkiss.dbeaver.ui.editors.sql.SQLEditorBase;
 
 /**
  * SQL context
  */
-public class SQLContext extends DocumentTemplateContext {
+public class SQLContext extends DocumentTemplateContext implements IDataSourceProvider {
 
-    public SQLContext(TemplateContextType type, IDocument document, int offset, int length)
-    {
-        super(type, document, offset, length);
-    }
+    private SQLEditorBase editor;
 
     public SQLContext(TemplateContextType type, IDocument document, Position position, SQLEditorBase editor)
     {
         super(type, document, position);
+        this.editor = editor;
     }
 
+    public SQLEditorBase getEditor()
+    {
+        return editor;
+    }
+
+    @Override
+    public DBPDataSource getDataSource()
+    {
+        return editor.getDataSource();
+    }
 }
