1e2275c3d1e4efe6aac134a405537e2577fc7ce2
jurgen <jurgen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2012-11-04 21:29:41 +0000

    SQL templates contexts
    Former-commit-id: ddfa55973972d8fe41e90558de433351e288efc9

---
+++ trunk/plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/editors/sql/templates/SQLContextTypeBase.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Sebastian Davids: sdavids@gmx.de - see bug 25376
+ *******************************************************************************/
+package org.jkiss.dbeaver.ui.editors.sql.templates;
+
+
+public class SQLContextTypeBase extends SQLContextTypeAbstract {
+
+	public static final String ID_SQL= "sql"; //$NON-NLS-1$
+
+    public SQLContextTypeBase()
+    {
+        super(ID_SQL, "SQL");
+    }
+
+}
