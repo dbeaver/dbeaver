728a4712f67971e5699bf26f211447d4d72e6809
jurgen <jurgen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2012-11-13 13:44:31 +0000

    Help UI
    Former-commit-id: ca6f45ed2efef095f253a8cb9634d721a0c2dbc9

--- trunk/plugins/org.jkiss.dbeaver.core.application/src/org/jkiss/dbeaver/ui/help/LightweightHelpUI.java
+++ trunk/plugins/org.jkiss.dbeaver.core.application/src/org/jkiss/dbeaver/ui/help/LightweightHelpUI.java
@@ -1,12 +1,31 @@
 package org.jkiss.dbeaver.ui.help;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.help.IContext;
+import org.eclipse.help.IHelpResource;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.browser.IWebBrowser;
+import org.eclipse.ui.browser.IWorkbenchBrowserSupport;
 import org.eclipse.ui.help.AbstractHelpUI;
+import org.jkiss.dbeaver.core.DBeaverActivator;
+import org.jkiss.dbeaver.core.DBeaverCore;
+import org.jkiss.dbeaver.core.application.ApplicationWorkbenchWindowAdvisor;
+import org.jkiss.utils.CommonUtils;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Lightweight help UI
  */
 public class LightweightHelpUI extends AbstractHelpUI {
+
+    static final Log log = LogFactory.getLog(LightweightHelpUI.class);
+
     @Override
     public void displayHelp()
     {
@@ -15,6 +34,25 @@ public class LightweightHelpUI extends AbstractHelpUI {
     @Override
     public void displayContext(IContext context, int x, int y)
     {
+        try {
+            IHelpResource[] relatedTopics = context.getRelatedTopics();
+            if (CommonUtils.isEmpty(relatedTopics)) {
+                return;
+            }
+            IHelpResource relatedTopic = relatedTopics[0];
+            String topicRef = relatedTopic.getHref();
+            //URL entry = DBeaverActivator.getInstance().getBundle().getEntry(topicRef);
+            URL platformURL = FileLocator.find(new URL("platform:/plugin" + topicRef));
+            URL fileURL = FileLocator.toFileURL(platformURL);
+            getExternalBrowser().openURL(fileURL);
+
+        } catch (PartInitException e) {
+            log.error(e);
+        } catch (MalformedURLException e) {
+            log.error(e);
+        } catch (IOException e) {
+            log.error(e);
+        }
     }
 
     @Override
@@ -27,4 +65,11 @@ public class LightweightHelpUI extends AbstractHelpUI {
     {
         return false;
     }
+
+    private IWebBrowser getExternalBrowser() throws PartInitException
+    {
+        IWorkbenchBrowserSupport support = PlatformUI.getWorkbench().getBrowserSupport();
+        return support.getExternalBrowser();
+    }
+
 }
