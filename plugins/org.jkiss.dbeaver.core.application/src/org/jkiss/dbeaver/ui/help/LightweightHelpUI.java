38b4e1009af7f6b847ce8d5f8b8c359b242a622e
jurgen <serge-rider@users.noreply.github.com>
2012-11-14 08:56:48 +0000

    Help UI
    Former-commit-id: 92fd50b091ac4f582012a4d87d81a5ba04f0bbb9

--- plugins/org.jkiss.dbeaver.core.application/src/org/jkiss/dbeaver/ui/help/LightweightHelpUI.java
+++ plugins/org.jkiss.dbeaver.core.application/src/org/jkiss/dbeaver/ui/help/LightweightHelpUI.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2010-2012 Serge Rieder
+ * serge@jkiss.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package org.jkiss.dbeaver.ui.help;
 
 import org.apache.commons.logging.Log;
@@ -9,9 +27,9 @@ import org.eclipse.help.IHelpResource;
 import org.eclipse.help.internal.toc.HrefUtil;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.browser.IWebBrowser;
 import org.eclipse.ui.browser.IWorkbenchBrowserSupport;
 import org.eclipse.ui.help.AbstractHelpUI;
+import org.jkiss.dbeaver.ui.IActionConstants;
 import org.jkiss.utils.CommonUtils;
 import org.osgi.framework.Bundle;
 
@@ -25,6 +43,8 @@ import java.util.Enumeration;
 public class LightweightHelpUI extends AbstractHelpUI {
 
     static final Log log = LogFactory.getLog(LightweightHelpUI.class);
+
+    private boolean useHelpView = true;
 
     @Override
     public void displayHelp()
@@ -57,11 +77,24 @@ public class LightweightHelpUI extends AbstractHelpUI {
             URL bundleURL = plugin.getEntry(topicPath);
             if (bundleURL != null) {
                 URL fileURL = FileLocator.toFileURL(bundleURL);
-                getExternalBrowser().openURL(fileURL);
+                showHelpPage(fileURL);
             }
 
         } catch (Exception e) {
             log.error(e);
+        }
+    }
+
+    private void showHelpPage(URL fileURL) throws PartInitException
+    {
+        IWorkbenchBrowserSupport support = PlatformUI.getWorkbench().getBrowserSupport();
+        useHelpView = support.isInternalWebBrowserAvailable();
+
+        if (useHelpView) {
+            LightweightHelpView helpView = (LightweightHelpView)PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView(IActionConstants.HELP_VIEW_ID);
+            helpView.getBrowser().setUrl(fileURL.toString());
+        } else {
+            support.getExternalBrowser().openURL(fileURL);
         }
     }
 
@@ -90,12 +123,6 @@ public class LightweightHelpUI extends AbstractHelpUI {
     public boolean isContextHelpDisplayed()
     {
         return false;
-    }
-
-    private IWebBrowser getExternalBrowser() throws PartInitException
-    {
-        IWorkbenchBrowserSupport support = PlatformUI.getWorkbench().getBrowserSupport();
-        return support.getExternalBrowser();
     }
 
 }
---
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/views/BaseBrowserView.java
@@ -0,0 +1,226 @@
+/*
+ * Copyright (C) 2010-2012 Serge Rieder
+ * serge@jkiss.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package org.jkiss.dbeaver.ui.views;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IStatusLineManager;
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.browser.*;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.*;
+import org.eclipse.ui.part.ViewPart;
+import org.jkiss.dbeaver.ui.DBIcon;
+
+
+/**
+ * Base Browser view.
+ */
+public class BaseBrowserView extends ViewPart {
+
+    static final Log log = LogFactory.getLog(BaseBrowserView.class);
+
+    public static final String MEMENTO_URL = "url"; //$NON-NLS-1$
+
+    private Browser browser;
+    private String initialUrl;
+
+    private Action backAction = new Action("Back", DBIcon.ARROW_LEFT.getImageDescriptor()) {
+        @Override
+        public void run() {
+            browser.back();
+        }
+    };
+
+    private Action forwardAction = new Action("Forward", DBIcon.ARROW_RIGHT.getImageDescriptor()) {
+        @Override
+        public void run() {
+            browser.forward();
+        }
+    };
+
+    private Action stopAction = new Action("Stop", DBIcon.REJECT.getImageDescriptor()) {
+        @Override
+        public void run() {
+            browser.stop();
+        }
+    };
+
+    private Action refreshAction = new Action("Refresh", DBIcon.REFRESH.getImageDescriptor()) {
+        @Override
+        public void run() {
+            browser.refresh();
+        }
+    };
+
+    /**
+     * Constructs a new <code>BaseBrowserView</code>.
+     */
+    public BaseBrowserView() {
+        initialUrl = "about:blank";
+    }
+
+    @Override
+    public void init(IViewSite site, IMemento memento) throws PartInitException {
+        super.init(site);
+        if (memento != null) {
+            String u = memento.getString(MEMENTO_URL);
+            if (u != null) {
+                initialUrl = u;
+            }
+        }
+    }
+
+    @Override
+    public void saveState(IMemento memento) {
+        memento.putString(MEMENTO_URL, browser.getUrl());
+    }
+
+    @Override
+    public void createPartControl(Composite parent) {
+        browser = createBrowser(parent, getViewSite().getActionBars());
+        browser.setUrl(initialUrl);
+    }
+
+    @Override
+    public void setFocus() {
+        if (browser != null && !browser.isDisposed()) {
+            browser.setFocus();
+        }
+    }
+
+    private Browser createBrowser(Composite parent, final IActionBars actionBars) {
+
+        GridLayout gridLayout = new GridLayout();
+        gridLayout.numColumns = 1;
+        parent.setLayout(gridLayout);
+
+        browser = new Browser(parent, SWT.NONE);
+        GridData data = new GridData();
+        data.horizontalAlignment = GridData.FILL;
+        data.verticalAlignment = GridData.FILL;
+        data.grabExcessHorizontalSpace = true;
+        data.grabExcessVerticalSpace = true;
+        browser.setLayoutData(data);
+
+        browser.addProgressListener(new ProgressAdapter() {
+            IProgressMonitor monitor = actionBars.getStatusLineManager().getProgressMonitor();
+            boolean working = false;
+            int workedSoFar;
+            @Override
+            public void changed(ProgressEvent event) {
+                if (event.total == 0) return;
+                if (!working) {
+                    if (event.current == event.total) return;
+                    monitor.beginTask("", event.total); //$NON-NLS-1$
+                    workedSoFar = 0;
+                    working = true;
+                }
+                monitor.worked(event.current - workedSoFar);
+                workedSoFar = event.current;
+            }
+            @Override
+            public void completed(ProgressEvent event) {
+                monitor.done();
+                working = false;
+            }
+        });
+        browser.addStatusTextListener(new StatusTextListener() {
+            IStatusLineManager status = actionBars.getStatusLineManager();
+            @Override
+            public void changed(StatusTextEvent event) {
+                status.setMessage(event.text);
+            }
+        });
+        browser.addLocationListener(new LocationAdapter() {
+            @Override
+            public void changed(LocationEvent event) {
+                backAction.setEnabled(browser.isBackEnabled());
+                forwardAction.setEnabled(browser.isForwardEnabled());
+//                if (event.top)
+//                    location.setText(event.location);
+            }
+        });
+        browser.addTitleListener(new TitleListener() {
+            @Override
+            public void changed(TitleEvent event) {
+                setPartName(event.title);
+            }
+        });
+/*
+        browser.addOpenWindowListener(new OpenWindowListener() {
+            public void open(WindowEvent event) {
+                BaseBrowserView.this.openWindow(event);
+            }
+        });
+*/
+        // TODO: should handle VisibilityWindowListener.show and .hide events
+        browser.addCloseWindowListener(new CloseWindowListener() {
+            @Override
+            public void close(WindowEvent event) {
+                BaseBrowserView.this.close();
+            }
+        });
+/*
+        location.addSelectionListener(new SelectionAdapter() {
+            public void widgetDefaultSelected(SelectionEvent e) {
+                browser.setUrl(location.getText());
+            }
+        });
+*/
+
+        // Hook the navigation actons as handlers for the retargetable actions
+        // defined in BrowserActionBuilder.
+        actionBars.setGlobalActionHandler("back", backAction); //$NON-NLS-1$
+        actionBars.setGlobalActionHandler("forward", forwardAction); //$NON-NLS-1$
+        actionBars.setGlobalActionHandler("stop", stopAction); //$NON-NLS-1$
+        actionBars.setGlobalActionHandler("refresh", refreshAction); //$NON-NLS-1$
+
+        IToolBarManager toolBarManager = actionBars.getToolBarManager();
+        toolBarManager.add(backAction);
+        toolBarManager.add(forwardAction);
+        toolBarManager.add(stopAction);
+        toolBarManager.add(refreshAction);
+
+        backAction.setEnabled(false);
+        forwardAction.setEnabled(false);
+
+        return browser;
+    }
+
+    /**
+     * Closes this browser view.
+     */
+    private void close() {
+        IWorkbenchPage page = getSite().getPage();
+        IWorkbenchWindow window = page.getWorkbenchWindow();
+        page.hideView(this);
+    }
+
+    public Browser getBrowser()
+    {
+        return browser;
+    }
+}
