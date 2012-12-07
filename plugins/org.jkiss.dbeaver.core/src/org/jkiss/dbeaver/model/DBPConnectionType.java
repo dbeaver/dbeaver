6844df26d9c5d6df45f891e3a3774a4b692a356c
jurgen <serge-rider@users.noreply.github.com>
2012-12-07 10:37:27 +0000

    Connection types support + ds toolbar changes reflect fix

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/model/DBPConnectionType.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/model/DBPConnectionType.java
@@ -10,8 +10,8 @@ import org.jkiss.dbeaver.core.DBeaverCore;
 public class DBPConnectionType {
 
     public static final DBPConnectionType DEV = new DBPConnectionType("dev", "Development", new RGB(0xFF, 0xFF, 0xFF), "Regular development database");
-    public static final DBPConnectionType TEST = new DBPConnectionType("test", "Test", new RGB(0xFF, 0xE4, 0xB5), "Test (QA) database");
-    public static final DBPConnectionType PROD = new DBPConnectionType("prod", "Production", new RGB(0xC4, 0xFF, 0xB5), "Production database");
+    public static final DBPConnectionType TEST = new DBPConnectionType("test", "Test", new RGB(0xC4, 0xFF, 0xB5), "Test (QA) database");
+    public static final DBPConnectionType PROD = new DBPConnectionType("prod", "Production", new RGB(0xFF, 0xE4, 0xB5), "Production database");
 
     public static final DBPConnectionType[] SYSTEM_TYPES = { DEV, TEST, PROD };
     public static final DBPConnectionType DEFAULT_TYPE = DEV;
@@ -19,14 +19,12 @@ public class DBPConnectionType {
     private String id;
     private String name;
     private Color color;
-    private RGB rgb;
     private String description;
 
     public DBPConnectionType(String id, String name, RGB rgb, String description)
     {
         this.id = id;
         this.name = name;
-        this.rgb = rgb;
         this.color = DBeaverCore.getInstance().getSharedTextColors().getColor(rgb);
         this.description = description;
     }
@@ -54,7 +52,6 @@ public class DBPConnectionType {
     public void setColor(Color color)
     {
         this.color = color;
-        this.rgb = color.getRGB();
     }
 
     public String getDescription()
