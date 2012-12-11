5a018772fe8aeedfd25bf78dea5e2163b6eaf643
jurgen <serge-rider@users.noreply.github.com>
2012-12-11 08:53:25 +0000

    NPE fix on diagram creation
    Former-commit-id: e654c5e17980f24e61ec7bf6488f63a1a455fae6

--- plugins/org.jkiss.dbeaver.erd/src/org/jkiss/dbeaver/ext/erd/navigator/CreateDiagramHandler.java
+++ plugins/org.jkiss.dbeaver.erd/src/org/jkiss/dbeaver/ext/erd/navigator/CreateDiagramHandler.java
@@ -18,36 +18,38 @@
  */
 package org.jkiss.dbeaver.ext.erd.navigator;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.jkiss.dbeaver.core.DBeaverCore;
 import org.jkiss.dbeaver.model.navigator.DBNResource;
+import org.jkiss.dbeaver.ui.UIUtils;
 import org.jkiss.dbeaver.ui.actions.DataSourceHandler;
 import org.jkiss.dbeaver.ui.dialogs.ActiveWizardDialog;
 
 public class CreateDiagramHandler extends DataSourceHandler {
 
-    static final Log log = LogFactory.getLog(CreateDiagramHandler.class);
-
     @Override
     public Object execute(ExecutionEvent event) throws ExecutionException
     {
         final ISelection selection = HandlerUtil.getCurrentSelection(event);
-        IFolder scriptFolder = null;
+        IFolder diagramFolder = null;
         if (selection != null && !selection.isEmpty() && selection instanceof IStructuredSelection) {
             final Object element = ((IStructuredSelection) selection).getFirstElement();
             if (element instanceof DBNResource && ((DBNResource)element).getResource() instanceof IFolder) {
-                scriptFolder = (IFolder) ((DBNResource)element).getResource();
+                diagramFolder = (IFolder) ((DBNResource)element).getResource();
             }
+        }
+        if (diagramFolder == null && DBeaverCore.getInstance().getProjectRegistry().getActiveProject() == null) {
+            UIUtils.showErrorDialog(HandlerUtil.getActiveShell(event), "ERD Error", "Can't create diagram without active project");
+            return null;
         }
         ActiveWizardDialog dialog = new ActiveWizardDialog(
             HandlerUtil.getActiveWorkbenchWindow(event),
-            new DiagramCreateWizard(scriptFolder));
+            new DiagramCreateWizard(diagramFolder));
         dialog.open();
 
         return null;
