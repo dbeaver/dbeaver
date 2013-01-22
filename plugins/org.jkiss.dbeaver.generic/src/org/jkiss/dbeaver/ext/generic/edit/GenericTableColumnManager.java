ac7b22040209e903baad93f264ef177a52be6250
jurgen <serge-rider@users.noreply.github.com>
2013-01-22 14:06:37 +0000

    Complex type editors

--- plugins/org.jkiss.dbeaver.generic/src/org/jkiss/dbeaver/ext/generic/edit/GenericTableColumnManager.java
+++ plugins/org.jkiss.dbeaver.generic/src/org/jkiss/dbeaver/ext/generic/edit/GenericTableColumnManager.java
@@ -54,7 +54,7 @@ public class GenericTableColumnManager extends JDBCTableColumnManager<GenericTab
         column.setName(DBObjectNameCaseTransformer.transformName(column, getNewColumnName(context, parent)));
         column.setTypeName(columnType == null ? "INTEGER" : columnType.getName());
         column.setMaxLength(columnType != null && columnType.getDataKind() == DBSDataKind.STRING ? 100 : 0);
-        column.setValueType(columnType == null ? Types.INTEGER : columnType.getValueType());
+        column.setValueType(columnType == null ? Types.INTEGER : columnType.getTypeID());
         column.setOrdinalPosition(-1);
         return column;
     }
