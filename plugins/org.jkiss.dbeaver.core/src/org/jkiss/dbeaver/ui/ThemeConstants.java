c14e21ebaf6051df26d6241780a311ecdc117fd2
jurgen <serge-rider@users.noreply.github.com>
2013-01-23 13:23:59 +0000

    RSV preview background config
    Former-commit-id: 098227b8c73298d4e80bfccbd8e74425a7634f96

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/ThemeConstants.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/ThemeConstants.java
@@ -27,4 +27,6 @@ public class ThemeConstants
     public static final String FONT_SQL_RESULT_SET = "org.jkiss.dbeaver.sql.resultset.font"; //$NON-NLS-1$
     public static final String COLOR_SQL_RESULT_SET_SELECTION_FORE = "org.jkiss.dbeaver.sql.resultset.color.selection.foreground"; //$NON-NLS-1$
     public static final String COLOR_SQL_RESULT_SET_SELECTION_BACK = "org.jkiss.dbeaver.sql.resultset.color.selection.background"; //$NON-NLS-1$
+
+    public static final String COLOR_SQL_RESULT_SET_PREVIEW_BACK = "org.jkiss.dbeaver.sql.resultset.color.preview.background"; //$NON-NLS-1$
 }
