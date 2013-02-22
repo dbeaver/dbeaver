93a709160a58329554fcd4a44b966b7669877673
jurgen <serge-rider@users.noreply.github.com>
2013-02-22 10:48:09 +0000

    Update site fixes UIUtils fix for plugin mode

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/UIUtils.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/UIUtils.java
@@ -1084,7 +1084,7 @@ public class UIUtils {
 
     public static ISelectionProvider getSelectionProvider(IServiceLocator serviceLocator)
     {
-        ISelectionProvider selectionProvider = (ISelectionProvider) serviceLocator.getService(IWorkbenchPartSite.class);
+        ISelectionProvider selectionProvider = (ISelectionProvider) serviceLocator.getService(ISelectionProvider.class);
         if (selectionProvider != null) {
             return selectionProvider;
         }
