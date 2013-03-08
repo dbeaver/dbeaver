fcbf3909aaad69a8d114918d1d7795220b82a628
jurgen <serge-rider@users.noreply.github.com>
2013-03-08 10:10:01 +0000

    Data transfer wizard
    Former-commit-id: 2b538b41c5b026315c4cbc0f55aafc2715585a78

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/tools/transfer/database/DatabaseConsumerPageMapping.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/tools/transfer/database/DatabaseConsumerPageMapping.java
@@ -18,14 +18,26 @@
  */
 package org.jkiss.dbeaver.tools.transfer.database;
 
+import org.eclipse.jface.viewers.CellLabelProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.jface.viewers.ViewerCell;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.jkiss.dbeaver.core.CoreMessages;
 import org.jkiss.dbeaver.tools.transfer.wizard.DataTransferWizard;
+import org.jkiss.dbeaver.ui.DBIcon;
+import org.jkiss.dbeaver.ui.UIUtils;
 import org.jkiss.dbeaver.ui.dialogs.ActiveWizardPage;
 
 public class DatabaseConsumerPageMapping extends ActiveWizardPage<DataTransferWizard> {
+
+    private TableViewer mappingViewer;
 
     public DatabaseConsumerPageMapping() {
         super("Entities mapping");
@@ -45,6 +57,81 @@ public class DatabaseConsumerPageMapping extends ActiveWizardPage<DataTransferWi
         composite.setLayout(gl);
         composite.setLayoutData(new GridData(GridData.FILL_BOTH));
 
+        {
+            // Target container
+            Composite containerPanel = new Composite(composite, SWT.NONE);
+            containerPanel.setLayout(new GridLayout(4, false));
+            containerPanel.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+            UIUtils.createControlLabel(containerPanel, "Target container");
+
+            Label containerIcon = new Label(containerPanel, SWT.NONE);
+            containerIcon.setImage(DBIcon.TYPE_UNKNOWN.getImage());
+
+            Text containerName = new Text(containerPanel, SWT.BORDER | SWT.READ_ONLY);
+            containerName.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+            Button browseButton = new Button(containerPanel, SWT.PUSH);
+            browseButton.setImage(DBIcon.TREE_FOLDER.getImage());
+            browseButton.setText("...");
+        }
+
+        {
+            // Mapping table
+            mappingViewer = new TableViewer(composite, SWT.BORDER | SWT.MULTI);
+            mappingViewer.getTable().setLayoutData(new GridData(GridData.FILL_BOTH));
+            mappingViewer.getTable().setLinesVisible(true);
+            mappingViewer.getTable().setHeaderVisible(true);
+
+            TableViewerColumn columnSource = new TableViewerColumn(mappingViewer, SWT.LEFT);
+            columnSource.setLabelProvider(new CellLabelProvider() {
+                @Override
+                public void update(ViewerCell cell)
+                {
+
+                }
+            });
+            columnSource.getColumn().setText("Source");
+
+            TableViewerColumn columnTarget = new TableViewerColumn(mappingViewer, SWT.LEFT);
+            columnTarget.setLabelProvider(new CellLabelProvider() {
+                @Override
+                public void update(ViewerCell cell)
+                {
+
+                }
+            });
+            columnTarget.getColumn().setText("Target");
+
+            TableViewerColumn columnType = new TableViewerColumn(mappingViewer, SWT.LEFT);
+            columnType.setLabelProvider(new CellLabelProvider() {
+                @Override
+                public void update(ViewerCell cell)
+                {
+
+                }
+            });
+            columnType.getColumn().setText("Type");
+        }
+
+        {
+            // Control buttons
+            Composite buttonsPanel = new Composite(composite, SWT.NONE);
+            buttonsPanel.setLayout(new GridLayout(3, false));
+            buttonsPanel.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+            Button mapTableButton = new Button(buttonsPanel, SWT.PUSH);
+            mapTableButton.setImage(DBIcon.TREE_TABLE.getImage());
+            mapTableButton.setText("Existing table ...");
+
+            Button createNewButton = new Button(buttonsPanel, SWT.PUSH);
+            createNewButton.setImage(DBIcon.TREE_VIEW.getImage());
+            createNewButton.setText("Create new ...");
+
+            Button columnsButton = new Button(buttonsPanel, SWT.PUSH);
+            columnsButton.setImage(DBIcon.TREE_COLUMNS.getImage());
+            columnsButton.setText("Columns' mappings ...");
+        }
+
         final DatabaseConsumerSettings settings = getWizard().getPageSettings(this, DatabaseConsumerSettings.class);
 
         setControl(composite);
@@ -55,6 +142,8 @@ public class DatabaseConsumerPageMapping extends ActiveWizardPage<DataTransferWi
     public void activatePage()
     {
         final DatabaseConsumerSettings settings = getWizard().getPageSettings(this, DatabaseConsumerSettings.class);
+
+        UIUtils.packColumns(mappingViewer.getTable(), true);
 
         updatePageCompletion();
     }
