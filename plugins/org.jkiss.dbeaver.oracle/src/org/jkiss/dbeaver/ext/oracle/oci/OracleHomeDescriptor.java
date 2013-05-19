0ced6e18d8ecb412c65f0bf015e2dba8f34734ee
jurgen <serge-rider@users.noreply.github.com>
2013-05-19 21:32:07 +0000

    Oracle OCI is obsolete

--- plugins/org.jkiss.dbeaver.oracle/src/org/jkiss/dbeaver/ext/oracle/oci/OracleHomeDescriptor.java
+++ plugins/org.jkiss.dbeaver.oracle/src/org/jkiss/dbeaver/ext/oracle/oci/OracleHomeDescriptor.java
@@ -24,20 +24,20 @@ import org.jkiss.dbeaver.model.impl.jdbc.JDBCClientHome;
 import org.jkiss.utils.CommonUtils;
 
 import java.io.File;
-import java.io.FileFilter;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.*;
+import java.util.Collection;
+import java.util.List;
 
 public class OracleHomeDescriptor extends JDBCClientHome
 {
     static final Log log = LogFactory.getLog(OracleHomeDescriptor.class);
 
+/*
     private static final String JAR_OJDBC6 = "ojdbc6.jar";
     private static final String JAR_OJDBC5 = "ojdbc5.jar";
     private static final String JAR_OJDBC4 = "ojdbc14.jar";
     private static final String JAR_OJDBC2 = "classes12.zip";
     private static final String JAR_OJDBC2_ZIP = "classes12.jar";
+*/
 
     private Integer oraVersion; // short version (9, 10, 11...)
     private String fullOraVersion;
@@ -120,6 +120,7 @@ public class OracleHomeDescriptor extends JDBCClientHome
         return tnsNames;
     }
 
+/*
     private Collection<File> getRequiredJars()
     {
         List<File> list = new ArrayList<File>();
@@ -165,37 +166,6 @@ public class OracleHomeDescriptor extends JDBCClientHome
             libMap.remove(JAR_OJDBC2);
             libMap.remove(JAR_OJDBC2_ZIP);
         }
-/*
-        switch (oraVersion) {
-            case 8:
-                list.add(oraHome + jdbcPathPrefix + "classes12.zip"); // JDBC drivers to connect to a 9i database. (JDK 1.2.x)
-                list.add(oraHome + jdbcPathPrefix + "nls_charset12.zip"); // Additional National Language character set support
-                break;
-            case 9:
-                list.add(oraHome + jdbcPathPrefix + "ojdbc14.jar"); // JDBC classes (JDK 1.4)
-                list.add(oraHome + jdbcPathPrefix + "ocrs12.jar"); // Additional RowSet support  
-                break;
-            case 10:
-                list.add(oraHome + jdbcPathPrefix + "ojdbc14.jar"); // JDBC classes (JDK 1.4 and 1.5)
-                list.add(oraHome + libPath + "xml.jar");
-                list.add(oraHome + libPath + "xmlcomp.jar");
-                list.add(oraHome + libPath + "xmlcomp2.jar");
-                list.add(oraHome + libPath + "xmlmesg.jar");
-                list.add(oraHome + libPath + "xmlparserv2.jar");
-                break;
-            case 11:
-                list.add(oraHome + jdbcPathPrefix + "ojdbc6.jar"); // Classes for use with JDK 1.6. It contains the JDBC driver classes except classes for NLS support in Oracle Object and Collection types.
-                list.add(oraHome + jdbcPathPrefix + "ojdbc5.jar"); // Classes for use with JDK 1.5. It contains the JDBC driver classes, except classes for NLS support in Oracle Object and Collection types.
-                list.add(oraHome + jdbcPathPrefix + "orai18n.jar"); //NLS classes for use with JDK 1.5, and 1.6. It contains classes for NLS support in Oracle Object and Collection types. This jar file replaces the old nls_charset jar/zip files.
-                list.add(oraHome + libPath + "xml.jar");
-                list.add(oraHome + libPath + "xmlcomp2.jar");
-                list.add(oraHome + libPath + "xmlmesg.jar");
-                list.add(oraHome + libPath + "xmlparserv2.jar");
-                list.add(oraHome + libPath + "xschema.jar");
-                list.add(oraHome + libPath + "xsu12.jar");
-                break;
-        }
-*/
         return libMap.values();
     }
 
@@ -211,9 +181,10 @@ public class OracleHomeDescriptor extends JDBCClientHome
         }
     }
 
+
     /**
      * Returns an array of urls of jar-libraries required for the driver.
-     */
+     *
     public URL[] getLibraries()
     {
         Collection<File> libraries = getRequiredJars();
@@ -228,4 +199,5 @@ public class OracleHomeDescriptor extends JDBCClientHome
         }
         return urls;
     }
+*/
 }
