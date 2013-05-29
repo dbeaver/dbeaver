7028ee7bf1ef6639496d11f625f8625b2e4313d9
jurgen <serge-rider@users.noreply.github.com>
2013-05-29 12:34:35 +0000

    Autocommit flag management improved

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/actions/datasource/DataSourceAutoCommitHandler.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/actions/datasource/DataSourceAutoCommitHandler.java
@@ -20,29 +20,20 @@ package org.jkiss.dbeaver.ui.actions.datasource;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.jface.preference.IPreferenceStore;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.commands.IElementUpdater;
-import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.ui.menus.UIElement;
-import org.jkiss.dbeaver.core.DBeaverUI;
 import org.jkiss.dbeaver.model.DBPDataSource;
 import org.jkiss.dbeaver.model.exec.DBCException;
 import org.jkiss.dbeaver.model.exec.DBCExecutionContext;
 import org.jkiss.dbeaver.model.exec.DBCExecutionPurpose;
 import org.jkiss.dbeaver.model.exec.DBCTransactionManager;
-import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
-import org.jkiss.dbeaver.model.runtime.DBRRunnableWithProgress;
 import org.jkiss.dbeaver.model.struct.DBSDataSourceContainer;
 import org.jkiss.dbeaver.runtime.VoidProgressMonitor;
 import org.jkiss.dbeaver.ui.DBIcon;
-import org.jkiss.dbeaver.ui.UIUtils;
 import org.jkiss.dbeaver.ui.actions.DataSourceHandler;
-import org.jkiss.dbeaver.ui.preferences.PrefConstants;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
 
 public class DataSourceAutoCommitHandler extends DataSourceHandler implements IElementUpdater
@@ -51,43 +42,15 @@ public class DataSourceAutoCommitHandler extends DataSourceHandler implements IE
     public Object execute(ExecutionEvent event) throws ExecutionException
     {
         DBSDataSourceContainer dataSourceContainer = getDataSourceContainer(event, true);
-        if (dataSourceContainer != null && dataSourceContainer.isConnected()) {
-            execute(HandlerUtil.getActiveShell(event), dataSourceContainer);
+        if (dataSourceContainer != null) {
+            boolean newAutocommit = !dataSourceContainer.isDefaultAutoCommit();
+            if (dataSourceContainer.isConnected()) {
+                // Get flag from connection
+                newAutocommit = !dataSourceContainer.isConnectionAutoCommit();
+            }
+            dataSourceContainer.setDefaultAutoCommit(newAutocommit, true);
         }
         return null;
-    }
-
-    public static void execute(Shell shell, final DBSDataSourceContainer dataSourceContainer) {
-        final DBPDataSource dataSource = dataSourceContainer.getDataSource();
-        try {
-            DBeaverUI.runInProgressService(new DBRRunnableWithProgress() {
-                @Override
-                public void run(DBRProgressMonitor monitor)
-                    throws InvocationTargetException, InterruptedException
-                {
-                    DBCExecutionContext context = dataSource.openContext(monitor, DBCExecutionPurpose.UTIL, "Change '" + dataSourceContainer.getName() + "' transactional mode");
-                    try {
-                        DBCTransactionManager txnManager = context.getTransactionManager();
-                        // Change auto-commit mode
-                        boolean newAutoCommit = !txnManager.isAutoCommit();
-                        txnManager.setAutoCommit(newAutoCommit);
-
-                        // Update data source settings
-                        IPreferenceStore preferenceStore = dataSourceContainer.getPreferenceStore();
-                        preferenceStore.setValue(PrefConstants.DEFAULT_AUTO_COMMIT, newAutoCommit);
-                        //dataSourceContainer.getRegistry().flushConfig();
-                    } catch (DBCException e) {
-                        throw new InvocationTargetException(e);
-                    } finally {
-                        context.close();
-                    }
-                }
-            });
-        } catch (InvocationTargetException e) {
-            UIUtils.showErrorDialog(shell, "Auto-Commit", "Error while toggle auto-commit", e.getTargetException());
-        } catch (InterruptedException e) {
-            // do nothing
-        }
     }
 
     @Override
