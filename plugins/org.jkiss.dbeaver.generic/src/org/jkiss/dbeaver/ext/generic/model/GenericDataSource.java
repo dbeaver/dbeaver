f183ee2316d855ffcbbbff29ab4a178d462519cb
jurgen <serge-rider@users.noreply.github.com>
2013-06-03 09:34:48 +0000

    Generic metadata reading refactoring
    Former-commit-id: 9e5a68e17ccc860f03fb2fce0d66c69b75952c30

--- plugins/org.jkiss.dbeaver.generic/src/org/jkiss/dbeaver/ext/generic/model/GenericDataSource.java
+++ plugins/org.jkiss.dbeaver.generic/src/org/jkiss/dbeaver/ext/generic/model/GenericDataSource.java
@@ -24,14 +24,13 @@ import org.eclipse.core.runtime.IAdaptable;
 import org.jkiss.dbeaver.DBException;
 import org.jkiss.dbeaver.ext.IDatabaseTermProvider;
 import org.jkiss.dbeaver.ext.generic.GenericConstants;
-import org.jkiss.dbeaver.ext.generic.model.meta.GenericMetadataReader;
-import org.jkiss.dbeaver.ext.generic.model.meta.StandardMetadataReader;
 import org.jkiss.dbeaver.model.DBPDataSource;
 import org.jkiss.dbeaver.model.DBPDataSourceInfo;
 import org.jkiss.dbeaver.model.DBPDriver;
 import org.jkiss.dbeaver.model.DBUtils;
 import org.jkiss.dbeaver.model.exec.DBCExecutionPurpose;
 import org.jkiss.dbeaver.model.exec.jdbc.*;
+import org.jkiss.dbeaver.model.impl.jdbc.JDBCConstants;
 import org.jkiss.dbeaver.model.impl.jdbc.JDBCDataSource;
 import org.jkiss.dbeaver.model.impl.jdbc.JDBCUtils;
 import org.jkiss.dbeaver.model.impl.jdbc.cache.JDBCBasicDataTypeCache;
@@ -69,7 +68,6 @@ public class GenericDataSource extends JDBCDataSource
     private String selectedEntityType;
     private String selectedEntityName;
     private boolean selectedEntityFromAPI;
-    private final GenericMetadataReader metadataReader;
 
     public GenericDataSource(DBRProgressMonitor monitor, DBSDataSourceContainer container)
         throws DBException
@@ -84,17 +82,6 @@ public class GenericDataSource extends JDBCDataSource
         if (CommonUtils.isEmpty(this.selectedEntityType)) {
             this.selectedEntityType = null;
         }
-        String metadataType = CommonUtils.toString(driver.getDriverParameter(GenericConstants.PARAM_METADATA_TYPE));
-        if (CommonUtils.isEmpty(metadataType)) {
-            metadataReader = new StandardMetadataReader();
-        } else {
-            metadataReader = GenericConstants.MetadataType.valueOf(metadataType.toLowerCase()).createReader();
-        }
-    }
-
-    GenericMetadataReader getMetadataReader()
-    {
-        return metadataReader;
     }
 
     @Override
@@ -284,10 +271,13 @@ public class GenericDataSource extends JDBCDataSource
                     JDBCResultSet dbResult = metaData.getCatalogs();
                     try {
                         while (dbResult.next()) {
-                            String catalogName = getMetadataReader().fetchCatalogName(dbResult);
+                            String catalogName = JDBCUtils.safeGetString(dbResult, JDBCConstants.TABLE_CAT);
                             if (CommonUtils.isEmpty(catalogName)) {
-                                log.debug("Catalog name is empty");
-                                continue;
+                                // Some drivers uses TABLE_QUALIFIER instead of catalog
+                                catalogName = JDBCUtils.safeGetStringTrimmed(dbResult, JDBCConstants.TABLE_QUALIFIER);
+                                if (CommonUtils.isEmpty(catalogName)) {
+                                    continue;
+                                }
                             }
                             if (catalogFilters == null || catalogFilters.matches(catalogName)) {
                                 catalogNames.add(catalogName);
@@ -369,14 +359,31 @@ public class GenericDataSource extends JDBCDataSource
                     if (context.getProgressMonitor().isCanceled()) {
                         break;
                     }
-                    String schemaName = getMetadataReader().fetchSchemaName(dbResult, catalog == null ? null : catalog.getName());
+                    String schemaName = JDBCUtils.safeGetString(dbResult, JDBCConstants.TABLE_SCHEM);
                     if (CommonUtils.isEmpty(schemaName)) {
-                        log.debug("Schema name is empty");
+                        // some drivers uses TABLE_OWNER column instead of TABLE_SCHEM
+                        schemaName = JDBCUtils.safeGetString(dbResult, JDBCConstants.TABLE_OWNER);
+                    }
+                    if (CommonUtils.isEmpty(schemaName)) {
                         continue;
                     }
                     if (schemaFilters != null && !schemaFilters.matches(schemaName)) {
                         // Doesn't match filter
                         continue;
+                    }
+                    String catalogName = JDBCUtils.safeGetString(dbResult, JDBCConstants.TABLE_CATALOG);
+
+                    if (!CommonUtils.isEmpty(catalogName)) {
+                        if (catalog == null) {
+                            // Invalid schema's catalog or schema without catalog (then do not use schemas as structure)
+                            log.warn("Catalog name (" + catalogName + ") found for schema '" + schemaName + "' while schema doesn't have parent catalog");
+                        } else if (!catalog.getName().equals(catalogName)) {
+                            if (!catalogSchemas) {
+                                // Just skip it - we have list of all existing schemas and this one belongs to another catalog
+                                continue;
+                            }
+                            log.warn("Catalog name '" + catalogName + "' differs from schema's catalog '" + catalog.getName() + "'");
+                        }
                     }
 
                     context.getProgressMonitor().subTask("Schema " + schemaName);
@@ -732,13 +739,9 @@ public class GenericDataSource extends JDBCDataSource
         @Override
         protected GenericTableType fetchObject(JDBCExecutionContext context, GenericDataSource owner, ResultSet resultSet) throws SQLException, DBException
         {
-            String tableType = getMetadataReader().fetchTableType(resultSet);
-            if (CommonUtils.isEmpty(tableType)) {
-                return null;
-            }
             return new GenericTableType(
                 GenericDataSource.this,
-                tableType);
+                JDBCUtils.safeGetString(resultSet, JDBCConstants.TABLE_TYPE));
         }
     }
 
--- plugins/org.jkiss.dbeaver.generic/src/org/jkiss/dbeaver/ext/generic/model/TableCache.java
+++ plugins/org.jkiss.dbeaver.generic/src/org/jkiss/dbeaver/ext/generic/model/TableCache.java
@@ -21,21 +21,23 @@ package org.jkiss.dbeaver.ext.generic.model;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jkiss.dbeaver.DBException;
-import org.jkiss.dbeaver.ext.generic.model.meta.GenericMetadataReader;
+import org.jkiss.dbeaver.ext.generic.GenericConstants;
 import org.jkiss.dbeaver.model.DBUtils;
 import org.jkiss.dbeaver.model.exec.jdbc.JDBCExecutionContext;
 import org.jkiss.dbeaver.model.exec.jdbc.JDBCStatement;
 import org.jkiss.dbeaver.model.impl.jdbc.JDBCConstants;
+import org.jkiss.dbeaver.model.impl.jdbc.JDBCUtils;
 import org.jkiss.dbeaver.model.impl.jdbc.cache.JDBCStructCache;
 import org.jkiss.utils.CommonUtils;
 
+import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
- * Tables cache implementation
+ * Generic tables cache implementation
  */
 public class TableCache extends JDBCStructCache<GenericStructContainer, GenericTable, GenericTableColumn> {
 
@@ -77,28 +79,50 @@ public class TableCache extends JDBCStructCache<GenericStructContainer, GenericT
     protected GenericTable fetchObject(JDBCExecutionContext context, GenericStructContainer owner, ResultSet dbResult)
         throws SQLException, DBException
     {
-        GenericMetadataReader.TableInfo tableInfo = owner.getDataSource().getMetadataReader().fetchTable(dbResult, owner);
-        if (tableInfo == null) {
-            return null;
-        }
-        if (CommonUtils.isEmpty(tableInfo.tableName)) {
-            log.debug("Empty table name in container " + owner.getName());
+        String tableName = JDBCUtils.safeGetStringTrimmed(dbResult, JDBCConstants.TABLE_NAME);
+        String tableType = JDBCUtils.safeGetStringTrimmed(dbResult, JDBCConstants.TABLE_TYPE);
+        String remarks = JDBCUtils.safeGetString(dbResult, JDBCConstants.REMARKS);
+
+        if (CommonUtils.isEmpty(tableName)) {
+            log.debug("Empty table name" + (owner == null ? "" : " in container " + owner.getName()));
             return null;
         }
 
-        if (tableInfo.tableType != null && INVALID_TABLE_TYPES.contains(tableInfo.tableType)) {
+        if (tableType != null && INVALID_TABLE_TYPES.contains(tableType)) {
             // Bad table type. Just skip it
             return null;
         }
 
-        if (tableInfo.systemTable && !owner.getDataSource().getContainer().isShowSystemObjects()) {
-            // Filter system tables
+        boolean isSystemTable = tableType != null && tableType.toUpperCase().contains("SYSTEM");
+        if (isSystemTable && !owner.getDataSource().getContainer().isShowSystemObjects()) {
             return null;
         }
 
+        // Skip "recycled" tables (Oracle)
+        if (tableName.startsWith("BIN$")) {
+            return null;
+        }
+/*
+        // Do not read table type object
+        // Actually dunno what to do with it and it often throws stupid warnings in debug
+
+        String typeName = JDBCUtils.safeGetString(dbResult, JDBCConstants.TYPE_NAME);
+        String typeCatalogName = JDBCUtils.safeGetString(dbResult, JDBCConstants.TYPE_CAT);
+        String typeSchemaName = JDBCUtils.safeGetString(dbResult, JDBCConstants.TYPE_SCHEM);
+        GenericCatalog typeCatalog = CommonUtils.isEmpty(typeCatalogName) ?
+            null :
+            getDataSourceContainer().getCatalog(context.getProgressMonitor(), typeCatalogName);
+        GenericSchema typeSchema = CommonUtils.isEmpty(typeSchemaName) ?
+            null :
+            typeCatalog == null ?
+                getDataSourceContainer().getSchema(context.getProgressMonitor(), typeSchemaName) :
+                typeCatalog.getSchema(typeSchemaName);
+*/
         return new GenericTable(
             owner,
-            tableInfo,
+            tableName,
+            tableType,
+            remarks,
             true);
     }
 
@@ -117,11 +141,33 @@ public class TableCache extends JDBCStructCache<GenericStructContainer, GenericT
     protected GenericTableColumn fetchChild(JDBCExecutionContext context, GenericStructContainer owner, GenericTable table, ResultSet dbResult)
         throws SQLException, DBException
     {
-        GenericMetadataReader.TableColumnInfo column = table.getDataSource().getMetadataReader().fetchTableColumn(dbResult, table);
-        if (column == null) {
-            return null;
+        String columnName = JDBCUtils.safeGetStringTrimmed(dbResult, JDBCConstants.COLUMN_NAME);
+        int valueType = JDBCUtils.safeGetInt(dbResult, JDBCConstants.DATA_TYPE);
+        int sourceType = JDBCUtils.safeGetInt(dbResult, JDBCConstants.SOURCE_DATA_TYPE);
+        String typeName = JDBCUtils.safeGetStringTrimmed(dbResult, JDBCConstants.TYPE_NAME);
+        long columnSize = JDBCUtils.safeGetLong(dbResult, JDBCConstants.COLUMN_SIZE);
+        boolean isNotNull = JDBCUtils.safeGetInt(dbResult, JDBCConstants.NULLABLE) == DatabaseMetaData.columnNoNulls;
+        int scale = JDBCUtils.safeGetInt(dbResult, JDBCConstants.DECIMAL_DIGITS);
+        int precision = 0;//GenericUtils.safeGetInt(dbResult, JDBCConstants.COLUMN_);
+        int radix = JDBCUtils.safeGetInt(dbResult, JDBCConstants.NUM_PREC_RADIX);
+        String defaultValue = JDBCUtils.safeGetString(dbResult, JDBCConstants.COLUMN_DEF);
+        String remarks = JDBCUtils.safeGetString(dbResult, JDBCConstants.REMARKS);
+        long charLength = JDBCUtils.safeGetLong(dbResult, JDBCConstants.CHAR_OCTET_LENGTH);
+        int ordinalPos = JDBCUtils.safeGetInt(dbResult, JDBCConstants.ORDINAL_POSITION);
+        boolean autoIncrement = "YES".equals(JDBCUtils.safeGetStringTrimmed(dbResult, JDBCConstants.IS_AUTOINCREMENT));
+        // Check for identity modifier [DBSPEC: MS SQL]
+        if (typeName.toUpperCase().endsWith(GenericConstants.TYPE_MODIFIER_IDENTITY)) {
+            autoIncrement = true;
+            typeName = typeName.substring(0, typeName.length() - GenericConstants.TYPE_MODIFIER_IDENTITY.length());
         }
 
-        return new GenericTableColumn(table, column);
+        return new GenericTableColumn(
+            table,
+            columnName,
+            typeName, valueType, sourceType, ordinalPos,
+            columnSize,
+            charLength, scale, precision, radix, isNotNull,
+            remarks, defaultValue, autoIncrement
+        );
     }
 }
