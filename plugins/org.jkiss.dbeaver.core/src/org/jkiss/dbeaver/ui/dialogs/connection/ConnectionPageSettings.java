93b92990c5290c90bd0d8753cc40586a568bd0e4
jurgen <serge-rider@users.noreply.github.com>
2013-06-07 09:14:26 +0000

    MP wizard fix
    Former-commit-id: 1952c7182fafe74843a7b8f4212a9d29e9e78b10

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/dialogs/connection/ConnectionPageSettings.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/dialogs/connection/ConnectionPageSettings.java
@@ -110,6 +110,7 @@ class ConnectionPageSettings extends ActiveWizardPage implements IDataSourceConn
             connectionEditor.saveSettings();
         }
     }
+
     @Override
     public void createControl(Composite parent)
     {
