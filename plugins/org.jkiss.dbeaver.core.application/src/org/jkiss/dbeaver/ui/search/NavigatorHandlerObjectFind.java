0a5b50866a1b8648521e95902dfff427d40a9df1
jurgen <serge-rider@users.noreply.github.com>
2013-07-01 20:05:44 +0000

    Search model split
    Former-commit-id: bdc98e6f624fc4a2c60b54cb97a45dce1fae189a

--- plugins/org.jkiss.dbeaver.core.application/src/org/jkiss/dbeaver/ui/search/NavigatorHandlerObjectFind.java
+++ plugins/org.jkiss.dbeaver.core.application/src/org/jkiss/dbeaver/ui/search/NavigatorHandlerObjectFind.java
@@ -16,7 +16,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-package org.jkiss.dbeaver.ui.actions.navigator;
+package org.jkiss.dbeaver.ui.search;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
