483dd492e1f411747cca456173183d88d9c9bfde
jurgen <serge-rider@users.noreply.github.com>
2013-07-01 20:19:26 +0000

    Search results view

--- plugins/org.jkiss.dbeaver.core.application/src/org/jkiss/dbeaver/ui/search/SearchDatabaseObjectsView.java
+++ plugins/org.jkiss.dbeaver.core.application/src/org/jkiss/dbeaver/ui/search/SearchDatabaseObjectsView.java
@@ -1,5 +1,8 @@
 package org.jkiss.dbeaver.ui.search;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CTabFolder;
+import org.eclipse.swt.custom.CTabItem;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 import org.jkiss.dbeaver.ui.IHelpContextIds;
@@ -7,20 +10,32 @@ import org.jkiss.dbeaver.ui.UIUtils;
 
 public class SearchDatabaseObjectsView extends ViewPart
 {
-    public static final String VIEW_ID = "org.jkiss.dbeaver.core.searchResults";
+    public static final String VIEW_ID = "org.jkiss.dbeaver.ui.search.SearchDatabaseObjectsView";
+
+    private CTabFolder resultsFolder;
 
     @Override
     public void createPartControl(Composite parent)
     {
-        Composite group = UIUtils.createPlaceholder(parent, 1);
+        resultsFolder = new CTabFolder(parent, SWT.TOP);
 
-        UIUtils.setHelp(group, IHelpContextIds.CTX_SEARCH_RESULTS);
+        UIUtils.setHelp(resultsFolder, IHelpContextIds.CTX_SEARCH_RESULTS);
     }
 
     @Override
     public void setFocus()
     {
+        resultsFolder.setFocus();
+    }
 
+    public void addResults(IObjectSearchResultPage resultPage)
+    {
+        resultPage.createControl(resultsFolder);
+
+        CTabItem tabItem = new CTabItem(resultsFolder, SWT.NONE);
+        tabItem.setData(resultPage);
+        tabItem.setControl(resultPage.getControl());
+        //tabItem.setText(resultPage.getTitle());
     }
 
 }
