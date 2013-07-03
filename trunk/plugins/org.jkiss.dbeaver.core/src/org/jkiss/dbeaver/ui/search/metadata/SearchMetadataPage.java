a111e821f29cac675afbf4eaa9c08faca2ebf63b
jurgen <jurgen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2013-07-03 14:07:11 +0000

    Meadat search ui fix
    Former-commit-id: 7ffa34c9a306a56c53481fb95349c17df7887b43

--- trunk/plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/search/metadata/SearchMetadataPage.java
+++ trunk/plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/search/metadata/SearchMetadataPage.java
@@ -167,12 +167,11 @@ public class SearchMetadataPage extends DialogPage implements IObjectSearchPage 
             final DBeaverCore core = DBeaverCore.getInstance();
 
             Group sourceGroup = UIUtils.createControlGroup(optionsGroup, CoreMessages.dialog_search_objects_group_objects_source, 1, GridData.FILL_BOTH, 0);
+            sourceGroup.setLayoutData(new GridData(GridData.FILL_BOTH));
             final DBNProject projectNode = core.getNavigatorModel().getRoot().getProject(core.getProjectRegistry().getActiveProject());
             DBNNode rootNode = projectNode == null ? core.getNavigatorModel().getRoot() : projectNode.getDatabases();
             dataSourceTree = new DatabaseNavigatorTree(sourceGroup, rootNode, SWT.SINGLE | SWT.BORDER);
-            gd = new GridData(GridData.FILL_BOTH);
-            //gd.heightHint = 100;
-            dataSourceTree.setLayoutData(gd);
+            dataSourceTree.setLayoutData(new GridData(GridData.FILL_BOTH));
 
             dataSourceTree.getViewer().addFilter(new ViewerFilter() {
                 @Override
@@ -230,6 +229,7 @@ public class SearchMetadataPage extends DialogPage implements IObjectSearchPage 
 
         {
             Group typesGroup = UIUtils.createControlGroup(optionsGroup, CoreMessages.dialog_search_objects_group_object_types, 1, GridData.FILL_BOTH, 0);
+            typesGroup.setLayoutData(new GridData(GridData.FILL_BOTH));
             typesTable = new Table(typesGroup, SWT.BORDER | SWT.CHECK | SWT.H_SCROLL | SWT.V_SCROLL);
             typesTable.addSelectionListener(new SelectionAdapter() {
                 @Override
