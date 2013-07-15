a8f1e61ddc506c1a11b8de78d7d9952eee7086bb
jurgen <jurgen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2013-07-15 16:06:22 +0000

    Perspective layout
    Former-commit-id: a0172ac2b439dd1cb611393112da53f30ddb0062

--- trunk/plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/perspective/DBeaverPerspective.java
+++ trunk/plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/perspective/DBeaverPerspective.java
@@ -18,7 +18,6 @@
  */
 package org.jkiss.dbeaver.ui.perspective;
 
-import org.eclipse.search2.internal.ui.SearchView;
 import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
