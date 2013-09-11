cedc28b9bc24c4d5fafe4c0c21f8afdf551eaf1c
titou10 <titou10@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2013-09-11 22:26:11 +0000

    Code Quality

--- plugins/org.jkiss.dbeaver.db2/src/org/jkiss/dbeaver/ext/db2/DB2Utils.java
+++ plugins/org.jkiss.dbeaver.db2/src/org/jkiss/dbeaver/ext/db2/DB2Utils.java
@@ -43,25 +43,26 @@ import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
  */
 public class DB2Utils {
 
-   private static final Log    LOG              = LogFactory.getLog(DB2Utils.class);
+   private static final Log    LOG                  = LogFactory.getLog(DB2Utils.class);
 
-   private static final String CALL_DB2LK_GEN   = "CALL SYSPROC.DB2LK_GENERATE_DDL(?,?)";
-   private static final String CALL_DB2LK_CLEAN = "CALL SYSPROC.DB2LK_CLEAN_TABLE(?)";
-   private static final String SEL_DB2LK        = "SELECT SQL_STMT FROM SYSTOOLS.DB2LOOK_INFO_V WHERE OP_TOKEN = ? ORDER BY OP_SEQUENCE WITH UR";
+   private static final String CALL_DB2LK_GEN       = "CALL SYSPROC.DB2LK_GENERATE_DDL(?,?)";
+   private static final String CALL_DB2LK_CLEAN     = "CALL SYSPROC.DB2LK_CLEAN_TABLE(?)";
+   private static final String SEL_DB2LK            = "SELECT SQL_STMT FROM SYSTOOLS.DB2LOOK_INFO_V WHERE OP_TOKEN = ? ORDER BY OP_SEQUENCE WITH UR";
+   private static final String DB2LK_COMMAND        = "-e -td %s -t %s";
 
-   private static final String CALL_INST_OBJ    = "CALL SYSPROC.SYSINSTALLOBJECTS(?,?,?,?)";
-   private static final String SYSTOOLS         = "SYSTOOLS";
+   private static final String CALL_INST_OBJ        = "CALL SYSPROC.SYSINSTALLOBJECTS(?,?,?,?)";
+   private static final String SYSTOOLS             = "SYSTOOLS";
+   private static final int    CALL_INST_OBJ_BAD_RC = -438;
 
-   private static final String LINE_SEP         = "\n";
+   private static final String LINE_SEP             = "\n";
 
    // Generate DDL
+   // TODO DF: Tables in SYSTOOLS tables must exist first
    public static String generateDDLforTable(DBRProgressMonitor monitor,
                                             String statementDelimiter,
                                             DB2DataSource dataSource,
                                             DB2Table db2Table) throws DBException {
-      LOG.debug("generate DDL for " + db2Table.getFullQualifiedName());
-
-      // TODO DF: Tables in SYSTOOLS tables must exist first
+      LOG.debug("Generate DDL for " + db2Table.getFullQualifiedName());
 
       monitor.beginTask("Generating DDL", 1);
 
@@ -69,50 +70,59 @@ public class DB2Utils {
       // Ref to db2look :
       // http://pic.dhe.ibm.com/infocenter/db2luw/v10r5/topic/com.ibm.db2.luw.admin.cmd.doc/doc/r0002051.html
       //
-      // Option that do not seem to work: -dp -a
+      // Option that do not seem to work: -dp -a ...
 
+      Integer token = 0;
       StringBuilder sb = new StringBuilder(2048);
+      String command = String.format(DB2LK_COMMAND, statementDelimiter, db2Table.getFullQualifiedName());
 
       JDBCExecutionContext context = dataSource.openContext(monitor, DBCExecutionPurpose.META, "Generate DDL");
-      JDBCCallableStatement stmtSP = null;
-      JDBCCallableStatement stmtSPClean = null;
-      JDBCPreparedStatement stmtSel = null;
-      JDBCResultSet dbResult = null;
-      Integer token = 0;
-
-      String command = "-e -td " + statementDelimiter + " -t " + db2Table.getFullQualifiedName();
-
       try {
-         LOG.debug("Calling Stored Proc with command : " + command);
+         LOG.debug("Calling DB2LK_GENERATE_DDL with command : " + command);
 
-         stmtSP = context.prepareCall(CALL_DB2LK_GEN);
-         stmtSP.registerOutParameter(2, java.sql.Types.INTEGER);
-         stmtSP.setString(1, command);
-         stmtSP.executeUpdate();
-         token = stmtSP.getInt(2);
+         JDBCCallableStatement stmtSP = context.prepareCall(CALL_DB2LK_GEN);
+         try {
+            stmtSP.registerOutParameter(2, java.sql.Types.INTEGER);
+            stmtSP.setString(1, command);
+            stmtSP.executeUpdate();
+            token = stmtSP.getInt(2);
+         } finally {
+            stmtSP.close();
+         }
 
          LOG.debug("Token = " + token);
 
          // Read result
-         stmtSel = context.prepareStatement(SEL_DB2LK);
-         stmtSel.setInt(1, token);
-         dbResult = stmtSel.executeQuery();
-
-         Clob ddlStmt;
-         Long ddlLength;
-         Long ddlStart = 1L;
-         while (dbResult.next()) {
-            ddlStmt = dbResult.getClob(1);
-            ddlLength = ddlStmt.length() + 1L;
-            sb.append(ddlStmt.getSubString(ddlStart, ddlLength.intValue()));
-            sb.append(LINE_SEP);
-            ddlStmt.free();
+         JDBCPreparedStatement stmtSel = context.prepareStatement(SEL_DB2LK);
+         try {
+            stmtSel.setInt(1, token);
+            JDBCResultSet dbResult = stmtSel.executeQuery();
+            try {
+               Clob ddlStmt;
+               Long ddlLength;
+               Long ddlStart = 1L;
+               while (dbResult.next()) {
+                  ddlStmt = dbResult.getClob(1);
+                  ddlLength = ddlStmt.length() + 1L;
+                  sb.append(ddlStmt.getSubString(ddlStart, ddlLength.intValue()));
+                  sb.append(LINE_SEP);
+                  ddlStmt.free();
+               }
+            } finally {
+               dbResult.close();
+            }
+         } finally {
+            stmtSel.close();
          }
 
          // Clean
-         stmtSPClean = context.prepareCall(CALL_DB2LK_CLEAN);
-         stmtSPClean.setInt(1, token);
-         stmtSPClean.executeUpdate();
+         JDBCCallableStatement stmtSPClean = context.prepareCall(CALL_DB2LK_CLEAN);
+         try {
+            stmtSPClean.setInt(1, token);
+            stmtSPClean.executeUpdate();
+         } finally {
+            stmtSPClean.close();
+         }
 
          LOG.debug("Terminated OK");
 
@@ -121,21 +131,7 @@ public class DB2Utils {
       } catch (SQLException e) {
          throw new DBException(e);
       } finally {
-         if (dbResult != null) {
-            dbResult.close();
-         }
-         if (stmtSP != null) {
-            stmtSP.close();
-         }
-         if (stmtSel != null) {
-            stmtSel.close();
-         }
-         if (stmtSPClean != null) {
-            stmtSPClean.close();
-         }
-         if (context != null) {
-            context.close();
-         }
+         context.close();
 
          monitor.done();
       }
@@ -144,51 +140,48 @@ public class DB2Utils {
    public static String checkExplainTables(DBRProgressMonitor monitor, DB2DataSource dataSource, String explainTableSchemaName) throws DBCException {
       LOG.debug("Check EXPLAIN tables in " + explainTableSchemaName);
 
-      // TODO DF: EXPLAIN tables can be created in different Tablespace/schema..
-
       monitor.beginTask("Check EXPLAIN tables", 1);
 
       JDBCExecutionContext context = dataSource.openContext(monitor, DBCExecutionPurpose.META, "Verify EXPLAIN tables");
-      JDBCCallableStatement stmtSP = null;
 
-      // First Check with given schema
       try {
-         stmtSP = context.prepareCall(CALL_INST_OBJ);
-         stmtSP.setString(1, "EXPLAIN"); // EXPLAIN
-         stmtSP.setString(2, "V"); // Verify
-         stmtSP.setString(3, ""); // Tablespace
-         stmtSP.setString(4, explainTableSchemaName); // Schema
-         stmtSP.execute();
-         LOG.debug("EXPLAIN tables with schema " + explainTableSchemaName + " found.");
-         return explainTableSchemaName;
+         // First Check with given schema
+         JDBCCallableStatement stmtSP = context.prepareCall(CALL_INST_OBJ);
+         try {
+            stmtSP.setString(1, "EXPLAIN"); // EXPLAIN
+            stmtSP.setString(2, "V"); // Verify
+            stmtSP.setString(3, ""); // Tablespace
+            stmtSP.setString(4, explainTableSchemaName); // Schema
+            stmtSP.execute();
+            LOG.debug("EXPLAIN tables with schema " + explainTableSchemaName + " found.");
+            return explainTableSchemaName;
 
-      } catch (SQLException e) {
-         System.out.println(e.getErrorCode() + " " + e.getSQLState() + " " + e.getMessage());
-         if (e.getErrorCode() == -438) {
-            LOG.info("No valid EXPLAIN tables found in schema " + explainTableSchemaName + ". Check within " + SYSTOOLS + ".");
-            try {
-               stmtSP.setString(4, SYSTOOLS); // Schema
-               stmtSP.execute();
-               LOG.info("EXPLAIN tables with schema " + SYSTOOLS + " found.");
-               return SYSTOOLS;
-            } catch (SQLException e2) {
-               System.out.println("" + e.getErrorCode());
-               if (e.getErrorCode() == -438) {
-                  LOG.info("No valid EXPLAIN tables found in schema " + SYSTOOLS);
-                  return null;
+         } catch (SQLException e) {
+            System.out.println(e.getErrorCode() + " " + e.getSQLState() + " " + e.getMessage());
+            if (e.getErrorCode() == CALL_INST_OBJ_BAD_RC) {
+               LOG.warn("No valid EXPLAIN tables found in schema " + explainTableSchemaName + ". Check within " + SYSTOOLS + ".");
+               try {
+                  stmtSP.setString(4, SYSTOOLS); // Schema
+                  stmtSP.execute();
+                  LOG.debug("EXPLAIN tables with schema " + SYSTOOLS + " found.");
+                  return SYSTOOLS;
+               } catch (SQLException e2) {
+                  System.out.println("" + e.getErrorCode());
+                  if (e.getErrorCode() == CALL_INST_OBJ_BAD_RC) {
+                     LOG.warn("No valid EXPLAIN tables found in schema " + SYSTOOLS);
+                     return null;
+                  }
+                  throw new DBCException(e);
                }
-               throw new DBCException(e);
             }
-         }
-         throw new DBCException(e);
-      } finally {
-         if (stmtSP != null) {
+            throw new DBCException(e);
+         } finally {
             stmtSP.close();
          }
-         if (context != null) {
-            context.close();
-         }
-
+      } catch (SQLException e1) {
+         throw new DBCException(e1);
+      } finally {
+         context.close();
          monitor.done();
       }
    }
@@ -196,33 +189,29 @@ public class DB2Utils {
    public static void createExplainTables(DBRProgressMonitor monitor, DB2DataSource dataSource, String explainTableSchemaName) throws DBCException {
       LOG.debug("Create EXPLAIN tables in " + explainTableSchemaName);
 
-      // TODO DF: EXPLAIN tables can be created in different Tablespace/schema..
-
       monitor.beginTask("Create EXPLAIN Tables", 1);
 
       JDBCExecutionContext context = dataSource.openContext(monitor, DBCExecutionPurpose.META, "Create EXPLAIN tables");
-      JDBCCallableStatement stmtSP = null;
-
       try {
-         stmtSP = context.prepareCall(CALL_INST_OBJ);
-         stmtSP.setString(1, "EXPLAIN"); // EXPLAIN
-         stmtSP.setString(2, "C"); // Create
-         stmtSP.setString(3, ""); // Tablespace
-         stmtSP.setString(4, explainTableSchemaName); // Schema
-         stmtSP.executeUpdate();
+         JDBCCallableStatement stmtSP = context.prepareCall(CALL_INST_OBJ);
+         try {
+            stmtSP.setString(1, "EXPLAIN"); // EXPLAIN
+            stmtSP.setString(2, "C"); // Create
+            stmtSP.setString(3, ""); // Tablespace
+            stmtSP.setString(4, explainTableSchemaName); // Schema
+            stmtSP.executeUpdate();
 
-         LOG.debug("Creation OK");
-      } catch (SQLException e) {
-         LOG.error("SQLException occured during EXPLAIN tables creation in schema " + explainTableSchemaName, e);
-         throw new DBCException(e);
-      } finally {
-         if (stmtSP != null) {
+            LOG.debug("Creation EXPLAIN Tables : OK");
+         } catch (SQLException e) {
+            LOG.error("SQLException occured during EXPLAIN tables creation in schema " + explainTableSchemaName, e);
+            throw new DBCException(e);
+         } finally {
             stmtSP.close();
          }
-         if (context != null) {
-            context.close();
-         }
-
+      } catch (SQLException e1) {
+         throw new DBCException(e1);
+      } finally {
+         context.close();
          monitor.done();
       }
    }
