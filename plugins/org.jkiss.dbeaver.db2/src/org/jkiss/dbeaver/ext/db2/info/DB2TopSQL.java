559f0bc7a406c9bf6a8bc54400ae9fe3ea23c39d
titou10 <titou10@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2013-09-13 13:34:30 +0000

    DB2Applications  support
    Former-commit-id: a436efd18c0ebda10c6936c4cdff5bcd1a60b49f

---
+++ plugins/org.jkiss.dbeaver.db2/src/org/jkiss/dbeaver/ext/db2/info/DB2TopSQL.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2013      Denis Forveille titou10.titou10@gmail.com
+ * Copyright (C) 2010-2013 Serge Rieder serge@jkiss.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.jkiss.dbeaver.ext.db2.info;
+
+import java.sql.ResultSet;
+
+import org.jkiss.dbeaver.ext.db2.model.DB2Schema;
+import org.jkiss.dbeaver.ext.db2.model.DB2SchemaObject;
+import org.jkiss.dbeaver.model.impl.jdbc.JDBCUtils;
+import org.jkiss.dbeaver.model.meta.Property;
+
+/**
+ * DB2 Top SQL
+ * 
+ * @author Denis Forveille
+ * 
+ */
+public class DB2TopSQL extends DB2SchemaObject {
+
+   private String owner;
+
+   // -----------------------
+   // Constructors
+   // -----------------------
+   public DB2TopSQL(DB2Schema schema, ResultSet dbResult) {
+      super(schema, JDBCUtils.safeGetString(dbResult, "SEQNAME"), true);
+
+      this.owner = JDBCUtils.safeGetString(dbResult, "OWNER");
+   }
+
+   // -----------------
+   // Properties
+   // -----------------
+
+   @Override
+   @Property(viewable = true, editable = false, order = 1)
+   public String getName() {
+      return super.getName();
+   }
+
+   @Property(viewable = true, editable = false, order = 2)
+   public DB2Schema getSchema() {
+      return super.getSchema();
+   }
+
+}
