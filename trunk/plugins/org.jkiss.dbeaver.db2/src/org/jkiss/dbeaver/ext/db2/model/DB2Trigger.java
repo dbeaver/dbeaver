e525c05130f60c04107e368afdc4367181e7e323
titou10 <titou10@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2013-09-15 12:58:22 +0000

    DB2: Fix the problem with indexes and trigger caches. Those object are linked to a base table but their schema may be different from this table. Former-commit-id: f6d0a3db43466ea749452579e5c0860bda9efcd2

--- trunk/plugins/org.jkiss.dbeaver.db2/src/org/jkiss/dbeaver/ext/db2/model/DB2Trigger.java
+++ trunk/plugins/org.jkiss.dbeaver.db2/src/org/jkiss/dbeaver/ext/db2/model/DB2Trigger.java
@@ -31,6 +31,7 @@ import org.jkiss.dbeaver.ext.db2.model.dict.DB2TriggerEvent;
 import org.jkiss.dbeaver.ext.db2.model.dict.DB2TriggerGranularity;
 import org.jkiss.dbeaver.ext.db2.model.dict.DB2TriggerTime;
 import org.jkiss.dbeaver.ext.db2.model.dict.DB2TriggerValid;
+import org.jkiss.dbeaver.ext.db2.model.dict.DB2YesNo;
 import org.jkiss.dbeaver.ext.db2.model.source.DB2SourceObject;
 import org.jkiss.dbeaver.ext.db2.model.source.DB2SourceType;
 import org.jkiss.dbeaver.model.DBPRefreshableObject;
@@ -84,7 +85,7 @@ public class DB2Trigger extends DB2SchemaObject implements DBSTrigger, DB2Source
    // Constructors
    // -----------------------
 
-   public DB2Trigger(DB2Schema schema, DB2Table table, ResultSet dbResult) {
+   public DB2Trigger(DBRProgressMonitor monitor, DB2Schema schema, DB2Table table, ResultSet dbResult) {
       super(schema, JDBCUtils.safeGetString(dbResult, "TRIGNAME"), true);
 
       this.table = table;
@@ -93,9 +94,6 @@ public class DB2Trigger extends DB2SchemaObject implements DBSTrigger, DB2Source
       this.ownerType = CommonUtils.valueOf(DB2OwnerType.class, JDBCUtils.safeGetString(dbResult, "OWNERTYPE"));
       this.time = CommonUtils.valueOf(DB2TriggerTime.class, JDBCUtils.safeGetString(dbResult, "TRIGTIME"));
       this.event = CommonUtils.valueOf(DB2TriggerEvent.class, JDBCUtils.safeGetString(dbResult, "TRIGEVENT"));
-      // DB2 v10 this.eventUpdate = JDBCUtils.safeGetBoolean(dbResult, "EVENTUPDATE", DB2YesNo.Y.name());
-      // DB2 v10 this.eventDelete = JDBCUtils.safeGetBoolean(dbResult, "EVENTDELETE", DB2YesNo.Y.name());
-      // DB2 v10 this.eventInsert = JDBCUtils.safeGetBoolean(dbResult, "EVENTINSERT", DB2YesNo.Y.name());
       this.granularity = CommonUtils.valueOf(DB2TriggerGranularity.class, JDBCUtils.safeGetString(dbResult, "GRANULARITY"));
       this.valid = CommonUtils.valueOf(DB2TriggerValid.class, JDBCUtils.safeGetString(dbResult, "VALID"));
       this.createTime = JDBCUtils.safeGetTimestamp(dbResult, "CREATE_TIME");
@@ -107,12 +105,18 @@ public class DB2Trigger extends DB2SchemaObject implements DBSTrigger, DB2Source
       this.collationName = JDBCUtils.safeGetString(dbResult, "COLLATIONNAME");
       this.collationSchemaOrderBy = JDBCUtils.safeGetString(dbResult, "COLLATIONSCHEMA_ORDERBY");
       this.collationNameOrderBy = JDBCUtils.safeGetString(dbResult, "COLLATIONNAME_ORDERBY");
-      // DB2 v10 this.secure = JDBCUtils.safeGetBoolean(dbResult, "SECURE", DB2YesNo.Y.name());
-      // DB2 v10 this.alterTime = JDBCUtils.safeGetTimestamp(dbResult, "ALTER_TIME");
-      // DB2 v10 this.libId = JDBCUtils.safeGetInteger(dbResult, "LIB_ID");
-      // DB2 v10 this.precompileOptions = JDBCUtils.safeGetString(dbResult, "PRECOMPILE_OPTIONS");
-      // DB2 v10this.compileOptions = JDBCUtils.safeGetString(dbResult, "COMPILE_OPTIONS");
       this.remarks = JDBCUtils.safeGetString(dbResult, "REMARKS");
+
+      // DB2 v10.1+ Columns
+      this.eventUpdate = JDBCUtils.safeGetBoolean(dbResult, "EVENTUPDATE", DB2YesNo.Y.name());
+      this.eventDelete = JDBCUtils.safeGetBoolean(dbResult, "EVENTDELETE", DB2YesNo.Y.name());
+      this.eventInsert = JDBCUtils.safeGetBoolean(dbResult, "EVENTINSERT", DB2YesNo.Y.name());
+      this.secure = JDBCUtils.safeGetBoolean(dbResult, "SECURE", DB2YesNo.Y.name());
+      this.alterTime = JDBCUtils.safeGetTimestamp(dbResult, "ALTER_TIME");
+      this.libId = JDBCUtils.safeGetInteger(dbResult, "LIB_ID");
+      this.precompileOptions = JDBCUtils.safeGetString(dbResult, "PRECOMPILE_OPTIONS");
+      this.compileOptions = JDBCUtils.safeGetString(dbResult, "COMPILE_OPTIONS");
+
    }
 
    @Override
--- trunk/plugins/org.jkiss.dbeaver.db2/src/org/jkiss/dbeaver/ext/db2/model/cache/DB2IndexCache.java
+++ trunk/plugins/org.jkiss.dbeaver.db2/src/org/jkiss/dbeaver/ext/db2/model/cache/DB2IndexCache.java
@@ -20,124 +20,65 @@ package org.jkiss.dbeaver.ext.db2.model.cache;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.List;
 
 import org.jkiss.dbeaver.DBException;
 import org.jkiss.dbeaver.ext.db2.model.DB2Index;
 import org.jkiss.dbeaver.ext.db2.model.DB2IndexColumn;
 import org.jkiss.dbeaver.ext.db2.model.DB2Schema;
 import org.jkiss.dbeaver.ext.db2.model.DB2Table;
-import org.jkiss.dbeaver.ext.db2.model.DB2TableColumn;
-import org.jkiss.dbeaver.ext.db2.model.dict.DB2IndexColOrder;
 import org.jkiss.dbeaver.model.exec.jdbc.JDBCExecutionContext;
 import org.jkiss.dbeaver.model.exec.jdbc.JDBCPreparedStatement;
 import org.jkiss.dbeaver.model.exec.jdbc.JDBCStatement;
 import org.jkiss.dbeaver.model.impl.jdbc.JDBCUtils;
-import org.jkiss.dbeaver.model.impl.jdbc.cache.JDBCCompositeCache;
+import org.jkiss.dbeaver.model.impl.jdbc.cache.JDBCStructCache;
 
 /**
- * Cache for DB2 Indexes
+ * Cache for DB2 Indexes at the Schema Level
  * 
  * @author Denis Forveille
  * 
  */
-public final class DB2IndexCache extends JDBCCompositeCache<DB2Schema, DB2Table, DB2Index, DB2IndexColumn> {
+public final class DB2IndexCache extends JDBCStructCache<DB2Schema, DB2Index, DB2IndexColumn> {
 
-   private static String SQL_IND_ALL;
-   private static String SQL_IND_TAB;
+   private static final String SQL_INDS_ALL = "SELECT * FROM SYSCAT.INDEXES WHERE INDSCHEMA = ? ORDER BY INDNAME WITH UR";
+   private static final String SQL_COLS_IND = "SELECT * FROM SYSCAT.INDEXCOLUSE WHERE INDSCHEMA = ? AND INDNAME = ? ORDER BY COLSEQ WITH UR";
 
-   static {
-      StringBuilder sb = new StringBuilder(512);
-      sb.append(" SELECT I.*");
-      sb.append("       ,ICU.COLNAME,ICU.COLSEQ,ICU.COLORDER,ICU.COLLATIONSCHEMA,ICU.COLLATIONNAME");
-      sb.append("   FROM SYSCAT.INDEXES I");
-      sb.append("       ,SYSCAT.INDEXCOLUSE ICU");
-      sb.append("  WHERE I.INDSCHEMA = ?");
-      sb.append("    AND ICU.INDNAME = I.INDNAME");
-      sb.append("    AND ICU.INDSCHEMA = I.INDSCHEMA");
-      sb.append("  ORDER BY I.INDNAME");
-      sb.append("         , ICU.COLSEQ");
-      sb.append(" WITH UR");
-      SQL_IND_ALL = sb.toString();
-
-      sb.setLength(0);
-
-      sb.append(" SELECT I.*");
-      sb.append("       ,ICU.COLNAME,ICU.COLSEQ,ICU.COLORDER,ICU.COLLATIONSCHEMA,ICU.COLLATIONNAME");
-      sb.append("   FROM SYSCAT.INDEXES I");
-      sb.append("       ,SYSCAT.INDEXCOLUSE ICU");
-      sb.append("  WHERE I.TABSCHEMA = ?");
-      sb.append("    AND I.TABNAME = ?");
-      sb.append("    AND ICU.INDNAME = I.INDNAME");
-      sb.append("    AND ICU.INDSCHEMA = I.INDSCHEMA");
-      sb.append("  ORDER BY I.INDSCHEMA");
-      sb.append("         , I.INDNAME");
-      sb.append("         , ICU.COLSEQ");
-      sb.append(" WITH UR");
-      SQL_IND_TAB = sb.toString();
-   }
-
-   public DB2IndexCache(DB2TableCache tableCache) {
-      // TODO DF: Bad Table "SCHEMA.TABLE" may have 2 indexes with same
-      // "INDNAME": SCHEMA.IX1 and OTHERSCHEMA.IX1 ...
-      super(tableCache, DB2Table.class, "TABNAME", "INDNAME");
+   public DB2IndexCache() {
+      super("INDNAME");
    }
 
    @Override
-   protected JDBCStatement prepareObjectsStatement(JDBCExecutionContext context, DB2Schema db2Schema, DB2Table forTable) throws SQLException {
-
-      String sql;
-      if (forTable != null) {
-         sql = SQL_IND_TAB;
-      } else {
-         sql = SQL_IND_ALL;
-      }
-
-      JDBCPreparedStatement dbStat = context.prepareStatement(sql);
+   protected JDBCStatement prepareObjectsStatement(JDBCExecutionContext context, DB2Schema db2Schema) throws SQLException {
+      JDBCPreparedStatement dbStat = context.prepareStatement(SQL_INDS_ALL);
       dbStat.setString(1, db2Schema.getName());
-      if (forTable != null) {
-         dbStat.setString(2, forTable.getName());
-      }
       return dbStat;
    }
 
    @Override
-   protected DB2Index fetchObject(JDBCExecutionContext context,
-                                  DB2Schema db2Schema,
-                                  DB2Table db2Table,
-                                  String indexName,
-                                  ResultSet dbResult) throws SQLException, DBException {
+   protected DB2Index fetchObject(JDBCExecutionContext context, DB2Schema db2Schema, ResultSet dbResult) throws SQLException,
+                                                                                                        DBException {
 
-      // Index schema may be different from current(owner) schema..
-      String indSchema = JDBCUtils.safeGetStringTrimmed(dbResult, "INDSCHEMA");
-      DB2Schema parentSchema = db2Schema.getDataSource().schemaLookup(context.getProgressMonitor(), db2Schema, indSchema);
+      // Look for related table
+      String tableSchemaName = JDBCUtils.safeGetStringTrimmed(dbResult, "TABSCHEMA");
+      String tableName = JDBCUtils.safeGetStringTrimmed(dbResult, "TABNAME");
+      DB2Schema tableSchema = db2Schema.getDataSource().schemaLookup(context.getProgressMonitor(), db2Schema, tableSchemaName);
+      DB2Table db2Table = tableSchema.getTable(context.getProgressMonitor(), tableName);
 
-      return new DB2Index(parentSchema, db2Table, indexName, dbResult);
+      return new DB2Index(context.getProgressMonitor(), db2Schema, db2Table, dbResult);
    }
 
    @Override
-   protected DB2IndexColumn fetchObjectRow(JDBCExecutionContext context, DB2Table db2Table, DB2Index db2Index, ResultSet dbResult) throws SQLException,
-                                                                                                                                  DBException {
-
-      String columnName = JDBCUtils.safeGetString(dbResult, "COLNAME");
-      Integer colSeq = JDBCUtils.safeGetInteger(dbResult, "COLSEQ");
-      DB2IndexColOrder colOrder = DB2IndexColOrder.valueOf(JDBCUtils.safeGetString(dbResult, "COLORDER"));
-      String collationSchema = JDBCUtils.safeGetString(dbResult, "COLLATIONSCHEMA");
-      String collationNane = JDBCUtils.safeGetString(dbResult, "COLLATIONNAME");
-
-      DB2TableColumn tableColumn = db2Table.getAttribute(context.getProgressMonitor(), columnName);
-      if (tableColumn == null) {
-         log.debug("Column '" + columnName + "' not found in table '" + db2Table.getName() + "' for index '" + db2Index.getName()
-                  + "'");
-         return null;
-      }
-
-      return new DB2IndexColumn(db2Index, tableColumn, colSeq, colOrder, collationSchema, collationNane);
+   protected JDBCStatement prepareChildrenStatement(JDBCExecutionContext context, DB2Schema db2Schema, DB2Index forIndex) throws SQLException {
+      JDBCPreparedStatement dbStat = context.prepareStatement(SQL_COLS_IND);
+      dbStat.setString(1, forIndex.getContainer().getName());
+      dbStat.setString(2, forIndex.getName());
+      return dbStat;
    }
 
    @Override
-   protected void cacheChildren(DB2Index db2Index, List<DB2IndexColumn> rows) {
-      db2Index.setColumns(rows);
+   protected DB2IndexColumn fetchChild(JDBCExecutionContext context, DB2Schema db2Schema, DB2Index db2Index, ResultSet dbResult) throws SQLException,
+                                                                                                                                DBException {
+      return new DB2IndexColumn(context.getProgressMonitor(), db2Index, dbResult);
    }
 
 }
