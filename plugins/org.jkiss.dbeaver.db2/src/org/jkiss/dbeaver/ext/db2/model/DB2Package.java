9d75169e18a3e386a8f8ad3180cf659494cf4e85
titou10 <titou10@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2013-09-28 14:26:21 +0000

    DB2: Added packages properties

--- plugins/org.jkiss.dbeaver.db2/src/org/jkiss/dbeaver/ext/db2/model/DB2Package.java
+++ plugins/org.jkiss.dbeaver.db2/src/org/jkiss/dbeaver/ext/db2/model/DB2Package.java
@@ -29,8 +29,10 @@ import org.jkiss.dbeaver.model.impl.jdbc.cache.JDBCObjectSimpleCache;
 import org.jkiss.dbeaver.model.meta.Association;
 import org.jkiss.dbeaver.model.meta.Property;
 import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
+import org.jkiss.dbeaver.runtime.VoidProgressMonitor;
 import org.jkiss.utils.CommonUtils;
 
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.Timestamp;
 import java.util.Collection;
@@ -51,23 +53,85 @@ public class DB2Package extends DB2SchemaObject implements DBPRefreshableObject 
     private Boolean valid;
     private String owner;
     private DB2OwnerType ownerType;
-    private Timestamp createTime;
+
+    private DB2Schema defaultSchema;
+    private String uniqueId;
+    private Long id;
+    private String version;
+
+    private Integer totalSections;
+    private String dateTimeFormat;
+    private String isolation;
+    private String concurrentAccessResolution;
+    private String blocking;
+    private Boolean insertBuf;
+    private String langLevel;
+    private String funcPath;
+    private Integer queryOpt;
+    private String degree;
+    private Boolean multiNodePlan;
+    private String intraParallel;
+    private String validate;
+    private String dynamicRules;
+    private String sqlerror;
+    private Boolean busTimeSensitive;
+    private Boolean sysTimeSensitive;
+    private Boolean keepDynamic;
+    private Boolean staticAsDynamic;
+
+    private Timestamp firstBindTime;
+    private Timestamp lastBindTime;
+    private Timestamp explicitBindTime;
     private Timestamp alterTime;
+    private Date lastUsed;
+
     private String remarks;
 
     // -----------------------
     // Constructors
     // -----------------------
 
-    public DB2Package(DB2Schema schema, ResultSet dbResult)
+    public DB2Package(DB2Schema schema, ResultSet dbResult) throws DBException
     {
         super(schema, JDBCUtils.safeGetString(dbResult, "PKGNAME"), true);
 
-        this.valid = JDBCUtils.safeGetBoolean(dbResult, "VALID", DB2YesNo.Y.name());
         this.owner = JDBCUtils.safeGetString(dbResult, "OWNER");
         this.ownerType = CommonUtils.valueOf(DB2OwnerType.class, JDBCUtils.safeGetString(dbResult, "OWNERTYPE"));
-        this.createTime = JDBCUtils.safeGetTimestamp(dbResult, "CREATE_TIME");
+
+        String defaultSchemaName = JDBCUtils.safeGetString(dbResult, "DEFAULT_SCHEMA");
+        this.defaultSchema = getDataSource().getSchema(VoidProgressMonitor.INSTANCE, defaultSchemaName);
+
+        this.uniqueId = JDBCUtils.safeGetString(dbResult, "UNIQUE_ID");
+        this.id = JDBCUtils.safeGetLong(dbResult, "PKGID");
+        this.valid = JDBCUtils.safeGetBoolean(dbResult, "VALID", DB2YesNo.Y.name());
+        this.version = JDBCUtils.safeGetString(dbResult, "PKGVERSION");
+
+        this.totalSections = JDBCUtils.safeGetInteger(dbResult, "TOTAL_SECT");
+        this.dateTimeFormat = JDBCUtils.safeGetString(dbResult, "FORMAT");
+        this.isolation = JDBCUtils.safeGetString(dbResult, "ISOLATION");
+        this.concurrentAccessResolution = JDBCUtils.safeGetString(dbResult, "CONCURRENTACCESSRESOLUTION");
+        this.blocking = JDBCUtils.safeGetString(dbResult, "BLOCKING");
+        this.insertBuf = JDBCUtils.safeGetBoolean(dbResult, "INSERT_BUF", DB2YesNo.Y.name());
+        this.langLevel = JDBCUtils.safeGetString(dbResult, "LANG_LEVEL");
+        this.funcPath = JDBCUtils.safeGetString(dbResult, "FUNC_PATH");
+        this.queryOpt = JDBCUtils.safeGetInteger(dbResult, "QUERYOPT");
+        this.degree = JDBCUtils.safeGetString(dbResult, "DEGREE");
+        this.multiNodePlan = JDBCUtils.safeGetBoolean(dbResult, "MULTINODE_PLANS", DB2YesNo.Y.name());
+        this.intraParallel = JDBCUtils.safeGetString(dbResult, "INTRA_PARALLEL");
+        this.validate = JDBCUtils.safeGetString(dbResult, "VALIDATE");
+        this.dynamicRules = JDBCUtils.safeGetString(dbResult, "DYNAMICRULES");
+        this.sqlerror = JDBCUtils.safeGetString(dbResult, "SQLERROR");
+        this.busTimeSensitive = JDBCUtils.safeGetBoolean(dbResult, "BUSTIMESENSITIVE", DB2YesNo.Y.name());
+        this.sysTimeSensitive = JDBCUtils.safeGetBoolean(dbResult, "SYSTIMESENSITIVE", DB2YesNo.Y.name());
+        this.keepDynamic = JDBCUtils.safeGetBoolean(dbResult, "KEEPDYNAMIC", DB2YesNo.Y.name());
+        this.staticAsDynamic = JDBCUtils.safeGetBoolean(dbResult, "STATICASDYNAMIC", DB2YesNo.Y.name());
+
+        this.firstBindTime = JDBCUtils.safeGetTimestamp(dbResult, "CREATE_TIME");
+        this.lastBindTime = JDBCUtils.safeGetTimestamp(dbResult, "LAST_BIND_TIME");
+        this.explicitBindTime = JDBCUtils.safeGetTimestamp(dbResult, "EXPLICIT_BIND_TIME");
         this.alterTime = JDBCUtils.safeGetTimestamp(dbResult, "ALTER_TIME");
+        this.lastUsed = JDBCUtils.safeGetDate(dbResult, "LASTUSED");
+
         this.remarks = JDBCUtils.safeGetString(dbResult, "REMARKS");
 
         packageDepCache = new JDBCObjectSimpleCache<DB2Package, DB2PackageDep>(DB2PackageDep.class, C_DEP, schema.getName(),
@@ -102,41 +166,198 @@ public class DB2Package extends DB2SchemaObject implements DBPRefreshableObject 
     // -----------------
     // Properties
     // -----------------
-    @Property(viewable = true, editable = false)
-    public Boolean getValid()
-    {
-        return valid;
-    }
 
-    @Property(viewable = false, editable = false, category = DB2Constants.CAT_OWNER)
-    public String getOwner()
-    {
-        return owner;
-    }
-
-    @Property(viewable = false, editable = false, category = DB2Constants.CAT_OWNER)
+    @Property(viewable = true, order = 3, category = DB2Constants.CAT_OWNER)
     public DB2OwnerType getOwnerType()
     {
         return ownerType;
     }
 
-    @Property(viewable = false, editable = false, category = DB2Constants.CAT_DATETIME)
-    public Timestamp getCreateTime()
+    @Property(viewable = true, order = 4, category = DB2Constants.CAT_OWNER)
+    public String getOwner()
     {
-        return createTime;
+        return owner;
     }
 
-    @Property(viewable = false, editable = false, category = DB2Constants.CAT_DATETIME)
+    @Property(viewable = false, order = 5)
+    public String getUniqueId()
+    {
+        return uniqueId;
+    }
+
+    @Property(viewable = true, order = 6)
+    public Long getId()
+    {
+        return id;
+    }
+
+    @Property(viewable = true, order = 7)
+    public String getVersion()
+    {
+        return version;
+    }
+
+    @Property(viewable = true, order = 8)
+    public Boolean getValid()
+    {
+        return valid;
+    }
+
+    @Override
+    @Property(viewable = false, order = 9)
+    public String getDescription()
+    {
+        return remarks;
+    }
+
+    @Property(viewable = true, order = 20)
+    public DB2Schema getDefaultSchema()
+    {
+        return defaultSchema;
+    }
+
+    @Property(viewable = true, order = 21)
+    public Integer getTotalSections()
+    {
+        return totalSections;
+    }
+
+    @Property(viewable = false, order = 22)
+    public String getDateTimeFormat()
+    {
+        return dateTimeFormat;
+    }
+
+    @Property(viewable = false, order = 23)
+    public String getIsolation()
+    {
+        return isolation;
+    }
+
+    @Property(viewable = false, order = 24)
+    public String getConcurrentAccessResolution()
+    {
+        return concurrentAccessResolution;
+    }
+
+    @Property(viewable = true, order = 25)
+    public String getBlocking()
+    {
+        return blocking;
+    }
+
+    @Property(viewable = false, order = 26)
+    public Boolean getInsertBuf()
+    {
+        return insertBuf;
+    }
+
+    @Property(viewable = false, order = 27)
+    public String getLangLevel()
+    {
+        return langLevel;
+    }
+
+    @Property(viewable = false, order = 28)
+    public String getFuncPath()
+    {
+        return funcPath;
+    }
+
+    @Property(viewable = true, order = 29)
+    public Integer getQueryOpt()
+    {
+        return queryOpt;
+    }
+
+    @Property(viewable = true, order = 30)
+    public String getDegree()
+    {
+        return degree;
+    }
+
+    @Property(viewable = false, order = 31)
+    public Boolean getMultiNodePlan()
+    {
+        return multiNodePlan;
+    }
+
+    @Property(viewable = false, order = 32)
+    public String getIntraParallel()
+    {
+        return intraParallel;
+    }
+
+    @Property(viewable = false, order = 33)
+    public String getValidate()
+    {
+        return validate;
+    }
+
+    @Property(viewable = false, order = 34)
+    public String getDynamicRules()
+    {
+        return dynamicRules;
+    }
+
+    @Property(viewable = false, order = 35)
+    public String getSqlerror()
+    {
+        return sqlerror;
+    }
+
+    @Property(viewable = false, order = 36)
+    public Boolean getBusTimeSensitive()
+    {
+        return busTimeSensitive;
+    }
+
+    @Property(viewable = false, order = 37)
+    public Boolean getSysTimeSensitive()
+    {
+        return sysTimeSensitive;
+    }
+
+    @Property(viewable = false, order = 38)
+    public Boolean getKeepDynamic()
+    {
+        return keepDynamic;
+    }
+
+    @Property(viewable = false, order = 39)
+    public Boolean getStaticAsDynamic()
+    {
+        return staticAsDynamic;
+    }
+
+    @Property(viewable = false, order = 50, category = DB2Constants.CAT_DATETIME)
+    public Timestamp getFirstBindTime()
+    {
+        return firstBindTime;
+    }
+
+    @Property(viewable = false, order = 51, category = DB2Constants.CAT_DATETIME)
+    public Timestamp getLastBindTime()
+    {
+        return lastBindTime;
+    }
+
+    @Property(viewable = false, order = 52, category = DB2Constants.CAT_DATETIME)
+    public Timestamp getExplicitBindTime()
+    {
+        return explicitBindTime;
+    }
+
+    @Property(viewable = false, order = 53, category = DB2Constants.CAT_DATETIME)
     public Timestamp getAlterTime()
     {
         return alterTime;
     }
 
-    @Override
-    @Property(viewable = true, editable = false)
-    public String getDescription()
+    @Property(viewable = false, order = 54, category = DB2Constants.CAT_DATETIME)
+    public Date getLastUsed()
     {
-        return remarks;
+        return lastUsed;
     }
 
 }
