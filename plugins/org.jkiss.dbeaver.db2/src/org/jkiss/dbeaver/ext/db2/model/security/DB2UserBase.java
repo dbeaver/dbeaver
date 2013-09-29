a6c875e90e53a6c846db3ba0634a02baacd26346
titou10 <titou10@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2013-09-29 15:04:36 +0000

    DB2: User/Group authorisations: split authorisation per objectType in its own tab Former-commit-id: 8f9e50a7c2835c47b3698da11da328a08485d133

--- plugins/org.jkiss.dbeaver.db2/src/org/jkiss/dbeaver/ext/db2/model/security/DB2UserBase.java
+++ plugins/org.jkiss.dbeaver.db2/src/org/jkiss/dbeaver/ext/db2/model/security/DB2UserBase.java
@@ -29,6 +29,7 @@ import org.jkiss.dbeaver.model.meta.Property;
 import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
 
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -52,14 +53,10 @@ public abstract class DB2UserBase extends DB2GlobalObject implements DBAUser, DB
     }
 
     // -----------------
-    // Associations
+    // Business Contract
     // -----------------
 
-    @Association
-    public Collection<DB2UserAuth> getUserAuths(DBRProgressMonitor monitor) throws DBException
-    {
-        return userAuthCache.getObjects(monitor, this);
-    }
+    public abstract DB2AuthIDType getType();
 
     @Override
     public boolean refreshObject(DBRProgressMonitor monitor) throws DBException
@@ -68,7 +65,82 @@ public abstract class DB2UserBase extends DB2GlobalObject implements DBAUser, DB
         return true;
     }
 
-    public abstract DB2AuthIDType getType();
+    // -----------------
+    // Associations
+    // -----------------
+
+    @Association
+    public Collection<DB2UserAuthBase> getUserAuths(DBRProgressMonitor monitor) throws DBException
+    {
+        return userAuthCache.getObjects(monitor, this);
+    }
+
+    // DF: all of those could probably cached also...
+
+    @Association
+    public Collection<DB2UserAuthTable> getTablesAuths(DBRProgressMonitor monitor) throws DBException
+    {
+        Collection<DB2UserAuthTable> listTablesAuths = new ArrayList<DB2UserAuthTable>();
+        for (DB2UserAuthBase db2UserAuth : userAuthCache.getObjects(monitor, this)) {
+            if (db2UserAuth instanceof DB2UserAuthTable) {
+                listTablesAuths.add((DB2UserAuthTable) db2UserAuth);
+            }
+
+        }
+        return listTablesAuths;
+    }
+
+    @Association
+    public Collection<DB2UserAuthView> getViewsAuths(DBRProgressMonitor monitor) throws DBException
+    {
+        Collection<DB2UserAuthView> listViewsAuths = new ArrayList<DB2UserAuthView>();
+        for (DB2UserAuthBase db2UserAuth : userAuthCache.getObjects(monitor, this)) {
+            if (db2UserAuth instanceof DB2UserAuthView) {
+                listViewsAuths.add((DB2UserAuthView) db2UserAuth);
+            }
+
+        }
+        return listViewsAuths;
+    }
+
+    @Association
+    public Collection<DB2UserAuthIndex> getIndexesAuths(DBRProgressMonitor monitor) throws DBException
+    {
+        Collection<DB2UserAuthIndex> listIndexesAuths = new ArrayList<DB2UserAuthIndex>();
+        for (DB2UserAuthBase db2UserAuth : userAuthCache.getObjects(monitor, this)) {
+            if (db2UserAuth instanceof DB2UserAuthIndex) {
+                listIndexesAuths.add((DB2UserAuthIndex) db2UserAuth);
+            }
+
+        }
+        return listIndexesAuths;
+    }
+
+    @Association
+    public Collection<DB2UserAuthSequence> getSequencesAuths(DBRProgressMonitor monitor) throws DBException
+    {
+        Collection<DB2UserAuthSequence> listSequenceAuths = new ArrayList<DB2UserAuthSequence>();
+        for (DB2UserAuthBase db2UserAuth : userAuthCache.getObjects(monitor, this)) {
+            if (db2UserAuth instanceof DB2UserAuthSequence) {
+                listSequenceAuths.add((DB2UserAuthSequence) db2UserAuth);
+            }
+
+        }
+        return listSequenceAuths;
+    }
+
+    @Association
+    public Collection<DB2UserAuthTablespace> getTablespacesAuths(DBRProgressMonitor monitor) throws DBException
+    {
+        Collection<DB2UserAuthTablespace> listTablespacesAuths = new ArrayList<DB2UserAuthTablespace>();
+        for (DB2UserAuthBase db2UserAuth : userAuthCache.getObjects(monitor, this)) {
+            if (db2UserAuth instanceof DB2UserAuthTablespace) {
+                listTablespacesAuths.add((DB2UserAuthTablespace) db2UserAuth);
+            }
+
+        }
+        return listTablespacesAuths;
+    }
 
     // -----------------
     // Properties
