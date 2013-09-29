8f9e50a7c2835c47b3698da11da328a08485d133
titou10 <titou10@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2013-09-29 15:04:36 +0000

    DB2: User/Group authorisations: split authorisation per objectType in its own tab

---
+++ plugins/org.jkiss.dbeaver.db2/src/org/jkiss/dbeaver/ext/db2/model/security/DB2UserAuthTable.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2013      Denis Forveille titou10.titou10@gmail.com
+ * Copyright (C) 2010-2013 Serge Rieder serge@jkiss.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.jkiss.dbeaver.ext.db2.model.security;
+
+import org.jkiss.dbeaver.DBException;
+import org.jkiss.dbeaver.ext.db2.model.DB2TableBase;
+import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
+
+import java.sql.ResultSet;
+
+/**
+ * DB2 User or Group Authorisations on Tables
+ * 
+ * @author Denis Forveille
+ */
+public class DB2UserAuthTable extends DB2UserAuthTableBase {
+
+    // -----------------------
+    // Constructors
+    // -----------------------
+
+    public DB2UserAuthTable(DBRProgressMonitor monitor, DB2UserBase userOrGroup, DB2TableBase db2TableBase, ResultSet resultSet)
+        throws DBException
+    {
+        super(monitor, userOrGroup, db2TableBase, resultSet);
+    }
+
+}
