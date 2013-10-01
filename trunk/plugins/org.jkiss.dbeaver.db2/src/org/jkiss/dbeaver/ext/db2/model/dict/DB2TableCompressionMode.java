eb49c6f8fd65d42659c0b9cf98e4c17781b10fa8
titou10 <titou10@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2013-10-01 13:02:40 +0000

    DB2: Added missing table attributes Former-commit-id: a36bd2b1c8dd426bde799d580bc4ea38b2042858

---
+++ trunk/plugins/org.jkiss.dbeaver.db2/src/org/jkiss/dbeaver/ext/db2/model/dict/DB2TableCompressionMode.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2013      Denis Forveille titou10.titou10@gmail.com
+ * Copyright (C) 2010-2013 Serge Rieder serge@jkiss.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.jkiss.dbeaver.ext.db2.model.dict;
+
+import org.jkiss.dbeaver.model.DBPNamedObject;
+
+/**
+ * DB2 Compression Mode
+ * 
+ * @author Denis Forveille
+ */
+public enum DB2TableCompressionMode implements DBPNamedObject {
+    B("Value + Row compression"),
+
+    N("None"),
+
+    R("Row"),
+
+    V("Value Compression");
+
+    private String name;
+
+    // -----------
+    // Constructor
+    // -----------
+
+    private DB2TableCompressionMode(String name)
+    {
+        this.name = name;
+    }
+
+    // -----------------------
+    // Display @Property Value
+    // -----------------------
+    @Override
+    public String toString()
+    {
+        return name;
+    }
+
+    // ----------------
+    // Standard Getters
+    // ----------------
+    @Override
+    public String getName()
+    {
+        return name;
+    }
+}
