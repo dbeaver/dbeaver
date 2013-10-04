e07be0829bde8db8ed40ab4d7f50467e7c00a418
titou10 <titou10@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2013-10-04 03:22:59 +0000

    DB2 : XML Schema dependencies

--- plugins/org.jkiss.dbeaver.db2/src/org/jkiss/dbeaver/ext/db2/model/DB2XMLSchemaDep.java
+++ plugins/org.jkiss.dbeaver.db2/src/org/jkiss/dbeaver/ext/db2/model/DB2XMLSchemaDep.java
@@ -20,7 +20,7 @@ package org.jkiss.dbeaver.ext.db2.model;
 
 import org.jkiss.dbeaver.DBException;
 import org.jkiss.dbeaver.ext.db2.editors.DB2ObjectType;
-import org.jkiss.dbeaver.ext.db2.model.dict.DB2PackageDepType;
+import org.jkiss.dbeaver.ext.db2.model.dict.DB2XMLSchemaDepType;
 import org.jkiss.dbeaver.model.impl.jdbc.JDBCUtils;
 import org.jkiss.dbeaver.model.meta.Property;
 import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
@@ -30,50 +30,37 @@ import org.jkiss.utils.CommonUtils;
 import java.sql.ResultSet;
 
 /**
- * DB2 Package Dependency
+ * DB2 XML Schema Dependency
  * 
  * @author Denis Forveille
  */
-public class DB2PackageDep extends DB2Object<DB2Package> {
+public class DB2XMLSchemaDep extends DB2Object<DB2XMLSchema> {
 
-    private DB2PackageDepType packageDepType;
+    private DB2XMLSchemaDepType xmlSchemaDepType;
     private DB2Schema depSchema;
     private String depModuleName;
     private String depModuleId;
     private String tabAuth;
-    private String binder;
-    private String binderType;
-    private String varAuth;
-    private String version;
 
     // -----------------------
     // Constructors
     // -----------------------
-    public DB2PackageDep(DB2Package db2Package, ResultSet resultSet) throws DBException
+    public DB2XMLSchemaDep(DB2XMLSchema db2XMLSchema, ResultSet resultSet) throws DBException
     {
         // TODO DF: Bad should be BTYPE+BSCHEMA+BNAME
-        super(db2Package, JDBCUtils.safeGetString(resultSet, "BNAME"), true);
+        super(db2XMLSchema, JDBCUtils.safeGetString(resultSet, "BNAME"), true);
 
         this.depModuleName = JDBCUtils.safeGetString(resultSet, "BMODULENAME");
         this.depModuleId = JDBCUtils.safeGetString(resultSet, "BMODULEID");
         this.tabAuth = JDBCUtils.safeGetString(resultSet, "TABAUTH");
-        this.binder = JDBCUtils.safeGetString(resultSet, "BINDER");
-        this.binderType = JDBCUtils.safeGetString(resultSet, "BINDERTYPE");
-        this.varAuth = JDBCUtils.safeGetString(resultSet, "VARAUTH");
-        // this.uniqueId = JDBCUtils.safeGetString(resultSet, "UNIQUE_ID");
-        this.version = JDBCUtils.safeGetString(resultSet, "PKGVERSION");
 
         String depSchemaName = JDBCUtils.safeGetStringTrimmed(resultSet, "BSCHEMA");
 
-        // Some dependencies are just numbers, not valid in a enum...
         String depType = JDBCUtils.safeGetString(resultSet, "BTYPE");
-        this.packageDepType = CommonUtils.valueOf(DB2PackageDepType.class, depType);
-        if (packageDepType == null) {
-            this.packageDepType = CommonUtils.valueOf(DB2PackageDepType.class, DB2PackageDepType.FAKE_PREFIX + depType);
-        }
+        this.xmlSchemaDepType = CommonUtils.valueOf(DB2XMLSchemaDepType.class, depType);
 
-        if (this.packageDepType != null) {
-            DB2ObjectType db2ObjectType = packageDepType.getDb2ObjectType();
+        if (this.xmlSchemaDepType != null) {
+            DB2ObjectType db2ObjectType = xmlSchemaDepType.getDb2ObjectType();
             if (db2ObjectType != null) {
                 depSchema = getDataSource().getSchemaCache().getCachedObject(depSchemaName);
             }
@@ -92,9 +79,9 @@ public class DB2PackageDep extends DB2Object<DB2Package> {
     }
 
     @Property(viewable = true, editable = false, order = 2)
-    public DB2PackageDepType getPackageDepType()
+    public DB2XMLSchemaDepType getXmlSchemaDepType()
     {
-        return packageDepType;
+        return xmlSchemaDepType;
     }
 
     @Property(viewable = true, editable = false, order = 3)
@@ -106,7 +93,7 @@ public class DB2PackageDep extends DB2Object<DB2Package> {
     @Property(viewable = true, editable = false, order = 4)
     public DBSObject getDepObject(DBRProgressMonitor monitor) throws DBException
     {
-        if (packageDepType == null || packageDepType.getDb2ObjectType() == null) {
+        if (xmlSchemaDepType == null || xmlSchemaDepType.getDb2ObjectType() == null) {
             return null;
         }
         // Some dependncies are in Modules...Concatenate modulename and name
@@ -114,25 +101,7 @@ public class DB2PackageDep extends DB2Object<DB2Package> {
         if (depModuleName != null) {
             name = depModuleName + "." + name;
         }
-        return packageDepType.getDb2ObjectType().findObject(monitor, depSchema, name);
-    }
-
-    @Property(viewable = true, editable = false, order = 5)
-    public String getVersion()
-    {
-        return version;
-    }
-
-    @Property(viewable = true, editable = false, order = 6)
-    public String getBinder()
-    {
-        return binder;
-    }
-
-    @Property(viewable = true, editable = false, order = 7)
-    public String getBinderType()
-    {
-        return binderType;
+        return xmlSchemaDepType.getDb2ObjectType().findObject(monitor, depSchema, name);
     }
 
     @Property(viewable = false, editable = false)
@@ -145,12 +114,6 @@ public class DB2PackageDep extends DB2Object<DB2Package> {
     public String getTabAuth()
     {
         return tabAuth;
-    }
-
-    @Property(viewable = false, editable = false)
-    public String getVarAuth()
-    {
-        return varAuth;
     }
 
 }
