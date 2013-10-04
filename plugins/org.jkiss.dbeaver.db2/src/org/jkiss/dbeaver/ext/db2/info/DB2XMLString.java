301872f7bf282624b7e9b3af51d36166dc2ced1e
titou10 <titou10@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2013-10-04 02:49:45 +0000

    DB2: Added global XML Strings

---
+++ plugins/org.jkiss.dbeaver.db2/src/org/jkiss/dbeaver/ext/db2/info/DB2XMLString.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (C) 2013      Denis Forveille titou10.titou10@gmail.com
+ * Copyright (C) 2010-2013 Serge Rieder serge@jkiss.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.jkiss.dbeaver.ext.db2.info;
+
+import org.jkiss.dbeaver.ext.db2.model.DB2DataSource;
+import org.jkiss.dbeaver.model.DBPDataSource;
+import org.jkiss.dbeaver.model.impl.jdbc.JDBCUtils;
+import org.jkiss.dbeaver.model.meta.Property;
+import org.jkiss.dbeaver.model.struct.DBSObject;
+
+import java.sql.ResultSet;
+
+/**
+ * DB2 XML Strings
+ * 
+ * @author Denis Forveille
+ */
+public class DB2XMLString implements DBSObject {
+
+    private DB2DataSource dataSource;
+
+    private Integer stringId;
+    private String string;
+    private String stringUTF8;
+
+    // -----------------------
+    // Constructors
+    // -----------------------
+    public DB2XMLString(DB2DataSource dataSource, ResultSet dbResult)
+    {
+        this.dataSource = dataSource;
+
+        this.stringId = JDBCUtils.safeGetInteger(dbResult, "STRINGID");
+        this.string = JDBCUtils.safeGetString(dbResult, "STRING");
+        this.stringUTF8 = JDBCUtils.safeGetString(dbResult, "STRING_UTF8");
+    }
+
+    @Override
+    public DBPDataSource getDataSource()
+    {
+        return dataSource;
+    }
+
+    @Override
+    public DBSObject getParentObject()
+    {
+        return dataSource.getContainer();
+    }
+
+    @Override
+    public boolean isPersisted()
+    {
+        return false;
+    }
+
+    @Override
+    public String getDescription()
+    {
+        return null;
+    }
+
+    // -----------------
+    // Properties
+    // -----------------
+
+    @Override
+    @Property(viewable = true, order = 1)
+    public String getName()
+    {
+        return stringId.toString();
+    }
+
+    @Property(viewable = true, order = 2)
+    public String getString()
+    {
+        return string;
+    }
+
+    @Property(viewable = true, order = 3)
+    public String getStringUTF8()
+    {
+        return stringUTF8;
+    }
+
+}
