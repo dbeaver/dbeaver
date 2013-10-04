fc33771188fb54ec02a8eaf99208c5fb5fd25e57
titou10 <titou10@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2013-10-04 12:11:21 +0000

    DB2: Work on managers Former-commit-id: fdbb3cbe4ce86023410447321218c476558a05a2

---
+++ plugins/org.jkiss.dbeaver.db2/src/org/jkiss/dbeaver/ext/db2/manager/DB2AliasManager.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2013      Denis Forveille titou10.titou10@gmail.com
+ * Copyright (C) 2010-2013 Serge Rieder serge@jkiss.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.jkiss.dbeaver.ext.db2.manager;
+
+import org.jkiss.dbeaver.ext.db2.model.DB2Alias;
+import org.jkiss.dbeaver.ext.db2.model.DB2Schema;
+
+/**
+ * DB2 Alias Manager
+ * 
+ * @author Denis Forveille
+ */
+public class DB2AliasManager extends DB2AbstractDropOnlyManager<DB2Alias, DB2Schema> {
+
+    private static final String SQL_DROP_MODULE = "DROP ALIAS %s FOR MODULE";
+    private static final String SQL_DROP_SEQUENCE = "DROP ALIAS %s FOR SEQUENCE";
+    private static final String SQL_DROP_TABLE = "DROP ALIAS %s FOR TABLE";
+
+    @Override
+    public String buildDropStatement(DB2Alias db2Alias)
+    {
+        String fullyQualifiedName = db2Alias.getFullQualifiedName();
+
+        switch (db2Alias.getType()) {
+        case MODULE:
+            return String.format(SQL_DROP_MODULE, fullyQualifiedName);
+        case SEQUENCE:
+            return String.format(SQL_DROP_SEQUENCE, fullyQualifiedName);
+        case TABLE:
+            return String.format(SQL_DROP_TABLE, fullyQualifiedName);
+        default:
+            throw new IllegalArgumentException(db2Alias.getType() + " as DB2AliasTYpe is not supported");
+        }
+    }
+}
