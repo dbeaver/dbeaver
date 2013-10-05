9ca713d496ef9440020424476d971bd73feed3b1
titou10 <titou10@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2013-10-05 15:36:08 +0000

    DB2: bug fix Former-commit-id: 9d1661e794d44470c4f924758b8336ed923b9bc9

--- trunk/plugins/org.jkiss.dbeaver.db2/src/org/jkiss/dbeaver/ext/db2/model/DB2Table.java
+++ trunk/plugins/org.jkiss.dbeaver.db2/src/org/jkiss/dbeaver/ext/db2/model/DB2Table.java
@@ -181,7 +181,9 @@ public class DB2Table extends DB2TableBase implements DBPNamedObject2, DBPRefres
         // DF: Refresh parent cache ie cache for schemaof indexes/triggers different from table schema?
         tableIndexCache.clearCache();
         tableTriggerCache.clearCache();
-        partitionCache.clearCache();
+        if (partitionCache != null) {
+            partitionCache.clearCache();
+        }
 
         getContainer().getConstraintCache().clearObjectCache(this);
         getContainer().getAssociationCache().clearObjectCache(this);
