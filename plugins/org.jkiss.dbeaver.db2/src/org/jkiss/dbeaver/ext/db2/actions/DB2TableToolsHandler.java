b985641ec286e3cdb207c4de4fed33518fe3ee97
jurgen <serge-rider@users.noreply.github.com>
2013-10-15 06:41:42 +0000

    Base DB2 tools dialog. Dialogs refactor
    Former-commit-id: 621bc95411a72f996bc6cc7fe31a5e74b17d437f

--- plugins/org.jkiss.dbeaver.db2/src/org/jkiss/dbeaver/ext/db2/actions/DB2TableToolsHandler.java
+++ plugins/org.jkiss.dbeaver.db2/src/org/jkiss/dbeaver/ext/db2/actions/DB2TableToolsHandler.java
@@ -21,27 +21,16 @@ package org.jkiss.dbeaver.ext.db2.actions;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchPartSite;
 import org.eclipse.ui.handlers.HandlerUtil;
-import org.jkiss.dbeaver.DBException;
-import org.jkiss.dbeaver.core.DBeaverUI;
-import org.jkiss.dbeaver.ext.db2.DB2Utils;
 import org.jkiss.dbeaver.ext.db2.model.DB2DataSource;
-import org.jkiss.dbeaver.ext.db2.model.DB2Index;
 import org.jkiss.dbeaver.ext.db2.model.DB2Table;
-import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
-import org.jkiss.dbeaver.model.runtime.DBRRunnableWithProgress;
 import org.jkiss.dbeaver.runtime.RuntimeUtils;
-import org.jkiss.dbeaver.runtime.VoidProgressMonitor;
 import org.jkiss.dbeaver.ui.UIUtils;
 
-import java.lang.reflect.InvocationTargetException;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
@@ -77,28 +66,13 @@ public class DB2TableToolsHandler extends AbstractHandler {
 
         if (!tables.isEmpty()) {
             IWorkbenchPart activePart = HandlerUtil.getActivePart(event);
-            Shell activeShell = HandlerUtil.getActiveShell(event);
 
-            try {
-                if (event.getCommand().getId().equals(CMD_REORG_ID)) {
-                    performReorg(activeShell, tables.get(0));
-                } else if (event.getCommand().getId().equals(CMD_REORGIX_ID)) {
-                    performReorgIx(activePart.getSite(), dataSource, tables);
-                } else if (event.getCommand().getId().equals(CMD_RUNSTATS_ID)) {
-                    performRunstats(activePart.getSite(), dataSource, tables);
-                }
-                // TOOD DF: refresh DB2Table
-                // db2Table.refreshObject(VoidProgressMonitor.INSTANCE);
-                // DBNModel.getInstance().refreshNodeContent(sourceSchema, this, DBNEvent.NodeChange.REFRESH);
-                // node.refreshNode(monitor, source);
-            } catch (InvocationTargetException e) {
-                UIUtils.showErrorDialog(activeShell, "Error", e.getTargetException().getMessage());
-            } catch (InterruptedException e) {
-                // NOP
-            } catch (SQLException e) {
-                UIUtils.showErrorDialog(activeShell, "Error", e.getMessage());
-            } catch (DBException e) {
-                UIUtils.showErrorDialog(activeShell, "Error", e.getMessage());
+            if (event.getCommand().getId().equals(CMD_REORG_ID)) {
+                performReorg(activePart.getSite(), dataSource, tables);
+            } else if (event.getCommand().getId().equals(CMD_REORGIX_ID)) {
+                performReorgIx(activePart.getSite(), dataSource, tables);
+            } else if (event.getCommand().getId().equals(CMD_RUNSTATS_ID)) {
+                performRunstats(activePart.getSite(), dataSource, tables);
             }
         }
 
@@ -108,36 +82,18 @@ public class DB2TableToolsHandler extends AbstractHandler {
     // -------
     // Helpers
     // -------
-    private void performReorg(Shell shell, final DB2Table db2Table) throws InvocationTargetException, InterruptedException,
-        SQLException, DBException
+    private void performReorg(final IWorkbenchPartSite partSite, DB2DataSource dataSource, final Collection<DB2Table> tables)
     {
 
-        List<String> listTempTsNames = DB2Utils.getListOfUsableTempTsNames(VoidProgressMonitor.INSTANCE, db2Table.getDataSource());
-
-        List<String> listIndexNames = new ArrayList<String>();
-        for (DB2Index db2Index : db2Table.getIndexes(VoidProgressMonitor.INSTANCE)) {
-            listIndexNames.add(db2Index.getFullQualifiedName());
-        }
-
-        DB2TableReorgDialog dialog = new DB2TableReorgDialog(shell, db2Table, listTempTsNames, listIndexNames);
-        if (dialog.open() != IDialogConstants.OK_ID) {
-            return;
-        }
-
-        final String sql = dialog.getCmdText();
-
-        DBeaverUI.runInProgressService(new DBRRunnableWithProgress() {
+        DB2TableReorgDialog2 dialog = new DB2TableReorgDialog2(partSite, dataSource, tables.iterator().next());
+        dialog.open();
+        dialog.setOnSuccess(new Runnable() {
             @Override
-            public void run(DBRProgressMonitor monitor) throws InvocationTargetException, InterruptedException
+            public void run()
             {
-                try {
-                    DB2Utils.callAdminCmd(monitor, db2Table.getDataSource(), sql);
-                } catch (SQLException e) {
-                    throw new InvocationTargetException(e);
-                }
+                UIUtils.showMessageBox(partSite.getShell(), "OK", "Reorg finished", SWT.ICON_INFORMATION);
             }
         });
-        UIUtils.showMessageBox(shell, "OK", "REORG OK..", SWT.ICON_INFORMATION);
     }
 
     private void performReorgIx(final IWorkbenchPartSite partSite, DB2DataSource dataSource, final Collection<DB2Table> tables)
