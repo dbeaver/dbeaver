8fb22249b8de346eabba64dc5a2ca610415dad62
jurgen <jurgen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2013-12-12 18:58:47 +0000

    Driver name in connection page
    Former-commit-id: ff06ec65bbc07638b5b0386aac5466765ab1361a

--- trunk/plugins/org.jkiss.dbeaver.generic/src/org/jkiss/dbeaver/ext/generic/GenericMessages.java
+++ trunk/plugins/org.jkiss.dbeaver.generic/src/org/jkiss/dbeaver/ext/generic/GenericMessages.java
@@ -22,67 +22,46 @@ package org.jkiss.dbeaver.ext.generic;
 import org.eclipse.osgi.util.NLS;
 
 public class GenericMessages extends NLS {
-	static final String BUNDLE_NAME = "org.jkiss.dbeaver.ext.generic.GenericResources"; //$NON-NLS-1$
+    static final String BUNDLE_NAME = "org.jkiss.dbeaver.ext.generic.GenericResources"; //$NON-NLS-1$
+    public static String dialog_connection_advanced_tab;
 
+    public static String dialog_connection_advanced_tab_tooltip;
 
-	public static String dialog_connection_advanced_tab;
+    public static String dialog_connection_browse_button;
 
+    public static String dialog_connection_database_schema_label;
 
-	public static String dialog_connection_advanced_tab_tooltip;
+    public static String dialog_connection_db_file_chooser_text;
 
+    public static String dialog_connection_db_folder_chooser_message;
 
-	public static String dialog_connection_browse_button;
+    public static String dialog_connection_db_folder_chooser_text;
 
+    public static String dialog_connection_edit_driver_button;
 
-	public static String dialog_connection_database_schema_label;
+    public static String dialog_connection_general_tab;
 
+    public static String dialog_connection_general_tab_tooltip;
 
-	public static String dialog_connection_db_file_chooser_text;
+    public static String dialog_connection_host_label;
+    public static String dialog_connection_driver;
+    public static String dialog_connection_jdbc_url_;
 
+    public static String dialog_connection_password_label;
 
-	public static String dialog_connection_db_folder_chooser_message;
+    public static String dialog_connection_path_label;
 
+    public static String dialog_connection_port_label;
 
-	public static String dialog_connection_db_folder_chooser_text;
+    public static String dialog_connection_server_label;
 
+    public static String dialog_connection_test_connection_button;
 
-	public static String dialog_connection_edit_driver_button;
-
-
-	public static String dialog_connection_general_tab;
-
-
-	public static String dialog_connection_general_tab_tooltip;
-
-
-	public static String dialog_connection_host_label;
-
-
-	public static String dialog_connection_jdbc_url_;
-
-
-	public static String dialog_connection_password_label;
-
-
-	public static String dialog_connection_path_label;
-
-
-	public static String dialog_connection_port_label;
-
-
-	public static String dialog_connection_server_label;
-
-
-	public static String dialog_connection_test_connection_button;
-
-
-	public static String dialog_connection_user_name_label;
-
-	static {
-		// initialize resource bundle
-		NLS.initializeMessages(BUNDLE_NAME, GenericMessages.class);
-	}
-
-	private GenericMessages() {
-	}
+    public static String dialog_connection_user_name_label;
+    static {
+        // initialize resource bundle
+        NLS.initializeMessages(BUNDLE_NAME, GenericMessages.class);
+    }
+    private GenericMessages() {
+    }
 }
