a0e1c68f98142f24e305deb734c42f10daea6500
jurgen <serge-rider@users.noreply.github.com>
2014-01-24 14:28:03 +0000

    SQL editor output tab

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/core/CoreMessages.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/core/CoreMessages.java
@@ -997,6 +997,7 @@ public class CoreMessages extends NLS {
 	public static String editors_sql_error_execution_plan_title;
 	public static String editors_sql_execution_log;
 	public static String editors_sql_explain_plan;
+    public static String editors_sql_output;
 	public static String editors_sql_job_execute_query;
 	public static String editors_sql_job_execute_script;
 	public static String editors_sql_save_on_close_message;
