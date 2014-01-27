53073aa94faec6d50100d2baf9f2515fe9583640
jurgen <serge-rider@users.noreply.github.com>
2014-01-27 21:06:42 +0000

    MySQL maintenance tools

--- plugins/org.jkiss.dbeaver.mysql/src/org/jkiss/dbeaver/ext/mysql/tools/maintenance/MySQLTableToolDialog.java
+++ plugins/org.jkiss.dbeaver.mysql/src/org/jkiss/dbeaver/ext/mysql/tools/maintenance/MySQLTableToolDialog.java
@@ -17,12 +17,24 @@
  */
 package org.jkiss.dbeaver.ext.mysql.tools.maintenance;
 
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.ui.IWorkbenchPartSite;
 import org.jkiss.dbeaver.ext.mysql.model.MySQLDataSource;
 import org.jkiss.dbeaver.ext.mysql.model.MySQLTable;
+import org.jkiss.dbeaver.ui.DBIcon;
+import org.jkiss.dbeaver.ui.UIUtils;
 import org.jkiss.dbeaver.ui.dialogs.sql.GenerateSQLDialog;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * Super class for handling dialogs related to table tools
@@ -33,9 +45,11 @@ import java.util.Collection;
 public abstract class MySQLTableToolDialog extends GenerateSQLDialog {
 
     protected final Collection<MySQLTable> selectedTables;
+    private Table tablesTable;
 
-    public MySQLTableToolDialog(IWorkbenchPartSite partSite, String title, MySQLDataSource dataSource,
-                                Collection<MySQLTable> selectedTables)
+    public MySQLTableToolDialog(
+        IWorkbenchPartSite partSite, String title, MySQLDataSource dataSource,
+        Collection<MySQLTable> selectedTables)
     {
         super(partSite, dataSource, title, null);
         this.selectedTables = selectedTables;
@@ -43,18 +57,55 @@ public abstract class MySQLTableToolDialog extends GenerateSQLDialog {
 
     protected String[] generateSQLScript()
     {
-        String[] lines = new String[selectedTables.size()];
+        List<MySQLTable> checkedObjects = getCheckedObjects();
+        String[] lines = new String[checkedObjects.size()];
         int index = 0;
         StringBuilder sb = new StringBuilder(512);
-        for (MySQLTable db2Table : selectedTables) {
+        for (MySQLTable table : checkedObjects) {
 //            sb.append("CALL SYSPROC.ADMIN_CMD('");
-            generateTableCommand(sb, db2Table);
+            generateTableCommand(sb, table);
 //            sb.append("')");
             lines[index++] = sb.toString();
             sb.setLength(0);
         }
 
         return lines;
+    }
+
+    private List<MySQLTable> getCheckedObjects() {
+        List<MySQLTable> checkedObjects = new ArrayList<MySQLTable>();
+        if (tablesTable != null) {
+            for (TableItem item : tablesTable.getItems()) {
+                if (item.getChecked()) {
+                    checkedObjects.add((MySQLTable) item.getData());
+                }
+            }
+        } else {
+            checkedObjects.addAll(selectedTables);
+        }
+        return checkedObjects;
+    }
+
+    protected void createObjectsSelector(Composite parent) {
+        UIUtils.createControlLabel(parent, "Tables");
+        tablesTable = new Table(parent, SWT.BORDER | SWT.CHECK);
+        tablesTable.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+        for (MySQLTable table : selectedTables) {
+            TableItem item = new TableItem(tablesTable, SWT.NONE);
+            item.setText(table.getFullQualifiedName());
+            item.setImage(DBIcon.TREE_TABLE.getImage());
+            item.setChecked(true);
+            item.setData(table);
+        }
+        tablesTable.addSelectionListener(SQL_CHANGE_LISTENER);
+        tablesTable.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                boolean hasChecked = !getCheckedObjects().isEmpty();
+                getButton(IDialogConstants.OK_ID).setEnabled(hasChecked);
+                getButton(IDialogConstants.DETAILS_ID).setEnabled(hasChecked);
+            }
+        });
     }
 
     protected abstract void generateTableCommand(StringBuilder sql, MySQLTable table);
