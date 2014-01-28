10bd09f503711d43531fe217c58b823f90257438
jurgen <serge-rider@users.noreply.github.com>
2014-01-28 13:38:27 +0000

    Multi objects SQL dialog fix
    Former-commit-id: 08b4486eafdecd724383dfa613d6d8a797e57834

---
+++ plugins/org.jkiss.dbeaver.mysql/src/org/jkiss/dbeaver/ext/mysql/tools/maintenance/TableToolDialog.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2010-2013 Serge Rieder serge@jkiss.org
+ * Copyright (C) 2011-2012 Eugene Fradkin eugene.fradkin@gmail.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.jkiss.dbeaver.ext.mysql.tools.maintenance;
+
+import org.eclipse.ui.IWorkbenchPartSite;
+import org.jkiss.dbeaver.ext.mysql.model.MySQLTable;
+import org.jkiss.dbeaver.ui.dialogs.sql.GenerateMultiSQLDialog;
+
+import java.util.Collection;
+
+/**
+ * Table truncate
+ */
+public abstract class TableToolDialog extends GenerateMultiSQLDialog<MySQLTable>
+{
+
+    public TableToolDialog(IWorkbenchPartSite partSite, String title, Collection<MySQLTable> objects) {
+        super(partSite, title, objects);
+    }
+
+    protected void executeSQL()
+    {
+        super.executeSQL();
+    }
+
+}
