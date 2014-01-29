b652ec64e5699b993953efe571374baa51bc5c78
jurgen <serge-rider@users.noreply.github.com>
2014-01-29 05:43:43 +0000

    MySQL maintenance tools
    Former-commit-id: 5192a023de05effd4455c6a33b8424e58ffa4952

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/dialogs/sql/GenerateMultiSQLDialog.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/dialogs/sql/GenerateMultiSQLDialog.java
@@ -17,24 +17,36 @@
  */
 package org.jkiss.dbeaver.ui.dialogs.sql;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.IJobChangeEvent;
+import org.eclipse.core.runtime.jobs.JobChangeAdapter;
 import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.operation.ModalContext;
+import org.eclipse.jface.wizard.ProgressMonitorPart;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.ui.IWorkbenchPartSite;
 import org.jkiss.dbeaver.model.DBUtils;
+import org.jkiss.dbeaver.model.exec.*;
+import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
 import org.jkiss.dbeaver.model.sql.SQLDataSource;
 import org.jkiss.dbeaver.model.struct.DBSObject;
+import org.jkiss.dbeaver.runtime.RuntimeUtils;
+import org.jkiss.dbeaver.runtime.jobs.DataSourceJob;
 import org.jkiss.dbeaver.ui.DBIcon;
 import org.jkiss.dbeaver.ui.UIUtils;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
 
 /**
  * Super class for handling dialogs related to
@@ -71,7 +83,7 @@ public abstract class GenerateMultiSQLDialog<T extends DBSObject> extends Genera
         return lines.toArray(new String[lines.size()]);
     }
 
-    private List<T> getCheckedObjects() {
+    public List<T> getCheckedObjects() {
         List<T> checkedObjects = new ArrayList<T>();
         if (objectsTable != null) {
             for (TableItem item : objectsTable.getItems()) {
@@ -113,6 +125,97 @@ public abstract class GenerateMultiSQLDialog<T extends DBSObject> extends Genera
         });
     }
 
+    @Override
+    protected void executeSQL() {
+        final String jobName = getShell().getText();
+        final List<T> objects = getCheckedObjects();
+        final Map<T, List<String>> objectsSQL = new LinkedHashMap<T, List<String>>();
+        for (T object : objects) {
+            final List<String> lines = new ArrayList<String>();
+            generateObjectCommand(lines, object);
+            objectsSQL.put(object, lines);
+        }
+        DataSourceJob job = new DataSourceJob(jobName, null, getDataSource()) {
+            @Override
+            protected IStatus run(DBRProgressMonitor monitor)
+            {
+                beginScriptProcessing();
+                monitor.beginTask(jobName, objects.size());
+                DBCSession session = getDataSource().openSession(monitor, DBCExecutionPurpose.UTIL, jobName);
+                try {
+                    for (int i = 0; i < objects.size(); i++) {
+                        if (monitor.isCanceled()) {
+                            break;
+                        }
+                        final T object = objects.get(i);
+                        monitor.subTask("Process " + DBUtils.getObjectFullName(object));
+                        beginObjectProcessing(object);
+                        try {
+                            final List<String> lines = objectsSQL.get(object);
+                            for (String line : lines) {
+                                DBCStatement statement = DBUtils.prepareStatement(session, line);
+                                try {
+                                    if (statement.executeStatement()) {
+                                        DBCResultSet resultSet = statement.openResultSet();
+                                        try {
+                                            processObjectResults(object, resultSet);
+                                        } finally {
+                                            resultSet.close();
+                                        }
+                                    }
+                                } finally {
+                                    statement.close();
+                                }
+                            }
+                        } finally {
+                            endObjectProcessing(object);
+                        }
+                        monitor.worked(1);
+                    }
+                } catch (DBCException e) {
+                    return RuntimeUtils.makeExceptionStatus(e);
+                } finally {
+                    session.close();
+                    monitor.done();
+                    endScriptProcessing();
+                }
+                return Status.OK_STATUS;
+            }
+        };
+        job.setUser(false);
+        job.addJobChangeListener(new JobChangeAdapter() {
+            @Override
+            public void done(IJobChangeEvent event) {
+            }
+        });
+        job.schedule();
+    }
+
     protected abstract void generateObjectCommand(List<String> sql, T object);
+
+    protected void beginScriptProcessing()
+    {
+
+    }
+
+    protected void endScriptProcessing()
+    {
+
+    }
+
+    protected void beginObjectProcessing(T object)
+    {
+
+    }
+
+    protected void endObjectProcessing(T object)
+    {
+
+    }
+
+     protected void processObjectResults(T object, DBCResultSet resultSet)
+    {
+
+    }
 
 }
