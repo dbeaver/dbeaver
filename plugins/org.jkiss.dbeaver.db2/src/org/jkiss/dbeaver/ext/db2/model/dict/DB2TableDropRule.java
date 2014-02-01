8ead45ede296e95c1ebefaa8afd4b0f610ec968c
jurgen <serge-rider@users.noreply.github.com>
2014-02-01 10:25:51 +0000

    2014 Copyright update

--- plugins/org.jkiss.dbeaver.db2/src/org/jkiss/dbeaver/ext/db2/model/dict/DB2TableDropRule.java
+++ plugins/org.jkiss.dbeaver.db2/src/org/jkiss/dbeaver/ext/db2/model/dict/DB2TableDropRule.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2013      Denis Forveille titou10.titou10@gmail.com
- * Copyright (C) 2010-2013 Serge Rieder serge@jkiss.org
+ * Copyright (C) 2010-2014 Serge Rieder serge@jkiss.org
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
