0e6831e3d20454be88e1d197f0deb9464a41cbfb
jurgen <serge-rider@users.noreply.github.com>
2014-02-01 09:48:16 +0000

    Oracle tools (stats for indexes)
    Former-commit-id: 82e777a12f68570a261481de3c9274cff6321d01

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/model/impl/sql/JDBCSQLDialect.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/model/impl/sql/JDBCSQLDialect.java
@@ -21,7 +21,6 @@ package org.jkiss.dbeaver.model.impl.sql;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jkiss.code.NotNull;
-import org.jkiss.code.Nullable;
 import org.jkiss.dbeaver.model.DBPIdentifierCase;
 import org.jkiss.dbeaver.model.DBPKeywordType;
 import org.jkiss.dbeaver.model.exec.jdbc.JDBCDatabaseMetaData;
