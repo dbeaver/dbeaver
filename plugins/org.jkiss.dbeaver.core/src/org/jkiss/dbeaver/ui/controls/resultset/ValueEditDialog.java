8b239c4ce4816b3bd246cd25dba7db4416830dfe
jurgen <serge-rider@users.noreply.github.com>
2014-04-04 20:45:43 +0000

    Filters value edit dialog

---
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/controls/resultset/ValueEditDialog.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright (C) 2010-2014 Serge Rieder
+ * serge@jkiss.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.jkiss.dbeaver.ui.controls.resultset;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Text;
+import org.jkiss.dbeaver.DBException;
+import org.jkiss.dbeaver.model.data.DBDValueController;
+import org.jkiss.dbeaver.model.data.DBDValueEditor;
+import org.jkiss.dbeaver.ui.UIUtils;
+import org.jkiss.dbeaver.ui.controls.lightgrid.GridCell;
+import org.jkiss.dbeaver.ui.dialogs.BaseDialog;
+
+class ValueEditDialog extends BaseDialog {
+
+    static final Log log = LogFactory.getLog(ValueEditDialog.class);
+
+    private final ResultSetViewer viewer;
+    private final GridCell cell;
+    private Object value;
+    private DBDValueEditor editor;
+    private Text textControl;
+
+    public ValueEditDialog(ResultSetViewer viewer, GridCell cell) {
+        super(viewer.getControl().getShell(), "Edit value", null);
+        this.viewer = viewer;
+        this.cell = cell;
+    }
+
+    @Override
+    protected Control createDialogArea(Composite parent)
+    {
+        Composite composite = (Composite) super.createDialogArea(parent);
+
+        Composite editorPlaceholder = UIUtils.createPlaceholder(composite, 1);
+        editorPlaceholder.setLayoutData(new GridData(GridData.FILL_BOTH));
+        editorPlaceholder.setLayout(new FillLayout());
+
+        final ResultSetViewer.ResultSetValueController valueController = new ResultSetViewer.ResultSetValueController(
+            viewer,
+            cell,
+            DBDValueController.EditType.PANEL,
+            editorPlaceholder);
+        try {
+            editor = valueController.getValueHandler().createEditor(valueController);
+            editor.primeEditorValue(valueController.getValue());
+        } catch (DBException e) {
+            log.error("Can't create inline value editor", e);
+        }
+        if (editor == null) {
+            textControl = new Text(composite, SWT.BORDER | SWT.WRAP | SWT.V_SCROLL);
+            textControl.setText("");
+            GridData gd = new GridData(GridData.FILL_BOTH);
+            gd.widthHint = 300;
+            gd.heightHint = 300;
+            gd.minimumHeight = 100;
+            gd.minimumWidth = 100;
+            textControl.setLayoutData(gd);
+        }
+
+        return parent;
+    }
+
+    @Override
+    protected void createButtonsForButtonBar(Composite parent)
+    {
+		createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, true);
+        createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, false);
+    }
+
+    @Override
+    protected void okPressed()
+    {
+        if (editor != null) {
+            try {
+                value = editor.extractEditorValue();
+            } catch (DBException e) {
+                log.error("Can't get editor value", e);
+            }
+        } else {
+            value = textControl.getText();
+        }
+        super.okPressed();
+    }
+
+    public Object getValue() {
+        return value;
+    }
+}
