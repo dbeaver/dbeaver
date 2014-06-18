96b1ee023c20cfbfa2d7b8f8bdc822992ed355b4
jurgen <serge-rider@users.noreply.github.com>
2014-06-18 13:14:08 +0000

    ERD: put keys in top attribute box Former-commit-id: 78a8654f7d1fb5658f6df051b98fd6a781f7b373

---
+++ plugins/org.jkiss.dbeaver.erd/src/org/jkiss/dbeaver/ext/erd/figures/AttributeItemFigure.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2010-2014 Serge Rieder
+ * serge@jkiss.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+/*
+ * Created on Jul 13, 2004
+ */
+package org.jkiss.dbeaver.ext.erd.figures;
+
+import org.eclipse.draw2d.*;
+import org.eclipse.draw2d.geometry.Insets;
+import org.jkiss.dbeaver.ext.erd.model.ERDEntity;
+import org.jkiss.dbeaver.ext.erd.model.ERDEntityAttribute;
+
+/**
+ * Figure used to hold the column labels
+ * @author Serge Rieder
+ */
+public class AttributeItemFigure extends EditableLabel
+{
+    private final ERDEntityAttribute attribute;
+
+	public AttributeItemFigure(ERDEntityAttribute attribute)
+	{
+        super(attribute.getLabelText());
+        this.attribute = attribute;
+        setIcon(attribute.getLabelImage());
+
+	}
+
+    public ERDEntityAttribute getAttribute() {
+        return attribute;
+    }
+}
