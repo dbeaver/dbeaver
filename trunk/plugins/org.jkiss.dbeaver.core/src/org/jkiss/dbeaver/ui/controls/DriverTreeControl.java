c4855f32be77bb600426f9342523be0045b29294
jurgen <jurgen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2015-02-07 21:02:05 +0000

    Driver tree redesign
    Former-commit-id: 0510a56837ea6df9e84506a3a1660ede682ff2bf

---
+++ trunk/plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/controls/DriverTreeControl.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2010-2014 Serge Rieder serge@jkiss.org
+ * Copyright (C) 2011-2012 Eugene Fradkin eugene.fradkin@gmail.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.jkiss.dbeaver.ui.controls;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.dialogs.FilteredTree;
+import org.eclipse.ui.dialogs.PatternFilter;
+import org.jkiss.dbeaver.registry.DataSourceProviderDescriptor;
+
+import java.util.List;
+
+/**
+ * DriverTreeControl
+ *
+ * @author Serge Rieder
+ */
+public class DriverTreeControl extends FilteredTree {
+
+    private static PatternFilter filter = new PatternFilter();
+    private final boolean expandRecent;
+    private final Object site;
+    private final List<DataSourceProviderDescriptor> providers;
+
+    public DriverTreeControl(Composite parent, Object site, List<DataSourceProviderDescriptor> providers, boolean expandRecent) {
+        super(parent, SWT.H_SCROLL | SWT.V_SCROLL | SWT.BORDER, filter, true);
+        this.site = site;
+        this.providers = providers;
+        this.expandRecent = expandRecent;
+    }
+
+    @Override
+    protected DriverTreeViewer doCreateTreeViewer(Composite parent, int style) {
+        DriverTreeViewer viewer = new DriverTreeViewer(parent, style);
+        viewer.initDrivers(site, providers, expandRecent);
+        return viewer;
+    }
+
+}
