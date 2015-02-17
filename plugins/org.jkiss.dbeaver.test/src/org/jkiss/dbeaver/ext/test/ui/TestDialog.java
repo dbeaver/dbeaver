430c620fc2f126620df798e4582da022bbf32281
jurgen <serge-rider@users.noreply.github.com>
2015-02-17 20:48:17 +0000

    VTabs Test plugin
    Former-commit-id: 22ce5e46c84b1be710b538ba2f19aa3267151205

--- plugins/org.jkiss.dbeaver.test/src/org/jkiss/dbeaver/ext/test/ui/TestDialog.java
+++ plugins/org.jkiss.dbeaver.test/src/org/jkiss/dbeaver/ext/test/ui/TestDialog.java
@@ -19,16 +19,36 @@
 package org.jkiss.dbeaver.ext.test.ui;
 
 import org.eclipse.jface.dialogs.TrayDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.jkiss.dbeaver.ui.DBIcon;
+import org.jkiss.dbeaver.ui.UIUtils;
+import org.jkiss.dbeaver.ui.controls.vtabs.ITabItem;
+import org.jkiss.dbeaver.ui.controls.vtabs.TabbedPropertyList;
 
 public class TestDialog extends TrayDialog {
+
+    private ITabItem[] tabs;
 
     public TestDialog(Shell shell)
     {
         super(shell);
+
+        tabs = new ITabItem[2];
+        tabs[0] = new TabItemImpl("Tab 1", DBIcon.TREE_TABLE.getImage());
+        tabs[1] = new TabItemImpl("Tab with long-long name", DBIcon.TREE_COLUMNS.getImage());
+    }
+
+    @Override
+    protected boolean isResizable() {
+        return true;
     }
 
     @Override
@@ -39,6 +59,21 @@ public class TestDialog extends TrayDialog {
         GridData gd = new GridData(GridData.FILL_BOTH);
         group.setLayoutData(gd);
 
+        Composite sheet = UIUtils.createPlaceholder(group, 2);
+        sheet.setLayoutData(new GridData(GridData.FILL_BOTH));
+
+        TabbedPropertyList tabbedPropertyList = new TabbedPropertyList(sheet);
+        gd = new GridData(GridData.FILL_BOTH);
+        gd.heightHint = 300;
+        gd.widthHint = 500;
+        tabbedPropertyList.setLayoutData(gd);
+        tabbedPropertyList.setElements(tabs);
+        tabbedPropertyList.select(0);
+
+        Text pane = new Text(sheet, SWT.NONE);
+        pane.setLayoutData(new GridData(GridData.FILL_BOTH));
+        //pane.setLayout(new FillLayout());
+
 /*
         Composite propsGroup = new Composite(group, SWT.NONE);
         propsGroup.setLayout(new GridLayout(2, false));
@@ -48,4 +83,38 @@ public class TestDialog extends TrayDialog {
         return group;
     }
 
+    private static class TabItemImpl implements ITabItem {
+        private String text;
+        private Image image;
+
+        private TabItemImpl(String text, Image image) {
+            this.text = text;
+            this.image = image;
+        }
+
+        @Override
+        public Image getImage() {
+            return image;
+        }
+
+        @Override
+        public String getText() {
+            return text;
+        }
+
+        @Override
+        public boolean isSelected() {
+            return false;
+        }
+
+        @Override
+        public boolean isIndented() {
+            return false;
+        }
+
+        @Override
+        public Composite createControl(Composite parent) {
+            return new Composite(parent, SWT.BORDER);
+        }
+    }
 }
