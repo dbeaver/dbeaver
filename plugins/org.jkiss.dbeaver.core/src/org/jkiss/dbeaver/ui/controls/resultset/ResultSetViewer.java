e9c634409b12c543214fa2e1c435e793ffba0e74
jurgen <serge-rider@users.noreply.github.com>
2015-02-28 20:11:16 +0000

    Tabbed UI fixes (-borders)
    Former-commit-id: 3c9252f3a9f36541286dcc3c56ce022f2b6270b2

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/controls/resultset/ResultSetViewer.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/controls/resultset/ResultSetViewer.java
@@ -778,7 +778,7 @@ public class ResultSetViewer extends Viewer
         statusBar.setLayoutData(gd);
         GridLayout gl = new GridLayout(4, false);
         gl.marginWidth = 0;
-        gl.marginHeight = 3;
+        gl.marginHeight = 0;
         //gl.marginBottom = 5;
         statusBar.setLayout(gl);
         
