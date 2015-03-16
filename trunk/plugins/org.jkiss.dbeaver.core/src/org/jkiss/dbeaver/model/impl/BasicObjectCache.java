65a8e460c6b6c515aca604fa14dc8cbfa12cf0cb
jurgen <jurgen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2015-03-16 22:07:48 +0000

    DBC uses zero-based attribute indexes Mongo cache fix.
    Former-commit-id: 5f3400fcae894ec67e680cc7f68d070268457c1e

---
+++ trunk/plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/model/impl/BasicObjectCache.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2010-2015 Serge Rieder
+ * serge@jkiss.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.jkiss.dbeaver.model.impl;
+
+import org.jkiss.code.Nullable;
+import org.jkiss.dbeaver.DBException;
+import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
+import org.jkiss.dbeaver.model.struct.DBSObject;
+
+import java.util.List;
+
+/**
+ * Basic objects cache.
+ */
+public abstract class BasicObjectCache<OWNER extends DBSObject, OBJECT extends DBSObject> extends AbstractObjectCache<OWNER, OBJECT> {
+
+    @Nullable
+    @Override
+    public OBJECT getObject(DBRProgressMonitor monitor, @Nullable OWNER owner, String name) throws DBException {
+        if (!isCached()) {
+            getObjects(monitor, owner);
+        }
+        return getCachedObject(name);
+    }
+}
