a0ec29ffacfc843b69e7accd4c60731007c49cce
jurgen <jurgen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2015-03-24 21:13:33 +0000

    Generic views editor fix
    Former-commit-id: 0810c6477e95414002bbcc75d6bd1d0ca54ef7ed

--- trunk/plugins/org.jkiss.dbeaver.generic/src/org/jkiss/dbeaver/ext/generic/model/meta/GenericMetaModel.java
+++ trunk/plugins/org.jkiss.dbeaver.generic/src/org/jkiss/dbeaver/ext/generic/model/meta/GenericMetaModel.java
@@ -120,6 +120,11 @@ public class GenericMetaModel {
                         case DatabaseMetaData.procedureResultUnknown: procedureType = DBSProcedureType.PROCEDURE; break;
                         default: procedureType = DBSProcedureType.UNKNOWN; break;
                     }
+                    if (specificName == null && procedureName.indexOf(';') != -1) {
+                        // [JDBC: SQL Server native driver]
+                        specificName = procedureName;
+                        procedureName = procedureName.substring(0, procedureName.lastIndexOf(';'));
+                    }
                     // Check for packages. Oracle (and may be some other databases) uses catalog name as storage for package name
                     String packageName = null;
                     GenericPackage procedurePackage = null;
