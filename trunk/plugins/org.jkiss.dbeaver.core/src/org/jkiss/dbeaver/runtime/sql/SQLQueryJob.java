c8c954f452b23c9cf4831ac45b67b05b2f5aff67
jurgen <jurgen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2015-04-07 17:29:18 +0000

    txn manager usage fix
    Former-commit-id: e571de8a31ba3b02cc70266f7897327a21856ca5

--- trunk/plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/runtime/sql/SQLQueryJob.java
+++ trunk/plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/runtime/sql/SQLQueryJob.java
@@ -134,7 +134,7 @@ public class SQLQueryJob extends DataSourceJob
     {
         statistics = new DBCStatistics();
         try {
-            DBCTransactionManager txnManager = DBUtils.getTransactionManager(getDataSource());
+            DBCTransactionManager txnManager = DBUtils.getTransactionManager(getExecutionContext());
             DBCSession session = getExecutionContext().openSession(monitor, queries.size() > 1 ? DBCExecutionPurpose.USER_SCRIPT : DBCExecutionPurpose.USER, "SQL Query");
             try {
                 // Set transaction settings (only if autocommit is off)
