095dd315ec0428ed58a4b7e50130e26b729ccc63
bert.herngreen <bert.herngreen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2015-05-02 15:16:35 +0000

    Former-commit-id: f52eff496f5fb8e4c1b971d7768be3774c4f9672

---
+++ plugins/org.jkiss.dbeaver.informix/org.jkiss.dbeaver.informix/src/org/jkiss/dbeaver/ext/informix/model/InformixMetaModel.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2010-2015 Serge Rieder
+ * serge@jkiss.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.jkiss.dbeaver.ext.informix.model;
+
+import org.jkiss.dbeaver.core.Log;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.jkiss.dbeaver.DBException;
+import org.jkiss.dbeaver.ext.generic.model.GenericProcedure;
+import org.jkiss.dbeaver.ext.generic.model.GenericTable;
+import org.jkiss.dbeaver.ext.generic.model.meta.GenericMetaModel;
+import org.jkiss.dbeaver.ext.informix.InformixUtils;
+import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
+
+/**
+ * InformixDataSource
+ */
+public class InformixMetaModel extends GenericMetaModel
+{
+    static final Log log = Log.getLog(InformixMetaModel.class);
+
+    public InformixMetaModel(IConfigurationElement cfg) {
+        super(cfg);
+    }
+
+    public String getViewDDL(DBRProgressMonitor monitor, GenericTable sourceObject) throws DBException {
+        return InformixUtils.getViewSource(monitor, sourceObject);
+    }
+
+    @Override
+    public String getProcedureDDL(DBRProgressMonitor monitor, GenericProcedure sourceObject) throws DBException {
+        return InformixUtils.getProcedureSource(monitor, sourceObject);
+    }
+}
