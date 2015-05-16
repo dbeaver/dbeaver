087ee644bb9df782f6be04fd705513540b29789a
bert.herngreen <bert.herngreen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2015-05-16 11:04:05 +0000

    Add support for dumping of trigger DLL
    Former-commit-id: d9c3565dda9266a52aaea77e3af7756130c0ccbf

--- trunk/plugins/org.jkiss.dbeaver.informix/src/org/jkiss/dbeaver/ext/informix/InformixUtils.java
+++ trunk/plugins/org.jkiss.dbeaver.informix/src/org/jkiss/dbeaver/ext/informix/InformixUtils.java
@@ -26,18 +26,14 @@ import org.jkiss.dbeaver.ext.generic.model.GenericDataSource;
 import org.jkiss.dbeaver.ext.generic.model.GenericProcedure;
 import org.jkiss.dbeaver.ext.generic.model.GenericTable;
 import org.jkiss.dbeaver.model.exec.DBCExecutionPurpose;
-import org.jkiss.dbeaver.model.exec.DBCSession;
-import org.jkiss.dbeaver.model.exec.DBCStatement;
-import org.jkiss.dbeaver.model.exec.DBCStatementType;
 import org.jkiss.dbeaver.model.exec.jdbc.JDBCPreparedStatement;
 import org.jkiss.dbeaver.model.exec.jdbc.JDBCResultSet;
 import org.jkiss.dbeaver.model.exec.jdbc.JDBCSession;
-import org.jkiss.dbeaver.model.impl.jdbc.JDBCUtils;
 import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
-import org.jkiss.dbeaver.model.struct.DBSObject;
 
-import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Informix utils
@@ -45,26 +41,20 @@ import java.sql.SQLException;
 public class InformixUtils {
 
 	static final Log log = Log.getLog(InformixUtils.class);
-
-	private static String getSource(DBRProgressMonitor monitor,
+	private static List<String> getSource(DBRProgressMonitor monitor,
 			String sqlStatement, String dbObjectName,
 			GenericDataSource datasource) throws DBException {
 		JDBCSession session = datasource.openSession(monitor,
 				DBCExecutionPurpose.META, "Load source code");
+		List<String> result = new ArrayList<String>();
 		try {
 			JDBCPreparedStatement dbStat = session
 					.prepareStatement(sqlStatement);
 			try {
 				JDBCResultSet dbResult = dbStat.executeQuery();
-				try {
-					StringBuilder sbResult = new StringBuilder();
-					while (dbResult.nextRow()) {
-						sbResult.append(dbResult.getString(1).trim());
-					}
-					return sbResult.toString();
-				} finally {
-					dbResult.close();
-				}
+				while (dbResult.nextRow())
+					result.add(dbResult.getString(1));
+				return result;
 			} finally {
 				dbStat.close();
 			}
@@ -78,6 +68,16 @@ public class InformixUtils {
 		}
 	}
 
+	private static String ListToString(List<String> value, String delimiter) {
+		StringBuilder sbResult = new StringBuilder();
+		for (String o : value) {
+			sbResult.append(o.trim());
+			if (delimiter != null && !delimiter.isEmpty())
+			sbResult.append(delimiter);
+		}
+		return sbResult.toString();
+	}
+
 	public static String getProcedureSource(DBRProgressMonitor monitor,
 			GenericProcedure procedure) throws DBException {
 		String sqlProcedure = String.format("select b.data "
@@ -85,8 +85,9 @@ public class InformixUtils {
 				+ "join sysprocedures p on b.procid=p.procid "
 				+ "where datakey='T' and p.procname = '%s'"
 				+ "order by b.procid, b.seqno", procedure.getName());
-		return getSource(monitor, sqlProcedure, procedure.getName(),
-				procedure.getDataSource());
+		return ListToString(
+				getSource(monitor, sqlProcedure, procedure.getName(),
+						procedure.getDataSource()), null);
 	}
 
 	public static String getViewSource(DBRProgressMonitor monitor,
@@ -95,7 +96,28 @@ public class InformixUtils {
 				+ "from informix.sysviews v "
 				+ "join systables s on s.tabid = v.tabid "
 				+ "where s.tabname = '%s'", view.getName());
-		return getSource(monitor, sqlView, view.getName(), view.getDataSource());
+		return ListToString(
+				getSource(monitor, sqlView, view.getName(),
+						view.getDataSource()), null);
+	}
+
+	// Triggers, Sequences?
+	public static String getTriggerDDL(DBRProgressMonitor monitor,
+			GenericTable table) throws DBException {
+		String sqlTrigger = String
+				.format("select tb.data " + "from systables ta "
+						+ "join systriggers tr on tr.tabid = ta.tabid "
+						+ "join systrigbody tb on tb.trigid = tr.trigid "
+						+ "where ta.tabname = '%s' and ta.tabtype='T' "
+						+ "and tb.datakey IN ('A', 'D') "
+						+ "order by tr.trigname, datakey desc, seqno ",
+						table.getName());
+		return ListToString(
+				getSource(monitor, sqlTrigger, table.getName(),
+						table.getDataSource()), "\n");
+		// systriggers.event:
+		// D = Delete trigger, I = Insert, U = Update trigger,S = Select,
+		// d = INSTEAD OF Delete, i = INSTEAD OF Insert,u = INSTEAD OF Update
 	}
 
 }
