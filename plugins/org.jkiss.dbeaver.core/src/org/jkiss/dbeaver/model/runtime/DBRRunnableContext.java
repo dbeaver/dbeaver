c27260afb8d6f08e4b50d20c6fc1fcc2d2947da7
jurgen <serge-rider@users.noreply.github.com>
2015-06-05 12:33:47 +0000

    Remove JFace from model

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/model/runtime/DBRRunnableContext.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/model/runtime/DBRRunnableContext.java
@@ -21,7 +21,7 @@ package org.jkiss.dbeaver.model.runtime;
 import java.lang.reflect.InvocationTargetException;
 
 /**
- * DBPViewCallback
+ * DBRRunnableContext
  */
 public interface DBRRunnableContext
 {
@@ -30,5 +30,9 @@ public interface DBRRunnableContext
      * If any exception will occure when running this process then it'll written in log
      * @param runnable runnable implementation
      */
-    public void runInProgressDialog(DBRRunnableWithProgress runnable) throws InvocationTargetException, InterruptedException;
+    public void run(
+        boolean fork,
+        boolean cancelable,
+        DBRRunnableWithProgress runnable)
+        throws InvocationTargetException, InterruptedException;
 }
