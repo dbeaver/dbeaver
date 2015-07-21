ffa5da2d9e13b27742a79023b4c771313eefe05f
jurgen <jurgen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2015-07-21 15:01:58 +0000

    Documents model
    Former-commit-id: 82b377b26daf4281ffced6df0457b6cf9f43ad9d

---
+++ trunk/plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/editors/BinaryEditorInput.java
@@ -0,0 +1,141 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2015 Serge Rieder (serge@jkiss.org)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (version 2)
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package org.jkiss.dbeaver.ui.editors;
+
+import org.eclipse.core.resources.IStorage;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IPersistableElement;
+import org.jkiss.dbeaver.model.DBIcon;
+import org.jkiss.dbeaver.ui.DBeaverIcons;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Arrays;
+
+/**
+ * BinaryEditorInput
+ */
+public class BinaryEditorInput implements IEditorInput {
+
+    private String name;
+    private byte[] value;
+    private boolean readOnly;
+    private IStorage storage;
+
+	public BinaryEditorInput(String name, byte[] value, boolean readOnly) {
+        this.name = name;
+        this.value = value;
+        this.readOnly = readOnly;
+	}
+
+	public int hashCode() {
+		return Arrays.hashCode(value);
+	}
+
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof BinaryEditorInput)) {
+			return false;
+		}
+		BinaryEditorInput other = (BinaryEditorInput) obj;
+		return Arrays.equals(value, other.value);
+	}
+
+	@Override
+    public boolean exists() {
+		return true;
+	}
+
+	@Override
+    public ImageDescriptor getImageDescriptor() {
+		return DBeaverIcons.getImageDescriptor(DBIcon.TREE_INFO);
+	}
+
+	/* (non-Javadoc)
+	 * Method declared on IEditorInput.
+	 */
+	@Override
+    public String getName() {
+		return name;
+	}
+
+	@Override
+    public IPersistableElement getPersistable() {
+		return null;
+	}
+
+	public IStorage getStorage() {
+        if (storage == null) {
+            storage = new IStorage() {
+                @Override
+                public InputStream getContents() throws CoreException
+                {
+                    return new ByteArrayInputStream(value);
+                }
+
+                @Override
+                public IPath getFullPath()
+                {
+                    return null;
+                }
+
+                @Override
+                public String getName()
+                {
+                    return name;
+                }
+
+                @Override
+                public boolean isReadOnly()
+                {
+                    return readOnly;
+                }
+
+                @Override
+                public Object getAdapter(Class adapter)
+                {
+                    return null;
+                }
+            };
+        }
+		return storage;
+	}
+
+	@Override
+    public String getToolTipText() {
+		return name;
+	}
+
+    public String toString() {
+		return new String(value);
+	}
+
+	@Override
+    public Object getAdapter(Class adapter) {
+        if (adapter == IStorage.class) {
+            return getStorage();
+        }
+		return null;
+	}
+
+}
