95c4515bf3ba32b24f9813b9b72d31098b7ce830
jurgen <serge-rider@users.noreply.github.com>
2015-08-19 09:49:51 +0000

    Derby extension
    Former-commit-id: 4f522a2d6057c5115115f4a9ac31423a9f1af75e

---
+++ plugins/org.jkiss.dbeaver.ext.derby/src/org/jkiss/dbeaver/ext/derby/DerbyUtils.java
@@ -0,0 +1,31 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2015 Serge Rieder (serge@jkiss.org)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (version 2)
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package org.jkiss.dbeaver.ext.derby;
+
+import org.jkiss.dbeaver.Log;
+
+/**
+ * Derby utils
+ */
+public class DerbyUtils {
+
+    static final Log log = Log.getLog(DerbyUtils.class);
+
+
+}
