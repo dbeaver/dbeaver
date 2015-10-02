b6c34ad62fc751a8c7cc0a681218ccbd53806a7e
jurgen <jurgen@701d3bc3-4ef2-464d-b00a-92476534ed6a>
2015-10-02 19:21:43 +0000

    Multiple results for DB2
    Former-commit-id: 842c95290d35e4b48332c4c59d36d9ae59ea331a

---
+++ trunk/plugins/org.jkiss.dbeaver.ext.db2/src/org/jkiss/dbeaver/ext/db2/model/DB2DataSourceInfo.java
@@ -0,0 +1,38 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2015 Serge Rieder (serge@jkiss.org)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (version 2)
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package org.jkiss.dbeaver.ext.db2.model;
+
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCDatabaseMetaData;
+import org.jkiss.dbeaver.model.impl.jdbc.JDBCDataSourceInfo;
+
+/**
+ * DB2 data source info
+ */
+class DB2DataSourceInfo extends JDBCDataSourceInfo {
+
+    public DB2DataSourceInfo(JDBCDatabaseMetaData metaData) {
+        super(metaData);
+    }
+
+    @Override
+    public boolean supportsMultipleResults() {
+        return true;
+    }
+
+}
