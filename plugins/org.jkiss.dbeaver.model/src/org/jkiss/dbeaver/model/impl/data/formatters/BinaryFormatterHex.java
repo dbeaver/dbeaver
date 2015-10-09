7cb0ee40b47a47251a85514191ee93343e835994
jurgen <serge-rider@users.noreply.github.com>
2015-10-09 14:31:49 +0000

    HBase model
    Former-commit-id: 7fe3b79f9e9dd6a1855d1272503e8a56870f3cf9

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/impl/data/formatters/BinaryFormatterHex.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/impl/data/formatters/BinaryFormatterHex.java
@@ -41,8 +41,8 @@ public class BinaryFormatterHex implements DBDBinaryFormatter {
     public String toString(byte[] bytes, int offset, int length)
     {
         char[] chars = new char[length * 2];
-        for (int i = offset; i < offset + length; i++) {
-            String hex = GeneralUtils.byteToHex[bytes[i] & 0x0ff];
+        for (int i = 0; i < length; i++) {
+            String hex = GeneralUtils.byteToHex[bytes[offset + i] & 0x0ff];
             chars[i * 2] = hex.charAt(0);
             chars[i * 2 + 1] = hex.charAt(1);
         }
