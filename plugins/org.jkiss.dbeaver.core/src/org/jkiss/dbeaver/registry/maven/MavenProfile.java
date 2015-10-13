823c745efdf06c307684dde08fb7ab41882be908
jurgen <serge-rider@users.noreply.github.com>
2015-10-13 15:28:35 +0000

    Maven profiles
    Former-commit-id: 3aa9d5054ae4dcd5da8c9b49e7fb651eb0ea3b1b

---
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/registry/maven/MavenProfile.java
@@ -0,0 +1,63 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2015 Serge Rieder (serge@jkiss.org)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (version 2)
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package org.jkiss.dbeaver.registry.maven;
+
+import org.w3c.dom.Element;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Maven build profile
+ */
+public class MavenProfile {
+
+    private final String id;
+    Map<String, String> properties = new LinkedHashMap<>();
+    List<MavenArtifactDependency> dependencies;
+    List<MavenArtifactDependency> dependencyManagement;
+
+    boolean active;
+
+    public MavenProfile(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Map<String, String> getProperties() {
+        return properties;
+    }
+
+    public List<MavenArtifactDependency> getDependencies() {
+        return dependencies;
+    }
+
+    public List<MavenArtifactDependency> getDependencyManagement() {
+        return dependencyManagement;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+
+}
