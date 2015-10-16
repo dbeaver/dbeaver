8080fe13a7efee854b04d7e61126c4f082dc133e
jurgen <serge-rider@users.noreply.github.com>
2015-10-16 14:11:01 +0000

    Do not close editor if RSV save fails

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/controls/resultset/spreadsheet/Spreadsheet.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/controls/resultset/spreadsheet/Spreadsheet.java
@@ -237,18 +237,23 @@ public class Spreadsheet extends LightGrid implements Listener {
                 {
                     final Control editorControl = presentation.openValueEditor(true);
                     if (editorControl != null && event.keyCode != SWT.CR) {
-                        // Forward the same key event to just created control
-                        final Event kdEvent = new Event();
-                        kdEvent.type = SWT.KeyDown;
-                        kdEvent.character = event.character;
-                        kdEvent.keyCode = event.keyCode;
-                        UIUtils.postEvent(editorControl, kdEvent);
+                        UIUtils.runInDetachedUI(getShell(), new Runnable() {
+                            @Override
+                            public void run() {
+                                // Forward the same key event to just created control
+                                final Event kdEvent = new Event();
+                                kdEvent.type = SWT.KeyDown;
+                                kdEvent.character = event.character;
+                                kdEvent.keyCode = event.keyCode;
+                                UIUtils.postEvent(editorControl, kdEvent);
 
-                        final Event kuEvent = new Event();
-                        kuEvent.type = SWT.KeyUp;
-                        kuEvent.character = event.character;
-                        kuEvent.keyCode = event.keyCode;
-                        UIUtils.postEvent(editorControl, kuEvent);
+                                final Event kuEvent = new Event();
+                                kuEvent.type = SWT.KeyUp;
+                                kuEvent.character = event.character;
+                                kuEvent.keyCode = event.keyCode;
+                                UIUtils.postEvent(editorControl, kuEvent);
+                            }
+                        });
                     }
                 } else if (event.keyCode == SWT.ESC) {
                     // Reset cell value
