4bdc22c42eb4900db9332c2a4d499832358727f8
serge-rider <serge.rider@gmail.com>
2015-11-20 15:02:56 +0300

    Data search fix

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/search/data/SearchDataQuery.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/search/data/SearchDataQuery.java
@@ -159,7 +159,8 @@ public class SearchDataQuery implements IObjectSearchQuery {
                                     try {
                                         value = new BigDecimal(params.searchString);
                                     } catch (Exception e3) {
-                                        value = params.searchString;
+                                        // Not a number
+                                        continue;
                                     }
                                 }
                             }
