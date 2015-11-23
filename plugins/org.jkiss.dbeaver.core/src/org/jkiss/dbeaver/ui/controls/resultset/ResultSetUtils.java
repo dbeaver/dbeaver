e65f9cf1fef83aea14e01d5cb7cb7d273117e8d1
serge-rider <serge.rider@gmail.com>
2015-11-23 18:47:30 +0300

    #30 RSV: fetched values resolution. MySQL: enum type handler fix.
    Former-commit-id: a46f3772210212e48f4849d8be5b29210acc79ca

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/controls/resultset/ResultSetUtils.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/controls/resultset/ResultSetUtils.java
@@ -123,7 +123,16 @@ public class ResultSetUtils
                     } else {
                         tableColumn = entity.getAttribute(monitor, attrMeta.getName());
                     }
-                    binding.setEntityAttribute(tableColumn);
+                    if (binding.setEntityAttribute(tableColumn)) {
+                        // We have new type and new value handler.
+                        // We have to fix already fetched values.
+                        // E.g. we fetched strings and found out that we should handle them as LOBs or enums.
+                        try {
+                            resolveAttributeValues(session, binding, rows);
+                        } catch (DBCException e) {
+                            log.warn("Error resolving attribute '" + binding.getName() + "' values", e);
+                        }
+                    }
                 }
             }
             monitor.worked(1);
@@ -170,6 +179,15 @@ public class ResultSetUtils
         }
         finally {
             monitor.done();
+        }
+    }
+
+    private static void resolveAttributeValues(DBCSession session, DBDAttributeBindingMeta attr, List<Object[]> rows)
+        throws DBCException
+    {
+        int pos = attr.getOrdinalPosition();
+        for (Object[] row : rows) {
+            row[pos] = attr.getValueHandler().getValueFromObject(session, attr.getEntityAttribute(), row[pos], false);
         }
     }
 
