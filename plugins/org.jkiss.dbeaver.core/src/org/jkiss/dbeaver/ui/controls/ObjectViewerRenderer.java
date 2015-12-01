1cc78b1c460a6a6f3956b098af31363c384e59f1
Serge Rider <serge@jkiss.org>
2015-12-01 10:53:56 +0300

    Boolean cells renderer fix (revert)

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/controls/ObjectViewerRenderer.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/controls/ObjectViewerRenderer.java
@@ -28,6 +28,7 @@ import org.eclipse.swt.widgets.*;
 import org.jkiss.code.Nullable;
 import org.jkiss.dbeaver.model.DBPNamedObject;
 import org.jkiss.dbeaver.model.DBUtils;
+import org.jkiss.dbeaver.ui.ImageUtils;
 import org.jkiss.dbeaver.ui.UIUtils;
 import org.jkiss.dbeaver.utils.GeneralUtils;
 
@@ -170,17 +171,17 @@ public abstract class ObjectViewerRenderer {
         Object cellValue = getCellValue(element, columnIndex);
         if (cellValue != null ) {
             GC gc = event.gc;
-            /*if (cellValue instanceof Boolean) {
-                //int columnWidth = UIUtils.getColumnWidth(item);
+            if (cellValue instanceof Boolean) {
+                //int columnWidth = event.width;
                 int columnHeight = isTree ? getTree().getItemHeight() : getTable().getItemHeight();
                 Image image = (Boolean)cellValue ?
                     (editable ? ImageUtils.getImageCheckboxEnabledOn() : ImageUtils.getImageCheckboxDisabledOn()) :
                     (editable ? ImageUtils.getImageCheckboxEnabledOff() : ImageUtils.getImageCheckboxDisabledOff());
                 final Rectangle imageBounds = image.getBounds();
-                gc.drawImage(image, event.x + 4*//*(columnWidth - imageBounds.width) / 2*//*, event.y + (columnHeight - imageBounds.height) / 2);
+                gc.drawImage(image, event.x + 4 /*+ (columnWidth - imageBounds.width) / 2*/, event.y + (columnHeight - imageBounds.height) / 2);
                 event.doit = false;
 //                            System.out.println("PAINT " + cellValue + " " + System.currentTimeMillis());
-            } else */if (isHyperlink(cellValue)) {
+            } else if (isHyperlink(cellValue)) {
                 // Print link
                 boolean isSelected = gc.getBackground().equals(selectionBackgroundColor);
                 prepareLinkStyle(cellValue, isSelected ? gc.getForeground() : null);
@@ -323,7 +324,7 @@ public abstract class ObjectViewerRenderer {
         } else if (value instanceof DBPNamedObject) {
             value = ((DBPNamedObject)value).getName();
         } else if (value instanceof Boolean) {
-            value = DBUtils.getBooleanString((Boolean) value);
+            value = "";//DBUtils.getBooleanString((Boolean) value);
         }
         return GeneralUtils.makeDisplayString(value).toString();
     }
