78745d49b5cdc2d4b385213c2200d8d82eba2cb9
Serge Rider <serge@jkiss.org>
2015-12-04 09:08:13 +0300

    Bookmark open error handler
    Former-commit-id: 592498a4ff4a0ca9989a4fc756e30d4c7c7c5a3c

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/resources/bookmarks/BookmarksHandlerImpl.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/resources/bookmarks/BookmarksHandlerImpl.java
@@ -33,6 +33,7 @@ import org.jkiss.dbeaver.model.runtime.DBRProgressListener;
 import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
 import org.jkiss.dbeaver.model.runtime.DBRRunnableWithProgress;
 import org.jkiss.dbeaver.model.DBPDataSourceContainer;
+import org.jkiss.dbeaver.runtime.ui.DBUserInterface;
 import org.jkiss.dbeaver.ui.UIUtils;
 import org.jkiss.dbeaver.ui.actions.navigator.NavigatorHandlerObjectOpen;
 import org.jkiss.dbeaver.ui.resources.AbstractResourceHandler;
@@ -122,6 +123,11 @@ public class BookmarksHandlerImpl extends AbstractResourceHandler {
                             openNodeByPath(dsNode, (IFile) resource, storage);
                             }
                         });
+                    } else {
+                        DBUserInterface.getInstance().showError(
+                            "Open bookmark",
+                            "Can't open bookmark",
+                            status);
                     }
                 }
             });
