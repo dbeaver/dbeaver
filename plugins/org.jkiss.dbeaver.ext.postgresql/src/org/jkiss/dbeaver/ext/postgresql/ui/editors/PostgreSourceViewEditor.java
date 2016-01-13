4f0ed47b1a1afcab6d20ce1149d47d42b9fc1bd4
serge-rider <serge.rider@gmail.com>
2016-01-13 15:27:27 +0300

    #12 Postgre source viewers

---
+++ plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/ui/editors/PostgreSourceViewEditor.java
@@ -0,0 +1,46 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2015 Serge Rieder (serge@jkiss.org)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (version 2)
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package org.jkiss.dbeaver.ext.postgresql.ui.editors;
+
+import org.jkiss.dbeaver.ext.postgresql.model.PostgreScriptObject;
+import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
+import org.jkiss.dbeaver.ui.editors.sql.SQLSourceViewer;
+
+/**
+ * PostgreSourceViewEditor
+ */
+public class PostgreSourceViewEditor extends SQLSourceViewer<PostgreScriptObject> {
+
+    public PostgreSourceViewEditor()
+    {
+    }
+
+    @Override
+    protected boolean isReadOnly()
+    {
+        return false;
+    }
+
+    @Override
+    protected void setSourceText(DBRProgressMonitor monitor, String sourceText)
+    {
+        getEditorInput().getPropertySource().setPropertyValue(monitor, "objectDefinitionText", sourceText);
+    }
+
+}
