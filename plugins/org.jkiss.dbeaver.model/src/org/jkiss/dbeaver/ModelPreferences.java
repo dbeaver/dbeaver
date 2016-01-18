cf723cd9f3507068be295ab1d24def1c5b222ac0
Serge Rider <serge@jkiss.org>
2016-01-19 00:22:54 +0300

    Disable LIMIT by default
    Former-commit-id: 1722f5d9d40c74c98016b9c80450e15910a36456

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/ModelPreferences.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/ModelPreferences.java
@@ -98,7 +98,7 @@ public final class ModelPreferences
         PrefUtils.setDefaultPreferenceValue(store, NET_TUNNEL_PORT_MAX, 60000);
 
         // ResultSet
-        PrefUtils.setDefaultPreferenceValue(store, RESULT_SET_MAX_ROWS_USE_SQL, true);
+        PrefUtils.setDefaultPreferenceValue(store, RESULT_SET_MAX_ROWS_USE_SQL, false);
         PrefUtils.setDefaultPreferenceValue(store, RESULT_SET_BINARY_PRESENTATION, DBDBinaryFormatter.FORMATS[0].getId());
         PrefUtils.setDefaultPreferenceValue(store, RESULT_SET_BINARY_STRING_MAX_LEN, 32);
 
