693a53a0479f5eb0d804485c743081c9749c9b34
Denis Forveille <titou10.titou10@gmail.com>
2016-01-28 12:12:11 -0500

    DB2: fixed nickname management (data viewing was broken, attributes showed were wrong etc..)

--- plugins/org.jkiss.dbeaver.ext.db2/src/org/jkiss/dbeaver/ext/db2/model/fed/DB2Nickname.java
+++ plugins/org.jkiss.dbeaver.ext.db2/src/org/jkiss/dbeaver/ext/db2/model/fed/DB2Nickname.java
@@ -18,24 +18,51 @@
  */
 package org.jkiss.dbeaver.ext.db2.model.fed;
 
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+
+import org.jkiss.code.NotNull;
 import org.jkiss.dbeaver.DBException;
+import org.jkiss.dbeaver.ext.db2.DB2Constants;
 import org.jkiss.dbeaver.ext.db2.DB2Messages;
+import org.jkiss.dbeaver.ext.db2.editors.DB2SourceObject;
 import org.jkiss.dbeaver.ext.db2.model.DB2Schema;
-import org.jkiss.dbeaver.ext.db2.model.DB2Table;
+import org.jkiss.dbeaver.ext.db2.model.DB2TableBase;
+import org.jkiss.dbeaver.ext.db2.model.DB2TableColumn;
+import org.jkiss.dbeaver.ext.db2.model.dict.DB2TableAccessMode;
+import org.jkiss.dbeaver.ext.db2.model.dict.DB2TablePartitionMode;
+import org.jkiss.dbeaver.ext.db2.model.dict.DB2TableStatus;
 import org.jkiss.dbeaver.ext.db2.model.dict.DB2YesNo;
+import org.jkiss.dbeaver.model.DBPNamedObject2;
+import org.jkiss.dbeaver.model.DBPRefreshableObject;
+import org.jkiss.dbeaver.model.exec.DBCException;
+import org.jkiss.dbeaver.model.impl.DBObjectNameCaseTransformer;
 import org.jkiss.dbeaver.model.impl.jdbc.JDBCUtils;
+import org.jkiss.dbeaver.model.impl.jdbc.cache.JDBCStructCache;
 import org.jkiss.dbeaver.model.meta.Property;
 import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
+import org.jkiss.dbeaver.model.struct.DBSObjectState;
 import org.jkiss.utils.CommonUtils;
-
-import java.sql.ResultSet;
 
 /**
  * DB2 Federated Nickname
  * 
  * @author Denis Forveille
  */
-public class DB2Nickname extends DB2Table {
+public class DB2Nickname extends DB2TableBase implements DBPNamedObject2, DBPRefreshableObject, DB2SourceObject {
+
+    private DB2TableStatus status;
+
+    private String dataCapture;
+    private String constChecked;
+    private DB2TablePartitionMode partitionMode;
+    private DB2TableAccessMode accessMode;
+
+    private Timestamp statsTime;
+    private Long card;
+    private Long nPages;
+    private Long fPages;
+    private Long overFLow;
 
     private String remoteTableName;
     private String remoteSchemaName;
@@ -51,6 +78,19 @@ public class DB2Nickname extends DB2Table {
     {
         super(monitor, db2Schema, dbResult);
 
+        this.status = CommonUtils.valueOf(DB2TableStatus.class, JDBCUtils.safeGetString(dbResult, "STATUS"));
+        this.statsTime = JDBCUtils.safeGetTimestamp(dbResult, "STATS_TIME");
+
+        this.dataCapture = JDBCUtils.safeGetString(dbResult, "DATACAPTURE");
+        this.constChecked = JDBCUtils.safeGetString(dbResult, "CONST_CHECKED");
+        this.partitionMode = CommonUtils.valueOf(DB2TablePartitionMode.class, JDBCUtils.safeGetString(dbResult, "PARTITION_MODE"));
+        this.accessMode = CommonUtils.valueOf(DB2TableAccessMode.class, JDBCUtils.safeGetString(dbResult, "ACCESS_MODE"));
+
+        this.card = JDBCUtils.safeGetLongNullable(dbResult, "CARD");
+        this.nPages = JDBCUtils.safeGetLongNullable(dbResult, "NPAGES");
+        this.fPages = JDBCUtils.safeGetLongNullable(dbResult, "FPAGES");
+        this.overFLow = JDBCUtils.safeGetLongNullable(dbResult, "OVERFLOW");
+
         this.remoteTableName = JDBCUtils.safeGetString(dbResult, "REMOTE_TABLE");
         this.remoteSchemaName = JDBCUtils.safeGetStringTrimmed(dbResult, "REMOTE_SCHEMA");
         this.remoteType = CommonUtils.valueOf(DB2NicknameRemoteType.class, JDBCUtils.safeGetString(dbResult, "REMOTE_TYPE"));
@@ -60,7 +100,47 @@ public class DB2Nickname extends DB2Table {
         if (serverName != null) {
             this.db2RemoteServer = getDataSource().getRemoteServer(monitor, serverName);
         }
+    }
 
+    // -----------------
+    // Business Contract
+    // -----------------
+    @Override
+    public boolean isView()
+    {
+        return false;
+    }
+
+    @Override
+    public JDBCStructCache<DB2Schema, DB2Nickname, DB2TableColumn> getCache()
+    {
+        return getContainer().getNicknameCache();
+    }
+
+    @Override
+    public boolean refreshObject(@NotNull DBRProgressMonitor monitor) throws DBException
+    {
+        super.refreshObject(monitor);
+
+        getContainer().getNicknameCache().clearChildrenCache(this);
+
+        // DF: Clear base index/trigger cache. Not cheap but didn't found
+        // another way..
+        getContainer().getIndexCache().clearCache();
+
+        return true;
+    }
+
+    @NotNull
+    @Override
+    public DBSObjectState getObjectState()
+    {
+        return status.getState();
+    }
+
+    @Override
+    public void refreshObjectState(@NotNull DBRProgressMonitor monitor) throws DBCException
+    {
     }
 
     @Override
@@ -73,34 +153,110 @@ public class DB2Nickname extends DB2Table {
     // Properties
     // -----------------
 
-    @Property(viewable = true, editable = false, order = 10)
-    public String getRemoteSchemaName()
+    @NotNull
+    @Override
+    @Property(viewable = true, editable = true, valueTransformer = DBObjectNameCaseTransformer.class, order = 1)
+    public String getName()
     {
-        return remoteSchemaName;
+        return super.getName();
     }
 
-    @Property(viewable = true, editable = false, order = 11)
-    public String getRemoteTableName()
+    @Property(viewable = true, editable = false, order = 4)
+    public DB2TableStatus getStatus()
     {
-        return remoteTableName;
+        return status;
     }
 
-    @Property(viewable = true, editable = false, order = 12)
-    public DB2NicknameRemoteType getRemoteType()
+    @Property(viewable = false, editable = false, order = 100)
+    public DB2TableAccessMode getAccessMode()
     {
-        return remoteType;
+        return accessMode;
     }
 
-    @Property(viewable = true, editable = false, order = 13)
+    @Property(viewable = true, editable = false, order = 101)
+    public Boolean getCachingAllowed()
+    {
+        return cachingAllowed;
+    }
+
+    @Property(viewable = false, editable = false, category = DB2Constants.CAT_STATS)
+    public Long getCard()
+    {
+        return card;
+    }
+
+    @Property(viewable = false, editable = false, category = DB2Constants.CAT_STATS)
+    public Timestamp getStatsTime()
+    {
+        return statsTime;
+    }
+
+    @Property(viewable = false, editable = false, category = DB2Constants.CAT_STATS)
+    public Long getnPages()
+    {
+        return nPages;
+    }
+
+    @Property(viewable = false, editable = false, category = DB2Constants.CAT_STATS)
+    public Long getfPages()
+    {
+        return fPages;
+    }
+
+    @Property(viewable = false, editable = false, category = DB2Constants.CAT_STATS)
+    public Long getOverFLow()
+    {
+        return overFLow;
+    }
+
+    @Property(viewable = false, editable = false, order = 109)
+    public String getDataCapture()
+    {
+        return dataCapture;
+    }
+
+    @Property(viewable = false, editable = false, order = 111)
+    public DB2TablePartitionMode getPartitionMode()
+    {
+        return partitionMode;
+    }
+
+    @Property(viewable = false, editable = false, order = 111)
+    public String getConstChecked()
+    {
+        return constChecked;
+    }
+
+    @Property(viewable = true, editable = false, order = 10, category = DB2Constants.CAT_REMOTE)
     public DB2RemoteServer getDb2RemoteServer()
     {
         return db2RemoteServer;
     }
 
-    @Property(viewable = true, editable = false, order = 14)
-    public Boolean getCachingAllowed()
+    @Property(viewable = true, editable = false, order = 11, category = DB2Constants.CAT_REMOTE)
+    public DB2NicknameRemoteType getRemoteType()
     {
-        return cachingAllowed;
+        return remoteType;
+    }
+
+    @Property(viewable = true, editable = false, order = 12, category = DB2Constants.CAT_REMOTE)
+    public String getRemoteSchemaName()
+    {
+        return remoteSchemaName;
+    }
+
+    @Property(viewable = true, editable = false, order = 13, category = DB2Constants.CAT_REMOTE)
+    public String getRemoteTableName()
+    {
+        return remoteTableName;
+    }
+
+    // Hide TableId for nicknames
+
+    @Property(viewable = false, hidden = true)
+    public Integer getTableId()
+    {
+        return super.getTableId();
     }
 
 }
