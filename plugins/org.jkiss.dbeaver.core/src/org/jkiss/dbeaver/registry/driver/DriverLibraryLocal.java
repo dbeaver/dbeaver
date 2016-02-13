abeee396518ef8eaaf68f465c88fafc8fb0c6dc4
Serge Rider <serge@jkiss.org>
2016-02-13 11:06:36 +0300

    Platform files load fix

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/registry/driver/DriverLibraryLocal.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/registry/driver/DriverLibraryLocal.java
@@ -95,13 +95,23 @@ public class DriverLibraryLocal extends DriverLibraryAbstract
         if (url != null) {
             try {
                 url = FileLocator.toFileURL(url);
-            }
-            catch (IOException ex) {
+            } catch (IOException ex) {
                 log.warn(ex);
             }
-        }
-        if (url != null) {
-            return new File(url.getFile());
+            if (url != null) {
+                return new File(url.getFile());
+            }
+        } else {
+            try {
+                url = FileLocator.toFileURL(new URL(localFilePath));
+                File urlFile = new File(url.getFile());
+                if (urlFile.exists()) {
+                    platformFile = urlFile;
+                }
+            }
+            catch (IOException ex) {
+                // ignore
+            }
         }
 
         // Nothing fits - just return plain url
