3195ea1ff47ad45b259f8043224eabc90ace3183
Serge Rider <serge@jkiss.org>
2016-02-24 12:29:56 +0300

    #218 Cassandra timestamp columns handler fixed
    Former-commit-id: ffc3305bdac6038c7e124557a31f081a004ade9d

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/controls/resultset/ResultSetDataReceiver.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/controls/resultset/ResultSetDataReceiver.java
@@ -45,7 +45,7 @@ class ResultSetDataReceiver implements DBDDataReceiver {
     private long offset;
     private long maxRows;
 
-    private Map<DBCAttributeMetaData, List<DBCException>> errors = new HashMap<>();
+    private Map<DBCAttributeMetaData, List<Exception>> errors = new HashMap<>();
 
     ResultSetDataReceiver(ResultSetViewer resultSetViewer)
     {
@@ -102,11 +102,11 @@ class ResultSetDataReceiver implements DBDDataReceiver {
                     metaColumns[i].getAttribute(),
                     metaColumns[i].getOrdinalPosition());
             }
-            catch (DBCException e) {
+            catch (Exception e) {
                 // Do not reports the same error multiple times
                 // There are a lot of error could occur during result set fetch
                 // We report certain error only once
-                List<DBCException> errorList = errors.get(metaColumns[i].getMetaAttribute());
+                List<Exception> errorList = errors.get(metaColumns[i].getMetaAttribute());
                 if (errorList == null) {
                     errorList = new ArrayList<>();
                     errors.put(metaColumns[i].getMetaAttribute(), errorList);
