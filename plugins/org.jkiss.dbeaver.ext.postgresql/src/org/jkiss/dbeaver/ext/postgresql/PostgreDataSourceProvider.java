9ed7f398c4667f57970d290e9af1103b7cba53ba
Serge Rider <serge@jkiss.org>
2016-03-12 19:09:15 +0300

    PostgreSQL local client support
    Former-commit-id: 5e820fd6cc91d94ade53456f3b82e1fe16d634bd

--- plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/PostgreDataSourceProvider.java
+++ plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/PostgreDataSourceProvider.java
@@ -19,20 +19,29 @@ package org.jkiss.dbeaver.ext.postgresql;
 
 import org.jkiss.code.NotNull;
 import org.jkiss.dbeaver.DBException;
+import org.jkiss.dbeaver.core.DBeaverCore;
 import org.jkiss.dbeaver.ext.postgresql.model.PostgreDataSource;
 import org.jkiss.dbeaver.model.DBPDataSource;
 import org.jkiss.dbeaver.model.DBPDataSourceContainer;
+import org.jkiss.dbeaver.model.connection.DBPClientHome;
+import org.jkiss.dbeaver.model.connection.DBPClientManager;
 import org.jkiss.dbeaver.model.connection.DBPConnectionConfiguration;
 import org.jkiss.dbeaver.model.connection.DBPDriver;
 import org.jkiss.dbeaver.model.impl.jdbc.JDBCDataSourceProvider;
 import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
+import org.jkiss.dbeaver.model.runtime.OSDescriptor;
+import org.jkiss.dbeaver.utils.WinRegistry;
 import org.jkiss.utils.CommonUtils;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
-public class PostgreDataSourceProvider extends JDBCDataSourceProvider {
+public class PostgreDataSourceProvider extends JDBCDataSourceProvider implements DBPClientManager {
 
+    public static final String PG_INSTALL_REG_KEY = "SOFTWARE\\PostgreSQL\\Installations";
+    public static final String PG_INSTALL_PROP_BASE_DIRECTORY = "Base Directory";
+    public static final String PG_INSTALL_PROP_VERSION = "Version";
+    public static final String PG_INSTALL_PROP_BRANDING = "Branding";
+    public static final String PG_INSTALL_PROP_DATA_DIRECTORY = "Data Directory";
     private static Map<String,String> connectionsProps;
 
     static {
@@ -97,4 +106,73 @@ public class PostgreDataSourceProvider extends JDBCDataSourceProvider {
         return new PostgreDataSource(monitor, container);
     }
 
+    ////////////////////////////////////////////////////////////////
+    // Local client
+
+    private static Map<String,PostgreServerHome> localServers = null;
+
+    @Override
+    public Collection<String> findClientHomeIds()
+    {
+        findLocalClients();
+        Set<String> homes = new LinkedHashSet<>();
+        for (PostgreServerHome home : localServers.values()) {
+            homes.add(home.getHomeId());
+        }
+        return homes;
+    }
+
+    @Override
+    public String getDefaultClientHomeId()
+    {
+        findLocalClients();
+        return localServers.isEmpty() ? null : localServers.values().iterator().next().getHomeId();
+    }
+
+    @Override
+    public DBPClientHome getClientHome(String homeId)
+    {
+        return getServerHome(homeId);
+    }
+
+    public static PostgreServerHome getServerHome(String homeId)
+    {
+        findLocalClients();
+        return localServers.get(homeId);
+    }
+
+    public synchronized static void findLocalClients()
+    {
+        if (localServers != null) {
+            return;
+        }
+        localServers = new LinkedHashMap<>();
+
+        // find homes in Windows registry
+        OSDescriptor localSystem = DBeaverCore.getInstance().getLocalSystem();
+        if (localSystem.isWindows()) {
+            try {
+                List<String> homeKeys = WinRegistry.readStringSubKeys(WinRegistry.HKEY_LOCAL_MACHINE, PG_INSTALL_REG_KEY);
+                if (homeKeys != null) {
+                    for (String homeKey : homeKeys) {
+                        Map<String, String> valuesMap = WinRegistry.readStringValues(WinRegistry.HKEY_LOCAL_MACHINE, PG_INSTALL_REG_KEY + "\\" + homeKey);
+                        if (valuesMap != null) {
+                            for (String key : valuesMap.keySet()) {
+                                if (PG_INSTALL_PROP_BASE_DIRECTORY.equalsIgnoreCase(key)) {
+                                    String baseDir = CommonUtils.removeTrailingSlash(valuesMap.get(PG_INSTALL_PROP_BASE_DIRECTORY));
+                                    String version = valuesMap.get(PG_INSTALL_PROP_VERSION);
+                                    String branding = valuesMap.get(PG_INSTALL_PROP_BRANDING);
+                                    String dataDir = valuesMap.get(PG_INSTALL_PROP_DATA_DIRECTORY);
+                                    localServers.put(homeKey, new PostgreServerHome(homeKey, baseDir, version, branding, dataDir));
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            } catch (Throwable e) {
+                log.warn("Error reading Windows registry", e);
+            }
+        }
+    }
 }
