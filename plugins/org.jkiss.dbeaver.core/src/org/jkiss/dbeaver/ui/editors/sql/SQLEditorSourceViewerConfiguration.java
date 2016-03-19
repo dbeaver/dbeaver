95781a000d9d03ad047a91b7ccf7127c6be2f130
Serge Rider <serge@jkiss.org>
2016-03-19 17:45:55 +0300

    #267 Tokenized formatter configuration
    Former-commit-id: 6500e0677fdf33654717ebb1c682c2b8a2ce862a

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/editors/sql/SQLEditorSourceViewerConfiguration.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/editors/sql/SQLEditorSourceViewerConfiguration.java
@@ -191,12 +191,16 @@ public class SQLEditorSourceViewerConfiguration extends TextSourceViewerConfigur
             @Override
             public void preferenceChange(PreferenceChangeEvent event)
             {
-                if (event.getProperty().equals(SQLPreferenceConstants.ENABLE_AUTO_ACTIVATION)) {
-                    assistant.enableAutoActivation(configStore.getBoolean(SQLPreferenceConstants.ENABLE_AUTO_ACTIVATION));
-                } else if (event.getProperty().equals(SQLPreferenceConstants.AUTO_ACTIVATION_DELAY)) {
-                    assistant.setAutoActivationDelay(configStore.getInt(SQLPreferenceConstants.AUTO_ACTIVATION_DELAY));
-                } else if (event.getProperty().equals(SQLPreferenceConstants.INSERT_SINGLE_PROPOSALS_AUTO)) {
-                    assistant.enableAutoInsert(configStore.getBoolean(SQLPreferenceConstants.INSERT_SINGLE_PROPOSALS_AUTO));
+                switch (event.getProperty()) {
+                    case SQLPreferenceConstants.ENABLE_AUTO_ACTIVATION:
+                        assistant.enableAutoActivation(configStore.getBoolean(SQLPreferenceConstants.ENABLE_AUTO_ACTIVATION));
+                        break;
+                    case SQLPreferenceConstants.AUTO_ACTIVATION_DELAY:
+                        assistant.setAutoActivationDelay(configStore.getInt(SQLPreferenceConstants.AUTO_ACTIVATION_DELAY));
+                        break;
+                    case SQLPreferenceConstants.INSERT_SINGLE_PROPOSALS_AUTO:
+                        assistant.enableAutoInsert(configStore.getBoolean(SQLPreferenceConstants.INSERT_SINGLE_PROPOSALS_AUTO));
+                        break;
                 }
             }
         };
@@ -211,12 +215,6 @@ public class SQLEditorSourceViewerConfiguration extends TextSourceViewerConfigur
         return assistant;
 
     }
-
-    /*
-    * @see SourceViewerConfiguration#getInformationControlCreator(ISourceViewer)
-    * @since 2.0
-    *
-    */
 
     @Override
     public IInformationControlCreator getInformationControlCreator(ISourceViewer sourceViewer)
@@ -241,7 +239,7 @@ public class SQLEditorSourceViewerConfiguration extends TextSourceViewerConfigur
         ContentFormatter formatter = new ContentFormatter();
         formatter.setDocumentPartitioning(SQLPartitionScanner.SQL_PARTITIONING);
 
-        IFormattingStrategy formattingStrategy = new SQLFormattingStrategy(editor.getSyntaxManager());
+        IFormattingStrategy formattingStrategy = new SQLFormattingStrategy(sourceViewer, this, editor.getSyntaxManager());
         for (String ct : SQLPartitionScanner.SQL_PARTITION_TYPES) {
             formatter.setFormattingStrategy(formattingStrategy, ct);
         }
@@ -339,18 +337,6 @@ public class SQLEditorSourceViewerConfiguration extends TextSourceViewerConfigur
         //return new BestMatchHover(this.getSQLEditor());
         return new SQLAnnotationHover(this.getSQLEditor());
     }
-
-    /**
-     * Sets the <code>ISQLDBProposalsService</code> object that provides content
-     * assist services for this viewer to the given object.
-     *
-    public void setDBProposalsService(SQLAssistProposalsService dbProposalsService)
-    {
-        fDBProposalsService = dbProposalsService;
-        if (completionProcessor != null) {
-            completionProcessor.setProposalsService(dbProposalsService);
-        }
-    }*/
 
     @Override
     public String[] getConfiguredContentTypes(ISourceViewer sourceViewer)
