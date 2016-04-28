bb7a95be0a5b6b39b3034d01f6ac6b5a54f532ae
Serge Rider <serge@jkiss.org>
2016-04-28 21:49:46 +0300

    #414 Trigger source view
    Former-commit-id: 5f4fef84ff5fdffdd6aefde2ef4d094aaed0143c

--- plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/model/PostgreTrigger.java
+++ plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/model/PostgreTrigger.java
@@ -20,21 +20,27 @@ package org.jkiss.dbeaver.ext.postgresql.model;
 import org.jkiss.code.NotNull;
 import org.jkiss.dbeaver.DBException;
 import org.jkiss.dbeaver.Log;
+import org.jkiss.dbeaver.model.DBUtils;
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCSession;
 import org.jkiss.dbeaver.model.impl.jdbc.JDBCUtils;
 import org.jkiss.dbeaver.model.impl.struct.AbstractTrigger;
 import org.jkiss.dbeaver.model.meta.Property;
 import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
+import org.jkiss.dbeaver.model.sql.SQLUtils;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * GenericProcedure
  */
-public class PostgreTrigger extends AbstractTrigger implements PostgreScriptObject
+public class PostgreTrigger extends AbstractTrigger implements PostgreObject, PostgreScriptObject
 {
     private static final Log log = Log.getLog(PostgreTrigger.class);
 
     private PostgreTableBase table;
+    private long objectId;
+    private String whenExpression;
     private String body;
 
     public PostgreTrigger(
@@ -44,12 +50,8 @@ public class PostgreTrigger extends AbstractTrigger implements PostgreScriptObje
         this.table = table;
 
         setName(JDBCUtils.safeGetString(dbResult, "tgname"));
-        this.body = JDBCUtils.safeGetString(dbResult, "tgqual");
-    }
-
-    public String getBody()
-    {
-        return body;
+        this.objectId = JDBCUtils.safeGetLong(dbResult, "oid");
+        this.whenExpression = JDBCUtils.safeGetString(dbResult, "tgqual");
     }
 
     @Override
@@ -57,6 +59,18 @@ public class PostgreTrigger extends AbstractTrigger implements PostgreScriptObje
     public PostgreTableBase getTable()
     {
         return table;
+    }
+
+    @Override
+    @Property(viewable = true, order = 5)
+    public long getObjectId() {
+        return objectId;
+    }
+
+    @Property(viewable = true, order = 6)
+    public String getWhenExpression()
+    {
+        return whenExpression;
     }
 
     @Override
@@ -72,11 +86,25 @@ public class PostgreTrigger extends AbstractTrigger implements PostgreScriptObje
         return table.getDataSource();
     }
 
+    @NotNull
+    @Override
+    public PostgreDatabase getDatabase() {
+        return table.getDatabase();
+    }
+
     @Override
     @Property(hidden = true, editable = true, updatable = true, order = -1)
     public String getObjectDefinitionText(DBRProgressMonitor monitor) throws DBException
     {
-        return getBody();
+        if (body == null) {
+            try (JDBCSession session = DBUtils.openMetaSession(monitor, getDataSource(), "Read trigger definition")) {
+                body = JDBCUtils.queryString(session, "SELECT pg_catalog.pg_get_triggerdef(?)", objectId);
+            } catch (SQLException e) {
+                throw new DBException(e, getDataSource());
+            }
+            body = SQLUtils.formatSQL(getDataSource(), body);
+        }
+        return body;
     }
 
     @Override
