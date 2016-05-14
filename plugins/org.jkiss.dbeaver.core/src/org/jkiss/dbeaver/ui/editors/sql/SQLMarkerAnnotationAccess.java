410effd83614dd3cab5631622d99156ef2242a11
Serge Rider <serge@jkiss.org>
2016-05-14 20:41:40 +0300

    SQL editor annotations view fixed

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/editors/sql/SQLMarkerAnnotationAccess.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/editors/sql/SQLMarkerAnnotationAccess.java
@@ -17,37 +17,11 @@
  */
 package org.jkiss.dbeaver.ui.editors.sql;
 
-import org.eclipse.jface.text.source.Annotation;
-import org.eclipse.jface.text.source.IAnnotationAccess;
+import org.eclipse.ui.texteditor.DefaultMarkerAnnotationAccess;
 
 /**
  * SQLMarkerAnnotationAccess
  */
-public class SQLMarkerAnnotationAccess implements IAnnotationAccess {
-
-    @Override
-    public Object getType(Annotation annotation) {
-        return annotation.getType();
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * @deprecated assumed to always return <code>true</code>
-     */
-    @Override
-    public boolean isMultiLine(Annotation annotation) {
-        return true;
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * @deprecated assumed to always return <code>true</code>
-     */
-    @Override
-    public boolean isTemporary(Annotation annotation) {
-        return !annotation.isPersistent();
-    }
+public class SQLMarkerAnnotationAccess extends DefaultMarkerAnnotationAccess {
 
 }
