717d840dbf714613212447f1408e7ecc3e475d15
Serge Rider <serge@jkiss.org>
2016-05-14 21:46:06 +0300

    SQL editor input refactoring
    Former-commit-id: c4732720fbdf6e5fdb2ada166c4dde2eade1c496

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/editors/ProjectFileEditorInput.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/editors/ProjectFileEditorInput.java
@@ -19,143 +19,43 @@ package org.jkiss.dbeaver.ui.editors;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IStorage;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.PlatformObject;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.IPathEditorInput;
-import org.eclipse.ui.IPersistableElement;
-import org.eclipse.ui.model.IWorkbenchAdapter;
-import org.eclipse.ui.model.WorkbenchAdapter;
+import org.eclipse.ui.part.FileEditorInput;
 import org.jkiss.dbeaver.model.DBIcon;
 import org.jkiss.dbeaver.ui.DBeaverIcons;
 
 /**
  * ProjectFileEditorInput
  */
-public class ProjectFileEditorInput extends PlatformObject implements IFileEditorInput, IPathEditorInput {
-	private IFile file;
+public class ProjectFileEditorInput extends FileEditorInput implements IFileEditorInput, IPathEditorInput {
 
-	/**
-	 * Creates an editor input based of the given file resource.
-	 *
-	 * @param file the file resource
-	 */
-	public ProjectFileEditorInput(IFile file) {
-		if (file == null)
-			throw new IllegalArgumentException();
-		this.file = file;
+    /**
+     * Creates an editor input based of the given file resource.
+     *
+     * @param file the file resource
+     */
+    public ProjectFileEditorInput(IFile file) {
+        super(file);
 
-	}
+    }
 
-	public int hashCode() {
-		return file.hashCode();
-	}
-
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (!(obj instanceof ProjectFileEditorInput)) {
-			return false;
-		}
-		ProjectFileEditorInput other = (ProjectFileEditorInput) obj;
-		return file.equals(other.file);
-	}
-
-	@Override
-    public boolean exists() {
-		return file.exists();
-	}
-
-	@Override
-	public IFile getFile() {
-		return file;
-	}
-
-    public IProject getProject()
-    {
+    public IProject getProject() {
         if (getFile() == null || !getFile().exists()) {
             return null;
         }
         return getFile().getProject();
     }
 
-	@Override
+    @Override
     public ImageDescriptor getImageDescriptor() {
-		return DBeaverIcons.getImageDescriptor(DBIcon.TYPE_UNKNOWN);
-	}
-
-	/* (non-Javadoc)
-	 * Method declared on IEditorInput.
-	 */
-	@Override
-    public String getName() {
-		return file.getName();
-	}
-
-	@Override
-    public IPersistableElement getPersistable() {
-		return null;
-	}
-
-	public IStorage getStorage() {
-		return file;
-	}
-
-	/* (non-Javadoc)
-	 * Method declared on IEditorInput.
-	 */
-	@Override
-    public String getToolTipText() {
-		return file.getFullPath().makeRelative().toString();
-	}
-
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	public String toString() {
-		return getClass().getName() + "(" + file.getFullPath() + ")"; //$NON-NLS-1$ //$NON-NLS-2$
-	}
-
-	/*
-	 * Allows for the return of an {@link IWorkbenchAdapter} adapter.
-	 *
-	 * @since 3.5
-	 *
-	 * @see org.eclipse.core.runtime.PlatformObject#getAdapter(java.lang.Class)
-	 */
-	@Override
-    public Object getAdapter(Class adapter) {
-		if (IWorkbenchAdapter.class.equals(adapter)) {
-			return new WorkbenchAdapter() {
-				@Override
-                public ImageDescriptor getImageDescriptor(Object object) {
-					return ProjectFileEditorInput.this.getImageDescriptor();
-				}
-
-				@Override
-                public String getLabel(Object o) {
-					return ProjectFileEditorInput.this.getName();
-				}
-
-				@Override
-                public Object getParent(Object o) {
-					return ProjectFileEditorInput.this.file.getParent();
-				}
-			};
-		}
-
-		return super.getAdapter(adapter);
-	}
+        return DBeaverIcons.getImageDescriptor(DBIcon.TYPE_UNKNOWN);
+    }
 
     @Override
-    public IPath getPath()
-    {
-        if (file == null) {
-        	return null;
-        }
-        return file.getLocation();
+    public Object getAdapter(Class adapter) {
+        return super.getAdapter(adapter);
     }
+
 }
