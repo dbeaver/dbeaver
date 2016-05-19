5c6f4d591441c45fd4b154a226fa6a7a373fdf8c
Serge Rider <serge@jkiss.org>
2016-05-19 20:51:43 +0300

    Fetch size configuration
    Former-commit-id: 552df2c3f387c8200f9b8dafec7142474d694c2e

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/core/CoreMessages.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/core/CoreMessages.java
@@ -1209,6 +1209,7 @@ public class CoreMessages extends NLS {
     public static String pref_page_database_resultsets_label_auto_fetch_segment;
     public static String pref_page_database_resultsets_label_use_sql;
     public static String pref_page_database_resultsets_label_server_side_order;
+	public static String pref_page_database_resultsets_label_fetch_size;
 
     public static String pref_page_query_manager_checkbox_ddl_executions;
 
