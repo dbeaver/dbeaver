9bd07389cdedc1c785af41fd2d9167b7360e0941
Serge Rider <serge@jkiss.org>
2016-05-21 13:38:40 +0300

    Code cleanup
    Former-commit-id: cc4084344e40a0810c8b6ba725be84e0c4da1a3b

--- plugins/org.jkiss.dbeaver.core.application/src/org/jkiss/dbeaver/core/application/Activator.java
+++ plugins/org.jkiss.dbeaver.core.application/src/org/jkiss/dbeaver/core/application/Activator.java
@@ -2,31 +2,29 @@ package org.jkiss.dbeaver.core.application;
 
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
-import org.jkiss.dbeaver.core.application.rpc.DBeaverInstanceServer;
-import org.jkiss.dbeaver.core.application.rpc.IInstanceController;
 import org.osgi.framework.BundleContext;
 
 public class Activator extends AbstractUIPlugin {
 
     public static final String PLUGIN_ID = "org.jkiss.dbeaver.core.application";
 
-	private static Activator instance;
+    private static Activator instance;
 
-	public static Activator getInstance() {
-		return instance;
-	}
+    public static Activator getInstance() {
+        return instance;
+    }
 
-	public void start(BundleContext bundleContext) throws Exception {
-		instance = this;
-		super.start(bundleContext);
-	}
+    public void start(BundleContext bundleContext) throws Exception {
+        instance = this;
+        super.start(bundleContext);
+    }
 
-	public void stop(BundleContext bundleContext) throws Exception {
-		super.stop(bundleContext);
-		instance = null;
-	}
+    public void stop(BundleContext bundleContext) throws Exception {
+        super.stop(bundleContext);
+        instance = null;
+    }
 
-	public static ImageDescriptor getImageDescriptor(String path) {
+    public static ImageDescriptor getImageDescriptor(String path) {
         return imageDescriptorFromPlugin(PLUGIN_ID, path);
     }
 
