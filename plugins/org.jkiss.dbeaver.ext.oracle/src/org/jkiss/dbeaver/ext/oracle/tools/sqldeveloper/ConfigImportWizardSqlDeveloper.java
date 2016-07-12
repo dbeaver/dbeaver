28456f528b561f6872700d25e135b4816aa0bc72
Serge Rider <serge@jkiss.org>
2016-07-12 16:22:55 +0300

    #588 SQL Developer import config

--- plugins/org.jkiss.dbeaver.ext.oracle/src/org/jkiss/dbeaver/ext/oracle/tools/sqldeveloper/ConfigImportWizardSqlDeveloper.java
+++ plugins/org.jkiss.dbeaver.ext.oracle/src/org/jkiss/dbeaver/ext/oracle/tools/sqldeveloper/ConfigImportWizardSqlDeveloper.java
@@ -15,7 +15,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-package org.jkiss.dbeaver.ext.import_config.wizards.sqldeveloper;
+package org.jkiss.dbeaver.ext.oracle.tools.sqldeveloper;
 
 import org.jkiss.dbeaver.ext.import_config.wizards.ConfigImportWizard;
 
