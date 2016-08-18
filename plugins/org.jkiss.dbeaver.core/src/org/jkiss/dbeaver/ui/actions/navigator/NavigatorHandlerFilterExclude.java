a239bdd650899c5240ee9b66f6117eefc1cfeb56
serge-rider <serge.rider@gmail.com>
2016-08-18 19:04:55 +0300

    #662 "Include" filter in navigator menu
    Former-commit-id: e82dfbf96a858fd0ef3726218040c6e417b79a6f

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/actions/navigator/NavigatorHandlerFilterExclude.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/actions/navigator/NavigatorHandlerFilterExclude.java
@@ -17,6 +17,7 @@
  */
 package org.jkiss.dbeaver.ui.actions.navigator;
 
+import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jface.viewers.ISelection;
@@ -25,65 +26,34 @@ import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.commands.IElementUpdater;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.ui.menus.UIElement;
-import org.jkiss.dbeaver.model.navigator.DBNDatabaseFolder;
 import org.jkiss.dbeaver.model.navigator.DBNNode;
-import org.jkiss.dbeaver.model.struct.DBSObjectFilter;
 import org.jkiss.dbeaver.ui.UIUtils;
 import org.jkiss.dbeaver.ui.navigator.NavigatorUtils;
 
-import java.util.HashMap;
 import java.util.Map;
 
-public class NavigatorHandlerFilterObjects extends NavigatorHandlerObjectCreateBase implements IElementUpdater {
+public class NavigatorHandlerFilterExclude extends AbstractHandler implements IElementUpdater {
 
     @Override
     public Object execute(ExecutionEvent event) throws ExecutionException {
-        final ISelection selection = HandlerUtil.getCurrentSelection(event);
-        if (selection instanceof IStructuredSelection) {
-            Map<DBNDatabaseFolder, DBSObjectFilter> folders = new HashMap<>();
-            for (Object item : ((IStructuredSelection)selection).toArray()) {
-                if (item instanceof DBNNode) {
-                    final DBNNode node = (DBNNode) item;
-                    DBNDatabaseFolder folder = (DBNDatabaseFolder) node.getParentNode();
-                    DBSObjectFilter nodeFilter = folders.get(folder);
-                    if (nodeFilter == null) {
-                        nodeFilter = folder.getNodeFilter(folder.getItemsMeta(), true);
-                        if (nodeFilter == null) {
-                            nodeFilter = new DBSObjectFilter();
-                        }
-                        folders.put(folder, nodeFilter);
-                    }
-                    nodeFilter.addExclude(node.getNodeName());
-                    nodeFilter.setEnabled(true);
-                }
-            }
-            // Save folders
-            for (Map.Entry<DBNDatabaseFolder, DBSObjectFilter> entry : folders.entrySet()) {
-                entry.getKey().setNodeFilter(entry.getKey().getItemsMeta(), entry.getValue());
-            }
-            // Refresh all folders
-            NavigatorHandlerRefresh.refreshNavigator(folders.keySet());
-        }
+        NavigatorUtils.filterSelection(HandlerUtil.getCurrentSelection(event), true);
         return null;
     }
 
     @Override
     public void updateElement(UIElement element, Map parameters)
     {
-        if (!updateUI) {
-            return;
-        }
         ISelectionProvider selectionProvider = UIUtils.getSelectionProvider(element.getServiceLocator());
         if (selectionProvider != null) {
             final ISelection selection = selectionProvider.getSelection();
             if (selection instanceof IStructuredSelection) {
                 final int objectCount = ((IStructuredSelection) selection).size();
                 if (objectCount > 1) {
-                    element.setText("Filter " + objectCount + " objects");
+                    element.setText("Hide " + objectCount + " objects");
                 } else {
                     DBNNode node = NavigatorUtils.getSelectedNode(selection);
                     if (node != null) {
-                        element.setText("Filter " + node.getNodeType());
+                        element.setText("Hide " + node.getNodeType());
                     }
                 }
             }
