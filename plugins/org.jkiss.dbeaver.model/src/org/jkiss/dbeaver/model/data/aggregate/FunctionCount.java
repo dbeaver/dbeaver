ab4dd651978d0614b2324ec364aee3b698926165
Serge Rider <serge@jkiss.org>
2016-08-22 22:50:18 +0300

    #188 Aggregate functions model
    Former-commit-id: ed20b59257c4dcffce4df84df6360e7a0b4bd8f6

---
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/data/aggregate/FunctionCount.java
@@ -0,0 +1,36 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2016 Serge Rieder (serge@jkiss.org)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (version 2)
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package org.jkiss.dbeaver.model.data.aggregate;
+
+/**
+ * FunctionCount
+ */
+public class FunctionCount implements IAggregateFunction {
+
+    private int count = 0;
+
+    @Override
+    public void accumulate(Number value) {
+        count++;
+    }
+
+    @Override
+    public Number getResult(int valueCount) {
+        return count;
+    }
+}
