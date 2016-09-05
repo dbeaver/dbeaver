6f17190f1b3501c9c1f2cd25c8290ca20b751f29
serge-rider <serge.rider@gmail.com>
2016-09-05 17:38:23 +0300

    Version update checker redesign
    Former-commit-id: a16b90edd6e561a97808143b1ac5e55fc1a196e2

--- plugins/org.jkiss.dbeaver.core.application/src/org/jkiss/dbeaver/core/application/update/DBeaverVersionChecker.java
+++ plugins/org.jkiss.dbeaver.core.application/src/org/jkiss/dbeaver/core/application/update/DBeaverVersionChecker.java
@@ -17,6 +17,7 @@
  */
 package org.jkiss.dbeaver.core.application.update;
 
+import org.jkiss.dbeaver.DBeaverPreferences;
 import org.jkiss.dbeaver.Log;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
@@ -28,6 +29,8 @@ import org.jkiss.dbeaver.registry.updater.VersionDescriptor;
 import org.jkiss.dbeaver.model.runtime.AbstractJob;
 
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.Random;
 
 /**
  * Version checker job
@@ -38,19 +41,46 @@ public class DBeaverVersionChecker extends AbstractJob {
 
     private final boolean showAlways;
 
-    public DBeaverVersionChecker(boolean showAlways)
+    public DBeaverVersionChecker(boolean force)
     {
         super("DBeaver new version release checker");
-        this.showAlways = showAlways;
+        this.showAlways = force;
     }
 
     @Override
     protected IStatus run(DBRProgressMonitor monitor)
     {
+        boolean showUpdateDialog = showAlways;
+        if (!showUpdateDialog) {
+            // Check for auto-update settings
+            showUpdateDialog = DBeaverCore.getGlobalPreferenceStore().getBoolean(DBeaverPreferences.UI_AUTO_UPDATE_CHECK);
+            if (showUpdateDialog) {
+
+                long lastVersionCheckTime = DBeaverCore.getGlobalPreferenceStore().getLong(DBeaverPreferences.UI_UPDATE_CHECK_TIME);
+                if (lastVersionCheckTime > 0) {
+                    // Do not check more often than daily
+                    Calendar cal = Calendar.getInstance();
+                    cal.setTimeInMillis(lastVersionCheckTime);
+                    int checkMonth = cal.get(Calendar.MONTH);
+                    int checkDay = cal.get(Calendar.DAY_OF_MONTH);
+                    cal.setTimeInMillis(System.currentTimeMillis());
+                    int curMonth = cal.get(Calendar.MONTH);
+                    int curDay = cal.get(Calendar.DAY_OF_MONTH);
+                    if (curMonth == checkMonth && curDay == checkDay) {
+                        // Already checked today
+                        return Status.OK_STATUS;
+                    }
+                }
+            }
+        }
         try {
+            DBeaverCore.getGlobalPreferenceStore().setValue(DBeaverPreferences.UI_UPDATE_CHECK_TIME, System.currentTimeMillis());
             VersionDescriptor versionDescriptor = new VersionDescriptor(VersionDescriptor.DEFAULT_VERSION_URL);
+
             if (versionDescriptor.getProgramVersion().compareTo(DBeaverCore.getVersion()) > 0) {
-                showUpdaterDialog(versionDescriptor);
+                if (showAlways || showUpdateDialog) {
+                    showUpdaterDialog(versionDescriptor);
+                }
             } else if (showAlways) {
                 showUpdaterDialog(null);
             }
