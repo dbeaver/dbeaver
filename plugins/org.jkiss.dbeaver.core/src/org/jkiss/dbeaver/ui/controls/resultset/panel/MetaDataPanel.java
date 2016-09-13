d103cf506408fa551870467c9fa5922274fd08df
Serge Rider <serge@jkiss.org>
2016-09-14 00:43:28 +0300

    #717 Metadata panel
    Former-commit-id: 4f0cea6d3cd343c983bf9e765b91cb33c5ac68fa

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/controls/resultset/panel/MetaDataPanel.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/controls/resultset/panel/MetaDataPanel.java
@@ -17,15 +17,27 @@
  */
 package org.jkiss.dbeaver.ui.controls.resultset.panel;
 
+import ch.qos.logback.core.db.dialect.DBUtil;
+import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.ToolBarManager;
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
 import org.jkiss.dbeaver.Log;
 import org.jkiss.dbeaver.model.DBPImage;
+import org.jkiss.dbeaver.model.DBUtils;
+import org.jkiss.dbeaver.model.data.DBDAttributeBinding;
+import org.jkiss.dbeaver.ui.DBeaverIcons;
 import org.jkiss.dbeaver.ui.UIIcon;
 import org.jkiss.dbeaver.ui.UIUtils;
+import org.jkiss.dbeaver.ui.ViewerColumnController;
+import org.jkiss.dbeaver.ui.controls.ListContentProvider;
+import org.jkiss.dbeaver.ui.controls.TreeContentProvider;
 import org.jkiss.dbeaver.ui.controls.resultset.IResultSetPanel;
 import org.jkiss.dbeaver.ui.controls.resultset.IResultSetPresentation;
 
@@ -39,7 +51,37 @@ public class MetaDataPanel implements IResultSetPanel {
     public static final String PANEL_ID = "results-metadata";
 
     private IResultSetPresentation presentation;
-    private Tree attributeList;
+    private TableViewer attributeList;
+    private ViewerColumnController attrController;
+
+    private enum AttributeProperty {
+        NAME("Attribute", "Attribute name", true, true),
+        LABEL("Label", "Attribute label", false, false),
+        POSITION("#", "Ordinal position", true, true),
+        TYPE("Type", "Data type name", true, true),
+        TYPE_ID("Type ID", "Data type ID", false, false),
+        LENGTH("Length", "Max length", true, false),
+
+        PRECISION("Precision", "Precision", false, false),
+        SCALE("Scale", "Scale", false, false),
+
+        TABLE("Table", "Table name", true, false),
+        SCHEMA("Schema", "Table schema name", false, false),
+        CATALOG("Catalog", "Table catalog name", false, false),
+        ;
+
+        private final String title;
+        private final String description;
+        private final boolean isDefault;
+        private final boolean isRequired;
+
+        AttributeProperty(String title, String description, boolean isDefault, boolean isRequired) {
+            this.title = title;
+            this.description = description;
+            this.isDefault = isDefault;
+            this.isRequired = isRequired;
+        }
+    }
 
     public MetaDataPanel() {
     }
@@ -63,10 +105,23 @@ public class MetaDataPanel implements IResultSetPanel {
     public Control createContents(final IResultSetPresentation presentation, Composite parent) {
         this.presentation = presentation;
 
-        this.attributeList = new Tree(parent, SWT.NONE);
-        this.attributeList.setLinesVisible(true);
+        this.attributeList = new TableViewer(parent, SWT.NONE);
+        this.attributeList.getTable().setHeaderVisible(true);
+        this.attributeList.getTable().setLinesVisible(true);
+        this.attrController = new ViewerColumnController(PANEL_ID, this.attributeList);
+        for (AttributeProperty prop : AttributeProperty.values()) {
+            attrController.addColumn(
+                prop.title,
+                prop.description,
+                SWT.LEFT,
+                prop.isDefault,
+                prop.isRequired,
+                new PropertyLabelProvider(prop));
+        }
+        attrController.createColumns();
+        this.attributeList.setContentProvider(new ListContentProvider());
 
-        return this.attributeList;
+        return this.attributeList.getTable();
     }
 
 
@@ -82,17 +137,69 @@ public class MetaDataPanel implements IResultSetPanel {
 
     @Override
     public void refresh() {
-        attributeList.setRedraw(false);
+        Table table = attributeList.getTable();
+        table.setRedraw(false);
         try {
-            UIUtils.packColumns(attributeList);
-
+            attributeList.setInput(presentation.getController().getModel().getVisibleAttributes());
+            for (TableColumn column : attributeList.getTable().getColumns()) {
+                column.pack();
+            }
+            //UIUtils.packColumns(table, false);
+            //this.attrController.repackColumns();
         } finally {
-            attributeList.setRedraw(true);
+            table.setRedraw(true);
         }
     }
 
     @Override
     public void contributeActions(ToolBarManager manager) {
+        manager.add(new Action("Configure columns", DBeaverIcons.getImageDescriptor(UIIcon.PROPERTIES)) {
+            @Override
+            public void run() {
+                attrController.configureColumns();
+            }
+        });
     }
 
+    private class PropertyLabelProvider extends ColumnLabelProvider {
+        private final AttributeProperty property;
+
+        private PropertyLabelProvider(AttributeProperty property) {
+            this.property = property;
+        }
+
+        @Override
+        public Image getImage(Object element) {
+            if (property == AttributeProperty.NAME) {
+                return DBeaverIcons.getImage(DBUtils.getTypeImage(((DBDAttributeBinding) element).getMetaAttribute()));
+            }
+            return null;
+        }
+
+        @Override
+        public String getText(Object element) {
+            DBDAttributeBinding binding = (DBDAttributeBinding) element;
+            switch (property) {
+                case NAME:
+                    return binding.getMetaAttribute().getName();
+                case LABEL:
+                    return binding.getMetaAttribute().getLabel();
+                case POSITION:
+                    return String.valueOf(binding.getMetaAttribute().getOrdinalPosition());
+                case TYPE:
+                    return binding.getMetaAttribute().getTypeName();
+                case TYPE_ID:
+                    return String.valueOf(binding.getMetaAttribute().getTypeID());
+                case LENGTH:
+                    return String.valueOf(binding.getMetaAttribute().getMaxLength());
+                case PRECISION:
+                    return String.valueOf(binding.getMetaAttribute().getPrecision());
+                case SCALE:
+                    return String.valueOf(binding.getMetaAttribute().getScale());
+                case TABLE:
+                    return binding.getMetaAttribute().getEntityName();
+            }
+            return null;
+        }
+    }
 }
