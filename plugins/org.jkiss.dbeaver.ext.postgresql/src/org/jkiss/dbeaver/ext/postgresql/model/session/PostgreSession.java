57f0d7de8250210ff73446fccdd2fd473583506c
Serge Rider <serge@jkiss.org>
2016-09-19 23:07:28 +0300

    #746 PG: terminate session fix
    Former-commit-id: f6c55172182590c8c869fe9a41f11cea4d223aa2

--- plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/model/session/PostgreSession.java
+++ plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/model/session/PostgreSession.java
@@ -32,7 +32,7 @@ public class PostgreSession implements DBAServerSession {
     private static final String CAT_CLIENT = "Client";
     private static final String CAT_TIMING = "Timings";
 
-    private long pid;
+    private int pid;
     private String user;
     private String clientHost;
     private String clientPort;
@@ -46,7 +46,7 @@ public class PostgreSession implements DBAServerSession {
     private String appName;
 
     public PostgreSession(ResultSet dbResult) {
-        this.pid = JDBCUtils.safeGetLong(dbResult, "pid");
+        this.pid = JDBCUtils.safeGetInt(dbResult, "pid");
         this.user = JDBCUtils.safeGetString(dbResult, "usename");
         this.clientHost = JDBCUtils.safeGetString(dbResult, "client_hostname");
         if (CommonUtils.isEmpty(this.clientHost)) {
@@ -66,7 +66,7 @@ public class PostgreSession implements DBAServerSession {
     }
 
     @Property(viewable = true, order = 1)
-    public long getPid()
+    public int getPid()
     {
         return pid;
     }
