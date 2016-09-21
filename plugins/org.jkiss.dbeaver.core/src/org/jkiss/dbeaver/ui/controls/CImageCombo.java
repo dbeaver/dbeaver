7e1758a4f60c11992216693afa8071900d0ef90d
Serge Rider <serge@jkiss.org>
2016-09-21 21:14:39 +0300

    Image combo fix (focusable + text control)
    Former-commit-id: ee17e0b76a2e3b9dac1f1b6b48bd6ee8881edfa8

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/controls/CImageCombo.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/controls/CImageCombo.java
@@ -69,7 +69,7 @@ public class CImageCombo extends Composite {
 
     private Composite comboComposite;
     private Label imageLabel;
-    private Label text;
+    private Text text;
     private Table table;
     private int visibleItemCount = 4;
     private int widthHint = SWT.DEFAULT;
@@ -137,7 +137,7 @@ public class CImageCombo extends Composite {
         this.imageLabel = new Label(this.comboComposite, SWT.NONE);
         this.imageLabel.setLayoutData(new GridData(GridData.FILL_VERTICAL | GridData.HORIZONTAL_ALIGN_BEGINNING | GridData.VERTICAL_ALIGN_CENTER));
 
-        this.text = new Label(this.comboComposite, SWT.NONE);
+        this.text = new Text(this.comboComposite, SWT.READ_ONLY);
         this.text.setLayoutData(new GridData(GridData.FILL_HORIZONTAL | GridData.VERTICAL_ALIGN_CENTER));
 
         this.comboComposite.setCursor(getDisplay().getSystemCursor(SWT.CURSOR_ARROW));
