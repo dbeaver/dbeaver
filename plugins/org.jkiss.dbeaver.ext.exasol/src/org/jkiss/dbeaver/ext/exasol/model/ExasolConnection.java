447917ffb012d2543245f9775f4fdda9afa38ef0
Karl Griesser <fullref@gmail.com>
2016-10-14 14:59:55 +0200

    new Con Object; Users only show if sufficient privs
    Former-commit-id: 25200ba261888cf347fa345c4c6258344ac5995c

--- plugins/org.jkiss.dbeaver.ext.exasol/src/org/jkiss/dbeaver/ext/exasol/model/ExasolConnection.java
+++ plugins/org.jkiss.dbeaver.ext.exasol/src/org/jkiss/dbeaver/ext/exasol/model/ExasolConnection.java
@@ -18,6 +18,111 @@
  */
 package org.jkiss.dbeaver.ext.exasol.model;
 
-public class ExasolConnection {
+import java.sql.Date;
+import java.sql.ResultSet;
+
+import org.jkiss.dbeaver.DBException;
+import org.jkiss.dbeaver.model.DBPDataSource;
+import org.jkiss.dbeaver.model.DBPRefreshableObject;
+import org.jkiss.dbeaver.model.DBPSaveableObject;
+import org.jkiss.dbeaver.model.impl.jdbc.JDBCUtils;
+import org.jkiss.dbeaver.model.meta.Property;
+import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
+import org.jkiss.dbeaver.model.struct.DBSObject;
+
+public class ExasolConnection
+		implements DBPRefreshableObject, DBPSaveableObject {
+
+	private ExasolDataSource dataSource;
+	private String connectionName;
+	private String connectionString;
+	private String userName;
+	private Date created;
+	private String comment;
+	private Boolean persisted;
+
+	public ExasolConnection(ExasolDataSource dataSource, ResultSet dbResult)
+	{
+		this.dataSource = dataSource;
+		if (dbResult != null) {
+			this.persisted = true;
+			this.connectionName = JDBCUtils.safeGetString(dbResult,
+					"CONNECTION_NAME");
+			this.connectionString = JDBCUtils.safeGetString(dbResult,
+					"CONNECTION_STRING");
+			this.created = JDBCUtils.safeGetDate(dbResult, "CREATED");
+			this.comment = JDBCUtils.safeGetString(dbResult,
+					"CONNECTION_COMMENT");
+			this.userName = JDBCUtils.safeGetString(dbResult, "USER_NAME");
+		} else {
+			this.connectionName = "new connection";
+			this.persisted = false;
+		}
+
+	}
+
+	@Override
+	@Property(viewable = true, order = 10)
+	public String getName()
+	{
+		return this.connectionName;
+	}
+
+	@Property(viewable = true, order = 20)
+	public String getConnectionString()
+	{
+		return this.connectionString;
+	}
+
+	@Property(viewable = true, order = 30)
+	public Date getCreated()
+	{
+		return this.created;
+	}
+
+	@Property(viewable = true, order = 30)
+	public String getUserName()
+	{
+		return this.userName;
+	}
+
+	@Override
+	@Property(viewable = true, order = 50)
+	public String getDescription()
+	{
+		return this.comment;
+	}
+
+	@Override
+	public DBSObject getParentObject()
+	{
+		return this.dataSource.getContainer();
+	}
+
+	@Override
+	public DBPDataSource getDataSource()
+	{
+		return this.dataSource;
+	}
+
+	@Override
+	public boolean isPersisted()
+	{
+		return this.persisted;
+	}
+
+	@Override
+	public void setPersisted(boolean persisted)
+	{
+		this.persisted = persisted;
+
+	}
+
+	@Override
+	public DBSObject refreshObject(DBRProgressMonitor monitor)
+			throws DBException
+	{
+		return this;
+	}
 
 }
