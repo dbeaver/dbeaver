6a892a9dfa3194a17b3fe52dc1d7cfe7bc0cbb9a
Serge Rider <serge@jkiss.org>
2016-10-23 12:58:42 +0300

    #875 Columns pack action. EEditor fix (reset changes on refresh)

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/editors/entity/EntityEditor.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/editors/entity/EntityEditor.java
@@ -680,13 +680,13 @@ public class EntityEditor extends MultiPageDatabaseEditor
     @Override
     public void refreshPart(final Object source, boolean force)
     {
-        // TODO: make smart content refresh
         if (force && getDatabaseObject().isPersisted()) {
             // Lists and commands should be refreshed only if we make real refresh from remote storage
             // Otherwise just update object's properties
             DBECommandContext commandContext = getCommandContext();
             if (commandContext != null) {
                 // FIXME: resetChanges refreshes editor one more time and eventually leads to node reload/close.
+                // FIXME: maybe already fixed??
                 commandContext.resetChanges();
             }
         }
