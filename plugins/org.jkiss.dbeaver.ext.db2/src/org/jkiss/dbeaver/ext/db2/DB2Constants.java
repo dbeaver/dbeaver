30eb806bc448b289a93ece1a52093b0261f4a31f
Serge Rider <serge@jkiss.org>
2016-11-07 22:53:38 +0300

    #940 DB2 rid_bit fix.

--- plugins/org.jkiss.dbeaver.ext.db2/src/org/jkiss/dbeaver/ext/db2/DB2Constants.java
+++ plugins/org.jkiss.dbeaver.ext.db2/src/org/jkiss/dbeaver/ext/db2/DB2Constants.java
@@ -125,6 +125,6 @@ public class DB2Constants {
         "YEAR", "YEARS", };
 
     public static final DBDPseudoAttribute PSEUDO_ATTR_RID_BIT         = new DBDPseudoAttribute(DBDPseudoAttributeType.ROWID,
-        "RID_BIT()", "RID_BIT($alias)", "rid_bit", "Unique physical row identifier", false);
+        "RID_BIT()", "RID_BIT($alias)", "RID_BIT", "Unique physical row identifier", false);
 
 }
