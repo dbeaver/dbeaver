cb4abadfb6ed7403d81fcb24a72c157b705f521e
Serge Rider <serge@jkiss.org>
2016-11-15 00:43:35 +0300

    #918 Hypelinks rendering performance.
    Former-commit-id: 03605b9d382390d6a518a51e2ec7864296712cb6

---
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/data/managers/URLPreviewValueManager.java
@@ -0,0 +1,51 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2016 Serge Rieder (serge@jkiss.org)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (version 2)
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package org.jkiss.dbeaver.ui.data.managers;
+
+import org.jkiss.code.NotNull;
+import org.jkiss.dbeaver.DBException;
+import org.jkiss.dbeaver.ui.data.IValueController;
+import org.jkiss.dbeaver.ui.data.IValueEditor;
+import org.jkiss.dbeaver.ui.data.editors.StringInlineEditor;
+import org.jkiss.dbeaver.ui.dialogs.data.TextViewDialog;
+
+/**
+ * URL preview value manager
+ */
+public class URLPreviewValueManager extends BaseValueManager {
+
+    @NotNull
+    @Override
+    public IValueController.EditType[] getSupportedEditTypes() {
+        return new IValueController.EditType[] {IValueController.EditType.INLINE, IValueController.EditType.PANEL, IValueController.EditType.EDITOR};
+    }
+
+    @Override
+    public IValueEditor createEditor(@NotNull IValueController controller) throws DBException {
+        switch (controller.getEditType()) {
+            case INLINE:
+            case PANEL:
+                return new StringInlineEditor(controller);
+            case EDITOR:
+                return new TextViewDialog(controller);
+            default:
+                return null;
+        }
+    }
+
+}
