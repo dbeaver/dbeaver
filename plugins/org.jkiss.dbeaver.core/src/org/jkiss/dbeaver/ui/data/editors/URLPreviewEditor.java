ac5ac4cc3e4ea00a337a8e4eb2a878ddd68a5770
Serge Rider <serge@jkiss.org>
2016-11-15 00:58:48 +0300

    #918 Hyperlinks rendering panel
    Former-commit-id: ba44a3797bcbf1967996bf196357c7f8d389ff97

---
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/data/editors/URLPreviewEditor.java
@@ -0,0 +1,59 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2016 Serge Rieder (serge@jkiss.org)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (version 2)
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package org.jkiss.dbeaver.ui.data.editors;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.widgets.Composite;
+import org.jkiss.code.Nullable;
+import org.jkiss.dbeaver.DBException;
+import org.jkiss.dbeaver.model.data.DBDDisplayFormat;
+import org.jkiss.dbeaver.model.exec.DBCException;
+import org.jkiss.dbeaver.ui.data.IValueController;
+
+/**
+* URLPreviewEditor
+*/
+public class URLPreviewEditor extends BaseValueEditor<Browser> {
+
+    public URLPreviewEditor(IValueController controller) {
+        super(controller);
+    }
+
+    @Override
+    protected Browser createControl(Composite editPlaceholder)
+    {
+        Browser browser = new Browser(editPlaceholder, SWT.NONE);
+        // Set browser settings
+
+        return browser;
+    }
+
+    @Override
+    public void primeEditorValue(@Nullable Object value) throws DBException
+    {
+        final String strValue = valueController.getValueHandler().getValueDisplayString(valueController.getValueType(), value, DBDDisplayFormat.EDIT);
+        control.setText("");
+        control.setUrl(strValue);
+    }
+
+    @Override
+    public Object extractEditorValue() throws DBCException {
+        return control.getUrl();
+    }
+}
