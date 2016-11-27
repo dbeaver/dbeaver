88e30367753410f1245211d59a3b333319ded4e6
Serge Rider <serge@jkiss.org>
2016-11-28 00:49:11 +0300

    H2 view definition, sequences

--- plugins/org.jkiss.dbeaver.ext.h2/src/org/jkiss/dbeaver/ext/h2/model/H2MetaModel.java
+++ plugins/org.jkiss.dbeaver.ext.h2/src/org/jkiss/dbeaver/ext/h2/model/H2MetaModel.java
@@ -18,20 +18,20 @@
 package org.jkiss.dbeaver.ext.h2.model;
 
 import org.eclipse.core.runtime.IConfigurationElement;
+import org.jkiss.code.NotNull;
 import org.jkiss.dbeaver.DBException;
-import org.jkiss.dbeaver.ext.generic.model.GenericDataSource;
-import org.jkiss.dbeaver.ext.generic.model.GenericProcedure;
-import org.jkiss.dbeaver.ext.generic.model.GenericTable;
-import org.jkiss.dbeaver.ext.generic.model.GenericTableColumn;
+import org.jkiss.dbeaver.ext.generic.model.*;
 import org.jkiss.dbeaver.ext.generic.model.meta.GenericMetaModel;
-import org.jkiss.dbeaver.model.DBPEvaluationContext;
 import org.jkiss.dbeaver.model.DBUtils;
 import org.jkiss.dbeaver.model.exec.jdbc.JDBCPreparedStatement;
 import org.jkiss.dbeaver.model.exec.jdbc.JDBCResultSet;
 import org.jkiss.dbeaver.model.exec.jdbc.JDBCSession;
+import org.jkiss.dbeaver.model.impl.jdbc.JDBCUtils;
 import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * H2MetaModel
@@ -40,6 +40,65 @@ public class H2MetaModel extends GenericMetaModel
 {
     public H2MetaModel(IConfigurationElement cfg) {
         super(cfg);
+    }
+
+    @Override
+    public String getViewDDL(DBRProgressMonitor monitor, GenericTable sourceObject) throws DBException {
+        GenericDataSource dataSource = sourceObject.getDataSource();
+        try (JDBCSession session = DBUtils.openMetaSession(monitor, dataSource, "Read H2 view source")) {
+            try (JDBCPreparedStatement dbStat = session.prepareStatement(
+                "SELECT VIEW_DEFINITION FROM INFORMATION_SCHEMA.VIEWS " +
+                    "WHERE TABLE_SCHEMA=? AND TABLE_NAME=?"))
+            {
+                dbStat.setString(1, sourceObject.getContainer().getName());
+                dbStat.setString(2, sourceObject.getName());
+                try (JDBCResultSet dbResult = dbStat.executeQuery()) {
+                    if (dbResult.nextRow()) {
+                        return dbResult.getString(1);
+                    }
+                    return "-- H2 view definition not found";
+                }
+            }
+        } catch (SQLException e) {
+            throw new DBException(e, dataSource);
+        }
+    }
+
+    @Override
+    public boolean supportsSequences(@NotNull GenericDataSource dataSource) {
+        return true;
+    }
+
+    @Override
+    public List<GenericSequence> loadSequences(@NotNull DBRProgressMonitor monitor, @NotNull GenericStructContainer container) throws DBException {
+        try (JDBCSession session = DBUtils.openMetaSession(monitor, container.getDataSource(), "Read sequences")) {
+            try (JDBCPreparedStatement dbStat = session.prepareStatement("SELECT * FROM INFORMATION_SCHEMA.SEQUENCES")) {
+                List<GenericSequence> result = new ArrayList<>();
+
+                try (JDBCResultSet dbResult = dbStat.executeQuery()) {
+                    while (dbResult.next()) {
+                        String name = JDBCUtils.safeGetString(dbResult, "SEQUENCE_NAME");
+                        if (name == null) {
+                            continue;
+                        }
+                        String description = JDBCUtils.safeGetString(dbResult, "REMARKS");
+                        GenericSequence sequence = new GenericSequence(
+                            container,
+                            name,
+                            description,
+                            JDBCUtils.safeGetLong(dbResult, "CURRENT_VALUE"),
+                            JDBCUtils.safeGetLong(dbResult, "MIN_VALUE"),
+                            JDBCUtils.safeGetLong(dbResult, "MAX_VALUE"),
+                            JDBCUtils.safeGetLong(dbResult, "INCREMENT")
+                        );
+                        result.add(sequence);
+                    }
+                }
+                return result;
+            }
+        } catch (SQLException e) {
+            throw new DBException(e, container.getDataSource());
+        }
     }
 
     @Override
