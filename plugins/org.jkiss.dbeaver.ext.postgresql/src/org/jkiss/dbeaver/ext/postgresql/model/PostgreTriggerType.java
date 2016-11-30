0727ae42b249497abdeedbb472d8819e1ffdedac
Serge Rider <serge@jkiss.org>
2016-11-30 20:35:34 +0300

    #978 PG trigger properties fix
    Former-commit-id: 764d48b98619daa19f1d80df277bb46355d76805

---
+++ plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/model/PostgreTriggerType.java
@@ -0,0 +1,47 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2016 Serge Rieder (serge@jkiss.org)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (version 2)
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package org.jkiss.dbeaver.ext.postgresql.model;
+
+import org.jkiss.dbeaver.model.DBPNamedObject;
+
+/**
+ * PostgreTriggerType
+ */
+public class PostgreTriggerType implements DBPNamedObject
+{
+    public static final PostgreTriggerType ROW = new PostgreTriggerType("ROW");
+    public static final PostgreTriggerType STATEMENT = new PostgreTriggerType("STATEMENT");
+
+    private final String name;
+
+    protected PostgreTriggerType(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String toString()
+    {
+        return getName();
+    }
+
+}
