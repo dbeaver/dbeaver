ef21d658cbfb76e3afa54d5a696207b23f9c9449
Serge Rider <serge@jkiss.org>
2016-11-30 23:07:35 +0300

    #1016 ERD refresh handler

---
+++ plugins/org.jkiss.dbeaver.ext.erd/src/org/jkiss/dbeaver/ext/erd/action/ERDHandlerRefresh.java
@@ -0,0 +1,46 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2016 Serge Rieder (serge@jkiss.org)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (version 2)
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package org.jkiss.dbeaver.ext.erd.action;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.ISources;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.jkiss.dbeaver.ext.erd.editor.ERDEditorAdapter;
+import org.jkiss.dbeaver.ext.erd.editor.ERDEditorPart;
+
+public class ERDHandlerRefresh extends AbstractHandler {
+    public ERDHandlerRefresh() {
+
+    }
+
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        Control control = (Control) HandlerUtil.getVariable(event, ISources.ACTIVE_FOCUS_CONTROL_NAME);
+        if (control != null) {
+            ERDEditorPart editor = ERDEditorAdapter.getEditor(control);
+            if (editor != null) {
+                editor.refreshDiagram(true);
+            }
+        }
+        return null;
+    }
+
+}
