bff7ef9b9f16f3c9569877101d50bc581c3e9add
Serge Rider <serge@jkiss.org>
2016-12-25 23:49:21 +0300

    #1099 Constraint delete fix (quote identifiers)
    Former-commit-id: 3428787664e41b5422a89a6d5e6f52ed31cdec4b

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/impl/sql/edit/struct/SQLForeignKeyManager.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/impl/sql/edit/struct/SQLForeignKeyManager.java
@@ -71,7 +71,7 @@ public abstract class SQLForeignKeyManager<OBJECT_TYPE extends JDBCTableConstrai
                 ModelMessages.model_jdbc_drop_foreign_key,
                 getDropForeignKeyPattern(command.getObject())
                     .replace(PATTERN_ITEM_TABLE, command.getObject().getTable().getFullyQualifiedName(DBPEvaluationContext.DDL))
-                    .replace(PATTERN_ITEM_CONSTRAINT, command.getObject().getName()))
+                    .replace(PATTERN_ITEM_CONSTRAINT, DBUtils.getQuotedIdentifier(command.getObject())))
         );
     }
 
