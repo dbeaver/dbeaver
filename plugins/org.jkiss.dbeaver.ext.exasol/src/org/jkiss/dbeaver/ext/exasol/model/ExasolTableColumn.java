f294e5d77fc4e97766e9d6a26c0bef14b50ab866
Charly <fullref@gmail.com>
2017-01-11 09:31:47 +0100

    Required has wrong value
    Fix wrong value for Required (NOT NULL)
    
    Former-commit-id: 462fe551e522bac6ee03e9cb62146b5cb4f336ae

--- plugins/org.jkiss.dbeaver.ext.exasol/src/org/jkiss/dbeaver/ext/exasol/model/ExasolTableColumn.java
+++ plugins/org.jkiss.dbeaver.ext.exasol/src/org/jkiss/dbeaver/ext/exasol/model/ExasolTableColumn.java
@@ -64,7 +64,7 @@ public class ExasolTableColumn extends JDBCTableColumn<ExasolTableBase>
         this.formatType = JDBCUtils.safeGetString(dbResult, "COLUMN_TYPE");
         setName(JDBCUtils.safeGetString(dbResult, "COLUMN_NAME"));
         setOrdinalPosition(JDBCUtils.safeGetInt(dbResult, "ORDINAL_POSITION"));
-        setRequired(JDBCUtils.safeGetBoolean(dbResult, "COLUMN_IS_NULLABLE"));
+        setRequired(! JDBCUtils.safeGetBoolean(dbResult, "COLUMN_IS_NULLABLE"));
         setDefaultValue(JDBCUtils.safeGetString(dbResult, "COLUMN_DEF"));
         setMaxLength(JDBCUtils.safeGetInt(dbResult, "COLUMN_SIZE"));
         setScale(JDBCUtils.safeGetInt(dbResult, "DECIMAL_DIGITS"));
