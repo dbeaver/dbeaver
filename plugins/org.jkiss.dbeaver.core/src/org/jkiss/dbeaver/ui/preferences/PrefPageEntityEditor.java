e710802ebe5c234fdde50d8e5e613aae722da545
Serge Rider <serge@jkiss.org>
2017-01-14 02:05:51 +0300

    Preference pages refactoring
    Former-commit-id: 9deabb7f9727365a3c540410bb0a34a9c43fe222

---
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/preferences/PrefPageEntityEditor.java
@@ -0,0 +1,122 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2016 Serge Rieder (serge@jkiss.org)
+ * Copyright (C) 2011-2012 Eugene Fradkin (eugene.fradkin@gmail.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (version 2)
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package org.jkiss.dbeaver.ui.preferences;
+
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.eclipse.ui.IWorkbenchPropertyPage;
+import org.jkiss.code.Nullable;
+import org.jkiss.dbeaver.DBeaverPreferences;
+import org.jkiss.dbeaver.core.CoreMessages;
+import org.jkiss.dbeaver.core.DBeaverCore;
+import org.jkiss.dbeaver.model.preferences.DBPPreferenceStore;
+import org.jkiss.dbeaver.ui.UIUtils;
+import org.jkiss.dbeaver.utils.PrefUtils;
+
+/**
+ * PrefPageEntityEditor
+ */
+public class PrefPageEntityEditor extends AbstractPrefPage implements IWorkbenchPreferencePage, IWorkbenchPropertyPage
+{
+    public static final String PAGE_ID = "org.jkiss.dbeaver.preferences.main.entityEditor"; //$NON-NLS-1$
+
+    private Button keepEditorsOnRestart;
+    private Button refreshEditorOnOpen;
+    private Button editorFullName;
+
+    public PrefPageEntityEditor()
+    {
+        super();
+        setPreferenceStore(new PreferenceStoreDelegate(DBeaverCore.getGlobalPreferenceStore()));
+    }
+
+    @Override
+    public void init(IWorkbench workbench)
+    {
+
+    }
+
+    @Override
+    protected Control createContents(Composite parent)
+    {
+        Composite composite = UIUtils.createPlaceholder(parent, 1, 5);
+
+        // Editors settings
+        {
+            Group groupEditors = UIUtils.createControlGroup(composite, CoreMessages.pref_page_ui_general_group_editors, 1, GridData.VERTICAL_ALIGN_BEGINNING, 0);
+
+            keepEditorsOnRestart = UIUtils.createCheckbox(groupEditors, CoreMessages.pref_page_ui_general_keep_database_editors, false);
+            keepEditorsOnRestart.setToolTipText("Remembers open editors (e.g. table editors) and reopens them after DBeaver restart.");
+
+            refreshEditorOnOpen = UIUtils.createCheckbox(groupEditors, CoreMessages.pref_page_ui_general_refresh_editor_on_open, false);
+            refreshEditorOnOpen.setToolTipText("Refreshes object from database every time you open this object's editor.\nYou may need this option if your database structure changes frequently (e.g. by SQL scripts).");
+
+            editorFullName = UIUtils.createCheckbox(groupEditors, "Show full object names in editors", false);
+            editorFullName.setLayoutData(new GridData(GridData.BEGINNING, GridData.BEGINNING, true, false, 2, 1));
+
+        }
+
+        performDefaults();
+
+        return composite;
+    }
+
+    @Override
+    protected void performDefaults()
+    {
+        DBPPreferenceStore store = DBeaverCore.getGlobalPreferenceStore();
+
+        keepEditorsOnRestart.setSelection(store.getBoolean(DBeaverPreferences.UI_KEEP_DATABASE_EDITORS));
+        refreshEditorOnOpen.setSelection(store.getBoolean(DBeaverPreferences.NAVIGATOR_REFRESH_EDITORS_ON_OPEN));
+        editorFullName.setSelection(store.getBoolean(DBeaverPreferences.NAVIGATOR_EDITOR_FULL_NAME));
+    }
+
+    @Override
+    public boolean performOk()
+    {
+        DBPPreferenceStore store = DBeaverCore.getGlobalPreferenceStore();
+
+        store.setValue(DBeaverPreferences.UI_KEEP_DATABASE_EDITORS, keepEditorsOnRestart.getSelection());
+        store.setValue(DBeaverPreferences.NAVIGATOR_REFRESH_EDITORS_ON_OPEN, refreshEditorOnOpen.getSelection());
+        store.setValue(DBeaverPreferences.NAVIGATOR_EDITOR_FULL_NAME, editorFullName.getSelection());
+
+        PrefUtils.savePreferenceStore(store);
+
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public IAdaptable getElement()
+    {
+        return null;
+    }
+
+    @Override
+    public void setElement(IAdaptable element)
+    {
+    }
+
+}
