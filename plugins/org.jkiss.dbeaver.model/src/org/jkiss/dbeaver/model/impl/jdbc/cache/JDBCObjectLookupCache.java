3c792897bbd474c5ecab015b185c4f03e3ecc1f8
Serge Rider <serge@jkiss.org>
2017-01-23 00:09:08 +0300

    Cache logging fix

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/impl/jdbc/cache/JDBCObjectLookupCache.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/impl/jdbc/cache/JDBCObjectLookupCache.java
@@ -92,7 +92,11 @@ public abstract class JDBCObjectLookupCache<OWNER extends DBSObject, OBJECT exte
         if (dataSource == null) {
             throw new DBException("Not connected to database");
         }
-        try (JDBCSession session = DBUtils.openMetaSession(monitor, dataSource, "Reload object '" + object + "' from " + owner.getName())) {
+        try (JDBCSession session = DBUtils.openMetaSession(monitor, dataSource,
+            object == null ?
+                "Load object '" + objectName + "' from " + owner.getName() :
+                "Reload object '" + object + "' from " + owner.getName()))
+        {
             try (JDBCStatement dbStat = prepareLookupStatement(session, owner, object, objectName)) {
                 dbStat.setFetchSize(1);
                 dbStat.executeStatement();
