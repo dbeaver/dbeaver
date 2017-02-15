b070a7a82bf5a4aad959f8d13ca26195c70877cc
Serge Rider <serge@jkiss.org>
2017-02-16 01:07:44 +0300

    #1264 Pending transactions command
    Former-commit-id: baee408669ebc93dfae5d6d3afe5a4521118ad5d

---
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/controls/txn/PendingTransactionsDialog.java
@@ -0,0 +1,62 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2017 Serge Rider (serge@jkiss.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jkiss.dbeaver.ui.controls.txn;
+
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.IDialogSettings;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+import org.jkiss.dbeaver.ui.UIUtils;
+
+public class PendingTransactionsDialog extends Dialog {
+
+    private static final String DIALOG_ID = "DBeaver.PendingTransactionsDialog";//$NON-NLS-1$
+
+    public PendingTransactionsDialog(Shell parentShell)
+    {
+        super(parentShell);
+    }
+
+    @Override
+    protected boolean isResizable() {
+    	return true;
+    }
+
+    protected IDialogSettings getDialogBoundsSettings()
+    {
+        return UIUtils.getDialogSettings(DIALOG_ID);
+    }
+
+    @Override
+    protected Control createDialogArea(Composite parent)
+    {
+        getShell().setText("Pending transactions");
+
+        Composite composite = (Composite) super.createDialogArea(parent);
+
+        return parent;
+    }
+
+    @Override
+    protected void createButtonsForButtonBar(Composite parent)
+    {
+        createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, true);
+    }
+
+}
