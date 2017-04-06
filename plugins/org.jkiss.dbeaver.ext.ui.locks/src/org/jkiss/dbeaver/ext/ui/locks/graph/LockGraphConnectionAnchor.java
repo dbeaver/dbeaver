c8928dbd134275ac84f6739a6edc85c737df4f53
Andrey Hitrin <andrey.hitrin@redsys.ru>
2017-04-06 13:01:16 +0300

    Plugin org.jkiss.dbeaver.ext.ui.locks and pom.xml.

---
+++ plugins/org.jkiss.dbeaver.ext.ui.locks/src/org/jkiss/dbeaver/ext/ui/locks/graph/LockGraphConnectionAnchor.java
@@ -0,0 +1,40 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2017 Andrew Khitrin (ahitrin@gmail.com) 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jkiss.dbeaver.ext.ui.locks.graph;
+
+import org.eclipse.draw2d.AbstractConnectionAnchor;
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.geometry.Point;
+
+public class LockGraphConnectionAnchor extends AbstractConnectionAnchor {
+
+	public LockGraphConnectionAnchor(IFigure owner) {
+		super(owner);
+	}
+
+	public Point getLocation(Point reference) {		
+		Point point = getOwner().getBounds().getCenter();
+		getOwner().translateToAbsolute(point);
+		if (reference.x < point.x)
+			point = getOwner().getBounds().getTop();
+		else
+			point = getOwner().getBounds().getBottom();
+		getOwner().translateToAbsolute(point);
+		return point;
+	}
+
+}
