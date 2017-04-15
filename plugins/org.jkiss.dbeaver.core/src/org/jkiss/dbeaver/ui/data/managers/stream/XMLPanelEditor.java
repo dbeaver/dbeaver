8c812e770826d6b86d5bea280f5170ecd07684df
Serge Rider <serge@jkiss.org>
2017-04-16 01:41:52 +0300

    Panel text editors release fix
    Former-commit-id: da649c30338be8817066456ba71b91539d910701

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/data/managers/stream/XMLPanelEditor.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/data/managers/stream/XMLPanelEditor.java
@@ -18,6 +18,8 @@ package org.jkiss.dbeaver.ui.data.managers.stream;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.PartInitException;
@@ -26,6 +28,7 @@ import org.jkiss.dbeaver.DBException;
 import org.jkiss.dbeaver.model.DBPMessageType;
 import org.jkiss.dbeaver.model.data.DBDContent;
 import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
+import org.jkiss.dbeaver.ui.UIUtils;
 import org.jkiss.dbeaver.ui.data.IValueController;
 import org.jkiss.dbeaver.ui.editors.StringEditorInput;
 import org.jkiss.dbeaver.ui.editors.SubEditorSite;
@@ -56,8 +59,17 @@ public class XMLPanelEditor extends AbstractTextPanelEditor {
             return new StyledText(valueController.getEditPlaceholder(), SWT.NONE);
         }
         editor.createPartControl(valueController.getEditPlaceholder());
-        initEditorSettings(editor.getEditorControl());
-        return editor.getEditorControl();
+        // TODO: move to base class. Refactor resource release
+        StyledText editorControl = editor.getEditorControl();
+        assert editorControl != null;
+        initEditorSettings(editorControl);
+        editorControl.addDisposeListener(new DisposeListener() {
+            @Override
+            public void widgetDisposed(DisposeEvent e) {
+                editor.releaseEditorInput();
+            }
+        });
+        return editorControl;
     }
 
     @Override
