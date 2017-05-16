efba7a1f13a8c79a6e7e41fe77bb51d5b3a23420
Andrey Hitrin <andrey.hitrin@redsys.ru>
2017-05-16 10:59:00 +0300

    If  condition not need at all

--- plugins/org.jkiss.dbeaver.ext.ui.locks/src/org/jkiss/dbeaver/ext/ui/locks/manage/LockGraphManager.java
+++ plugins/org.jkiss.dbeaver.ext.ui.locks/src/org/jkiss/dbeaver/ext/ui/locks/manage/LockGraphManager.java
@@ -173,9 +173,9 @@ public abstract class LockGraphManager<LOCK_TYPE extends DBAServerLock<?>, ID_TY
 		}
 		
 		for(LOCK_TYPE root : roots) {
-			
-			if (root.waitThis().size() > 0)
+		
 			createGraph(root);
+			
 		}
 		
 	}
