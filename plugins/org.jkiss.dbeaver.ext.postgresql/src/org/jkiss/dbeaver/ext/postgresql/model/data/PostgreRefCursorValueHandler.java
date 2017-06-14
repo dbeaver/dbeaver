54256b13f89188ce65cf99ffd459318a2eb3de35
Serge Rider <serge@jkiss.org>
2017-06-15 01:10:05 +0300

    #1735 PG: ref cursors data type support
    Former-commit-id: 90908113fed28f35757d8c585b7884c27fa59b0a

---
+++ plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/model/data/PostgreRefCursorValueHandler.java
@@ -0,0 +1,70 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2017 Serge Rider (serge@jkiss.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jkiss.dbeaver.ext.postgresql.model.data;
+
+import org.jkiss.code.NotNull;
+import org.jkiss.dbeaver.DBException;
+import org.jkiss.dbeaver.Log;
+import org.jkiss.dbeaver.ext.postgresql.PostgreUtils;
+import org.jkiss.dbeaver.ext.postgresql.model.PostgreDataSource;
+import org.jkiss.dbeaver.ext.postgresql.model.PostgreDataType;
+import org.jkiss.dbeaver.ext.postgresql.model.PostgreDataTypeAttribute;
+import org.jkiss.dbeaver.model.data.DBDComposite;
+import org.jkiss.dbeaver.model.exec.DBCException;
+import org.jkiss.dbeaver.model.exec.DBCSession;
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCPreparedStatement;
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCResultSet;
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCSession;
+import org.jkiss.dbeaver.model.impl.jdbc.JDBCStructImpl;
+import org.jkiss.dbeaver.model.impl.jdbc.data.JDBCComposite;
+import org.jkiss.dbeaver.model.impl.jdbc.data.JDBCCompositeStatic;
+import org.jkiss.dbeaver.model.impl.jdbc.data.handlers.JDBCStructValueHandler;
+import org.jkiss.dbeaver.model.struct.DBSTypedObject;
+
+import java.sql.SQLException;
+import java.sql.Struct;
+import java.sql.Types;
+import java.util.Collection;
+import java.util.Iterator;
+
+/**
+ * PostgreArrayValueHandler
+ */
+public class PostgreRefCursorValueHandler extends JDBCStructValueHandler {
+    private static final Log log = Log.getLog(PostgreRefCursorValueHandler.class);
+    public static final PostgreRefCursorValueHandler INSTANCE = new PostgreRefCursorValueHandler();
+
+    @Override
+    protected Object fetchColumnValue(DBCSession session, JDBCResultSet resultSet, DBSTypedObject type, int index) throws DBCException, SQLException {
+        // Fetch as string
+        // Fetching cursor as objet will close it so it won;'t be possible to use cursor in consequent queries
+        return resultSet.getString(index);
+    }
+
+    @Override
+    protected void bindParameter(
+        JDBCSession session,
+        JDBCPreparedStatement statement,
+        DBSTypedObject paramType,
+        int paramIndex,
+        Object value)
+        throws DBCException, SQLException
+    {
+        throw new DBCException("Cursor value binding not supported");
+    }
+
+}
