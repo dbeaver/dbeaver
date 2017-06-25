b6c8dd3c9e09a6b0d9a2489939b6f7ffe0bb84c5
Serge Rider <serge@jkiss.org>
2017-06-26 00:41:17 +0300

    DS toolbar NPE fix
    Former-commit-id: 7e1a0f821037710d5b83b24436b8e8d4bd722067

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/perspective/DataSourceManagementToolbar.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/perspective/DataSourceManagementToolbar.java
@@ -100,7 +100,7 @@ public class DataSourceManagementToolbar implements DBPRegistryListener, DBPEven
 
     private final List<DBPDataSourceRegistry> handledRegistries = new ArrayList<>();
     private final List<DatabaseListReader> dbListReads = new ArrayList<>();
-    private IFile activeFile;
+    private volatile IFile activeFile;
 
     private static class DatabaseListReader extends DataSourceJob {
         private final List<DBNDatabaseNode> nodeList = new ArrayList<>();
@@ -831,7 +831,7 @@ public class DataSourceManagementToolbar implements DBPRegistryListener, DBPEven
                 @Override
                 public void run() {
                     final int selConnection = connectionCombo.getSelectionIndex();
-                    if (selConnection > 0) {
+                    if (selConnection > 0 && activeFile != null) {
                         DBPDataSourceContainer visibleContainer = connectionCombo.getItem(selConnection);
                         DBPDataSourceContainer newContainer = EditorUtils.getFileDataSource(activeFile);
                         if (newContainer != visibleContainer) {
