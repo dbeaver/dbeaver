9f091fdcedebd9375104aca1d1b06279ffedb64d
Serge Rider <serge@jkiss.org>
2017-08-02 01:09:57 +0300

    SQL script context model additions

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/sql/SQLScriptContext.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/sql/SQLScriptContext.java
@@ -17,7 +17,10 @@
 
 package org.jkiss.dbeaver.model.sql;
 
+import org.eclipse.ui.IEditorInput;
 import org.jkiss.code.NotNull;
+import org.jkiss.dbeaver.model.DBPDataSource;
+import org.jkiss.dbeaver.model.exec.DBCExecutionContext;
 
 import java.io.PrintWriter;
 import java.io.Writer;
@@ -34,10 +37,30 @@ public class SQLScriptContext {
 
     private final Map<String, Object> data = new HashMap<>();
 
+    @NotNull
+    private final DBCExecutionContext executionContext;
+    @NotNull
+    private final IEditorInput editorInput;
+    @NotNull
     private final PrintWriter outputWriter;
 
-    public SQLScriptContext(Writer outputWriter) {
+    public SQLScriptContext(
+            @NotNull DBCExecutionContext executionContext,
+            @NotNull IEditorInput editorInput,
+            @NotNull Writer outputWriter) {
+        this.executionContext = executionContext;
+        this.editorInput = editorInput;
         this.outputWriter = new PrintWriter(outputWriter);
+    }
+
+    @NotNull
+    public DBCExecutionContext getExecutionContext() {
+        return executionContext;
+    }
+
+    @NotNull
+    public IEditorInput getEditorInput() {
+        return editorInput;
     }
 
     @NotNull
