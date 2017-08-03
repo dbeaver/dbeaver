89377944fe7b83d972c356b682328937093020d0
Serge Rider <serge@jkiss.org>
2017-08-03 21:52:49 +0300

    #739 SQL editor: include command
    Former-commit-id: ff2bb0bb94a0361cd5e45464040eaf034a3abd76

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/runtime/sql/commands/SQLCommandInclude.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/runtime/sql/commands/SQLCommandInclude.java
@@ -16,15 +16,21 @@
  */
 package org.jkiss.dbeaver.runtime.sql.commands;
 
+import org.eclipse.ui.IURIEditorInput;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.jkiss.dbeaver.DBException;
 import org.jkiss.dbeaver.DBeaverPreferences;
 import org.jkiss.dbeaver.core.DBeaverCore;
 import org.jkiss.dbeaver.core.DBeaverUI;
+import org.jkiss.dbeaver.model.exec.DBCSession;
+import org.jkiss.dbeaver.model.exec.DBCStatistics;
 import org.jkiss.dbeaver.model.sql.SQLControlCommand;
+import org.jkiss.dbeaver.model.sql.SQLQuery;
+import org.jkiss.dbeaver.model.sql.SQLQueryResult;
 import org.jkiss.dbeaver.model.sql.SQLScriptContext;
 import org.jkiss.dbeaver.runtime.sql.SQLControlCommandHandler;
-import org.jkiss.dbeaver.ui.editors.EditorUtils;
+import org.jkiss.dbeaver.runtime.sql.SQLQueryListener;
+import org.jkiss.dbeaver.ui.editors.StringEditorInput;
 import org.jkiss.dbeaver.ui.editors.sql.SQLEditor;
 import org.jkiss.dbeaver.ui.editors.sql.handlers.OpenHandler;
 import org.jkiss.dbeaver.utils.GeneralUtils;
@@ -32,6 +38,7 @@ import org.jkiss.utils.CommonUtils;
 import org.jkiss.utils.IOUtils;
 
 import java.io.*;
+import java.net.URI;
 
 /**
  * Control command handler
@@ -49,9 +56,8 @@ public class SQLCommandInclude implements SQLControlCommandHandler {
             fileName = fileName.substring(1, fileName.length() - 1);
         }
 
-        //scriptContext.getOutputWriter().println(parameter);
-        File curFile = EditorUtils.getLocalFileFromInput(scriptContext.getEditorInput());
-        File incFile = new File(curFile.getParent(), fileName);
+        File curFile = scriptContext.getSourceFile();
+        File incFile = curFile == null ? new File(fileName) : new File(curFile.getParent(), fileName);
         if (!incFile.exists()) {
             incFile = new File(fileName);
         }
@@ -66,23 +72,70 @@ public class SQLCommandInclude implements SQLControlCommandHandler {
         } catch (IOException e) {
             throw new DBException("IO error reading file '" + fileName + "'", e);
         }
-        final String inputFileName = fileName;
+        final File finalIncFile = incFile;
         DBeaverUI.syncExec(new Runnable() {
             @Override
             public void run() {
-                IWorkbenchWindow workbenchWindow = DBeaverUI.getActiveWorkbenchWindow();
+                final IWorkbenchWindow workbenchWindow = DBeaverUI.getActiveWorkbenchWindow();
+                final IncludeEditorInput input = new IncludeEditorInput(finalIncFile, fileContents);
                 SQLEditor sqlEditor = OpenHandler.openSQLConsole(
                         workbenchWindow,
                         scriptContext.getExecutionContext().getDataSource().getContainer(),
-                        inputFileName,
-                        fileContents);
-                sqlEditor.processSQL(false, true);
-
-                //workbenchWindow.getActivePage().closeEditor(sqlEditor, false);
+                        input);
+                final SQLQueryListener scriptListener = new IncludeScriptListener(workbenchWindow, sqlEditor);
+                sqlEditor.processSQL(false, true, null, scriptListener);
             }
         });
 
         return true;
     }
 
+    private static class IncludeScriptListener implements SQLQueryListener {
+        private final IWorkbenchWindow workbenchWindow;
+        private final SQLEditor editor;
+        public IncludeScriptListener(IWorkbenchWindow workbenchWindow, SQLEditor editor) {
+            this.workbenchWindow = workbenchWindow;
+            this.editor = editor;
+        }
+
+        @Override
+        public void onStartScript() {
+
+        }
+
+        @Override
+        public void onStartQuery(DBCSession session, SQLQuery query) {
+
+        }
+
+        @Override
+        public void onEndQuery(DBCSession session, SQLQueryResult result) {
+
+        }
+
+        @Override
+        public void onEndScript(DBCStatistics statistics, boolean hasErrors) {
+            DBeaverUI.syncExec(new Runnable() {
+                @Override
+                public void run() {
+                    workbenchWindow.getActivePage().closeEditor(editor, false);
+                }
+            });
+        }
+    }
+
+    private static class IncludeEditorInput extends StringEditorInput implements IURIEditorInput {
+
+        private final File incFile;
+
+        IncludeEditorInput(File incFile, CharSequence value) {
+            super(incFile.getName(), value, true, GeneralUtils.DEFAULT_ENCODING);
+            this.incFile = incFile;
+        }
+
+        @Override
+        public URI getURI() {
+            return incFile.toURI();
+        }
+    }
 }
