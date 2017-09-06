7ffc584035efb37dcf13d6e11e9f78a0c0932979
serge-rider <serge@jkiss.org>
2017-09-07 00:13:30 +0300

    Test data generator
    Former-commit-id: cf6c0007c014d8bdb6ff986a4f221bfc9b8d3a13

---
+++ plugins/org.jkiss.dbeaver.test/src/org/jkiss/dbeaver/ext/test/tools/HugeTableGenerator.java
@@ -0,0 +1,42 @@
+package org.jkiss.dbeaver.ext.test.tools;
+
+import java.sql.*;
+import java.util.Properties;
+
+public class HugeTableGenerator {
+
+    public static void main(String[] args) throws SQLException {
+
+        final String url = "jdbc:postgresql://localhost/postgres";
+        final Properties props = new Properties();
+        props.setProperty("user", "");
+        props.setProperty("password", "");
+
+        try (Connection conn = DriverManager.getConnection(url, props)) {
+            conn.setAutoCommit(true);
+
+            try (PreparedStatement stmt = conn.prepareStatement(
+                        "CREATE TABLE BigTable (table_key int8, some_string varchar(64), create_time timestamp, primary key(table_key))"))
+            {
+                stmt.execute();
+            }
+            // 10kk records
+            conn.setAutoCommit(false);
+            try (PreparedStatement stmt = conn.prepareStatement(
+                    "INSERT INTO BigTable(table_key, some_string, create_time) values(?,?,?)")) {
+                for (int i = 0; i < 10000000; i++) {
+                    stmt.setInt(1, i);
+                    stmt.setString(2, "Row " + i);
+                    stmt.setTimestamp(3, new Timestamp(System.currentTimeMillis()));
+                    stmt.execute();
+                    if (i % 100000 == 0) {
+                        conn.commit();
+                        System.out.println(i + " records");
+                    }
+                }
+                conn.commit();
+            }
+        }
+    }
+
+}
