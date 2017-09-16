ccc308c006580dfd3a9aa7945bf69987501e6337
serge-rider <serge@jkiss.org>
2017-09-16 23:21:24 +0300

    #69 FireBird data type model

---
+++ plugins/org.jkiss.dbeaver.ext.firebird/src/org/jkiss/dbeaver/ext/firebird/model/FireBirdDataSource.java
@@ -0,0 +1,108 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2017 Serge Rider (serge@jkiss.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jkiss.dbeaver.ext.firebird.model;
+
+import org.jkiss.dbeaver.DBException;
+import org.jkiss.dbeaver.Log;
+import org.jkiss.dbeaver.ext.generic.GenericConstants;
+import org.jkiss.dbeaver.ext.generic.model.*;
+import org.jkiss.dbeaver.ext.generic.model.meta.GenericMetaModel;
+import org.jkiss.dbeaver.ext.generic.model.meta.GenericMetaObject;
+import org.jkiss.dbeaver.model.DBPDataSourceContainer;
+import org.jkiss.dbeaver.model.DBUtils;
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCDatabaseMetaData;
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCPreparedStatement;
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCResultSet;
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCSession;
+import org.jkiss.dbeaver.model.impl.jdbc.JDBCConstants;
+import org.jkiss.dbeaver.model.impl.jdbc.JDBCUtils;
+import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
+import org.jkiss.dbeaver.model.struct.DBSObjectFilter;
+import org.jkiss.utils.CommonUtils;
+import org.jkiss.utils.IntKeyMap;
+
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FireBirdDataSource extends GenericDataSource {
+
+    private static final Log log = Log.getLog(FireBirdDataSource.class);
+
+    private static class MetaFieldInfo {
+        int type;
+        String name;
+        String description;
+
+        MetaFieldInfo(int type, String name, String description) {
+            this.type = type;
+            this.name = name;
+            this.description = description;
+        }
+
+        @Override
+        public String toString() {
+            return name + ":" + type;
+        }
+    }
+
+    private Map<String, IntKeyMap<MetaFieldInfo>> metaFields = new HashMap<>();
+
+    public FireBirdDataSource(DBRProgressMonitor monitor, DBPDataSourceContainer container, GenericMetaModel metaModel)
+        throws DBException
+    {
+        super(monitor, container, metaModel, new GenericSQLDialect());
+    }
+
+    @Override
+    public void initialize(DBRProgressMonitor monitor) throws DBException {
+        // Read metadata
+        try (JDBCSession session = DBUtils.openMetaSession(monitor, this, "Read generic metadata")) {
+            // Read metadata
+            try (JDBCPreparedStatement dbStat = session.prepareStatement("SELECT * FROM RDB$TYPES")) {
+                monitor.subTask("Load FireBird types");
+                try (JDBCResultSet dbResult = dbStat.executeQuery()) {
+                    while (dbResult.next()) {
+                        if (monitor.isCanceled()) {
+                            break;
+                        }
+                        String fieldName = JDBCUtils.safeGetString(dbResult, "RDB$FIELD_NAME");
+                        int fieldType = JDBCUtils.safeGetInt(dbResult, "RDB$TYPE");
+                        String typeName = JDBCUtils.safeGetString(dbResult, "RDB$TYPE_NAME");
+                        String fieldDescription = JDBCUtils.safeGetString(dbResult, "RDB$SYSTEM_FLAG");
+                        IntKeyMap<MetaFieldInfo> metaFields = this.metaFields.get(fieldName);
+                        if (metaFields == null) {
+                            metaFields = new IntKeyMap<>();
+                            this.metaFields.put(fieldName, metaFields);
+                        }
+                        metaFields.put(fieldType, new MetaFieldInfo(fieldType, typeName, fieldDescription));
+                    }
+                }
+            }
+
+        } catch (SQLException ex) {
+            log.error("Error reading FB metadata", ex);
+        }
+
+
+        // Init
+        super.initialize(monitor);
+    }
+}
---
+++ plugins/org.jkiss.dbeaver.ext.firebird/src/org/jkiss/dbeaver/ext/firebird/model/FireBirdDataTypeCache.java
@@ -0,0 +1,107 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2017 Serge Rider (serge@jkiss.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jkiss.dbeaver.ext.firebird.model;
+
+import org.jkiss.dbeaver.DBException;
+import org.jkiss.dbeaver.Log;
+import org.jkiss.dbeaver.ext.generic.model.GenericDataSource;
+import org.jkiss.dbeaver.ext.generic.model.GenericStructContainer;
+import org.jkiss.dbeaver.model.DBUtils;
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCPreparedStatement;
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCResultSet;
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCSession;
+import org.jkiss.dbeaver.model.impl.jdbc.JDBCUtils;
+import org.jkiss.dbeaver.model.impl.jdbc.cache.JDBCBasicDataTypeCache;
+import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * SQLiteDataTypeCache
+ */
+public class FireBirdDataTypeCache extends JDBCBasicDataTypeCache<GenericStructContainer, FireBirdDataType>
+{
+    private static final Log log = Log.getLog(FireBirdDataTypeCache.class);
+
+    public FireBirdDataTypeCache(GenericStructContainer owner) {
+        super(owner);
+    }
+
+    @Override
+    protected synchronized void loadObjects(DBRProgressMonitor monitor, GenericStructContainer container) throws DBException {
+        FireBirdDataSource dataSource = (FireBirdDataSource) container.getDataSource();
+
+        if (dataSource == null) {
+            throw new DBException("Not connected to database");
+        }
+        // Load domain types
+        List<FireBirdDataType> tmpObjectList = new ArrayList<>();
+
+        for (FireBirdFieldType fieldType : FireBirdFieldType.values()) {
+            FireBirdDataType dataType = new FireBirdDataType(dataSource, fieldType);
+            tmpObjectList.add(dataType);
+        }
+
+        try {
+            try (JDBCSession session = DBUtils.openMetaSession(monitor, dataSource.getDataSource(), "Load FireBird domain types")) {
+                try (JDBCPreparedStatement dbStat = session.prepareStatement(
+                    "SELECT F.* FROM RDB$FIELDS F\n" +
+                    "WHERE f.RDB$FIELD_NAME NOT LIKE '%$%'"))
+                {
+                    monitor.subTask("Load FireBird domain types");
+                    try (JDBCResultSet dbResult = dbStat.executeQuery()) {
+                        while (dbResult.next()) {
+                            if (monitor.isCanceled()) {
+                                break;
+                            }
+                            String typeName = JDBCUtils.safeGetString(dbResult, "RDB$FIELD_NAME");
+                            int fieldLength = JDBCUtils.safeGetInt(dbResult, "RDB$FIELD_LENGTH");
+                            int fieldScale = JDBCUtils.safeGetInt(dbResult, "RDB$FIELD_SCALE");
+                            int fieldType = JDBCUtils.safeGetInt(dbResult, "RDB$FIELD_TYPE");
+                            int fieldSubType = JDBCUtils.safeGetInt(dbResult, "RDB$FIELD_SUB_TYPE");
+                            int charLength = JDBCUtils.safeGetInt(dbResult, "RDB$CHARACTER_LENGTH");
+                            int collationId = JDBCUtils.safeGetInt(dbResult, "RDB$COLLATION_ID");
+                            int charsetId = JDBCUtils.safeGetInt(dbResult, "RDB$CHARACTER_SET_ID");
+                            String typeDescription = JDBCUtils.safeGetString(dbResult, "RDB$DESCRIPTION");
+
+                            FireBirdFieldType fieldDT = FireBirdFieldType.getById(fieldType);
+                            if (fieldDT == null) {
+                                log.error("Field type '" + fieldType + "' not found");
+                                continue;
+                            }
+
+                            FireBirdDataType dataType = new FireBirdDataType(
+                                dataSource, fieldDT, fieldSubType, typeName, typeDescription, false, true, fieldLength, fieldScale, fieldScale);
+                            tmpObjectList.add(dataType);
+                        }
+                    }
+                }
+            } catch (SQLException ex) {
+                throw new DBException(ex, dataSource);
+            }
+        } catch (DBException e) {
+            if (!handleCacheReadError(e)) {
+                throw e;
+            }
+        }
+
+        mergeCache(tmpObjectList);
+    }
+
+}
