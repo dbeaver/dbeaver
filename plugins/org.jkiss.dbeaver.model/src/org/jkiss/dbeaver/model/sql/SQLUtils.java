521479276b605dcc9d8540544e77b751f5c845b7
serge-rider <serge@jkiss.org>
2017-09-18 22:37:38 +0300

    #2169 Identifiers split function fix
    Former-commit-id: ca11f3c3b4e1948c23210d0099af0ba4ed83012e

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/sql/SQLUtils.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/sql/SQLUtils.java
@@ -776,7 +776,7 @@ public final class SQLUtils {
                 String startQuote = quotePair[0];
                 String endQuote = quotePair[1];
                 if (!CommonUtils.isEmpty(startQuote) && !CommonUtils.isEmpty(endQuote) && name.startsWith(startQuote)) {
-                    int endPos = name.indexOf(endQuote);
+                    int endPos = name.indexOf(endQuote, startQuote.length());
                     if (endPos != -1) {
                         // Quoted part
                         nameList.add(name.substring(startQuote.length(), endPos));
