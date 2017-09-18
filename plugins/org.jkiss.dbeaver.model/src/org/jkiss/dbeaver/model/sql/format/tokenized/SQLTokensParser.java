8dae564bd5c9567778b16e2329bdd27f380c5b73
serge-rider <serge@jkiss.org>
2017-09-19 01:12:29 +0300

    #2174 SQL Server quoted identifiers format fix

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/sql/format/tokenized/SQLTokensParser.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/sql/format/tokenized/SQLTokensParser.java
@@ -95,6 +95,8 @@ class SQLTokensParser {
         case '!': // greater than operator
         case '~': // greater than operator
         case '`': // apos
+        case '[': // bracket open
+        case ']': // bracket close
             return true;
         default:
             return false;
@@ -214,21 +216,31 @@ class SQLTokensParser {
         } else {
             if (fChar == '\'' || isQuoteChar(fChar)) {
                 fPos++;
-                char quoteChar = fChar;
+                char endQuoteChar = fChar;
+                // Close quote char may differ
+                if (quoteStrings != null) {
+                    for (String[] quoteString : quoteStrings) {
+                        if (quoteString[0].charAt(0) == endQuoteChar) {
+                            endQuoteChar = quoteString[1].charAt(0);
+                            break;
+                        }
+                    }
+                }
+
                 StringBuilder s = new StringBuilder();
-                s.append(quoteChar);
+                s.append(fChar);
                 for (;;) {
                     fChar = fBefore.charAt(fPos);
                     s.append(fChar);
                     fPos++;
                     char fNextChar = fPos >= fBefore.length() - 1 ? 0 : fBefore.charAt(fPos);
-                    if (fChar == quoteChar && fNextChar == quoteChar) {
+                    if (fChar == endQuoteChar && fNextChar == endQuoteChar) {
                         // Escaped quote
                         s.append(fChar);
                         fPos++;
                         continue;
                     }
-                    if (fChar == quoteChar) {
+                    if (fChar == endQuoteChar) {
                         return new FormatterToken(TokenType.VALUE, s.toString(), start_pos);
                     }
                 }
