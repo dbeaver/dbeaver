4babb9f2ac2e47abfccfe1e20cf002ed690df6db
Alexander Fedorov <alexander.fedorov@jkiss.org>
2017-11-18 17:21:42 +0300

    #2543 implemented handling for multiple NIO Paths
    Former-commit-id: 7c883610ada047f214caa8f59f0310ee4884c08c

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/actions/navigator/NavigatorHandlerLinkFolder.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/actions/navigator/NavigatorHandlerLinkFolder.java
@@ -1,6 +1,7 @@
 /*
  * DBeaver - Universal Database Manager
  * Copyright (C) 2010-2017 Serge Rider (serge@jkiss.org)
+ * Copyright (C) 2017 Alexander Fedorov (alexander.fedorov@jkiss.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +17,10 @@
  */
 package org.jkiss.dbeaver.ui.actions.navigator;
 
-import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.resources.IFolder;
@@ -31,21 +35,27 @@ import org.eclipse.ui.handlers.HandlerUtil;
 public class NavigatorHandlerLinkFolder extends NavigatorHandlerCreateLink {
 
     @Override
-    protected String selectTarget(ExecutionEvent event)
+    protected List<Path> selectTarget(ExecutionEvent event)
     {
         Shell shell = HandlerUtil.getActiveShell(event);
-        DirectoryDialog dialog = new DirectoryDialog(shell, SWT.NONE);
+        DirectoryDialog dialog = new DirectoryDialog(shell, SWT.OPEN);
         String folder = dialog.open();
-        return folder;
+        if (folder == null) {
+            return Collections.emptyList();
+        }
+        Path folderPath = Paths.get(folder);
+        return Collections.singletonList(folderPath);
     }
 
     @Override
-    protected void createLink(IResource resource, String path, IProgressMonitor monitor) throws CoreException
+    protected void createLink(IResource resource, List<Path> paths, IProgressMonitor monitor) throws CoreException
     {
-        IFolder container = (IFolder)resource;
-        final File externalFolder = new File(path);
-        final IFolder linkedFolder = container.getFolder(externalFolder.getName());
-        linkedFolder.createLink(externalFolder.toURI(), IResource.NONE, monitor);
+        IFolder container = (IFolder) resource;
+        for (Path path : paths) {
+            String folderName = path.getFileName().toString();
+            final IFolder linkedFolder = container.getFolder(folderName);
+            linkedFolder.createLink(path.toUri(), IResource.NONE, monitor);
+        }
     }
 
 }
