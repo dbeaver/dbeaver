60b4518373844edf6adbdd4d1262897f4a33918f
serge-rider <serge@jkiss.org>
2017-11-21 23:48:41 +0300

    #2290 Vertica: projections model and md read
    Former-commit-id: 8f27bceaf854eb5f8f187a33f5f058033046c908

--- plugins/org.jkiss.dbeaver.ext.vertica/src/org/jkiss/dbeaver/ext/vertica/model/VerticaSchema.java
+++ plugins/org.jkiss.dbeaver.ext.vertica/src/org/jkiss/dbeaver/ext/vertica/model/VerticaSchema.java
@@ -16,14 +16,23 @@
  */
 package org.jkiss.dbeaver.ext.vertica.model;
 
+import org.jkiss.code.NotNull;
+import org.jkiss.code.Nullable;
 import org.jkiss.dbeaver.DBException;
 import org.jkiss.dbeaver.Log;
 import org.jkiss.dbeaver.ext.generic.model.GenericCatalog;
 import org.jkiss.dbeaver.ext.generic.model.GenericDataSource;
 import org.jkiss.dbeaver.ext.generic.model.GenericSchema;
+import org.jkiss.dbeaver.model.DBUtils;
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCPreparedStatement;
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCResultSet;
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCSession;
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCStatement;
+import org.jkiss.dbeaver.model.impl.jdbc.cache.JDBCStructLookupCache;
 import org.jkiss.dbeaver.model.meta.Association;
 import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
 
+import java.sql.SQLException;
 import java.util.Collection;
 
 /**
@@ -33,13 +42,78 @@ public class VerticaSchema extends GenericSchema
 {
     private static final Log log = Log.getLog(VerticaSchema.class);
 
+    ProjectionCache projectionCache = new ProjectionCache();
+
     public VerticaSchema(GenericDataSource dataSource, GenericCatalog catalog, String schemaName) {
         super(dataSource, catalog, schemaName);
     }
 
     @Association
     public Collection<VerticaProjection> getProjections(DBRProgressMonitor monitor) throws DBException {
-        return null;
+        return projectionCache.getAllObjects(monitor, this);
+    }
+
+    public class ProjectionCache extends JDBCStructLookupCache<VerticaSchema, VerticaProjection, VerticaProjectionColumn> {
+
+        ProjectionCache()
+        {
+            super("projection_name");
+            setListOrderComparator(DBUtils.nameComparator());
+        }
+
+        @NotNull
+        @Override
+        public JDBCStatement prepareLookupStatement(@NotNull JDBCSession session, @NotNull VerticaSchema schema, @Nullable VerticaProjection object, @Nullable String objectName) throws SQLException {
+            final JDBCPreparedStatement dbStat = session.prepareStatement(
+                "SELECT * FROM v_catalog.projections WHERE projections_schema=?" +
+                    (object == null && objectName == null ? "" : " AND projection_name=?")
+            );
+            dbStat.setString(1, schema.getName());
+            if (object != null || objectName != null) dbStat.setString(2, object != null ? object.getName() : objectName);
+            return dbStat;
+        }
+
+        @Override
+        protected VerticaProjection fetchObject(@NotNull JDBCSession session, @NotNull VerticaSchema owner, @NotNull JDBCResultSet dbResult)
+            throws SQLException, DBException
+        {
+            return new VerticaProjection(VerticaSchema.this, dbResult.getString("projection_name"));
+        }
+
+        @Override
+        protected JDBCStatement prepareChildrenStatement(@NotNull JDBCSession session, @NotNull VerticaSchema owner, @Nullable VerticaProjection forTable)
+            throws SQLException
+        {
+            StringBuilder sql = new StringBuilder();
+            sql.append("SELECT c.relname,a.*,pg_catalog.pg_get_expr(ad.adbin, ad.adrelid, true) as def_value,dsc.description" +
+                "\nFROM pg_catalog.pg_attribute a" +
+                "\nINNER JOIN pg_catalog.pg_class c ON (a.attrelid=c.oid)" +
+                "\nLEFT OUTER JOIN pg_catalog.pg_attrdef ad ON (a.attrelid=ad.adrelid AND a.attnum = ad.adnum)" +
+                "\nLEFT OUTER JOIN pg_catalog.pg_description dsc ON (c.oid=dsc.objoid AND a.attnum = dsc.objsubid)" +
+                "\nWHERE NOT a.attisdropped");
+            if (forTable != null) {
+                sql.append(" AND c.oid=?");
+            } else {
+                sql.append(" AND c.relnamespace=? AND c.relkind not in ('i','c')");
+            }
+            sql.append(" ORDER BY a.attnum");
+
+            JDBCPreparedStatement dbStat = session.prepareStatement(sql.toString());
+            if (forTable != null) {
+                dbStat.setString(1, forTable.getName());
+            } else {
+                dbStat.setString(1, VerticaSchema.this.getName());
+            }
+            return dbStat;
+        }
+
+        @Override
+        protected VerticaProjectionColumn fetchChild(@NotNull JDBCSession session, @NotNull VerticaSchema owner, @NotNull VerticaProjection table, @NotNull JDBCResultSet dbResult)
+            throws SQLException, DBException
+        {
+            return new VerticaProjectionColumn(table, true);
+        }
+
     }
 
 }
