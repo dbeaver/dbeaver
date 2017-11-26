f47af493e09fa13c78915e56f7553f86585b9131
Alexander Fedorov <alexander.fedorov@jkiss.org>
2017-11-26 23:21:28 +0300

    #2556 add IPgSqlDebugController
    Former-commit-id: 8f9743a3a09126abf144466ab06cc6ccf796ae10

---
+++ plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/postgresql/internal/debug/core/PgSqlDebugController.java
@@ -0,0 +1,28 @@
+package org.jkiss.dbeaver.postgresql.internal.debug.core;
+
+import org.jkiss.dbeaver.postgresql.debug.core.IPgSqlDebugController;
+
+public class PgSqlDebugController implements IPgSqlDebugController {
+
+    @Override
+    public void resume()
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void suspend()
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void terminate()
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
--- plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/postgresql/internal/debug/core/PgSqlLaunchDelegate.java
+++ plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/postgresql/internal/debug/core/PgSqlLaunchDelegate.java
@@ -5,6 +5,7 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
+import org.jkiss.dbeaver.postgresql.debug.core.IPgSqlDebugController;
 import org.jkiss.dbeaver.postgresql.internal.debug.core.model.PgSqlDebugTarget;
 
 public class PgSqlLaunchDelegate implements ILaunchConfigurationDelegate {
@@ -14,8 +15,9 @@ public class PgSqlLaunchDelegate implements ILaunchConfigurationDelegate {
             throws CoreException
     {
         //FIXME:AF: extract attributes from configuration and create controller here
-        PgSqlDebugTarget target = new PgSqlDebugTarget(launch);
-
+        IPgSqlDebugController controller = new PgSqlDebugController();
+        PgSqlDebugTarget target = new PgSqlDebugTarget(launch, controller);
+        launch.addDebugTarget(target);
     }
 
 }
