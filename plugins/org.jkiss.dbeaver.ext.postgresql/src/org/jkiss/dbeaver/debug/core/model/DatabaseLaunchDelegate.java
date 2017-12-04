b2dcdccad8722df97b44364e6c751f8f393106d9
Alexander Fedorov <alexander.fedorov@jkiss.org>
2017-12-04 10:34:03 +0300

    #2556 extract DatabaseLaunchDelegate
    Former-commit-id: 1911ede0d0b1149cb205921ed2c3279da9a59893

---
+++ plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/debug/core/model/DatabaseLaunchDelegate.java
@@ -0,0 +1,41 @@
+package org.jkiss.dbeaver.debug.core.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.model.LaunchConfigurationDelegate;
+import org.jkiss.dbeaver.debug.core.DebugCore;
+
+public abstract class DatabaseLaunchDelegate extends LaunchConfigurationDelegate {
+
+    @Override
+    public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor)
+            throws CoreException
+    {
+        String datasourceId = DebugCore.extractDatasource(configuration);
+        String databaseName = DebugCore.extractDatabase(configuration);
+        Map<String, Object> attributes = extractAttributes(configuration);
+        IDatabaseDebugController controller = createController(datasourceId, databaseName, attributes);
+        DatabaseProcess process = createProcess(launch, configuration.getName());
+        DatabaseDebugTarget target = createDebugTarget(launch, controller, process);
+        launch.addDebugTarget(target);
+    }
+
+    protected Map<String, Object> extractAttributes(ILaunchConfiguration configuration)
+    {
+        Map<String, Object> attributes = new HashMap<>();
+        return attributes;
+    }
+
+    protected abstract DatabaseDebugController createController(String datasourceId, String databaseName, Map<String, Object> attributes);
+
+    protected abstract DatabaseProcess createProcess(ILaunch launch, String name);
+
+    protected abstract DatabaseDebugTarget createDebugTarget(ILaunch launch, IDatabaseDebugController controller,
+            DatabaseProcess process);
+
+}
