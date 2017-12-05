424c0c66a528f4ec866b135e8f939df0f4d629a7
Alexander Fedorov <alexander.fedorov@jkiss.org>
2017-12-05 08:39:02 +0300

    #2556 extract DatabaseDebugModelPresentation
    Former-commit-id: ca0e450f792d6e8bc085d1ce2d8f5390ab0c6e8c

--- plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/postgresql/internal/debug/ui/PgSqlDebugModelPresentation.java
+++ plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/postgresql/internal/debug/ui/PgSqlDebugModelPresentation.java
@@ -1,13 +1,11 @@
 package org.jkiss.dbeaver.postgresql.internal.debug.ui;
 
 import org.eclipse.debug.core.model.IValue;
-import org.eclipse.debug.ui.IDebugModelPresentation;
 import org.eclipse.debug.ui.IValueDetailListener;
-import org.eclipse.jface.viewers.LabelProvider;
-import org.eclipse.swt.graphics.Image;
 import org.eclipse.ui.IEditorInput;
+import org.jkiss.dbeaver.debug.ui.DatabaseDebugModelPresentation;
 
-public class PgSqlDebugModelPresentation extends LabelProvider implements IDebugModelPresentation {
+public class PgSqlDebugModelPresentation extends DatabaseDebugModelPresentation {
 
     @Override
     public IEditorInput getEditorInput(Object element)
@@ -18,27 +16,6 @@ public class PgSqlDebugModelPresentation extends LabelProvider implements IDebug
 
     @Override
     public String getEditorId(IEditorInput input, Object element)
-    {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public void setAttribute(String attribute, Object value)
-    {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public Image getImage(Object element)
-    {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public String getText(Object element)
     {
         // TODO Auto-generated method stub
         return null;
