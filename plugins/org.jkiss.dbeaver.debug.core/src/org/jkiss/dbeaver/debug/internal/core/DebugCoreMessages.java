f1d55b0732022970746090dae5e12e9b69f05a9a
Alexander Fedorov <alexander.fedorov@jkiss.org>
2017-12-06 09:38:17 +0300

    #2556 i18n for DatabaseDebugController
    Former-commit-id: ae7f1f1ad2d7b32063288bb1909a923167513b5b

--- plugins/org.jkiss.dbeaver.debug.core/src/org/jkiss/dbeaver/debug/internal/core/DebugCoreMessages.java
+++ plugins/org.jkiss.dbeaver.debug.core/src/org/jkiss/dbeaver/debug/internal/core/DebugCoreMessages.java
@@ -4,6 +4,10 @@ import org.eclipse.osgi.util.NLS;
 
 public class DebugCoreMessages extends NLS {
     private static final String BUNDLE_NAME = "org.jkiss.dbeaver.debug.internal.core.DebugCoreMessages"; //$NON-NLS-1$
+    public static String DatabaseDebugController_debug_context_purpose;
+    public static String DatabaseDebugController_debug_session_name;
+    public static String DatabaseDebugController_e_connecting_datasource;
+    public static String DatabaseDebugController_e_opening_debug_context;
     public static String DebugCore_e_read_attribute_generic;
     public static String DebugCore_e_read_attribute_null;
     public static String DebugCore_e_unable_to_retrieve_modes;
