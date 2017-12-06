727bd1022068d3ca31d0330ffaa638df8850ef6e
Alexander Fedorov <alexander.fedorov@jkiss.org>
2017-12-06 13:39:54 +0300

    #2556 add PosgreSQL Debug UI facade

---
+++ bundles/org.jkiss.dbeaver.postgresql.debug.ui/src/org/jkiss/dbeaver/postgresql/debug/ui/PostgreSqlDebugUi.java
@@ -0,0 +1,7 @@
+package org.jkiss.dbeaver.postgresql.debug.ui;
+
+public class PostgreSqlDebugUi {
+
+    public static final String BUNDLE_SYMBOLIC_NAME = "org.jkiss.dbeaver.postgresql.debug.ui"; //$NON-NLS-1$
+
+}
