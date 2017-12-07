6d6634248aa20d3e245ad4fdb34ec23d486af2ef
Alexander Fedorov <alexander.fedorov@jkiss.org>
2017-12-07 11:31:53 +0300

    #2556 add IToggleBreakpointTarget implementation

---
+++ bundles/org.jkiss.dbeaver.debug.ui/src/org/jkiss/dbeaver/debug/ui/actions/ToggleSqlBreakpointTarget.java
@@ -0,0 +1,72 @@
+package org.jkiss.dbeaver.debug.ui.actions;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.debug.ui.actions.IToggleBreakpointsTargetExtension2;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.ui.IWorkbenchPart;
+
+public class ToggleSqlBreakpointTarget implements IToggleBreakpointsTargetExtension2 {
+
+    @Override
+    public void toggleBreakpoints(IWorkbenchPart part, ISelection selection) throws CoreException
+    {
+        toggleLineBreakpoints(part, selection);
+    }
+
+    @Override
+    public boolean canToggleBreakpoints(IWorkbenchPart part, ISelection selection)
+    {
+        return canToggleLineBreakpoints(part, selection);
+    }
+
+    @Override
+    public void toggleLineBreakpoints(IWorkbenchPart part, ISelection selection) throws CoreException
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean canToggleLineBreakpoints(IWorkbenchPart part, ISelection selection)
+    {
+        return true;
+    }
+
+    @Override
+    public void toggleMethodBreakpoints(IWorkbenchPart part, ISelection selection) throws CoreException
+    {
+        //nothing by default
+    }
+
+    @Override
+    public boolean canToggleMethodBreakpoints(IWorkbenchPart part, ISelection selection)
+    {
+        return false;
+    }
+
+    @Override
+    public void toggleWatchpoints(IWorkbenchPart part, ISelection selection) throws CoreException
+    {
+        //nothing by default
+    }
+
+    @Override
+    public boolean canToggleWatchpoints(IWorkbenchPart part, ISelection selection)
+    {
+        return false;
+    }
+
+    @Override
+    public void toggleBreakpointsWithEvent(IWorkbenchPart part, ISelection selection, Event event) throws CoreException
+    {
+        //nothing by default
+    }
+
+    @Override
+    public boolean canToggleBreakpointsWithEvent(IWorkbenchPart part, ISelection selection, Event event)
+    {
+        return false;
+    }
+
+}
