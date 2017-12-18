b5109d32e9e996ef5e928ab1989d0d1ce8cb4e11
Alexander Fedorov <alexander.fedorov@jkiss.org>
2017-12-18 11:14:21 +0300

    #2556 repackage ui to ext.postgresql

--- plugins/org.jkiss.dbeaver.ext.postgresql.debug.ui/src/org/jkiss/dbeaver/ext/postgresql/debug/ui/PostgreSqlDebugUi.java
+++ plugins/org.jkiss.dbeaver.ext.postgresql.debug.ui/src/org/jkiss/dbeaver/ext/postgresql/debug/ui/PostgreSqlDebugUi.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jkiss.dbeaver.postgresql.debug.ui;
+package org.jkiss.dbeaver.ext.postgresql.debug.ui;
 
 public class PostgreSqlDebugUi {
 
