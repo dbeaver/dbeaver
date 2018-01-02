7f3a6134a7942c8914e366fc31705a513e2df85b
serge-rider <serge@jkiss.org>
2018-01-02 12:21:39 +0300

    #2633 Sybase & SQL Server schema read change
    Former-commit-id: 57b51449dd09d9b2f485bc2c7a24a8b5567ce84d

--- plugins/org.jkiss.dbeaver.ext.mssql/src/org/jkiss/dbeaver/ext/mssql/model/SQLServerDatabase.java
+++ plugins/org.jkiss.dbeaver.ext.mssql/src/org/jkiss/dbeaver/ext/mssql/model/SQLServerDatabase.java
@@ -16,14 +16,8 @@
  */
 package org.jkiss.dbeaver.ext.mssql.model;
 
-import org.jkiss.dbeaver.DBException;
 import org.jkiss.dbeaver.ext.generic.model.GenericCatalog;
 import org.jkiss.dbeaver.ext.generic.model.GenericDataSource;
-import org.jkiss.dbeaver.ext.generic.model.GenericSchema;
-import org.jkiss.dbeaver.ext.generic.model.GenericTable;
-import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
-
-import java.util.Collection;
 
 /**
 * SQL Server database
@@ -33,14 +27,25 @@ public class SQLServerDatabase extends GenericCatalog {
     SQLServerDatabase(GenericDataSource dataSource, String catalogName) {
         super(dataSource, catalogName);
     }
-
+/*
     @Override
     public Collection<GenericSchema> getSchemas(DBRProgressMonitor monitor) throws DBException {
-        return super.getSchemas(monitor);
+        // Do not read schemas
+        return null;
     }
+
 
     @Override
     public Collection<GenericTable> getTables(DBRProgressMonitor monitor) throws DBException {
-        return super.getTables(monitor);
+        // Read all tables from all schemas
+        List<GenericTable> allTables = new ArrayList<>();
+        for (GenericSchema schema : super.getSchemas(monitor)) {
+            Collection<GenericTable> tables = schema.getTables(monitor);
+            if (!CommonUtils.isEmpty(tables)) {
+                allTables.addAll(tables);
+            }
+        }
+        return allTables;
     }
+*/
 }
