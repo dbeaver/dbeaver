f0c660f1acbfac6a96147dd6f80e36d9da18d0b9
Alexander Fedorov <alexander.fedorov@jkiss.org>
2018-01-08 10:30:24 +0300

    #2556 override context menu id for editor and ruler

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/editors/sql/SQLEditorContributions.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/editors/sql/SQLEditorContributions.java
@@ -2,6 +2,9 @@ package org.jkiss.dbeaver.ui.editors.sql;
 
 public class SQLEditorContributions {
 
-    public static final String SQL_EDITOR_CONTEXT = "org.jkiss.dbeaver.ui.editors.sql";
+    public static final String SQL_EDITOR_CONTEXT = "org.jkiss.dbeaver.ui.editors.sql"; //$NON-NLS-1$
+
+    public static final String SQL_EDITOR_CONTEXT_MENU_ID = "#SQLEditorContext"; //$NON-NLS-1$
+    public static final String SQL_RULER_CONTEXT_MENU_ID = "#SQLRulerContext"; //$NON-NLS-1$
 
 }
