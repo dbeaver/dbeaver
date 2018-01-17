e38d61e8f06265a85e5629e7b495929954293063
serge-rider <serge@jkiss.org>
2018-01-17 16:45:53 +0300

    #2619 Filter drop-down redesign

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/controls/resultset/valuefilter/FilterValueEditMenu.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/controls/resultset/valuefilter/FilterValueEditMenu.java
@@ -16,162 +16,143 @@
  */
 package org.jkiss.dbeaver.ui.controls.resultset.valuefilter;
 
-import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.jface.layout.TableColumnLayout;
 import org.eclipse.jface.viewers.ColumnLabelProvider;
 import org.eclipse.jface.viewers.ColumnWeightData;
-import org.eclipse.jface.viewers.ISelectionChangedListener;
-import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.FocusAdapter;
 import org.eclipse.swt.events.FocusEvent;
-import org.eclipse.swt.events.FocusListener;
-import org.eclipse.swt.events.ModifyEvent;
-import org.eclipse.swt.events.ModifyListener;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
-import org.eclipse.swt.events.TraverseEvent;
-import org.eclipse.swt.events.TraverseListener;
-import org.eclipse.swt.events.VerifyEvent;
-import org.eclipse.swt.events.VerifyListener;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.Listener;
-import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.*;
 import org.jkiss.code.NotNull;
-import org.jkiss.code.Nullable;
+import org.jkiss.dbeaver.core.DBeaverUI;
 import org.jkiss.dbeaver.model.DBPDataKind;
 import org.jkiss.dbeaver.model.data.DBDAttributeBinding;
 import org.jkiss.dbeaver.model.data.DBDDisplayFormat;
 import org.jkiss.dbeaver.model.data.DBDLabelValuePair;
 import org.jkiss.dbeaver.model.exec.DBCLogicalOperator;
+import org.jkiss.dbeaver.ui.UIUtils;
 import org.jkiss.dbeaver.ui.controls.resultset.ResultSetRow;
 import org.jkiss.dbeaver.ui.controls.resultset.ResultSetViewer;
 
-public class FilterValueEditMenu extends Composite{
+public class FilterValueEditMenu extends Dialog {
 
-	private Shell shell;
+    private static final String DIALOG_ID = "DBeaver.FilterValueEditMenu";//$NON-NLS-1$
+
     private Object value;
-    private Display display;
-	private GenericFilterValueEdit filter;
-    
-	public FilterValueEditMenu(Composite parent, int style, @Nullable Control trigger,@NotNull ResultSetViewer viewer, @NotNull DBDAttributeBinding attr, @NotNull ResultSetRow[] rows) {
-		super(parent, style);
-		filter = new GenericFilterValueEdit(viewer, attr, rows, DBCLogicalOperator.IN);
-		this.display = Display.getDefault();
-		init(trigger);
-	}
+    private GenericFilterValueEdit filter;
+    private Point location;
 
-private void init(Control trigger) {
-		
-		shell = new Shell(display, SWT.BORDER);
-		shell.setLayout(new GridLayout());
-		
-//		shell.addTraverseListener(new TraverseListener() {
-//			
-//			@Override
-//			public void keyTraversed(TraverseEvent e) {
-//				if (e.detail == SWT.TRAVERSE_ESCAPE)
-//		    		 shell.setVisible(false);
-//		    		 shell.close(); 	
-//			}
-//		});
-		shell.addListener(SWT.Deactivate, e-> {
-	       	 if (shell != null && !shell.isDisposed()) {
-	    		 shell.setVisible(false);	    
-	    		 shell.close();    		 
-	    	 }
-		   	});
-		
-	   	createMultiValueSelector(shell);
-	   	shell.pack();
-		
-		
-		if(trigger != null) {
-			trigger.addListener(SWT.MouseDown, e -> {
-				setVisible(true);
-		    });
-		}
-	    
-	}
+    public FilterValueEditMenu(Shell parentShell, @NotNull ResultSetViewer viewer, @NotNull DBDAttributeBinding attr, @NotNull ResultSetRow[] rows) {
+        super(parentShell);
+        setShellStyle(SWT.SHELL_TRIM);
+        filter = new GenericFilterValueEdit(viewer, attr, rows, DBCLogicalOperator.IN);
+    }
+
+    @Override
+    protected IDialogSettings getDialogBoundsSettings()
+    {
+        return UIUtils.getDialogSettings(DIALOG_ID);
+    }
+
+    @Override
+    protected boolean isResizable()
+    {
+        return true;
+    }
+
+    @Override
+    protected Point getInitialLocation(Point initialSize) {
+        if (location != null) {
+            return location;
+        }
+        return super.getInitialLocation(initialSize);
+    }
+
+    @Override
+    protected Control createDialogArea(Composite parent)
+    {
+        getShell().setText("Filter by column value");
+
+        Composite group = (Composite) super.createDialogArea(parent);
+        UIUtils.createControlLabel(group, "Choose value(s) to filter by");
 
 
-	public void setLocation(Point location) {
-		shell.setLocation(location);
-	}
-	
-	public int open() {		
-		shell.open();		
-		while (!shell.isDisposed()) {
-		    if (!display.readAndDispatch()) {
-		        display.sleep();
-		    }
-		}
-		return isAnythingSelected() ?  IDialogConstants.OK_ID : IDialogConstants.CANCEL_ID;
-	}
-	
-
-	
-	
-	private boolean isAnythingSelected() {
-		return value != null;
-	}
-
-
-	private void createMultiValueSelector(Composite parent) {
-		Composite tableComposite = new Composite(parent, SWT.NONE);
-		GridData gd = new GridData(GridData.FILL_BOTH);
+        Composite tableComposite = new Composite(group, SWT.NONE);
+        GridData gd = new GridData(GridData.FILL_BOTH);
         gd.widthHint = 400;
         gd.heightHint = 300;
         tableComposite.setLayoutData(gd);
-		
-        filter.setupTable(tableComposite, SWT.BORDER | SWT.SINGLE | SWT.NO_SCROLL | SWT.V_SCROLL, false, false, SWT.NONE);
-        filter.table.getTable().setBackground( Display.getCurrent().getSystemColor(SWT.COLOR_WIDGET_BACKGROUND));
+        tableComposite.setLayout(new GridLayout(1, false));
+
+        filter.setupTable(tableComposite, SWT.BORDER | SWT.SINGLE | SWT.NO_SCROLL | SWT.V_SCROLL, false, false, new GridData(GridData.FILL_BOTH));
+        Table table = filter.table.getTable();
 
         TableViewerColumn resultsetColumn = new TableViewerColumn(filter.table, SWT.NONE);
         resultsetColumn.setLabelProvider(new ColumnLabelProvider() {
             @Override
             public String getText(Object element) {
-                return filter.attr.getValueHandler().getValueDisplayString(filter.attr, ((DBDLabelValuePair)element).getValue(), DBDDisplayFormat.UI);
-            }});
-        
+                return filter.attr.getValueHandler().getValueDisplayString(filter.attr, ((DBDLabelValuePair) element).getValue(), DBDDisplayFormat.UI);
+            }
+        });
+
         resultsetColumn.getColumn().setResizable(false);
         TableColumnLayout tableLayout = new TableColumnLayout();
         tableComposite.setLayout(tableLayout);
-        
+
         // Resize the column to fit the contents
- 		resultsetColumn.getColumn().pack(); 	   
- 	    int resultsetWidth = resultsetColumn.getColumn().getWidth(); 	    
- 	    // Set  column to fill 100%, but with its packed width as minimum
- 	    tableLayout.setColumnData(resultsetColumn.getColumn(), new ColumnWeightData(100, resultsetWidth));	
-        
-  
- 	    filter.table.addSelectionChangedListener( new ISelectionChangedListener() {
-			
-			@Override
-			public void selectionChanged(SelectionChangedEvent event) {
-				value = ((DBDLabelValuePair) event.getStructuredSelection().getFirstElement());
-				shell.setVisible(false);
-				
-			}
-		});
- 	    
- 	    
-           
-        if ( filter.attr.getDataKind() == DBPDataKind.STRING) {
-        	filter.addFilterTextbox(parent);
+        resultsetColumn.getColumn().pack();
+        int resultsetWidth = resultsetColumn.getColumn().getWidth();
+        // Set  column to fill 100%, but with its packed width as minimum
+        tableLayout.setColumnData(resultsetColumn.getColumn(), new ColumnWeightData(100, resultsetWidth));
+
+        FocusAdapter focusListener = new FocusAdapter() {
+            @Override
+            public void focusLost(FocusEvent e) {
+                DBeaverUI.asyncExec(() -> {
+                    if (!UIUtils.isParent(getShell(), getShell().getDisplay().getFocusControl())) {
+                        cancelPressed();
+                    }
+                });
+            }
+        };
+        table.addFocusListener(focusListener);
+
+        filter.table.addSelectionChangedListener(event -> {
+            value = event.getStructuredSelection().getFirstElement();
+            okPressed();
+        });
+        filter.table.addDoubleClickListener(event -> {
+            value = filter.table.getStructuredSelection().getFirstElement();
+            okPressed();
+        });
+
+
+        if (filter.attr.getDataKind() == DBPDataKind.STRING) {
+            filter.addFilterTextbox(parent);
         }
         filter.filterPattern = null;
         filter.loadValues();
+        table.setFocus();
+
+        return tableComposite;
     }
-	
-	public Object getValue() {	          
+
+    @Override
+    protected Control createButtonBar(Composite parent) {
+        return UIUtils.createPlaceholder(parent, 1);
+    }
+
+    public void setLocation(Point location) {
+        this.location = location;
+    }
+
+    public Object getValue() {
         return ((DBDLabelValuePair) value).getValue();
     }
 }
