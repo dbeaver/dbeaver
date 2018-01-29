2799074eb1a15b7a7423424ea94f8e99c3e4db8c
Evgeny Fradkin <Eugene.Fradkin@gmail.com>
2018-01-29 12:04:49 +0300

    #1599 equals and hashCode methods are added to the Pair class
    Former-commit-id: 562d31c582937b926876e98042e64bccc1a85ee2

--- bundles/org.jkiss.utils/src/org/jkiss/utils/Pair.java
+++ bundles/org.jkiss.utils/src/org/jkiss/utils/Pair.java
@@ -1,5 +1,7 @@
 package org.jkiss.utils;
 
+import java.util.Objects;
+
 /**
  * Pair
  */
@@ -34,5 +36,31 @@ public class Pair<T1, T2> {
     @Override
     public String toString() {
         return first + "=" + second;
+    }
+
+    /**
+     * Checks the two objects for equality by delegating to their respective
+     * {@link Object#equals(Object)} methods.
+     *
+     * @param o the {@link Pair} to which this one is to be checked for equality
+     * @return true if the underlying objects of the Pair are both considered
+     * equal
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Pair)) {
+            return false;
+        }
+        Pair<?, ?> p = (Pair<?, ?>) o;
+        return Objects.equals(p.first, first) && Objects.equals(p.second, second);
+    }
+
+    /**
+     * Compute a hash code using the hash codes of the underlying objects
+     * @return a hashcode of the Pair
+     */
+    @Override
+    public int hashCode() {
+        return (first == null ? 0 : first.hashCode()) ^ (second == null ? 0 : second.hashCode());
     }
 }
