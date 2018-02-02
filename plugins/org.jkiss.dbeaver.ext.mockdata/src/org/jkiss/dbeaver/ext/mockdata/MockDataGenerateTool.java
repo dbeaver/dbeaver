6f5aa335a06da0bce47451f746710febfb83f93f
Evgeny Fradkin <Eugene.Fradkin@gmail.com>
2018-02-02 23:55:39 +0300

    #1599 Mock Data. Properties validation. Correct progress monitor.
    Former-commit-id: 854daa7317ff26c72e2bf875ec447906ed55e71f

--- plugins/org.jkiss.dbeaver.ext.mockdata/src/org/jkiss/dbeaver/ext/mockdata/MockDataGenerateTool.java
+++ plugins/org.jkiss.dbeaver.ext.mockdata/src/org/jkiss/dbeaver/ext/mockdata/MockDataGenerateTool.java
@@ -52,7 +52,7 @@ public class MockDataGenerateTool implements IExternalTool {
 
             @Override
             protected void finishPressed() {
-                if (doRemoveDataConfirmation()) {
+                if (doValidationConfirmation((MockDataWizardPageSettings) getCurrentPage())) {
                     return;
                 }
                 super.finishPressed();
@@ -62,14 +62,18 @@ public class MockDataGenerateTool implements IExternalTool {
             protected void nextPressed() {
                 IWizardPage currentPage = getCurrentPage();
                 if (currentPage instanceof MockDataWizardPageSettings) {
-                    if (doRemoveDataConfirmation()) {
+                    if (doValidationConfirmation((MockDataWizardPageSettings) currentPage)) {
                         return;
                     }
                 }
                 super.nextPressed();
             }
 
-            private boolean doRemoveDataConfirmation() {
+            private boolean doValidationConfirmation(MockDataWizardPageSettings currentPage) {
+                if (!currentPage.validateProperties()) {
+                    this.setErrorMessage("All numeric properties should be positive.");
+                    return true;
+                }
                 if (mockDataSettings.isRemoveOldData() && !removeOldDataConfirmed) {
                     if (UIUtils.confirmAction(getShell(), MockDataMessages.tools_mockdata_wizard_title, MockDataMessages.tools_mockdata_confirm_delete_old_data_message)) {
                         removeOldDataConfirmed = true;
