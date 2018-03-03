5e5d4675383f05b71431b1219b23c96a05a51c86
Alexander Fedorov <alexander.fedorov@jkiss.org>
2018-03-03 12:51:07 +0300

    #2556 #copyright
    Former-commit-id: 2c37f135c1569107b1ce7f5b96b2cdaefae7367e

--- plugins/org.jkiss.dbeaver.debug.core/src/org/jkiss/dbeaver/debug/sourcelookup/DatasourceSourceContainer.java
+++ plugins/org.jkiss.dbeaver.debug.core/src/org/jkiss/dbeaver/debug/sourcelookup/DatasourceSourceContainer.java
@@ -15,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.jkiss.dbeaver.debug.sourcelookup;
 
 import org.eclipse.core.resources.IProject;
--- plugins/org.jkiss.dbeaver.debug.ui/src/org/jkiss/dbeaver/debug/ui/LaunchContributionFactory.java
+++ plugins/org.jkiss.dbeaver.debug.ui/src/org/jkiss/dbeaver/debug/ui/LaunchContributionFactory.java
@@ -1,7 +1,7 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2017 Serge Rider (serge@jkiss.org)
- * Copyright (C) 2017 Alexander Fedorov (alexander.fedorov@jkiss.org)
+ * Copyright (C) 2010-2018 Serge Rider (serge@jkiss.org)
+ * Copyright (C) 2017-2018 Alexander Fedorov (alexander.fedorov@jkiss.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,6 +15,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+
 package org.jkiss.dbeaver.debug.ui;
 
 import org.eclipse.jface.action.MenuManager;
