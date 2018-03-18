72ceb4b599f7755ac17ca8cce3e493a8de971eaf
serge-rider <serge@jkiss.org>
2018-03-18 17:50:57 +0300

    #2202 SSH implementations moved to separate plugins

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/registry/network/NetworkHandlerRegistry.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/registry/network/NetworkHandlerRegistry.java
@@ -22,6 +22,7 @@ import org.eclipse.core.runtime.Platform;
 import org.jkiss.dbeaver.model.DBPDataSourceContainer;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class NetworkHandlerRegistry
@@ -47,6 +48,7 @@ public class NetworkHandlerRegistry
                 NetworkHandlerDescriptor formatterDescriptor = new NetworkHandlerDescriptor(ext);
                 descriptors.add(formatterDescriptor);
             }
+            descriptors.sort(Comparator.comparingInt(NetworkHandlerDescriptor::getOrder));
         }
     }
 
