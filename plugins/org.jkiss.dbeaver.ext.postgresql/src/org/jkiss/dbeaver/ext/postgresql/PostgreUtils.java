7a5ab59f5a5d12a4c8e43f47a075871ae843b820
serge-rider <serge@jkiss.org>
2018-04-09 01:30:17 +0300

    #3206 Permission grants read refactoring

--- plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/PostgreUtils.java
+++ plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/PostgreUtils.java
@@ -23,17 +23,20 @@ import org.jkiss.code.NotNull;
 import org.jkiss.code.Nullable;
 import org.jkiss.dbeaver.DBException;
 import org.jkiss.dbeaver.Log;
+import org.jkiss.dbeaver.ext.postgresql.edit.PostgreCommandGrantPrivilege;
 import org.jkiss.dbeaver.ext.postgresql.model.*;
 import org.jkiss.dbeaver.model.DBPDataKind;
 import org.jkiss.dbeaver.model.DBPDataSource;
 import org.jkiss.dbeaver.model.DBPEvaluationContext;
 import org.jkiss.dbeaver.model.DBUtils;
 import org.jkiss.dbeaver.model.connection.DBPDriver;
+import org.jkiss.dbeaver.model.edit.DBEPersistAction;
 import org.jkiss.dbeaver.model.exec.DBCException;
 import org.jkiss.dbeaver.model.exec.jdbc.JDBCPreparedStatement;
 import org.jkiss.dbeaver.model.exec.jdbc.JDBCResultSet;
 import org.jkiss.dbeaver.model.exec.jdbc.JDBCSession;
 import org.jkiss.dbeaver.model.impl.AbstractObjectCache;
+import org.jkiss.dbeaver.model.impl.edit.SQLDatabasePersistActionComment;
 import org.jkiss.dbeaver.model.impl.jdbc.JDBCUtils;
 import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
 import org.jkiss.dbeaver.model.struct.DBSObject;
@@ -481,5 +484,25 @@ public class PostgreUtils {
         }
         opt.append(")");
         return opt.toString();
+    }
+
+    public static void getObjectGrantPermissionActions(DBRProgressMonitor monitor, PostgrePermissionsOwner table, List<DBEPersistAction> actions, Map<String, Object> options) throws DBException {
+        if (CommonUtils.getOption(options, PostgreConstants.OPTION_DDL_SHOW_PERMISSIONS)) {
+            // Permissions
+            Collection<PostgrePermission> permissions = table.getPermissions(monitor);
+            if (!CommonUtils.isEmpty(permissions)) {
+                actions.add(new SQLDatabasePersistActionComment(table.getDataSource(), "Permissions"));
+                for (PostgrePermission permission : permissions) {
+                    if (permission.hasAllPrivileges(table)) {
+                        Collections.addAll(actions,
+                            new PostgreCommandGrantPrivilege(permission.getOwner(), true, permission, new PostgrePrivilegeType[] { PostgrePrivilegeType.ALL })
+                                .getPersistActions(options));
+                    } else {
+                        PostgreCommandGrantPrivilege grant = new PostgreCommandGrantPrivilege(permission.getOwner(), true, permission, permission.getPrivileges());
+                        Collections.addAll(actions, grant.getPersistActions(options));
+                    }
+                }
+            }
+        }
     }
 }
