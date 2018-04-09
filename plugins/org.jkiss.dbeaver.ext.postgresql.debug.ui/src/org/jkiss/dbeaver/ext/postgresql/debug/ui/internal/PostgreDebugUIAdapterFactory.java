7deceaa35010a57702fd7df0f15445247e05bdc8
Alexander Fedorov <alexander.fedorov@jkiss.org>
2018-04-09 09:46:27 +0300

    #2357 add DebugEditorAdvisor to remove "postgresql.source.view" hardcode
    Former-commit-id: 723e9db02e0c50e78859799c30651bc492ab0ac0

---
+++ plugins/org.jkiss.dbeaver.ext.postgresql.debug.ui/src/org/jkiss/dbeaver/ext/postgresql/debug/ui/internal/PostgreDebugUIAdapterFactory.java
@@ -0,0 +1,51 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2018 Serge Rider (serge@jkiss.org)
+ * Copyright (C) 2017-2018 Alexander Fedorov (alexander.fedorov@jkiss.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jkiss.dbeaver.ext.postgresql.debug.ui.internal;
+
+import org.eclipse.core.runtime.IAdapterFactory;
+import org.jkiss.dbeaver.debug.ui.DebugEditorAdvisor;
+import org.jkiss.dbeaver.ext.postgresql.model.PostgreDataSource;
+import org.jkiss.dbeaver.model.DBPDataSource;
+import org.jkiss.dbeaver.model.DBPDataSourceContainer;
+
+public class PostgreDebugUIAdapterFactory implements IAdapterFactory {
+
+    private static final Class<?>[] CLASSES = new Class[] { DebugEditorAdvisor.class };
+    
+    private DebugEditorAdvisor debugEditorAdvisor = new PostgreSourceEditorAdvisor();
+
+    @Override
+    public <T> T getAdapter(Object adaptableObject, Class<T> adapterType) {
+        if (adapterType == DebugEditorAdvisor.class) {
+            if (adaptableObject instanceof DBPDataSourceContainer) {
+                DBPDataSourceContainer sourceContainer = (DBPDataSourceContainer) adaptableObject;
+                DBPDataSource dataSource = sourceContainer.getDataSource();
+                if (dataSource instanceof PostgreDataSource) {
+                    return adapterType.cast(debugEditorAdvisor);
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Class<?>[] getAdapterList() {
+        return CLASSES;
+    }
+
+}
