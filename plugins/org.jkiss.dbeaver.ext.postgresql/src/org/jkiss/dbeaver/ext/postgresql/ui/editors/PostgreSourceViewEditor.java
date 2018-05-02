1acc87f1eaaf5833d6e690ca011558ca2a1aed78
serge-rider <serge@jkiss.org>
2018-05-02 13:46:10 +0300

    PG function view: replace "omit header" with "show header"

--- plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/ui/editors/PostgreSourceViewEditor.java
+++ plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/ui/editors/PostgreSourceViewEditor.java
@@ -17,7 +17,6 @@
 
 package org.jkiss.dbeaver.ext.postgresql.ui.editors;
 
-import org.eclipse.e4.core.services.events.IEventBroker;
 import org.eclipse.jface.action.ControlContribution;
 import org.eclipse.jface.action.IContributionManager;
 import org.eclipse.jface.action.Separator;
@@ -26,18 +25,13 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Display;
-import org.jkiss.dbeaver.ext.postgresql.PostgreActivator;
 import org.jkiss.dbeaver.ext.postgresql.PostgreConstants;
 import org.jkiss.dbeaver.ext.postgresql.model.*;
 import org.jkiss.dbeaver.model.DBPScriptObject;
 import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
 import org.jkiss.dbeaver.ui.UIUtils;
-import org.jkiss.dbeaver.ui.editors.IDatabaseEditorInput;
 import org.jkiss.dbeaver.ui.editors.sql.SQLSourceViewer;
-import org.jkiss.dbeaver.utils.GeneralUtils;
-import org.osgi.service.event.Event;
-import org.osgi.service.event.EventHandler;
+import org.jkiss.utils.CommonUtils;
 
 import java.util.Map;
 
@@ -46,48 +40,12 @@ import java.util.Map;
  */
 public class PostgreSourceViewEditor extends SQLSourceViewer<PostgreScriptObject> {
     
-    private static final String TOPIC_DEBUGGER_SOURCE = GeneralUtils.encodeTopic(DBPScriptObject.OPTION_DEBUGGER_SOURCE);
-    private Button omitHeaderCheck;
+    private Button showHeaderCheck;
     private Boolean showPermissions;
     private boolean showColumnComments = true;
 
-    private IEventBroker eventBroker;
-    private TopicEventHandler topicEventHandler = new TopicEventHandler();
-
     public PostgreSourceViewEditor()
     {
-    }
-
-    private class TopicEventHandler implements EventHandler {
-
-        @Override
-        public void handleEvent(Event event) {
-            String topic = event.getTopic();
-            if (TOPIC_DEBUGGER_SOURCE.equals(topic)) {
-                Object data = event.getProperty(IEventBroker.DATA);
-                if (data instanceof String) {
-                    String nodePath = (String) data;
-                    IDatabaseEditorInput editorInput = getEditorInput();
-                    if (nodePath.equals(editorInput.getNavigatorNode().getNodeItemPath())) {
-                        Object omitValue = editorInput.getAttribute(DBPScriptObject.OPTION_DEBUGGER_SOURCE);
-                        boolean omitHeader = Boolean.parseBoolean(String.valueOf(omitValue));
-                        if (!omitHeader) {
-                            setOmitHeader(true);
-                        }
-                    }
-                }
-            }
-        }
-
-    }
-
-    @Override
-    public void dispose() {
-        if (eventBroker != null) {
-            eventBroker.unsubscribe(topicEventHandler);
-            eventBroker = null;
-        }
-        super.dispose();
     }
 
     @Override
@@ -115,10 +73,6 @@ public class PostgreSourceViewEditor extends SQLSourceViewer<PostgreScriptObject
     protected void contributeEditorCommands(IContributionManager contributionManager)
     {
         super.contributeEditorCommands(contributionManager);
-        if (eventBroker == null) {
-            eventBroker = PostgreActivator.getDefault().getEventBroker();
-            eventBroker.subscribe(TOPIC_DEBUGGER_SOURCE, topicEventHandler);
-        }
         PostgreScriptObject sourceObject = getSourceObject();
         if (sourceObject instanceof PostgreProcedure) {
             contributionManager.add(new Separator());
@@ -126,15 +80,16 @@ public class PostgreSourceViewEditor extends SQLSourceViewer<PostgreScriptObject
                 @Override
                 protected Control createControl(Composite parent) {
                     Composite ph = UIUtils.createPlaceholder(parent, 1, 0);
-                    omitHeaderCheck = UIUtils.createCheckbox(ph, "Omit procedure header", "Show only procedure body without auto-generated header", false, 0);
-                    Object omitValue = getEditorInput().getAttribute(DBPScriptObject.OPTION_DEBUGGER_SOURCE);
-                    boolean omitHeader = Boolean.parseBoolean(String.valueOf(omitValue));
-                    omitHeaderCheck.setSelection(omitHeader);
-                    omitHeaderCheck.addSelectionListener(new SelectionAdapter() {
+                    showHeaderCheck = UIUtils.createCheckbox(ph, "Show header", "Shows auto-generated function header", false, 0);
+                    boolean showHeader = CommonUtils.getBoolean(
+                        getEditorInput().getAttribute(DBPScriptObject.OPTION_DEBUGGER_SOURCE), false);
+                    showHeaderCheck.setSelection(showHeader);
+                    showHeaderCheck.addSelectionListener(new SelectionAdapter() {
                         @Override
                         public void widgetSelected(SelectionEvent e) {
-                            boolean omitHeader = omitHeaderCheck.getSelection();
-                            getEditorInput().setAttribute(DBPScriptObject.OPTION_DEBUGGER_SOURCE, omitHeader);
+                            getEditorInput().setAttribute(
+                                DBPScriptObject.OPTION_DEBUGGER_SOURCE,
+                                showHeaderCheck.getSelection());
                             refreshPart(PostgreSourceViewEditor.this, true);
                         }
                     });
@@ -177,24 +132,12 @@ public class PostgreSourceViewEditor extends SQLSourceViewer<PostgreScriptObject
         }
     }
 
-    private void setOmitHeader(boolean omitHeader) {
-        Display.getDefault().syncExec(new Runnable() {
-            
-            @Override
-            public void run() {
-                getEditorInput().setAttribute(DBPScriptObject.OPTION_DEBUGGER_SOURCE, omitHeader);
-                omitHeaderCheck.setSelection(omitHeader);
-                refreshPart(this, true);
-            }
-        });
-    }
-
     @Override
     protected Map<String, Object> getSourceOptions() {
         Map<String, Object> options = super.getSourceOptions();
-        Object omitValue = getEditorInput().getAttribute(DBPScriptObject.OPTION_DEBUGGER_SOURCE);
-        boolean omitHeader = Boolean.parseBoolean(String.valueOf(omitValue));
-        options.put(DBPScriptObject.OPTION_DEBUGGER_SOURCE, omitHeader);
+        boolean inDebug = !CommonUtils.getBoolean(
+            getEditorInput().getAttribute(DBPScriptObject.OPTION_DEBUGGER_SOURCE), false);
+        options.put(DBPScriptObject.OPTION_DEBUGGER_SOURCE, inDebug);
         options.put(PostgreConstants.OPTION_DDL_SHOW_PERMISSIONS, getShowPermissions());
         options.put(PostgreConstants.OPTION_DDL_SHOW_COLUMN_COMMENTS, showColumnComments);
         return options;
