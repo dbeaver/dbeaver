d46de7101192b21a689005f901de1310a943407b
serge-rider <serge@jkiss.org>
2018-05-05 16:18:38 +0300

    Debug shortcuts redesign

--- plugins/org.jkiss.dbeaver.debug.ui/src/org/jkiss/dbeaver/debug/ui/DebugUI.java
+++ plugins/org.jkiss.dbeaver.debug.ui/src/org/jkiss/dbeaver/debug/ui/DebugUI.java
@@ -29,6 +29,8 @@ import org.jkiss.dbeaver.model.struct.DBSObject;
 public class DebugUI {
 
     public static final String BUNDLE_SYMBOLIC_NAME = "org.jkiss.dbeaver.debug.ui"; //$NON-NLS-1$
+    public static final String DEBUG_LAUNCH_GROUP_ID = "org.jkiss.dbeaver.debug.launchGroup";
+    public static final String DEBUG_OPEN_CONFIGURATION_COMMAND_ID = "org.jkiss.dbeaver.debug.ui.commands.command.debugConfigurations"; //$NON-NLS-1$
 
     public static DBGEditorAdvisor findEditorAdvisor(DBPDataSourceContainer dataSourceContainer) {
         DBGEditorAdvisor advisor = Adapters.adapt(dataSourceContainer, DBGEditorAdvisor.class);
