d811ead84f0c3ed6ff7c322cfc5143c9db151d2d
serge@jkiss.org <lfyeyf[eq87>
2018-06-08 13:46:54 +0300

    Panel text editors refactoring
    Former-commit-id: c496b6f6b6a906c099f403b0d4f3aa166f130d61

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/data/managers/stream/TextPanelEditor.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/data/managers/stream/TextPanelEditor.java
@@ -16,51 +16,16 @@
  */
 package org.jkiss.dbeaver.ui.data.managers.stream;
 
-import org.eclipse.jface.resource.JFaceResources;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.StyledText;
-import org.jkiss.code.NotNull;
-import org.jkiss.dbeaver.DBException;
-import org.jkiss.dbeaver.model.data.DBDContent;
-import org.jkiss.dbeaver.model.impl.StringContentStorage;
-import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
 import org.jkiss.dbeaver.ui.data.IValueController;
-import org.jkiss.dbeaver.utils.ContentUtils;
+import org.jkiss.dbeaver.ui.editors.text.BaseTextEditor;
 
 /**
 * TextPanelEditor
 */
-public class TextPanelEditor extends AbstractTextPanelEditor {
+public class TextPanelEditor extends AbstractTextPanelEditor<BaseTextEditor> {
 
     @Override
-    public StyledText createControl(IValueController valueController)
-    {
-        StyledText text = new StyledText(valueController.getEditPlaceholder(), SWT.MULTI | SWT.V_SCROLL);
-        text.setEditable(!valueController.isReadOnly());
-        text.setFont(JFaceResources.getFont(JFaceResources.TEXT_FONT));
-        initEditorSettings(text);
-        return text;
+    protected BaseTextEditor createEditorParty(IValueController valueController) {
+        return new TextEditorPart();
     }
-
-    @Override
-    public void primeEditorValue(@NotNull DBRProgressMonitor monitor, @NotNull StyledText control, @NotNull DBDContent value) throws DBException
-    {
-        monitor.subTask("Read text value");
-        if (value.isNull()) {
-            control.setText("");
-        } else {
-            String strValue = ContentUtils.getContentStringValue(monitor, value);
-            control.setText(strValue);
-        }
-    }
-
-    @Override
-    public void extractEditorValue(@NotNull DBRProgressMonitor monitor, @NotNull StyledText control, @NotNull DBDContent value) throws DBException
-    {
-        monitor.subTask("Read text value");
-        value.updateContents(
-            monitor,
-            new StringContentStorage(control.getText()));
-    }
-
 }
