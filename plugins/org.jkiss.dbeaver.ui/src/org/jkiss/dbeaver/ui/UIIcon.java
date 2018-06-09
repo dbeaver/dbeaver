e40281c9b2a82f4a19dc63d082baf8bd97c88a50
serge-rider <serge@jkiss.org>
2018-06-09 09:47:11 +0300

    UI icon add
    Former-commit-id: 74dd0c1e0af01ff23f2123ad304eae5409f96112

--- plugins/org.jkiss.dbeaver.ui/src/org/jkiss/dbeaver/ui/UIIcon.java
+++ plugins/org.jkiss.dbeaver.ui/src/org/jkiss/dbeaver/ui/UIIcon.java
@@ -186,6 +186,7 @@ public class UIIcon {
 
     public static final DBIcon SAVE_TO_DATABASE = new DBIcon("save_to_db", "sql/save_to_database.png"); //$NON-NLS-1$ //$NON-NLS-2$
 
+    public static final DBIcon PANEL_CUSTOMIZE = new DBIcon("panel_customize", "sql/panel_customize.png"); //$NON-NLS-1$ //$NON-NLS-2$
     public static final DBIcon PANEL_VALUE = new DBIcon("panel_value", "sql/panel_value.png"); //$NON-NLS-1$ //$NON-NLS-2$
     public static final DBIcon PANEL_AGGREGATE = new DBIcon("panel_aggregate", "sql/panel_aggregate.png"); //$NON-NLS-1$ //$NON-NLS-2$
     public static final DBIcon PANEL_METADATA = new DBIcon("panel_metadata", "sql/panel_metadata.png"); //$NON-NLS-1$ //$NON-NLS-2$
