fe863a86877865e3891e2e9d880e48ae7273795b
serge-rider <serge@jkiss.org>
2018-06-26 01:22:53 +0300

    SQL presentation panels

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/editors/sql/registry/SQLPresentationPanelDescriptor.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/editors/sql/registry/SQLPresentationPanelDescriptor.java
@@ -23,28 +23,24 @@ import org.jkiss.dbeaver.model.DBPImage;
 import org.jkiss.dbeaver.registry.AbstractContextDescriptor;
 import org.jkiss.dbeaver.registry.RegistryConstants;
 import org.jkiss.dbeaver.ui.editors.sql.SQLEditorPresentation;
+import org.jkiss.dbeaver.ui.editors.sql.SQLEditorPresentationPanel;
 import org.jkiss.utils.CommonUtils;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
 
 /**
- * SQLPresentationDescriptor
+ * SQLPresentationPanelDescriptor
  */
-public class SQLPresentationDescriptor extends AbstractContextDescriptor {
-
-    public static final String EXTENSION_ID = "org.jkiss.dbeaver.sqlPresentation"; //$NON-NLS-1$
+public class SQLPresentationPanelDescriptor extends AbstractContextDescriptor {
 
     private final String id;
     private final String label;
     private final String description;
     private final ObjectType implClass;
     private final DBPImage icon;
-    private final SQLEditorPresentation.ActivationType activationType;
-    private final List<SQLPresentationPanelDescriptor> panels = new ArrayList<>();
+    private final boolean isSingleton;
 
-    public SQLPresentationDescriptor(IConfigurationElement config)
+    public SQLPresentationPanelDescriptor(IConfigurationElement config)
     {
         super(config);
         this.id = config.getAttribute(RegistryConstants.ATTR_ID);
@@ -52,17 +48,7 @@ public class SQLPresentationDescriptor extends AbstractContextDescriptor {
         this.description = config.getAttribute(RegistryConstants.ATTR_DESCRIPTION);
         this.implClass = new ObjectType(config.getAttribute(RegistryConstants.ATTR_CLASS));
         this.icon = iconToImage(config.getAttribute(RegistryConstants.ATTR_ICON));
-        String activationStr = config.getAttribute("activation");
-        if (CommonUtils.isEmpty(activationStr)) {
-            this.activationType = SQLEditorPresentation.ActivationType.HIDDEN;
-        } else {
-            this.activationType = SQLEditorPresentation.ActivationType.valueOf(activationStr.toUpperCase(Locale.ENGLISH));
-        }
-        for (IConfigurationElement panelConfig : config.getChildren("panel")) {
-            // Load functions
-            SQLPresentationPanelDescriptor presentationDescriptor = new SQLPresentationPanelDescriptor(panelConfig);
-            this.panels.add(presentationDescriptor);
-        }
+        this.isSingleton = CommonUtils.getBoolean(config.getAttribute("singleton"), true);
     }
 
     public String getId() {
@@ -81,18 +67,14 @@ public class SQLPresentationDescriptor extends AbstractContextDescriptor {
         return icon;
     }
 
-    public SQLEditorPresentation.ActivationType getActivationType() {
-        return activationType;
+    public boolean isSingleton() {
+        return isSingleton;
     }
 
-    public List<SQLPresentationPanelDescriptor> getPanels() {
-        return panels;
-    }
-
-    public SQLEditorPresentation createPresentation()
+    public SQLEditorPresentationPanel createPanel()
         throws DBException
     {
-        return implClass.createInstance(SQLEditorPresentation.class);
+        return implClass.createInstance(SQLEditorPresentationPanel.class);
     }
 
 }
