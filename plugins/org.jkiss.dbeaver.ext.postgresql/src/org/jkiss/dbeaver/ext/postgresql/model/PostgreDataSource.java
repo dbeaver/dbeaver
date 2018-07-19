454cc4e9fdfc6dbfd1ec74b303dfaa5981c62704
Tobias Stadler <ts.stadler@gmx.de>
2018-07-19 23:08:57 +0200

    catalogQuery always has the datallowconn clause, so we must pass false as firstClaus to JDBCUtils.appendFilterClause

--- plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/model/PostgreDataSource.java
+++ plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/model/PostgreDataSource.java
@@ -502,7 +502,7 @@ public class PostgreDataSource extends JDBCDataSource implements DBSObjectSelect
             DBSObjectFilter catalogFilters = owner.getContainer().getObjectFilter(PostgreDatabase.class, null, false);
             if (showNDD) {
                 if (catalogFilters != null) {
-                    JDBCUtils.appendFilterClause(catalogQuery, catalogFilters, "datname", true);
+                    JDBCUtils.appendFilterClause(catalogQuery, catalogFilters, "datname", false);
                 }
                 catalogQuery.append("\nORDER BY db.datname");
             }
