f0a44619f1c4c33309eaa39dd2e5b80079dfd224
Serge Rider <serge@jkiss.org>
2018-08-03 16:16:02 +0300

    Invalidation logging fix
    Former-commit-id: 5ac5f652c57fbf7328f7be4eb275f54022dda388

--- plugins/org.jkiss.dbeaver.net.ssh/src/org/jkiss/dbeaver/model/net/ssh/SSHTunnelImpl.java
+++ plugins/org.jkiss.dbeaver.net.ssh/src/org/jkiss/dbeaver/model/net/ssh/SSHTunnelImpl.java
@@ -112,7 +112,7 @@ public class SSHTunnelImpl implements DBWTunnel {
                 try {
                     implementation.invalidateTunnel(monitor1);
                 } catch (Exception e) {
-                    log.error("Error invalidating SSH tunnel", e);
+                    log.debug("Error invalidating SSH tunnel", e);
                 }
             },
             "Ping SSH tunnel " + dataSource.getContainer().getName(),
