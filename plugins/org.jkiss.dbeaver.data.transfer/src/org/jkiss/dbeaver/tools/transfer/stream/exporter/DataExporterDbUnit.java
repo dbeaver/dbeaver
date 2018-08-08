5512db61882f19b751acb4efca6b27c31be64bab
serge-rider <serge@jkiss.org>
2018-08-08 14:17:24 +0300

    #130 CSV import. Exporters refactoring

--- plugins/org.jkiss.dbeaver.data.transfer/src/org/jkiss/dbeaver/tools/transfer/stream/exporter/DataExporterDbUnit.java
+++ plugins/org.jkiss.dbeaver.data.transfer/src/org/jkiss/dbeaver/tools/transfer/stream/exporter/DataExporterDbUnit.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jkiss.dbeaver.tools.transfer.stream.impl;
+package org.jkiss.dbeaver.tools.transfer.stream.exporter;
 
 import java.io.IOException;
 import java.io.InputStream;
