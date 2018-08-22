d99c6890787df14d158c07b08cb4a3b071e3f8a8
Serge Rider <serge@jkiss.org>
2018-08-22 15:28:57 +0300

    #4020 Schema list reading fix

--- plugins/org.jkiss.dbeaver.ext.vertica/src/org/jkiss/dbeaver/ext/vertica/model/VerticaDataSource.java
+++ plugins/org.jkiss.dbeaver.ext.vertica/src/org/jkiss/dbeaver/ext/vertica/model/VerticaDataSource.java
@@ -44,7 +44,7 @@ public class VerticaDataSource extends GenericDataSource {
     }
 
     @Override
-    protected boolean isOmitCatalog() {
+    public boolean isOmitCatalog() {
         return true;
     }
 
