b6eeef3e357951c1b90774059a035b3248af66bc
Serge Rider <serge@jkiss.org>
2018-08-29 13:28:39 +0300

    #4090 PG restore - allow directory selection

--- plugins/org.jkiss.dbeaver.ui/src/org/jkiss/dbeaver/ui/controls/TextWithOpenFile.java
+++ plugins/org.jkiss.dbeaver.ui/src/org/jkiss/dbeaver/ui/controls/TextWithOpenFile.java
@@ -18,6 +18,7 @@ package org.jkiss.dbeaver.ui.controls;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.FileDialog;
 import org.jkiss.dbeaver.ui.dialogs.DialogUtils;
 
@@ -28,17 +29,32 @@ public class TextWithOpenFile extends TextWithOpen
 {
     private String title;
     private String[] filterExt;
+    private boolean openFolder = false;
+
     public TextWithOpenFile(Composite parent, String title, String[] filterExt) {
         super(parent);
         this.title = title;
         this.filterExt = filterExt;
     }
 
+    public void setOpenFolder(boolean openFolder) {
+        this.openFolder = openFolder;
+    }
+
     protected void openBrowser() {
-        FileDialog fd = new FileDialog(getShell(), SWT.OPEN | SWT.SINGLE);
-        fd.setText(title);
-        fd.setFilterExtensions(filterExt);
-        String selected = DialogUtils.openFileDialog(fd);
+        String selected;
+        if (openFolder) {
+            DirectoryDialog fd = new DirectoryDialog(getShell(), SWT.OPEN | SWT.SINGLE);
+            if (title != null) {
+                fd.setText(title);
+            }
+            selected = fd.open();
+        } else {
+            FileDialog fd = new FileDialog(getShell(), SWT.OPEN | SWT.SINGLE);
+            fd.setText(title);
+            fd.setFilterExtensions(filterExt);
+            selected = DialogUtils.openFileDialog(fd);
+        }
         if (selected != null) {
             setText(selected);
         }
