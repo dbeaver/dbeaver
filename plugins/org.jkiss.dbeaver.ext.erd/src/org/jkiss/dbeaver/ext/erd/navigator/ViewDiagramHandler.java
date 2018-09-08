f8b46b0068659a568af9f8d9dad756036a242c55
serge-rider <serge@jkiss.org>
2018-09-08 10:32:03 +0300

    ERD: view ERD context menu
    Former-commit-id: 37ba083ee5f7345a7974a4aea6e5e67e96cab617

---
+++ plugins/org.jkiss.dbeaver.ext.erd/src/org/jkiss/dbeaver/ext/erd/navigator/ViewDiagramHandler.java
@@ -0,0 +1,72 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2018 Serge Rider (serge@jkiss.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jkiss.dbeaver.ext.erd.navigator;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.Adapters;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.part.MultiPageEditorPart;
+import org.jkiss.dbeaver.ext.erd.editor.ERDEditorEmbedded;
+import org.jkiss.dbeaver.model.navigator.DBNDatabaseNode;
+import org.jkiss.dbeaver.model.struct.DBSDataContainer;
+import org.jkiss.dbeaver.model.struct.DBSEntity;
+import org.jkiss.dbeaver.model.struct.DBSObject;
+import org.jkiss.dbeaver.model.struct.DBSObjectContainer;
+import org.jkiss.dbeaver.ui.UIUtils;
+import org.jkiss.dbeaver.ui.actions.AbstractDataSourceHandler;
+import org.jkiss.dbeaver.ui.actions.navigator.NavigatorHandlerObjectOpen;
+import org.jkiss.dbeaver.ui.controls.resultset.IResultSetContainer;
+import org.jkiss.dbeaver.ui.controls.resultset.ResultSetViewer;
+import org.jkiss.dbeaver.ui.dialogs.ActiveWizardDialog;
+import org.jkiss.dbeaver.ui.editors.data.DatabaseDataEditor;
+
+import java.util.Collections;
+
+public class ViewDiagramHandler extends AbstractDataSourceHandler {
+
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException
+    {
+        ISelection selection = HandlerUtil.getCurrentSelection(event);
+        if (selection instanceof IStructuredSelection) {
+            for (Object item : ((IStructuredSelection) selection).toList()) {
+                DBSObject object = Adapters.adapt(item, DBSEntity.class);
+                if (object == null) {
+                    object = Adapters.adapt(item, DBSObjectContainer.class);
+                }
+                if (object != null) {
+                    DBNDatabaseNode node = NavigatorHandlerObjectOpen.getNodeByObject(object);
+                    if (node != null) {
+                        NavigatorHandlerObjectOpen.openEntityEditor(
+                            node,
+                            ERDEditorEmbedded.class.getName(),
+                            null,
+                            null,
+                            UIUtils.getActiveWorkbenchWindow(),
+                            true);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+}
