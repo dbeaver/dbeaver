139cafc478be2ab1c791ca92667cd1df2fddfa06
Serge Rider <serge@jkiss.org>
2018-09-26 15:04:38 +0300

    #4280 Data transfer progress messages fix

--- plugins/org.jkiss.dbeaver.data.transfer/src/org/jkiss/dbeaver/tools/transfer/wizard/DataTransferJob.java
+++ plugins/org.jkiss.dbeaver.data.transfer/src/org/jkiss/dbeaver/tools/transfer/wizard/DataTransferJob.java
@@ -89,7 +89,7 @@ public class DataTransferJob extends AbstractJob {
                 // Show message box
                 UIUtils.showMessageBox(
                     null,
-                    "Data transfer",
+                    DTMessages.data_transfer_wizard_name,
                     "Data transfer completed " + (hasErrors ? "with errors " : "") + "(" + RuntimeUtils.formatExecutionTime(time) + ")",
                     hasErrors ? SWT.ICON_ERROR : SWT.ICON_INFORMATION);
             }
@@ -104,7 +104,8 @@ public class DataTransferJob extends AbstractJob {
         IDataTransferSettings consumerSettings = settings.getNodeSettings(consumer);
 
         setName(NLS.bind(DTMessages.data_transfer_wizard_job_container_name,
-            CommonUtils.truncateString(producer.getObjectName(), 200)));
+            CommonUtils.truncateString(producer.getObjectName(), 200),
+            CommonUtils.truncateString(consumer.getObjectName(), 200)));
 
         IDataTransferSettings nodeSettings = settings.getNodeSettings(producer);
         try {
