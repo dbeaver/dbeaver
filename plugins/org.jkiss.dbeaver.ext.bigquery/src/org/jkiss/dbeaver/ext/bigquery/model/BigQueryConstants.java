f7c4ef18647d7e6369be65f020c3db38bbd4bcb0
serge-rider <serge@jkiss.org>
2018-09-30 20:06:42 +0300

    #1687 BigQuery bundle

---
+++ plugins/org.jkiss.dbeaver.ext.bigquery/src/org/jkiss/dbeaver/ext/bigquery/model/BigQueryConstants.java
@@ -0,0 +1,32 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2017 Serge Rider (serge@jkiss.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jkiss.dbeaver.ext.bigquery.model;
+
+import org.jkiss.dbeaver.model.DBConstants;
+
+/**
+ * Snowflake constants
+ */
+public class BigQueryConstants
+{
+    public static final String PROP_AUTHENTICATOR = DBConstants.INTERNAL_PROP_PREFIX + "authenticator@";
+
+    public static final String PROP_OAUTH_TYPE = DBConstants.INTERNAL_PROP_PREFIX + "oauth-type@";
+
+    public static final String DEFAULT_HOST_NAME = "https://www.googleapis.com/bigquery/v2";
+    public static final int DEFAULT_PORT = 433;
+}
