92e60839c7359fffee4494aadb3eee08ac653517
serge-rider <serge@jkiss.org>
2018-10-12 00:14:40 +0300

    JDBC URL parser/generator refactoring

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/registry/driver/DriverDescriptor.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/registry/driver/DriverDescriptor.java
@@ -65,20 +65,6 @@ public class DriverDescriptor extends AbstractDescriptor implements DBPDriver {
     private static final String DRIVERS_FOLDER = "drivers"; //$NON-NLS-1$
     private static final String PROP_DRIVERS_LOCATION = "DRIVERS_LOCATION";
 
-    private static final char URL_GROUP_START = '{'; //$NON-NLS-1$
-    private static final char URL_GROUP_END = '}'; //$NON-NLS-1$
-    private static final char URL_OPTIONAL_START = '['; //$NON-NLS-1$
-    private static final char URL_OPTIONAL_END = ']'; //$NON-NLS-1$
-
-    public static final String PROP_HOST = "host"; //$NON-NLS-1$
-    public static final String PROP_PORT = "port"; //$NON-NLS-1$
-    public static final String PROP_DATABASE = "database"; //$NON-NLS-1$
-    public static final String PROP_SERVER = "server"; //$NON-NLS-1$
-    public static final String PROP_FOLDER = "folder"; //$NON-NLS-1$
-    public static final String PROP_FILE = "file"; //$NON-NLS-1$
-    public static final String PROP_USER = "user"; //$NON-NLS-1$
-    public static final String PROP_PASSWORD = "password"; //$NON-NLS-1$
-
     private static final String LICENSE_ACCEPT_KEY = "driver.license.accept.";
 
     public static class DriverFileInfo {
@@ -1303,81 +1289,6 @@ public class DriverDescriptor extends AbstractDescriptor implements DBPDriver {
             this.providerId = providerId;
             this.driverId = driverId;
         }
-    }
-
-    public static class MetaURL {
-
-        private List<String> urlComponents = new ArrayList<>();
-        private Set<String> availableProperties = new HashSet<>();
-        private Set<String> requiredProperties = new HashSet<>();
-
-        public List<String> getUrlComponents() {
-            return urlComponents;
-        }
-
-        public Set<String> getAvailableProperties() {
-            return availableProperties;
-        }
-
-        public Set<String> getRequiredProperties() {
-            return requiredProperties;
-        }
-    }
-
-    public static MetaURL parseSampleURL(String sampleURL) throws DBException {
-        MetaURL metaURL = new MetaURL();
-        int offsetPos = 0;
-        for (; ; ) {
-            int divPos = sampleURL.indexOf(URL_GROUP_START, offsetPos);
-            if (divPos == -1) {
-                break;
-            }
-            int divPos2 = sampleURL.indexOf(URL_GROUP_END, divPos);
-            if (divPos2 == -1) {
-                throw new DBException("Bad sample URL: " + sampleURL);
-            }
-            String propName = sampleURL.substring(divPos + 1, divPos2);
-            boolean isOptional = false;
-            int optDiv1 = sampleURL.lastIndexOf(URL_OPTIONAL_START, divPos);
-            int optDiv1c = sampleURL.lastIndexOf(URL_OPTIONAL_END, divPos);
-            int optDiv2 = sampleURL.indexOf(URL_OPTIONAL_END, divPos2);
-            int optDiv2c = sampleURL.indexOf(URL_OPTIONAL_START, divPos2);
-            if (optDiv1 != -1 && optDiv2 != -1 && (optDiv1c == -1 || optDiv1c < optDiv1) && (optDiv2c == -1 || optDiv2c > optDiv2)) {
-                divPos = optDiv1;
-                divPos2 = optDiv2;
-                isOptional = true;
-            }
-            if (divPos > offsetPos) {
-                metaURL.urlComponents.add(sampleURL.substring(offsetPos, divPos));
-            }
-            metaURL.urlComponents.add(sampleURL.substring(divPos, divPos2 + 1));
-            metaURL.availableProperties.add(propName);
-            if (!isOptional) {
-                metaURL.requiredProperties.add(propName);
-            }
-            offsetPos = divPos2 + 1;
-        }
-        if (offsetPos < sampleURL.length() - 1) {
-            metaURL.urlComponents.add(sampleURL.substring(offsetPos));
-        }
-/*
-        // Check for required parts
-        for (String component : urlComponents) {
-            boolean isRequired = !component.startsWith("[");
-            int divPos = component.indexOf('{');
-            if (divPos != -1) {
-                int divPos2 = component.indexOf('}', divPos);
-                if (divPos2 != -1) {
-                    String propName = component.substring(divPos + 1, divPos2);
-                    availableProperties.add(propName);
-                    if (isRequired) {
-                        requiredProperties.add(propName);
-                    }
-                }
-            }
-        }
-*/
-        return metaURL;
     }
 
     public static class DriversParser implements SAXListener {
