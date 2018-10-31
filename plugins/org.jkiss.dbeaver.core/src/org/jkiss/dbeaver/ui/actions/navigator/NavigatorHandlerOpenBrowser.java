e66eb4bf3afe7b4a641119c955f223ce2bd52e8f
serge-rider <serge@jkiss.org>
2018-10-31 22:29:36 +0300

    #4520 Database browser view

---
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/actions/navigator/NavigatorHandlerOpenBrowser.java
@@ -0,0 +1,55 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2017 Serge Rider (serge@jkiss.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jkiss.dbeaver.ui.actions.navigator;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.jkiss.dbeaver.model.navigator.DBNNode;
+import org.jkiss.dbeaver.runtime.ui.DBUserInterface;
+import org.jkiss.dbeaver.ui.navigator.database.DatabaseBrowserView;
+
+public class NavigatorHandlerOpenBrowser extends NavigatorHandlerObjectBase {
+
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        final ISelection selection = HandlerUtil.getCurrentSelection(event);
+
+        if (selection instanceof IStructuredSelection) {
+            final IStructuredSelection structSelection = (IStructuredSelection)selection;
+            Object element = structSelection.getFirstElement();
+            if (!(element instanceof DBNNode)) {
+                return null;
+            }
+            DBNNode node = (DBNNode)element;
+            IWorkbenchWindow workbenchWindow = HandlerUtil.getActiveWorkbenchWindow(event);
+            String nodeId = DatabaseBrowserView.getSecondaryIdFromNode(node);
+            try {
+                workbenchWindow.getActivePage().showView(DatabaseBrowserView.VIEW_ID, nodeId, IWorkbenchPage.VIEW_ACTIVATE);
+            } catch (PartInitException e) {
+                DBUserInterface.getInstance().showError("Database browser", "Error opening database browser", e);
+            }
+        }
+        return null;
+    }
+
+}
