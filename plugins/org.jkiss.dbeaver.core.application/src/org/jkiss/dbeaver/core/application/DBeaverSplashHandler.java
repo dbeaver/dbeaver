c46cb1cfa6dcf7ca736c07b365e50b9dec9509b5
serge-rider <serge@jkiss.org>
2018-11-05 16:52:09 +0300

    #4539 Splashscreen handler fix
    Former-commit-id: 529b495ddfbe7fe8628920d42e74d0b05ecd8771

--- plugins/org.jkiss.dbeaver.core.application/src/org/jkiss/dbeaver/core/application/DBeaverSplashHandler.java
+++ plugins/org.jkiss.dbeaver.core.application/src/org/jkiss/dbeaver/core/application/DBeaverSplashHandler.java
@@ -62,11 +62,12 @@ public class DBeaverSplashHandler extends BasicSplashHandler {
 
         try {
             initVisualization();
+
+            getBundleProgressMonitor().beginTask("Loading", TOTAL_LOADING_TASKS);
         } catch (Exception e) {
             e.printStackTrace(System.err);
         }
 
-        getBundleProgressMonitor().beginTask("Loading", TOTAL_LOADING_TASKS);
     }
 
     private void initVisualization() {
@@ -143,228 +144,15 @@ public class DBeaverSplashHandler extends BasicSplashHandler {
 		if (message == null || message.isEmpty() || message.startsWith(">")) {
 			return;
 		}
-		IProgressMonitor activeMonitor = getActiveMonitor();
-		if (activeMonitor != null) {
-			activeMonitor.setTaskName(message);
-			activeMonitor.worked(1);
-		}
-	}
-
-    /*
-     private final static int F_LABEL_HORIZONTAL_INDENT = 175;
-
-     private final static int F_BUTTON_WIDTH_HINT = 80;
-
-     private final static int F_TEXT_WIDTH_HINT = 175;
-
-     private final static int F_COLUMN_COUNT = 3;
-
-     private Composite fCompositeLogin;
-
-     private Text fTextUsername;
-
-     private Text fTextPassword;
-
-     private Button fButtonOK;
-
-     private Button fButtonCancel;
-
-     private boolean fAuthenticated;
-
-     */
-/**
-	 * 
-	 */
-/*
-	public DBeaverSplashHandler() {
-		fCompositeLogin = null;
-		fTextUsername = null;
-		fTextPassword = null;
-		fButtonOK = null;
-		fButtonCancel = null;
-		fAuthenticated = false;
-	}
-
-    public void init(final Shell splash) {
-		// Store the shell
-		super.init(splash);
-		// Configure the shell layout
-		configureUISplash();
-		// Create UI
-		createUI();		
-		// Create UI listeners
-		createUIListeners();
-		// Force the splash screen to layout
-		splash.layout(true);
-		// Keep the splash screen visible and prevent the RCP application from 
-		// loading until the close button is clicked.
-		doEventLoop();
-	}
-	
-	private void doEventLoop() {
-		Shell splash = getSplash();
-		while (fAuthenticated == false) {
-			if (splash.getDisplay().readAndDispatch() == false) {
-				splash.getDisplay().sleep();
-			}
-		}
-	}
-
-	private void createUIListeners() {
-		// Create the OK button listeners
-		createUIListenersButtonOK();
-		// Create the cancel button listeners
-		createUIListenersButtonCancel();
-	}
-
-	private void createUIListenersButtonCancel() {
-		fButtonCancel.addSelectionListener(new SelectionAdapter() {
-			public void widgetSelected(SelectionEvent e) {
-				handleButtonCancelWidgetSelected();
-			}
-		});		
-	}
-
-	private void handleButtonCancelWidgetSelected() {
-		// Abort the loading of the RCP application
-		getSplash().getDisplay().close();
-		System.exit(0);		
-	}
-	
-	private void createUIListenersButtonOK() {
-		fButtonOK.addSelectionListener(new SelectionAdapter() {
-			public void widgetSelected(SelectionEvent e) {
-				handleButtonOKWidgetSelected();
-			}
-		});				
-	}
-
-	private void handleButtonOKWidgetSelected() {
-		String username = fTextUsername.getText();
-		String password = fTextPassword.getText();
-		// Aunthentication is successful if a user provides any username and
-		// any password
-		if ((username.length() > 0) &&
-				(password.length() > 0)) {
-			fAuthenticated = true;
-		} else {
-			MessageDialog.openError(
-					getSplash(),
-					"Authentication Failed",  //NON-NLS-1
-					"A username and password must be specified to login.");  //NON-NLS-1
-		}
-	}
-	
-	private void createUI() {
-		// Create the login panel
-		createUICompositeLogin();
-		// Create the blank spanner
-		createUICompositeBlank();
-		// Create the user name label
-		createUILabelUserName();
-		// Create the user name text widget
-		createUITextUserName();
-		// Create the password label
-		createUILabelPassword();
-		// Create the password text widget
-		createUITextPassword();
-		// Create the blank label
-		createUILabelBlank();
-		// Create the OK button
-		createUIButtonOK();
-		// Create the cancel button
-		createUIButtonCancel();
-	}		
-	
-	private void createUIButtonCancel() {
-		// Create the button
-		fButtonCancel = new Button(fCompositeLogin, SWT.PUSH);
-		fButtonCancel.setText("Cancel"); //NON-NLS-1
-		// Configure layout data
-		GridData data = new GridData(SWT.NONE, SWT.NONE, false, false);
-		data.widthHint = F_BUTTON_WIDTH_HINT;	
-		data.verticalIndent = 10;
-		fButtonCancel.setLayoutData(data);
-	}
-
-	private void createUIButtonOK() {
-		// Create the button
-		fButtonOK = new Button(fCompositeLogin, SWT.PUSH);
-		fButtonOK.setText("OK"); //NON-NLS-1
-		// Configure layout data
-		GridData data = new GridData(SWT.NONE, SWT.NONE, false, false);
-		data.widthHint = F_BUTTON_WIDTH_HINT;
-		data.verticalIndent = 10;
-		fButtonOK.setLayoutData(data);
-	}
-
-	private void createUILabelBlank() {
-		Label label = new Label(fCompositeLogin, SWT.NONE);
-		label.setVisible(false);
-	}
-
-	private void createUITextPassword() {
-		// Create the text widget
-		int style = SWT.PASSWORD | SWT.BORDER;
-		fTextPassword = new Text(fCompositeLogin, style);
-		// Configure layout data
-		GridData data = new GridData(SWT.NONE, SWT.NONE, false, false);
-		data.widthHint = F_TEXT_WIDTH_HINT;
-		data.horizontalSpan = 2;
-		fTextPassword.setLayoutData(data);		
-	}
-
-	private void createUILabelPassword() {
-		// Create the label
-		Label label = new Label(fCompositeLogin, SWT.NONE);
-		label.setText("&Password:"); //NON-NLS-1
-		// Configure layout data
-		GridData data = new GridData();
-		data.horizontalIndent = F_LABEL_HORIZONTAL_INDENT;
-		label.setLayoutData(data);					
-	}
-
-	private void createUITextUserName() {
-		// Create the text widget
-		fTextUsername = new Text(fCompositeLogin, SWT.BORDER);
-		// Configure layout data
-		GridData data = new GridData(SWT.NONE, SWT.NONE, false, false);
-		data.widthHint = F_TEXT_WIDTH_HINT;
-		data.horizontalSpan = 2;
-		fTextUsername.setLayoutData(data);		
-	}
-
-	private void createUILabelUserName() {
-		// Create the label
-		Label label = new Label(fCompositeLogin, SWT.NONE);
-		label.setText("&User Name:"); //NON-NLS-1
-		// Configure layout data
-		GridData data = new GridData();
-		data.horizontalIndent = F_LABEL_HORIZONTAL_INDENT;
-		label.setLayoutData(data);		
-	}
-
-	private void createUICompositeBlank() {
-		Composite spanner = new Composite(fCompositeLogin, SWT.NONE);
-		GridData data = new GridData(SWT.FILL, SWT.FILL, true, true);
-		data.horizontalSpan = F_COLUMN_COUNT;
-		spanner.setLayoutData(data);
-	}
-
-	private void createUICompositeLogin() {
-		// Create the composite
-		fCompositeLogin = new Composite(getSplash(), SWT.BORDER);
-		GridLayout layout = new GridLayout(F_COLUMN_COUNT, false);
-		fCompositeLogin.setLayout(layout);		
-	}
-
-	private void configureUISplash() {
-		// Configure layout
-		FillLayout layout = new FillLayout(); 
-		getSplash().setLayout(layout);
-		// Force shell to inherit the splash background
-		getSplash().setBackgroundMode(SWT.INHERIT_DEFAULT);
-	}
-*/
+        try {
+            IProgressMonitor activeMonitor = getActiveMonitor();
+            if (activeMonitor != null) {
+                activeMonitor.setTaskName(message);
+                activeMonitor.worked(1);
+            }
+        } catch (Throwable e) {
+            e.printStackTrace(System.err);
+        }
+    }
 
 }
