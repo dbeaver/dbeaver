06ca9efa43e27935dd9c07e08e0b0010f2d4e4ff
Serge Rider <serge@jkiss.org>
2018-11-08 18:27:10 +0300

    #810 SQL Server model refactoring.

--- plugins/org.jkiss.dbeaver.ext.mssql/src/org/jkiss/dbeaver/ext/mssql/model/generic/SQLServerGenericDataSource.java
+++ plugins/org.jkiss.dbeaver.ext.mssql/src/org/jkiss/dbeaver/ext/mssql/model/generic/SQLServerGenericDataSource.java
@@ -14,14 +14,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jkiss.dbeaver.ext.mssql.model;
+package org.jkiss.dbeaver.ext.mssql.model.generic;
 
-import org.eclipse.core.runtime.IAdaptable;
 import org.jkiss.dbeaver.DBException;
 import org.jkiss.dbeaver.ModelPreferences;
 import org.jkiss.dbeaver.ext.generic.model.GenericDataSource;
 import org.jkiss.dbeaver.ext.mssql.SQLServerConstants;
 import org.jkiss.dbeaver.ext.mssql.SQLServerUtils;
+import org.jkiss.dbeaver.ext.mssql.model.SQLServerDialect;
 import org.jkiss.dbeaver.model.DBConstants;
 import org.jkiss.dbeaver.model.DBPDataKind;
 import org.jkiss.dbeaver.model.DBPDataSourceContainer;
@@ -29,23 +29,28 @@ import org.jkiss.dbeaver.model.DBUtils;
 import org.jkiss.dbeaver.model.connection.DBPConnectionConfiguration;
 import org.jkiss.dbeaver.model.connection.DBPDriver;
 import org.jkiss.dbeaver.model.exec.DBCException;
-import org.jkiss.dbeaver.model.exec.plan.DBCQueryPlanner;
-import org.jkiss.dbeaver.model.impl.jdbc.JDBCDataSource;
 import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
-import org.jkiss.dbeaver.model.struct.DBSInstanceContainer;
-import org.jkiss.dbeaver.model.struct.DBSObjectSelector;
 import org.jkiss.utils.CommonUtils;
 
 import java.sql.Types;
 import java.util.HashMap;
 import java.util.Map;
 
-public abstract class SQLServerDataSource extends JDBCDataSource implements DBSObjectSelector, DBSInstanceContainer, DBCQueryPlanner, IAdaptable {
+public class SQLServerGenericDataSource extends GenericDataSource {
 
-    SQLServerDataSource(DBRProgressMonitor monitor, DBPDataSourceContainer container)
+    public SQLServerGenericDataSource(DBRProgressMonitor monitor, DBPDataSourceContainer container)
         throws DBException
     {
-        super(monitor, container, new SQLServerDialect());
+        this(monitor, container,
+            new SQLServerMetaModel(
+                SQLServerUtils.isDriverSqlServer(container.getDriver())
+            ));
+    }
+
+    SQLServerGenericDataSource(DBRProgressMonitor monitor, DBPDataSourceContainer container, SQLServerMetaModel metaModel)
+        throws DBException
+    {
+        super(monitor, container, metaModel, new SQLServerDialect());
     }
 
     @Override
@@ -112,6 +117,11 @@ public abstract class SQLServerDataSource extends JDBCDataSource implements DBSO
         } else {
             return super.getConnectionUserPassword(connectionInfo);
         }
+    }
+
+    @Override
+    protected boolean isPopulateClientAppName() {
+        return false;
     }
 
 }
