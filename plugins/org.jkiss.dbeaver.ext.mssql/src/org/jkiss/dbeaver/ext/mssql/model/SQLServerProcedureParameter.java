5928093a5e339056b1574836dbbf7ce87c09271f
serge-rider <serge@jkiss.org>
2018-11-11 13:47:38 +0300

    #810 SQL Server: procedures/parameters
    Former-commit-id: 9396fe20c774b8bda472c22a6c5abb6b2076e0fb

--- plugins/org.jkiss.dbeaver.ext.mssql/src/org/jkiss/dbeaver/ext/mssql/model/SQLServerProcedureParameter.java
+++ plugins/org.jkiss.dbeaver.ext.mssql/src/org/jkiss/dbeaver/ext/mssql/model/SQLServerProcedureParameter.java
@@ -17,69 +17,165 @@
 package org.jkiss.dbeaver.ext.mssql.model;
 
 import org.jkiss.code.NotNull;
-import org.jkiss.dbeaver.model.impl.jdbc.struct.JDBCAttribute;
+import org.jkiss.code.Nullable;
+import org.jkiss.dbeaver.DBException;
+import org.jkiss.dbeaver.model.DBPDataKind;
+import org.jkiss.dbeaver.model.DBUtils;
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCResultSet;
+import org.jkiss.dbeaver.model.impl.jdbc.JDBCUtils;
 import org.jkiss.dbeaver.model.meta.Property;
-import org.jkiss.dbeaver.model.struct.DBSTypedObject;
+import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
+import org.jkiss.dbeaver.model.struct.DBSAttributeBase;
+import org.jkiss.dbeaver.model.struct.DBSObject;
 import org.jkiss.dbeaver.model.struct.rdb.DBSProcedureParameter;
 import org.jkiss.dbeaver.model.struct.rdb.DBSProcedureParameterKind;
 
 /**
  * SQLServerProcedureParameter
  */
-public class SQLServerProcedureParameter extends JDBCAttribute implements DBSProcedureParameter, DBSTypedObject
-{
+public class SQLServerProcedureParameter implements DBSProcedureParameter, DBSAttributeBase, DBSObject {
     private SQLServerProcedure procedure;
+    private String paramName;
+    private int ordinalPosition;
+    private SQLServerDataType dataType;
+    private int maxLength;
+    private int scale;
+    private int precision;
     private DBSProcedureParameterKind parameterKind;
+    private boolean nullable;
+    private String defaultValue;
 
     public SQLServerProcedureParameter(
-            SQLServerProcedure procedure,
-            String columnName,
-            String typeName,
-            int valueType,
-            int ordinalPosition,
-            long columnSize,
-            Integer scale,
-            Integer precision,
-            boolean notNull,
-            DBSProcedureParameterKind parameterKind)
+        DBRProgressMonitor monitor,
+        SQLServerProcedure procedure,
+        JDBCResultSet dbResult)
+        throws DBException
     {
-        super(columnName,
-            typeName,
-            valueType,
-            ordinalPosition,
-            columnSize,
-            scale,
-            precision,
-            notNull,
-            false);
         this.procedure = procedure;
-        this.parameterKind = parameterKind;
+        this.paramName = JDBCUtils.safeGetString(dbResult, "name");
+        this.ordinalPosition = JDBCUtils.safeGetInt(dbResult, "parameter_id");
+
+        int typeID = JDBCUtils.safeGetInt(dbResult, "user_type_id");
+        this.dataType = procedure.getContainer().getDatabase().getDataType(monitor, typeID);
+
+        boolean isOutput = JDBCUtils.safeGetInt(dbResult, "is_output") != 0;
+        boolean isCursor = JDBCUtils.safeGetInt(dbResult, "is_cursor_ref") != 0;
+        boolean readonly = JDBCUtils.safeGetInt(dbResult, "is_readonly") != 0;
+        this.parameterKind = isCursor ? DBSProcedureParameterKind.RESULTSET :
+            (isOutput ? DBSProcedureParameterKind.OUT :
+                (readonly ? DBSProcedureParameterKind.IN : DBSProcedureParameterKind.INOUT));
+
+        this.maxLength = JDBCUtils.safeGetInt(dbResult, "max_length");
+        this.scale = JDBCUtils.safeGetInt(dbResult, "scale");
+        this.precision = JDBCUtils.safeGetInt(dbResult, "precision");
+
+        if (JDBCUtils.safeGetInt(dbResult, "has_default_value") != 0) {
+            defaultValue = JDBCUtils.safeGetString(dbResult, "default_value");
+        }
+        nullable = JDBCUtils.safeGetInt(dbResult, "is_nullable") != 0;
     }
 
     @NotNull
     @Override
-    public SQLServerDataSource getDataSource()
-    {
+    public SQLServerDataSource getDataSource() {
         return procedure.getDataSource();
     }
 
+    @Nullable
     @Override
-    public SQLServerProcedure getParentObject()
-    {
+    public String getDescription() {
+        return null;
+    }
+
+    @Override
+    public SQLServerProcedure getParentObject() {
         return procedure;
     }
 
-    @NotNull
     @Override
-    @Property(viewable = true, order = 10)
-    public DBSProcedureParameterKind getParameterKind()
-    {
-        return parameterKind;
+    public boolean isPersisted() {
+        return true;
     }
 
     @NotNull
     @Override
-    public DBSTypedObject getParameterType() {
-        return this;
+    @Property(viewable = true, order = 1)
+    public String getName() {
+        return paramName;
     }
+
+    @NotNull
+    @Override
+    @Property(viewable = true, order = 2)
+    public SQLServerDataType getParameterType() {
+        return dataType;
+    }
+
+    @NotNull
+    @Override
+    @Property(viewable = true, order = 3)
+    public DBSProcedureParameterKind getParameterKind() {
+        return parameterKind;
+    }
+
+    @Override
+    @Property(viewable = true, order = 4)
+    public int getOrdinalPosition() {
+        return ordinalPosition;
+    }
+
+    @Override
+    public boolean isRequired() {
+        return !nullable;
+    }
+
+    @Override
+    public boolean isAutoGenerated() {
+        return false;
+    }
+
+    @Override
+    public String getTypeName() {
+        return dataType.getTypeName();
+    }
+
+    @Override
+    public String getFullTypeName() {
+        return DBUtils.getFullTypeName(this);
+    }
+
+    @Override
+    public int getTypeID() {
+        return dataType.getTypeID();
+    }
+
+    @Override
+    public DBPDataKind getDataKind() {
+        return dataType.getDataKind();
+    }
+
+    @Override
+    public Integer getScale() {
+        return scale;
+    }
+
+    @Override
+    public Integer getPrecision() {
+        return precision;
+    }
+
+    @Override
+    public long getMaxLength() {
+        return maxLength;
+    }
+
+    @Property(viewable = true, order = 5)
+    public String getDefaultValue() {
+        return defaultValue;
+    }
+
+    public void setDefaultValue(String defaultValue) {
+        this.defaultValue = defaultValue;
+    }
+
 }
