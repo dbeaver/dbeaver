ae3e6ab3feb56b24ff3a4eda0ee676ea6ef8ca29
serge-rider <serge@jkiss.org>
2018-11-14 22:17:19 +0300

    #4265 Redshift external schemas reading (add error handle)
    Former-commit-id: fe1296fd15c00de0d9e88561f0e594a4a8d28ba9

--- plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/model/impls/redshift/PostgreServerRedshift.java
+++ plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/model/impls/redshift/PostgreServerRedshift.java
@@ -17,7 +17,7 @@
 package org.jkiss.dbeaver.ext.postgresql.model.impls.redshift;
 
 import org.jkiss.dbeaver.DBException;
-import org.jkiss.dbeaver.ext.postgresql.PostgreConstants;
+import org.jkiss.dbeaver.Log;
 import org.jkiss.dbeaver.ext.postgresql.model.PostgreDataSource;
 import org.jkiss.dbeaver.ext.postgresql.model.PostgreDatabase;
 import org.jkiss.dbeaver.ext.postgresql.model.PostgreSchema;
@@ -30,7 +30,6 @@ import org.jkiss.dbeaver.model.exec.jdbc.JDBCSession;
 import org.jkiss.dbeaver.model.exec.jdbc.JDBCStatement;
 import org.jkiss.dbeaver.model.impl.jdbc.JDBCUtils;
 import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
-import org.jkiss.dbeaver.model.struct.DBSObjectFilter;
 
 import java.sql.SQLException;
 import java.util.HashMap;
@@ -40,6 +39,8 @@ import java.util.Map;
  * PostgreServerRedshift
  */
 public class PostgreServerRedshift extends PostgreServerExtensionBase {
+
+    private static final Log log = Log.getLog(PostgreServerRedshift.class);
 
     public PostgreServerRedshift(PostgreDataSource dataSource) {
         super(dataSource);
@@ -175,6 +176,8 @@ public class PostgreServerRedshift extends PostgreServerExtensionBase {
                         esSchemaMap.put(esSchemaName, esSchemaOptions);
                     }
                 }
+            } catch (Throwable e) {
+                log.debug("Error reading Redshift external schemas", e);
             }
 
             // 2. Rad standard schemas
