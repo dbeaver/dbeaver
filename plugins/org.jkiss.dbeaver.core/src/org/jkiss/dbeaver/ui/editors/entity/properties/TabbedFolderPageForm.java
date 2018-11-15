676f1004bd16e6bc65d07725b16710d643112596
serge-rider <serge@jkiss.org>
2018-11-16 00:46:36 +0300

    Form editor: link text

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/editors/entity/properties/TabbedFolderPageForm.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/editors/entity/properties/TabbedFolderPageForm.java
@@ -573,7 +573,7 @@ public class TabbedFolderPageForm extends TabbedFolderPage implements IRefreshab
     }
 
     private String getLinktitle(Object value) {
-        return value == null ? "          " : "<a>" + objectValueToString(value) + "</a>";
+        return value == null ? "N/A" : "<a>" + objectValueToString(value) + "</a>";
     }
 
     private void loadEditorValues(Map<DBPPropertyDescriptor, Object> editorValues) {
