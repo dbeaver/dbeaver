5526a4619e1bb071998eb0027668ed193a59911f
serge-rider <serge@jkiss.org>
2018-11-18 13:27:06 +0300

    #4461 PostgreSQL + GIS: support new geometry values create

--- plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/model/PostgreTableColumn.java
+++ plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/model/PostgreTableColumn.java
@@ -17,17 +17,27 @@
 package org.jkiss.dbeaver.ext.postgresql.model;
 
 import org.jkiss.dbeaver.DBException;
+import org.jkiss.dbeaver.Log;
 import org.jkiss.dbeaver.ext.postgresql.PostgreUtils;
+import org.jkiss.dbeaver.model.DBUtils;
 import org.jkiss.dbeaver.model.exec.jdbc.JDBCResultSet;
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCSession;
+import org.jkiss.dbeaver.model.gis.GisAttribute;
+import org.jkiss.dbeaver.model.impl.jdbc.JDBCUtils;
 import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
 
+import java.sql.SQLException;
 import java.util.Collection;
 
 /**
  * PostgreTableColumn
  */
-public class PostgreTableColumn extends PostgreAttribute<PostgreTableBase> implements PostgrePermissionsOwner
+public class PostgreTableColumn extends PostgreAttribute<PostgreTableBase> implements PostgrePermissionsOwner, GisAttribute
 {
+    private static final Log log = Log.getLog(PostgreTableColumn.class);
+
+    private int srid = -1;
+
     public PostgreTableColumn(PostgreTableBase table) {
         super(table);
     }
@@ -51,4 +61,19 @@ public class PostgreTableColumn extends PostgreAttribute<PostgreTableBase> imple
         return PostgreUtils.extractPermissionsFromACL(monitor, this, getAcl());
     }
 
+    @Override
+    public int getAttributeSRID(DBRProgressMonitor monitor) {
+        if (srid == -1) {
+            try (JDBCSession session = DBUtils.openMetaSession(monitor, this, "Load table inheritance info")) {
+                Object sridValue = JDBCUtils.queryObject(session, "SELECT Find_SRID('" + getSchema().getName() + "', '" + getTable().getName() + "', '" + getName() + "')");
+                if (sridValue instanceof Number) {
+                    srid = ((Number) sridValue).intValue();
+                }
+            } catch (SQLException e) {
+                log.debug("Error reading attribute " + getName() + " SRID", e);
+                srid = 0;
+            }
+        }
+        return srid;
+    }
 }
