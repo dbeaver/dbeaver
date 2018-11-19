d4610a5aaa7ada5ee9c1b92e705b5f93830fbc50
Serge Rider <serge@jkiss.org>
2018-11-19 15:51:20 +0300

    #4632 SQL Server: database and schema filtering fix

--- plugins/org.jkiss.dbeaver.ext.mssql/src/org/jkiss/dbeaver/ext/mssql/model/SQLServerDataSource.java
+++ plugins/org.jkiss.dbeaver.ext.mssql/src/org/jkiss/dbeaver/ext/mssql/model/SQLServerDataSource.java
@@ -30,10 +30,7 @@ import org.jkiss.dbeaver.model.connection.DBPDriver;
 import org.jkiss.dbeaver.model.exec.DBCException;
 import org.jkiss.dbeaver.model.exec.DBCExecutionPurpose;
 import org.jkiss.dbeaver.model.exec.DBCSession;
-import org.jkiss.dbeaver.model.exec.jdbc.JDBCDatabaseMetaData;
-import org.jkiss.dbeaver.model.exec.jdbc.JDBCResultSet;
-import org.jkiss.dbeaver.model.exec.jdbc.JDBCSession;
-import org.jkiss.dbeaver.model.exec.jdbc.JDBCStatement;
+import org.jkiss.dbeaver.model.exec.jdbc.*;
 import org.jkiss.dbeaver.model.impl.jdbc.JDBCDataSource;
 import org.jkiss.dbeaver.model.impl.jdbc.JDBCExecutionContext;
 import org.jkiss.dbeaver.model.impl.jdbc.JDBCUtils;
@@ -297,7 +294,11 @@ public class SQLServerDataSource extends JDBCDataSource implements DBSObjectSele
                 JDBCUtils.appendFilterClause(sql, databaseFilters, "name", true);
             }
             sql.append("\nORDER BY db.name");
-            return session.prepareStatement(sql.toString());
+            JDBCPreparedStatement dbStat = session.prepareStatement(sql.toString());
+            if (databaseFilters != null) {
+                JDBCUtils.setFilterParameters(dbStat, 1, databaseFilters);
+            }
+            return dbStat;
         }
 
         @Override
