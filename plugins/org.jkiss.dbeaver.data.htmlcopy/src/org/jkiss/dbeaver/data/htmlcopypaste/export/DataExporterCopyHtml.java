ceaffa20a7d706720bdd66faa7f8a8ac4edf8b6e
Regis Frischinger <regis.frischinger@prevo.ch>
2018-11-29 10:54:11 +0100

    name change

--- plugins/org.jkiss.dbeaver.data.htmlcopy/src/org/jkiss/dbeaver/data/htmlcopypaste/export/DataExporterCopyHtml.java
+++ plugins/org.jkiss.dbeaver.data.htmlcopy/src/org/jkiss/dbeaver/data/htmlcopypaste/export/DataExporterCopyHtml.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jkiss.dbeaver.data.htmlcopy.export;
+package org.jkiss.dbeaver.data.htmlcopypaste.export;
 
 import java.awt.Image;
 import java.awt.image.BufferedImage;
