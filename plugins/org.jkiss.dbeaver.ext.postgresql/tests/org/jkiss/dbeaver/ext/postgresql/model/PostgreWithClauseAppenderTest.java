b9e1b57ac3a01b9cc963ccd503c442299d69117e
Gavin Shaw <gshaw@pivotal.io>
2018-12-13 12:57:33 -0500

    Refactor unit tests for PostgreWithClauseAppender
    [#162594529]

--- plugins/org.jkiss.dbeaver.ext.postgresql/tests/org/jkiss/dbeaver/ext/postgresql/model/PostgreWithClauseAppenderTest.java
+++ plugins/org.jkiss.dbeaver.ext.postgresql/tests/org/jkiss/dbeaver/ext/postgresql/model/PostgreWithClauseAppenderTest.java
@@ -3,35 +3,37 @@ package org.jkiss.dbeaver.ext.postgresql.model;
 import org.jkiss.dbeaver.ext.postgresql.model.impls.greenplum.GreenplumTable;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.jkiss.dbeaver.ext.postgresql.model.PostgreWithClauseAppender.generateWithClause;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class PostgreWithClauseAppenderTest {
 
+    @Mock
     private PostgreTableRegular table;
+
+    @Mock
     private PostgreTableBase tableBase;
 
+    @Mock
     private PostgreDataSource dataSource;
+
+    @Mock
     private PostgreServerExtension serverExtension;
 
     @Before
     public void setup() {
-        dataSource = mock(PostgreDataSource.class);
-        serverExtension = mock(PostgreServerExtension.class);
-        tableBase = mock(PostgreTableBase.class);
     }
 
     @Test
     public void generateWithClause_whenOidsAreSupportedAndNoRelOptions_shouldDisplayWithClauseWithOidsAsTrue() {
-        table = mock(PostgreTableRegular.class);
-
-        when(serverExtension.supportsOids()).thenReturn(true);
-        when(dataSource.getServerType()).thenReturn(serverExtension);
-        when(table.getDataSource()).thenReturn(dataSource);
-        when(table.isHasOids()).thenReturn(true);
+        setupGeneralWhenMocks(true, true);
 
         String withClause = generateWithClause(table, tableBase);
         assertEquals("\nWITH (\n\tOIDS=TRUE\n)", withClause);
@@ -39,12 +41,7 @@ public class PostgreWithClauseAppenderTest {
 
     @Test
     public void generateWithClause_whenTableSupportsOidsButDoesNotHaveOidsAndNoOptions_shouldNotDisplayWithClause() {
-        table = mock(PostgreTableRegular.class);
-
-        when(serverExtension.supportsOids()).thenReturn(true);
-        when(dataSource.getServerType()).thenReturn(serverExtension);
-        when(table.getDataSource()).thenReturn(dataSource);
-        when(table.isHasOids()).thenReturn(false);
+        setupGeneralWhenMocks(true, false);
 
         String withClause = generateWithClause(table, tableBase);
         assertEquals("", withClause);
@@ -54,10 +51,7 @@ public class PostgreWithClauseAppenderTest {
     public void generateWithClause_whenTableIsAGreenPlumTableWithOptions_shouldDisplayWithClauseWithRelOptions() {
         table = mock(GreenplumTable.class);
 
-        when(serverExtension.supportsOids()).thenReturn(false);
-        when(dataSource.getServerType()).thenReturn(serverExtension);
-        when(table.getDataSource()).thenReturn(dataSource);
-        when(table.isHasOids()).thenReturn(false);
+        setupGeneralWhenMocks(false, false);
 
         when(tableBase.getRelOptions()).thenReturn(new String[]{"appendonly=true"});
 
@@ -69,10 +63,7 @@ public class PostgreWithClauseAppenderTest {
     public void generateWithClause_whenTableIsGreenPlumTableWithOidsAndOptions_shouldDisplayWithClauseWithOidsAndOptions() {
         table = mock(GreenplumTable.class);
 
-        when(serverExtension.supportsOids()).thenReturn(true);
-        when(dataSource.getServerType()).thenReturn(serverExtension);
-        when(table.getDataSource()).thenReturn(dataSource);
-        when(table.isHasOids()).thenReturn(true);
+        setupGeneralWhenMocks(true, true);
 
         when(tableBase.getRelOptions()).thenReturn(new String[]{"appendonly=true"});
 
@@ -84,10 +75,7 @@ public class PostgreWithClauseAppenderTest {
     public void generateWithClause_whenTableIsGreenPlumTableWithoutOidsAndWithoutOptions_shouldNotDisplayWithClause() {
         table = mock(GreenplumTable.class);
 
-        when(serverExtension.supportsOids()).thenReturn(false);
-        when(dataSource.getServerType()).thenReturn(serverExtension);
-        when(table.getDataSource()).thenReturn(dataSource);
-        when(table.isHasOids()).thenReturn(false);
+        setupGeneralWhenMocks(false, false);
 
         when(tableBase.getRelOptions()).thenReturn(null);
 
@@ -99,10 +87,7 @@ public class PostgreWithClauseAppenderTest {
     public void generateWithClause_whenTableIsGreenPlumTableWithOidsWithoutOptions_shouldDisplayWithClauseWithOids() {
         table = mock(GreenplumTable.class);
 
-        when(serverExtension.supportsOids()).thenReturn(true);
-        when(dataSource.getServerType()).thenReturn(serverExtension);
-        when(table.getDataSource()).thenReturn(dataSource);
-        when(table.isHasOids()).thenReturn(true);
+        setupGeneralWhenMocks(true, true);
 
         when(tableBase.getRelOptions()).thenReturn(null);
 
@@ -114,14 +99,18 @@ public class PostgreWithClauseAppenderTest {
     public void generateWithClause_whenTableIsGreenPlumTableWithOidsWithMultipleOptions_shouldDisplayWithClauseWithOidsAndAllTheOptions() {
         table = mock(GreenplumTable.class);
 
-        when(serverExtension.supportsOids()).thenReturn(true);
-        when(dataSource.getServerType()).thenReturn(serverExtension);
-        when(table.getDataSource()).thenReturn(dataSource);
-        when(table.isHasOids()).thenReturn(true);
+        setupGeneralWhenMocks(true, true);
 
         when(tableBase.getRelOptions()).thenReturn(new String[]{"appendonly=true", "orientation=column"});
 
         String withClause = generateWithClause(table, tableBase);
         assertEquals("\nWITH (\n\tOIDS=TRUE, appendonly=true, orientation=column\n)", withClause);
+    }
+
+    private void setupGeneralWhenMocks(boolean supportOids, boolean hasOids) {
+        when(serverExtension.supportsOids()).thenReturn(supportOids);
+        when(dataSource.getServerType()).thenReturn(serverExtension);
+        when(table.getDataSource()).thenReturn(dataSource);
+        when(table.isHasOids()).thenReturn(hasOids);
     }
 }
