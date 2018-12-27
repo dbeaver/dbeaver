061c44a4a6694e56fd7a572356633ab468123945
Serge Rider <serge@jkiss.org>
2018-12-27 16:52:30 +0300

    #4857 SQL Server: default schema (dbo) detection

--- plugins/org.jkiss.dbeaver.ext.mssql/src/org/jkiss/dbeaver/ext/mssql/model/SQLServerDatabase.java
+++ plugins/org.jkiss.dbeaver.ext.mssql/src/org/jkiss/dbeaver/ext/mssql/model/SQLServerDatabase.java
@@ -25,6 +25,7 @@ import org.jkiss.dbeaver.model.DBPRefreshableObject;
 import org.jkiss.dbeaver.model.DBPSaveableObject;
 import org.jkiss.dbeaver.model.DBPSystemObject;
 import org.jkiss.dbeaver.model.DBUtils;
+import org.jkiss.dbeaver.model.exec.DBCSession;
 import org.jkiss.dbeaver.model.exec.jdbc.JDBCPreparedStatement;
 import org.jkiss.dbeaver.model.exec.jdbc.JDBCResultSet;
 import org.jkiss.dbeaver.model.exec.jdbc.JDBCSession;
@@ -35,8 +36,10 @@ import org.jkiss.dbeaver.model.impl.jdbc.cache.JDBCObjectLookupCache;
 import org.jkiss.dbeaver.model.meta.Association;
 import org.jkiss.dbeaver.model.meta.Property;
 import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
+import org.jkiss.dbeaver.model.runtime.VoidProgressMonitor;
 import org.jkiss.dbeaver.model.struct.DBSObject;
 import org.jkiss.dbeaver.model.struct.DBSObjectFilter;
+import org.jkiss.dbeaver.model.struct.DBSObjectSelector;
 import org.jkiss.dbeaver.model.struct.rdb.DBSCatalog;
 import org.jkiss.utils.LongKeyMap;
 
@@ -47,7 +50,7 @@ import java.util.List;
 /**
 * SQL Server database
 */
-public class SQLServerDatabase implements DBSCatalog, DBPSaveableObject, DBPRefreshableObject, DBPSystemObject{
+public class SQLServerDatabase implements DBSCatalog, DBPSaveableObject, DBPRefreshableObject, DBPSystemObject, DBSObjectSelector {
 
     private static final Log log = Log.getLog(SQLServerDatabase.class);
 
@@ -142,6 +145,32 @@ public class SQLServerDatabase implements DBSCatalog, DBPSaveableObject, DBPRefr
         log.debug("Data type '" + typeID + "' not found in database " + getName());
         return null;
     }
+
+    ///////////////////////////////////////////////////////
+    // Schema selector
+
+    @Override
+    public boolean supportsDefaultChange() {
+        return false;
+    }
+
+    @Override
+    public DBSObject getDefaultObject() {
+        return schemaCache.getCachedObject(SQLServerConstants.DEFAULT_SCHEMA_NAME);
+    }
+
+    @Override
+    public void setDefaultObject(DBRProgressMonitor monitor, DBSObject object) throws DBException {
+        throw new DBException("SQL Server doesn't support default schema change");
+    }
+
+    @Override
+    public boolean refreshDefaultObject(DBCSession session) throws DBException {
+        return false;
+    }
+
+    ///////////////////////////////////////////////////////
+    // Caches
 
     private class DataTypeCache extends JDBCObjectCache<SQLServerDatabase, SQLServerDataType> {
 
