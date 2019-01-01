03553cda2d18707c6bcadbd7f046061d11e74cb5
serge-rider <serge@jkiss.org>
2019-01-01 16:14:05 +0300

    #10 SQL Editor extraction: plugin bundle stub

---
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/editors/sql/internal/SQLEditorMessages.java
@@ -0,0 +1,33 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2017 Serge Rider (serge@jkiss.org)
+ * Copyright (C) 2011-2012 Eugene Fradkin (eugene.fradkin@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jkiss.dbeaver.ui.editors.sql.internal;
+
+import org.eclipse.osgi.util.NLS;
+
+public class SQLEditorMessages extends NLS {
+	static final String BUNDLE_NAME = "org.jkiss.dbeaver.ui.editors.sql.internal.SQLEditorMessages"; //$NON-NLS-1$
+
+
+    static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, SQLEditorMessages.class);
+	}
+
+	private SQLEditorMessages() {
+	}
+}
