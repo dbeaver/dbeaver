1a3c7f1d8739ab45f1aa157db3eea8a4eeb11312
Gavin Shaw <gshaw@pivotal.io>
2019-01-09 15:10:13 -0500

    Add `Unlogged` to Greenplum table DDL for Greenplum 6
    - Created a GreenplumTableManager to handle Greenplum tables.
    - Unlogged tables are available in Postgres 9.1 and later.

---
+++ plugins/org.jkiss.dbeaver.ext.greenplum/src/org/jkiss/dbeaver/ext/greenplum/edit/GreenplumTableManager.java
@@ -0,0 +1,52 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2017 Serge Rider (serge@jkiss.org)
+ * Copyright (C) 2019 Dmitriy Dubson (ddubson@pivotal.io)
+ * Copyright (C) 2019 Gavin Shaw (gshaw@pivotal.io)
+ * Copyright (C) 2019 Zach Marcin (zmarcin@pivotal.io)
+ * Copyright (C) 2019 Nikhil Pawar (npawar@pivotal.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jkiss.dbeaver.ext.greenplum.edit;
+
+import org.jkiss.dbeaver.DBException;
+import org.jkiss.dbeaver.ext.greenplum.model.GreenplumTable;
+import org.jkiss.dbeaver.ext.postgresql.edit.PostgreTableManager;
+import org.jkiss.dbeaver.ext.postgresql.model.PostgreTableBase;
+import org.jkiss.dbeaver.model.edit.DBEPersistAction;
+import org.jkiss.dbeaver.model.impl.edit.SQLDatabasePersistAction;
+import org.jkiss.dbeaver.model.messages.ModelMessages;
+import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Greenplum table manager
+ */
+public class GreenplumTableManager extends PostgreTableManager {
+
+    @Override
+    protected void addStructObjectCreateActions(DBRProgressMonitor monitor, List<DBEPersistAction> actions, StructCreateCommand command, Map<String, Object> options) throws DBException {
+        PostgreTableBase tableBase = command.getObject();
+
+        super.addStructObjectCreateActions(monitor, actions, command, options);
+
+        if (tableBase instanceof GreenplumTable && ((GreenplumTable) tableBase).isUnloggedTable()) {
+            actions.set(0,
+                    new SQLDatabasePersistAction(ModelMessages.model_jdbc_create_new_table,
+                            ((GreenplumTable) tableBase).addUnloggedClause(actions.get(0).getScript())));
+        }
+    }
+}
