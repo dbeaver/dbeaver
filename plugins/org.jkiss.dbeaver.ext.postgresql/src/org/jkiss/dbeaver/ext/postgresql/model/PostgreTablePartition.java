83e3b1fe1d095c1f032ce4564feff1176dee26af
serge-rider <serge@jkiss.org>
2019-01-10 21:56:55 +0300

    License header update (2019)
    Former-commit-id: b5121d4a6e8f3f21079920180b0fb14ada6d3349

--- plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/model/PostgreTablePartition.java
+++ plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/model/PostgreTablePartition.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2017 Serge Rider (serge@jkiss.org)
+ * Copyright (C) 2010-2019 Serge Rider (serge@jkiss.org)
  *               2017 Andrew Khitrin   (andrew@jkiss.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
