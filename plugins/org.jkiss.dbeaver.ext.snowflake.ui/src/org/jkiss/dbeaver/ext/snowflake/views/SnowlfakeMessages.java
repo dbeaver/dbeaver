e174a91261422b9c5bb9843031e07afc74be92d3
RiedNyko <RiedNyko@DESKTOP-2UV2M6T>
2019-01-25 23:25:11 +0300

    dbeaver-corp/dbeaver-i18n#25

---
+++ plugins/org.jkiss.dbeaver.ext.snowflake.ui/src/org/jkiss/dbeaver/ext/snowflake/views/SnowlfakeMessages.java
@@ -0,0 +1,26 @@
+package org.jkiss.dbeaver.ext.snowflake.views;
+
+import org.eclipse.osgi.util.NLS;
+
+public class SnowlfakeMessages extends NLS {
+	private static final String BUNDLE_NAME = "org.jkiss.dbeaver.ext.snowflake.views.SnowlfakeResources"; //$NON-NLS-1$
+	public static String label_authenticator;
+	public static String label_click_on_test_connection;
+	public static String label_connection;
+	public static String label_database;
+	public static String label_host;
+	public static String label_password;
+	public static String label_port;
+	public static String label_role;
+	public static String label_schema;
+	public static String label_security;
+	public static String label_user;
+	public static String label_warehouse;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, SnowlfakeMessages.class);
+	}
+
+	private SnowlfakeMessages() {
+	}
+}
