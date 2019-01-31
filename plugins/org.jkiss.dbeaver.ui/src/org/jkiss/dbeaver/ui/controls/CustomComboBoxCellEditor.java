cfae656e0c9720723414887df7e5446f72f1f2f6
Serge Rider <serge@jkiss.org>
2019-01-31 19:16:40 +0300

    #4583 Combo auto-completion in lists

--- plugins/org.jkiss.dbeaver.ui/src/org/jkiss/dbeaver/ui/controls/CustomComboBoxCellEditor.java
+++ plugins/org.jkiss.dbeaver.ui/src/org/jkiss/dbeaver/ui/controls/CustomComboBoxCellEditor.java
@@ -16,6 +16,7 @@
  */
 package org.jkiss.dbeaver.ui.controls;
 
+import org.eclipse.jface.fieldassist.SimpleContentProposalProvider;
 import org.eclipse.jface.viewers.ColumnViewer;
 import org.eclipse.jface.viewers.ComboBoxCellEditor;
 import org.eclipse.swt.SWT;
@@ -25,6 +26,7 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.jkiss.dbeaver.model.DBPNamedObject;
+import org.jkiss.dbeaver.ui.UIUtils;
 import org.jkiss.utils.CommonUtils;
 
 /**
@@ -32,7 +34,7 @@ import org.jkiss.utils.CommonUtils;
  */
 public class CustomComboBoxCellEditor extends ComboBoxCellEditor {
 
-    //private SimpleContentProposalProvider proposalProvider;
+    private SimpleContentProposalProvider proposalProvider;
 
     public CustomComboBoxCellEditor(Composite parent, String[] items) {
         super(parent, items);
@@ -64,9 +66,9 @@ public class CustomComboBoxCellEditor extends ComboBoxCellEditor {
      */
     public void setItems(String[] items) {
         super.setItems(items);
-//        if (proposalProvider != null) {
-//            proposalProvider.setProposals(items);
-//        }
+        if (proposalProvider != null) {
+            proposalProvider.setProposals(items);
+        }
     }
 
     @Override
@@ -81,9 +83,9 @@ public class CustomComboBoxCellEditor extends ComboBoxCellEditor {
         if ((getStyle() & SWT.READ_ONLY) == 0) {
             // Install proposal provider for editable combos
             // In fact it was a bad idea to use proposals in inline combo editors (#2409)
-            //proposalProvider = new SimpleContentProposalProvider(comboBox.getItems());
-            //proposalProvider.setFiltering(true);
-            //UIUtils.installContentProposal(comboBox, new CComboContentAdapter(), proposalProvider, true, true);
+            proposalProvider = new SimpleContentProposalProvider(comboBox.getItems());
+            proposalProvider.setFiltering(true);
+            UIUtils.installContentProposal(comboBox, new CComboContentAdapter(), proposalProvider, true, true);
         }
 
         return comboBox;
