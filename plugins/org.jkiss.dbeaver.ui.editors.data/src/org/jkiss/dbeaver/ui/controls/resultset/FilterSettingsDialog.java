25c645e5154b9e7e1150fddc423658ee46f7a4cc
serge-rider <serge@jkiss.org>
2019-02-03 00:53:19 +0300

    #4011 Filter field rendering fix
    Former-commit-id: d1ff0a5fbfc799c3165c216e258fe6d7d6320014

--- plugins/org.jkiss.dbeaver.ui.editors.data/src/org/jkiss/dbeaver/ui/controls/resultset/FilterSettingsDialog.java
+++ plugins/org.jkiss.dbeaver.ui.editors.data/src/org/jkiss/dbeaver/ui/controls/resultset/FilterSettingsDialog.java
@@ -115,7 +115,7 @@ class FilterSettingsDialog extends HelpEnabledDialog {
             FilteredTree filteredTree = new FilteredTree(columnsGroup, SWT.SINGLE | SWT.FULL_SELECTION | SWT.CHECK, new PatternFilter(), true) {
                 @Override
                 protected TreeViewer doCreateTreeViewer(Composite parent, int style) {
-                    columnsViewer = new CheckboxTreeViewer(columnsGroup, style);
+                    columnsViewer = new CheckboxTreeViewer(parent, style);
                     return columnsViewer;
                 }
             };
@@ -143,7 +143,6 @@ class FilterSettingsDialog extends HelpEnabledDialog {
             final Tree columnsTree = columnsViewer.getTree();
             GridData gd = new GridData(GridData.FILL_BOTH);
             gd.heightHint = 300;
-            //gd.heightHint = 300;
             columnsTree.setLayoutData(gd);
             columnsTree.setHeaderVisible(true);
             columnsTree.setLinesVisible(true);
