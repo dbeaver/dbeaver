880bd6c7da416e3d7540beb6b32e654b3a0387a5
serge-rider <serge@jkiss.org>
2019-02-05 01:11:11 +0300

    #5191 Dashboard model + view manager
    Former-commit-id: cd76e9a47bc7143dd496e2c0a843cbe544da09c9

---
+++ plugins/org.jkiss.dbeaver.ui.dashboard/src/org/jkiss/dbeaver/ui/dashboard/model/DashboardType.java
@@ -0,0 +1,44 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2019 Serge Rider (serge@jkiss.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jkiss.dbeaver.ui.dashboard.model;
+
+/**
+ * Dashboard type.
+ */
+public enum DashboardType {
+
+    HISTOGRAM("Histogram", "Chart showing time series graphs"),
+    BAR("Bar", "Bar chart"),
+    PIE("Pie", "Pie chart"),
+    METER("Meter", "Single value meter");
+
+    private final String title;
+    private final String description;
+
+    DashboardType(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
