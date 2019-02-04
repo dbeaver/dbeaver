9045bcda975b57aac09c3dbd9544aa79e3e9c759
Serge Rider <serge@jkiss.org>
2019-02-04 20:14:25 +0300

    #5191 Dashboard view, commands stubs

---
+++ plugins/org.jkiss.dbeaver.ui.dashboard/src/org/jkiss/dbeaver/ui/dashboard/registry/DashboardDescriptor.java
@@ -0,0 +1,67 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2019 Serge Rider (serge@jkiss.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jkiss.dbeaver.ui.dashboard.registry;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.jkiss.code.NotNull;
+import org.jkiss.dbeaver.model.app.DBPPlatformLanguage;
+import org.jkiss.dbeaver.model.impl.AbstractContextDescriptor;
+
+/**
+ * DashboardDescriptor
+ */
+public class DashboardDescriptor extends AbstractContextDescriptor implements DBPPlatformLanguage
+{
+    public static final String EXTENSION_ID = "org.jkiss.dbeaver.dashboard"; //$NON-NLS-1$
+
+    private final String id;
+    private final String label;
+    private final String description;
+
+    public DashboardDescriptor(
+        IConfigurationElement config)
+    {
+        super(config);
+
+        this.id = config.getAttribute("id");
+        this.label = config.getAttribute("label");
+        this.description = config.getAttribute("description");
+    }
+
+    @NotNull
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @NotNull
+    @Override
+    public String getLabel()
+    {
+        return label;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return id;
+    }
+}
