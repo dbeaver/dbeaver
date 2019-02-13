d6c83986a6c5a3d75ed0faab813644eca69aaad0
serge-rider <serge@jkiss.org>
2019-02-13 23:17:47 +0300

    #4209 Dashboards: map queries model
    Former-commit-id: c3e83bad899c3c9705c48a9e18981ce5ce28f3e9

---
+++ plugins/org.jkiss.dbeaver.ui.dashboard/src/org/jkiss/dbeaver/ui/dashboard/registry/DashboardMapQueryDescriptor.java
@@ -0,0 +1,67 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2019 Serge Rider (serge@jkiss.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jkiss.dbeaver.ui.dashboard.registry;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.jkiss.code.NotNull;
+import org.jkiss.dbeaver.DBException;
+import org.jkiss.dbeaver.model.DBPImage;
+import org.jkiss.dbeaver.model.impl.AbstractContextDescriptor;
+import org.jkiss.dbeaver.ui.dashboard.control.DashboardRenderer;
+import org.jkiss.dbeaver.ui.dashboard.model.DashboardConstants;
+import org.jkiss.dbeaver.ui.dashboard.model.DashboardDataType;
+import org.jkiss.dbeaver.ui.dashboard.model.DashboardViewType;
+import org.jkiss.utils.CommonUtils;
+
+/**
+ * DashboardMapQueryDescriptor
+ */
+public class DashboardMapQueryDescriptor extends AbstractContextDescriptor
+{
+    private String id;
+    private String queryText;
+    private long updatePeriod;
+
+    DashboardMapQueryDescriptor(
+        IConfigurationElement config)
+    {
+        super(config);
+
+        this.id = config.getAttribute("id");
+        this.queryText = config.getValue();
+        this.updatePeriod = CommonUtils.toInt(config.getAttribute("updatePeriod"), DashboardConstants.DEF_DASHBOARD_UPDATE_PERIOD);
+    }
+
+    @NotNull
+    public String getId() {
+        return id;
+    }
+
+    public long getUpdatePeriod() {
+        return updatePeriod;
+    }
+
+    public String getQueryText() {
+        return queryText;
+    }
+
+    @Override
+    public String toString() {
+        return id;
+    }
+
+}
