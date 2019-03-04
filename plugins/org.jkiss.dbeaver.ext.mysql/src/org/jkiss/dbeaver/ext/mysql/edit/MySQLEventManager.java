a24fa91e7b0f16befdbf1542bad3ed214d9e4660
RiedNyko <jbwkz2205@yandex.ru>
2019-03-04 16:41:05 +0300

    Add/Edit event dialog #3761

--- plugins/org.jkiss.dbeaver.ext.mysql/src/org/jkiss/dbeaver/ext/mysql/edit/MySQLEventManager.java
+++ plugins/org.jkiss.dbeaver.ext.mysql/src/org/jkiss/dbeaver/ext/mysql/edit/MySQLEventManager.java
@@ -8,13 +8,11 @@ import org.jkiss.dbeaver.DBException;
 import org.jkiss.dbeaver.ext.mysql.model.MySQLCatalog;
 import org.jkiss.dbeaver.ext.mysql.model.MySQLEvent;
 import org.jkiss.dbeaver.model.DBPDataSource;
-import org.jkiss.dbeaver.model.DBPEvaluationContext;
 import org.jkiss.dbeaver.model.DBUtils;
 import org.jkiss.dbeaver.model.edit.DBECommandContext;
 import org.jkiss.dbeaver.model.edit.DBEPersistAction;
 import org.jkiss.dbeaver.model.impl.DBSObjectCache;
 import org.jkiss.dbeaver.model.impl.edit.SQLDatabasePersistAction;
-import org.jkiss.dbeaver.model.impl.jdbc.JDBCUtils;
 import org.jkiss.dbeaver.model.impl.sql.edit.SQLObjectEditor;
 import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
 
@@ -22,35 +20,33 @@ public class MySQLEventManager extends SQLObjectEditor<MySQLEvent, MySQLCatalog>
 
     @Nullable
     @Override
-	protected MySQLEvent createDatabaseObject(DBRProgressMonitor monitor, DBECommandContext context,
-			MySQLCatalog parent, Object copyFrom) throws DBException {
-		
-		return null;
-	}
+    protected MySQLEvent createDatabaseObject(DBRProgressMonitor monitor, DBECommandContext context, MySQLCatalog parent, Object copyFrom) throws DBException 
+    {
+        return null;
+    }
     
     public DBSObjectCache<MySQLCatalog, MySQLEvent> getObjectsCache(MySQLEvent object)
     {
         return object.getCatalog().getEventCache(); 
     }
 
-	@Override
-	public long getMakerOptions(DBPDataSource dataSource) {
-		return FEATURE_EDITOR_ON_CREATE;
+    @Override
+    public long getMakerOptions(DBPDataSource dataSource) 
+    {
+        return FEATURE_EDITOR_ON_CREATE;
 	}
 
 	
 
-	@Override
-	protected void addObjectCreateActions(DBRProgressMonitor monitor, List<DBEPersistAction> actions, SQLObjectEditor<MySQLEvent, MySQLCatalog>.ObjectCreateCommand command, Map<String, Object> options) 
-	{
+    @Override
+    protected void addObjectCreateActions(DBRProgressMonitor monitor, List<DBEPersistAction> actions, SQLObjectEditor<MySQLEvent, MySQLCatalog>.ObjectCreateCommand command, Map<String, Object> options) {
         final MySQLEvent event = command.getObject();
-        final StringBuilder script = new StringBuilder("CREATE EVENT " + DBUtils.getQuotedIdentifier(event) + "\n" +
-        "ON SCHEDULE AT" + event.getTimeZone() + "INTERVAL" + event.getIntervalValue());
+        final StringBuilder script = new StringBuilder("CREATE EVENT " + DBUtils.getQuotedIdentifier(event));
 
         actions.add(
-            new SQLDatabasePersistAction("Create event", event.getEventBody()) //$NON-NLS-2$
+            new SQLDatabasePersistAction("Create event", script.toString()) //$NON-NLS-2$
         );
-	}
+    }
     @Override
     protected void addObjectModifyActions(DBRProgressMonitor monitor, List<DBEPersistAction> actionList, ObjectChangeCommand command, Map<String, Object> options)
     {
@@ -63,23 +59,12 @@ public class MySQLEventManager extends SQLObjectEditor<MySQLEvent, MySQLCatalog>
     }
 
     
-	@Override
-	protected void addObjectDeleteActions(List<DBEPersistAction> actions,
-			SQLObjectEditor<MySQLEvent, MySQLCatalog>.ObjectDeleteCommand command, Map<String, Object> options) {
+    @Override
+    protected void addObjectDeleteActions(List<DBEPersistAction> actions, SQLObjectEditor<MySQLEvent, MySQLCatalog>.ObjectDeleteCommand command, Map<String, Object> options) {
         actions.add(
-                new SQLDatabasePersistAction("Drop event", "DROP EVENT " + DBUtils.getQuotedIdentifier(command.getObject())) //$NON-NLS-2$
-            );
+            new SQLDatabasePersistAction("Drop event", "DROP EVENT " + DBUtils.getQuotedIdentifier(command.getObject())) //$NON-NLS-2$
+        );
 		
-	}
-    private void appendDatabaseModifiers(MySQLEvent event, StringBuilder script) {
-        if (event.getTimeZone() != null) {
-            script.append("\nON SCHEDULE AT ").append(event.getTimeZone());
-        }
-        if (event.getIntervalValue() != null) {
-            script.append("\nINTERVAL ").append(event.getIntervalValue());
-        }
-        if (event.getIntervalValue() != null) {
-            script.append("\nINTERVAL ").append(event.getIntervalValue());
-        }
     }
+
 }
