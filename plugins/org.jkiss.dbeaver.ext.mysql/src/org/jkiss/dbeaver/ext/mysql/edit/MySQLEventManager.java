6e6d586c4e8673829d2e4b3b602389318be205eb
RiedNyko <jbwkz2205@yandex.ru>
2019-03-05 17:10:10 +0300

    Add/Edit event dialog #3761 fix1
    Former-commit-id: a5bdbe731d04365e9adab28f340a596e28015456

--- plugins/org.jkiss.dbeaver.ext.mysql/src/org/jkiss/dbeaver/ext/mysql/edit/MySQLEventManager.java
+++ plugins/org.jkiss.dbeaver.ext.mysql/src/org/jkiss/dbeaver/ext/mysql/edit/MySQLEventManager.java
@@ -1,7 +1,5 @@
 package org.jkiss.dbeaver.ext.mysql.edit;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Map;
 
@@ -9,8 +7,6 @@ import org.jkiss.code.Nullable;
 import org.jkiss.dbeaver.DBException;
 import org.jkiss.dbeaver.ext.mysql.model.MySQLCatalog;
 import org.jkiss.dbeaver.ext.mysql.model.MySQLEvent;
-import org.jkiss.dbeaver.ext.mysql.model.MySQLProcedure;
-import org.jkiss.dbeaver.model.DBConstants;
 import org.jkiss.dbeaver.model.DBPDataSource;
 import org.jkiss.dbeaver.model.DBUtils;
 import org.jkiss.dbeaver.model.edit.DBECommandContext;
@@ -19,11 +15,9 @@ import org.jkiss.dbeaver.model.impl.DBSObjectCache;
 import org.jkiss.dbeaver.model.impl.edit.SQLDatabasePersistAction;
 import org.jkiss.dbeaver.model.impl.sql.edit.SQLObjectEditor;
 import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
-import org.jkiss.dbeaver.model.sql.SQLUtils;
 import org.jkiss.dbeaver.model.struct.DBSEntityType;
 import org.jkiss.dbeaver.ui.UITask;
 import org.jkiss.dbeaver.ui.editors.object.struct.EntityEditPage;
-import org.jkiss.utils.CommonUtils;
 
 public class MySQLEventManager extends SQLObjectEditor<MySQLEvent, MySQLCatalog> {
 
@@ -42,31 +36,30 @@ public class MySQLEventManager extends SQLObjectEditor<MySQLEvent, MySQLCatalog>
         return new UITask<MySQLEvent>() {
             @Override
             protected MySQLEvent runTask() {
-                EntityEditPage editPage = new EntityEditPage(parent.getDataSource(), DBSEntityType.EVENT);
-                if (!editPage.edit()) {
-                    return null;
-                }
-                MySQLEvent newEvent = new MySQLEvent(parent, editPage.getEntityName());
-                newEvent.setObjectDefinitionText("SELECT 1");
-                return newEvent;
+            	EntityEditPage editPage = new EntityEditPage(parent.getDataSource(), DBSEntityType.EVENT);
+            	if (!editPage.edit()) {
+            		return null;
+            		}
+            	MySQLEvent newEvent = new MySQLEvent(parent, editPage.getEntityName());
+            	newEvent.setObjectDefinitionText("SELECT 1");
+            	return newEvent;
+            	}
+            }.execute();
             }
-        }.execute();
-    }
 
     @Override
     protected void addObjectCreateActions(DBRProgressMonitor monitor, List<DBEPersistAction> actions, SQLObjectEditor<MySQLEvent, MySQLCatalog>.ObjectCreateCommand command, Map<String, Object> options) {
-        final MySQLEvent event = command.getObject();
-        final StringBuilder script = new StringBuilder();
-        try {
-		    script.append(event.getObjectDefinitionText(monitor, options));
-		} catch (DBException e) {
-		    log.error(e);
-		}
+    	final MySQLEvent event = command.getObject();
+    	final StringBuilder script = new StringBuilder();
+    	try {
+    		script.append(event.getObjectDefinitionText(monitor, options));
+        } catch (DBException e) {
+            log.error(e);
+        }
 
         actions.add(new SQLDatabasePersistAction("Create event", script.toString())); // $NON-NLS-2$
-        
-    }
 
+    }
 
     @Override
     protected void addObjectModifyActions(DBRProgressMonitor monitor, List<DBEPersistAction> actionList, ObjectChangeCommand command, Map<String, Object> options) {
@@ -74,8 +67,7 @@ public class MySQLEventManager extends SQLObjectEditor<MySQLEvent, MySQLCatalog>
 
     @Override
     protected void addObjectDeleteActions(List<DBEPersistAction> actions, SQLObjectEditor<MySQLEvent, MySQLCatalog>.ObjectDeleteCommand command, Map<String, Object> options) {
-        actions.add(new SQLDatabasePersistAction("Drop event", "DROP EVENT " + DBUtils.getQuotedIdentifier(command.getObject())) //$NON-NLS-1$
-        );
+        actions.add(new SQLDatabasePersistAction("Drop event", "DROP EVENT " + DBUtils.getQuotedIdentifier(command.getObject())));
 
     }
 
