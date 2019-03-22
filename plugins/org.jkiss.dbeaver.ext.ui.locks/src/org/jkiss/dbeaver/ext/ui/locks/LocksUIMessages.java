ae6b3e7042a9351794fefd5b5714ffd0d726003d
RiedNyko <jbwkz2205@yandex.ru>
2019-03-22 14:25:22 +0300

    dbeaver-corp/dbeaver-i18n#114 fix1

--- plugins/org.jkiss.dbeaver.ext.ui.locks/src/org/jkiss/dbeaver/ext/ui/locks/LocksUIMessages.java
+++ plugins/org.jkiss.dbeaver.ext.ui.locks/src/org/jkiss/dbeaver/ext/ui/locks/LocksUIMessages.java
@@ -6,6 +6,7 @@ public class LocksUIMessages extends NLS {
 	private static final String BUNDLE_NAME = "org.jkiss.dbeaver.ext.ui.locks.LocksUIResources"; //$NON-NLS-1$
 	public static String actions_refresh_control_kill_waiting_session;
 	public static String actions_refresh_control_refresh_locks;
+    public static String create_editor_control_name_lock;
 	static {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, LocksUIMessages.class);
