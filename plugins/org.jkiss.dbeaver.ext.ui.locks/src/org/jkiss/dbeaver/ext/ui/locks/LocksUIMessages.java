27f4bba9ba5024e195d15f73fff3ea7f5155bb34
RiedNyko <jbwkz2205@yandex.ru>
2019-03-22 14:27:22 +0300

    fixed tabs

--- plugins/org.jkiss.dbeaver.ext.ui.locks/src/org/jkiss/dbeaver/ext/ui/locks/LocksUIMessages.java
+++ plugins/org.jkiss.dbeaver.ext.ui.locks/src/org/jkiss/dbeaver/ext/ui/locks/LocksUIMessages.java
@@ -3,15 +3,15 @@ package org.jkiss.dbeaver.ext.ui.locks;
 import org.eclipse.osgi.util.NLS;
 
 public class LocksUIMessages extends NLS {
-	private static final String BUNDLE_NAME = "org.jkiss.dbeaver.ext.ui.locks.LocksUIResources"; //$NON-NLS-1$
-	public static String actions_refresh_control_kill_waiting_session;
-	public static String actions_refresh_control_refresh_locks;
+    private static final String BUNDLE_NAME = "org.jkiss.dbeaver.ext.ui.locks.LocksUIResources"; //$NON-NLS-1$
+    public static String actions_refresh_control_kill_waiting_session;
+    public static String actions_refresh_control_refresh_locks;
     public static String create_editor_control_name_lock;
-	static {
-		// initialize resource bundle
-		NLS.initializeMessages(BUNDLE_NAME, LocksUIMessages.class);
-	}
+    static {
+        // initialize resource bundle
+        NLS.initializeMessages(BUNDLE_NAME, LocksUIMessages.class);
+    }
 
-	private LocksUIMessages() {
-	}
+    private LocksUIMessages() {
+    }
 }
