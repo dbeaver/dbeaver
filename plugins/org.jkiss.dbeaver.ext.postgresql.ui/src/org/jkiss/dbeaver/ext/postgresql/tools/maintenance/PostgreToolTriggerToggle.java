a69879e45680e2b3cf0d77cfc0d7142eb07cb06a
Serge Rider <serge@jkiss.org>
2019-04-12 17:02:36 +0300

    #3374 PG trigger state

--- plugins/org.jkiss.dbeaver.ext.postgresql.ui/src/org/jkiss/dbeaver/ext/postgresql/tools/maintenance/PostgreToolTriggerToggle.java
+++ plugins/org.jkiss.dbeaver.ext.postgresql.ui/src/org/jkiss/dbeaver/ext/postgresql/tools/maintenance/PostgreToolTriggerToggle.java
@@ -16,9 +16,7 @@
  */
 package org.jkiss.dbeaver.ext.postgresql.tools.maintenance;
 
-import java.util.Collection;
-import java.util.List;
-
+import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchPartSite;
@@ -27,44 +25,77 @@ import org.jkiss.dbeaver.DBException;
 import org.jkiss.dbeaver.ext.postgresql.model.PostgreObject;
 import org.jkiss.dbeaver.ext.postgresql.model.PostgreTrigger;
 import org.jkiss.dbeaver.model.DBUtils;
+import org.jkiss.dbeaver.model.exec.DBCException;
+import org.jkiss.dbeaver.model.navigator.DBNDatabaseNode;
 import org.jkiss.dbeaver.model.struct.DBSObject;
+import org.jkiss.dbeaver.runtime.DBWorkbench;
 import org.jkiss.dbeaver.tools.IExternalTool;
+import org.jkiss.dbeaver.ui.UIUtils;
+import org.jkiss.dbeaver.ui.navigator.NavigatorUtils;
 import org.jkiss.utils.CommonUtils;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Collection;
+import java.util.List;
 
 public abstract class PostgreToolTriggerToggle implements IExternalTool {
 
     private boolean isEnable;
 
     protected PostgreToolTriggerToggle(boolean enable) {
-	this.isEnable = enable;
+        this.isEnable = enable;
     }
 
     @Override
     public void execute(IWorkbenchWindow window, IWorkbenchPart activePart, Collection<DBSObject> objects)
-	    throws DBException {
-	List<PostgreTrigger> trigger = CommonUtils.filterCollection(objects, PostgreTrigger.class);
-	if (!trigger.isEmpty()) {
-	    SQLDialog dialog = new SQLDialog(activePart.getSite(), trigger);
-	    dialog.open();
-	}
+        throws DBException
+    {
+        List<PostgreTrigger> triggeList = CommonUtils.filterCollection(objects, PostgreTrigger.class);
+        if (!triggeList.isEmpty()) {
+            SQLDialog dialog = new SQLDialog(activePart.getSite(), triggeList);
+            if (dialog.open() == IDialogConstants.OK_ID) {
+                refreshObjectsState(triggeList);
+            }
+        }
+    }
+
+    private void refreshObjectsState(List<PostgreTrigger> triggeList) {
+        try {
+            UIUtils.runInProgressDialog(monitor -> {
+                for (PostgreTrigger trigger : triggeList) {
+                    try {
+                        DBNDatabaseNode triggerNode = NavigatorUtils.getNodeByObject(trigger);
+                        if (triggerNode != null) {
+                            triggerNode.refreshNode(monitor, PostgreToolTriggerToggle.this);
+                        } else {
+                            trigger.refreshObjectState(monitor);
+                        }
+                    } catch (DBException e) {
+                        throw new InvocationTargetException(e);
+                    }
+                }
+            });
+        } catch (InvocationTargetException e) {
+            DBWorkbench.getPlatformUI().showError("Refresh triggers state", "Error refreshign trigger state", e.getTargetException());
+        }
     }
 
     class SQLDialog extends TableToolDialog {
 
-	public SQLDialog(IWorkbenchPartSite partSite, List<PostgreTrigger> selectedTrigger) {
-	    super(partSite, (isEnable ? "Enable" : "Disable") + " trigger", selectedTrigger);
-	}
+        public SQLDialog(IWorkbenchPartSite partSite, List<PostgreTrigger> selectedTrigger) {
+            super(partSite, (isEnable ? "Enable" : "Disable") + " trigger", selectedTrigger);
+        }
 
-	@Override
-	protected void generateObjectCommand(List<String> lines, PostgreObject object) {
-	    lines.add("ALTER TABLE " + ((PostgreTrigger) object).getTable() + " " + (isEnable ? "ENABLE" : "DISABLE")
-		    + " TRIGGER " + DBUtils.getQuotedIdentifier((PostgreTrigger) object));
-	}
+        @Override
+        protected void generateObjectCommand(List<String> lines, PostgreObject object) {
+            lines.add("ALTER TABLE " + ((PostgreTrigger) object).getTable() + " " + (isEnable ? "ENABLE" : "DISABLE")
+                + " TRIGGER " + DBUtils.getQuotedIdentifier(object));
+        }
 
-	@Override
-	protected void createControls(Composite parent) {
-	    createObjectsSelector(parent);
-	}
+        @Override
+        protected void createControls(Composite parent) {
+            createObjectsSelector(parent);
+        }
     }
 
 }
