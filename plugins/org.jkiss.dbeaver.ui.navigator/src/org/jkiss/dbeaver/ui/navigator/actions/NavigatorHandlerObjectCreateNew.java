17eb981151b5db231516edd28225620dc3f8275a
Serge Rider <serge@jkiss.org>
2019-04-19 18:15:53 +0300

    #4935 Create object menu

--- plugins/org.jkiss.dbeaver.ui.navigator/src/org/jkiss/dbeaver/ui/navigator/actions/NavigatorHandlerObjectCreateNew.java
+++ plugins/org.jkiss.dbeaver.ui.navigator/src/org/jkiss/dbeaver/ui/navigator/actions/NavigatorHandlerObjectCreateNew.java
@@ -18,28 +18,42 @@ package org.jkiss.dbeaver.ui.navigator.actions;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.action.IContributionItem;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.osgi.util.NLS;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchPartSite;
+import org.eclipse.ui.actions.CompoundContributionItem;
 import org.eclipse.ui.commands.IElementUpdater;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.menus.CommandContributionItem;
+import org.eclipse.ui.menus.CommandContributionItemParameter;
 import org.eclipse.ui.menus.UIElement;
-import org.jkiss.dbeaver.ui.internal.UINavigatorMessages;
 import org.jkiss.dbeaver.model.DBIcon;
+import org.jkiss.dbeaver.model.DBPDataSourceContainer;
 import org.jkiss.dbeaver.model.DBPImage;
 import org.jkiss.dbeaver.model.navigator.*;
 import org.jkiss.dbeaver.model.navigator.meta.DBXTreeNode;
+import org.jkiss.dbeaver.ui.ActionUtils;
 import org.jkiss.dbeaver.ui.DBeaverIcons;
 import org.jkiss.dbeaver.ui.UIIcon;
+import org.jkiss.dbeaver.ui.UIUtils;
+import org.jkiss.dbeaver.ui.internal.UINavigatorMessages;
+import org.jkiss.dbeaver.ui.navigator.NavigatorCommands;
 import org.jkiss.dbeaver.ui.navigator.NavigatorUtils;
 import org.jkiss.utils.CommonUtils;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class NavigatorHandlerObjectCreateNew extends NavigatorHandlerObjectCreateBase implements IElementUpdater {
 
     @Override
     public Object execute(ExecutionEvent event) throws ExecutionException {
+        String objectType = event.getParameter(NavigatorCommands.PARAM_OBJECT_TYPE);
+
         final ISelection selection = HandlerUtil.getCurrentSelection(event);
         DBNNode node = NavigatorUtils.getSelectedNode(selection);
         if (node != null) {
@@ -97,5 +111,61 @@ public class NavigatorHandlerObjectCreateNew extends NavigatorHandlerObjectCreat
         return null;
     }
 
+    public static class MenuCreateContributor extends CompoundContributionItem {
+
+        private static final IContributionItem[] EMPTY_MENU = new IContributionItem[0];
+
+        @Override
+        protected IContributionItem[] getContributionItems() {
+
+            IWorkbenchPage activePage = UIUtils.getActiveWorkbenchWindow().getActivePage();
+            IWorkbenchPart activePart = activePage.getActivePart();
+            if (activePart == null) {
+                return EMPTY_MENU;
+            }
+            IWorkbenchPartSite site = activePart.getSite();
+            final ISelectionProvider selectionProvider = site.getSelectionProvider();
+            if (selectionProvider == null) {
+                return EMPTY_MENU;
+            }
+            ISelection selection = selectionProvider.getSelection();
+            if (selection.isEmpty() || !(selection instanceof IStructuredSelection)) {
+                return EMPTY_MENU;
+            }
+
+            List<IContributionItem> createActions = new ArrayList<>();
+
+            Set<Class<?>> addedClasses = new HashSet<>();
+            for (Object element : ((IStructuredSelection) selection).toArray()) {
+                if (!(element instanceof DBNNode)) {
+                    continue;
+                }
+                DBNNode node = (DBNNode) element;
+
+                if (node instanceof DBNDataSource) {
+                    if (!addedClasses.contains(DBNLocalFolder.class)) {
+                        addedClasses.add(DBNLocalFolder.class);
+                        createActions.add(ActionUtils.makeCommandContribution(site, NavigatorCommands.CMD_CREATE_LOCAL_FOLDER));
+                    }
+                    if (!addedClasses.contains(DBNDataSource.class)) {
+                        addedClasses.add(DBNDataSource.class);
+
+                        CommandContributionItemParameter params = new CommandContributionItemParameter(
+                            site,
+                            NavigatorCommands.CMD_OBJECT_CREATE,
+                            NavigatorCommands.CMD_OBJECT_CREATE,
+                            CommandContributionItem.STYLE_PUSH
+                        );
+                        Map<String, String> parameters = new HashMap<>();
+                        parameters.put(NavigatorCommands.PARAM_OBJECT_TYPE, DBPDataSourceContainer.class.getName());
+                        params.parameters = parameters;
+                        createActions.add(new CommandContributionItem(params));
+                    }
+                }
+            }
+
+            return createActions.toArray(new IContributionItem[0]);
+        }
+    }
 
 }
