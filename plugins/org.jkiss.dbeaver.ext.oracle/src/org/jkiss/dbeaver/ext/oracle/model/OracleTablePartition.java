31317a9f85726366b3774287a976cc2c32fbfaa3
RiedNyko <jbwkz2205@yandex.ru>
2019-04-20 16:43:12 +0300

    Oracle Sub partition #4733

--- plugins/org.jkiss.dbeaver.ext.oracle/src/org/jkiss/dbeaver/ext/oracle/model/OracleTablePartition.java
+++ plugins/org.jkiss.dbeaver.ext.oracle/src/org/jkiss/dbeaver/ext/oracle/model/OracleTablePartition.java
@@ -17,19 +17,17 @@
 
 package org.jkiss.dbeaver.ext.oracle.model;
 
+import org.jkiss.dbeaver.DBException;
 import org.jkiss.dbeaver.model.meta.Association;
-import org.jkiss.utils.CommonUtils;
+import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
 
 import java.sql.ResultSet;
 import java.util.Collection;
-import java.util.List;
 
 /**
  * Table partition
  */
 public class OracleTablePartition extends OraclePartitionBase<OracleTablePhysical> {
-
-    private List<OracleTablePartition> subPartitions;
 
     protected OracleTablePartition(
         OracleTablePhysical oracleTable,
@@ -40,19 +38,9 @@ public class OracleTablePartition extends OraclePartitionBase<OracleTablePhysica
     }
 
     @Association
-    public Collection<OracleTablePartition> getSubPartitions()
+    public Collection<OracleTablePartition> getSubPartitions(DBRProgressMonitor monitor) throws DBException
     {
-        return subPartitions;
-    }
-
-    public void setSubPartitions(List<OracleTablePartition> subPartitions)
-    {
-        this.subPartitions = subPartitions;
-    }
-
-    public boolean hasSubPartitions()
-    {
-        return !CommonUtils.isEmpty(subPartitions);
+        return getParentObject().getSubPartitions(monitor, this);
     }
 
 }
--- plugins/org.jkiss.dbeaver.ext.oracle/src/org/jkiss/dbeaver/ext/oracle/model/OracleTablePhysical.java
+++ plugins/org.jkiss.dbeaver.ext.oracle/src/org/jkiss/dbeaver/ext/oracle/model/OracleTablePhysical.java
@@ -179,6 +179,18 @@ public abstract class OracleTablePhysical extends OracleTableBase implements DBS
         }
     }
 
+    @Association
+    public Collection<OracleTablePartition> getSubPartitions(DBRProgressMonitor monitor, OracleTablePartition partition)
+        throws DBException
+    {
+        if (partitionCache == null) {
+            return null;
+        } else {
+            this.partitionCache.getAllObjects(monitor, this);
+            return this.partitionCache.getChildren(monitor, this, partition);
+        }
+    }
+
     @Override
     public DBSObject refreshObject(@NotNull DBRProgressMonitor monitor) throws DBException
     {
