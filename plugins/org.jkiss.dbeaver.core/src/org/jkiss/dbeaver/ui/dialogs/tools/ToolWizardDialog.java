7d6618e02a79051ae4d50ca10dd5beff0cde0ca7
serge-rider <serge@jkiss.org>
2019-04-21 16:35:17 +0300

    #5716 Native client selector in tool wizards dialog
    Former-commit-id: e685b6f05ef7bc3cb260a3bdabf93fea51c72635

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/dialogs/tools/ToolWizardDialog.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/dialogs/tools/ToolWizardDialog.java
@@ -21,17 +21,29 @@ package org.jkiss.dbeaver.ui.dialogs.tools;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.wizard.IWizard;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.jkiss.dbeaver.core.CoreMessages;
+import org.jkiss.dbeaver.model.DBPDataSourceContainer;
+import org.jkiss.dbeaver.model.DBPImage;
 import org.jkiss.dbeaver.ui.dialogs.ActiveWizardDialog;
+import org.jkiss.dbeaver.ui.dialogs.BaseDialog;
+import org.jkiss.dbeaver.ui.dialogs.connection.ClientHomesPanel;
+import org.jkiss.dbeaver.ui.dialogs.connection.ClientHomesSelector;
 
 /**
  * Tool wizard dialog
  */
 public class ToolWizardDialog extends ActiveWizardDialog
 {
+
+    public static final int CLIENT_CONFIG_ID = 1000;
+
     public ToolWizardDialog(IWorkbenchWindow window, IWizard wizard)
     {
         super(window, wizard);
@@ -42,10 +54,78 @@ public class ToolWizardDialog extends ActiveWizardDialog
     @Override
     protected void createButtonsForButtonBar(Composite parent)
     {
+        if (getWizard() instanceof AbstractToolWizard<?, ?>) {
+            boolean nativeClientRequired = ((AbstractToolWizard) getWizard()).isNativeClientHomeRequired();
+            if (nativeClientRequired) {
+                parent.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+
+                Button configButton = createButton(parent, CLIENT_CONFIG_ID, "  Client configuration ...  ", false);
+                configButton.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_BEGINNING));
+
+                Label spacer = new Label(parent, SWT.NONE);
+                spacer.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+
+                ((GridLayout)parent.getLayout()).numColumns++;
+                ((GridLayout)parent.getLayout()).makeColumnsEqualWidth = false;
+            }
+        }
+
         super.createButtonsForButtonBar(parent);
         Button cancelButton = getButton(IDialogConstants.CANCEL_ID);
         cancelButton.setText(IDialogConstants.CLOSE_LABEL);
         Button finishButton = getButton(IDialogConstants.FINISH_ID);
         finishButton.setText(CoreMessages.tools_wizard_dialog_button_start);
     }
+
+    @Override
+    protected void buttonPressed(int buttonId) {
+        if (buttonId == CLIENT_CONFIG_ID) {
+            openClientConfiguration();
+        }
+        super.buttonPressed(buttonId);
+    }
+
+    private void openClientConfiguration() {
+        AbstractToolWizard toolWizard = (AbstractToolWizard) getWizard();
+        DBPDataSourceContainer dataSource = toolWizard.getDataSourceContainer();
+        if (dataSource != null) {
+            NativeClientConfigDialog dialog = new NativeClientConfigDialog(getShell(), dataSource);
+            if (dialog.open() == IDialogConstants.OK_ID) {
+                toolWizard.updateErrorMessage();
+                updateButtons();
+            }
+        }
+    }
+
+    private static class NativeClientConfigDialog extends BaseDialog  {
+
+        private final DBPDataSourceContainer dataSource;
+        private ClientHomesSelector homesSelector;
+
+        public NativeClientConfigDialog(Shell parentShell, DBPDataSourceContainer dataSource) {
+            super(parentShell, "Configure local client for " + dataSource.getName(), dataSource.getDriver().getIcon());
+            this.dataSource = dataSource;
+        }
+
+        @Override
+        protected Composite createDialogArea(Composite parent) {
+            Composite dialogArea = super.createDialogArea(parent);
+
+            homesSelector = new ClientHomesSelector(dialogArea, SWT.NONE, "Native client");
+            homesSelector.populateHomes(dataSource.getDriver(), dataSource.getConnectionConfiguration().getClientHomeId(), false);
+            homesSelector.getPanel().setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+            return dialogArea;
+        }
+
+        @Override
+        protected void okPressed() {
+            String selectedHome = homesSelector.getSelectedHome();
+            dataSource.getConnectionConfiguration().setClientHomeId(selectedHome);
+            dataSource.getRegistry().flushConfig();
+
+            super.okPressed();
+        }
+    }
+
 }
