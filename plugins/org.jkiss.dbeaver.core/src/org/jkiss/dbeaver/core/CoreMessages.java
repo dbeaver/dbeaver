89e633ced2b140363c7c938c5a5f0984797fc098
Serge Rider <serge@jkiss.org>
2019-05-14 17:04:28 +0300

    UI Connection refactoring

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/core/CoreMessages.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/core/CoreMessages.java
@@ -124,7 +124,6 @@ public class CoreMessages extends NLS {
 	public static String dialog_connection_wizard_final_button_events;
 	public static String dialog_connection_wizard_final_checkbox_filter_catalogs;
 	public static String dialog_connection_wizard_final_checkbox_filter_schemas;
-	public static String dialog_connection_wizard_final_checkbox_save_password_locally;
     public static String dialog_connection_wizard_final_checkbox_auto_commit;
 	public static String dialog_connection_wizard_final_checkbox_show_system_objects;
 	public static String dialog_connection_wizard_final_checkbox_show_util_objects;
@@ -455,11 +454,6 @@ public class CoreMessages extends NLS {
 	public static String tools_wizard_page_log_task_progress_log;
 	public static String tools_wizard_page_log_task_started_at;
 	
-	public static String dialog_connection_edit_driver_button;
-	public static String dialog_connection_driver;
-	public static String dialog_connection_advanced_settings;
-	public static String dialog_connection_env_variables_hint;
-
 	public static String editor_file_open_in_explorer;
 	public static String editor_file_copy_path;
 	public static String editor_file_rename;
@@ -617,9 +611,6 @@ public class CoreMessages extends NLS {
 
 	// Connection edit
 	public static String dialog_connection_edit_title;
-	public static String dialog_connection_edit_connection_settings_variables_hint_label;
-	public static String dialog_connection_edit_wizard_conn_conf_general_link;
-	public static String dialog_connection_edit_wizard_conn_conf_network_link;
 
 	public static String dialog_connection_edit_wizard_general;
 	public static String dialog_connection_edit_wizard_general_bootstrap_query_title;
---
+++ plugins/org.jkiss.dbeaver.ui.editors.connection/src/org/jkiss/dbeaver/ui/dialogs/connection/ConnectionPageAbstract.java
@@ -0,0 +1,237 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2019 Serge Rider (serge@jkiss.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jkiss.dbeaver.ui.dialogs.connection;
+
+import org.eclipse.jface.dialogs.DialogPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CLabel;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.jkiss.dbeaver.ModelPreferences;
+import org.jkiss.dbeaver.model.DBPDataSourceContainer;
+import org.jkiss.dbeaver.model.connection.DBPConnectionConfiguration;
+import org.jkiss.dbeaver.model.connection.DBPDriver;
+import org.jkiss.dbeaver.registry.DataSourceDescriptor;
+import org.jkiss.dbeaver.runtime.DBWorkbench;
+import org.jkiss.dbeaver.ui.IDataSourceConnectionEditor;
+import org.jkiss.dbeaver.ui.IDataSourceConnectionEditorSite;
+import org.jkiss.dbeaver.ui.UIUtils;
+import org.jkiss.dbeaver.ui.internal.UIConnectionMessages;
+import org.jkiss.utils.CommonUtils;
+
+/**
+ * ConnectionPageAbstract
+ */
+
+public abstract class ConnectionPageAbstract extends DialogPage implements IDataSourceConnectionEditor
+{
+    protected IDataSourceConnectionEditorSite site;
+    // Driver name
+    protected Text driverText;
+    protected Button savePasswordCheck;
+
+    public IDataSourceConnectionEditorSite getSite() {
+        return site;
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+    }
+
+    @Override
+    public void setSite(IDataSourceConnectionEditorSite site)
+    {
+        this.site = site;
+    }
+
+    protected boolean isCustomURL()
+    {
+        return false;
+    }
+
+    @Override
+    public void loadSettings() {
+        DBPDriver driver = site.getDriver();
+        if (driver != null && driverText != null) {
+            driverText.setText(CommonUtils.toString(driver.getFullName()));
+        }
+
+        if (savePasswordCheck != null) {
+            DataSourceDescriptor dataSource = (DataSourceDescriptor) getSite().getActiveDataSource();
+            if (dataSource != null) {
+                savePasswordCheck.setSelection(dataSource.isSavePassword());
+            } else {
+                savePasswordCheck.setSelection(true);
+            }
+        }
+    }
+
+    @Override
+    public void saveSettings(DBPDataSourceContainer dataSource)
+    {
+        saveConnectionURL(dataSource.getConnectionConfiguration());
+        if (savePasswordCheck != null) {
+            DataSourceDescriptor descriptor = (DataSourceDescriptor) dataSource;
+            descriptor.setSavePassword(savePasswordCheck.getSelection());
+
+            if (!descriptor.isSavePassword()) {
+                descriptor.resetPassword();
+            }
+        }
+    }
+
+    protected void saveConnectionURL(DBPConnectionConfiguration connectionInfo)
+    {
+        if (!isCustomURL()) {
+            connectionInfo.setUrl(
+                site.getDriver().getDataSourceProvider().getConnectionURL(
+                    site.getDriver(),
+                    connectionInfo));
+        }
+    }
+
+    protected void createDriverPanel(Composite parent) {
+        int numColumns = ((GridLayout) parent.getLayout()).numColumns;
+
+        Composite panel = UIUtils.createPlaceholder(parent, 4, 5);
+        GridData gd = new GridData(GridData.FILL_BOTH);
+        gd.horizontalSpan = numColumns;
+        panel.setLayoutData(gd);
+
+        {
+            Composite placeholder = UIUtils.createPlaceholder(panel, 1, 5);
+            gd = new GridData(GridData.FILL_HORIZONTAL | GridData.HORIZONTAL_ALIGN_BEGINNING | GridData.VERTICAL_ALIGN_END);
+            gd.horizontalSpan = 4;
+            gd.grabExcessHorizontalSpace = true;
+            gd.grabExcessVerticalSpace = true;
+            placeholder.setLayoutData(gd);
+
+            if (DBWorkbench.getPlatform().getPreferenceStore().getBoolean(ModelPreferences.CONNECT_USE_ENV_VARS)) {
+                CLabel infoLabel = UIUtils.createInfoLabel(placeholder, UIConnectionMessages.dialog_connection_edit_connection_settings_variables_hint_label);
+                gd = new GridData(GridData.FILL_HORIZONTAL | GridData.HORIZONTAL_ALIGN_BEGINNING | GridData.VERTICAL_ALIGN_END);
+                infoLabel.setLayoutData(gd);
+                infoLabel.setToolTipText(UIConnectionMessages.dialog_connection_env_variables_hint);
+            }
+
+            if (site.isNew()) {
+                Label divLabel = new Label(placeholder, SWT.SEPARATOR | SWT.HORIZONTAL);
+                divLabel.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+                Composite linksComposite = UIUtils.createPlaceholder(placeholder, 2, 2);
+                linksComposite.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+                Label advancedLabel = UIUtils.createControlLabel(linksComposite, UIConnectionMessages.dialog_connection_advanced_settings);
+                advancedLabel.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+                if (!site.getDriver().isEmbedded()) {
+                    Button netConfigLink = new Button(linksComposite, SWT.PUSH);
+                    netConfigLink.setText(UIConnectionMessages.dialog_connection_edit_wizard_conn_conf_network_link);
+                    netConfigLink.addSelectionListener(new SelectionAdapter() {
+                        @Override
+                        public void widgetSelected(SelectionEvent e) {
+                            site.openSettingsPage("ConnectionPageNetwork");
+                        }
+                    });
+                    netConfigLink.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_END));
+                    //((GridLayout)linksComposite.getLayout()).numColumns++;
+                }
+                {
+                    if (!site.getDriver().isEmbedded()) {
+                        UIUtils.createEmptyLabel(linksComposite, 1, 1);
+                    }
+                    Button netConfigLink = new Button(linksComposite, SWT.PUSH);
+                    netConfigLink.setText(UIConnectionMessages.dialog_connection_edit_wizard_conn_conf_general_link);
+                    netConfigLink.addSelectionListener(new SelectionAdapter() {
+                        @Override
+                        public void widgetSelected(SelectionEvent e) {
+                            site.openSettingsPage("ConnectionPageGeneral");
+                        }
+                    });
+                    netConfigLink.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_END));
+                    //((GridLayout)linksComposite.getLayout()).numColumns++;
+                }
+            }
+
+        }
+
+        Label divLabel = new Label(panel, SWT.SEPARATOR | SWT.HORIZONTAL);
+        gd = new GridData(GridData.FILL_HORIZONTAL);
+        gd.horizontalSpan = 4;
+        divLabel.setLayoutData(gd);
+
+        Label driverLabel = new Label(panel, SWT.NONE);
+        driverLabel.setText(UIConnectionMessages.dialog_connection_driver);
+        gd = new GridData(GridData.HORIZONTAL_ALIGN_END);
+        driverLabel.setLayoutData(gd);
+
+        driverText = new Text(panel, SWT.READ_ONLY);
+        gd = new GridData(GridData.FILL_HORIZONTAL);
+        gd.grabExcessHorizontalSpace = true;
+        gd.horizontalSpan = 2;
+        //gd.widthHint = 200;
+        driverText.setLayoutData(gd);
+
+        Button driverButton = new Button(panel, SWT.PUSH);
+        driverButton.setText(UIConnectionMessages.dialog_connection_edit_driver_button);
+        gd = new GridData(GridData.HORIZONTAL_ALIGN_END);
+        driverButton.setLayoutData(gd);
+        driverButton.addSelectionListener(new SelectionListener()
+        {
+            @Override
+            public void widgetSelected(SelectionEvent e)
+            {
+                if (site.openDriverEditor()) {
+                    updateDriverInfo(site.getDriver());
+                }
+            }
+
+            @Override
+            public void widgetDefaultSelected(SelectionEvent e)
+            {
+            }
+        });
+    }
+
+    protected void updateDriverInfo(DBPDriver driver) {
+
+    }
+
+    protected void createSavePasswordButton(Composite parent) {
+        createSavePasswordButton(parent, 1);
+    }
+
+    protected void createSavePasswordButton(Composite parent, int hSpan) {
+        DataSourceDescriptor dataSource = (DataSourceDescriptor)getSite().getActiveDataSource();
+        savePasswordCheck = UIUtils.createCheckbox(parent,
+            UIConnectionMessages.dialog_connection_wizard_final_checkbox_save_password_locally,
+            dataSource == null || dataSource.isSavePassword());
+        GridData gd = new GridData(GridData.HORIZONTAL_ALIGN_BEGINNING);
+        if (hSpan > 1) {
+            gd.horizontalSpan = hSpan;
+        }
+        savePasswordCheck.setLayoutData(gd);
+    }
+
+}
