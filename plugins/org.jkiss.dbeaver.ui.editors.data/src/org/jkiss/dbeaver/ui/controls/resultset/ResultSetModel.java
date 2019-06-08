15de47a592c62aa522922539ef035b513c5adb88
serge-rider <serge@jkiss.org>
2019-06-08 17:56:09 +0300

    RSV: gradient colors for whole line fix. Color for out-of-range values fix.
    Former-commit-id: ddccd42ec5d641ff9b64e3b3eda8319178459f2b

--- plugins/org.jkiss.dbeaver.ui.editors.data/src/org/jkiss/dbeaver/ui/controls/resultset/ResultSetModel.java
+++ plugins/org.jkiss.dbeaver.ui.editors.data/src/org/jkiss/dbeaver/ui/controls/resultset/ResultSetModel.java
@@ -638,6 +638,12 @@ public class ResultSetModel {
                                         foreground = acs.colorForeground;
                                         RGB rowRGB = ResultSetUtils.makeGradientValue(acs.colorBackground.getRGB(), acs.colorBackground2.getRGB(), minValue, maxValue, value);
                                         background = UIUtils.getSharedColor(rowRGB);
+                                    } else if (value < minValue) {
+                                        foreground = acs.colorForeground;
+                                        background = acs.colorBackground;
+                                    } else if (value > maxValue) {
+                                        foreground = acs.colorForeground2;
+                                        background = acs.colorBackground2;
                                     }
                                 }
                             }
@@ -656,8 +662,8 @@ public class ResultSetModel {
                                 row.colorInfo = colorInfo;
                             }
                             if (!acs.singleColumn) {
-                                colorInfo.rowForeground = acs.colorForeground;
-                                colorInfo.rowBackground = acs.colorBackground;
+                                colorInfo.rowForeground = foreground;
+                                colorInfo.rowBackground = background;
                             } else {
                                 // Single column color
                                 if (foreground != null) {
