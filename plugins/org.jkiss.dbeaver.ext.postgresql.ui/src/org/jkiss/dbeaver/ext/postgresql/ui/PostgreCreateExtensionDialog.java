5dfe9992a06d2ef0aa81c7538a09d6d524a4cb8f
Andrew Khitrin <ahitrin@gmail.com>
2019-07-05 13:31:16 +0300

    #6051 Extensions presentation UI

--- plugins/org.jkiss.dbeaver.ext.postgresql.ui/src/org/jkiss/dbeaver/ext/postgresql/ui/PostgreCreateExtensionDialog.java
+++ plugins/org.jkiss.dbeaver.ext.postgresql.ui/src/org/jkiss/dbeaver/ext/postgresql/ui/PostgreCreateExtensionDialog.java
@@ -19,14 +19,22 @@ package org.jkiss.dbeaver.ext.postgresql.ui;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.viewers.CellLabelProvider;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.ViewerCell;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ControlAdapter;
+import org.eclipse.swt.events.ControlEvent;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.Text;
 import org.jkiss.dbeaver.DBException;
 import org.jkiss.dbeaver.ext.postgresql.PostgreMessages;
@@ -36,6 +44,8 @@ import org.jkiss.dbeaver.ext.postgresql.model.PostgreExtension;
 import org.jkiss.dbeaver.model.runtime.AbstractJob;
 import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
 import org.jkiss.dbeaver.ui.UIUtils;
+import org.jkiss.dbeaver.ui.controls.ListContentProvider;
+import org.jkiss.dbeaver.ui.controls.ViewerColumnController;
 import org.jkiss.dbeaver.ui.dialogs.BaseDialog;
 import org.jkiss.dbeaver.utils.GeneralUtils;
 
@@ -50,9 +60,8 @@ public class PostgreCreateExtensionDialog extends BaseDialog
     private PostgreAvailableExtension extension;
     private final PostgreExtension newextension;
     private List<PostgreSchema> allSchemas;
-    private String name;
     private PostgreSchema schema;
-    private List<PostgreAvailableExtension> installed;
+    private TableViewer extTable;
 
     public PostgreCreateExtensionDialog(Shell parentShell, PostgreExtension extension) {
         super(parentShell, PostgreMessages.dialog_create_extension_title, null);
@@ -69,29 +78,75 @@ public class PostgreCreateExtensionDialog extends BaseDialog
 
         final Composite group = new Composite(composite, SWT.NONE);
         group.setLayout(new GridLayout(2, false));
-        group.setLayoutData(new GridData(GridData.FILL_BOTH));
+        GridData gd = new GridData(GridData.FILL_BOTH);
+        gd.widthHint = 600;
+        gd.heightHint = 200;
+        gd.verticalIndent = 0;
+        gd.horizontalIndent = 0;
+        group.setLayoutData(gd);
 
         final Text databaseText = UIUtils.createLabelText(group, "Database", newextension.getDatabase().getName(), SWT.BORDER | SWT.READ_ONLY); //$NON-NLS-2$
-
-        final Combo extensionCombo = UIUtils.createLabelCombo(group, PostgreMessages.dialog_create_extension_name, SWT.BORDER | SWT.DROP_DOWN | SWT.READ_ONLY);
         final Combo schemaCombo = UIUtils.createLabelCombo(group, PostgreMessages.dialog_create_extension_schema, SWT.BORDER | SWT.DROP_DOWN | SWT.READ_ONLY);
+        final Label lblExtension = UIUtils.createLabel(group, PostgreMessages.dialog_create_extension_name);
+        
+        extTable = new TableViewer(group, SWT.BORDER | SWT.UNDERLINE_SINGLE | SWT.V_SCROLL | SWT.H_SCROLL | SWT.FULL_SELECTION);
 
-        schemaCombo.addSelectionListener(new SelectionAdapter() {
+        {
+            final Table table = extTable.getTable();
+            table.setLayoutData(new GridData(GridData.FILL_BOTH));
+            table.setLinesVisible(true);
+            table.setHeaderVisible(true);
+            table.addControlListener(new ControlAdapter() {
+                @Override
+                public void controlResized(ControlEvent e)
+                {
+                    UIUtils.packColumns(table);
+                    UIUtils.maxTableColumnsWidth(table);
+                    table.removeControlListener(this);
+                }
+            });
+            
+         }
+        
+        ViewerColumnController columnController = new ViewerColumnController("AvailabelExtensionDialog", extTable);
+        columnController.addColumn(PostgreMessages.dialog_create_extension_column_name, null, SWT.NONE, true, true, new CellLabelProvider() {
+            @Override
+            public void update(ViewerCell cell)
+            {
+                cell.setText(((PostgreAvailableExtension) cell.getElement()).getName());
+            }});
+        columnController.addColumn(PostgreMessages.dialog_create_extension_column_version, null, SWT.NONE, true, true, new CellLabelProvider() {
+            @Override
+            public void update(ViewerCell cell)
+            {
+                cell.setText(((PostgreAvailableExtension) cell.getElement()).getVersion());
+            }});
+        columnController.addColumn(PostgreMessages.dialog_create_extension_column_description, null, SWT.NONE, true, true, new CellLabelProvider() {
+            @Override
+            public void update(ViewerCell cell)
+            {
+                cell.setText(((PostgreAvailableExtension) cell.getElement()).getDescription());
+            }});
+        columnController.createColumns();
+        
+        extTable.addSelectionChangedListener(event -> {
+            IStructuredSelection selection = (IStructuredSelection) event.getSelection();
+            if (!selection.isEmpty()) {
+                extension = (PostgreAvailableExtension) selection.getFirstElement();//installed.get(extensionCombo.getSelectionIndex());
+                checkEnabled();
+            }
+
+        });
+        extTable.setContentProvider(new ListContentProvider());
+       
+         schemaCombo.addSelectionListener(new SelectionAdapter() {
             @Override
             public void widgetSelected(SelectionEvent e) {
                 schema = allSchemas.get(schemaCombo.getSelectionIndex());
                 checkEnabled();
             }
         });
-        
-        extensionCombo.addSelectionListener(new SelectionAdapter() {
-            @Override
-            public void widgetSelected(SelectionEvent e) {
-                extension = installed.get(extensionCombo.getSelectionIndex());
-                extensionCombo.setToolTipText(installed.get(extensionCombo.getSelectionIndex()).getDescription());
-                checkEnabled();
-            }
-        });       
+              
 
         new AbstractJob("Load schemas") {
 
@@ -116,11 +171,9 @@ public class PostgreCreateExtensionDialog extends BaseDialog
             @Override
             protected IStatus run(DBRProgressMonitor monitor) {
                 try {
-                    installed = new ArrayList<>(newextension.getDatabase().getAvailableExtensions(monitor));
-                    UIUtils.syncExec(() -> {
-                        for (PostgreAvailableExtension e : installed) {
-                            extensionCombo.add(e.getName());
-                        }
+                    final List<PostgreAvailableExtension> installed = new ArrayList<>(newextension.getDatabase().getAvailableExtensions(monitor));
+                    UIUtils.syncExec(() -> {                        
+                        extTable.setInput(installed);
                     });
                 } catch (DBException e) {
                     return GeneralUtils.makeExceptionStatus(e);
