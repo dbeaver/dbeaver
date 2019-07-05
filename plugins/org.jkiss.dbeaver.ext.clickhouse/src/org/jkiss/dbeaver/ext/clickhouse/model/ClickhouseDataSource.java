1c0191eb7ff93290715b47ced7a2b12065c42507
serge@jkiss.org <serge@jkiss.org>
2019-07-05 16:05:44 +0200

    #6201 Clickhouse: support type aliases

--- plugins/org.jkiss.dbeaver.ext.clickhouse/src/org/jkiss/dbeaver/ext/clickhouse/model/ClickhouseDataSource.java
+++ plugins/org.jkiss.dbeaver.ext.clickhouse/src/org/jkiss/dbeaver/ext/clickhouse/model/ClickhouseDataSource.java
@@ -16,12 +16,17 @@
  */
 package org.jkiss.dbeaver.ext.clickhouse.model;
 
+import org.jkiss.code.NotNull;
+import org.jkiss.code.Nullable;
 import org.jkiss.dbeaver.DBException;
 import org.jkiss.dbeaver.Log;
 import org.jkiss.dbeaver.ext.generic.model.GenericDataSource;
 import org.jkiss.dbeaver.ext.generic.model.meta.GenericMetaModel;
 import org.jkiss.dbeaver.model.DBPDataSourceContainer;
 import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
+import org.jkiss.dbeaver.model.struct.DBSDataType;
+
+import java.util.Date;
 
 public class ClickhouseDataSource extends GenericDataSource {
 
@@ -33,4 +38,26 @@ public class ClickhouseDataSource extends GenericDataSource {
         super(monitor, container, metaModel, new ClickhouseSQLDialect());
     }
 
+    @Nullable
+    @Override
+    public DBSDataType resolveDataType(@NotNull DBRProgressMonitor monitor, @NotNull String typeFullName) throws DBException {
+        if (String.class.getName().equals(typeFullName)) {
+            return resolveDataType(monitor, "String");
+        } else if (Integer.class.getName().equals(typeFullName)) {
+            return resolveDataType(monitor, "Int32");
+        } else if (Log.class.getName().equals(typeFullName)) {
+            return resolveDataType(monitor, "Int64");
+        } else if (Short.class.getName().equals(typeFullName)) {
+            return resolveDataType(monitor, "Int16");
+        } else if (Byte.class.getName().equals(typeFullName)) {
+            return resolveDataType(monitor, "Int8");
+        } else if (Float.class.getName().equals(typeFullName)) {
+            return resolveDataType(monitor, "Float32");
+        } else if (Double.class.getName().equals(typeFullName)) {
+            return resolveDataType(monitor, "Float64");
+        } else if (Date.class.getName().equals(typeFullName)) {
+            return resolveDataType(monitor, "DateTime");
+        }
+        return super.resolveDataType(monitor, typeFullName);
+    }
 }
