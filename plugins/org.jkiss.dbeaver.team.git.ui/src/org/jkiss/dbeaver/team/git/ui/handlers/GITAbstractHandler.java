67ee534f53e4ee4469dc6a0af7cacfa1448b7d09
serge@jkiss.org <serge@jkiss.org>
2019-07-11 13:23:31 +0200

    Git: commit handler fix
    Former-commit-id: 9fff070c30e7935b1422e42ccf41e59d461f16bc

--- plugins/org.jkiss.dbeaver.team.git.ui/src/org/jkiss/dbeaver/team/git/ui/handlers/GITAbstractHandler.java
+++ plugins/org.jkiss.dbeaver.team.git.ui/src/org/jkiss/dbeaver/team/git/ui/handlers/GITAbstractHandler.java
@@ -19,41 +19,69 @@ package org.jkiss.dbeaver.team.git.ui.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.egit.ui.internal.operations.GitScopeUtil;
 import org.eclipse.egit.ui.internal.selection.SelectionUtils;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.jkiss.dbeaver.Log;
+import org.jkiss.dbeaver.ui.editors.EditorUtils;
 
 public abstract class GITAbstractHandler extends AbstractHandler {
+
+    private static final Log log = Log.getLog(GITAbstractHandler.class);
 
     protected IResource[] getResourcesInScope(ExecutionEvent event)
         throws ExecutionException {
         try {
-            IResource[] selectedResources = getSelectedResources(event);
-            if (selectedResources.length > 0) {
-                IWorkbenchPart part = HandlerUtil.getActivePart(event);
-                return GitScopeUtil.getRelatedChanges(part, selectedResources);
+            IWorkbenchPart activePart = HandlerUtil.getActivePart(event);
+            if (activePart instanceof IEditorPart) {
+                IFile editorFile = EditorUtils.getFileFromInput(((IEditorPart) activePart).getEditorInput());
+                if (editorFile != null) {
+                    return new IResource[] { editorFile };
+                } else {
+                    return new IResource[0];
+                }
             } else {
-                return new IResource[0];
+                IResource[] selectedResources = getSelectedResources(event);
+                if (selectedResources.length > 0) {
+                    IWorkbenchPart part = HandlerUtil.getActivePart(event);
+                    return GitScopeUtil.getRelatedChanges(part, selectedResources);
+                } else {
+                    return new IResource[0];
+                }
             }
-        } catch (InterruptedException e) {
+        } catch (Exception e) {
             // ignore, we will not show the commit dialog in case the user
             // cancels the scope operation
+            log.error(e);
             return null;
         }
     }
 
     protected Repository getRepository(boolean warn, ExecutionEvent event) {
-        IStructuredSelection selection = HandlerUtil.getCurrentStructuredSelection(event);
-        if (warn) {
-            Shell shell = HandlerUtil.getActiveShell(event);
-            return SelectionUtils.getRepositoryOrWarn(selection, shell);
+        Shell shell = HandlerUtil.getActiveShell(event);
+        IWorkbenchPart activePart = HandlerUtil.getActivePart(event);
+        if (activePart instanceof IEditorPart) {
+            IFile editorFile = EditorUtils.getFileFromInput(((IEditorPart) activePart).getEditorInput());
+            if (editorFile != null) {
+                return SelectionUtils.getRepositoryOrWarn(new StructuredSelection(editorFile), shell);
+            } else {
+                return null;
+            }
         } else {
-            return SelectionUtils.getRepository(selection);
+            IStructuredSelection selection = HandlerUtil.getCurrentStructuredSelection(event);
+            if (warn) {
+                return SelectionUtils.getRepositoryOrWarn(selection, shell);
+            } else {
+                return SelectionUtils.getRepository(selection);
+            }
         }
     }
 
@@ -63,6 +91,17 @@ public abstract class GITAbstractHandler extends AbstractHandler {
     }
 
     protected Repository[] getRepositories(ExecutionEvent event) {
+        IWorkbenchPart activePart = HandlerUtil.getActivePart(event);
+        if (activePart instanceof IEditorPart) {
+            IFile editorFile = EditorUtils.getFileFromInput(((IEditorPart) activePart).getEditorInput());
+            if (editorFile != null) {
+                Repository repository = SelectionUtils.getRepository(new StructuredSelection(editorFile));
+                if (repository != null) {
+                    return new Repository[] { repository };
+                }
+            }
+            return null;
+        }
         IStructuredSelection selection = HandlerUtil.getCurrentStructuredSelection(event);
         return SelectionUtils.getRepositories(selection);
     }
