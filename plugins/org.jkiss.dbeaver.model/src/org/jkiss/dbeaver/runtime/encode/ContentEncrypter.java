33f18e93b9af2ddbcca6f4ad4e4b735d7cda21a2
Serge Rider <serge@jkiss.org>
2019-07-17 15:41:37 +0200

    Utilities model
    Former-commit-id: e9e8bc1c099561fadfc12d80b74e9be624974e3a

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/runtime/encode/ContentEncrypter.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/runtime/encode/ContentEncrypter.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jkiss.dbeaver.model.impl.app;
+package org.jkiss.dbeaver.runtime.encode;
 
 import org.jkiss.utils.IOUtils;
 
