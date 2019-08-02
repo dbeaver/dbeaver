c0cdda2ac0c410ffcb6615035f62a0f70977bdc6
loro2 <51793898+loro2@users.noreply.github.com>
2019-08-02 10:00:21 +0200

    Add StringBuilder option to Copy as Source panel
    Former-commit-id: ad69f4f8f69cfb9ff1906bb9f0801132200613bb

--- plugins/org.jkiss.dbeaver.ui.editors.sql/src/org/jkiss/dbeaver/ui/editors/sql/convert/impl/CPPSQLConverter.java
+++ plugins/org.jkiss.dbeaver.ui.editors.sql/src/org/jkiss/dbeaver/ui/editors/sql/convert/impl/CPPSQLConverter.java
@@ -19,13 +19,15 @@ package org.jkiss.dbeaver.ui.editors.sql.convert.impl;
 
 import org.jkiss.utils.CommonUtils;
 
+import java.util.Map;
+
 /**
  * CPPSQLConverter
  */
 public class CPPSQLConverter extends SourceCodeSQLConverter {
 
     @Override
-    protected void convertSourceLines(StringBuilder result, String[] sourceLines, String lineDelimiter) {
+    protected void convertSourceLines(StringBuilder result, String[] sourceLines, String lineDelimiter, Map<String, Object> options) {
         for (int i = 0; i < sourceLines.length; i++) {
             String line = sourceLines[i];
             result.append('"').append(CommonUtils.escapeJavaString(line)).append(lineDelimiter).append('"');
--- plugins/org.jkiss.dbeaver.ui.editors.sql/src/org/jkiss/dbeaver/ui/editors/sql/convert/impl/DelphiSQLConverter.java
+++ plugins/org.jkiss.dbeaver.ui.editors.sql/src/org/jkiss/dbeaver/ui/editors/sql/convert/impl/DelphiSQLConverter.java
@@ -19,13 +19,15 @@ package org.jkiss.dbeaver.ui.editors.sql.convert.impl;
 
 import org.jkiss.utils.CommonUtils;
 
+import java.util.Map;
+
 /**
  * DelphiSQLConverter
  */
 public class DelphiSQLConverter extends SourceCodeSQLConverter {
 
     @Override
-    protected void convertSourceLines(StringBuilder result, String[] sourceLines, String lineDelimiter) {
+    protected void convertSourceLines(StringBuilder result, String[] sourceLines, String lineDelimiter, Map<String, Object> options) {
         boolean trailingLineFeed = lineDelimiter.startsWith("#");
         for (int i = 0; i < sourceLines.length; i++) {
             String line = sourceLines[i];
