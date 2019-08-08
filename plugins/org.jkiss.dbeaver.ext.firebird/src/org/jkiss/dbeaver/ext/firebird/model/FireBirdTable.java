e1dd899c2c6ec4891f56cb193e052127e866e912
Serge Rider <serge@jkiss.org>
2019-08-08 17:25:24 +0200

    Annotations

--- plugins/org.jkiss.dbeaver.ext.firebird/src/org/jkiss/dbeaver/ext/firebird/model/FireBirdTable.java
+++ plugins/org.jkiss.dbeaver.ext.firebird/src/org/jkiss/dbeaver/ext/firebird/model/FireBirdTable.java
@@ -16,6 +16,7 @@
  */
 package org.jkiss.dbeaver.ext.firebird.model;
 
+import org.jkiss.code.NotNull;
 import org.jkiss.code.Nullable;
 import org.jkiss.dbeaver.DBException;
 import org.jkiss.dbeaver.ext.generic.model.GenericStructContainer;
@@ -43,7 +44,7 @@ public class FireBirdTable extends GenericTable implements DBPNamedObject2 {
     }
 
     @Override
-    public synchronized List<FireBirdTableColumn> getAttributes(DBRProgressMonitor monitor) throws DBException {
+    public synchronized List<FireBirdTableColumn> getAttributes(@NotNull DBRProgressMonitor monitor) throws DBException {
         Collection<? extends GenericTableColumn> childColumns = super.getAttributes(monitor);
         if (childColumns == null) {
             return Collections.emptyList();
