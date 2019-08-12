794f1ac8c0d1a7d317a353576e35cd7fe9a303a3
Serge Rider <serge@jkiss.org>
2019-08-12 11:33:15 +0200

    Filter value dialog fix

--- plugins/org.jkiss.dbeaver.ui.editors.data/src/org/jkiss/dbeaver/ui/controls/resultset/valuefilter/FilterValueEditDialog.java
+++ plugins/org.jkiss.dbeaver.ui.editors.data/src/org/jkiss/dbeaver/ui/controls/resultset/valuefilter/FilterValueEditDialog.java
@@ -114,12 +114,14 @@ public class FilterValueEditDialog extends BaseDialog{
         if (handler.editor == null) {
         	handler.textControl = new Text(editorPlaceholder, SWT.BORDER | SWT.WRAP | SWT.V_SCROLL);
         	handler.textControl.setText("");
+/*
             GridData gd = new GridData(GridData.FILL_BOTH);
             gd.widthHint = 300;
             gd.heightHint = 300;
             gd.minimumHeight = 100;
             gd.minimumWidth = 100;
             handler.textControl.setLayoutData(gd);
+*/
         }
     }
 
