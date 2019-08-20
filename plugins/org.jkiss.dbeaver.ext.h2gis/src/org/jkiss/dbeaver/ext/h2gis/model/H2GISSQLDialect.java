a5c257a12ed11e428a7f0c66f89183d5f73462df
ebocher <erwan.bocher@univ-ubs.fr>
2019-08-20 21:17:35 +0200

    Clean up. Add doc

--- plugins/org.jkiss.dbeaver.ext.h2gis/src/org/jkiss/dbeaver/ext/h2gis/model/H2GISSQLDialect.java
+++ plugins/org.jkiss.dbeaver.ext.h2gis/src/org/jkiss/dbeaver/ext/h2gis/model/H2GISSQLDialect.java
@@ -1,6 +1,9 @@
 /*
- * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2019 Serge Rider (serge@jkiss.org)
+ * H2GIS ecplise plugin to register a H2GIS spatial database to 
+ * DBeaver, the  Universal Database Manager
+ *
+ * For more information, please consult: <http://www.h2gis.org/>
+ * or contact directly: info_at_h2gis.org
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +16,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 package org.jkiss.dbeaver.ext.h2gis.model;
 
@@ -20,12 +24,18 @@ import org.jkiss.dbeaver.ext.generic.model.GenericSQLDialect;
 import org.jkiss.dbeaver.model.exec.jdbc.JDBCDatabaseMetaData;
 import org.jkiss.dbeaver.model.impl.jdbc.JDBCDataSource;
 
+
+/**
+ * Used to create the H2GIS SQL dialect 
+ *
+ * @author Erwan Bocher, CNRS
+ * @author Serge Rider (serge@jkiss.org)
+ */
 public class H2GISSQLDialect extends GenericSQLDialect {
 
     public H2GISSQLDialect() {
         super("H2GIS");
     }
-
 
     public void initDriverSettings(JDBCDataSource dataSource, JDBCDatabaseMetaData metaData) {
         super.initDriverSettings(dataSource, metaData);
