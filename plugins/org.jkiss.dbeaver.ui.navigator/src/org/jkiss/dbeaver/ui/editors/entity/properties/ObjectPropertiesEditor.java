ae8bdec89f37a79088d9ef9644d1e02fa5a46eb2
serge-rider <serge@jkiss.org>
2019-08-20 09:38:53 +0300

    Entity editor init fix

--- plugins/org.jkiss.dbeaver.ui.navigator/src/org/jkiss/dbeaver/ui/editors/entity/properties/ObjectPropertiesEditor.java
+++ plugins/org.jkiss.dbeaver.ui.navigator/src/org/jkiss/dbeaver/ui/editors/entity/properties/ObjectPropertiesEditor.java
@@ -253,7 +253,7 @@ public class ObjectPropertiesEditor extends AbstractDatabaseObjectEditor<DBSObje
 
 //        if (propsPlaceholder != null) {
             Point propsSize = propsPlaceholder.computeSize(SWT.DEFAULT, SWT.DEFAULT, true);
-            Point sashSize = getParentSize(sashForm);
+            Point sashSize = sashForm.getParent().getSize();
             if (sashSize.x > 0 && sashSize.y > 0) {
                 float ratio = (float) propsSize.y / (float) sashSize.y;
                 int propsRatio = Math.min(1000, (int) (1000 * ratio));
