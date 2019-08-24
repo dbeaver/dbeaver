e5b521650894af362ae4e42c3806e857dc8aaeef
serge-rider <serge@jkiss.org>
2019-08-24 11:16:06 +0300

    Attribute selector: show only ROWID pseudo attributes

--- plugins/org.jkiss.dbeaver.ui.editors.base/src/org/jkiss/dbeaver/ui/editors/object/struct/AttributesSelectorPage.java
+++ plugins/org.jkiss.dbeaver.ui.editors.base/src/org/jkiss/dbeaver/ui/editors/object/struct/AttributesSelectorPage.java
@@ -250,7 +250,7 @@ public abstract class AttributesSelectorPage extends BaseObjectEditPage {
             protected IStatus run(DBRProgressMonitor monitor) {
                 try {
                     for (DBSEntityAttribute attr : CommonUtils.safeCollection(entity.getAttributes(monitor))) {
-                        if (isShowHiddenAttributes() || !DBUtils.isHiddenObject(attr)) {
+                        if (isShowHiddenAttributes() || !DBUtils.isHiddenObject(attr) || DBUtils.isRowIdAttribute(attr)) {
                             attrList.add(attr);
                         }
                     }
