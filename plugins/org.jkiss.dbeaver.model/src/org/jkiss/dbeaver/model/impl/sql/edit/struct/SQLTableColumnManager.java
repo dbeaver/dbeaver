e23e2b4cfe31a902b0dab159e393aa945eabc06d
serge-rider <serge@jkiss.org>
2019-09-02 21:22:34 +0300

    #6662 Drop column DDL fix
    Former-commit-id: efa424f129b0d92a43d4d537297fb864353da578

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/impl/sql/edit/struct/SQLTableColumnManager.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/impl/sql/edit/struct/SQLTableColumnManager.java
@@ -164,7 +164,7 @@ public abstract class SQLTableColumnManager<OBJECT_TYPE extends DBSEntityAttribu
     {
         actions.add(
             new SQLDatabasePersistAction(
-                ModelMessages.model_jdbc_drop_table_column, "ALTER TABLE " + DBUtils.getObjectFullName(command.getObject(), DBPEvaluationContext.DDL) + //$NON-NLS-2$
+                ModelMessages.model_jdbc_drop_table_column, "ALTER TABLE " + DBUtils.getObjectFullName(command.getObject().getParentObject(), DBPEvaluationContext.DDL) + //$NON-NLS-2$
                     " DROP " + (hasDDLFeature(command.getObject(), DDL_FEATURE_OMIT_COLUMN_CLAUSE_IN_DROP) ? "" : "COLUMN ") + DBUtils.getQuotedIdentifier(command.getObject())) //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
         );
     }
