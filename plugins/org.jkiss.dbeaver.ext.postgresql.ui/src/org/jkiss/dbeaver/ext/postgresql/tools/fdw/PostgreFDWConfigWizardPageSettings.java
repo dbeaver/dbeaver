a53cc9dd7e8a8a53d0bceefdec6860f3be61804b
serge-rider <serge@jkiss.org>
2019-09-15 12:27:13 +0300

    #6745 FDW wizard stub

---
+++ plugins/org.jkiss.dbeaver.ext.postgresql.ui/src/org/jkiss/dbeaver/ext/postgresql/tools/fdw/PostgreFDWConfigWizardPageSettings.java
@@ -0,0 +1,92 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2019 Serge Rider (serge@jkiss.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jkiss.dbeaver.ext.postgresql.tools.fdw;
+
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Listener;
+import org.jkiss.dbeaver.ui.UIUtils;
+import org.jkiss.dbeaver.ui.controls.TextWithOpenFile;
+import org.jkiss.dbeaver.ui.dialogs.ActiveWizardPage;
+
+
+class PostgreFDWConfigWizardPageSettings extends ActiveWizardPage<PostgreFDWConfigWizard>
+{
+
+    private TextWithOpenFile inputFileText;
+    private Combo formatCombo;
+    private Button cleanFirstButton;
+
+    protected PostgreFDWConfigWizardPageSettings(PostgreFDWConfigWizard wizard)
+    {
+        super("Settings");
+        setTitle("Configure foreign data wrappers");
+        setDescription("Choose which databases/tables you need to configure");
+    }
+
+    @Override
+    public boolean isPageComplete()
+    {
+        return super.isPageComplete();
+    }
+
+    @Override
+    public void createControl(Composite parent)
+    {
+        Composite composite = UIUtils.createPlaceholder(parent, 1);
+
+        Listener updateListener = event -> updateState();
+
+/*
+        Group formatGroup = UIUtils.createControlGroup(composite, PostgreMessages.wizard_restore_page_setting_label_setting, 2, GridData.FILL_HORIZONTAL, 0);
+        formatCombo = UIUtils.createLabelCombo(formatGroup, PostgreMessages.wizard_restore_page_setting_label_format, SWT.DROP_DOWN | SWT.READ_ONLY);
+        formatCombo.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_BEGINNING));
+        for (PostgreBackupWizard.ExportFormat format : PostgreBackupWizard.ExportFormat.values()) {
+            formatCombo.add(format.getTitle());
+        }
+        formatCombo.select(wizard.format.ordinal());
+        formatCombo.addListener(SWT.Selection, updateListener);
+
+        cleanFirstButton = UIUtils.createCheckbox(formatGroup,
+        	PostgreMessages.wizard_restore_page_setting_btn_clean_first,
+            null,
+            false,
+            2
+        );
+        cleanFirstButton.addListener(SWT.Selection, updateListener);
+
+        Group inputGroup = UIUtils.createControlGroup(composite, PostgreMessages.wizard_restore_page_setting_label_input, 2, GridData.FILL_HORIZONTAL, 0);
+        UIUtils.createControlLabel(inputGroup, PostgreMessages.wizard_restore_page_setting_label_backup_file);
+        inputFileText = new TextWithOpenFile(inputGroup, PostgreMessages.wizard_restore_page_setting_label_choose_backup_file, new String[] {"*.backup","*"});
+        inputFileText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+        inputFileText.getTextControl().addListener(SWT.Modify, updateListener);
+
+        createExtraArgsInput(inputGroup);
+
+        createSecurityGroup(composite);
+*/
+
+        setControl(composite);
+    }
+
+    protected void updateState()
+    {
+        getContainer().updateButtons();
+    }
+
+}
