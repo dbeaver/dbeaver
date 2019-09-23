c6ae1693cdf6f3fe2a2b8cc8b16c506a8e42f591
Serge Rider <serge@jkiss.org>
2019-09-23 15:27:18 +0300

    #6803 SQL parameters binding dialog: show query preview
    Former-commit-id: d4b55fa9933dcf688aa319b6fe11a406b785b099

--- plugins/org.jkiss.dbeaver.ui.editors.sql/src/org/jkiss/dbeaver/ui/editors/sql/dialogs/SQLQueryParameterBindDialog.java
+++ plugins/org.jkiss.dbeaver.ui.editors.sql/src/org/jkiss/dbeaver/ui/editors/sql/dialogs/SQLQueryParameterBindDialog.java
@@ -20,14 +20,21 @@ import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.jface.dialogs.StatusDialog;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.SashForm;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.*;
+import org.eclipse.ui.IWorkbenchPartSite;
+import org.jkiss.dbeaver.Log;
 import org.jkiss.dbeaver.model.DBIcon;
+import org.jkiss.dbeaver.model.impl.DataSourceContextProvider;
 import org.jkiss.dbeaver.model.sql.SQLQuery;
 import org.jkiss.dbeaver.model.sql.SQLQueryParameter;
+import org.jkiss.dbeaver.runtime.DBWorkbench;
+import org.jkiss.dbeaver.runtime.ui.UIServiceSQL;
 import org.jkiss.dbeaver.ui.DBeaverIcons;
 import org.jkiss.dbeaver.ui.UIUtils;
 import org.jkiss.dbeaver.ui.controls.CustomTableEditor;
@@ -46,9 +53,11 @@ import java.util.List;
 public class SQLQueryParameterBindDialog extends StatusDialog {
 
     private static final String DIALOG_ID = "DBeaver.SQLQueryParameterBindDialog";//$NON-NLS-1$
-
     private static final String PARAM_HIDE_IF_SET = "PARAM_HIDE_IF_SET";//$NON-NLS-1$
 
+    private static final Log log = Log.getLog(SQLQueryParameterBindDialog.class);
+
+    private IWorkbenchPartSite site;
     private SQLQuery query;
     private List<SQLQueryParameter> parameters;
     private final Map<String, List<SQLQueryParameter>> dupParameters = new HashMap<>();
@@ -57,9 +66,10 @@ public class SQLQueryParameterBindDialog extends StatusDialog {
     private Button hideIfSetCheck;
     private Table paramTable;
 
-    public SQLQueryParameterBindDialog(Shell shell, SQLQuery query, List<SQLQueryParameter> parameters)
+    public SQLQueryParameterBindDialog(IWorkbenchPartSite site, SQLQuery query, List<SQLQueryParameter> parameters)
     {
-        super(shell);
+        super(site.getShell());
+        this.site = site;
         this.query = query;
         this.parameters = parameters;
 
@@ -98,77 +108,104 @@ public class SQLQueryParameterBindDialog extends StatusDialog {
         getShell().setText(SQLEditorMessages.dialog_sql_param_title);
         final Composite composite = (Composite)super.createDialogArea(parent);
 
-        paramTable = new Table(composite, SWT.SINGLE | SWT.FULL_SELECTION | SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
-        final GridData gd = new GridData(GridData.FILL_BOTH);
-        gd.widthHint = 400;
-        gd.heightHint = 200;
-        paramTable.setLayoutData(gd);
-        paramTable.setHeaderVisible(true);
-        paramTable.setLinesVisible(true);
+        SashForm sash = new SashForm(composite, SWT.HORIZONTAL);
+        sash.setLayoutData(new GridData(GridData.FILL_BOTH));
 
-        final TableColumn indexColumn = UIUtils.createTableColumn(paramTable, SWT.LEFT, "#");
-        indexColumn.addListener(SWT.Selection, new TableColumnSortListener(paramTable, 0));
-        indexColumn.setWidth(40);
-        final TableColumn nameColumn = UIUtils.createTableColumn(paramTable, SWT.LEFT, SQLEditorMessages.dialog_sql_param_column_name);
-        nameColumn.addListener(SWT.Selection, new TableColumnSortListener(paramTable, 1));
-        nameColumn.setWidth(100);
-        final TableColumn valueColumn = UIUtils.createTableColumn(paramTable, SWT.LEFT, SQLEditorMessages.dialog_sql_param_column_value);
-        valueColumn.setWidth(200);
+        {
+            final Composite paramsComposite = UIUtils.createComposite(sash, 1);
 
-        fillParameterList(isHideIfSet());
+            paramTable = new Table(paramsComposite, SWT.SINGLE | SWT.FULL_SELECTION | SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
+            final GridData gd = new GridData(GridData.FILL_BOTH);
+            gd.widthHint = 400;
+            gd.heightHint = 200;
+            paramTable.setLayoutData(gd);
+            paramTable.setHeaderVisible(true);
+            paramTable.setLinesVisible(true);
 
-        final CustomTableEditor tableEditor = new CustomTableEditor(paramTable) {
-            {
-                firstTraverseIndex = 2;
-                lastTraverseIndex = 2;
-                editOnEnter = false;
-            }
-            @Override
-            protected Control createEditor(Table table, int index, TableItem item) {
-                if (index != 2) {
-                    return null;
+            final TableColumn indexColumn = UIUtils.createTableColumn(paramTable, SWT.LEFT, "#");
+            indexColumn.addListener(SWT.Selection, new TableColumnSortListener(paramTable, 0));
+            indexColumn.setWidth(40);
+            final TableColumn nameColumn = UIUtils.createTableColumn(paramTable, SWT.LEFT, SQLEditorMessages.dialog_sql_param_column_name);
+            nameColumn.addListener(SWT.Selection, new TableColumnSortListener(paramTable, 1));
+            nameColumn.setWidth(100);
+            final TableColumn valueColumn = UIUtils.createTableColumn(paramTable, SWT.LEFT, SQLEditorMessages.dialog_sql_param_column_value);
+            valueColumn.setWidth(200);
+
+            fillParameterList(isHideIfSet());
+
+            final CustomTableEditor tableEditor = new CustomTableEditor(paramTable) {
+                {
+                    firstTraverseIndex = 2;
+                    lastTraverseIndex = 2;
+                    editOnEnter = false;
                 }
-                SQLQueryParameter param = (SQLQueryParameter) item.getData();
-                Text editor = new Text(table, SWT.BORDER);
-                editor.setText(CommonUtils.notEmpty(param.getValue()));
-                editor.selectAll();
 
-                editor.addTraverseListener(e -> {
-                    if (e.detail == SWT.TRAVERSE_RETURN && (e.stateMask & SWT.CTRL) == SWT.CTRL) {
-                        UIUtils.asyncExec(SQLQueryParameterBindDialog.this::okPressed);
+                @Override
+                protected Control createEditor(Table table, int index, TableItem item) {
+                    if (index != 2) {
+                        return null;
                     }
-                });
+                    SQLQueryParameter param = (SQLQueryParameter) item.getData();
+                    Text editor = new Text(table, SWT.BORDER);
+                    editor.setText(CommonUtils.notEmpty(param.getValue()));
+                    editor.selectAll();
 
-                return editor;
-            }
-            @Override
-            protected void saveEditorValue(Control control, int index, TableItem item) {
-                SQLQueryParameter param = (SQLQueryParameter) item.getData();
-                String newValue = ((Text) control).getText();
-                item.setText(2, newValue);
+                    editor.addTraverseListener(e -> {
+                        if (e.detail == SWT.TRAVERSE_RETURN && (e.stateMask & SWT.CTRL) == SWT.CTRL) {
+                            UIUtils.asyncExec(SQLQueryParameterBindDialog.this::okPressed);
+                        }
+                    });
 
-                param.setValue(newValue);
-                if (param.isNamed()) {
-                    final List<SQLQueryParameter> dups = dupParameters.get(param.getName());
-                    if (dups != null) {
-                        for (SQLQueryParameter dup : dups) {
-                            dup.setValue(newValue);
+                    return editor;
+                }
+
+                @Override
+                protected void saveEditorValue(Control control, int index, TableItem item) {
+                    SQLQueryParameter param = (SQLQueryParameter) item.getData();
+                    String newValue = ((Text) control).getText();
+                    item.setText(2, newValue);
+
+                    param.setValue(newValue);
+                    if (param.isNamed()) {
+                        final List<SQLQueryParameter> dups = dupParameters.get(param.getName());
+                        if (dups != null) {
+                            for (SQLQueryParameter dup : dups) {
+                                dup.setValue(newValue);
+                            }
                         }
                     }
+
+                    savedParamValues.put(
+                        param.getName().toUpperCase(Locale.ENGLISH),
+                        new SQLQueryParameterRegistry.ParameterInfo(param.getName(), newValue));
                 }
+            };
 
-                savedParamValues.put(
-                    param.getName().toUpperCase(Locale.ENGLISH),
-                    new SQLQueryParameterRegistry.ParameterInfo(param.getName(), newValue));
+            if (!parameters.isEmpty()) {
+                UIUtils.asyncExec(() -> {
+                    paramTable.select(0);
+                    tableEditor.showEditor(paramTable.getItem(0), 2);
+                });
             }
-        };
-
-        if (!parameters.isEmpty()) {
-            UIUtils.asyncExec(() -> {
-                paramTable.select(0);
-                tableEditor.showEditor(paramTable.getItem(0), 2);
-            });
         }
+        {
+            final Composite queryComposite = new Composite(sash, SWT.BORDER);
+            queryComposite.setLayout(new FillLayout());
+
+            try {
+                DBWorkbench.getService(UIServiceSQL.class).createSQLPanel(
+                    site,
+                    queryComposite,
+                    new DataSourceContextProvider(query.getDataSource()),
+                    "Query preview",
+                    false,
+                    query.getText());
+            } catch (Exception e) {
+                log.error(e);
+            }
+        }
+
+        sash.setWeights(new int[] { 600, 400 });
 
         hideIfSetCheck = UIUtils.createCheckbox(composite,
             SQLEditorMessages.dialog_sql_param_hide_checkbox, SQLEditorMessages.dialog_sql_param_hide_checkbox_tip,
