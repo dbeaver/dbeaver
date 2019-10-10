ba9bae8547bcbeae7cec9b3b45b38125c175634d
Serge Rider <serge@jkiss.org>
2019-10-10 16:03:32 +0300

    #6959 SQL parameters binding: use modeless dialog (allow copy from editor/results)

--- plugins/org.jkiss.dbeaver.ui.editors.sql/src/org/jkiss/dbeaver/ui/editors/sql/dialogs/SQLQueryParameterBindDialog.java
+++ plugins/org.jkiss.dbeaver.ui.editors.sql/src/org/jkiss/dbeaver/ui/editors/sql/dialogs/SQLQueryParameterBindDialog.java
@@ -74,6 +74,7 @@ public class SQLQueryParameterBindDialog extends StatusDialog {
     public SQLQueryParameterBindDialog(IWorkbenchPartSite site, SQLQuery query, List<SQLQueryParameter> parameters)
     {
         super(site.getShell());
+        setShellStyle(SWT.CLOSE | SWT.TITLE | SWT.BORDER | SWT.RESIZE | getDefaultOrientation());
         this.site = site;
         StringWriter dummyWriter = new StringWriter();
         this.queryContext = new SQLScriptContext(null, new DataSourceContextProvider(query.getDataSource()), null, dummyWriter);
