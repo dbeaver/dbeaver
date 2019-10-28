91ebecaabb64580ff797bbc51bee2807dc043d46
Serge Rider <serge@jkiss.org>
2019-10-28 18:05:10 +0300

    #7074 MySQL: respect algorithm in view definition
    Former-commit-id: 2dfe3f245844b9edf5021808d7278cf1f27ac312

--- plugins/org.jkiss.dbeaver.ext.mysql/src/org/jkiss/dbeaver/ext/mysql/model/MySQLDialect.java
+++ plugins/org.jkiss.dbeaver.ext.mysql/src/org/jkiss/dbeaver/ext/mysql/model/MySQLDialect.java
@@ -45,6 +45,7 @@ class MySQLDialect extends JDBCSQLDialect {
         "AUTO_INCREMENT",
         "DATABASES",
         "COLUMNS",
+        "ALGORITHM"
     };
 
     public static final String[][] MYSQL_QUOTE_STRINGS = {
