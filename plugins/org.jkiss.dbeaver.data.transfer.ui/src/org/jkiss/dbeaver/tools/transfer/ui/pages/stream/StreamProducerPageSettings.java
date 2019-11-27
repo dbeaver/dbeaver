91738379af7b9e8cfa6e0a48927a693a8e3c7f53
Serge Rider <serge@jkiss.org>
2019-11-27 21:12:05 +0300

    NPE fix

--- plugins/org.jkiss.dbeaver.data.transfer.ui/src/org/jkiss/dbeaver/tools/transfer/ui/pages/stream/StreamProducerPageSettings.java
+++ plugins/org.jkiss.dbeaver.data.transfer.ui/src/org/jkiss/dbeaver/tools/transfer/ui/pages/stream/StreamProducerPageSettings.java
@@ -26,6 +26,7 @@ import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableItem;
 import org.jkiss.dbeaver.model.DBPEvaluationContext;
 import org.jkiss.dbeaver.model.DBUtils;
+import org.jkiss.dbeaver.model.preferences.DBPPropertyDescriptor;
 import org.jkiss.dbeaver.runtime.properties.PropertySourceCustom;
 import org.jkiss.dbeaver.tools.transfer.DataTransferPipe;
 import org.jkiss.dbeaver.tools.transfer.DataTransferSettings;
@@ -146,8 +147,9 @@ public class StreamProducerPageSettings extends ActiveWizardPage<DataTransferWiz
     @Override
     public void activatePage() {
         DataTransferProcessorDescriptor processor = getProducerProcessor();
+        DBPPropertyDescriptor[] properties = processor == null ? new DBPPropertyDescriptor[0] : processor.getProperties();
         propertySource = new PropertySourceCustom(
-            processor.getProperties(),
+            properties,
             getWizard().getSettings().getProcessorProperties());
         propsEditor.loadProperties(propertySource);
 
