6ab57b365a9b2cc47a84a256b77cacd889ec3971
Serge Rider <serge@jkiss.org>
2019-12-04 11:44:43 +0300

    #7353 PG: double dollar rule fix (allow digits)

--- plugins/org.jkiss.dbeaver.ext.postgresql.ui/src/org/jkiss/dbeaver/ext/postgresql/ui/sql/PostgreDollarQuoteRule.java
+++ plugins/org.jkiss.dbeaver.ext.postgresql.ui/src/org/jkiss/dbeaver/ext/postgresql/ui/sql/PostgreDollarQuoteRule.java
@@ -51,7 +51,7 @@ class PostgreDollarQuoteRule implements IRule {
                     }
                     return blockToken;
                 }
-            } while (Character.isLetter(c) || c == '_');
+            } while (Character.isLetterOrDigit(c) || c == '_');
 
             for (int i = 0; i < charsRead; i++) {
                 scanner.unread();
