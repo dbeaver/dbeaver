21dad04bfd782967a012c372b9f22966c1af3612
riednyko <jbwkz2205@yandex.ru>
2019-12-26 11:46:17 +0300

    Added swtbot test class

---
+++ test/org.jkiss.dbeaver.ext.test/src/org/jkiss/dbeaver/ext/test/swtbot/SWTbotTest.java
@@ -0,0 +1,31 @@
+package org.jkiss.dbeaver.ext.test.swtbot;
+
+import static org.eclipse.swtbot.swt.finder.waits.Conditions.shellCloses;
+
+import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+public class SWTbotTest {
+    private static SWTWorkbenchBot bot;
+    @BeforeClass
+    public static void initBot() {
+        bot = new SWTWorkbenchBot();	
+    }
+    
+    @AfterClass
+    public static void afterClass() {
+    	bot.resetWorkbench();
+    }
+ 
+    @Test
+    public void testSampleMenu() {
+
+
+    }
+    
+}
