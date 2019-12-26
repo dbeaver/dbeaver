940d69a53dbb2edc8ba2a6d104987b13593c484a
riednyko <jbwkz2205@yandex.ru>
2019-12-26 12:05:33 +0300

    Added class SWTbotTest

---
+++ test/org.jkiss.dbeaver.ext.test/src/org/jkiss/dbeaver/ext/test/swtbot/SWTbotTest.java
@@ -0,0 +1,32 @@
+package org.jkiss.dbeaver.ext.test.swtbot;
+
+import static org.eclipse.swtbot.swt.finder.waits.Conditions.shellCloses;
+
+import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+public class SWTbotTest {
+    private static SWTWorkbenchBot bot;
+    @BeforeClass
+    public static void initBot() {
+        bot = new SWTWorkbenchBot();	
+    }
+    
+    @AfterClass
+    public static void afterClass() {
+    	bot.resetWorkbench();
+    }
+ 
+    @Test
+    public void testSampleMenu() {
+		bot.toolbarDropDownButtonWithTooltip("Новое соединение").menuItem("PostgreSQL").click();
+		bot.tabItem("Общее").activate();
+		bot.button("Finish").click();
+    }
+    
+}
