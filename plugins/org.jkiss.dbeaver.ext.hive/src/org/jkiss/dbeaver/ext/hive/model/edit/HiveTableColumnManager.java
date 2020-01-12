9038b4d72f36aa10929d18bcac4f062575ab6bd9
Serge Rider <serge@jkiss.org>
2020-01-12 16:22:16 +0300

    #7529 Hive column manager
    Former-commit-id: 9d42533cbd88a67fa2c2ee91c4cf5d20a256f2d5

---
+++ plugins/org.jkiss.dbeaver.ext.hive/src/org/jkiss/dbeaver/ext/hive/model/edit/HiveTableColumnManager.java
@@ -0,0 +1,38 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2019 Serge Rider (serge@jkiss.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jkiss.dbeaver.ext.hive.model.edit;
+
+import org.jkiss.dbeaver.ext.generic.edit.GenericTableColumnManager;
+import org.jkiss.dbeaver.ext.generic.model.GenericTableBase;
+import org.jkiss.dbeaver.ext.generic.model.GenericTableColumn;
+import org.jkiss.dbeaver.model.impl.edit.DBECommandAbstract;
+import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
+
+import java.util.Map;
+
+/**
+ * HiveTableColumnManager
+ */
+public class HiveTableColumnManager extends GenericTableColumnManager {
+
+    @Override
+    public StringBuilder getNestedDeclaration(DBRProgressMonitor monitor, GenericTableBase owner, DBECommandAbstract<GenericTableColumn> command, Map<String, Object> options) {
+        StringBuilder decl = super.getNestedDeclaration(monitor, owner, command, options);
+        return decl;
+    }
+
+}
