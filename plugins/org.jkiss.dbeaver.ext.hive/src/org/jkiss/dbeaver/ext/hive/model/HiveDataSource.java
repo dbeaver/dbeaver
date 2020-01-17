798cf70870cd07f99543a8696024a6276272895a
Serge Rider <serge@jkiss.org>
2020-01-17 22:39:50 +0300

    Apache Hive server/driver version detection

--- plugins/org.jkiss.dbeaver.ext.hive/src/org/jkiss/dbeaver/ext/hive/model/HiveDataSource.java
+++ plugins/org.jkiss.dbeaver.ext.hive/src/org/jkiss/dbeaver/ext/hive/model/HiveDataSource.java
@@ -23,6 +23,8 @@ import org.jkiss.dbeaver.ext.generic.model.GenericDataSource;
 import org.jkiss.dbeaver.ext.generic.model.meta.GenericMetaModel;
 import org.jkiss.dbeaver.ext.hive.model.jdbc.HiveJdbcFactory;
 import org.jkiss.dbeaver.model.DBPDataSourceContainer;
+import org.jkiss.dbeaver.model.DBPDataSourceInfo;
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCDatabaseMetaData;
 import org.jkiss.dbeaver.model.exec.jdbc.JDBCFactory;
 import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
 import org.jkiss.dbeaver.model.sql.SQLState;
@@ -35,6 +37,11 @@ public class HiveDataSource extends GenericDataSource {
         throws DBException
     {
         super(monitor, container, metaModel, new HiveSQLDialect());
+    }
+
+    @Override
+    protected DBPDataSourceInfo createDataSourceInfo(DBRProgressMonitor monitor, @NotNull JDBCDatabaseMetaData metaData) {
+        return new HiveDataSourceInfo(monitor, this, metaData);
     }
 
     @NotNull
