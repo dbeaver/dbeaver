d2479422fc90ab8106e9dfb0453895f3edbb0094
Serge Rider <serge@jkiss.org>
2020-01-27 14:52:17 +0300

    SQL job statistics fix
    Former-commit-id: 174618e83c6fdebace06c998df5b949589acc3ed

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/impl/AsyncServerOutputReader.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/impl/AsyncServerOutputReader.java
@@ -16,14 +16,19 @@
  */
 package org.jkiss.dbeaver.model.impl;
 
+import org.jkiss.code.NotNull;
 import org.jkiss.dbeaver.model.exec.DBCException;
 import org.jkiss.dbeaver.model.exec.DBCExecutionContext;
 import org.jkiss.dbeaver.model.exec.DBCStatement;
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCSession;
 import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
 import org.jkiss.dbeaver.model.sql.SQLQueryResult;
 
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.sql.SQLWarning;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class AsyncServerOutputReader extends DefaultServerOutputReader {
         @Override
@@ -32,10 +37,19 @@ public class AsyncServerOutputReader extends DefaultServerOutputReader {
         }
 
         @Override
-        public void readServerOutput(DBRProgressMonitor monitor, DBCExecutionContext context, SQLQueryResult queryResult, DBCStatement statement, PrintWriter output) throws DBCException {
+        public void readServerOutput(@NotNull DBRProgressMonitor monitor, @NotNull DBCExecutionContext context, SQLQueryResult queryResult, DBCStatement statement, @NotNull PrintWriter output) throws DBCException {
             if (statement == null) {
                 super.readServerOutput(monitor, context, queryResult, null, output);
             } else {
+                try {
+                    SQLWarning connWarning = ((JDBCSession) statement.getSession()).getWarnings();
+                    if (connWarning != null) {
+                        dumpWarnings(output, Collections.singletonList(connWarning));
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
                 Throwable[] statementWarnings = statement.getStatementWarnings();
                 if (statementWarnings != null && statementWarnings.length > 0) {
                     dumpWarnings(output, Arrays.asList(statementWarnings));
--- plugins/org.jkiss.dbeaver.ui.editors.sql/src/org/jkiss/dbeaver/ui/editors/sql/execute/SQLQueryJob.java
+++ plugins/org.jkiss.dbeaver.ui.editors.sql/src/org/jkiss/dbeaver/ui/editors/sql/execute/SQLQueryJob.java
@@ -606,14 +606,11 @@ public class SQLQueryJob extends DataSourceJob
         } else {
             // Single statement
             long updateCount = statistics.getRowsUpdated();
-            if (updateCount >= 0) {
-                fakeResultSet.addColumn("Updated Rows", DBPDataKind.NUMERIC);
-                fakeResultSet.addColumn("Query", DBPDataKind.STRING);
-                fakeResultSet.addColumn("Finish time", DBPDataKind.DATETIME);
-                fakeResultSet.addRow(updateCount, query.getText(), new Date());
-            } else {
-                fakeResultSet.addColumn("Result", DBPDataKind.NUMERIC);
-            }
+            fakeResultSet.addColumn("Updated Rows", DBPDataKind.NUMERIC);
+            fakeResultSet.addColumn("Query", DBPDataKind.STRING);
+            fakeResultSet.addColumn("Finish time", DBPDataKind.DATETIME);
+            fakeResultSet.addRow(updateCount, query.getText(), new Date());
+
             executeResult.setResultSetName("Result");
         }
         fetchQueryData(session, fakeResultSet, resultInfo, executeResult, dataReceiver, false);
