47e946731f04fb5ecfc5c06330464c4ad748b102
Serge Rider <serge@jkiss.org>
2020-02-08 20:19:29 +0300

    #7443 PG: exclude primary key indexes from index list

--- plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/edit/PostgreTableManagerBase.java
+++ plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/edit/PostgreTableManagerBase.java
@@ -31,7 +31,6 @@ import org.jkiss.dbeaver.model.impl.sql.edit.struct.SQLTableManager;
 import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
 import org.jkiss.dbeaver.model.sql.SQLUtils;
 import org.jkiss.dbeaver.model.struct.DBSEntityAssociation;
-import org.jkiss.dbeaver.model.struct.rdb.DBSTableIndex;
 import org.jkiss.utils.CommonUtils;
 
 import java.util.Collection;
@@ -124,8 +123,4 @@ public abstract class PostgreTableManagerBase extends SQLTableManager<PostgreTab
         }
     }
 
-    @Override
-    protected boolean isIncludeIndexInDDL(DBSTableIndex index) {
-        return !((PostgreIndex)index).isPrimaryKeyIndex() && super.isIncludeIndexInDDL(index);
-    }
 }
