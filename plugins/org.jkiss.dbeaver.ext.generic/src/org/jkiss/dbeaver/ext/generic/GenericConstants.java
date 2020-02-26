c54eadf195a9b81650880fb2595c974c4504feba
Serge Rider <serge@jkiss.org>
2020-02-26 16:32:52 +0300

    #7948 Brackets in DROP config

--- plugins/org.jkiss.dbeaver.ext.generic/src/org/jkiss/dbeaver/ext/generic/GenericConstants.java
+++ plugins/org.jkiss.dbeaver.ext.generic/src/org/jkiss/dbeaver/ext/generic/GenericConstants.java
@@ -51,6 +51,7 @@ public class GenericConstants {
     public static final String PARAM_STRING_ESCAPE_CHAR = "string-escape-char";
     public static final String PARAM_EMBEDDED = "embedded";
     public static final String PARAM_DDL_DROP_COLUMN_SHORT = "ddl-drop-column-short";
+    public static final String PARAM_DDL_DROP_COLUMN_BRACKETS = "ddl-drop-column-brackets";
     public static final String PARAM_LEGACY_DIALECT = "legacy-sql-dialect";
     public static final String PARAM_QUOTE_RESERVED_WORDS = "quote-reserved-words";
     public static final String PARAM_USE_SEARCH_STRING_ESCAPE = "use-search-string-escape";
