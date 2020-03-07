d503bfe558c28c88735dfd2300e784d785c1ca6d
Serge Rider <serge@jkiss.org>
2020-03-07 15:55:07 +0300

    #1290 SQLite: table column rename
    Former-commit-id: 248d42d48100a40e4ded2cef6fc2632e4f6ea5e0

---
+++ plugins/org.jkiss.dbeaver.ext.sqlite/src/org/jkiss/dbeaver/ext/sqlite/edit/SQLiteTableColumnManager.java
@@ -0,0 +1,58 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2020 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jkiss.dbeaver.ext.sqlite.edit;
+
+import org.jkiss.dbeaver.DBException;
+import org.jkiss.dbeaver.ext.generic.edit.GenericTableColumnManager;
+import org.jkiss.dbeaver.ext.generic.model.GenericTableColumn;
+import org.jkiss.dbeaver.model.DBUtils;
+import org.jkiss.dbeaver.model.edit.DBECommandContext;
+import org.jkiss.dbeaver.model.edit.DBEObjectRenamer;
+import org.jkiss.dbeaver.model.edit.DBEPersistAction;
+import org.jkiss.dbeaver.model.exec.DBCExecutionContext;
+import org.jkiss.dbeaver.model.impl.edit.SQLDatabasePersistAction;
+import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * SQLiteTableColumnManager
+ */
+public class SQLiteTableColumnManager extends GenericTableColumnManager
+    implements DBEObjectRenamer<GenericTableColumn>
+{
+
+    @Override
+    protected void addObjectRenameActions(DBRProgressMonitor monitor, DBCExecutionContext executionContext, List<DBEPersistAction> actions, ObjectRenameCommand command, Map<String, Object> options)
+    {
+        final GenericTableColumn column = command.getObject();
+
+        actions.add(
+            new SQLDatabasePersistAction(
+                "Rename column",
+                "ALTER TABLE " + DBUtils.getQuotedIdentifier(column.getTable()) + " RENAME COLUMN " +
+                    DBUtils.getQuotedIdentifier(column.getDataSource(), command.getOldName()) +
+                    " TO " + DBUtils.getQuotedIdentifier(column.getDataSource(), command.getNewName())));
+    }
+
+    @Override
+    public void renameObject(DBECommandContext commandContext, GenericTableColumn object, String newName) throws DBException {
+        processObjectRename(commandContext, object, newName);
+    }
+
+}
