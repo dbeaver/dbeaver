1c127c5691885ade91eca09777d88b02eef7ec04
serge-rider <serge@jkiss.org>
2020-04-01 14:26:47 +0300

    #365 Driver properties model

---
+++ plugins/org.jkiss.dbeaver.registry/src/org/jkiss/dbeaver/registry/DataSourceNavigatorSettings.java
@@ -0,0 +1,86 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2020 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jkiss.dbeaver.registry;
+
+import org.jkiss.dbeaver.model.navigator.DBNBrowseSettings;
+
+/**
+ * Browse settings
+ */
+public class DataSourceNavigatorSettings implements DBNBrowseSettings {
+
+    private boolean showSystemObjects;
+    private boolean showUtilityObjects;
+    private boolean showOnlyEntities;
+    private boolean mergeEntities;
+    private boolean hideFolders;
+    private boolean hideSchemas;
+
+    @Override
+    public boolean isShowSystemObjects() {
+        return showSystemObjects;
+    }
+
+    public void setShowSystemObjects(boolean showSystemObjects) {
+        this.showSystemObjects = showSystemObjects;
+    }
+
+    @Override
+    public boolean isShowUtilityObjects() {
+        return showUtilityObjects;
+    }
+
+    public void setShowUtilityObjects(boolean showUtilityObjects) {
+        this.showUtilityObjects = showUtilityObjects;
+    }
+
+    @Override
+    public boolean isShowOnlyEntities() {
+        return showOnlyEntities;
+    }
+
+    public void setShowOnlyEntities(boolean showOnlyEntities) {
+        this.showOnlyEntities = showOnlyEntities;
+    }
+
+    @Override
+    public boolean isMergeEntities() {
+        return mergeEntities;
+    }
+
+    public void setMergeEntities(boolean mergeEntities) {
+        this.mergeEntities = mergeEntities;
+    }
+
+    @Override
+    public boolean isHideFolders() {
+        return hideFolders;
+    }
+
+    public void setHideFolders(boolean hideFolders) {
+        this.hideFolders = hideFolders;
+    }
+
+    @Override
+    public boolean isHideSchemas() {
+        return hideSchemas;
+    }
+
+    public void setHideSchemas(boolean hideSchemas) {
+        this.hideSchemas = hideSchemas;
+    }
+}
