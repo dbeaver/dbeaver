01d41960a8d52ececae12caf6728f2072934126f
serge-rider <serge@jkiss.org>
2020-05-13 19:15:49 +0300

    CB-120 Session cache reser on login/logout
    Former-commit-id: 3b1f0e91ea6d16b3bae936abac341b3efed2ad75

--- plugins/org.jkiss.dbeaver.ext.oracle/src/org/jkiss/dbeaver/ext/oracle/model/auth/OracleAuthOS.java
+++ plugins/org.jkiss.dbeaver.ext.oracle/src/org/jkiss/dbeaver/ext/oracle/model/auth/OracleAuthOS.java
@@ -19,11 +19,9 @@ package org.jkiss.dbeaver.ext.oracle.model.auth;
 
 import org.jkiss.code.NotNull;
 import org.jkiss.dbeaver.DBException;
-import org.jkiss.dbeaver.model.DBConstants;
 import org.jkiss.dbeaver.model.DBPDataSource;
-import org.jkiss.dbeaver.model.DBPDataSourceContainer;
-import org.jkiss.dbeaver.model.auth.DBAAuthModel;
 import org.jkiss.dbeaver.model.connection.DBPConnectionConfiguration;
+import org.jkiss.dbeaver.model.impl.auth.AuthModelDatabaseNative;
 import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
 import org.jkiss.utils.StandardConstants;
 
@@ -32,21 +30,17 @@ import java.util.Properties;
 /**
  * Oracle OS auth model.
  */
-public class OracleAuthOS implements DBAAuthModel {
+public class OracleAuthOS extends AuthModelDatabaseNative {
 
     public static final String ID = "oracle_os";
 
     @Override
     public void initAuthentication(@NotNull DBRProgressMonitor monitor, @NotNull DBPDataSource dataSource, @NotNull DBPConnectionConfiguration configuration, @NotNull Properties connProperties) throws DBException {
-        connProperties.remove(DBConstants.DATA_SOURCE_PROPERTY_USER);
-        connProperties.remove(DBConstants.DATA_SOURCE_PROPERTY_PASSWORD);
+        configuration.setUserName(null);
+        configuration.setUserPassword(null);
 
         connProperties.put("v$session.osuser", System.getProperty(StandardConstants.ENV_USER_NAME));
-    }
-
-    @Override
-    public void endAuthentication(@NotNull DBPDataSourceContainer dataSource, @NotNull DBPConnectionConfiguration configuration, @NotNull Properties connProperties) {
-
+        super.initAuthentication(monitor, dataSource, configuration, connProperties);
     }
 
 }
