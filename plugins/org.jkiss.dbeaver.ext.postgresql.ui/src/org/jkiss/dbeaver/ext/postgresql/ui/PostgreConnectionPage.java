3041213a90057ec80a1859e0e3b394f5eb2b3ff3
Anastasiya Volkova <anvolka@yandex.ru>
2020-05-14 17:19:10 +0300

    #8636 port field size unified for different os

--- plugins/org.jkiss.dbeaver.ext.postgresql.ui/src/org/jkiss/dbeaver/ext/postgresql/ui/PostgreConnectionPage.java
+++ plugins/org.jkiss.dbeaver.ext.postgresql.ui/src/org/jkiss/dbeaver/ext/postgresql/ui/PostgreConnectionPage.java
@@ -84,7 +84,7 @@ public class PostgreConnectionPage extends ConnectionPageWithAuth implements ICo
 
         portText = UIUtils.createLabelText(addrGroup, PostgreMessages.dialog_setting_connection_port, null, SWT.BORDER);
         gd = new GridData(GridData.VERTICAL_ALIGN_BEGINNING);
-        gd.widthHint = 40;
+        gd.widthHint = UIUtils.getFontHeight(portText) * 5;
         portText.setLayoutData(gd);
         portText.addVerifyListener(UIUtils.getIntegerVerifyListener(Locale.getDefault()));
         portText.addModifyListener(textListener);
