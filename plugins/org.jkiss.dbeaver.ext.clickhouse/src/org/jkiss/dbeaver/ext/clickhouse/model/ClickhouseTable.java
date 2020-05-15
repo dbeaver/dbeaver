9ca3e55e90b31d222f143bd525b0b02314bf0005
serge-rider <serge@jkiss.org>
2020-05-15 19:20:36 +0300

    #8714 Clickhouse statistics

--- plugins/org.jkiss.dbeaver.ext.clickhouse/src/org/jkiss/dbeaver/ext/clickhouse/model/ClickhouseTable.java
+++ plugins/org.jkiss.dbeaver.ext.clickhouse/src/org/jkiss/dbeaver/ext/clickhouse/model/ClickhouseTable.java
@@ -17,23 +17,37 @@
 package org.jkiss.dbeaver.ext.clickhouse.model;
 
 import org.jkiss.code.Nullable;
+import org.jkiss.dbeaver.Log;
 import org.jkiss.dbeaver.ext.generic.model.GenericStructContainer;
 import org.jkiss.dbeaver.ext.generic.model.GenericTable;
 import org.jkiss.dbeaver.model.DBPObjectStatistics;
+import org.jkiss.dbeaver.model.DBUtils;
+import org.jkiss.dbeaver.model.exec.DBCSession;
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCPreparedStatement;
 import org.jkiss.dbeaver.model.exec.jdbc.JDBCResultSet;
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCSession;
 import org.jkiss.dbeaver.model.impl.jdbc.JDBCUtils;
 import org.jkiss.dbeaver.model.meta.Property;
 import org.jkiss.dbeaver.model.preferences.DBPPropertySource;
+import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
 import org.jkiss.utils.ByteNumberFormat;
 
 import java.sql.SQLException;
+import java.util.Date;
 
 /**
  * ClickhouseTable
  */
 public class ClickhouseTable extends GenericTable implements DBPObjectStatistics
 {
+    private static final Log log = Log.getLog(ClickhouseTable.class);
+
     private Long tableSize;
+    private long tableRows;
+    private Date lastModifyTime;
+    private String maxDate;
+    private String minDate;
+    private String engine;
 
     ClickhouseTable(GenericStructContainer container, @Nullable String tableName, @Nullable String tableType, @Nullable JDBCResultSet dbResult) {
         super(container, tableName, tableType, dbResult);
@@ -44,10 +58,42 @@ public class ClickhouseTable extends GenericTable implements DBPObjectStatistics
         return tableSize != null;
     }
 
-    @Property(viewable = false, order = 20, formatter = ByteNumberFormat.class)
+    @Property(category = CAT_STATISTICS, viewable = true, order = 20, formatter = ByteNumberFormat.class)
     @Override
     public long getStatObjectSize() {
         return tableSize == null ? 0 : tableSize;
+    }
+
+    @Property(category = CAT_STATISTICS, viewable = true, order = 21)
+    @Nullable
+    @Override
+    public synchronized Long getRowCount(DBRProgressMonitor monitor) {
+        readStatistics(monitor);
+        return tableRows;
+    }
+
+    @Property(category = CAT_STATISTICS, order = 22)
+    public Date getLastModifyTime(DBRProgressMonitor monitor) {
+        readStatistics(monitor);
+        return lastModifyTime;
+    }
+
+    @Property(category = CAT_STATISTICS, order = 23)
+    public String getMinDate(DBRProgressMonitor monitor) {
+        readStatistics(monitor);
+        return minDate;
+    }
+
+    @Property(category = CAT_STATISTICS, order = 24)
+    public String getMaxDate(DBRProgressMonitor monitor) {
+        readStatistics(monitor);
+        return maxDate;
+    }
+
+    @Property(category = CAT_STATISTICS, viewable = true, order = 25)
+    public String getEngine(DBRProgressMonitor monitor) {
+        readStatistics(monitor);
+        return engine;
     }
 
     @Nullable
@@ -56,8 +102,43 @@ public class ClickhouseTable extends GenericTable implements DBPObjectStatistics
         return null;
     }
 
+    private void readStatistics(DBRProgressMonitor monitor) {
+        if (hasStatistics()) {
+            return;
+        }
+        try (DBCSession session = DBUtils.openMetaSession(monitor, this, "Read relation statistics")) {
+            try (JDBCPreparedStatement dbStat = ((JDBCSession)session).prepareStatement(
+                "select " +
+                    "sum(bytes) as table_size, " +
+                    "sum(rows) as table_rows, " +
+                    "max(modification_time) as latest_modification," +
+                    "min(min_date) AS min_date," +
+                    "max(max_date) AS max_date," +
+                    "any(engine) as engine\n" +
+                    "FROM system.parts\n" +
+                    "WHERE database=? AND table=?\n" +
+                    "GROUP BY table"))
+            {
+                dbStat.setString(1, getContainer().getName());
+                dbStat.setString(2, getName());
+                try (JDBCResultSet dbResult = dbStat.executeQuery()) {
+                    if (dbResult.next()) {
+                        fetchStatistics(dbResult);
+                    }
+                }
+            } catch (SQLException e) {
+                log.error("Error reading table statistics", e);
+            }
+        }
+    }
+
     void fetchStatistics(JDBCResultSet dbResult) throws SQLException {
         tableSize = JDBCUtils.safeGetLong(dbResult, "table_size");
+        tableRows = JDBCUtils.safeGetLong(dbResult, "table_rows");
+        lastModifyTime = JDBCUtils.safeGetTimestamp(dbResult, "latest_modification");
+        maxDate = JDBCUtils.safeGetString(dbResult, "max_date");
+        minDate = JDBCUtils.safeGetString(dbResult, "min_date");
+        engine = JDBCUtils.safeGetString(dbResult, "engine");
     }
 
 
