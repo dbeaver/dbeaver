ab49519479fe427f6813b0470d6e6a9d957d4521
Serge Rider <serge@jkiss.org>
2020-05-22 13:35:15 +0300

    #8635 Firebird view definition read fix

--- plugins/org.jkiss.dbeaver.ext.firebird/src/org/jkiss/dbeaver/ext/firebird/model/FireBirdView.java
+++ plugins/org.jkiss.dbeaver.ext.firebird/src/org/jkiss/dbeaver/ext/firebird/model/FireBirdView.java
@@ -17,10 +17,22 @@
 package org.jkiss.dbeaver.ext.firebird.model;
 
 import org.jkiss.code.Nullable;
+import org.jkiss.dbeaver.DBException;
+import org.jkiss.dbeaver.ext.firebird.FireBirdUtils;
 import org.jkiss.dbeaver.ext.generic.model.GenericStructContainer;
+import org.jkiss.dbeaver.model.DBPScriptObject;
 import org.jkiss.dbeaver.model.exec.jdbc.JDBCResultSet;
+import org.jkiss.dbeaver.model.meta.Property;
+import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
+import org.jkiss.dbeaver.model.struct.DBSObjectWithScript;
+import org.jkiss.dbeaver.model.struct.rdb.DBSView;
+import org.jkiss.utils.CommonUtils;
 
-public class FireBirdView extends FireBirdTable {
+import java.util.Map;
+
+public class FireBirdView extends FireBirdTable implements DBSObjectWithScript, DBSView {
+
+    private String ddl;
 
     public FireBirdView(GenericStructContainer container, @Nullable String tableName, @Nullable String tableType, @Nullable JDBCResultSet dbResult) {
         super(container, tableName, tableType, dbResult);
@@ -30,4 +42,22 @@ public class FireBirdView extends FireBirdTable {
     public boolean isView() {
         return true;
     }
+
+    @Override
+    @Property(hidden = true, editable = true, updatable = true, order = -1)
+    public String getObjectDefinitionText(DBRProgressMonitor monitor, Map<String, Object> options) throws DBException {
+        if (CommonUtils.getOption(options, DBPScriptObject.OPTION_REFRESH)) {
+            ddl = null;
+        }
+        if (ddl == null) {
+            ddl = isPersisted() ? FireBirdUtils.getViewSource(monitor, this) : "";
+        }
+        return ddl;
+    }
+
+    @Override
+    public void setObjectDefinitionText(String source) {
+        this.ddl = source;
+    }
+
 }
