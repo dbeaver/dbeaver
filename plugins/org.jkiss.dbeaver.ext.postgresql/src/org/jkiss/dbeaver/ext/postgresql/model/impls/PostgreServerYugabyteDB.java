e7ea1c78863eba80aaa0ed896f7bb07926bd6a79
Nikhil Chandrappa <nikhilmcn@gmail.com>
2020-06-04 19:15:31 -0400

    Initial commit to Support YugabyteDB - #8898
    Former-commit-id: a127200f90483562ec3546e5d8f202844cf73a71

--- plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/model/impls/PostgreServerYugabyteDB.java
+++ plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/model/impls/PostgreServerYugabyteDB.java
@@ -150,7 +150,7 @@ public class PostgreServerYugabyteDB extends PostgreServerExtensionBase {
 
     @Override
     public boolean supportsExplainPlanVerbose() {
-        return ture;
+        return true;
     }
 
     @Override
