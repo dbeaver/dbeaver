4199c28178bf581fe88a9562211e1df9d6e0841c
LonwoLonwo <anvolka@yandex.ru>
2020-06-05 15:53:47 +0300

    #8893 Tools to tasks. PostgreSQL. Refresh materialized view. Table/Database analyze

--- plugins/org.jkiss.dbeaver.ext.postgresql.ui/src/org/jkiss/dbeaver/ext/postgresql/tools/maintenance/PostgreToolRefreshMView.java
+++ plugins/org.jkiss.dbeaver.ext.postgresql.ui/src/org/jkiss/dbeaver/ext/postgresql/tools/maintenance/PostgreToolRefreshMView.java
@@ -16,71 +16,30 @@
  */
 package org.jkiss.dbeaver.ext.postgresql.tools.maintenance;
 
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Group;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.ui.IWorkbenchPart;
-import org.eclipse.ui.IWorkbenchPartSite;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.jkiss.dbeaver.DBException;
-import org.jkiss.dbeaver.ext.postgresql.PostgreMessages;
-import org.jkiss.dbeaver.ext.postgresql.model.PostgreMaterializedView;
-import org.jkiss.dbeaver.ext.postgresql.model.PostgreObject;
-import org.jkiss.dbeaver.model.DBPEvaluationContext;
+import org.jkiss.dbeaver.ext.postgresql.tasks.PostgreSQLTasks;
 import org.jkiss.dbeaver.model.struct.DBSObject;
-import org.jkiss.dbeaver.ui.UIUtils;
+import org.jkiss.dbeaver.tasks.ui.wizard.TaskConfigurationWizardDialog;
+import org.jkiss.dbeaver.ui.navigator.NavigatorUtils;
 import org.jkiss.dbeaver.ui.tools.IUserInterfaceTool;
-import org.jkiss.utils.CommonUtils;
 
 import java.util.Collection;
-import java.util.List;
 
 /**
- * Table analyze
+ * MView Refresh
  */
 public class PostgreToolRefreshMView implements IUserInterfaceTool
 {
     @Override
     public void execute(IWorkbenchWindow window, IWorkbenchPart activePart, Collection<DBSObject> objects) throws DBException
     {
-        List<PostgreMaterializedView> tables = CommonUtils.filterCollection(objects, PostgreMaterializedView.class);
-        if (!tables.isEmpty()) {
-            SQLDialog dialog = new SQLDialog(activePart.getSite(), tables);
-            dialog.open();
-        }
+        TaskConfigurationWizardDialog.openNewTaskDialog(
+                window,
+                NavigatorUtils.getSelectedProject(),
+                PostgreSQLTasks.TASK_MVIEW_REFRESH,
+                new StructuredSelection(objects.toArray()));
     }
-
-    static class SQLDialog extends TableToolDialog {
-
-        private Button withDataCheck;
-
-        public SQLDialog(IWorkbenchPartSite partSite, List<PostgreMaterializedView> selectedTables)
-        {
-            super(partSite, PostgreMessages.tool_refresh_mview_title_table, selectedTables);
-        }
-
-        @Override
-        protected void generateObjectCommand(List<String> lines, PostgreObject object) {
-            String sql = "REFRESH MATERIALIZED VIEW " + ((PostgreMaterializedView) object).getFullyQualifiedName(DBPEvaluationContext.DDL) + " ";
-            if (withDataCheck.getSelection()) {
-                sql += "WITH DATA";
-            } else {
-                sql += "WITH NO DATA";
-            }
-            lines.add(sql);
-        }
-
-        @Override
-        protected void createControls(Composite parent) {
-
-            Group optionsGroup = UIUtils.createControlGroup(parent, PostgreMessages.tool_refresh_mview_group_option, 1, 0, 0);
-            optionsGroup.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-            withDataCheck = UIUtils.createCheckbox(optionsGroup,  PostgreMessages.tool_refresh_mview_with_data, PostgreMessages.tool_refresh_mview_with_data_tooltip, true, 0);
-            withDataCheck.addSelectionListener(SQL_CHANGE_LISTENER);
-
-            createObjectsSelector(parent);
-        }
-    }
-
 }
---
+++ plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/tasks/PostgreToolWithStatus.java
@@ -0,0 +1,93 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2020 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jkiss.dbeaver.ext.postgresql.tasks;
+
+import org.jkiss.dbeaver.model.edit.DBEPersistAction;
+import org.jkiss.dbeaver.model.exec.DBCException;
+import org.jkiss.dbeaver.model.exec.DBCResultSet;
+import org.jkiss.dbeaver.model.exec.DBCSession;
+import org.jkiss.dbeaver.model.exec.DBCStatement;
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCResultSet;
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCStatement;
+import org.jkiss.dbeaver.model.meta.Property;
+import org.jkiss.dbeaver.model.sql.task.SQLToolExecuteHandler;
+import org.jkiss.dbeaver.model.sql.task.SQLToolExecuteSettings;
+import org.jkiss.dbeaver.model.sql.task.SQLToolRunStatisticsGenerator;
+import org.jkiss.dbeaver.model.sql.task.SQLToolStatistics;
+import org.jkiss.dbeaver.model.struct.DBSObject;
+
+import java.sql.SQLException;
+import java.sql.SQLWarning;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public abstract class PostgreToolWithStatus<OBJECT_TYPE extends DBSObject, SETTINGS extends SQLToolExecuteSettings<OBJECT_TYPE>>
+        extends SQLToolExecuteHandler<OBJECT_TYPE, SETTINGS> implements SQLToolRunStatisticsGenerator<OBJECT_TYPE, SETTINGS, DBEPersistAction> {
+
+    @Override
+    public List<ToolStatus> getExecuteStatistics(OBJECT_TYPE object, SETTINGS settings, DBEPersistAction action, DBCSession session, DBCStatement dbStat) throws DBCException {
+        DBCResultSet dbResult = dbStat.openResultSet();
+        if (!(dbResult instanceof JDBCResultSet)) {
+            return Collections.emptyList();
+        }
+        List<ToolStatus> statusList = new ArrayList<>();
+        try {
+            int warnNum = 0;
+            SQLWarning warning = ((JDBCStatement) dbStat).getWarnings();
+            while (warning != null) {
+                statusList.add(new ToolStatus(object, warning.getMessage()));
+                warnNum++;
+                warning = warning.getNextWarning();
+            }
+            if (warnNum == 0) {
+                statusList.add(new ToolStatus(object, "Done"));
+            }
+        } catch (SQLException e) {
+            // ignore
+        }
+        return statusList;
+    }
+
+    public class ToolStatus extends SQLToolStatistics<OBJECT_TYPE> {
+        private final String messageOne;
+        //private final String messageTwo;
+
+        ToolStatus(OBJECT_TYPE object, String messageOne) {
+            super(object);
+            this.messageOne = messageOne;
+            //this.messageTwo = messageTwo;
+        }
+
+        @Property(viewable = true, order = 1)
+        @Override
+        public OBJECT_TYPE getObject() {
+            return super.getObject();
+        }
+
+        @Property(viewable = true, order = 2)
+        public String getMessageOne() {
+            return messageOne;
+        }
+
+       // @Property(viewable = true, order = 3)
+       // public String getMessageText() {
+      //      return messageTwo;
+       // }
+
+    }
+}
