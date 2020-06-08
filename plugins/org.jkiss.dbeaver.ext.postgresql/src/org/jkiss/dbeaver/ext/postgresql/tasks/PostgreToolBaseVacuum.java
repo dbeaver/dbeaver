6202a4b3c644b9ce3c12d1344a68ac6c0cc56586
LonwoLonwo <anvolka@yandex.ru>
2020-06-08 17:45:48 +0300

    #8893 Table/database vacuum - version control removed
    Former-commit-id: e6c7e5028d0ea6d268e7e54eed3efa4a95f37c02

--- plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/tasks/PostgreToolBaseVacuum.java
+++ plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/tasks/PostgreToolBaseVacuum.java
@@ -1,6 +1,5 @@
 package org.jkiss.dbeaver.ext.postgresql.tasks;
 
-import org.jkiss.dbeaver.ext.postgresql.model.PostgreDataSource;
 import org.jkiss.dbeaver.ext.postgresql.model.PostgreTableBase;
 import org.jkiss.dbeaver.model.DBPEvaluationContext;
 import org.jkiss.dbeaver.model.edit.DBEPersistAction;
@@ -20,26 +19,14 @@ public class PostgreToolBaseVacuum extends PostgreToolWithStatus<DBSObject, Post
     @Override
     public void generateObjectQueries(DBCSession session, PostgreToolBaseVacuumSettings settings, List<DBEPersistAction> queries, DBSObject object) throws DBCException {
         String sql = "VACUUM (";
-        boolean isFull = settings.isFull();
-        boolean isFreeze = settings.isFreeze();
-        if(isFull) sql += "FULL, ";
-        if(isFreeze) sql += "FREEZE, ";
+        if(settings.isFull()) sql += "FULL, ";
+        if(settings.isFreeze()) sql += "FREEZE, ";
         sql += "VERBOSE";
-        boolean isAnalyzed = settings.isAnalyzed();
-        boolean isDisabled = settings.isDisableSkipping();
-        if(isAnalyzed) sql += ", ANALYZE";
-        PostgreDataSource postgreDataSource = (PostgreDataSource) session.getExecutionContext().getDataSource();
-        if(isDisabled && (postgreDataSource.isServerVersionAtLeast(9, 6))){
-            sql += ", DISABLE_PAGE_SKIPPING";
-        }
-        if(postgreDataSource.isServerVersionAtLeast(12, 0)){
-            boolean isSkipLocked = settings.isSkipLocked();
-            boolean isIndexCleaning = settings.isIndexCleaning();
-            boolean isTruncated = settings.isTruncated();
-            if(isSkipLocked) sql += ", SKIP_LOCKED";
-            if(isIndexCleaning) sql += ", INDEX_CLEANUP";
-            if(isTruncated) sql += ", TRUNCATE";
-        }
+        if(settings.isAnalyzed()) sql += ", ANALYZE";
+        if(settings.isDisableSkipping()) sql += ", DISABLE_PAGE_SKIPPING";
+        if(settings.isSkipLocked()) sql += ", SKIP_LOCKED";
+        if(settings.isIndexCleaning()) sql += ", INDEX_CLEANUP";
+        if(settings.isTruncated()) sql += ", TRUNCATE";
         sql += ")";
         if(object instanceof PostgreTableBase){
             PostgreTableBase postObject = (PostgreTableBase) object;
