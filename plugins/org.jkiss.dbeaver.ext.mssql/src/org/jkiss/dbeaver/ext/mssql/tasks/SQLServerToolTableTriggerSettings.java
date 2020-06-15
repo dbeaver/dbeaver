f1f02768b27457c0be3c20af905504469fc17130
LonwoLonwo <anvolka@yandex.ru>
2020-06-15 20:17:24 +0300

    #8976 Tools to tasks. MS SQL Server. Enable/Disable triggers
    Former-commit-id: 7defabcca2fd4da85dd2fd2b505eb4bc3c9a1503

--- plugins/org.jkiss.dbeaver.ext.mssql/src/org/jkiss/dbeaver/ext/mssql/tasks/SQLServerToolTableTriggerSettings.java
+++ plugins/org.jkiss.dbeaver.ext.mssql/src/org/jkiss/dbeaver/ext/mssql/tasks/SQLServerToolTableTriggerSettings.java
@@ -16,8 +16,8 @@
  */
 package org.jkiss.dbeaver.ext.mssql.tasks;
 
-import org.jkiss.dbeaver.ext.mssql.model.SQLServerTableBase;
+import org.jkiss.dbeaver.ext.mssql.model.SQLServerTableTrigger;
 import org.jkiss.dbeaver.model.sql.task.SQLToolExecuteSettings;
 
-public class SQLServerToolTableRebuildSettings extends SQLToolExecuteSettings<SQLServerTableBase> {
+public class SQLServerToolTableTriggerSettings extends SQLToolExecuteSettings<SQLServerTableTrigger> {
 }
