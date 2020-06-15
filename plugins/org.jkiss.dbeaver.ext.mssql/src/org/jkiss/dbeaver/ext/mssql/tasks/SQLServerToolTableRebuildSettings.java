f5e19a63826893fb2fcf42223a40196e305b8e0a
LonwoLonwo <anvolka@yandex.ru>
2020-06-15 19:18:47 +0300

    #8976 Tolls to tasks. MS SQLServer. Rebuild table index

---
+++ plugins/org.jkiss.dbeaver.ext.mssql/src/org/jkiss/dbeaver/ext/mssql/tasks/SQLServerToolTableRebuildSettings.java
@@ -0,0 +1,7 @@
+package org.jkiss.dbeaver.ext.mssql.tasks;
+
+import org.jkiss.dbeaver.ext.mssql.model.SQLServerTableBase;
+import org.jkiss.dbeaver.model.sql.task.SQLToolExecuteSettings;
+
+public class SQLServerToolTableRebuildSettings extends SQLToolExecuteSettings<SQLServerTableBase> {
+}
