263d4ac87b14c08486a258768a6dbf434d3ca83c
Anastasiya Volkova <anvolka@yandex.ru>
2020-06-26 15:56:39 +0300

    #6069 refresh for check constraints fixed

--- plugins/org.jkiss.dbeaver.ext.mysql/src/org/jkiss/dbeaver/ext/mysql/model/MySQLTable.java
+++ plugins/org.jkiss.dbeaver.ext.mysql/src/org/jkiss/dbeaver/ext/mysql/model/MySQLTable.java
@@ -560,6 +560,7 @@ public class MySQLTable extends MySQLTableBase implements DBPObjectStatistics
     @Override
     public DBSObject refreshObject(@NotNull DBRProgressMonitor monitor) throws DBException {
         getContainer().constraintCache.clearObjectCache(this);
+        getContainer().checkConstraintCache.clearObjectCache(this);
         getContainer().indexCache.clearObjectCache(this);
         getContainer().triggerCache.clearChildrenOf(this);
         this.referenceCache = null;
