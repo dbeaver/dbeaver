f4875b49fe91e5ef7a40e6645403d6f68a3dc0eb
Serge Rider <serge@jkiss.org>
2020-07-02 20:40:48 +0300

    View toggle handler

--- plugins/org.jkiss.dbeaver.ui.navigator/src/org/jkiss/dbeaver/ui/navigator/actions/ToggleViewAction.java
+++ plugins/org.jkiss.dbeaver.ui.navigator/src/org/jkiss/dbeaver/ui/navigator/actions/ToggleViewAction.java
@@ -98,7 +98,11 @@ public class ToggleViewAction extends Action implements IPartListener
             if (view == null) {
                 activePage.showView(viewId);
             } else {
-                activePage.hideView(view);
+                if (!activePage.isPartVisible(view)) {
+                    activePage.bringToTop(view);
+                } else {
+                    activePage.hideView(view);
+                }
             }
         } catch (PartInitException ex) {
             DBWorkbench.getPlatformUI().showError(viewId, "Can't open view " + viewId, ex);
