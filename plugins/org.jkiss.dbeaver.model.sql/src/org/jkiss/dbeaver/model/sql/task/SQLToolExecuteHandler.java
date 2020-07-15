501fdfa63a986414380751844bfaffb02ddc4d3b
LonwoLonwo <anvolka@yandex.ru>
2020-07-15 17:06:32 +0300

    #9010 error status for tasks on deleted objects added
    Former-commit-id: 6539317ca73b4c61143a6df7873cff9f7bfea0f8

--- plugins/org.jkiss.dbeaver.model.sql/src/org/jkiss/dbeaver/model/sql/task/SQLToolExecuteHandler.java
+++ plugins/org.jkiss.dbeaver.model.sql/src/org/jkiss/dbeaver/model/sql/task/SQLToolExecuteHandler.java
@@ -85,14 +85,19 @@ public abstract class SQLToolExecuteHandler<OBJECT_TYPE extends DBSObject, SETTI
 
         List<OBJECT_TYPE> objectList = settings.getObjectList();
         Exception lastError = null;
-        if(objectList.isEmpty()){
-            logStream.write("Object(s) for tool execution not found");
-            log.debug("Object(s) for tool execution not found");
-        }
 
         listener.taskStarted(settings);
         try {
             monitor.beginTask("Execute tool '" + task.getType().getName() + "'", objectList.size());
+            if(objectList.isEmpty()){
+                List<Throwable> warnings = settings.getWarnings();
+                if (!warnings.isEmpty()) {
+                    Throwable throwable = warnings.get(0);
+                    if (throwable instanceof DBException) {
+                        throw (DBException) throwable;
+                    }
+                }
+            }
             for (OBJECT_TYPE object : objectList) {
                 monitor.subTask("Process [" + DBUtils.getObjectFullName(object, DBPEvaluationContext.UI) + "]");
                 try (DBCSession session = DBUtils.openUtilSession(monitor, object, "Execute " + task.getType().getName())) {
