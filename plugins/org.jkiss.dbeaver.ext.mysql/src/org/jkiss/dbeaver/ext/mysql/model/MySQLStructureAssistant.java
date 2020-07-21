50942ef1481cdb1468dbb33fa4592555bfc4f9bf
Anastasiya Volkova <anvolka@yandex.ru>
2020-07-22 00:27:40 +0300

    #9155 mysql check constraints added to metadata search
    Former-commit-id: a6e6acdd12b706bdd8ce62829738c45da8a7a899

--- plugins/org.jkiss.dbeaver.ext.mysql/src/org/jkiss/dbeaver/ext/mysql/model/MySQLStructureAssistant.java
+++ plugins/org.jkiss.dbeaver.ext.mysql/src/org/jkiss/dbeaver/ext/mysql/model/MySQLStructureAssistant.java
@@ -214,7 +214,8 @@ public class MySQLStructureAssistant extends JDBCStructureAssistant<MySQLExecuti
                     final String constrName = JDBCUtils.safeGetString(dbResult, MySQLConstants.COL_CONSTRAINT_NAME);
                     final String constrType = JDBCUtils.safeGetString(dbResult, MySQLConstants.COL_CONSTRAINT_TYPE);
                     final boolean isFK = MySQLConstants.CONSTRAINT_FOREIGN_KEY.equals(constrType);
-                    objects.add(new AbstractObjectReference(constrName, dataSource.getCatalog(catalogName), null, isFK ? MySQLTableForeignKey.class : MySQLTableUniqueKey.class, RelationalObjectType.TYPE_CONSTRAINT) {
+                    final boolean isCheck = MySQLConstants.CONSTRAINT_CHECK.equals(constrType);
+                    objects.add(new AbstractObjectReference(constrName, dataSource.getCatalog(catalogName), null, isFK ? MySQLTableForeignKey.class : MySQLTableConstraint.class, RelationalObjectType.TYPE_CONSTRAINT) {
                         @Override
                         public DBSObject resolveObject(DBRProgressMonitor monitor) throws DBException {
                             MySQLCatalog tableCatalog = catalog != null ? catalog : dataSource.getCatalog(catalogName);
@@ -228,7 +229,11 @@ public class MySQLStructureAssistant extends JDBCStructureAssistant<MySQLExecuti
                             DBSObject constraint;
                             if (isFK) {
                                 constraint = table.getAssociation(monitor, constrName);
-                            } else {
+                            }
+                            else if (isCheck) {
+                                constraint = table.getCheckConstraint(monitor, constrName);
+                            }
+                            else {
                                 constraint = table.getConstraint(monitor, constrName);
                             }
                             if (constraint == null) {
