3b16eb14f1c26541108657f3d8fbddbc6a83386f
Serge Rider <serge@jkiss.org>
2020-07-27 15:43:02 +0300

    #8859 ORacle: Numeric data type max precision fix
    Former-commit-id: 433548615272a24e76edf6ce091605c3e08024c4

--- plugins/org.jkiss.dbeaver.ext.oracle/src/org/jkiss/dbeaver/ext/oracle/model/OracleConstants.java
+++ plugins/org.jkiss.dbeaver.ext.oracle/src/org/jkiss/dbeaver/ext/oracle/model/OracleConstants.java
@@ -161,6 +161,7 @@ public class OracleConstants {
     public static final int EC_FEATURE_NOT_SUPPORTED = 17023;
     public static final int EC_NO_RESULTSET_AVAILABLE = 17283;
     public static final int EC_PASSWORD_EXPIRED = 28001;
+    public static final int NUMERIC_MAX_PRECISION = 38;
 
     /**
      * Connection type
