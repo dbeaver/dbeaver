3443e5f608f225feebf231969122aec2ceda3ec8
serge-rider <serge@jkiss.org>
2020-08-02 22:23:13 +0300

    #8556 Data import wizard enhancement

--- plugins/org.jkiss.dbeaver.data.transfer.ui/src/org/jkiss/dbeaver/tools/transfer/ui/handlers/DataTransferPropertyTester.java
+++ plugins/org.jkiss.dbeaver.data.transfer.ui/src/org/jkiss/dbeaver/tools/transfer/ui/handlers/DataTransferPropertyTester.java
@@ -19,7 +19,14 @@ package org.jkiss.dbeaver.tools.transfer.ui.handlers;
 import org.eclipse.core.expressions.PropertyTester;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.swt.widgets.Display;
+import org.jkiss.dbeaver.DBException;
+import org.jkiss.dbeaver.Log;
+import org.jkiss.dbeaver.model.navigator.DBNDatabaseNode;
 import org.jkiss.dbeaver.model.navigator.DBNNode;
+import org.jkiss.dbeaver.model.runtime.VoidProgressMonitor;
+import org.jkiss.dbeaver.model.struct.DBSDataContainer;
+import org.jkiss.dbeaver.model.struct.DBSObject;
+import org.jkiss.dbeaver.model.struct.DBSObjectContainer;
 import org.jkiss.dbeaver.tools.transfer.registry.DataTransferNodeDescriptor;
 import org.jkiss.dbeaver.tools.transfer.registry.DataTransferProcessorDescriptor;
 import org.jkiss.dbeaver.tools.transfer.registry.DataTransferRegistry;
@@ -31,8 +38,11 @@ import org.jkiss.utils.CommonUtils;
  */
 public class DataTransferPropertyTester extends PropertyTester
 {
+    private static final Log log = Log.getLog(DataTransferPropertyTester.class);
+
     public static final String NAMESPACE = "org.jkiss.dbeaver.tools.transfer";
     public static final String PROP_SUPPORTS_IMPORT_FROM = "supportsImportFrom";
+    public static final String PROP_SUPPORTS_IMPORT_TO = "supportsImportTo";
 
     public DataTransferPropertyTester() {
         super();
@@ -50,18 +60,33 @@ public class DataTransferPropertyTester extends PropertyTester
         }
 
         switch (property) {
-            case PROP_SUPPORTS_IMPORT_FROM:
-                IFile inputFile = (IFile)receiver;
+            case PROP_SUPPORTS_IMPORT_FROM: {
+                IFile inputFile = (IFile) receiver;
                 String extension = inputFile.getFileExtension();
                 if (!CommonUtils.isEmpty(extension)) {
                     DataTransferNodeDescriptor producerDesc = DataTransferRegistry.getInstance().getNodeById(StreamTransferProducer.NODE_ID);
                     if (producerDesc != null) {
-                        for (DataTransferProcessorDescriptor processor :  producerDesc.getProcessors()) {
+                        for (DataTransferProcessorDescriptor processor : producerDesc.getProcessors()) {
 
                         }
                     }
                 }
                 return false;
+            }
+            case PROP_SUPPORTS_IMPORT_TO: {
+                if (receiver instanceof DBNDatabaseNode) {
+                    DBSObject object = ((DBNDatabaseNode) receiver).getObject();
+                    if (object instanceof DBSObjectContainer) {
+                        try {
+                            Class<? extends DBSObject> childType = ((DBSObjectContainer) object).getChildType(new VoidProgressMonitor());
+                            return DBSDataContainer.class.isAssignableFrom(childType);
+                        } catch (DBException e) {
+                            log.error(e);
+                        }
+                    }
+                }
+                return false;
+            }
         }
         return false;
     }
