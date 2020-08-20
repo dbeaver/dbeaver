695de95d7a8573c19f7e3871caf7dc9323bfe753
Anastasiya Volkova <anvolka@yandex.ru>
2020-08-20 14:29:31 +0300

    #8960 UUID transformers united, new mixed-endian (GUID) type added
    Former-commit-id: fc1cba27fd64792bd7ed0dec22d7cd47e20b9737

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/impl/data/transformers/UUIDAttributeTransformer.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/impl/data/transformers/UUIDAttributeTransformer.java
@@ -32,24 +32,53 @@ import org.jkiss.dbeaver.model.struct.DBSTypedObject;
 import org.jkiss.dbeaver.utils.GeneralUtils;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * Transforms binary attribute value into UUID.
+ *
+ *  Ordered UUID storage format saves space and improves INSERT performance as described in the linked article.
+ *  @see <a href="https://www.percona.com/blog/2014/12/19/store-uuid-optimized-way/">Store UUID in an optimized way</a>
+ *
  */
 public class UUIDAttributeTransformer implements DBDAttributeTransformer {
+    private static final String PROP_TYPE = "type";
+    private static final String PROP_CASE = "case";
+    private static final String PROP_ORDERED = "Ordered";
+    private static final String PROP_MIXED_ENDIAN = "Mixed-Endian";
+    private static final String PROP_UPPER_CASE = "Upper case";
 
     @Override
     public void transformAttribute(@NotNull DBCSession session, @NotNull DBDAttributeBinding attribute, @NotNull List<Object[]> rows, @NotNull Map<String, Object> options) throws DBException {
+        boolean isOrdered = false;
+        boolean isMixedEndian = false;
+        boolean isUpperCase = false;
+        if (options.get(PROP_TYPE).equals(PROP_ORDERED)) {
+            isOrdered = true;
+        } else if (options.get(PROP_TYPE).equals(PROP_MIXED_ENDIAN)) {
+            isMixedEndian = true;
+        }
+        if (options.get(PROP_CASE).equals(PROP_UPPER_CASE)) {
+            isUpperCase = true;
+        }
         attribute.setPresentationAttribute(
             new TransformerPresentationAttribute(attribute, "UUID", -1, DBPDataKind.BINARY));
 
-        attribute.setTransformHandler(new UUIDValueHandler(attribute.getValueHandler()));
+        attribute.setTransformHandler(new UUIDValueHandler(attribute.getValueHandler(), isOrdered, isMixedEndian, isUpperCase));
     }
 
     private class UUIDValueHandler extends ProxyValueHandler {
-        public UUIDValueHandler(DBDValueHandler target) {
+        private boolean isOrdered;
+        private boolean isMixedEndian;
+        private boolean isUpperCase;
+
+        public UUIDValueHandler(DBDValueHandler target, boolean isOrdered, boolean isMixedEndian, boolean isUpperCase) {
             super(target);
+            this.isOrdered = isOrdered;
+            this.isMixedEndian = isMixedEndian;
+            this.isUpperCase = isUpperCase;
         }
 
         @NotNull
@@ -62,10 +91,50 @@ public class UUIDAttributeTransformer implements DBDAttributeTransformer {
                 bytes = ((JDBCContentBytes) value).getRawValue();
             }
             if (bytes != null) {
-                try {
-                    return GeneralUtils.getUUIDFromBytes(bytes).toString();
-                } catch (Exception e) {
-                    return String.valueOf(value);
+                if (bytes.length < 16) {
+                    throw new IllegalArgumentException("UUID length must be at least 16 bytes (actual length = " + bytes.length + ")");
+                }
+                if (isOrdered) {
+                    // byte shift operations from Ebean ORM project pull request #1308
+                    long mostSigBits = ((long)bytes[4] << 56) // XXXXXXXX-____-____-...
+                            + ((long)(bytes[5] & 255) << 48)
+                            + ((long)(bytes[6] & 255) << 40)
+                            + ((long)(bytes[7] & 255) << 32)
+                            + ((long)(bytes[2] & 255) << 24)      // ________-XXXX-____-...
+                            + ((bytes[3] & 255) << 16)
+                            + ((bytes[0] & 255) <<  8)            // ________-____-XXXX-...
+                            + ((bytes[1] & 255) <<  0);
+                    long leastSigBits = ((long)bytes[8] << 56)// ________-____-____-XXXX-...
+                            + ((long)(bytes[9] & 255) << 48)
+                            + ((long)(bytes[10] & 255) << 40)     // ________-____-____-____-XXXXXXXXXXXX
+                            + ((long)(bytes[11] & 255) << 32)
+                            + ((long)(bytes[12] & 255) << 24)
+                            + ((bytes[13] & 255) << 16)
+                            + ((bytes[14] & 255) <<  8)
+                            + ((bytes[15] & 255) <<  0);
+                    if (isUpperCase) {
+                        return new UUID(mostSigBits, leastSigBits).toString().toUpperCase(Locale.ENGLISH);
+                    }
+                    return new UUID(mostSigBits, leastSigBits).toString();
+                } else if (isMixedEndian) {
+                    try {
+                        if (isUpperCase) {
+                            return GeneralUtils.getMixedEndianUUIDFromBytes(bytes).toString().toUpperCase(Locale.ENGLISH);
+                        }
+                        return GeneralUtils.getMixedEndianUUIDFromBytes(bytes).toString();
+                    } catch (Exception e) {
+                        return String.valueOf(value);
+                    }
+                }
+                else {
+                    try {
+                        if (isUpperCase) {
+                            return GeneralUtils.getUUIDFromBytes(bytes).toString().toUpperCase(Locale.ENGLISH);
+                        }
+                        return GeneralUtils.getUUIDFromBytes(bytes).toString();
+                    } catch (Exception e) {
+                        return String.valueOf(value);
+                    }
                 }
             }
             return super.getValueDisplayString(column, value, format);
