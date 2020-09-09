40e88e2ddae2b21f2db3c670f9509a8acbff88ef
Nikita Akilov <nikita.n.akilov@gmail.com>
2020-09-09 16:37:15 +0300

    add ru translations to dashboards
    Former-commit-id: 70faa9f34faa7adc9e6cee89200bdd6cf0c04224

--- plugins/org.jkiss.dbeaver.ui.dashboard/src/org/jkiss/dbeaver/ui/dashboard/internal/UIDashboardMessages.java
+++ plugins/org.jkiss.dbeaver.ui.dashboard/src/org/jkiss/dbeaver/ui/dashboard/internal/UIDashboardMessages.java
@@ -24,6 +24,66 @@ public class UIDashboardMessages extends NLS {
     public static String pref_page_dashboards_group_common;
     public static String pref_page_dashboards_open_separate_connection_label;
 
+    //add dashboard dialog
+    public static String dialog_add_dashboard_dialog_title;
+    public static String dialog_add_dashboard_column_name;
+    public static String dialog_add_dashboard_column_description;
+    public static String dialog_add_dashboard_message_no_more_dashboards_for;
+    public static String dialog_add_dashboard_button_manage;
+    public static String dialog_add_dashboard_button_cancel;
+    public static String dialog_add_dashboard_button_add;
+
+    //edit dashboard dialog
+    public static String dialog_edit_dashboard_title;
+    public static String dialog_edit_dashboard_infolabels_predifined_dashboard;
+    public static String dialog_edit_dashboard_maininfo;
+    public static String dialog_edit_dashboard_maininfo_labels_id;
+    public static String dialog_edit_dashboard_maininfo_labels_name;
+    public static String dialog_edit_dashboard_maininfo_labels_db;
+    public static String dialog_edit_dashboard_maininfo_buttons_select;
+    public static String dialog_edit_dashboard_maininfo_labels_description;
+    public static String dialog_edit_dashboard_maininfo_combos_datatype;
+    public static String dialog_edit_dashboard_maininfo_combos_datatype_tooltip;
+    public static String dialog_edit_dashboard_maininfo_combos_calctype;
+    public static String dialog_edit_dashboard_maininfo_combos_calctype_tooltip;
+    public static String dialog_edit_dashboard_maininfo_combos_valuetype;
+    public static String dialog_edit_dashboard_maininfo_combos_valuetype_tooltip;
+    public static String dialog_edit_dashboard_maininfo_combos_interval;
+    public static String dialog_edit_dashboard_maininfo_combos_interval_tooltip;
+    public static String dialog_edit_dashboard_maininfo_combos_fetchtype;
+    public static String dialog_edit_dashboard_maininfo_combos_fetchtype_tooltip;
+    public static String dialog_edit_dashboard_queries;
+    public static String dialog_edit_dashboard_queries_keys;
+    public static String dialog_edit_dashboard_queries_labels;
+    public static String dialog_edit_dashboard_queries_infolabels_separator;
+    public static String dialog_edit_dashboard_rendering;
+    public static String dialog_edit_dashboard_rendering_combos_defaultview;
+    public static String dialog_edit_dashboard_rendering_combos_defaultview_tooltip;
+    public static String dialog_edit_dashboard_rendering_labels_updateperiod;
+    public static String dialog_edit_dashboard_rendering_labels_maxitems;
+
+    //dashboard item config dialog
+    public static String dialog_dashboard_item_config_title;
+    public static String dialog_dashboard_item_config_dashboardinfo;
+    public static String dialog_dashboard_item_config_dashboardinfo_labels_name;
+    public static String dialog_dashboard_item_config_dashboardinfo_labels_description;
+    public static String dialog_dashboard_item_config_dashboardupdate;
+    public static String dialog_dashboard_item_config_dashboardupdate_labels_updateperiod;
+    public static String dialog_dashboard_item_config_dashboardupdate_labels_maxitems;
+    public static String dialog_dashboard_item_config_dashboardview;
+    public static String dialog_dashboard_item_config_dashboardview_combos_view;
+    public static String dialog_dashboard_item_config_dashboardview_combos_view_tooltip;
+    public static String dialog_dashboard_item_config_dashboardview_checkboxes_legend;
+    public static String dialog_dashboard_item_config_dashboardview_checkboxes_legend_tooltip;
+    public static String dialog_dashboard_item_config_dashboardview_checkboxes_grid;
+    public static String dialog_dashboard_item_config_dashboardview_checkboxes_grid_tooltip;
+    public static String dialog_dashboard_item_config_dashboardview_checkboxes_domainaxis;
+    public static String dialog_dashboard_item_config_dashboardview_checkboxes_domainaxis_tooltip;
+    public static String dialog_dashboard_item_config_dashboardview_checkboxes_rangeaxis;
+    public static String dialog_dashboard_item_config_dashboardview_checkboxes_rangeaxis_tooltip;
+    public static String dialog_dashboard_item_config_buttons_configuration;
+    public static String dialog_dashboard_item_config_buttons_sqlqueries;
+    public static String dialog_dashboard_item_config_buttons_sqlqueries_dash;
 
     static {
         // initialize resource bundle
