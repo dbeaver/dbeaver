0e41b8a7b7936c99d049599f0b7612f9566506c4
halitanildonmez <halitanil.donmez@gmail.com>
2020-09-20 14:55:51 +0200

    Issue #1089 styling fix and remove unused variables, update name

--- plugins/org.jkiss.dbeaver.ui.editors.sql/src/org/jkiss/dbeaver/ui/editors/sql/commands/OpenLinkInWindowHandler.java
+++ plugins/org.jkiss.dbeaver.ui.editors.sql/src/org/jkiss/dbeaver/ui/editors/sql/commands/OpenLinkInWindowHandler.java
@@ -10,7 +10,7 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.commands.IElementUpdater;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.ui.menus.UIElement;
-import org.jkiss.dbeaver.Log;
+import org.jkiss.dbeaver.runtime.DBWorkbench;
 import org.jkiss.dbeaver.ui.editors.sql.SQLEditor;
 import org.jkiss.dbeaver.ui.editors.sql.internal.SQLEditorMessages;
 import org.jkiss.dbeaver.utils.RuntimeUtils;
@@ -22,44 +22,42 @@ import java.net.URISyntaxException;
 
 public class OpenLinkInWindowHandler extends AbstractHandler implements IElementUpdater {
 
-	private static final Log log = Log.getLog(OpenLinkInWindowHandler.class);
-	
-	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		SQLEditor  editor = RuntimeUtils.getObjectAdapter(HandlerUtil.getActiveEditor(event), SQLEditor.class);
-		if (editor == null) {
-			log.debug("editor was null");
-			return null;
-		}
-		
-		ISelection selection = editor.getSelectionProvider().getSelection();
-		if (selection.isEmpty() || !(selection instanceof TextSelection)) {
-			log.debug("log was null");
-			return null;
-		}
-		
-		TextSelection textSelection = (TextSelection) selection;
-		log.debug("The selected text is " + textSelection.getText());
-		
-		// TODO: should be constant
-		// TODO: how to handle the spaces, handle url generation
-		String googleLink = "http://www.google.com/search?q=" + textSelection.getText().replaceAll(" ", "%20");
-		// It should not even be possible to use DBeaver on mobile
-		if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
-			try {
-				Desktop.getDesktop().browse(new URI(googleLink));
-			} catch (IOException | URISyntaxException e) {
-				// TODO: handle the error
-				this.log.error("error while opening the browser", e);
-			}
-		} else {
-			log.debug("Desktop is not supported");
-		}
-		return null;
-	}
+    private static final String TITLE = "Search selection in web";
+    private static final String SEARCH_WEB_ADDRESS_PREFIX = "http://www.google.com/search?q=";
+
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        SQLEditor editor = RuntimeUtils.getObjectAdapter(HandlerUtil.getActiveEditor(event), SQLEditor.class);
+        if (editor == null) {
+            DBWorkbench.getPlatformUI().showError(TITLE, "No suitable editor was found for SQL");
+            return null;
+        }
+
+        ISelection selection = editor.getSelectionProvider().getSelection();
+        if (selection.isEmpty() || !(selection instanceof TextSelection)) {
+            DBWorkbench.getPlatformUI().showError(TITLE, "No text was selected");
+            return null;
+        }
+
+        TextSelection textSelection = (TextSelection) selection;
+        // TODO: how to handle the spaces, handle url generation
+        String googleLink = SEARCH_WEB_ADDRESS_PREFIX + textSelection.getText().replaceAll(" ", "%20");
+        // It should not even be possible to use DBeaver on mobile
+        if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+            try {
+                Desktop.getDesktop().browse(new URI(googleLink));
+            } catch (IOException | URISyntaxException e) {
+                DBWorkbench.getPlatformUI().showError(TITLE, "Exception when searching.", e);
+            }
+        } else {
+            DBWorkbench.getPlatformUI().showError(TITLE, "Desktop is not supported.");
+        }
+        return null;
+    }
+    
     @Override
     public void updateElement(UIElement element, Map parameters) {
-        element.setText(SQLEditorMessages.editors_sql_actions_search_at_google);
-        element.setTooltip(SQLEditorMessages.editors_sql_actions_search_at_google_tip);
+        element.setText(SQLEditorMessages.editors_sql_actions_search_selected_text_online);
+        element.setTooltip(SQLEditorMessages.editors_sql_actions_search_selected_text_online_tip);
     }
 }
