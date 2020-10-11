968b73209e0c7cfc8c9415423b9a8ca2f3b79b05
Nikita Akilov <nikita.n.akilov@gmail.com>
2020-10-11 21:10:25 +0300

    dbeaver/dbeaver-ee#521 style fixes

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/task/DBTScheduler.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/task/DBTScheduler.java
@@ -28,30 +28,29 @@ import java.util.List;
  * Scheduler
  */
 public interface DBTScheduler {
-    String FEATURE_START_TIME = "startTime";
     String FEATURE_FREQUENCY_MINUTELY = "frequency.minutely";
-    String FEATURE_FREQUENCY_MINUTELY_START_DATETIME = FEATURE_FREQUENCY_MINUTELY + "start_datetime";
+    String FEATURE_FREQUENCY_MINUTELY_START_DATETIME = FEATURE_FREQUENCY_MINUTELY + "startDatetime";
     String FEATURE_FREQUENCY_HOURLY = "frequency.hourly";
-    String FEATURE_FREQUENCY_HOURLY_START_DATETIME = FEATURE_FREQUENCY_HOURLY + "start_datetime";
+    String FEATURE_FREQUENCY_HOURLY_START_DATETIME = FEATURE_FREQUENCY_HOURLY + "startDatetime";
     String FEATURE_FREQUENCY_DAILY = "frequency.daily";
-    String FEATURE_FREQUENCY_DAILY_START_DATE = "frequency.daily.start_date";
+    String FEATURE_FREQUENCY_DAILY_START_DATE = FEATURE_FREQUENCY_DAILY + ".startDate";
     String FEATURE_FREQUENCY_WEEKLY = "frequency.weekly";
-    String FEATURE_FREQUENCY_WEEKLY_START_DATE = FEATURE_FREQUENCY_WEEKLY + ".start_date";
+    String FEATURE_FREQUENCY_WEEKLY_START_DATE = FEATURE_FREQUENCY_WEEKLY + ".startDate";
     String FEATURE_FREQUENCY_MONTHLY = "frequency.monthly";
-    String FEATURE_FREQUENCY_MONTHLY_START_DATE = FEATURE_FREQUENCY_MONTHLY + ".start_date";
+    String FEATURE_FREQUENCY_MONTHLY_START_DATE = FEATURE_FREQUENCY_MONTHLY + ".startDate";
     String FEATURE_FREQUENCY_ONETIME = "frequency.onetime";
-    String FEATURE_FREQUENCY_ONETIME_START_DATETIME = "frequency.onetime.start_datetime";
+    String FEATURE_FREQUENCY_ONETIME_START_DATETIME = FEATURE_FREQUENCY_ONETIME + "startDatetime";
     String FEATURE_FREQUENCY_EVENT = "frequency.event";
     String FEATURE_FREQUENCY_WEEKLY_RECURRENCE = FEATURE_FREQUENCY_WEEKLY + ".recurrence";
     String FEATURE_FREQUENCY_MONTHLY_LAST_DAY = FEATURE_FREQUENCY_MONTHLY + ".last_day";
-    String FEATURE_FREQUENCY_HOURLY_RECURRENCE_GREATER_THAN_23 = FEATURE_FREQUENCY_HOURLY + ".recurrence.greater_than_23";
-    String FEATURE_FREQUENCY_MINUTELY_RECURRENCE_GREATER_THAN_59 = FEATURE_FREQUENCY_MINUTELY + ".recurrence.greater_than_59";
-    String FEATURE_FREQUENCY_MINUTELY_RECURRENCE_GREATER_THAN_31 = FEATURE_FREQUENCY_DAILY + ".recurrence.greater_than_31";
+    String FEATURE_FREQUENCY_HOURLY_RECURRENCE_GREATER_THAN_23 = FEATURE_FREQUENCY_HOURLY + ".recurrence.greaterThan23";
+    String FEATURE_FREQUENCY_MINUTELY_RECURRENCE_GREATER_THAN_59 = FEATURE_FREQUENCY_MINUTELY + ".recurrence.greaterThan59";
+    String FEATURE_FREQUENCY_MINUTELY_RECURRENCE_GREATER_THAN_31 = FEATURE_FREQUENCY_DAILY + ".recurrence.greaterThan31";
 
     @NotNull
     String getSchedulerName();
 
-    Collection<String> getSchedulerSupportedFeatures();
+    boolean supportsFeature(String feature);
 
     @NotNull
     List<DBTTaskScheduleInfo> getAllScheduledTasks();
