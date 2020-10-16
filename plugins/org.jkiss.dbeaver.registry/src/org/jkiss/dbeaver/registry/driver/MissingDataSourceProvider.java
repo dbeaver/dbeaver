1b117fddc04dd3eadc6839551652b9438708a71a
Serge Rider <serge@jkiss.org>
2020-10-16 15:37:09 +0300

    #10085 Missing datasource provider error handle

---
+++ plugins/org.jkiss.dbeaver.registry/src/org/jkiss/dbeaver/registry/driver/MissingDataSourceProvider.java
@@ -0,0 +1,71 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2020 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jkiss.dbeaver.registry.driver;
+
+import org.jkiss.code.NotNull;
+import org.jkiss.dbeaver.DBException;
+import org.jkiss.dbeaver.model.DBPDataSource;
+import org.jkiss.dbeaver.model.DBPDataSourceContainer;
+import org.jkiss.dbeaver.model.DBPDataSourceProvider;
+import org.jkiss.dbeaver.model.app.DBPPlatform;
+import org.jkiss.dbeaver.model.connection.DBPConnectionConfiguration;
+import org.jkiss.dbeaver.model.connection.DBPDriver;
+import org.jkiss.dbeaver.model.preferences.DBPPropertyDescriptor;
+import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
+
+/**
+ * MissingDataSourceProvider
+ */
+public class MissingDataSourceProvider implements DBPDataSourceProvider {
+
+    private final String providerId;
+
+    public MissingDataSourceProvider(String providerId) {
+        this.providerId = providerId;
+    }
+
+    @Override
+    public void init(@NotNull DBPPlatform platform) {
+
+    }
+
+    @Override
+    public long getFeatures() {
+        return 0;
+    }
+
+    @Override
+    public DBPPropertyDescriptor[] getConnectionProperties(DBRProgressMonitor monitor, DBPDriver driver, DBPConnectionConfiguration connectionInfo) throws DBException {
+        return new DBPPropertyDescriptor[0];
+    }
+
+    @NotNull
+    @Override
+    public DBPDataSource openDataSource(@NotNull DBRProgressMonitor monitor, @NotNull DBPDataSourceContainer container) throws DBException {
+        throw new DBException("Data source provider '" + providerId + "' not found");
+    }
+
+    @Override
+    public String getConnectionURL(DBPDriver driver, DBPConnectionConfiguration connectionInfo) {
+        return connectionInfo.getUrl();
+    }
+
+    @Override
+    public String toString() {
+        return providerId;
+    }
+}
