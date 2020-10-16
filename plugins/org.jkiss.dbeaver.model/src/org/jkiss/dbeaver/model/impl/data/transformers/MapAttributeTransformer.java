89cca2a93bba59ec24deaf5283199631d49e5655
Serge Rider <serge@jkiss.org>
2020-10-16 17:21:42 +0300

    dbeaver/dbeaver-ee#550 Map transform fix (max position calculation)
    Former-commit-id: 4d2d3a607a4f686c172ce50ea6dc6f46c6be18ef

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/impl/data/transformers/MapAttributeTransformer.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/impl/data/transformers/MapAttributeTransformer.java
@@ -101,30 +101,29 @@ public class MapAttributeTransformer implements DBDAttributeTransformer {
 
     private static void createNestedMapBindings(DBCSession session, DBDAttributeBinding topAttribute, List<Pair<DBSAttributeBase, Object[]>> nestedAttributes, List<Object[]> rows) throws DBException {
         int maxPosition = 0;
-        for (Pair<DBSAttributeBase, Object[]> attr : nestedAttributes) {
-            maxPosition = Math.max(maxPosition, attr.getFirst().getOrdinalPosition());
-        }
         List<DBDAttributeBinding> nestedBindings = topAttribute.getNestedBindings();
         if (nestedBindings == null) {
             nestedBindings = new ArrayList<>();
-        } else {
-            for (DBDAttributeBinding binding : nestedBindings) {
-                maxPosition = Math.max(maxPosition, binding.getOrdinalPosition());
-            }
         }
+        for (Pair<DBSAttributeBase, Object[]> nestedAttr : nestedAttributes) {
+            DBSAttributeBase attribute = nestedAttr.getFirst();
+            maxPosition = Math.max(maxPosition, attribute.getOrdinalPosition());
+            DBDAttributeBinding nestedBinding = DBUtils.findObject(nestedBindings, attribute.getName());
+            if (nestedBinding == null) {
+                nestedBinding = new DBDAttributeBindingType(topAttribute, attribute, nestedBindings.size());
+                nestedBindings.add(nestedBinding);
+            }
+            maxPosition = Math.max(maxPosition, nestedBinding.getOrdinalPosition());
+        }
+
+
         Object[] fakeRow = new Object[maxPosition + 1];
 
         List<Object[]> fakeRows = Collections.singletonList(fakeRow);
         for (Pair<DBSAttributeBase, Object[]> nestedAttr : nestedAttributes) {
             DBSAttributeBase attribute = nestedAttr.getFirst();
             Object[] values = nestedAttr.getSecond();
-            DBDAttributeBinding nestedBinding = null;
-            for (DBDAttributeBinding binding : nestedBindings) {
-                if (binding.getName().equals(attribute.getName())) {
-                    nestedBinding = binding;
-                    break;
-                }
-            }
+            DBDAttributeBinding nestedBinding = DBUtils.findObject(nestedBindings, attribute.getName());
             if (nestedBinding == null) {
                 nestedBinding = new DBDAttributeBindingType(topAttribute, attribute, nestedBindings.size());
                 nestedBindings.add(nestedBinding);
