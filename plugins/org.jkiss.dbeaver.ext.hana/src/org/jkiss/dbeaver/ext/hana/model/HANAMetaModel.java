9ebab4566033ac082388e1c612ed9329189ce043
kai-morich <mail@kai-morich.de>
2020-11-06 08:15:08 +0100

    log and throw error if HANA schemas cannot be loaded due to password or license expired

--- plugins/org.jkiss.dbeaver.ext.hana/src/org/jkiss/dbeaver/ext/hana/model/HANAMetaModel.java
+++ plugins/org.jkiss.dbeaver.ext.hana/src/org/jkiss/dbeaver/ext/hana/model/HANAMetaModel.java
@@ -69,18 +69,19 @@ public class HANAMetaModel extends GenericMetaModel
     public List<GenericSchema> loadSchemas(JDBCSession session, GenericDataSource dataSource, GenericCatalog catalog) throws DBException {
         List<GenericSchema> schemas = super.loadSchemas(session, dataSource, catalog);
         if (schemas == null) {
-            // GenericDataSource.initialize ignores errors when reading schema, as some databases do not support schemas.
-            // instead of showing an empty schema list with no indication that there was an error,
-            // try to provoke the error again (e.g. alter password required) and show as pseudo schema.
-            schemas = new ArrayList<>();
             try {
-                session.prepareStatement("SELECT * FROM SYS.DUMMY").execute();
-            } catch(SQLException e) {
-                schemas.add(new GenericSchema(dataSource, catalog, "<error: "+e.getMessage()+">"));
-                return schemas;
+                // when password or license are expired the DB is started in restricted mode. 
+                // nearly everything else, including schema read is rejected. 
+                // basically only commands to fix password or license are allowed. 
+                // replace null from super.loadSchemas with exception, by provoking the SQL error again.
+                session.prepareStatement("SELECT count(*) FROM SYS.SCHEMAS").execute();
+                // should not be reached:
+                log.error("Can't read schema list but can select from SYS.SCHEMAS");
+                throw new DBException("Can't read schema list but can select from SYS.SCHEMAS");
+            } catch (SQLException e) {
+                log.error("Can't read schema list", e);
+                throw new DBException(e, dataSource);
             }
-            schemas.add(new GenericSchema(dataSource, catalog, "<see error log window>>"));
-            return schemas;
         }
         GenericSchema publicSchema = new GenericSchema(dataSource, catalog, PUBLIC_SCHEMA_NAME);
         int i;
