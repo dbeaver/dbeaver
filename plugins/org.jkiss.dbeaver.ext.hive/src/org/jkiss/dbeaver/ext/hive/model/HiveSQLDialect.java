d652eb0309d966963f66f632975a5a5afe0a8af4
Anastasiya Volkova <anvolka@yandex.ru>
2020-11-13 10:38:46 +0300

    #6439 Hive functions, reserved and non reserved keywords added
    Former-commit-id: 687bf26f33277da4338cdf9f29308c5061abc45b

--- plugins/org.jkiss.dbeaver.ext.hive/src/org/jkiss/dbeaver/ext/hive/model/HiveSQLDialect.java
+++ plugins/org.jkiss.dbeaver.ext.hive/src/org/jkiss/dbeaver/ext/hive/model/HiveSQLDialect.java
@@ -20,17 +20,99 @@ import org.jkiss.dbeaver.ext.generic.model.GenericSQLDialect;
 import org.jkiss.dbeaver.model.exec.jdbc.JDBCDatabaseMetaData;
 import org.jkiss.dbeaver.model.impl.jdbc.JDBCDataSource;
 
+import java.util.Arrays;
+
 public class HiveSQLDialect extends GenericSQLDialect {
 
-    public static final String[][] DEFAULT_QUOTE_STRINGS = {{"`", "`"}};
+    private static final String[][] DEFAULT_QUOTE_STRINGS = {{"`", "`"}};
 
     public HiveSQLDialect() {
         super("HiveQL");
     }
 
+    private static final String[] RESERVED_KEYWORDS = {
+            "CONF",
+            "EXCHANGE",
+            "EXTENDED",
+            "DATABASE",
+            "IMPORT",
+            "MACRO",
+            "LESS",
+            "PERCENT",
+            "REDUCE",
+            "TRUNCATE",
+            "SHOW",
+            "SYNC",
+            "VIEWS"
+    };
+
+    private static final String[] NON_RESERVED_KEYWORDS = {
+            "DEFAULT",
+            "AFTER",
+            "CASCADE",
+            "DATA",
+            "DEFINED",
+            "INDEX",
+            "ISOLATION",
+            "KEY",
+            "LEVEL",
+            "LIMIT",
+            "OPTION",
+            "RENAME",
+            "RESTRICT",
+            "SCHEMA",
+            "TRANSACTION",
+            "VIEW"
+    };
+
+    private static final String[] HIVE_EXTRA_FUNCTIONS = {
+            "ARRAY_CONTAINS",
+            "COALESCE",
+            "CURRENT_DATABASE",
+            "DATE_ADD",
+            "DATE_FORMAT",
+            "DATEDIFF",
+            "DAY",
+            "DAYOFWEEK",
+            "HOUR",
+            "MINUTE",
+            "MONTH",
+            "RLIKE",
+            "SECOND",
+            "SIZE",
+            "SUBSTRING_INDEX",
+            "TO_DATE",
+            "TRUNC",
+            "VERSION",
+            "YEAR",
+            "WEEKOFYEAR"
+    };
+
+    private static final String[] HIVE_STRING_FUNCTIONS = {
+            "ASCII",
+            "CONCAT",
+            "GET_JSON_OBJECT",
+            "ENCODE",
+            "LENGTH",
+            "LPAD",
+            "LTRIM",
+            "REGEXP",
+            "REGEXP_EXTRACT",
+            "REGEXP_REPLACE",
+            "REPLACE",
+            "SUBSTR",
+            "SUBSTRING",
+            "QUOTE"
+    };
+
     public void initDriverSettings(JDBCDataSource dataSource, JDBCDatabaseMetaData metaData) {
         super.initDriverSettings(dataSource, metaData);
-        removeSQLKeyword("DEFAULT");
+        for (String keyword : NON_RESERVED_KEYWORDS) {
+            removeSQLKeyword(keyword);
+        }
+        addSQLKeywords(Arrays.asList(RESERVED_KEYWORDS));
+        addFunctions(Arrays.asList(HIVE_EXTRA_FUNCTIONS));
+        addFunctions(Arrays.asList(HIVE_STRING_FUNCTIONS));
     }
 
     @Override
