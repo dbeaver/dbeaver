6860428fdefc96e0275a163b49fb1c92b146499a
serge-rider <serge@jkiss.org>
2020-11-22 21:38:27 +0300

    #10460 HANA triggers metadata read fix

--- plugins/org.jkiss.dbeaver.ext.hana/src/org/jkiss/dbeaver/ext/hana/model/HANAMetaModel.java
+++ plugins/org.jkiss.dbeaver.ext.hana/src/org/jkiss/dbeaver/ext/hana/model/HANAMetaModel.java
@@ -175,8 +175,9 @@ public class HANAMetaModel extends GenericMetaModel
             return Collections.emptyList();
         }
         try (JDBCSession session = DBUtils.openMetaSession(monitor, container, "Read triggers")) {
-            try (JDBCPreparedStatement dbStat = session.prepareStatement("SELECT TRIGGER_NAME FROM SYS.TRIGGERS WHERE SUBJECT_TABLE_NAME=?")) {
-                dbStat.setString(1, table.getName());
+            try (JDBCPreparedStatement dbStat = session.prepareStatement("SELECT TRIGGER_NAME FROM SYS.TRIGGERS WHERE SUBJECT_TABLE_SCHEMA=? AND SUBJECT_TABLE_NAME=?")) {
+                dbStat.setString(1, table.getSchema().getName());
+                dbStat.setString(2, table.getName());
                 List<GenericTrigger> result = new ArrayList<>();
                 try (JDBCResultSet dbResult = dbStat.executeQuery()) {
                     while (dbResult.next()) {
