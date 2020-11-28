22ab5e8004f07dea440359a845c53851fbe0e85e
Serge Rider <serge@jkiss.org>
2020-11-28 18:32:10 +0300

    #10535 Migrate to E2020-12. Dependencies update. Equinot start levels.

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/core/DBeaverCoreAdapterFactory.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/core/DBeaverCoreAdapterFactory.java
@@ -1,7 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
  * Copyright (C) 2010-2020 DBeaver Corp and others
- * Copyright (C) 2017-2018 Alexander Fedorov (alexander.fedorov@jkiss.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
