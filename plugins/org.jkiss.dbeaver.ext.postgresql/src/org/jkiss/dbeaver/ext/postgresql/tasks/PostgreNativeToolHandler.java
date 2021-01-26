347951385822656744688a6b68549fed609a3444
Serge Rider <serge@jkiss.org>
2021-01-26 16:37:21 +0300

    #11119 Native tool password management enhancement

--- plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/tasks/PostgreNativeToolHandler.java
+++ plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/tasks/PostgreNativeToolHandler.java
@@ -1,7 +1,14 @@
 package org.jkiss.dbeaver.ext.postgresql.tasks;
 
+import org.jkiss.dbeaver.DBException;
 import org.jkiss.dbeaver.ext.postgresql.PostgreConstants;
+import org.jkiss.dbeaver.model.DBConstants;
+import org.jkiss.dbeaver.model.DBPDataSourceContainer;
+import org.jkiss.dbeaver.model.auth.DBAAuthCredentials;
+import org.jkiss.dbeaver.model.auth.DBAAuthModel;
 import org.jkiss.dbeaver.model.connection.DBPConnectionConfiguration;
+import org.jkiss.dbeaver.model.impl.auth.AuthModelDatabaseNative;
+import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
 import org.jkiss.dbeaver.model.struct.DBSObject;
 import org.jkiss.dbeaver.tasks.nativetool.AbstractNativeToolHandler;
 import org.jkiss.dbeaver.tasks.nativetool.AbstractNativeToolSettings;
@@ -11,15 +18,35 @@ import org.jkiss.utils.CommonUtils;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Properties;
 
 public abstract class PostgreNativeToolHandler<SETTINGS extends AbstractNativeToolSettings<BASE_OBJECT>, BASE_OBJECT extends DBSObject, PROCESS_ARG>
     extends AbstractNativeToolHandler<SETTINGS, BASE_OBJECT, PROCESS_ARG> {
 
     @Override
-    protected void setupProcessParameters(SETTINGS settings, PROCESS_ARG arg, ProcessBuilder process) {
+    protected void setupProcessParameters(DBRProgressMonitor monitor, SETTINGS settings, PROCESS_ARG arg, ProcessBuilder process) {
         String userPassword = settings.getToolUserPassword();
         if (CommonUtils.isEmpty(userPassword)) {
-            userPassword = settings.getDataSourceContainer().getActualConnectionConfiguration().getUserPassword();
+            // Try to obtain password thru auth model (mnakes sense for IAM-like models)
+            DBPDataSourceContainer dataSourceContainer = settings.getDataSourceContainer();
+            DBPConnectionConfiguration cfg = new DBPConnectionConfiguration(dataSourceContainer.getActualConnectionConfiguration());
+            DBAAuthModel authModel = cfg.getAuthModel();
+            if (authModel != AuthModelDatabaseNative.INSTANCE) {
+                DBAAuthCredentials credentials = authModel.loadCredentials(dataSourceContainer, cfg);
+                try {
+                    Properties connProperties = new Properties();
+                    authModel.initAuthentication(monitor, dataSourceContainer.getDataSource(), credentials, cfg, connProperties);
+                    Object authPassword = connProperties.get(DBConstants.DATA_SOURCE_PROPERTY_PASSWORD);
+                    if (authPassword != null) {
+                        userPassword = CommonUtils.toString(authPassword);
+                    }
+                } catch (DBException e) {
+                    // ignore
+                }
+            }
+            if (CommonUtils.isEmpty(userPassword)) {
+                userPassword = dataSourceContainer.getActualConnectionConfiguration().getUserPassword();
+            }
         }
         if (!CommonUtils.isEmpty(userPassword)) {
             process.environment().put("PGPASSWORD", userPassword);
