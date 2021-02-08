6ace9b2260eb5cf9068fade002cc98219b455d62
Serge Rider <serge@jkiss.org>
2021-02-08 16:52:52 +0300

    #10908 Disable auto assistance when it turned off in datasource preferences
    Former-commit-id: 506766240d5c4a6958b1efd9c30355be6a16fc1f

--- plugins/org.jkiss.dbeaver.ui.editors.sql/src/org/jkiss/dbeaver/ui/editors/sql/syntax/SQLContentAssistant.java
+++ plugins/org.jkiss.dbeaver.ui.editors.sql/src/org/jkiss/dbeaver/ui/editors/sql/syntax/SQLContentAssistant.java
@@ -17,13 +17,18 @@
 package org.jkiss.dbeaver.ui.editors.sql.syntax;
 
 import org.eclipse.jface.text.contentassist.ContentAssistant;
+import org.jkiss.dbeaver.ui.editors.sql.SQLEditorBase;
+import org.jkiss.dbeaver.ui.editors.sql.SQLPreferenceConstants;
 
 /**
  * SQL Completion proposal
  */
 public class SQLContentAssistant extends ContentAssistant {
-    public SQLContentAssistant() {
+    private final SQLEditorBase editor;
+
+    public SQLContentAssistant(SQLEditorBase editor) {
         super(); // Sync. Maybe we should make it async
+        this.editor = editor;
         enableColoredLabels(true);
     }
 
@@ -35,6 +40,9 @@ public class SQLContentAssistant extends ContentAssistant {
     private class SQLAutoAssistListener extends AutoAssistListener {
         @Override
         protected void showAssist(int showStyle) {
+            if (showStyle == 1 && !editor.getActivePreferenceStore().getBoolean(SQLPreferenceConstants.ENABLE_AUTO_ACTIVATION)) {
+                return;
+            }
             SQLCompletionProcessor.setSimpleMode(true);
             try {
                 super.showAssist(showStyle);
