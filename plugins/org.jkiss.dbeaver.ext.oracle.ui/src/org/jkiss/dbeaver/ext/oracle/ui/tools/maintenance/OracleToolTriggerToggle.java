28366bb3549dc0b91ad93f210baf88f964545a07
LonwoLonwo <anvolka@yandex.ru>
2021-04-26 19:44:00 +0300

    #11312 disable/enable table trigger tool added
    Former-commit-id: a7bde72343259ac98707bf268561a838b724544b

---
+++ plugins/org.jkiss.dbeaver.ext.oracle.ui/src/org/jkiss/dbeaver/ext/oracle/ui/tools/maintenance/OracleToolTriggerToggle.java
@@ -0,0 +1,54 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2021 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jkiss.dbeaver.ext.oracle.ui.tools.maintenance;
+
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.jkiss.dbeaver.model.struct.DBSObject;
+import org.jkiss.dbeaver.tasks.ui.wizard.TaskConfigurationWizardDialog;
+import org.jkiss.dbeaver.ui.navigator.NavigatorUtils;
+import org.jkiss.dbeaver.ui.tools.IUserInterfaceTool;
+
+import java.util.Collection;
+
+public abstract class OracleToolTriggerToggle implements IUserInterfaceTool {
+
+    private boolean isEnable;
+
+    public OracleToolTriggerToggle(boolean isEnable) {
+        this.isEnable = isEnable;
+    }
+
+    @Override
+    public void execute(IWorkbenchWindow window, IWorkbenchPart activePart, Collection<DBSObject> objects) {
+        if (isEnable) {
+            TaskConfigurationWizardDialog.openNewTaskDialog(
+                window,
+                NavigatorUtils.getSelectedProject(),
+                "oracleToolTriggerEnable",
+                new StructuredSelection(objects.toArray()));
+        } else {
+            TaskConfigurationWizardDialog.openNewTaskDialog(
+                window,
+                NavigatorUtils.getSelectedProject(),
+                "oracleToolTriggerDisable",
+                new StructuredSelection(objects.toArray()));
+        }
+    }
+
+}
