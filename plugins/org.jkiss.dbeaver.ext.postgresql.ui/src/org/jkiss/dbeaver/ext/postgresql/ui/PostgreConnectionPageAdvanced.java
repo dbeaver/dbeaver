a606c158cc5b077be3d4793d87338a414a776e0e
serge-rider <serge@jkiss.org>
2021-05-05 22:19:33 +0300

    #11727 PG database statistics read + configuration
    Former-commit-id: 4e782e1f71e3d35851c0dcc09c3b726860e1056e

--- plugins/org.jkiss.dbeaver.ext.postgresql.ui/src/org/jkiss/dbeaver/ext/postgresql/ui/PostgreConnectionPageAdvanced.java
+++ plugins/org.jkiss.dbeaver.ext.postgresql.ui/src/org/jkiss/dbeaver/ext/postgresql/ui/PostgreConnectionPageAdvanced.java
@@ -47,6 +47,7 @@ public class PostgreConnectionPageAdvanced extends ConnectionPageAbstract
     private Button showNonDefault;
     private Button showTemplates;
     private Button showUnavailable;
+    private Button showDatabaseStatistics;
     private Button readAllDataTypes;
     private Button usePreparedStatements;
     private Combo ddPlainBehaviorCombo;
@@ -89,6 +90,7 @@ public class PostgreConnectionPageAdvanced extends ConnectionPageAbstract
             });
             showTemplates = UIUtils.createCheckbox(secureGroup, PostgreMessages.dialog_setting_connection_show_templates, PostgreMessages.dialog_setting_connection_show_templates_tip, false, 2);
             showUnavailable = UIUtils.createCheckbox(secureGroup, PostgreMessages.dialog_setting_connection_show_not_available_for_conn, PostgreMessages.dialog_setting_connection_show_not_available_for_conn_tip, false, 2);
+            showDatabaseStatistics = UIUtils.createCheckbox(secureGroup, PostgreMessages.dialog_setting_connection_database_statistics, PostgreMessages.dialog_setting_connection_database_statistics_tip, false, 2);
             readAllDataTypes = UIUtils.createCheckbox(secureGroup, PostgreMessages.dialog_setting_connection_read_all_data_types, PostgreMessages.dialog_setting_connection_read_all_data_types_tip, false, 2);
         }
 
@@ -148,6 +150,9 @@ public class PostgreConnectionPageAdvanced extends ConnectionPageAbstract
             CommonUtils.getBoolean(connectionInfo.getProviderProperty(PostgreConstants.PROP_SHOW_UNAVAILABLE_DB),
             globalPrefs.getBoolean(PostgreConstants.PROP_SHOW_UNAVAILABLE_DB)));
         showUnavailable.setEnabled(showNonDefault.getSelection());
+        showDatabaseStatistics.setSelection(
+            CommonUtils.getBoolean(connectionInfo.getProviderProperty(PostgreConstants.PROP_SHOW_DATABASE_STATISTICS),
+                globalPrefs.getBoolean(PostgreConstants.PROP_SHOW_DATABASE_STATISTICS)));
         readAllDataTypes.setSelection(
                 CommonUtils.getBoolean(connectionInfo.getProviderProperty(PostgreConstants.PROP_READ_ALL_DATA_TYPES),
                         globalPrefs.getBoolean(PostgreConstants.PROP_READ_ALL_DATA_TYPES)));
@@ -172,6 +177,7 @@ public class PostgreConnectionPageAdvanced extends ConnectionPageAbstract
         connectionCfg.setProviderProperty(PostgreConstants.PROP_SHOW_NON_DEFAULT_DB, String.valueOf(showNonDefault.getSelection()));
         connectionCfg.setProviderProperty(PostgreConstants.PROP_SHOW_TEMPLATES_DB, String.valueOf(showTemplates.getSelection()));
         connectionCfg.setProviderProperty(PostgreConstants.PROP_SHOW_UNAVAILABLE_DB, String.valueOf(showUnavailable.getSelection()));
+        connectionCfg.setProviderProperty(PostgreConstants.PROP_SHOW_DATABASE_STATISTICS, String.valueOf(showDatabaseStatistics.getSelection()));
         connectionCfg.setProviderProperty(PostgreConstants.PROP_READ_ALL_DATA_TYPES, String.valueOf(readAllDataTypes.getSelection()));
         if (usePreparedStatements != null) {
             connectionCfg.setProviderProperty(PostgreConstants.PROP_USE_PREPARED_STATEMENTS, String.valueOf(usePreparedStatements.getSelection()));
