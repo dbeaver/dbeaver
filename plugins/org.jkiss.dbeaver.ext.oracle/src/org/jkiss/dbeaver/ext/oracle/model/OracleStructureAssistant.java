da3bc779997310cf94df371cc1fdf94f8df6576e
Nikita Akilov <nikita.n.akilov@gmail.com>
2021-05-24 17:00:26 +0300

    #11327 code review fixes

--- plugins/org.jkiss.dbeaver.ext.oracle/src/org/jkiss/dbeaver/ext/oracle/model/OracleStructureAssistant.java
+++ plugins/org.jkiss.dbeaver.ext.oracle/src/org/jkiss/dbeaver/ext/oracle/model/OracleStructureAssistant.java
@@ -356,7 +356,7 @@ public class OracleStructureAssistant implements DBSStructureAssistant<OracleExe
     }
 
     private boolean searchInSynonyms() {
-        String prop = dataSource.getContainer().getConnectionConfiguration().getProviderProperty(OracleConstants.PROP_SEARCH_METADATA_IN_SYNONYMS);
-        return CommonUtils.getBoolean(prop);
+        String property = dataSource.getContainer().getConnectionConfiguration().getProperty(OracleConstants.PROP_SEARCH_METADATA_IN_SYNONYMS);
+        return CommonUtils.getBoolean(property);
     }
 }
