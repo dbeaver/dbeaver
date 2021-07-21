de66dd5990d845579cd921a7120708a9000c4fc5
Nikita Akilov <nikita.n.akilov@gmail.com>
2021-07-21 17:36:43 +0300

    #12837 completely disable filter text (otherwise it is possible to set focus on it and even use autocomplete)

--- plugins/org.jkiss.dbeaver.ui.editors.data/src/org/jkiss/dbeaver/ui/controls/resultset/ResultSetFilterPanel.java
+++ plugins/org.jkiss.dbeaver.ui.editors.data/src/org/jkiss/dbeaver/ui/controls/resultset/ResultSetFilterPanel.java
@@ -395,7 +395,7 @@ class ResultSetFilterPanel extends Composite implements IContentProposalProvider
             this.filterExpandPanel.setEnabled(enable);
             refreshButton.setEnabled(enable);
             historyPanel.setEnabled(enable);
-            filtersText.setEditable(enable && viewer.supportsDataFilter());
+            filtersText.setEnabled(enable && viewer.supportsDataFilter());
             executePanel.setEnabled(enable);
         } finally {
             setRedraw(true);
