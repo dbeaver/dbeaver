3df9da229c970770a6bfcfc8117399b212e18fbd
liuyizhuo <liuyizhuo.lyz@alipay.com>
2021-07-22 21:07:36 +0800

    Add tenant module
    Override connectionPage to add tenant module into it. Override AuthDatabaseNative.

---
+++ plugins/org.jkiss.dbeaver.ext.oceanbase/src/org/jkiss/dbeaver/ext/oceanbase/model/auth/OceanbaseAuthModelDatabaseNative.java
@@ -0,0 +1,31 @@
+package org.jkiss.dbeaver.ext.oceanbase.model.auth;
+
+import java.util.Properties;
+
+import org.jkiss.code.NotNull;
+import org.jkiss.dbeaver.DBException;
+import org.jkiss.dbeaver.model.DBPDataSource;
+import org.jkiss.dbeaver.model.connection.DBPConnectionConfiguration;
+import org.jkiss.dbeaver.model.impl.auth.AuthModelDatabaseNative;
+import org.jkiss.dbeaver.model.impl.auth.AuthModelDatabaseNativeCredentials;
+import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
+import org.jkiss.utils.CommonUtils;
+
+public class OceanbaseAuthModelDatabaseNative extends AuthModelDatabaseNative {
+	
+	public static final String ID = "oceanbase_native";
+	@Override
+    public Object initAuthentication(@NotNull DBRProgressMonitor monitor, @NotNull DBPDataSource dataSource, AuthModelDatabaseNativeCredentials credentials, DBPConnectionConfiguration configuration, @NotNull Properties connProperties) throws DBException {
+		String userName = configuration.getUserName();
+		System.out.println("current user is " + userName);
+		if (!CommonUtils.isEmpty(userName) ) {
+			if (!userName.contains("@")) {
+				userName += "@" + configuration.getServerName();
+			}
+        }
+
+        credentials.setUserName(userName);
+        return super.initAuthentication(monitor, dataSource, credentials, configuration, connProperties);
+	}
+
+}
