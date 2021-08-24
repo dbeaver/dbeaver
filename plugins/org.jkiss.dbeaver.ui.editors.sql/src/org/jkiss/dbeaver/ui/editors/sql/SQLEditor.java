bfd9e1ed9a0b021093679f09feaed8f190601c06
Serge Rider <serge@jkiss.org>
2021-08-24 16:48:05 +0300

    Revert "Revert "#12464 do not open new result tab if no result tabs are open when executing explain" Side effect: resultset toolbar is disabled. Need better solution"
    This reverts commit ea002c33

--- plugins/org.jkiss.dbeaver.ui.editors.sql/src/org/jkiss/dbeaver/ui/editors/sql/SQLEditor.java
+++ plugins/org.jkiss.dbeaver.ui.editors.sql/src/org/jkiss/dbeaver/ui/editors/sql/SQLEditor.java
@@ -238,7 +238,7 @@ public class SQLEditor extends SQLEditorBase implements
             if (resultTabs.getItemCount() == 0) {
                 if (resultsSash.getMaximizedControl() == null) {
                     // Hide results
-                    toggleResultPanel(false);
+                    toggleResultPanel(false, true);
                 }
             }
         }
@@ -1383,7 +1383,7 @@ public class SQLEditor extends SQLEditorBase implements
         } else {
             sqlExtraPanelSash.setMaximizedControl(sqlExtraPanelSash.getChildren()[0]);
             // Show results
-            showResultsPanel();
+            showResultsPanel(true);
         }
 
         if (view == outputViewer.getControl()) {
@@ -1643,7 +1643,7 @@ public class SQLEditor extends SQLEditorBase implements
         return presentationStack.getChildren()[EXTRA_CONTROL_INDEX];
     }
 
-    public void toggleResultPanel(boolean switchFocus) {
+    public void toggleResultPanel(boolean switchFocus, boolean createQueryProcessor) {
         UIUtils.syncExec(() -> {
             if (resultsSash.getMaximizedControl() == null) {
                 resultsSash.setMaximizedControl(sqlEditorPanel);
@@ -1651,7 +1651,7 @@ public class SQLEditor extends SQLEditorBase implements
             } else {
                 // Show both editor and results
                 // Check for existing query processors (maybe all result tabs were closed)
-                if (resultTabs.getItemCount() == 0) {
+                if (resultTabs.getItemCount() == 0 && createQueryProcessor) {
                     createQueryProcessor(true, true);
                 }
 
@@ -2006,7 +2006,7 @@ public class SQLEditor extends SQLEditorBase implements
     }
 
     private void explainQueryPlan(SQLQuery sqlQuery) {
-        showResultsPanel();
+        showResultsPanel(false);
         DBCQueryPlanner planner = GeneralUtils.adapt(getDataSource(), DBCQueryPlanner.class);
 
         DBCPlanStyle planStyle = planner.getPlanStyle();
@@ -2024,9 +2024,9 @@ public class SQLEditor extends SQLEditorBase implements
         }
     }
 
-    private void showResultsPanel() {
+    private void showResultsPanel(boolean createQueryProcessor) {
         if (resultsSash.getMaximizedControl() != null) {
-            toggleResultPanel(false);
+            toggleResultPanel(false, createQueryProcessor);
         }
         UIUtils.syncExec(() -> {
             if (resultsSash.isDownHidden()) {
@@ -3992,7 +3992,7 @@ public class SQLEditor extends SQLEditorBase implements
     }
 
     private void runPostExecuteActions(@Nullable SQLQueryResult result) {
-        showResultsPanel();
+        showResultsPanel(true);
 
         final DBCExecutionContext executionContext = getExecutionContext();
         if (executionContext != null) {
