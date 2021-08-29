a776da8761578c2d0791d29e952cd48017a1bfbf
Anastasiya Volkova <anvolka@yandex.ru>
2021-08-29 22:55:19 +0300

    #13392 ability to update spatial data in table in Exasol added; geometry type update statement quotation fixed

---
+++ plugins/org.jkiss.dbeaver.ext.exasol/src/org/jkiss/dbeaver/ext/exasol/model/ExasolGeometryValueHandler.java
@@ -0,0 +1,33 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2021 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jkiss.dbeaver.ext.exasol.model;
+
+import org.jkiss.code.NotNull;
+import org.jkiss.dbeaver.data.gis.handlers.GISGeometryValueHandler;
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCPreparedStatement;
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCSession;
+import org.locationtech.jts.geom.Geometry;
+
+import java.sql.SQLException;
+
+public class ExasolGeometryValueHandler extends GISGeometryValueHandler {
+
+    protected void bindGeometryParameter(@NotNull JDBCSession session, @NotNull JDBCPreparedStatement statement, int paramIndex, @NotNull Geometry value) throws SQLException {
+        statement.setString(paramIndex, value.toString()); // Just convert to string for Exasol (doesn't work with bytes)
+    }
+
+}
