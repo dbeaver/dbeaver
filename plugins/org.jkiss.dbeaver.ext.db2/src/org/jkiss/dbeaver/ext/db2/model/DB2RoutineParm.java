bba793bf577193bf795f10d5eeb1aeace5cf6241
Nikita Akilov <26031301+akilovich@users.noreply.github.com>
2021-09-08 18:00:23 +0300

    #13838 generate name for anonymous routine parameter

--- plugins/org.jkiss.dbeaver.ext.db2/src/org/jkiss/dbeaver/ext/db2/model/DB2RoutineParm.java
+++ plugins/org.jkiss.dbeaver.ext.db2/src/org/jkiss/dbeaver/ext/db2/model/DB2RoutineParm.java
@@ -42,6 +42,7 @@ import java.sql.ResultSet;
  * @author Denis Forveille
  */
 public class DB2RoutineParm implements DBSProcedureParameter, DBSTypedObject, DBSTypedObjectEx {
+    private static final String UNNAMED_PARAM_PREFIX = "parameter#";
 
     private final DB2Routine  procedure;
     private String            name;
@@ -67,7 +68,14 @@ public class DB2RoutineParm implements DBSProcedureParameter, DBSTypedObject, DB
 
         DB2DataSource db2DataSource = getDataSource();
 
-        this.name = JDBCUtils.safeGetStringTrimmed(dbResult, "PARMNAME");
+        String parmName = JDBCUtils.safeGetStringTrimmed(dbResult, "PARMNAME");
+        if (parmName == null) {
+            // Some parameters (e.g. all parameters in system-defined routines) may not have a name. Let's name them based on their ordinal
+            name = UNNAMED_PARAM_PREFIX + JDBCUtils.safeGetInt(dbResult, "ORDINAL");
+        } else {
+            name = parmName;
+        }
+
         this.scale = JDBCUtils.safeGetInteger(dbResult, "SCALE");
         this.length = JDBCUtils.safeGetInteger(dbResult, "LENGTH");
         this.remarks = JDBCUtils.safeGetStringTrimmed(dbResult, "REMARKS");
