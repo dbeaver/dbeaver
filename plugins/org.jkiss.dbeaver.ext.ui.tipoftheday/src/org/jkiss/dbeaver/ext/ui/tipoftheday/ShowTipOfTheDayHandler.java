c5ede4c4f44e2f5b8630fe07cddbf115831911de
Serge Rider <serge@jkiss.org>
2021-10-01 19:15:20 +0300

    #14105 Fix CVEs

--- plugins/org.jkiss.dbeaver.ext.ui.tipoftheday/src/org/jkiss/dbeaver/ext/ui/tipoftheday/ShowTipOfTheDayHandler.java
+++ plugins/org.jkiss.dbeaver.ext.ui.tipoftheday/src/org/jkiss/dbeaver/ext/ui/tipoftheday/ShowTipOfTheDayHandler.java
@@ -28,6 +28,7 @@ import org.jkiss.dbeaver.runtime.DBWorkbench;
 import org.jkiss.utils.CommonUtils;
 import org.xml.sax.SAXException;
 
+import javax.xml.XMLConstants;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
@@ -88,7 +89,10 @@ public class ShowTipOfTheDayHandler extends AbstractHandler {
             try (InputStream tipsInputStream = url.openConnection().getInputStream()) {
 
                 SAXParserFactory factory = SAXParserFactory.newInstance();
+                factory.setFeature( XMLConstants.FEATURE_SECURE_PROCESSING, true );
+
                 SAXParser saxParser = factory.newSAXParser();
+
 
                 TipsXmlHandler handler = new TipsXmlHandler();
                 saxParser.parse(tipsInputStream, handler);
