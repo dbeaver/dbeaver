5b26242cdf4b42184f2b679a48cfdb410cb10042
serge-rider <serge@jkiss.org>
2021-10-10 14:34:56 +0300

    dbeaver/dbeaver-ee#1078 ERD diagram change listener

--- plugins/org.jkiss.dbeaver.erd.ui/src/org/jkiss/dbeaver/erd/ui/model/ERDModelAdapterDefault.java
+++ plugins/org.jkiss.dbeaver.erd.ui/src/org/jkiss/dbeaver/erd/ui/model/ERDModelAdapterDefault.java
@@ -18,6 +18,9 @@ package org.jkiss.dbeaver.erd.ui.model;
 
 import org.eclipse.gef3.EditPart;
 import org.eclipse.gef3.Request;
+import org.jkiss.dbeaver.erd.ui.editor.ERDEditorPart;
+
+import java.beans.PropertyChangeEvent;
 
 /**
  * Empty model adapter
@@ -36,6 +39,11 @@ public class ERDModelAdapterDefault implements ERDModelAdapter {
 
     @Override
     public void performPartRequest(EditPart editPart, Request request) {
+
+    }
+
+    @Override
+    public void handlePropertyChange(ERDEditorPart editorPart, PropertyChangeEvent event) {
 
     }
 }
