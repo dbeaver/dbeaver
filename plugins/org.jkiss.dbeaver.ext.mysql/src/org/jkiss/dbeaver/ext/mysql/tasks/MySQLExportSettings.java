c16995d32e3d98af0d68db15385f8732fcfcbeda
ShadelessFox <yanuari.channel@gmail.com>
2021-10-19 19:29:06 +0300

    #12389 Add time components variables for native tools + data transfer

--- plugins/org.jkiss.dbeaver.ext.mysql/src/org/jkiss/dbeaver/ext/mysql/tasks/MySQLExportSettings.java
+++ plugins/org.jkiss.dbeaver.ext.mysql/src/org/jkiss/dbeaver/ext/mysql/tasks/MySQLExportSettings.java
@@ -330,9 +330,8 @@ public class MySQLExportSettings extends AbstractImportExportSettings<DBSObject>
                 case NativeToolUtils.VARIABLE_DATE:
                     return RuntimeUtils.getCurrentDate();
                 default:
-                    System.getProperty(name);
+                    return NativeToolUtils.replaceVariables(name);
             }
-            return null;
         });
         return new File(getOutputFolder(), outFileName);
     }
