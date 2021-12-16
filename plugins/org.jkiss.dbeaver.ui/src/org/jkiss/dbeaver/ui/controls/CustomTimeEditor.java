a402f5e54a52461da322bb8cd2c4c2b42c49e149
Georgi Gvinepadze <fireshowel@gmail.com>
2021-12-16 13:28:26 +0300

    #12010 Revert "Revert time editor (for ea)"

--- plugins/org.jkiss.dbeaver.ui/src/org/jkiss/dbeaver/ui/controls/CustomTimeEditor.java
+++ plugins/org.jkiss.dbeaver.ui/src/org/jkiss/dbeaver/ui/controls/CustomTimeEditor.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2021 DBeaver Corp and others
+ * Copyright (C) 2010-2017 Serge Rider (serge@jkiss.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,62 +16,112 @@
  */
 package org.jkiss.dbeaver.ui.controls;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Map;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.DateTime;
 import org.jkiss.code.Nullable;
 import org.jkiss.dbeaver.DBException;
+import org.jkiss.dbeaver.Log;
+import org.jkiss.dbeaver.model.impl.data.formatters.TimestampFormatSample;
 
 /**
  * CustomTimeEditor
  */
 public class CustomTimeEditor {
-    private Text timeEditor;
-//    private Button timePickerButton;
+	private final static String FORMAT_PATTERN = "pattern";
+	private DateTime dateEditor;
+	private DateTime timeEditor;
+	private Composite basePart;
+	private static final String TIMESTAMP_DEFAULT_FORMAT = "yyyy-MM-dd HH:mm:ss";
+	private String format = "";
 
-    public CustomTimeEditor(Composite parent, int style) {
-        this.timeEditor = new Text(parent, style);
-/*
-        Composite ph = UIUtils.createPlaceholder(parent, 2);
-        this.timeEditor = new Text(ph, style);
+	private static final Log log = Log.getLog(ViewerColumnController.class);
 
-        this.timePickerButton = new Button(ph, SWT.FLAT | SWT.ARROW | SWT.DOWN);
-        this.timePickerButton.addSelectionListener(new SelectionAdapter() {
-            @Override
-            public void widgetSelected(SelectionEvent e) {
-                super.widgetSelected(e);
-                UIUtils.showMessageBox(timePickerButton.getShell(), "asdf", "sdf", SWT.ICON_INFORMATION);
-            }
-        });
-*/
-    }
+	public CustomTimeEditor(Composite parent, int style) {
+		basePart = new Composite(parent, SWT.BORDER);
+		GridLayout layout = new GridLayout(2, false);
+		layout.marginHeight = 1;
+		layout.marginWidth = 1;
+		basePart.setLayout(layout);
 
-    public void setValue(@Nullable String value) {
-        if (value == null) {
-            timeEditor.setText("");
-        } else {
-            timeEditor.setText(value);
-        }
-    }
+		this.dateEditor = new DateTime(basePart, SWT.DATE | SWT.DROP_DOWN | style);
+		GridData layoutDataDate = new GridData(GridData.FILL, GridData.FILL, true, true, 1, 1);
+		GridData layoutDataTime = new GridData(GridData.END, GridData.FILL, true, true, 1, 1);
+		this.dateEditor.setLayoutData(layoutDataDate);
+		this.timeEditor = new DateTime(basePart, SWT.TIME | SWT.DROP_DOWN | style);
+		this.timeEditor.setLayoutData(layoutDataTime);
+		this.format = getTimestampFormat();
+	}
 
-    public String getValue()
-        throws DBException {
-        final String timeText = timeEditor.getText();
-        if (timeText.isEmpty()) {
-            return null;
-        }
-        return timeText;
-    }
+	public void addSelectionAdapter(SelectionAdapter listener) {
+		if (dateEditor != null && !dateEditor.isDisposed()) {
+			dateEditor.addSelectionListener(listener);
+		}
+		if (timeEditor != null && !timeEditor.isDisposed()) {
+			timeEditor.addSelectionListener(listener);
+		}
+	}
 
+	private String getTimestampFormat() {
+		TimestampFormatSample prefFormatt = new TimestampFormatSample();
+		Map<Object, Object> map = prefFormatt.getDefaultProperties(Locale.getDefault());
+		Object pattern = map.get(FORMAT_PATTERN);
+		if (pattern instanceof String) {
+			format = (String) pattern;
+			return format;
+		}
+		return TIMESTAMP_DEFAULT_FORMAT;
+	}
 
-    public void setEditable(boolean editable) {
-        timeEditor.setEditable(editable);
-    }
+	public void setValue(@Nullable String value) {
+		Calendar calendar = Calendar.getInstance();
+		if (value != null) {
+			try {
+				Date date = new SimpleDateFormat(format).parse((String) value);
+				calendar.setTime(date);
+			} catch (ParseException e) {
+				log.error("Input value is null", e);
+			}
 
-    public Text getControl() {
-        return timeEditor;
-    }
+		}
+		dateEditor.setDate(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+				calendar.get(Calendar.DAY_OF_MONTH));
+		timeEditor.setTime(calendar.get(Calendar.HOUR), calendar.get(Calendar.MINUTE), calendar.get(Calendar.SECOND));
+	}
 
-    public void selectAll() {
-        timeEditor.selectAll();
-    }
+	public String getValue() throws DBException {
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(dateEditor.getYear(), dateEditor.getMonth(), dateEditor.getDay(), timeEditor.getHours(),
+				timeEditor.getMinutes(), timeEditor.getSeconds());
+
+		return new SimpleDateFormat(format).format(calendar.getTime());
+
+	}
+
+	public void setEditable(boolean editable) {
+		if (this.dateEditor != null && !this.dateEditor.isDisposed()) {
+			this.dateEditor.setEnabled(editable);
+		}
+
+		if (this.timeEditor != null && !this.timeEditor.isDisposed()) {
+			this.timeEditor.setEnabled(editable);
+		}
+	}
+
+	public Composite getControl() {
+		return basePart;
+	}
+
+	public void selectAll() {
+	}
 }
