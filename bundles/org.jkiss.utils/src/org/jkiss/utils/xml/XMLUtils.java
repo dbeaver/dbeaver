b635ec491269ba18fcd347f895187bd70824f3cf
DBeaver Team <26031301+akilovich@users.noreply.github.com>
2022-01-03 19:33:27 +0400

    dbeaver/dbeaver-ee#1166 set secure processing feature before creating builder

--- bundles/org.jkiss.utils/src/org/jkiss/utils/xml/XMLUtils.java
+++ bundles/org.jkiss.utils/src/org/jkiss/utils/xml/XMLUtils.java
@@ -62,8 +62,8 @@ public class XMLUtils {
     public static Document parseDocument(InputSource source) throws XMLException {
         try {
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            DocumentBuilder xmlBuilder = dbf.newDocumentBuilder();
             dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+            DocumentBuilder xmlBuilder = dbf.newDocumentBuilder();
             return xmlBuilder.parse(source);
         } catch (Exception er) {
             throw new XMLException("Error parsing XML document", er);
