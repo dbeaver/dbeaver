0595dec7509132bc87c513f10e13173db49b093f
serge-rider <serge@jkiss.org>
2022-01-24 22:14:14 +0300

    Loging monitor usage

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/runtime/LoggingProgressMonitor.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/runtime/LoggingProgressMonitor.java
@@ -17,6 +17,7 @@
 package org.jkiss.dbeaver.model.runtime;
 
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.jkiss.dbeaver.Log;
 
 import java.io.PrintStream;
 
@@ -25,17 +26,30 @@ import java.io.PrintStream;
  */
 public class LoggingProgressMonitor extends DefaultProgressMonitor {
 
+    public LoggingProgressMonitor(Log log) {
+        super(new LoggingMonitorProxy(log));
+    }
+
     public LoggingProgressMonitor() {
-        super(new LoggingMonitorProxy());
+        super(new LoggingMonitorProxy(null));
     }
 
     private static class LoggingMonitorProxy implements IProgressMonitor {
 
+        private final Log log;
         private PrintStream out = System.out;
+
+        public LoggingMonitorProxy(Log log) {
+            this.log = log;
+        }
 
         @Override
         public void beginTask(String name, int totalWork) {
-            out.println(name);
+            if (log != null) {
+                log.debug(name);
+            } else {
+                out.println(name);
+            }
         }
 
         @Override
@@ -65,7 +79,11 @@ public class LoggingProgressMonitor extends DefaultProgressMonitor {
 
         @Override
         public void subTask(String name) {
-            out.println("\t" + name);
+            if (log != null) {
+                log.debug("\t" + name);
+            } else {
+                out.println("\t" + name);
+            }
         }
 
         @Override
