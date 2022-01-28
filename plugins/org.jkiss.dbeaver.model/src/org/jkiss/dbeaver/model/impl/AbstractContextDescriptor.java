66612296f2dc5cf080694549300fbc796b8259c3
serge-rider <serge@jkiss.org>
2022-01-28 14:21:30 +0300

    CB-1580 CB handlers model

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/impl/AbstractContextDescriptor.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/impl/AbstractContextDescriptor.java
@@ -26,7 +26,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * ToolDescriptor
+ * AbstractContextDescriptor
  */
 public abstract class AbstractContextDescriptor extends AbstractDescriptor
 {
