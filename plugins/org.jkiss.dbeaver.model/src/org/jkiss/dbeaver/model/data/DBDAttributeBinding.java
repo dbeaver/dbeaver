d268f2bf66b815d6dc023f437d0f14cdae860c67
Serge Rider <serge@jkiss.org>
2022-02-16 11:28:11 +0300

    dbeaver/dbeaver-ee#1326 Complex attribute transform fix

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/data/DBDAttributeBinding.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/data/DBDAttributeBinding.java
@@ -310,7 +310,7 @@ public abstract class DBDAttributeBinding implements DBSObject, DBSAttributeBase
         DBSAttributeBase attribute = getAttribute();
         final DBDAttributeTransformer[] transformers = DBVUtils.findAttributeTransformers(this, null);
         if (transformers != null) {
-            session.getProgressMonitor().subTask("Transform attribute '" + attribute.getName() + "'");
+            //session.getProgressMonitor().subTask("Transform attribute '" + attribute.getName() + "'");
             final Map<String, Object> transformerOptions = DBVUtils.getAttributeTransformersOptions(this);
             for (DBDAttributeTransformer transformer : transformers) {
                 transformer.transformAttribute(session, this, rows, transformerOptions);
--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/data/DBDAttributeBindingType.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/data/DBDAttributeBindingType.java
@@ -27,6 +27,7 @@ import org.jkiss.dbeaver.model.struct.*;
 import org.jkiss.utils.CommonUtils;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Type attribute value binding info
@@ -150,6 +151,8 @@ public class DBDAttributeBindingType extends DBDAttributeBindingNested implement
         }
         if (ownerValue instanceof DBDComposite) {
             return ((DBDComposite) ownerValue).getAttributeValue(attribute);
+        } else if (ownerValue instanceof Map) {
+            return ((Map<?, ?>) ownerValue).get(getName());
         }
 
         DBDAttributeBinding parent = getParent(1);
