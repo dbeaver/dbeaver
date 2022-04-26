5b93ff416b4ce8496035fcf6a9f45fd7f0ced5e0
serge-rider <serge@jkiss.org>
2022-04-26 18:06:09 +0300

    dbeaver/dbeaver-ee#1257 Node action renderer fix

--- plugins/org.jkiss.dbeaver.ui.navigator/src/org/jkiss/dbeaver/ui/navigator/database/StatisticsNavigatorNodeRenderer.java
+++ plugins/org.jkiss.dbeaver.ui.navigator/src/org/jkiss/dbeaver/ui/navigator/database/StatisticsNavigatorNodeRenderer.java
@@ -219,7 +219,7 @@ public class StatisticsNavigatorNodeRenderer extends DefaultNavigatorNodeRendere
                 boolean isSelected = (event.stateMask & SWT.SELECTED) != 0;
                 boolean isHover = false;
                 if (!isSelected && !isHover) {
-                    continue;
+                    return null;
                 }
             }
             widthOccupied += 2; // Margin
@@ -313,7 +313,7 @@ public class StatisticsNavigatorNodeRenderer extends DefaultNavigatorNodeRendere
                 boolean isSelected = (event.stateMask & SWT.SELECTED) != 0;
                 boolean isHover = false;
                 if (!isSelected && !isHover) {
-                    continue;
+                    return widthOccupied;
                 }
             }
             widthOccupied += 2; // Margin
