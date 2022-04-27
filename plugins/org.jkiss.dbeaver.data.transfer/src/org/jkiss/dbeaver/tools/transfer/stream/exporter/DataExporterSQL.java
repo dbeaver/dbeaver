f8db9e850af40828e278cd5c21fde0c4c7ead783
CalmaKarma <54739936+CalmaKarma@users.noreply.github.com>
2022-04-27 20:52:52 +0800

    #16204 Append exported data to existing .sql files  (#16310)
    * make DataExporterSQL appendable, fix a bug that latter tables first SQL statement was appended to the end of the last SQL statement of the former table, instead of begin a new line.
    
    * Deleted redundant return statement.
    
    * Add comment on a empty method body.
    
    * Corrected code format. Changed the if else structure.
    
    * Changed back the comment.

--- plugins/org.jkiss.dbeaver.data.transfer/src/org/jkiss/dbeaver/tools/transfer/stream/exporter/DataExporterSQL.java
+++ plugins/org.jkiss.dbeaver.data.transfer/src/org/jkiss/dbeaver/tools/transfer/stream/exporter/DataExporterSQL.java
@@ -16,6 +16,7 @@
  */
 package org.jkiss.dbeaver.tools.transfer.stream.exporter;
 
+import org.jkiss.code.NotNull;
 import org.jkiss.dbeaver.DBException;
 import org.jkiss.dbeaver.Log;
 import org.jkiss.dbeaver.model.*;
@@ -29,6 +30,7 @@ import org.jkiss.dbeaver.model.sql.SQLUtils;
 import org.jkiss.dbeaver.model.sql.parser.SQLIdentifierDetector;
 import org.jkiss.dbeaver.model.struct.DBSDataManipulator;
 import org.jkiss.dbeaver.tools.transfer.DTUtils;
+import org.jkiss.dbeaver.tools.transfer.stream.IAppendableDataExporter;
 import org.jkiss.dbeaver.tools.transfer.stream.IStreamDataExporterSite;
 import org.jkiss.dbeaver.utils.ContentUtils;
 import org.jkiss.dbeaver.utils.GeneralUtils;
@@ -45,7 +47,7 @@ import java.util.stream.Stream;
 /**
  * SQL Exporter
  */
-public class DataExporterSQL extends StreamExporterAbstract {
+public class DataExporterSQL extends StreamExporterAbstract implements IAppendableDataExporter {
 
     private static final Log log = Log.getLog(DataExporterSQL.class);
 
@@ -225,7 +227,7 @@ public class DataExporterSQL extends StreamExporterAbstract {
                     }
                     sqlBuffer.append(";");
                 } else if (insertKeyword == InsertKeyword.INSERT_ALL && rowCount % rowsInStatement == 0) {
-                    sqlBuffer.append("\n").append(identifierCase.transform(KEYWORD_SELECT_FROM_DUAL)).append(";");
+                    sqlBuffer.append(rowDelimiter).append(identifierCase.transform(KEYWORD_SELECT_FROM_DUAL)).append(";");
                 }
                 if (lineBeforeRows) {
                     sqlBuffer.append(rowDelimiter);
@@ -372,14 +374,15 @@ public class DataExporterSQL extends StreamExporterAbstract {
     @Override
     public void exportFooter(DBRProgressMonitor monitor) {
         PrintWriter out = getWriter();
-        if (insertKeyword == InsertKeyword.INSERT_ALL) {
-            if (rowCount > 0) {
-                out.write("\n" + identifierCase.transform(KEYWORD_SELECT_FROM_DUAL) + ";");
-            }
-        } else if (!oneLineEntry){
-            if (rowCount > 0) {
+        if (rowCount > 0) {
+        	if (insertKeyword == InsertKeyword.INSERT_ALL) {
+                out.write(rowDelimiter + identifierCase.transform(KEYWORD_SELECT_FROM_DUAL) + ";");
+            } else if (!oneLineEntry) {
                 addOnConflictExpression(out);
                 out.write(";");
+                out.write(rowDelimiter);
+            } else {
+                out.write(rowDelimiter);
             }
         }
     }
@@ -417,7 +420,18 @@ public class DataExporterSQL extends StreamExporterAbstract {
             ContentUtils.close(reader);
         }
     }
-
+    
+    @Override
+    public void importData(@NotNull IStreamDataExporterSite site) {
+    	// This method is called before this.init().
+    	// No pre-initialization process is needed.
+    }
+    
+    @Override
+    public boolean shouldTruncateOutputFileBeforeExport() {
+        return false;
+    }
+    
     private SQLDialect.MultiValueInsertMode getDefaultMultiValueInsertMode() {
         SQLDialect.MultiValueInsertMode insertMode = SQLDialect.MultiValueInsertMode.NOT_SUPPORTED;
         if (dialect != null && rowsInStatement != 1) {
