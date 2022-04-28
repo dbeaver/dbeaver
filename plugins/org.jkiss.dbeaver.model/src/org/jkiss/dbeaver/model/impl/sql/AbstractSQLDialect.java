8adba762484b386a6aab4f3b2c8f98f5fe1cdf66
ChristineTT <ChristineTT@users.noreply.github.com>
2022-04-28 16:38:22 +0800

    Fix Words that start with underscore are treated as keywords

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/impl/sql/AbstractSQLDialect.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/impl/sql/AbstractSQLDialect.java
@@ -316,7 +316,7 @@ public abstract class AbstractSQLDialect implements SQLDialect {
 
     @Override
     public boolean isWordStart(int ch) {
-    	return Character.isUnicodeIdentifierStart(ch) || ch == '_';
+        return Character.isUnicodeIdentifierStart(ch) || ch == '_';
     }
 
     @Override
