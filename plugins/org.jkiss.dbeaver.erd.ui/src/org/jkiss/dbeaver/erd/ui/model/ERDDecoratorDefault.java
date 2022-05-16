a56da9857b4659c3fcf2502f4adf784a199b645e
Gvinepadze Georgy <fireshowel@gmail.com>
2022-05-16 17:19:51 +0300

    #15755 fix inverted condition

--- plugins/org.jkiss.dbeaver.erd.ui/src/org/jkiss/dbeaver/erd/ui/model/ERDDecoratorDefault.java
+++ plugins/org.jkiss.dbeaver.erd.ui/src/org/jkiss/dbeaver/erd/ui/model/ERDDecoratorDefault.java
@@ -72,7 +72,7 @@ public class ERDDecoratorDefault implements ERDDecorator {
     @Override
     public Insets getDefaultEntityInsets() {
         final DBPPreferenceStore store = ERDUIActivator.getDefault().getPreferences();
-        if (store.getString(ERDUIConstants.PREF_ROUTING_TYPE).equals(ERDUIConstants.ROUTING_MIKAMI)) {
+        if (!store.getString(ERDUIConstants.PREF_ROUTING_TYPE).equals(ERDUIConstants.ROUTING_MIKAMI)) {
             return new Insets(20, 20, 10, 20);
         } else {
             return new Insets(50, 50, 50, 50);
