f6bbe709ceb9df59b7a05de6abe538bcd19906ac
Serge Rider <serge@jkiss.org>
2022-06-20 18:27:00 +0200

    #16669 Do original column name resolution for condition clauses (#16832)
    * #16669 Do original column name resolution for condition clauses
    
    * #16669 Fix attribute name transform in WHERE

--- test/org.jkiss.dbeaver.ext.postgresql.test/src/org/jkiss/dbeaver/ext/postgresql/model/PostgreDialectFunctionsTest.java
+++ test/org.jkiss.dbeaver.ext.postgresql.test/src/org/jkiss/dbeaver/ext/postgresql/model/PostgreDialectFunctionsTest.java
@@ -106,7 +106,7 @@ public class PostgreDialectFunctionsTest {
         Mockito.when(mockAttributeBinding.getName()).thenReturn(column.getName());
         Mockito.when(mockAttributeBinding.getFullyQualifiedName(DBPEvaluationContext.DML)).thenReturn(column.getName());
 
-        String typeCastClause = postgreDialect.getCastedAttributeName(mockAttributeBinding);
+        String typeCastClause = postgreDialect.getCastedAttributeName(mockAttributeBinding, mockAttributeBinding.getName());
         String expectedTypeCast = "column1::text"; // We use this method only for column names in condition. JSON column name must be casted to text as in getTypeCastClause will be casted column data
         Assert.assertEquals(expectedTypeCast, typeCastClause);
     }
@@ -142,7 +142,7 @@ public class PostgreDialectFunctionsTest {
         Mockito.when(mockAttributeBinding.getName()).thenReturn(column.getName());
         Mockito.when(mockAttributeBinding.getFullyQualifiedName(DBPEvaluationContext.DML)).thenReturn(column.getName());
 
-        String typeCastClause = postgreDialect.getCastedAttributeName(mockAttributeBinding);
+        String typeCastClause = postgreDialect.getCastedAttributeName(mockAttributeBinding, mockAttributeBinding.getName());
         String expectedTypeCast = "column1::text"; // We use this method only for column names in condition. JSON column name must be casted to text as in getTypeCastClause will be casted column data
         Assert.assertEquals(expectedTypeCast, typeCastClause);
     }
