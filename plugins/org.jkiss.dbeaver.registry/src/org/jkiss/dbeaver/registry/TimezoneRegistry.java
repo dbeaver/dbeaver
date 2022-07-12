827f5f072c0925b2b021e27a47913122bd424842
Serge Rider <serge@jkiss.org>
2022-07-12 13:01:56 +0300

    dbeaver/dbeaver-ee#1730 Platform initialization sequence fix (logging)

--- plugins/org.jkiss.dbeaver.registry/src/org/jkiss/dbeaver/registry/TimezoneRegistry.java
+++ plugins/org.jkiss.dbeaver.registry/src/org/jkiss/dbeaver/registry/TimezoneRegistry.java
@@ -21,7 +21,6 @@ import org.jkiss.code.Nullable;
 import org.jkiss.dbeaver.ModelPreferences;
 import org.jkiss.dbeaver.model.DBConstants;
 import org.jkiss.dbeaver.model.preferences.DBPPreferenceStore;
-import org.jkiss.dbeaver.runtime.DBWorkbench;
 
 import java.time.Instant;
 import java.time.ZoneId;
@@ -38,7 +37,7 @@ public class TimezoneRegistry {
     }
 
     public static void setDefaultZone(@Nullable ZoneId id) {
-        DBPPreferenceStore preferenceStore = DBWorkbench.getPlatform().getPreferenceStore();
+        DBPPreferenceStore preferenceStore = ModelPreferences.getPreferences();
         if (id != null) {
             if (!TimeZone.getDefault().getID().equals(id.getId())) {
                 TimeZone.setDefault(TimeZone.getTimeZone(id));
@@ -56,7 +55,7 @@ public class TimezoneRegistry {
 
     public static void overrideTimezone() {
         userDefaultTimezone = System.getProperty("user.timezone");
-        DBPPreferenceStore preferenceStore = DBWorkbench.getPlatform().getPreferenceStore();
+        DBPPreferenceStore preferenceStore = ModelPreferences.getPreferences();
         final String timezone = preferenceStore.getString(ModelPreferences.CLIENT_TIMEZONE);
         if (timezone != null && !timezone.equals(DBConstants.DEFAULT_TIMEZONE)) {
             TimeZone.setDefault(TimeZone.getTimeZone(timezone));
