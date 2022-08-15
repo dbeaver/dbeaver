5c7177d27034d24fc9192b783dafcefbfa295214
Georgy Gvinepadze <fireshowel@gmail.com>
2022-08-15 11:28:20 +0300

    #17316 use label instead of flag

--- plugins/org.jkiss.dbeaver.registry/src/org/jkiss/dbeaver/registry/BaseProjectImpl.java
+++ plugins/org.jkiss.dbeaver.registry/src/org/jkiss/dbeaver/registry/BaseProjectImpl.java
@@ -286,22 +286,17 @@ public abstract class BaseProjectImpl implements DBPProject {
         synchronized (metadataSync) {
             final List<String> resources = new ArrayList<>();
 
-            for (var resource : resourceProperties.entrySet()) {
-                boolean containsRequiredProperties = true;
+            outer: for (var resource : resourceProperties.entrySet()) {
                 final Map<String, Object> props = resource.getValue();
-
                 for (var property : properties.entrySet()) {
                     final String propName = property.getKey();
                     final Object propValue = property.getValue();
 
                     if (!props.containsKey(propName) || !Objects.equals(props.get(propName), propValue)) {
-                        containsRequiredProperties = false;
-                        break;
+                        continue outer;
                     }
                 }
-                if (containsRequiredProperties) {
-                    resources.add(resource.getKey());
-                }
+                resources.add(resource.getKey());
             }
 
             return resources.toArray(String[]::new);
