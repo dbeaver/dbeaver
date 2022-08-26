59e43ea5f42ea747680ea6a08b109550e700dfba
George <fireshowel@gmail.com>
2022-08-26 13:21:57 +0300

    #16749 add complex values inline editing (#17382)
    * #16749 add complex values inline editing
    
    * dbeaver/dbeaver-ee#877 read only for array element cells
    
    Signed-off-by: Georgy Gvinepadze <fireshowel@gmail.com>
    
    Signed-off-by: Georgy Gvinepadze <fireshowel@gmail.com>

--- plugins/org.jkiss.dbeaver.ui.editors.data/src/org/jkiss/dbeaver/ui/data/managers/ComplexValueManager.java
+++ plugins/org.jkiss.dbeaver.ui.editors.data/src/org/jkiss/dbeaver/ui/data/managers/ComplexValueManager.java
@@ -22,6 +22,7 @@ import org.jkiss.dbeaver.ui.data.IValueController;
 import org.jkiss.dbeaver.ui.data.IValueEditor;
 import org.jkiss.dbeaver.ui.data.dialogs.DefaultValueViewDialog;
 import org.jkiss.dbeaver.ui.data.editors.ComplexValueInlineEditor;
+import org.jkiss.dbeaver.ui.data.editors.StringInlineEditor;
 
 /**
  * Complex value manager.
@@ -41,6 +42,8 @@ public abstract class ComplexValueManager extends BaseValueManager {
         switch (controller.getEditType()) {
             case PANEL:
                 return new ComplexValueInlineEditor(controller);
+            case INLINE:
+                return new StringInlineEditor(controller);
             case EDITOR:
                 return new DefaultValueViewDialog(controller);
             default:
