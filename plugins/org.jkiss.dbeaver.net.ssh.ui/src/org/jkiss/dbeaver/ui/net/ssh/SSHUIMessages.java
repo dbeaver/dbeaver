f035594d3e3dab8f1671f62dc95bb50b5597ead0
Georgii <fireshowel@gmail.com>
2022-09-20 16:23:11 +0400

    #15899 Improve ssh configuration page & improve password popup (#17369)
    * #15899 Improve ssh configuration page & improve password popup
    
    * #15899 suggestions from code review
    
    * #15899 remove native ssh dialog, use dbeaver one & fix wrong configuration override
    
    * #15899 remove passphrase
    
    * #15899 suggestions from code review
    
    * #15889 suggestions from code review

--- plugins/org.jkiss.dbeaver.net.ssh.ui/src/org/jkiss/dbeaver/ui/net/ssh/SSHUIMessages.java
+++ plugins/org.jkiss.dbeaver.net.ssh.ui/src/org/jkiss/dbeaver/ui/net/ssh/SSHUIMessages.java
@@ -54,9 +54,16 @@ public class SSHUIMessages extends NLS {
 	public static String model_ssh_configurator_variables_hint_label;
 	public static String model_ssh_configurator_ssh_documentation_link;
 
+
+    public static String model_ssh_dialog_credentials;
+    public static String model_ssh_dialog_credentials_username;
+    public static String model_ssh_dialog_credentials_password;
+    public static String model_ssh_dialog_credentials_passphrase;
+
     public static String jsch_remote_host_identifier_changed_warning_title;
 
-	static {
+
+    static {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, SSHUIMessages.class);
 	}
