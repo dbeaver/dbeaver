6e19d11fb9a408f74834a2e6c7490a80e22678ac
LonwoLonwo <anvolka@yandex.ru>
2022-10-07 10:38:48 +0300

    #16445 support of UUID MariaDB data type added

--- plugins/org.jkiss.dbeaver.ext.mysql/src/org/jkiss/dbeaver/ext/mysql/MySQLConstants.java
+++ plugins/org.jkiss.dbeaver.ext.mysql/src/org/jkiss/dbeaver/ext/mysql/MySQLConstants.java
@@ -214,6 +214,7 @@ public class MySQLConstants {
     public static final String TYPE_ENUM = "enum";
     public static final String TYPE_SET = "set";
     public static final String TYPE_VARBINARY = "VARBINARY";
+    public static final String TYPE_UUID = "uuid";
 
     public static final String BIN_FOLDER = "bin";
     public static final String ENV_VAR_MYSQL_PWD = "MYSQL_PWD";
