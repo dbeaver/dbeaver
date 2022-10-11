5ab937c8a616ce97b6f6cac71550d84bd8a37966
E1izabeth <kuzenkova.el@yandex.ru>
2022-10-11 14:46:57 +0300

    #17619 Add name escaping to quoteIdentifier method

--- plugins/org.jkiss.dbeaver.ext.mysql/src/org/jkiss/dbeaver/ext/mysql/model/MySQLDialect.java
+++ plugins/org.jkiss.dbeaver.ext.mysql/src/org/jkiss/dbeaver/ext/mysql/model/MySQLDialect.java
@@ -204,6 +204,13 @@ public class MySQLDialect extends JDBCSQLDialect {
     
     @NotNull
     @Override
+    protected String quoteIdentifier(@NotNull String str, @NotNull String[][] quoteStrings) {
+        // Escape with first (default) quote string
+        return quoteStrings[0][0] + escapeString(str) + quoteStrings[0][1];
+    }
+    
+    @NotNull
+    @Override
     public String escapeString(String string) {
         return string.replace("'", "''").replace("`", "``").replaceAll("\\\\(?![_%?])", "\\\\\\\\");
     }
