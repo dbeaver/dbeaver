dac7b82d6003a002fbd04bb20f7e5b83a5742896
ShadelessFox <yanuari.channel@gmail.com>
2022-10-14 15:11:01 +0200

    #8673 Code cleanup

--- plugins/org.jkiss.dbeaver.ext.oracle/src/org/jkiss/dbeaver/ext/oracle/data/OracleTimestampValueHandler.java
+++ plugins/org.jkiss.dbeaver.ext.oracle/src/org/jkiss/dbeaver/ext/oracle/data/OracleTimestampValueHandler.java
@@ -60,12 +60,9 @@ public class OracleTimestampValueHandler extends JDBCDateTimeValueHandler {
 
     @Override
     public Object fetchValueObject(@NotNull DBCSession session, @NotNull DBCResultSet resultSet, @NotNull DBSTypedObject type, int index) throws DBCException {
-        if (resultSet instanceof JDBCResultSet) {
-            final JDBCResultSet dbResults = (JDBCResultSet) resultSet;
+        if (resultSet instanceof JDBCResultSet && OracleConstants.TYPE_NAME_DATE.equals(type.getTypeName())) {
             try {
-                if (OracleConstants.TYPE_NAME_DATE.equals(type.getTypeName())) {
-                    return dbResults.getDate(index + 1);
-                }
+                return ((JDBCResultSet) resultSet).getDate(index + 1);
             } catch (SQLException e) {
                 log.debug("Exception caught when fetching date value", e);
             }
