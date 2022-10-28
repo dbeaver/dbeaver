6ac601c827b32651ec4afd9cc8ba9a2654a54d6a
Aleksandr Skoblikov <aleksandr.skoblikov@dbeaver.com>
2022-10-28 20:36:59 +0200

    CB-2450 increase qm events check period

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/runtime/qm/QMMCollectorImpl.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/runtime/qm/QMMCollectorImpl.java
@@ -50,7 +50,7 @@ public class QMMCollectorImpl extends DefaultExecutionHandler implements QMMColl
 
     private static final Log log = Log.getLog(QMMCollectorImpl.class);
 
-    private static final long EVENT_DISPATCH_PERIOD = 250;
+    private static final long EVENT_DISPATCH_PERIOD = 3000;
     private static final int MAX_HISTORY_EVENTS = 10000;
 
     // Session map
