074f9a4f561522e3173b43f82fcde79fe472f667
Nikita Akilov <26031301+n5a5@users.noreply.github.com>
2022-11-07 22:06:57 +0100

    dbeaver/dbeaver#18137 fix npe in ConnectionTestDialog

--- plugins/org.jkiss.dbeaver.ui/src/org/jkiss/dbeaver/ui/dialogs/BaseDialog.java
+++ plugins/org.jkiss.dbeaver.ui/src/org/jkiss/dbeaver/ui/dialogs/BaseDialog.java
@@ -21,6 +21,7 @@ import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
@@ -118,5 +119,12 @@ public class BaseDialog extends Dialog
         composite.setFont(parent.getFont());
 
         return composite;
+    }
+
+    // Overloaded just to add the @Nullable annotation
+    @Override
+    @Nullable
+    protected Button getButton(int id) {
+        return super.getButton(id);
     }
 }
