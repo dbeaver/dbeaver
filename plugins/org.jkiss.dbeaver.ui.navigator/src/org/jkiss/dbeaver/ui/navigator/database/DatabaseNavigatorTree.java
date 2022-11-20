24b40bf98176faa4a5ffad560ed24afe0b5737a8
Юрий Плотников <yuriy.plotnikov@rt.ru>
2022-11-20 20:45:19 +0300

    #9444 NPE fixed

--- plugins/org.jkiss.dbeaver.ui.navigator/src/org/jkiss/dbeaver/ui/navigator/database/DatabaseNavigatorTree.java
+++ plugins/org.jkiss.dbeaver.ui.navigator/src/org/jkiss/dbeaver/ui/navigator/database/DatabaseNavigatorTree.java
@@ -787,6 +787,8 @@ public class DatabaseNavigatorTree extends Composite implements INavigatorListen
                     }
                     patternShort = "*" + patternShort;
                     this.matcherShort = new TextMatcherExt(patternShort, true, false);
+                } else {
+                    this.dotPattern = null;
                 }
             } else {
                 super.setPattern(null);
