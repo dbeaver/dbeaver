80478f69a7ff497a9e09ec10a1c7cbdc7b3e078d
serge-rider <serge@jkiss.org>
2022-12-14 10:09:04 +0100

    dbeaver/pro#1253 Move content types definition to model. Split resource types and resource handlers

--- plugins/org.jkiss.dbeaver.model.sql/src/org/jkiss/dbeaver/model/sql/SQLContentTypeDescriber.java
+++ plugins/org.jkiss.dbeaver.model.sql/src/org/jkiss/dbeaver/model/sql/SQLContentTypeDescriber.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jkiss.dbeaver.ui.editors.sql;
+package org.jkiss.dbeaver.model.sql;
 
 import org.eclipse.core.internal.content.TextContentDescriber;
 
