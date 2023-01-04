af16e49cdf03cd51af37269a04a43066a86c140c
github.nouser@nouser.github.com <github.nouser@nouser.github.com>
2023-01-04 12:16:10 +0100

    Update DBCStatistics.java

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/exec/DBCStatistics.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/exec/DBCStatistics.java
@@ -104,6 +104,10 @@ public class DBCStatistics implements DBCExecutionResult {
         return startTime + getTotalTime();
     }
 
+    public long getStartTime(){
+        return this.startTime;
+    }
+
     public int getStatementsCount() {
         return statementsCount;
     }
