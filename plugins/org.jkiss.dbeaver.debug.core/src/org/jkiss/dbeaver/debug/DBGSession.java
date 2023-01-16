85ca6582673a63c727e812ca4fa1e030484df449
Georgii <fireshowel@gmail.com>
2023-01-16 16:59:44 +0400

    dbeaver/pro#1344 update Copyright (#18782)
    Signed-off-by: Georgii Gvinepadze <fireshowel@gmail.com>
    
    Signed-off-by: Georgii Gvinepadze <fireshowel@gmail.com>

--- plugins/org.jkiss.dbeaver.debug.core/src/org/jkiss/dbeaver/debug/DBGSession.java
+++ plugins/org.jkiss.dbeaver.debug.core/src/org/jkiss/dbeaver/debug/DBGSession.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2022 DBeaver Corp and others
+ * Copyright (C) 2010-2023 DBeaver Corp and others
  * Copyright (C) 2017-2018 Andrew Khitrin (ahitrin@gmail.com)
  * Copyright (C) 2017-2018 Alexander Fedorov (alexander.fedorov@jkiss.org)
  *
