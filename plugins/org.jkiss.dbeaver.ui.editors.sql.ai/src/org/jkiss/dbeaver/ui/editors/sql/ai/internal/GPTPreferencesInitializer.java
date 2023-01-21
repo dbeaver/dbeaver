5d4e73c50d71cff62777700b8c16b1f4d95bc3d9
serge-rider <serge@jkiss.org>
2023-01-21 21:26:00 +0300

    dbeaver/pro#1343 Default preferences

--- plugins/org.jkiss.dbeaver.ui.editors.sql.ai/src/org/jkiss/dbeaver/ui/editors/sql/ai/internal/GPTPreferencesInitializer.java
+++ plugins/org.jkiss.dbeaver.ui.editors.sql.ai/src/org/jkiss/dbeaver/ui/editors/sql/ai/internal/GPTPreferencesInitializer.java
@@ -28,7 +28,7 @@ public class GPTPreferencesInitializer extends AbstractPreferenceInitializer {
     @Override
     public void initializeDefaultPreferences() {
         DBPPreferenceStore store = DBWorkbench.getPlatform().getPreferenceStore();
-        PrefUtils.setDefaultPreferenceValue(store, GPTPreferences.GPT_MODEL, GPTModel.TEXT_DAVINCI02.getName());
+        PrefUtils.setDefaultPreferenceValue(store, GPTPreferences.GPT_MODEL, GPTModel.CODE_DAVINCI.getName());
         PrefUtils.setDefaultPreferenceValue(store, GPTPreferences.GPT_MODEL_MAX_TOKENS, 250);
         PrefUtils.setDefaultPreferenceValue(store, GPTPreferences.GPT_MODEL_TEMPERATURE, 0.0f);
     }
