470778fbff385e438d4f5d9db8fc1e28fda0d9f1
Юрий Плотников <yuriy.plotnikov@rt.ru>
2023-01-24 20:56:58 +0300

    suggestions fixes 2 dbeaver#16352

--- plugins/org.jkiss.dbeaver.ext.mssql/src/org/jkiss/dbeaver/ext/mssql/model/SQLServerDialect.java
+++ plugins/org.jkiss.dbeaver.ext.mssql/src/org/jkiss/dbeaver/ext/mssql/model/SQLServerDialect.java
@@ -263,9 +263,9 @@ public class SQLServerDialect extends JDBCSQLDialect implements TPRuleProvider, 
 
     @Override
     public void generateStoredProcedureCall(
-	    StringBuilder sql, 
-		DBSProcedure proc, 
-		Collection<? extends DBSProcedureParameter> parameters,
+        StringBuilder sql, 
+        DBSProcedure proc, 
+        Collection<? extends DBSProcedureParameter> parameters,
         boolean castParams
     ) {
         List<DBSProcedureParameter> inParameters = new ArrayList<>();
@@ -273,7 +273,7 @@ public class SQLServerDialect extends JDBCSQLDialect implements TPRuleProvider, 
         String schemaName = proc.getContainer().getParentObject().getName();
         sql.append("USE [").append(schemaName).append("]\n");
         //sql.append("GO\n\n");
-        sql.append("DECLARE	@return_value int\n\n");
+        sql.append("DECLARE @return_value int\n\n");
         sql.append("EXEC\t@return_value = [").append(proc.getContainer().getName()).append("].[").append(proc.getName()).append("]\n");
         for (int i = 0; i < inParameters.size(); i++) {
             String name = inParameters.get(i).getName();
