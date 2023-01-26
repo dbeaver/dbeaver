0d65c19274dfcbd5322f0f17a8832f64a5613247
Юрий Плотников <yuriy.plotnikov@rt.ru>
2023-01-26 17:31:19 +0300

    code optimization enum_field_filtering#14986

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/impl/jdbc/struct/JDBCTableColumn.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/impl/jdbc/struct/JDBCTableColumn.java
@@ -200,14 +200,14 @@ public abstract class JDBCTableColumn<TABLE_TYPE extends DBSEntity> extends JDBC
         query.append("\nFROM ").append(DBUtils.getObjectFullName(getTable(), DBPEvaluationContext.DML));
         if (valuePattern instanceof String) {
             query.append("\nWHERE ");
-            boolean needCast = false;
-            String typeExt = "";
             DBSDataType dataType = DBUtils.getDataType(this);
-            if (dataType != null) typeExt = CommonUtils.toString(dataType.geTypeExtension());
-            needCast = "E".equals(typeExt); // for future - maybe not only Enum? But I have no such deep knowledge
-            if (needCast) {
-                identifier = "cast(" + identifier + " as varchar(1000))"; // it could be better for PostgreSQL to specify just "varchar" or "text"
-                                                                          // but this not conforms SQL standard, so I left varchar(1000) - it works  
+            if (dataType != null) {
+                String typeExt = CommonUtils.toString(dataType.geTypeExtension());
+                if ("E".equals(typeExt)) { // for future - maybe not only Enum? But I have no such deep knowledge
+                    // it could be better for PostgreSQL to specify just "varchar" or "text"
+                    // but this not conforms SQL standard, so I left varchar(1000) - it works  
+                    identifier = "cast(" + identifier + " as varchar(1000))";
+                }    
             }
             if (getDataKind() == DBPDataKind.STRING) {
                 final SQLDialect dialect = getDataSource().getSQLDialect();
