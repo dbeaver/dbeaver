3bdbad42da03fd041e6a82fd850df90a3e54b526
Georgii <fireshowel@gmail.com>
2023-02-27 16:59:55 +0400

    #17421 fix varbyte recogntion (#19167)

--- plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/PostgreConstants.java
+++ plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/PostgreConstants.java
@@ -106,6 +106,7 @@ public class PostgreConstants {
     public static final String TYPE_JSONB = "jsonb";
     public static final String TYPE_BIT = "bit";
     public static final String TYPE_VARBIT = "varbit";
+    public static final String TYPE_VARBYTE = "varbyte";
     public static final String TYPE_REFCURSOR = "refcursor";
     public static final String TYPE_MONEY = "money";
     public static final String TYPE_GEOMETRY = "geometry";
