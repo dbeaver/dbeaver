6419c331c9f521373da7cc4313ea9d63898e778c
Cheese <cheese.javaee@gmail.com>
2023-03-14 12:14:06 +0000

    #17396 switch tabs to 4 spaces

--- plugins/org.jkiss.dbeaver.ext.tidb/src/org/jkiss/dbeaver/ext/tidb/mysql/model/TiDBDialect.java
+++ plugins/org.jkiss.dbeaver.ext.tidb/src/org/jkiss/dbeaver/ext/tidb/mysql/model/TiDBDialect.java
@@ -10,9 +10,9 @@ import org.jkiss.dbeaver.model.impl.jdbc.JDBCDataSource;
 import org.jkiss.utils.ArrayUtils;
 
 public class TiDBDialect extends MySQLDialect {
-	public static final String[] TIDB_NON_TRANSACTIONAL_KEYWORDS = new String[]{ "OPTIMISTIC", "PESSIMISTIC" };
-	
-	private static final String[] TIDB_ADVANCED_KEYWORDS = {
+    public static final String[] TIDB_NON_TRANSACTIONAL_KEYWORDS = new String[]{ "OPTIMISTIC", "PESSIMISTIC" };
+    
+    private static final String[] TIDB_ADVANCED_KEYWORDS = {
         "AUTO_RANDOM",
         "PLACEMENT",
         "POLICY",
@@ -22,47 +22,47 @@ public class TiDBDialect extends MySQLDialect {
         "NONCLUSTERED",
         "CLUSTERED"
     };
-	
-	private static final String[] TIDB_EXTRA_FUNCTIONS = {
-            "TIDB_BOUNDED_STALENESS",
-            "TIDB_DECODE_KEY",
-            "TIDB_DECODE_PLAN",
-            "TIDB_IS_DDL_OWNER",
-            "TIDB_PARSE_TSO",
-            "TIDB_VERSION",
-            "TIDB_DECODE_SQL_DIGESTS",
-            "TIDB_SHARD"
-	};
-	
-	public TiDBDialect() {
-		super("TiDB", "tidb");
+    
+    private static final String[] TIDB_EXTRA_FUNCTIONS = {
+        "TIDB_BOUNDED_STALENESS",
+        "TIDB_DECODE_KEY",
+        "TIDB_DECODE_PLAN",
+        "TIDB_IS_DDL_OWNER",
+        "TIDB_PARSE_TSO",
+        "TIDB_VERSION",
+        "TIDB_DECODE_SQL_DIGESTS",
+        "TIDB_SHARD"
+    };
+    
+    public TiDBDialect() {
+        super("TiDB", "tidb");
     }
-	
-	@Override
+    
+    @Override
     public void initDriverSettings(JDBCSession session, JDBCDataSource dataSource, JDBCDatabaseMetaData metaData) {
-		super.initBaseDriverSettings(session, dataSource, metaData);
-		
-		for (String kw : TIDB_ADVANCED_KEYWORDS) {
+        super.initBaseDriverSettings(session, dataSource, metaData);
+        
+        for (String kw : TIDB_ADVANCED_KEYWORDS) {
             addSQLKeyword(kw);
         }
-		for (String kw : TIDB_NON_TRANSACTIONAL_KEYWORDS) {
+        for (String kw : TIDB_NON_TRANSACTIONAL_KEYWORDS) {
             addSQLKeyword(kw);
         }
         addFunctions(Arrays.asList(TIDB_EXTRA_FUNCTIONS));
     }
-	
-	@NotNull
+    
+    @NotNull
     public String[] getNonTransactionKeywords() {
         return ArrayUtils.concatArrays(
-    			MySQLDialect.MYSQL_NON_TRANSACTIONAL_KEYWORDS,
-    			TIDB_NON_TRANSACTIONAL_KEYWORDS
-    	    );
+            MySQLDialect.MYSQL_NON_TRANSACTIONAL_KEYWORDS,
+            TIDB_NON_TRANSACTIONAL_KEYWORDS
+        );
     }
-	
-	@NotNull
+    
+    @NotNull
     @Override
     public String[] getExecuteKeywords() {
-		// TiDB do not support the stored procedures
+        // TiDB do not support the stored procedures
         return new String[]{};
     }
 }
