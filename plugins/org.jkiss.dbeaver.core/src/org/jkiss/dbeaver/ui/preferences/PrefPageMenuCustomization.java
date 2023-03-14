0bfe2af3a224b5cd5ab3ff0915468109c5aef455
E1izabeth <kuzenkova.el@yandex.ru>
2023-03-14 20:42:57 +0300

    #18300 WIP on toolbar customization and preference page

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/preferences/PrefPageMenuCustomization.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/preferences/PrefPageMenuCustomization.java
@@ -19,58 +19,182 @@ package org.jkiss.dbeaver.ui.preferences;
 import org.eclipse.core.commands.common.NotDefinedException;
 import org.eclipse.jface.action.IContributionItem;
 import org.eclipse.jface.action.ToolBarManager;
+import org.eclipse.jface.layout.GridLayoutFactory;
+import org.eclipse.jface.preference.IPersistentPreferenceStore;
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.viewers.*;
+import org.eclipse.jface.viewers.CellEditor.LayoutData;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.*;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.commands.ICommandService;
 import org.eclipse.ui.menus.CommandContributionItem;
+import org.eclipse.ui.menus.IMenuService;
 import org.jkiss.code.NotNull;
+import org.jkiss.dbeaver.ModelPreferences;
+import org.jkiss.dbeaver.core.DBeaverActivator;
+import org.jkiss.dbeaver.model.app.DBPApplication;
+import org.jkiss.dbeaver.model.preferences.DBPPreferenceStore;
+import org.jkiss.dbeaver.model.sql.SQLScriptCommitType;
+import org.jkiss.dbeaver.model.sql.SQLScriptErrorHandling;
+import org.jkiss.dbeaver.runtime.DBWorkbench;
 import org.jkiss.dbeaver.ui.*;
+import org.jkiss.dbeaver.ui.actions.ToolBarConfigurationDescriptor;
+import org.jkiss.dbeaver.ui.actions.ToolBarConfigurationPropertyTester;
+import org.jkiss.dbeaver.ui.actions.ToolBarConfigurationRegistry;
 import org.jkiss.dbeaver.ui.controls.*;
 import org.jkiss.dbeaver.ui.editors.sql.SQLEditorCommands;
+import org.jkiss.dbeaver.ui.editors.sql.SQLPreferenceConstants;
 import org.jkiss.dbeaver.ui.registry.ConfirmationDescriptor;
+import org.jkiss.dbeaver.utils.PrefUtils;
 import org.jkiss.utils.CommonUtils;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 
 public class PrefPageMenuCustomization extends AbstractPrefPage implements IWorkbenchPreferencePage {
+    
+    private abstract class CheckableNode extends TreeNode {
 
-    private class CommandWithStatus {
-
-        private String commandId;
-        private String name;
-        private boolean enabled;
+        private boolean isChecked;
+        private boolean isGrayed;
         
-        public CommandWithStatus(String commandId, String name, boolean enabled) {
-            this.commandId = commandId;
-            this.name = name;
-            this.enabled = enabled;
+        public CheckableNode(Object value) {
+            super(value);
         }
 
+        public boolean isChecked() {
+            return isChecked;
+        }
+        
+        public void setChecked(boolean value) {
+            isChecked = value;
+        }
+        
+        public boolean isGrayed() {
+            return isGrayed;
+        }
+        
+        public void setGrayed(boolean value) {
+            isGrayed = value;
+        }
     }
+    
+    private class ToolBarNode extends CheckableNode {
+        ToolBarConfigurationDescriptor toolbar;
+        List<ToolItemNode> subnodes;
+        
+        public ToolBarNode(ToolBarConfigurationDescriptor value) {
+            super(value.getName());
+            this.toolbar = value;
+            this.subnodes = toolbar.items().stream().map(t -> new ToolItemNode(this, t)).collect(Collectors.toList());
+            setChildren(subnodes.toArray(ToolItemNode[]::new));
+            update();
+        }
+        
+        public void update() {
+            long checkedCount = subnodes.stream().filter(t -> t.isChecked()).count();
+            if (checkedCount == 0) {
+                super.setChecked(false);
+                super.setGrayed(false);
+            } else if (toolbar.items().size() == checkedCount) {
+                super.setChecked(true);
+                super.setGrayed(false);
+            } else {
+                super.setChecked(false);
+                super.setGrayed(true);
+            }
+        }
+        
+        @Override
+        public void setChecked(boolean value) {
+            subnodes.forEach(n -> n.setChecked(value));
+            super.setChecked(value);
+        }
+        
+        @Override
+        public String toString() {
+            return toolbar.getName();
+        }
+    }
+    
+    private class ToolItemNode extends CheckableNode {
+        final ToolBarConfigurationDescriptor.Item item;
+        final String name;
+        final ToolBarNode owner;
+        
+        public ToolItemNode(ToolBarNode owner, ToolBarConfigurationDescriptor.Item item) {
+            super(item);
+            this.item = item;
+            this.name = getItemName(item);
+            this.owner = owner;
+            
+            setChecked(item.isVisible());
+        }
+        
+        @Override
+        public String toString() {
+            return name;
+        }
+        
+        public void apply() {
+            item.setVisible(isChecked());
+        }
+        
+        public void restore() {
+            setChecked(item.isVisible());
+        }
+        
+        public void reset() {
+            setChecked(item.isVisibleByDefault());
+        }
+    }
+    
+    
+    private final ICommandService cmdSvc;
+    private final Collection<String> knownCommands;
+    private final List<ToolBarNode> toolBarNodes;
 
-
-    // TODO: get toolbars from SQL Editor
-    ToolBarManager topBarMan = new ToolBarManager();
-    Table commandsTable;
-    List<CommandWithStatus> commands;
-    private Map<ConfirmationDescriptor, Object> changedConfirmations = new HashMap<>();
-
+    private CheckboxTreeViewer treeViewer;
+    
+    public PrefPageMenuCustomization() {
+        cmdSvc = PlatformUI.getWorkbench().getService(ICommandService.class);
+        knownCommands = cmdSvc.getDefinedCommandIds();
+        
+        toolBarNodes = ToolBarConfigurationRegistry.getInstance().getKnownToolBars().stream()
+            .map(t -> new ToolBarNode(t)).collect(Collectors.toList());
+    }
+    
     @Override
     public void init(IWorkbench workbench) {
-        // TODO: get it from toolbar manager - it's only for testing here now
-        topBarMan.add(ActionUtils.makeCommandContribution(UIUtils.getActiveWorkbenchWindow(), SQLEditorCommands.CMD_EXECUTE_STATEMENT));
-        topBarMan.add(ActionUtils.makeCommandContribution(UIUtils.getActiveWorkbenchWindow(), SQLEditorCommands.CMD_EXECUTE_STATEMENT_NEW));
-        topBarMan.add(ActionUtils.makeCommandContribution(UIUtils.getActiveWorkbenchWindow(), SQLEditorCommands.CMD_EXECUTE_SCRIPT));
-        topBarMan.add(ActionUtils.makeCommandContribution(UIUtils.getActiveWorkbenchWindow(), SQLEditorCommands.CMD_EXPLAIN_PLAN));
-        commands = new ArrayList<>();
+    }
 
+    private String getItemName(ToolBarConfigurationDescriptor.Item item) {
+        if (item.getName() != null) {
+            return item.getName();
+        }
+        if (item.getCommandId() != null && knownCommands.contains(item.getCommandId())) {
+            try {
+                return cmdSvc.getCommand(item.getCommandId()).getName();
+            } catch (NotDefinedException e) {
+                // TODO: log error
+                e.printStackTrace();
+            }
+        }
+        return item.getKey();
     }
 
     @NotNull
@@ -78,104 +202,94 @@ public class PrefPageMenuCustomization extends AbstractPrefPage implements IWork
     protected Control createPreferenceContent(@NotNull Composite parent) {
         Composite composite = UIUtils.createPlaceholder(parent, 1);
 
-        Group groupEditors = UIUtils.createControlGroup(composite, "Top SQL Editor toolbar", 1, GridData.VERTICAL_ALIGN_BEGINNING, 0);
+        Group groupEditors = UIUtils.createControlGroup(composite, "Toolbar items visiblity", 1, GridData.FILL_BOTH, 0);
 
-        TableViewer tableViewer = new TableViewer(
-                groupEditors,
-                SWT.BORDER | SWT.UNDERLINE_SINGLE | SWT.V_SCROLL | SWT.H_SCROLL | SWT.FULL_SELECTION);
-
-        commandsTable = tableViewer.getTable();
-        commandsTable.setLayoutData(new GridData(GridData.FILL_BOTH));
-        commandsTable.setHeaderVisible(true);
-        commandsTable.setLinesVisible(true);
-
-        ViewerColumnController<Object, Object> columnsController = new ViewerColumnController<>(
-            "PrefPageMenuCustomizationEditor", //$NON-NLS-1$
-            tableViewer
-        );
-
-        columnsController.addBooleanColumn(
-                "Visible",
-                "Indicates whether this command should be added to the toolbar or not",
-                SWT.CENTER,
-                true,
-                true,
-                item -> {
-                    if (item instanceof CommandWithStatus) {
-                        return  ((CommandWithStatus) item).enabled;
-                    }
-                    return false;
-                }, new EditingSupport(tableViewer) {
-
-                    @Override
-                    protected CellEditor getCellEditor(Object element) {
-                        return new CustomCheckboxCellEditor(tableViewer.getTable(), true);
-                    }
-
-                    @Override
-                    protected boolean canEdit(Object element) {
-                        return true;
-                    }
-
-                    @Override
-                    protected Object getValue(Object element) {
-                        if (element instanceof CommandWithStatus) {
-                            return ((CommandWithStatus) element).enabled;
-                        }
-                        return false;
-                    }
-
-                    @Override
-                    protected void setValue(Object element, Object value) {
-                        if (element instanceof CommandWithStatus) {
-                            CommandWithStatus command = (CommandWithStatus) element;
-                            command.enabled = CommonUtils.getBoolean(value, true);
-                            //changedCommands.put((command).commandId, value);
-                        }
-
-                    }
-                });
-
-        // TODO: add icon for each column
-
-        columnsController.addColumn(
-                "Command",
-                "Tip",
-                SWT.LEFT,
-                true,
-                true,
-                new ColumnLabelProvider() {
-                    @Override
-                    public String getText(Object element) {
-                        if (element instanceof CommandWithStatus) {
-                            return ((CommandWithStatus) element).name;
-                        }
-                        return super.getText(element);
-                    }
-
-                });
-
-        columnsController.createColumns(false);
-        tableViewer.setContentProvider(new ListContentProvider());
-        new DefaultViewerToolTipSupport(tableViewer);
-
-        for (IContributionItem item : topBarMan.getItems()) {
-            if (item instanceof CommandContributionItem) {
-                CommandContributionItem command = (CommandContributionItem) item;
-                String commandName;
-                try {
-                    commandName = command.getCommand().getName();
-                } catch (NotDefinedException e) {
-                    commandName = command.getCommand().getId();
-                }
-                this.commands.add(new CommandWithStatus(command.getId(), commandName, true));
+        treeViewer = new CheckboxTreeViewer(groupEditors, SWT.BORDER | SWT.UNDERLINE_SINGLE | SWT.V_SCROLL | SWT.H_SCROLL | SWT.FULL_SELECTION | SWT.CHECK);
+        treeViewer.getTree().setLayoutData(new GridData(GridData.FILL_BOTH));
+        TreeViewerEditor.create(treeViewer, new ColumnViewerEditorActivationStrategy(treeViewer) { 
+            @Override
+            protected boolean isEditorActivationEvent(ColumnViewerEditorActivationEvent event) {
+                return false;
             }
-        }
-        tableViewer.setInput(this.commands);
-        tableViewer.refresh();
-
-        UIUtils.asyncExec(() -> UIUtils.packColumns(commandsTable, false));
-
+        }, 0);
+        treeViewer.addDoubleClickListener(new IDoubleClickListener() {
+            @Override
+            public void doubleClick(DoubleClickEvent event) {
+                System.out.println(event);
+            }
+        });
+        treeViewer.setCheckStateProvider(new ICheckStateProvider() {
+            @Override
+            public boolean isGrayed(Object element) { 
+                if (element instanceof CheckableNode) {
+                    return ((CheckableNode)element).isGrayed();
+                }
+                return false;
+            }
+            @Override
+            public boolean isChecked(Object element) {
+                if (element instanceof CheckableNode) {
+                    return ((CheckableNode)element).isChecked();
+                }
+                return false;
+            }
+        });
+        treeViewer.addCheckStateListener(new ICheckStateListener() {
+            @Override
+            public void checkStateChanged(CheckStateChangedEvent event) {
+                Object node = event.getElement();   
+                if (node instanceof CheckableNode) {
+                    ((CheckableNode)node).setChecked(event.getChecked());
+                    if (node instanceof ToolItemNode) {
+                        ((ToolItemNode)node).owner.update();
+                        treeViewer.refresh();
+                    }
+                    else if (node instanceof ToolBarNode) {
+                        ((ToolBarNode)node).update();
+                        treeViewer.refresh();
+                    }
+                }
+            }
+        });
+        treeViewer.setContentProvider(new TreeNodeContentProvider());
+        treeViewer.setInput(toolBarNodes.toArray(ToolBarNode[]::new));
+        
+        forEachToolItem(node -> node.restore());
+        
+        treeViewer.refresh();
+        treeViewer.expandAll();
+        
         return composite;
     }
+    
+    private void forEachToolItem(Consumer<ToolItemNode> action) {
+        for (ToolBarNode toolBar: toolBarNodes) {
+            for (ToolItemNode item: toolBar.subnodes) {
+                action.accept(item);
+            }   
+            toolBar.update();
+        }
+        treeViewer.refresh();
+    }
+    
+    @Override
+    protected void performDefaults() {
+        forEachToolItem(node -> node.reset());
+    }
+
+    @Override
+    public final boolean performOk() {
+        forEachToolItem(node -> node.apply());
+        IPreferenceStore prefs = DBeaverActivator.getInstance().getPreferenceStore();
+        if (prefs.needsSaving() && prefs instanceof IPersistentPreferenceStore) {
+            try {
+                ((IPersistentPreferenceStore)prefs).save();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        ToolBarConfigurationPropertyTester.fireVisibilityPropertyChange();
+        return true;
+    }
+
 }
