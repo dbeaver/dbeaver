724fcaa6821cc04e92c88bd3ea7490b91e35d17c
hantmac <hantmac@outlook.com>
2023-03-31 15:43:28 +0800

    feat: support new data source: Databend

---
+++ plugins/org.jkiss.dbeaver.ext.databend/src/org/jkiss/dbeaver/ext/databend/model/DatabendConstants.java
@@ -0,0 +1,6 @@
+package org.jkiss.dbeaver.ext.databend.model;
+
+public class DatabendConstants {
+    public static final String JDBC_URL_PREFIX = "jdbc:databend://";
+    public static final String SSL_PARAM = "ssl"; //$NON-NLS-1$
+}
