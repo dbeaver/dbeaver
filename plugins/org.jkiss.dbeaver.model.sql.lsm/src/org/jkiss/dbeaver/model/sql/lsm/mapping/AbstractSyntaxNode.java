2fd93e057a1893ab7c3905390b3dba530d6a9ac5
Elizabeth <e1izabeth.k@outlook.com>
2023-04-07 14:41:38 +0300

    #19031 antlr (#19546)
    * #19031 Add plugin and grammars
    
    * #19031 Refactor grammar
    
    * #19031 Add grammar extension for mysql dialect, switch to dbeaver repo for antlr dependencies
    
    * #19031 Add syntax tree to model mapping and select statement model partially
    
    * #19031 Rename package
    
    * #19031 Add tests
    
    * #19031 Add select quantifier mapping and test
    
    * #19031 Refactor SyntaxModel internals out and switch from parallel syntax tree structure to ANTLR tree nodes overrides
    
    * #19031 Fix code style
    
    * #19031 Fix code style
    
    * #19031 Refactor model mapping and add error handling to model
    
    * #19031 Fix code style
    
    * #19031 Fix code style
    
    * #19031 Rename bundle and packages
    
    * #19031 Fix test reference in pom
    
    * #19031 Rename package
    
    * #19031 Fix duplicated header in generated resources
    
    * #19031 Remove unnecessary bundle version

---
+++ plugins/org.jkiss.dbeaver.model.sql.lsm/src/org/jkiss/dbeaver/model/sql/lsm/mapping/AbstractSyntaxNode.java
@@ -0,0 +1,69 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2023 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jkiss.dbeaver.model.sql.lsm.mapping;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public abstract class AbstractSyntaxNode {
+    private static final Map<Class<? extends AbstractSyntaxNode>, String> syntaxNodeNameByType = new HashMap<>(
+        Map.of(ConcreteSyntaxNode.class, "")
+    );
+    
+    private static String getNodeName(Class<? extends AbstractSyntaxNode> type) {
+        return syntaxNodeNameByType.computeIfAbsent(
+            type,
+            t -> Optional.ofNullable(t.getAnnotation(SyntaxNode.class).name()).orElse(t.getName())
+        );
+    }
+    
+    public static final int UNDEFINED_POSITION = -1;
+    
+    // TODO consider revising
+    private final String name;
+    private int startPos = UNDEFINED_POSITION;
+    private int endPos = UNDEFINED_POSITION;
+    
+    protected AbstractSyntaxNode() {
+        this.name = getNodeName(this.getClass());
+    }
+    
+    protected AbstractSyntaxNode(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+
+    public int getStartPosition() {
+        return this.startPos;
+    }
+    
+    public int getEndPosition() {
+        return this.endPos;
+    }
+    
+    void setStartPosition(int value) {
+        this.startPos = value;
+    }
+
+    void setEndPosition(int value) {
+        this.endPos = value;
+    }
+}
---
+++ plugins/org.jkiss.dbeaver.model.sql.lsm/src/org/jkiss/dbeaver/model/sql/lsm/mapping/internal/CustomXPathUtils.java
@@ -0,0 +1,133 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2023 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jkiss.dbeaver.model.sql.lsm.mapping.internal;
+
+
+import org.antlr.v4.runtime.misc.Interval;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.TerminalNode;
+import org.antlr.v4.runtime.tree.Tree;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import java.util.Iterator;
+import java.util.Stack;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+import javax.xml.xpath.XPathException;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathNodes;
+
+public class CustomXPathUtils {
+
+    public static void flattenExclusiveImpl(
+        Node root,
+        XPathExpression expr,
+        boolean justLeaves,
+        NodesList<Node> result
+    ) throws XPathExpressionException {
+        Stack<Node> stack = new Stack<>();
+        stack.add(root);
+        while (stack.size() > 0) {
+            Node node = stack.pop();
+            
+            XPathNodes subnodes = expr.evaluateExpression(node, XPathNodes.class);
+            if (justLeaves) {
+                if (subnodes.size() > 0) {
+                    reverseIterableOf(subnodes).forEach(stack::add);
+                } else {
+                    result.add(node);                    
+                }
+            } else {
+                result.ensureCapacity(result.size() + subnodes.size());
+                subnodes.forEach(result::add);
+                reverseIterableOf(subnodes).forEach(stack::add);
+            }
+        }
+    }
+    
+    public static Stream<Node> streamOf(final NodeList list) {
+        return StreamSupport.stream(iterableOf(list).spliterator(), false);
+    }
+    
+    public static Iterable<Node> iterableOf(final NodeList list) {
+        return new Iterable<Node>() {
+            @Override
+            public Iterator<Node> iterator() {
+                return new Iterator<>() {
+                    private int index = 0;
+                    
+                    @Override
+                    public boolean hasNext() {
+                        return index < list.getLength();
+                    }
+
+                    @Override
+                    public Node next() {
+                        return list.item(index++);
+                    }
+                };
+            }
+        };
+    }
+    
+    public static Iterable<Node> reverseIterableOf(final XPathNodes list) {
+        return new Iterable<Node>() {
+            @Override
+            public Iterator<Node> iterator() {
+                return new Iterator<>() {
+                    private int index = list.size();
+
+                    @Override
+                    public boolean hasNext() {
+                        return index > 0;
+                    }
+
+                    @Override
+                    public Node next() {
+                        try {
+                            return list.get(--index);
+                        } catch (XPathException e) {
+                            return null;
+                        }
+                    }
+                };
+            }
+        };
+    }
+    
+    public static String getText(Tree node) {
+        String result;
+        if (node instanceof ParseTree) {
+            result = ((ParseTree) node).getText(); // consider extracting whitespaces but not comments
+        } else {
+            // TODO there are things like firstTerm and lastTerm in the ANTLR tree class impl already, consider them
+            Tree first = node, last = node;
+            while (!(first instanceof TerminalNode) && first.getChildCount() > 0) {
+                first = first.getChild(0);
+            }
+            while (!(last instanceof TerminalNode) && last.getChildCount() > 0) {
+                last = last.getChild(last.getChildCount() - 1);
+            }
+            TerminalNode a = (TerminalNode) first, b = (TerminalNode) last;
+            Interval textRange = Interval.of(a.getSourceInterval().a, b.getSourceInterval().b);
+            result = b.getSymbol().getTokenSource().getInputStream().getText(textRange);
+        }
+        return result;
+    }    
+}
