d0c502cbe4facced1b1cb48cc5c55c7f2fe16119
ShadelessFox <yanuari.channel@gmail.com>
2023-07-18 16:57:25 +0300

    #17692 Reconnect to the last chosen database on demand

--- plugins/org.jkiss.dbeaver.data.transfer/src/org/jkiss/dbeaver/tools/transfer/DTConstants.java
+++ plugins/org.jkiss.dbeaver.data.transfer/src/org/jkiss/dbeaver/tools/transfer/DTConstants.java
@@ -30,6 +30,7 @@ public class DTConstants {
 
     public static final String PREF_FALLBACK_OUTPUT_DIRECTORY = "fallbackOutputDirectory";
     public static final String DEFAULT_FALLBACK_OUTPUT_DIRECTORY = System.getProperty(StandardConstants.ENV_TMP_DIR);
+    public static final String PREF_RECONNECT_TO_LAST_DATABASE = "reconnectToLastDatabase";
 
     public static final String PRODUCT_FEATURE_ADVANCED_DATA_TRANSFER = "database/data/transfer/advanced";
 
