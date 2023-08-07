2efe2f0c70c2a753c8c426bfcaaad693c90f4dab
Anastasiya <45152336+LonwoLonwo@users.noreply.github.com>
2023-08-07 11:55:25 +0300

    #20765 use <undefined> name for nullable data types (#20766)
    * #20765 use <undefined> name for nullable data types
    
    * #20765 localization

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/DBUtils.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/DBUtils.java
@@ -34,6 +34,7 @@ import org.jkiss.dbeaver.model.impl.DBObjectNameCaseTransformer;
 import org.jkiss.dbeaver.model.impl.data.DBDValueError;
 import org.jkiss.dbeaver.model.impl.data.DefaultValueHandler;
 import org.jkiss.dbeaver.model.impl.sql.BasicSQLDialect;
+import org.jkiss.dbeaver.model.messages.ModelMessages;
 import org.jkiss.dbeaver.model.navigator.DBNDatabaseFolder;
 import org.jkiss.dbeaver.model.runtime.DBRProgressListener;
 import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
@@ -1733,6 +1734,10 @@ public final class DBUtils {
     @NotNull
     public static String getFullTypeName(DBPDataSource dataSource, @NotNull DBSTypedObject typedObject) {
         String typeName = typedObject.getTypeName();
+        if (CommonUtils.isEmpty(typeName)) {
+            // No answer from the driver side
+            return "<" + ModelMessages.dbutils_type_name_unknown + ">";
+        }
         String typeModifiers = SQLUtils.getColumnTypeModifiers(dataSource, typedObject, typeName, typedObject.getDataKind());
         return typeModifiers == null ? typeName : (typeName + typeModifiers);
     }
