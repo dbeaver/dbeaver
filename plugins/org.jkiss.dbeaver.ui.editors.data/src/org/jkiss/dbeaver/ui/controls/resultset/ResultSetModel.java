d43ca55acdbe5fb65ad1a954e036e87a78032ea7
VIJAYAP4 <priya.vijayakumar@lexisnexis.com>
2023-08-10 15:03:40 -0400

    dbeaver#20528  comparison only for string types

--- plugins/org.jkiss.dbeaver.ui.editors.data/src/org/jkiss/dbeaver/ui/controls/resultset/ResultSetModel.java
+++ plugins/org.jkiss.dbeaver.ui.editors.data/src/org/jkiss/dbeaver/ui/controls/resultset/ResultSetModel.java
@@ -1123,7 +1123,12 @@ public class ResultSetModel {
                     }
                     Object cell1 = getCellValue(new ResultSetCellLocation(binding, row1));
                     Object cell2 = getCellValue(new ResultSetCellLocation(binding, row2));
-                    result = DBUtils.compareDataValues((Object) cell1.toString().toLowerCase(), (Object) cell2.toString().toLowerCase());
+                    if(cell1 instanceof String && cell2 instanceof String) {
+                    	result = DBUtils.compareDataValues((Object) cell1.toString().toLowerCase(), (Object) cell2.toString().toLowerCase());                    	
+                    }else {
+                    	result = DBUtils.compareDataValues(cell1, cell2);
+                    }
+                          
                     if (co.isOrderDescending()) {
                         result = -result;
                     }
