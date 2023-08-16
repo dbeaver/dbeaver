e0868cf7097cdd868d0b6e4c9e275fa17bb1298b
zennken <chkim@altibase.com>
2023-08-16 13:41:59 +0900

    dbeaver/dbeaver#20126 Bulk update for License header

--- plugins/org.jkiss.dbeaver.ext.altibase/src/org/jkiss/dbeaver/ext/altibase/edit/AltibaseTableManager.java
+++ plugins/org.jkiss.dbeaver.ext.altibase/src/org/jkiss/dbeaver/ext/altibase/edit/AltibaseTableManager.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2022 DBeaver Corp and others
+ * Copyright (C) 2010-2023 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.jkiss.dbeaver.ext.altibase.edit;
 
 import org.jkiss.code.NotNull;
--- plugins/org.jkiss.dbeaver.ext.altibase/src/org/jkiss/dbeaver/ext/altibase/sql/AltibaseDialectAdapterFactory.java
+++ plugins/org.jkiss.dbeaver.ext.altibase/src/org/jkiss/dbeaver/ext/altibase/sql/AltibaseDialectAdapterFactory.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.jkiss.dbeaver.ext.altibase.sql;
 
 import org.eclipse.core.runtime.IAdapterFactory;
