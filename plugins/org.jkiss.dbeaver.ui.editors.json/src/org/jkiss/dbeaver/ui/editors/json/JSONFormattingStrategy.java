76645143977925efb1fc2724bd1e7fc4f76fdc27
Serge Rider <serge@jkiss.org>
2023-08-21 17:20:58 +0200

    JSON formatting errors logging

--- plugins/org.jkiss.dbeaver.ui.editors.json/src/org/jkiss/dbeaver/ui/editors/json/JSONFormattingStrategy.java
+++ plugins/org.jkiss.dbeaver.ui.editors.json/src/org/jkiss/dbeaver/ui/editors/json/JSONFormattingStrategy.java
@@ -18,8 +18,6 @@ package org.jkiss.dbeaver.ui.editors.json;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
 import org.eclipse.jface.text.formatter.ContextBasedFormattingStrategy;
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.jkiss.utils.CommonUtils;
@@ -27,44 +25,37 @@ import org.jkiss.utils.CommonUtils;
 /**
  * The formatting strategy that transforms SQL keywords to upper case
  */
-public class JSONFormattingStrategy extends ContextBasedFormattingStrategy
-{
+public class JSONFormattingStrategy extends ContextBasedFormattingStrategy {
     private ISourceViewer sourceViewer;
     private JSONSourceViewerConfiguration svConfig;
 
-    JSONFormattingStrategy(ISourceViewer sourceViewer, JSONSourceViewerConfiguration svConfig)
-    {
+    JSONFormattingStrategy(ISourceViewer sourceViewer, JSONSourceViewerConfiguration svConfig) {
         this.sourceViewer = sourceViewer;
         this.svConfig = svConfig;
     }
 
     @Override
-    public void formatterStarts(String initialIndentation)
-    {
+    public void formatterStarts(String initialIndentation) {
     }
 
     @Override
-    public String format(String content, boolean isLineStart, String indentation, int[] positions)
-    {
+    public String format(String content, boolean isLineStart, String indentation, int[] positions) {
         if (CommonUtils.isEmpty(content)) {
             return content;
         }
-        JsonParser parser = new JsonParser();
-        JsonElement jsonElement = parser.parse(content);
-
         Gson gson = new GsonBuilder()
             .serializeNulls()
             .disableHtmlEscaping()
             .setPrettyPrinting()
             .setLenient()
             .create();
-        String formattedJson = gson.toJson(jsonElement);
+        String formattedJson = gson.toJson(
+            gson.fromJson(content, Object.class));
         return formattedJson;
     }
 
     @Override
-    public void formatterStops()
-    {
+    public void formatterStops() {
     }
 
 }
