88fbe788d747bbc810fd7a9acf35f1650e165263
huangpeijian <huangpeijian@sics.com>
2023-09-18 11:33:55 +0800

    ci:add package script

---
+++ package.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -xe
+
+package_version=$1
+long_commit_id=$(git rev-parse HEAD)
+zip_package_name="dbeaver-${package_version}-${long_commit_id}.zip"
+package_dir="product/community/target/products/org.jkiss.dbeaver.core.product"
+
+cd ${package_dir}
+zip -r ${zip_package_name} linux win32
