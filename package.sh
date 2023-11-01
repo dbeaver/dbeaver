59eaac77e526d28ec5547189671888b98d26a7be
liangxiong <liangxiong@sics.ac.cn>
2023-11-01 16:56:23 +0800

    fix: update package doc

--- package.sh
+++ package.sh
@@ -18,11 +18,12 @@ mac_zip_package_name="${zip_basename}-${package_version}-${mac_package_name}.zip
 
 package_dir="product/community/target/products/org.jkiss.dbeaver.core.product"
 
+rm -rf macosx/cocoa/aarch64
 cd "${base_pwd}/${package_dir}/win32"
 zip -r ${win32_zip_package_name} win32
 cp ${win32_zip_package_name} ${base_pwd}/${package_dir}
 cd ${base_pwd}/${package_dir}
 zip -r ${mac_zip_package_name} macosx
 zip -r ${linux_x86_zip_package_name} linux/gtk/x86_64
-zip -r ${linux_arrch64_zip_package_name} linux/gtk/aarch64
+
 
