b0fba5e4a91cb597470d689595bd1c2e7d39ce07
huangpeijian <huangpeijian@sics.com>
2023-11-01 14:19:37 +0800

    ci:update win32 linux macos package

--- package.sh
+++ package.sh
@@ -3,11 +3,26 @@ set -xe
 
 package_version=$1
 package_name_prefix=$2
+win32_package_name="win32"
+linux_x86_package_name="linux-x86_64"
+linux_arrch64_package_name="linux-arrch64"
+mac_package_name="macosx"
+base_pwd=`pwd`
+
 zip_basename=`basename ${package_name_prefix} *.zip`
-short_commit_id=$(git rev-parse --short HEAD)
-zip_package_name="${zip_basename}-${package_version}-${short_commit_id}.zip"
+
+win32_zip_package_name="${zip_basename}-${package_version}-${win32_package_name}.zip"
+linux_x86_zip_package_name="${zip_basename}-${package_version}-${linux_x86_package_name}.zip"
+linux_arrch64_zip_package_name="${zip_basename}-${package_version}-${linux_arrch64_package_name}.zip"
+mac_zip_package_name="${zip_basename}-${package_version}-${mac_package_name}.zip"
+
 package_dir="product/community/target/products/org.jkiss.dbeaver.core.product"
 
-cd ${package_dir}
-rm -rf macosx/cocoa/aarch64
-zip -r ${zip_package_name} linux win32 macosx
+cd "${base_pwd}/${package_dir}/win32"
+zip -r ${win32_zip_package_name} win32
+cp ${win32_zip_package_name} ${base_pwd}/${package_dir}
+cd ${base_pwd}/${package_dir}
+zip -r ${mac_zip_package_name} macosx
+zip -r ${linux_x86_zip_package_name} linux/gtk/x86_64
+zip -r ${linux_arrch64_zip_package_name} linux/gtk/aarch64
+
