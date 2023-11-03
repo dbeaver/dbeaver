a9deb227e7ab0c1d1f5d4c85cb73ea2e38ac848e
Serge Rider <serge@jkiss.org>
2023-11-03 17:30:28 +0100

    dbeaver/pro#2063 NIO FS events propogation fix

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/navigator/fs/DBNFileSystems.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/navigator/fs/DBNFileSystems.java
@@ -264,8 +264,15 @@ public class DBNFileSystems extends DBNNode implements DBPHiddenObject, EFSNIOLi
                 if (rootNode != null) {
                     String[] pathSegments = resource.getFullPath().segments();
                     DBNPathBase parentNode = rootNode;
-                    for (int i = 2; i < pathSegments.length - 1; i++) {
+                    // NIO path format /[config-id]/root-id/folder1/file1
+                    for (int i = 1; i < pathSegments.length - 1; i++) {
                         String itemName = pathSegments[i];
+                        if (itemName.equals(rootNode.getName())) {
+                            // In some NIOs the first item is config ID and the second is root folder name
+                            // Skip root folder then
+                            // FIXME: we need a unified approach. Ot at least we need to know which FS support config ID as first item
+                            continue;
+                        }
                         DBNPathBase childNode = parentNode.getChild(itemName);
                         if (childNode == null) {
                             log.debug("Cannot find child node '" + itemName + "' in '" + parentNode.getNodeItemPath() + "'");
@@ -275,14 +282,10 @@ public class DBNFileSystems extends DBNNode implements DBPHiddenObject, EFSNIOLi
                     }
 
                     switch (action) {
-                        case CREATE:
-                            parentNode.addChildResource(resource.getNioPath());
-                            break;
-                        case DELETE:
-                            parentNode.removeChildResource(resource.getNioPath());
-                            break;
-                        default:
-                            break;
+                        case CREATE -> parentNode.addChildResource(resource.getNioPath());
+                        case DELETE -> parentNode.removeChildResource(resource.getNioPath());
+                        default -> {
+                        }
                     }
                 }
                 break;
