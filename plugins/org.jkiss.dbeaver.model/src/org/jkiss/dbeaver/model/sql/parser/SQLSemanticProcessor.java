6a590ea4ea7d4b7c512ca821255b81ce4558f0f7
Serge Rider <serge@jkiss.org>
2023-11-10 09:57:52 +0100

    NPE fix

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/sql/parser/SQLSemanticProcessor.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/sql/parser/SQLSemanticProcessor.java
@@ -64,7 +64,7 @@ public class SQLSemanticProcessor {
     private static final boolean ALLOW_COMPLEX_PARSING = false;
 
     public static Statement parseQuery(@Nullable SQLDialect dialect, @NotNull String sql) throws DBCException {
-        String sqlWithoutComments = SQLUtils.stripComments(dialect, sql);
+        String sqlWithoutComments = dialect == null ? sql : SQLUtils.stripComments(dialect, sql);
         CCJSqlParser parser = new CCJSqlParser(new StringProvider(sqlWithoutComments));
         try {
             parser.withAllowComplexParsing(ALLOW_COMPLEX_PARSING);
