8daeac6c8311d92a77ba37da332b31f5e07395ff
Anastasiya Volkova <anvolka@yandex.ru>
2023-12-04 15:09:31 +0300

    dbeaver/pro#2204 code cleanup

--- plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/model/PostgreEventTrigger.java
+++ plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/model/PostgreEventTrigger.java
@@ -47,7 +47,6 @@ public class PostgreEventTrigger extends PostgreTriggerBase {
         sql_drop
     }
 
-    private final PostgreDatabase database;
     private long objectId;
     private long routineId;
     private TriggerEventTypes eventType;
@@ -57,7 +56,6 @@ public class PostgreEventTrigger extends PostgreTriggerBase {
 
     PostgreEventTrigger(@NotNull PostgreDatabase database, @NotNull String name, @NotNull JDBCResultSet dbResult) {
         super(database, name, true);
-        this.database = database;
         this.objectId = JDBCUtils.safeGetLong(dbResult, "oid");
         this.routineId = JDBCUtils.safeGetLong(dbResult, "evtfoid");
         String eventTypeName = JDBCUtils.safeGetString(dbResult, "evtevent");
@@ -68,7 +66,6 @@ public class PostgreEventTrigger extends PostgreTriggerBase {
 
     public PostgreEventTrigger(@NotNull PostgreDatabase database, String name) {
         super(database, name, false);
-        this.database = database;
     }
 
     @Override
@@ -173,7 +170,7 @@ public class PostgreEventTrigger extends PostgreTriggerBase {
     @NotNull
     @Override
     public DBSObject getParentObject() {
-        return database;
+        return getDatabase();
     }
 
     @Override
