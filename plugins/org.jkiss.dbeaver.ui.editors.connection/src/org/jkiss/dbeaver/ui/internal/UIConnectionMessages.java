1cbb57651a4ec693dbe8f9f48b4beccf2a63f5b4
Georgii <fireshowel@gmail.com>
2023-12-11 20:57:15 +0400

    dbeaver/pro#2186 update certificate error message (#22061)

--- plugins/org.jkiss.dbeaver.ui.editors.connection/src/org/jkiss/dbeaver/ui/internal/UIConnectionMessages.java
+++ plugins/org.jkiss.dbeaver.ui.editors.connection/src/org/jkiss/dbeaver/ui/internal/UIConnectionMessages.java
@@ -163,8 +163,10 @@ public class UIConnectionMessages extends NLS {
     public static String dialog_driver_download_auto_page_download_rate;
     public static String dialog_driver_download_auto_page_download_failed_msg;
     public static String dialog_driver_download_auto_page_download_failed_cert_msg;
+    public static String dialog_driver_download_auto_page_download_failed_cert_msg_advanced;
     public static String dialog_driver_download_network_unavailable_msg;
     public static String dialog_driver_download_network_unavailable_cert_msg;
+    public static String dialog_driver_download_network_unavailable_cert_msg_advanced;
     // Driver edit
 	public static String dialog_edit_driver_setting;
     public static String dialog_edit_driver_type_label;
