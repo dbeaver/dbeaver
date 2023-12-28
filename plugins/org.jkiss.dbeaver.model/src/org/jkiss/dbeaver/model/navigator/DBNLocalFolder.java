a6e150897615cdc2897e99783b1480ae0da5634a
Ainur <ainur.iagudin@dbeaver.com>
2023-12-28 15:04:18 +0100

    CB-3952 unify validation for connection folders and rm resources

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/navigator/DBNLocalFolder.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/navigator/DBNLocalFolder.java
@@ -25,6 +25,7 @@ import org.jkiss.dbeaver.model.meta.Property;
 import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
 import org.jkiss.dbeaver.model.runtime.VoidProgressMonitor;
 import org.jkiss.dbeaver.model.struct.DBSObject;
+import org.jkiss.dbeaver.utils.GeneralUtils;
 import org.jkiss.utils.ArrayUtils;
 import org.jkiss.utils.CommonUtils;
 
@@ -245,6 +246,7 @@ public class DBNLocalFolder extends DBNNode implements DBNContainer
     @Override
     public void rename(DBRProgressMonitor monitor, String newName) throws DBException
     {
+        GeneralUtils.validateResourceName(newName);
         getDataSourceRegistry().moveFolder(folder.getFolderPath(), generateNewFolderPath(folder.getParent(), newName));
         DBNModel.updateConfigAndRefreshDatabases(this);
     }
