e3cd5d61bd0d94059a5fee11c3d97f4175a142ae
Sergei Kovalchuk <32031248+serjiokov@users.noreply.github.com>
2024-01-03 15:19:49 +0100

    dbeaver/pro#2305 [ERD] Diagram is not shown due to connection closed … (#22255)
    * dbeaver/pro#2305 [ERD] Diagram is not shown due to connection closed by server side
    
    Fire the exception to original class and handle exception
    
    * dbeaver/pro#2305 [ERD] Diagram is not shown due to connection closed by server side
    
    Code style fix
    
    ---------
    
    Co-authored-by: Matvey16 <82543000+Matvey16@users.noreply.github.com>

--- plugins/org.jkiss.dbeaver.erd.ui/src/org/jkiss/dbeaver/erd/ui/internal/ERDUIMessages.java
+++ plugins/org.jkiss.dbeaver.erd.ui/src/org/jkiss/dbeaver/erd/ui/internal/ERDUIMessages.java
@@ -143,6 +143,8 @@ public class ERDUIMessages extends NLS {
     public static String erd_accessibility_attribute_part_type;
     public static String erd_accessibility_attribute_part_nullability;
     public static String erd_accessibility_attribute_part_comments;
+    public static String erd_error_of_loading_diagram_label;
+    public static String erd_error_of_loading_diagram_title;
 
     private ERDUIMessages() {
     }
