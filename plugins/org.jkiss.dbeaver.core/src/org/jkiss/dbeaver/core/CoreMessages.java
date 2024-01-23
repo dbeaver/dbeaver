0346839bd4fc543310c6fc051042992597c1221d
Serge Rider <serge@jkiss.org>
2024-01-23 15:39:01 +0100

    dbeaver/pro#2247 Idle connection config fix

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/core/CoreMessages.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/core/CoreMessages.java
@@ -53,7 +53,6 @@ public class CoreMessages extends NLS {
     public static String action_menu_transaction_auto_close_enabled;
     public static String action_menu_transaction_auto_close_enabled_tip;
     public static String action_menu_transaction_auto_close_ttl_tip;
-
     public static String action_menu_tools_find_in_navigator;
     public static String action_menu_transaction_pref_page_link;
     public static String action_menu_transaction_pref_page_link_extended;
