97a1d60b0807970b9b27ae65d0924f86be3c7387
Sergei Kovalchuk <sergei.kovalchuk@dbeaver.com>
2024-01-23 08:12:19 +0100

    dbeaver/dbeaver#21574 Reformatted code

--- plugins/org.jkiss.dbeaver.launcher/src/org/eclipse/equinox/launcher/Constants.java
+++ plugins/org.jkiss.dbeaver.launcher/src/org/eclipse/equinox/launcher/Constants.java
@@ -30,50 +30,50 @@ public class Constants {
     public static final String INTERNAL_OS_OS390 = "OS/390"; //$NON-NLS-1$
     public static final String INTERNAL_OS_ZOS = "z/OS"; //$NON-NLS-1$
 
-    public static final String ARCH_X86 = "x86";//$NON-NLS-1$
-    public static final String ARCH_X86_64 = "x86_64";//$NON-NLS-1$
+    public static final String ARCH_X86 = "x86"; //$NON-NLS-1$
+    public static final String ARCH_X86_64 = "x86_64"; //$NON-NLS-1$
 
     /**
      * Constant string (value "win32") indicating the platform is running on a
      * Window 32-bit operating system (e.g., Windows 98, NT, 2000).
      */
-    public static final String OS_WIN32 = "win32";//$NON-NLS-1$
+    public static final String OS_WIN32 = "win32"; //$NON-NLS-1$
 
     /**
      * Constant string (value "linux") indicating the platform is running on a
      * Linux-based operating system.
      */
-    public static final String OS_LINUX = "linux";//$NON-NLS-1$
+    public static final String OS_LINUX = "linux"; //$NON-NLS-1$
 
     /**
      * Constant string (value "aix") indicating the platform is running on an
      * AIX-based operating system.
      */
-    public static final String OS_AIX = "aix";//$NON-NLS-1$
+    public static final String OS_AIX = "aix"; //$NON-NLS-1$
 
     /**
      * Constant string (value "solaris") indicating the platform is running on a
      * Solaris-based operating system.
      */
-    public static final String OS_SOLARIS = "solaris";//$NON-NLS-1$
+    public static final String OS_SOLARIS = "solaris"; //$NON-NLS-1$
 
     /**
      * Constant string (value "hpux") indicating the platform is running on an
      * HP/UX-based operating system.
      */
-    public static final String OS_HPUX = "hpux";//$NON-NLS-1$
+    public static final String OS_HPUX = "hpux"; //$NON-NLS-1$
 
     /**
      * Constant string (value "qnx") indicating the platform is running on a
      * QNX-based operating system.
      */
-    public static final String OS_QNX = "qnx";//$NON-NLS-1$
+    public static final String OS_QNX = "qnx"; //$NON-NLS-1$
 
     /**
      * Constant string (value "macosx") indicating the platform is running on a
      * Mac OS X operating system.
      */
-    public static final String OS_MACOSX = "macosx";//$NON-NLS-1$
+    public static final String OS_MACOSX = "macosx"; //$NON-NLS-1$
 
     /**
      * Constant string (value "os/400") indicating the platform is running on a
@@ -97,43 +97,43 @@ public class Constants {
      * Constant string (value "unknown") indicating the platform is running on a
      * machine running an unknown operating system.
      */
-    public static final String OS_UNKNOWN = "unknown";//$NON-NLS-1$
+    public static final String OS_UNKNOWN = "unknown"; //$NON-NLS-1$
 
     /**
      * Constant string (value "win32") indicating the platform is running on a
      * machine using the Windows windowing system.
      */
-    public static final String WS_WIN32 = "win32";//$NON-NLS-1$
+    public static final String WS_WIN32 = "win32"; //$NON-NLS-1$
 
     /**
      * Constant string (value "wpf") indicating the platform is running on a
      * machine using the Windows Presendation Foundation system.
      */
-    public static final String WS_WPF = "wpf";//$NON-NLS-1$
+    public static final String WS_WPF = "wpf"; //$NON-NLS-1$
 
     /**
      * Constant string (value "motif") indicating the platform is running on a
      * machine using the Motif windowing system.
      */
-    public static final String WS_MOTIF = "motif";//$NON-NLS-1$
+    public static final String WS_MOTIF = "motif"; //$NON-NLS-1$
 
     /**
      * Constant string (value "gtk") indicating the platform is running on a
      * machine using the GTK windowing system.
      */
-    public static final String WS_GTK = "gtk";//$NON-NLS-1$
+    public static final String WS_GTK = "gtk"; //$NON-NLS-1$
 
     /**
      * Constant string (value "photon") indicating the platform is running on a
      * machine using the Photon windowing system.
      */
-    public static final String WS_PHOTON = "photon";//$NON-NLS-1$
+    public static final String WS_PHOTON = "photon"; //$NON-NLS-1$
 
     /**
      * Constant string (value "carbon") indicating the platform is running on a
      * machine using the Carbon windowing system (Mac OS X).
      */
-    public static final String WS_CARBON = "carbon";//$NON-NLS-1$
+    public static final String WS_CARBON = "carbon"; //$NON-NLS-1$
 
     /**
      * Constant string (value "cocoa") indicating the platform is running on a
@@ -145,5 +145,5 @@ public class Constants {
      * Constant string (value "unknown") indicating the platform is running on a
      * machine running an unknown windowing system.
      */
-    public static final String WS_UNKNOWN = "unknown";//$NON-NLS-1$
+    public static final String WS_UNKNOWN = "unknown"; //$NON-NLS-1$
 }
