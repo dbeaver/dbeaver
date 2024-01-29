989916d5eefedb3b3deb894cffa6808dbd19c72c
Sergei Kovalchuk <serjiokov@gmail.com>
2024-01-29 13:58:55 +0100

    dbeaver/pro#2197 Error decrypting secure credentials
    Fixed circle child requesting

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/navigator/DBNProject.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/navigator/DBNProject.java
@@ -178,7 +178,7 @@ public class DBNProject extends DBNResource implements DBNNodeExtendable {
             dataSourceRegistry.checkForErrors();
         } catch (Throwable e) {
             project.dispose();
-            return new DBNNode[]{} ;
+            return new DBNNode[] {};
         }
 
         final List<DBNNode> children = new ArrayList<>();
