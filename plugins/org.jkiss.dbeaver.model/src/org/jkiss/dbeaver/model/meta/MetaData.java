968747707888be7795de4007f338af03a42fbb01
DenisSinelnikov <denis.sinelnikov@dbaever.com>
2024-01-30 16:48:02 +0400

    CB-4457. Add new resultsRow

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/meta/MetaData.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/meta/MetaData.java
@@ -16,5 +16,13 @@
  */
 package org.jkiss.dbeaver.model.meta;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface MetaData {
+    String name() default "";
 }
