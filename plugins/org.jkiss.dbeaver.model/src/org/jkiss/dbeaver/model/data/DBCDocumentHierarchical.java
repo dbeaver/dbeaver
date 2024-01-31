79bb6f0ee1ea61fe99e17f16a2245b33801a4876
DenisSinelnikov <denis.sinelnikov@dbaever.com>
2024-01-31 17:20:35 +0400

    CB-4457. Refactor api for firestore

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/data/DBCDocumentHierarchical.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/data/DBCDocumentHierarchical.java
@@ -38,4 +38,8 @@ public interface DBCDocumentHierarchical<ENTITY extends DBSEntity> {
     void createChildEntity(String name, @NotNull DBRProgressMonitor monitor) throws DBCException;
 
     void deleteChildEntity(String name, @NotNull DBRProgressMonitor monitor) throws DBCException;
+
+    String getPath();
+
+    Object getDocumentId();
 }
