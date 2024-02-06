e54961c80c7004a67d5887c137d7635bc731e878
LonwoLonwo <anvolka@yandex.ru>
2024-02-06 14:58:13 +0200

    dbeaver/pro#2190 ability to create sybase procedures via UI added

--- plugins/org.jkiss.dbeaver.ext.mssql/src/org/jkiss/dbeaver/ext/mssql/model/generic/SQLServerGenericProcedure.java
+++ plugins/org.jkiss.dbeaver.ext.mssql/src/org/jkiss/dbeaver/ext/mssql/model/generic/SQLServerGenericProcedure.java
@@ -16,24 +16,90 @@
  */
 package org.jkiss.dbeaver.ext.mssql.model.generic;
 
+import org.jkiss.code.NotNull;
+import org.jkiss.code.Nullable;
+import org.jkiss.dbeaver.DBException;
 import org.jkiss.dbeaver.ext.generic.model.GenericFunctionResultType;
+import org.jkiss.dbeaver.ext.generic.model.GenericPackage;
 import org.jkiss.dbeaver.ext.generic.model.GenericProcedure;
 import org.jkiss.dbeaver.ext.generic.model.GenericStructContainer;
 import org.jkiss.dbeaver.model.DBPEvaluationContext;
+import org.jkiss.dbeaver.model.meta.Property;
+import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
+import org.jkiss.dbeaver.model.struct.DBSObject;
+import org.jkiss.dbeaver.model.struct.DBSObjectWithScript;
 import org.jkiss.dbeaver.model.struct.rdb.DBSProcedureType;
+import org.jkiss.dbeaver.utils.GeneralUtils;
+
+import java.util.Map;
 
 /**
 * SQL Server procedure
 */
-public class SQLServerGenericProcedure extends GenericProcedure {
+public class SQLServerGenericProcedure extends GenericProcedure implements DBSObjectWithScript {
+
+    private String source;
 
     public SQLServerGenericProcedure(GenericStructContainer container, String procedureName, String specificName, String description, DBSProcedureType procedureType, GenericFunctionResultType functionResultType) {
         super(container, procedureName, specificName, description, procedureType, functionResultType);
     }
 
+    // Create new object
+    public SQLServerGenericProcedure(
+        GenericStructContainer container,
+        String name
+    ) {
+        super(container, name, null, DBSProcedureType.PROCEDURE, "", false);
+    }
+
+    @Override
+    public String getSource() {
+        return source;
+    }
 
     @Override
     public String getFullyQualifiedName(DBPEvaluationContext context) {
         return super.getFullyQualifiedName(context);
+    }
+
+    @Property(hidden = true, editable = true, updatable = true, order = -1)
+    public String getObjectDefinitionText(DBRProgressMonitor monitor, Map<String, Object> options) throws DBException {
+        if (source == null) {
+            if (!persisted) {
+                source =
+                    "CREATE " + getProcedureType().name() + " " + getFullyQualifiedName(DBPEvaluationContext.DDL) +
+                        GeneralUtils.getDefaultLineSeparator() +
+                        (getProcedureType() == DBSProcedureType.FUNCTION ? "RETURNS INT" + GeneralUtils.getDefaultLineSeparator() : "") +
+                        "AS " + GeneralUtils.getDefaultLineSeparator() +
+                        "SELECT 1";
+            } else {
+                source = getDataSource().getMetaModel().getProcedureDDL(monitor, this);
+            }
+        }
+        return source;
+    }
+
+    @Override
+    @Property(hidden = true)
+    public GenericPackage getPackage() {
+        return super.getPackage();
+    }
+
+    @Override
+    @Property(hidden = true)
+    public GenericFunctionResultType getFunctionResultType() {
+        return super.getFunctionResultType();
+    }
+
+    @Override
+    public void setObjectDefinitionText(String sourceText) {
+        this.source = sourceText;
+    }
+
+    @Nullable
+    @Override
+    public DBSObject refreshObject(@NotNull DBRProgressMonitor monitor) throws DBException {
+        this.source = null;
+        return super.refreshObject(monitor);
     }
 }
