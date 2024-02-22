2b14075d63b90431a9bc02cda852824b68fe0a33
serge-rider <serge@dbeaver.com>
2024-02-22 11:04:01 +0100

    dbeaver/dbeaver#22882 Statement close fix

--- plugins/org.jkiss.dbeaver.ext.db2/src/org/jkiss/dbeaver/ext/db2/model/DB2TableForeignKeyColumn.java
+++ plugins/org.jkiss.dbeaver.ext.db2/src/org/jkiss/dbeaver/ext/db2/model/DB2TableForeignKeyColumn.java
@@ -1,6 +1,5 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2013-2015 Denis Forveille (titou10.titou10@gmail.com)
  * Copyright (C) 2010-2024 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
