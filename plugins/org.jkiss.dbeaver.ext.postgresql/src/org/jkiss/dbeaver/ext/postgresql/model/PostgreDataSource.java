cc370ebdbe7c9e28db010ac95e1a673dd5515901
LonwoLonwo <anvolka@yandex.ru>
2024-04-02 17:42:31 +0200

    #23298 hide administration folders for PG; do not read jobs

--- plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/model/PostgreDataSource.java
+++ plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/model/PostgreDataSource.java
@@ -43,7 +43,6 @@ import org.jkiss.dbeaver.model.impl.jdbc.*;
 import org.jkiss.dbeaver.model.impl.jdbc.cache.JDBCObjectLookupCache;
 import org.jkiss.dbeaver.model.impl.net.SSLHandlerTrustStoreImpl;
 import org.jkiss.dbeaver.model.impl.sql.QueryTransformerLimit;
-import org.jkiss.dbeaver.model.meta.Association;
 import org.jkiss.dbeaver.model.meta.ForTest;
 import org.jkiss.dbeaver.model.net.DBWHandlerConfiguration;
 import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
@@ -97,7 +96,6 @@ public class PostgreDataSource extends JDBCDataSource implements DBSInstanceCont
     private volatile boolean hasStatistics;
     private boolean supportsEnumTable;
     private boolean supportsReltypeColumn = true;
-    private boolean supportsJobs;
 
     private static final String LEGACY_UA_TIMEZONE = "Europe/Kiev";
     private static final String NEW_UA_TIMEZONE = "Europe/Kyiv";
@@ -465,13 +463,6 @@ public class PostgreDataSource extends JDBCDataSource implements DBSInstanceCont
                 log.debug("Error reading pg_class.reltype " + e.getMessage());
                 supportsReltypeColumn = false;
             }
-            try {
-                JDBCUtils.queryString(session, "SELECT 1 FROM pgagent.pga_job WHERE 1<>1 LIMIT 1");
-                supportsJobs = true;
-            } catch (Exception e) {
-                log.debug("Error reading pgagent.pga_job " + e.getMessage());
-                supportsJobs = false;
-            }
         }
 
         // Read databases
@@ -745,14 +736,6 @@ public class PostgreDataSource extends JDBCDataSource implements DBSInstanceCont
 
     public boolean supportsRoles() {
         return getServerType().supportsRoles() && !getContainer().getNavigatorSettings().isShowOnlyEntities() && !getContainer().getNavigatorSettings().isHideFolders();
-    }
-
-    /**
-     * Show Jobs and Scheduling only if a database has pgagent extension.
-     */
-    @Association
-    public boolean supportsJobs() {
-        return supportsJobs;
     }
 
     @NotNull
