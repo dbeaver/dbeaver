d16c5760e9493dc0fdc1e1456ff0ce21ea5404b1
Serge Rider <serge@jkiss.org>
2024-04-15 14:27:25 +0200

    dbeaver/pro#2705 Disable credentials save (by policy) (#23319)
    * dbeaver/pro#2705 Disable credentials save (by policy)
    
    * dbeaver/pro#2705 Disable credentials save in popups
    
    * dbeaver/pro#2705 Disable credentials save in popups

--- plugins/org.jkiss.dbeaver.ui.editors.connection/src/org/jkiss/dbeaver/ui/dialogs/connection/DatabaseNativeAuthModelConfigurator.java
+++ plugins/org.jkiss.dbeaver.ui.editors.connection/src/org/jkiss/dbeaver/ui/dialogs/connection/DatabaseNativeAuthModelConfigurator.java
@@ -26,6 +26,8 @@ import org.jkiss.dbeaver.Log;
 import org.jkiss.dbeaver.model.DBPDataSourceContainer;
 import org.jkiss.dbeaver.model.access.DBAAuthModel;
 import org.jkiss.dbeaver.model.impl.auth.AuthModelDatabaseNative;
+import org.jkiss.dbeaver.registry.BasePolicyDataProvider;
+import org.jkiss.dbeaver.registry.DBConnectionConstants;
 import org.jkiss.dbeaver.registry.DataSourceDescriptor;
 import org.jkiss.dbeaver.runtime.DBWorkbench;
 import org.jkiss.dbeaver.runtime.ui.UIServiceSecurity;
@@ -56,6 +58,9 @@ public class DatabaseNativeAuthModelConfigurator implements IObjectPropertyConfi
     protected ToolBar userManagementToolbar;
 
     protected DBPDataSourceContainer dataSource;
+
+    public static final boolean CREDENTIALS_SAVE_RESTRICTED = BasePolicyDataProvider.getInstance()
+        .isPolicyEnabled(DBConnectionConstants.POLICY_RESTRICT_PASSWORD_SAVE);
 
     public void createControl(@NotNull Composite authPanel, DBAAuthModel<?> object, @NotNull Runnable propertyChangeListener) {
         boolean userNameApplicable = true;
@@ -106,8 +111,13 @@ public class DatabaseNativeAuthModelConfigurator implements IObjectPropertyConfi
         }
         if (this.passwordText != null) {
             this.passwordText.setText(CommonUtils.notEmpty(dataSource.getConnectionConfiguration().getUserPassword()));
-            this.savePasswordCheck.setSelection(dataSource.isSavePassword() || isForceSaveCredentials());
-            this.passwordText.setEnabled(dataSource.isSavePassword());
+            if (CREDENTIALS_SAVE_RESTRICTED) {
+                this.savePasswordCheck.setSelection(false);
+                this.savePasswordCheck.setEnabled(false);
+            } else {
+                this.savePasswordCheck.setSelection(dataSource.isSavePassword() || isForceSaveCredentials());
+                this.passwordText.setEnabled(dataSource.isSavePassword());
+            }
         }
         if (dataSource.isTemporary()) {
             if (this.passwordText != null) {
@@ -133,8 +143,12 @@ public class DatabaseNativeAuthModelConfigurator implements IObjectPropertyConfi
         } else {
             dataSource.getConnectionConfiguration().setUserPassword(null);
         }
-        if (this.savePasswordCheck != null) {
-            dataSource.setSavePassword(this.savePasswordCheck.getSelection());
+        if (CREDENTIALS_SAVE_RESTRICTED) {
+            dataSource.setSavePassword(false);
+        } else {
+            if (this.savePasswordCheck != null) {
+                dataSource.setSavePassword(this.savePasswordCheck.getSelection());
+            }
         }
     }
 
@@ -189,7 +203,8 @@ public class DatabaseNativeAuthModelConfigurator implements IObjectPropertyConfi
 
         savePasswordCheck = UIUtils.createCheckbox(panel,
             UIConnectionMessages.dialog_connection_wizard_final_checkbox_save_password,
-            dataSource == null || dataSource.isSavePassword() || isForceSaveCredentials());
+            !CREDENTIALS_SAVE_RESTRICTED &&
+                (dataSource == null || dataSource.isSavePassword() || isForceSaveCredentials()));
         savePasswordCheck.setToolTipText(UIConnectionMessages.dialog_connection_wizard_final_checkbox_save_password);
         savePasswordCheck.addSelectionListener(new SelectionAdapter() {
             @Override
@@ -222,13 +237,14 @@ public class DatabaseNativeAuthModelConfigurator implements IObjectPropertyConfi
     private void showPasswordText(UIServiceSecurity serviceSecurity) {
         boolean passHidden = (passwordText.getStyle() & SWT.PASSWORD) == SWT.PASSWORD;
         if (passHidden) {
-            if (!serviceSecurity.validatePassword(
-                dataSource.getProject(),
-                "Enter project password",
-                "Enter project password to unlock connection password view",
-                true))
-            {
-                return;
+            if (dataSource.getRegistry().getDataSource(dataSource.getId()) != null) {
+                if (!serviceSecurity.validatePassword(
+                    dataSource.getProject(),
+                    "Enter project password",
+                    "Enter project password to unlock connection password view",
+                    true)) {
+                    return;
+                }
             }
         }
 
