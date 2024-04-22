35f9eddd088562aa5fcd3cc8108a6ed3b61d3d32
serge-rider <serge@dbeaver.com>
2024-04-22 11:59:04 +0200

    Add bc bundles deps

--- product/localRepository/install-3rd-party-deps.sh
+++ product/localRepository/install-3rd-party-deps.sh
@@ -1,2 +1,6 @@
 cd contrib
 mvn install:install-file -Dfile=com.ibm.icu.base_58.2.0.v20170418-1837.jar -DgroupId=com.ibm.icu -DartifactId=com.ibm.icu.base -Dversion=58.2.0 -Dpackaging=jar
+mvn install:install-file -Dfile=bcpkix-jdk18on-1.77.jar -DgroupId= -DartifactId=bcpkix -Dversion=1.77 -Dpackaging=jar
+mvn install:install-file -Dfile=bcpg-jdk18on-1.77.jar -DgroupId= -DartifactId=bcpg -Dversion=1.77 -Dpackaging=jar
+mvn install:install-file -Dfile=bcprov-jdk18on-1.77.jar -DgroupId= -DartifactId=bcprov -Dversion=1.77 -Dpackaging=jar
+mvn install:install-file -Dfile=bcutil-jdk18on-1.77.jar -DgroupId= -DartifactId=bcutil -Dversion=1.77 -Dpackaging=jar
