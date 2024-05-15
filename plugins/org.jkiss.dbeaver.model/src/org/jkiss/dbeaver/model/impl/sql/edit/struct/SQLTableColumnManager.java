2b249d3b8ac86d619963ab7f7fb32ac8443d4f75
ShadelessFox <yanuari.channel@gmail.com>
2024-05-15 12:55:07 +0200

    dbeaver/pro#2787 Default value quoting fix
    I have no idea how it's supposed to work

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/impl/sql/edit/struct/SQLTableColumnManager.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/impl/sql/edit/struct/SQLTableColumnManager.java
@@ -309,7 +309,7 @@ public abstract class SQLTableColumnManager<OBJECT_TYPE extends DBSEntityAttribu
             @NotNull StringBuilder sql,
             @NotNull DBECommandAbstract<OBJECT_TYPE> command
         ) {
-            String defaultValue = CommonUtils.toString(column.getDefaultValue()).trim();
+            String defaultValue = CommonUtils.toString(column.getDefaultValue());
             if (!CommonUtils.isEmpty(defaultValue)) {
                 DBPDataKind dataKind = column.getDataKind();
                 boolean useQuotes = isUsesQuotes(defaultValue, dataKind);
@@ -319,16 +319,17 @@ public abstract class SQLTableColumnManager<OBJECT_TYPE extends DBSEntityAttribu
         }
 
         protected boolean isUsesQuotes(@NotNull String defaultValue, @NotNull DBPDataKind dataKind) {
-            boolean useQuotes = false;
             if (!defaultValue.startsWith(QUOTE) && !defaultValue.endsWith(QUOTE)) {
-                if (dataKind == DBPDataKind.DATETIME) {
-                    final char firstChar = defaultValue.charAt(0);
-                    if (!Character.isLetter(firstChar) && firstChar != '(' && firstChar != '[') {
-                        useQuotes = true;
+                if (dataKind == DBPDataKind.STRING || dataKind == DBPDataKind.DATETIME) {
+                    final String trimmed = defaultValue.trim();
+                    if (trimmed.isEmpty()) {
+                        return true;
                     }
+                    final char firstChar = trimmed.charAt(0);
+                    return !Character.isLetter(firstChar) && firstChar != '(' && firstChar != '[';
                 }
             }
-            return useQuotes;
+            return false;
         }
 
         protected void appendDefaultValue(@NotNull StringBuilder sql, @NotNull String defaultValue, boolean useQuotes) {
