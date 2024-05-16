5cb56d9dfa86ae05553b06abfbdfdd1a46578ea4
Sergei Kovalchuk <serjiokov@gmail.com>
2024-05-16 14:26:52 +0200

    dbeaver/pro#2623 Timeout settings for tasks HTF

--- plugins/org.jkiss.dbeaver.registry/src/org/jkiss/dbeaver/registry/task/TaskManagerImpl.java
+++ plugins/org.jkiss.dbeaver.registry/src/org/jkiss/dbeaver/registry/task/TaskManagerImpl.java
@@ -19,7 +19,10 @@ package org.jkiss.dbeaver.registry.task;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.stream.JsonWriter;
+
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.IJobChangeEvent;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.core.runtime.jobs.JobChangeAdapter;
@@ -47,6 +50,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 /**
  * TaskManagerImpl
@@ -68,6 +72,7 @@ public class TaskManagerImpl implements DBTTaskManager {
     private final List<TaskImpl> tasks = new ArrayList<>();
     private final List<TaskFolderImpl> tasksFolders = new ArrayList<>();
     private final Path statisticsFolder;
+    private Job serviceJob;
 
     public TaskManagerImpl(BaseProjectImpl projectMetadata, Path statisticsFolder) {
         this.projectMetadata = projectMetadata;
@@ -308,8 +313,17 @@ public class TaskManagerImpl implements DBTTaskManager {
     @Override
     public DBTTaskRunStatus runTask(@NotNull DBRProgressMonitor monitor, @NotNull DBTTask task, @NotNull DBTTaskExecutionListener listener) throws DBException {
         final TaskRunJob job = createJob((TaskImpl) task, listener);
-        final IStatus result = job.runDirectly(monitor);
-        final Throwable error = result.getException();
+        job.schedule();
+        if (serviceJob == null) {
+            serviceJob = new ServiceJob();
+            serviceJob.schedule();
+        }
+        try {
+            job.join();
+        } catch (InterruptedException e) {
+            throw new DBException("Error executing task", e);
+        }
+        final Throwable error = job.getResult().getException();
         if (error != null) {
             if (error instanceof DBException e) {
                 throw e;
@@ -325,6 +339,10 @@ public class TaskManagerImpl implements DBTTaskManager {
     public TaskRunJob scheduleTask(@NotNull DBTTask task, @NotNull DBTTaskExecutionListener listener) {
         final TaskRunJob runJob = createJob((TaskImpl) task, listener);
         runJob.schedule();
+        if (serviceJob == null) {
+            serviceJob = new ServiceJob();
+            serviceJob.schedule();
+        }
         return runJob;
     }
 
@@ -394,9 +412,11 @@ public class TaskManagerImpl implements DBTTaskManager {
                     String taskFolderName = JSONUtils.getString(taskJSON, TaskConstants.TAG_TASK_FOLDER);
                     Date createTime = systemDateFormat.parse(JSONUtils.getString(taskJSON, TaskConstants.TAG_CREATE_TIME));
                     Date updateTime = systemDateFormat.parse(JSONUtils.getString(taskJSON, TaskConstants.TAG_UPDATE_TIME));
+                    int maxExecutionTime = JSONUtils.getInteger(taskJSON, TaskConstants.TAG_MAX_EXEC_TIME);
                     Map<String, Object> state = JSONUtils.getObject(taskJSON, TaskConstants.TAG_STATE);
 
                     DBTTaskType taskDescriptor = getRegistry().getTaskType(task);
+                    
                     if (taskDescriptor == null) {
                         log.error("Can't find task descriptor " + task);
                         continue;
@@ -413,6 +433,7 @@ public class TaskManagerImpl implements DBTTaskManager {
                         taskFolder,
                         state
                     );
+                    taskConfig.setMaxExecutionTime(maxExecutionTime);
                     if (taskFolder != null) {
                         taskFolder.addTaskToFolder(taskConfig);
                         if (!tasksFolders.contains(taskFolder)) {
@@ -545,9 +566,35 @@ public class TaskManagerImpl implements DBTTaskManager {
             JSONUtils.field(jsonWriter, TaskConstants.TAG_CREATE_TIME, systemDateFormat.format(task.getCreateTime()));
             JSONUtils.field(jsonWriter, TaskConstants.TAG_UPDATE_TIME, systemDateFormat.format(task.getUpdateTime()));
             JSONUtils.serializeProperties(jsonWriter, TaskConstants.TAG_STATE, task.getProperties());
+            if (task.getMaxExecutionTime() > 0) {
+                JSONUtils.field(jsonWriter, TaskConstants.TAG_MAX_EXEC_TIME, task.getMaxExecutionTime());
+            }
             jsonWriter.endObject();
         }
         jsonWriter.endObject();
+    }
+
+    private class ServiceJob extends Job {
+        private static final int TASK_SLEEP_TIME = 1000;
+
+        public ServiceJob() {
+            super("Task canceling job");
+            setSystem(true);
+        }
+
+        @Override
+        protected IStatus run(
+            IProgressMonitor monitor) {
+            CopyOnWriteArraySet<TaskRunJob> copyOfRunningTasks = new CopyOnWriteArraySet<>(runningTasks);
+            for (TaskRunJob taskJob : copyOfRunningTasks) {
+                if (taskJob.isFinished() || taskJob.isCanceled()) {
+                    continue;
+                }
+                taskJob.cancelByTimeReached();
+            }
+            schedule(TASK_SLEEP_TIME);
+            return Status.OK_STATUS;
+        }
     }
 
 }
