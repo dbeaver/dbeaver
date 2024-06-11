e7159e5c6e414dda729b86acb8aa30aaaf3a0b12
serge-rider <serge@dbeaver.com>
2024-06-11 15:03:40 +0200

    dbeaver/dbeaver#23203 Disable filters confirmation if entity metadata is missing

--- plugins/org.jkiss.dbeaver.ui.editors.data/src/org/jkiss/dbeaver/ui/controls/resultset/ResultSetViewer.java
+++ plugins/org.jkiss.dbeaver.ui.editors.data/src/org/jkiss/dbeaver/ui/controls/resultset/ResultSetViewer.java
@@ -2635,14 +2635,19 @@ public class ResultSetViewer extends Viewer
                 // Column enumeration is expensive
             }
         }
-        if (isExpensiveFilter && getDataSource().getContainer().isExtraMetadataReadEnabled() &&
-            ConfirmationDialog.confirmAction(
-                viewerPanel.getShell(),
-                ConfirmationDialog.WARNING, ResultSetPreferences.CONFIRM_FILTER_RESULTSET,
-                ConfirmationDialog.CONFIRM,
-                curAttribute.getName()) != IDialogConstants.OK_ID)
-        {
-            return;
+        if (isExpensiveFilter) {
+            if (curAttribute.getEntityAttribute() == null) {
+                // No entity reference - we cannot fetch any filter values for this attribute anyway
+            } else {
+                if (ConfirmationDialog.confirmAction(
+                    viewerPanel.getShell(),
+                    ConfirmationDialog.WARNING, ResultSetPreferences.CONFIRM_FILTER_RESULTSET,
+                    ConfirmationDialog.CONFIRM,
+                    curAttribute.getName()) != IDialogConstants.OK_ID)
+                {
+                    return;
+                }
+            }
         }
 
         Collection<ResultSetRow> selectedRows = getSelection().getSelectedRows();
--- plugins/org.jkiss.dbeaver.ui.editors.data/src/org/jkiss/dbeaver/ui/controls/resultset/valuefilter/GenericFilterValueEdit.java
+++ plugins/org.jkiss.dbeaver.ui.editors.data/src/org/jkiss/dbeaver/ui/controls/resultset/valuefilter/GenericFilterValueEdit.java
@@ -702,10 +702,7 @@ class GenericFilterValueEdit {
             }
             try {
                 monitor.subTask("Read enumeration");
-                List<DBDLabelValuePair> valueEnumeration = null;
-                if (executionContext.getDataSource().getContainer().isExtraMetadataReadEnabled()) {
-                    valueEnumeration = readEnumeration(monitor);
-                }
+                List<DBDLabelValuePair> valueEnumeration = readEnumeration(monitor);
                 if (valueEnumeration == null) {
                     populateValues(Collections.emptyList());
                     return Status.OK_STATUS;
