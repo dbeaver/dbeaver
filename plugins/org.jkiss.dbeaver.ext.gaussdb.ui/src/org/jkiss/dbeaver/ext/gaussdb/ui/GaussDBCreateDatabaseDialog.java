40a2c9e1cd0e20ba719926268ab81d77792c2c06
GaussDB-Fans <kerenyzf@126.com>
2024-06-18 03:59:10 +0800

    Prepare for GaussDB

---
+++ plugins/org.jkiss.dbeaver.ext.gaussdb.ui/src/org/jkiss/dbeaver/ext/gaussdb/ui/GaussDBCreateDatabaseDialog.java
@@ -0,0 +1,14 @@
+package org.jkiss.dbeaver.ext.gaussdb.ui;
+
+import org.eclipse.swt.widgets.Shell;
+import org.jkiss.dbeaver.model.DBPImage;
+import org.jkiss.dbeaver.ui.dialogs.BaseDialog;
+
+public class GaussDBCreateDatabaseDialog extends BaseDialog {
+
+   public GaussDBCreateDatabaseDialog(Shell parentShell, String title, DBPImage icon) {
+      super(parentShell, title, icon);
+      // TODO Auto-generated constructor stub
+   }
+   
+}
