f9f9ea27c00ee31e5707a12ac4c4ce223a2b5d98
Sergei Kovalchuk <serjiokov@gmail.com>
2024-07-11 17:26:07 +0200

    dbeaver/dbeaver#21619 Simplify the logic

--- plugins/org.jkiss.dbeaver.ext.clickhouse/src/org/jkiss/dbeaver/ext/clickhouse/model/data/ClickHouseDateTimeValueHandler.java
+++ plugins/org.jkiss.dbeaver.ext.clickhouse/src/org/jkiss/dbeaver/ext/clickhouse/model/data/ClickHouseDateTimeValueHandler.java
@@ -16,17 +16,8 @@
  */
 package org.jkiss.dbeaver.ext.clickhouse.model.data;
 
-import org.jkiss.code.NotNull;
 import org.jkiss.dbeaver.model.data.DBDFormatSettings;
-import org.jkiss.dbeaver.model.exec.DBCException;
-import org.jkiss.dbeaver.model.exec.DBCResultSet;
-import org.jkiss.dbeaver.model.exec.DBCSession;
-import org.jkiss.dbeaver.model.exec.jdbc.JDBCResultSet;
 import org.jkiss.dbeaver.model.impl.jdbc.data.handlers.JDBCDateTimeValueHandler;
-import org.jkiss.dbeaver.model.struct.DBSTypedObject;
-
-import java.sql.SQLException;
-import java.sql.Types;
 
 public class ClickHouseDateTimeValueHandler extends JDBCDateTimeValueHandler {
 
@@ -35,43 +26,8 @@ public class ClickHouseDateTimeValueHandler extends JDBCDateTimeValueHandler {
     }
 
     @Override
-    public Object fetchValueObject(@NotNull DBCSession session,
-        @NotNull DBCResultSet resultSet,
-        @NotNull DBSTypedObject type,
-        int index
-    ) throws DBCException {
-        try {
-            if (resultSet instanceof JDBCResultSet) {
-                JDBCResultSet dbResults = (JDBCResultSet) resultSet;
-
-                // check for native format
-                if (formatSettings.isUseNativeDateTimeFormat()) {
-                    try {
-                        return dbResults.getString(index + 1);
-                    } catch (SQLException e) {
-                        log.debug("Can't read date/time value as string: " + e.getMessage());
-                    }
-                }
-
-                // It seems that some drivers doesn't support reading date/time values with
-                // explicit calendar
-                // So let's use simple version
-                switch (type.getTypeID()) {
-                    case Types.TIME:
-                    case Types.TIME_WITH_TIMEZONE:
-                        return dbResults.getTime(index + 1);
-                    // The datetime is type is incorrect fetch from driver skip formatting and
-                    // return as string from LocalTime object
-                    case Types.DATE:
-                    default:
-                        Object value = dbResults.getObject(index + 1);
-                        return getValueFromObject(session, type, value, false, false);
-                }
-            }
-        } catch (Exception e) {
-            throw new DBCException(e.getMessage(), e);
-        }
-        return super.fetchValueObject(session, resultSet, type, index);
+    protected boolean isReadDateAsObject() {
+        return true;
     }
 
 }
