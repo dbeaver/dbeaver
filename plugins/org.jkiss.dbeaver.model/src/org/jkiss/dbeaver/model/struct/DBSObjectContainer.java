7d5f1054d638e1fb8fed7d517f26490b2be79931
serge-rider <serge@dbeaver.com>
2024-07-16 15:54:47 +0200

    Nullable annotation fixed

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/struct/DBSObjectContainer.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/struct/DBSObjectContainer.java
@@ -51,9 +51,7 @@ public interface DBSObjectContainer extends DBSObject
      * @return collection of child objects (not null).
      *  Objects type depends on implementor (catalogs, schemas, tables, etc)
      * @throws DBException on any DB error
-     * @param monitor progress monitor. If null passed then implementation
-     *                mustn't perform any expensive operations like cache reading.
-     *                In this case it can return null as a result (but also may return some cached collection)
+     * @param monitor progress monitor
      */
     @DPIElement
     Collection<? extends DBSObject> getChildren(@NotNull DBRProgressMonitor monitor) throws DBException;
