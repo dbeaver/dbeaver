9f3792b9647811d8adb5d044b745fe67d1f9543f
Sergei Kovalchuk <serjiokov@gmail.com>
2024-07-17 14:21:57 +0200

    dbeaver/pro#2782 Import connections from latest DBvizualizer is not working
    ssh configuration import

---
+++ plugins/org.jkiss.dbeaver.ext.import_config/src/org/jkiss/dbeaver/ext/import_config/dbvis/DbvisConfigurationCreatorv233.java
@@ -0,0 +1,192 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2024 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jkiss.dbeaver.ext.import_config.dbvis;
+
+import org.jkiss.dbeaver.DBException;
+import org.jkiss.dbeaver.ext.import_config.wizards.ImportConnectionInfo;
+import org.jkiss.dbeaver.ext.import_config.wizards.ImportData;
+import org.jkiss.dbeaver.ext.import_config.wizards.ImportDriverInfo;
+import org.jkiss.dbeaver.model.net.DBWHandlerConfiguration;
+import org.jkiss.dbeaver.model.net.ssh.SSHConstants;
+import org.jkiss.dbeaver.registry.network.NetworkHandlerDescriptor;
+import org.jkiss.dbeaver.registry.network.NetworkHandlerRegistry;
+import org.jkiss.utils.CommonUtils;
+import org.jkiss.utils.xml.XMLException;
+import org.jkiss.utils.xml.XMLUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class DbvisConfigurationCreatorv233 extends DbvisAbstractConfigurationCreator {
+
+    public static final String VERSION = "version.24.3.x"; //$NON-NLS-1$
+    public static final String CONFIG_FOLDER = "config233"; //$NON-NLS-1$
+    public static final String CONFIG_FILE = "dbvis.xml"; //$NON-NLS-1$
+
+    @Override
+    public ImportData create(
+        ImportData importData,
+        File configFile) throws DBException {
+        try {
+            // SshServers
+            Map<String, DbvisSshServerConfiguration> sshServerConfigurations = new LinkedHashMap<>();
+            DbvisSshServerConfiguration sshConfiguration = null;
+            File sshServersFile = new File(configFile.getParent(), "sshservers.xml");
+            if (sshServersFile.exists()) {
+                Document sshConfigDocument = XMLUtils.parseDocument(sshServersFile);
+                Element sshServersElement = XMLUtils.getChildElement(sshConfigDocument.getDocumentElement(), "SshServers");
+                if (sshServersElement != null) {
+                    for (Element sshServerElement : XMLUtils.getChildElementList(sshServersElement, "SshServer")) {
+                        DbvisSshServerConfiguration sshConfig = new DbvisSshServerConfiguration();
+                        sshConfig.setId(sshServerElement.getAttribute("id"));
+                        sshConfig.setName(XMLUtils.getChildElementBody(sshServerElement, "Name"));
+                        sshConfig.setDescription(XMLUtils.getChildElementBody(sshServerElement, "Description"));
+                        sshConfig.setIsEnabled(XMLUtils.getChildElementBody(sshServerElement, "Enabled"));
+                        sshConfig.setSshHost(XMLUtils.getChildElementBody(sshServerElement, "SshHost"));
+                        sshConfig.setSshPort(XMLUtils.getChildElementBody(sshServerElement, "SshPort"));
+                        sshConfig.setSshUserid(XMLUtils.getChildElementBody(sshServerElement, "SshUserid"));
+                        sshConfig.setAuthenticationType(XMLUtils.getChildElementBody(sshServerElement, "SshHost"));
+                        sshConfig.setSshPrivateKeyFile(XMLUtils.getChildElementBody(sshServerElement, "SshPrivateKeyFile"));
+                        sshConfig.setSshImplementationType(XMLUtils.getChildElementBody(sshServerElement, "SshImplementationType"));
+                        sshServerConfigurations.put(sshConfig.getId(), sshConfig);
+                    }
+                }
+            }
+            Document configDocument = XMLUtils.parseDocument(configFile);
+            Element databasesElement = XMLUtils.getChildElement(configDocument.getDocumentElement(), "Databases");
+            if (databasesElement != null) {
+                for (Element dbElement : XMLUtils.getChildElementList(databasesElement, "Database")) {
+                    String alias = XMLUtils.getChildElementBody(dbElement, "Alias");
+                    String url = XMLUtils.getChildElementBody(dbElement, "Url");
+                    String driverName = XMLUtils.getChildElementBody(dbElement, "Driver");
+                    String user = XMLUtils.getChildElementBody(dbElement, "Userid");
+                    String password = null;
+                    // String passwordEncoded = XMLUtils.getChildElementBody(dbElement, "Password");
+                    String hostName = null, port = null, database = null;
+                    Element urlVarsElement = XMLUtils.getChildElement(dbElement, "UrlVariables");
+                    if (urlVarsElement != null) {
+                        Element driverElement = XMLUtils.getChildElement(urlVarsElement, "Driver");
+                        if (driverElement != null) {
+                            String templateId = driverElement.getAttribute("TemplateId");
+                            String driverId = driverElement.getAttribute("DriverId");
+                            String[] driverIdSegments = driverId.split("-");
+                            for (Element urlVarElement : XMLUtils.getChildElementList(driverElement, "UrlVariable")) {
+                                final String varName = urlVarElement.getAttribute("UrlVariableName");
+                                final String varValue = XMLUtils.getElementBody(urlVarElement);
+                                if ("Server".equals(varName)) {
+                                    hostName = varValue;
+                                } else if ("Port".equals(varName)) {
+                                    port = varValue;
+                                } else if ("Database".equals(varName)) {
+                                    database = varValue;
+                                }
+                            }
+                            StringBuilder builder = new StringBuilder("databaseinfo/user/driverTypes/");
+                            builder.append(templateId)
+                                .append("_")
+                                .append(driverId)
+                                .append("/")
+                                .append("driverType")
+                                .append("_")
+                                .append(templateId)
+                                .append("_")
+                                .append(driverIdSegments[0])
+                                .append(".xml");
+                            File driverFile = new File(configFile.getParent(), builder.toString());
+                            Document driverTypeDocument = XMLUtils.parseDocument(driverFile);
+                            Element driverTypeDocumentElement = driverTypeDocument.getDocumentElement();
+                            String name = XMLUtils.getChildElementBody(driverTypeDocumentElement, "Label");
+                            String sampleURL = XMLUtils.getChildElementBody(driverTypeDocumentElement, "URLFormat");
+                            String driverClass = XMLUtils.getChildElementBody(driverTypeDocumentElement, "DefaultClass");
+                            String identifier = XMLUtils.getChildElementBody(driverTypeDocumentElement, "Identifier");
+                            String originId = XMLUtils.getChildElementBody(driverTypeDocumentElement, "OriginId");
+                            if (!CommonUtils.isEmpty(name) && !CommonUtils.isEmpty(sampleURL) && !CommonUtils.isEmpty(driverClass)) {
+                                ImportDriverInfo driver = new ImportDriverInfo(identifier, name, sampleURL, driverClass);
+                                adaptSampleUrl(driver);
+                                importData.addDriver(driver);
+                            }
+
+                            Element sshServers = XMLUtils.getChildElement(dbElement, "SshServers");
+                            if (sshServers != null) {
+                                for (Element sshServer : XMLUtils.getChildElementList(sshServers, "SshServer")) {
+                                    String enabled = XMLUtils.getChildElementBody(sshServer, "Enabled");
+                                    if (enabled.equals("true")) {
+                                        sshConfiguration = sshServerConfigurations.get(sshServer.getAttribute("id"));
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if (!CommonUtils.isEmpty(alias) && !CommonUtils.isEmpty(driverName)
+                        && (!CommonUtils.isEmpty(url) || !CommonUtils.isEmpty(hostName))) {
+                        ImportDriverInfo driver = importData.getDriver(driverName);
+                        if (driver != null) {
+                            ImportConnectionInfo connectionInfo = new ImportConnectionInfo(
+                                driver,
+                                dbElement.getAttribute("id"),
+                                alias,
+                                url,
+                                hostName,
+                                port,
+                                database,
+                                user,
+                                password);
+                            if (sshConfiguration != null) {
+                                NetworkHandlerDescriptor sslHD = NetworkHandlerRegistry.getInstance().getDescriptor("ssh_tunnel");
+                                DBWHandlerConfiguration sshHandler = new DBWHandlerConfiguration(sslHD, null);
+                                sshHandler.setUserName(sshConfiguration.getSshUserid());
+                                sshHandler.setSavePassword(true);
+                                sshHandler.setProperty(DBWHandlerConfiguration.PROP_HOST, sshConfiguration.getSshHost());
+                                sshHandler.setProperty(DBWHandlerConfiguration.PROP_PORT, sshConfiguration.getSshPort());
+                                if (sshConfiguration.getAuthenticationType() == SSHConstants.AuthType.PUBLIC_KEY) {
+                                    sshHandler.setProperty(SSHConstants.PROP_AUTH_TYPE, SSHConstants.AuthType.PUBLIC_KEY);
+                                    sshHandler.setProperty(SSHConstants.PROP_KEY_PATH, sshConfiguration.getSshPrivateKeyFile());
+                                } else {
+                                    sshHandler.setProperty(SSHConstants.PROP_AUTH_TYPE, SSHConstants.AuthType.PASSWORD);
+                                }
+                                sshHandler.setProperty(SSHConstants.PROP_IMPLEMENTATION, sshConfiguration.getSshImplementationType());
+                                sshHandler.setEnabled(true);
+                                connectionInfo.addNetworkHandler(sshHandler);
+                            }
+                            importData.addConnection(connectionInfo);
+                        }
+                    }
+                }
+            }
+        } catch (XMLException e) {
+            throw new DBException("Configuration parse error: " + e.getMessage());
+        }
+        return importData;
+    }
+
+    @Override
+    public String getConfigurationFile() {
+        return CONFIG_FILE;
+    }
+
+    @Override
+    public String getConfigurationFolder() {
+        return CONFIG_FOLDER;
+    }
+
+}
---
+++ plugins/org.jkiss.dbeaver.ext.import_config/src/org/jkiss/dbeaver/ext/import_config/dbvis/DbvisSshServerConfiguration.java
@@ -0,0 +1,182 @@
+package org.jkiss.dbeaver.ext.import_config.dbvis;
+
+import org.jkiss.dbeaver.model.net.ssh.SSHConstants;
+
+public class DbvisSshServerConfiguration {
+    private String id;
+    private String name;
+    private String description;
+    private String isEnabled;
+    private String sshHost;
+    private String sshPort;
+    private String sshUserid;
+    private SSHConstants.AuthType authenticationType;
+    private String sshPrivateKeyFile;
+    private String useSshConfigFile;
+    private String sshConfigFile;
+    private String sshKnownHostsFile;
+    private String sshConnectTimeout;
+    private String sshKeepAliveInterval;
+    private String sshMaxAuthTries;
+    private String savePasswords;
+    private String promptPassphrase;
+    private String sshJumpServerId;
+    private String sshImplementationType;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getIsEnabled() {
+        return isEnabled;
+    }
+
+    public String getSshHost() {
+        return sshHost;
+    }
+
+    public String getSshPort() {
+        return sshPort;
+    }
+
+    public String getSshUserid() {
+        return sshUserid;
+    }
+
+    public SSHConstants.AuthType getAuthenticationType() {
+        return authenticationType;
+    }
+
+    public String getSshPrivateKeyFile() {
+        return sshPrivateKeyFile;
+    }
+
+    public String getUseSshConfigFile() {
+        return useSshConfigFile;
+    }
+
+    public String getSshConfigFile() {
+        return sshConfigFile;
+    }
+
+    public String getSshKnownHostsFile() {
+        return sshKnownHostsFile;
+    }
+
+    public String getSshConnectTimeout() {
+        return sshConnectTimeout;
+    }
+
+    public String getSshKeepAliveInterval() {
+        return sshKeepAliveInterval;
+    }
+
+    public String getSshMaxAuthTries() {
+        return sshMaxAuthTries;
+    }
+
+    public String getSavePasswords() {
+        return savePasswords;
+    }
+
+    public String getPromptPassphrase() {
+        return promptPassphrase;
+    }
+
+    public String getSshJumpServerId() {
+        return sshJumpServerId;
+    }
+
+    public String getSshImplementationType() {
+        return sshImplementationType;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setIsEnabled(String isEnabled) {
+        this.isEnabled = isEnabled;
+    }
+
+    public void setSshHost(String sshHost) {
+        this.sshHost = sshHost;
+    }
+
+    public void setSshPort(String sshPort) {
+        this.sshPort = sshPort;
+    }
+
+    public void setSshUserid(String sshUserid) {
+        this.sshUserid = sshUserid;
+    }
+
+    public void setAuthenticationType(String authenticationType) {
+        if (authenticationType == "PUBLIC_KEY") {
+            this.authenticationType = SSHConstants.AuthType.PUBLIC_KEY;
+        } else {
+            this.authenticationType = SSHConstants.AuthType.PASSWORD;
+        }
+    }
+ 
+    public void setSshPrivateKeyFile(String sshPrivateKeyFile) {
+        this.sshPrivateKeyFile = sshPrivateKeyFile;
+    }
+
+    public void setUseSshConfigFile(String useSshConfigFile) {
+        this.useSshConfigFile = useSshConfigFile;
+    }
+
+    public void setSshConfigFile(String sshConfigFile) {
+        this.sshConfigFile = sshConfigFile;
+    }
+
+    public void setSshKnownHostsFile(String sshKnownHostsFile) {
+        this.sshKnownHostsFile = sshKnownHostsFile;
+    }
+
+    public void setSshConnectTimeout(String sshConnectTimeout) {
+        this.sshConnectTimeout = sshConnectTimeout;
+    }
+
+    public void setSshKeepAliveInterval(String sshKeepAliveInterval) {
+        this.sshKeepAliveInterval = sshKeepAliveInterval;
+    }
+
+    public void setSshMaxAuthTries(String sshMaxAuthTries) {
+        this.sshMaxAuthTries = sshMaxAuthTries;
+    }
+
+    public void setSavePasswords(String savePasswords) {
+        this.savePasswords = savePasswords;
+    }
+
+    public void setPromptPassphrase(String promptPassphrase) {
+        this.promptPassphrase = promptPassphrase;
+    }
+
+    public void setSshJumpServerId(String sshJumpServerId) {
+        this.sshJumpServerId = sshJumpServerId;
+    }
+
+    public void setSshImplementationType(String sshImplementationType) {
+        this.sshImplementationType = sshImplementationType;
+    }
+
+}
