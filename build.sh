fb0053fee04c00342fb391267b3d8cbefdf6fb59
Nikita Akilov <26031301+n5a5@users.noreply.github.com>
2024-07-31 13:05:29 +0200

    dbeaver/dbeaver-infra#114 use maven wrapper instead of standalone maven

--- build.sh
+++ build.sh
@@ -2,7 +2,4 @@
 
 [ ! -d ../dbeaver-common ] && git clone https://github.com/dbeaver/dbeaver-common.git ../dbeaver-common
 
-cd product/aggregate
-mvn clean install -Pall-platforms -T 1C
-cd ../..
-
+../dbeaver-common/mvnw clean install -Pall-platforms -T 1C -f "product/aggregate"
