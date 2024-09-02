0184f4b6814a151f38a11df2a3b77a04b070b89c
Elizabeth <e1izabeth.k@outlook.com>
2024-09-02 21:02:46 +0300

    dbeaver/pro#3257 Fix query processor selection, when processor has multiple tabs and some of them are unpinned

--- plugins/org.jkiss.dbeaver.ui.editors.sql/src/org/jkiss/dbeaver/ui/editors/sql/SQLEditor.java
+++ plugins/org.jkiss.dbeaver.ui.editors.sql/src/org/jkiss/dbeaver/ui/editors/sql/SQLEditor.java
@@ -2782,7 +2782,7 @@ public class SQLEditor extends SQLEditorBase implements
                             curQueryProcessor = processor;
                             break;
                         }
-                        anyNotPinnedTab = anyNotPinnedTab || processor.hasNotPinnedTabs();
+                        anyNotPinnedTab = anyNotPinnedTab || !processor.hasPinnedTabs();
                     }
                 }
                 // Just create a new query processor
@@ -3642,15 +3642,6 @@ public class SQLEditor extends SQLEditorBase implements
         public boolean hasPinnedTabs() {
             for (QueryResultsContainer container : resultContainers) {
                 if (container.isPinned()) {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        public boolean hasNotPinnedTabs() {
-            for (QueryResultsContainer container : resultContainers) {
-                if (!container.isPinned()) {
                     return true;
                 }
             }
