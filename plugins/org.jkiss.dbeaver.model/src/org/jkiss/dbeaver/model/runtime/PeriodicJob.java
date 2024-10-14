a9b809a660d6705bb5233fe346a7c6a1eb09431f
Aleksandr Skoblikov <aleksandr.skoblikov@dbeaver.com>
2024-10-14 13:46:29 +0400

    CB-5424 update logs

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/runtime/PeriodicJob.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/runtime/PeriodicJob.java
@@ -43,9 +43,9 @@ public abstract class PeriodicJob extends AbstractJob {
             return Status.OK_STATUS;
         }
 
-        log.info(getName() + " job is started");
+        log.info(getName() + " job started");
         doJob(monitor);
-        log.info(getName() + " job is finished");
+        log.info(getName() + " job finished");
         // If the platform is still running after the job is completed, reschedule the job
         if (!platform.isShuttingDown()) {
             scheduleMonitor();
