af65089ce82b1fe1f06a128679f89067f3d585cf
Serge Rider <serge@dbeaver.com>
2024-11-05 09:43:37 +0100

    dbeaver/dbeaver#23361 Include in dbeaver and cloudbeaver community

--- tools/build.cmd
+++ tools/build.cmd
@@ -1,5 +1,6 @@
 cd ..
 IF NOT EXIST ..\dbeaver-common git clone https://github.com/dbeaver/dbeaver-common.git ..\dbeaver-common
+IF NOT EXIST ..\dbeaver-jdbc-libsql git clone https://github.com/dbeaver/dbeaver-jdbc-libsql.git ..\dbeaver-jdbc-libsql
 cd product/aggregate
 call mvn clean package -Pall-platforms -T 1C
 cd ../..
