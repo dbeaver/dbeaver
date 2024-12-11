81c99716ca8ad67d1187d544d666cbf99c7a4af9
Ainur <ainur.iagudin@dbeaver.com>
2024-12-11 16:39:21 +0100

    CB-3508 do not have results in task info event

--- plugins/org.jkiss.dbeaver.model.event/src/org/jkiss/dbeaver/model/websocket/event/session/WSSessionTaskInfoEvent.java
+++ plugins/org.jkiss.dbeaver.model.event/src/org/jkiss/dbeaver/model/websocket/event/session/WSSessionTaskInfoEvent.java
@@ -29,23 +29,15 @@ public class WSSessionTaskInfoEvent extends WSAbstractSessionEvent {
     @Nullable
     private final String statusName;
     private final boolean running;
-    @Nullable
-    private final Object result;
-    @Nullable
-    private final Object extendedResult;
 
     public WSSessionTaskInfoEvent(
         @NotNull String taskId,
         @Nullable String statusName,
-        @Nullable Object result,
-        @Nullable Object extendedResult,
         boolean running
     ) {
         super(WSEventType.SESSION_TASK_INFO_UPDATED);
         this.taskId = taskId;
         this.statusName = statusName;
-        this.result = result;
-        this.extendedResult = extendedResult;
         this.running = running;
     }
 
@@ -57,16 +49,6 @@ public class WSSessionTaskInfoEvent extends WSAbstractSessionEvent {
     @Nullable
     public String getStatusName() {
         return statusName;
-    }
-
-    @Nullable
-    public Object getResult() {
-        return result;
-    }
-
-    @Nullable
-    public Object getExtendedResult() {
-        return extendedResult;
     }
 
     public boolean isRunning() {
