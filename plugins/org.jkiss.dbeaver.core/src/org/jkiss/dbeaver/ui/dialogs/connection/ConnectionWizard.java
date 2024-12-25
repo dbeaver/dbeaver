ebb1f7e13998a00b2dedb9202787130438870328
IvanGorshechnikov <ivan.gorshechnikov1@gmail.com>
2024-12-25 15:12:34 +0100

    #36432 Fix after review

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/dialogs/connection/ConnectionWizard.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/dialogs/connection/ConnectionWizard.java
@@ -236,7 +236,7 @@ public abstract class ConnectionWizard extends ActiveWizard implements IConnecti
 
                 new ConnectionTestDialog(
                     getShell(),
-                    activeDataSource,
+                    targetDataSource,
                     op.getServerVersion(),
                     op.getClientVersion(),
                     op.getConnectTime()).open();
