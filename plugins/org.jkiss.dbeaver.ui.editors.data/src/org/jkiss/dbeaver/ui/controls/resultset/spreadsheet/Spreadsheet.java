77b770e25f5e8ac5664ddc5448aa09ba1218a489
Serge Rider <serge@dbeaver.com>
2025-01-16 05:20:30 +0100

    dbeaver/pro#3965 Context menu fix

--- plugins/org.jkiss.dbeaver.ui.editors.data/src/org/jkiss/dbeaver/ui/controls/resultset/spreadsheet/Spreadsheet.java
+++ plugins/org.jkiss.dbeaver.ui.editors.data/src/org/jkiss/dbeaver/ui/controls/resultset/spreadsheet/Spreadsheet.java
@@ -443,8 +443,8 @@ public class Spreadsheet extends LightGrid implements Listener {
             } else {
                 presentation.fillContextMenu(
                     manager,
-                    isHoveringOnRowHeader() ? null : getColumnByPosition(focusPos),
-                    isHoveringOnHeader() ? null : getRowByPosition(focusPos)
+                    isHoveringOnRowHeader() ? getFocusColumn() : getColumnByPosition(focusPos),
+                    isHoveringOnHeader() ? getFocusRow() : getRowByPosition(focusPos)
                 );
             }
         });
