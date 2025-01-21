944323f4603be15cc9634ac7568e9480bfce9ca2
Serge Rider <serge@dbeaver.com>
2025-01-21 05:30:37 +0100

    dbeaver/pro#3984 File handlers model/registry

--- plugins/org.jkiss.dbeaver.ui.app.standalone/src/org/jkiss/dbeaver/ui/app/standalone/rpc/DBeaverInstanceServer.java
+++ plugins/org.jkiss.dbeaver.ui.app.standalone/src/org/jkiss/dbeaver/ui/app/standalone/rpc/DBeaverInstanceServer.java
@@ -29,6 +29,7 @@ import org.jkiss.code.Nullable;
 import org.jkiss.dbeaver.Log;
 import org.jkiss.dbeaver.model.DBPDataSourceContainer;
 import org.jkiss.dbeaver.model.app.DBPPlatformDesktop;
+import org.jkiss.dbeaver.model.app.DBPProject;
 import org.jkiss.dbeaver.model.app.DBPWorkspace;
 import org.jkiss.dbeaver.registry.DataSourceUtils;
 import org.jkiss.dbeaver.runtime.DBWorkbench;
@@ -165,21 +166,26 @@ public class DBeaverInstanceServer implements IInstanceController {
         final IWorkbenchWindow window = UIUtils.getActiveWorkbenchWindow();
         final Shell shell = window.getShell();
         UIUtils.asyncExec(() -> {
-            for (String filePath : fileNames) {
-                File file = new File(filePath);
-                if (file.exists()) {
-                    filesToConnect.add(file);
-                    if (dataSourceContainer != null) {
-                        EditorUtils.setFileDataSource(file, new SQLNavigatorContext(dataSourceContainer));
-                    }
-                    EditorUtils.openExternalFileEditor(file, window);
-                } else {
-                    DBWorkbench.getPlatformUI().showError("Open file", "Can't open '" + file.getAbsolutePath() + "': file doesn't exist");
-                }
-            }
+            openFileEditors(window, fileNames);
             shell.setMinimized(false);
             shell.forceActive();
         });
+    }
+
+    private void openFileEditors(@NotNull IWorkbenchWindow window, @NotNull String [] fileNames) {
+        for (String filePath : fileNames) {
+            Path path = Path.of(filePath);
+            if (Files.exists(path)) {
+                File file = path.toFile();
+                filesToConnect.add(file);
+                if (dataSourceContainer != null) {
+                    EditorUtils.setFileDataSource(file, new SQLNavigatorContext(dataSourceContainer));
+                }
+                EditorUtils.openExternalFileEditor(file, window);
+            } else {
+                DBWorkbench.getPlatformUI().showError("Open file", "Can't open '" + filePath + "': file doesn't exist");
+            }
+        }
     }
 
     @Override
@@ -187,30 +193,35 @@ public class DBeaverInstanceServer implements IInstanceController {
         // Do not log it (#3788)
         //log.debug("Open external database connection [" + connectionSpec + "]");
         InstanceConnectionParameters instanceConParameters = new InstanceConnectionParameters();
-        final DBPDataSourceContainer dataSource = DataSourceUtils.getDataSourceBySpec(
-            DBWorkbench.getPlatform().getWorkspace().getActiveProject(),
+        DBPProject activeProject = DBWorkbench.getPlatform().getWorkspace().getActiveProject();
+        if (activeProject == null) {
+            log.error("No active project in workspace");
+            return;
+        }
+        dataSourceContainer = DataSourceUtils.getDataSourceBySpec(
+            activeProject,
             GeneralUtils.replaceVariables(connectionSpec, SystemVariablesResolver.INSTANCE),
             instanceConParameters,
             false,
             instanceConParameters.createNewConnection);
-        if (dataSource == null) {
+        if (dataSourceContainer == null) {
             filesToConnect.clear();
             return;
         }
         if (!CommonUtils.isEmpty(filesToConnect)) {
             for (File file : filesToConnect) {
-                EditorUtils.setFileDataSource(file, new SQLNavigatorContext(dataSource));
+                EditorUtils.setFileDataSource(file, new SQLNavigatorContext(dataSourceContainer));
             }
         }
         if (instanceConParameters.openConsole) {
             final IWorkbenchWindow workbenchWindow = UIUtils.getActiveWorkbenchWindow();
             UIUtils.syncExec(() -> {
-                SQLEditorHandlerOpenEditor.openSQLConsole(workbenchWindow, new SQLNavigatorContext(dataSource), dataSource.getName(), "");
+                SQLEditorHandlerOpenEditor.openSQLConsole(workbenchWindow, new SQLNavigatorContext(dataSourceContainer), dataSourceContainer.getName(), "");
                 workbenchWindow.getShell().forceActive();
 
             });
         } else if (instanceConParameters.makeConnect) {
-            DataSourceHandler.connectToDataSource(null, dataSource, null);
+            DataSourceHandler.connectToDataSource(null, dataSourceContainer, null);
         }
         filesToConnect.clear();
     }
@@ -248,11 +259,9 @@ public class DBeaverInstanceServer implements IInstanceController {
 
         UIUtils.syncExec(() -> {
             IWorkbenchWindow window = UIUtils.getActiveWorkbenchWindow();
-            if (window != null) {
-                IWorkbenchPage page = window.getActivePage();
-                if (page != null) {
-                    page.closeAllEditors(false);
-                }
+            IWorkbenchPage page = window.getActivePage();
+            if (page != null) {
+                page.closeAllEditors(false);
             }
         });
     }
@@ -318,19 +327,21 @@ public class DBeaverInstanceServer implements IInstanceController {
 
         @Override
         public boolean setParameter(String name, String value) {
-            switch (name) {
-                case "connect":
+            return switch (name) {
+                case "connect" -> {
                     makeConnect = CommonUtils.toBoolean(value);
-                    return true;
-                case "openConsole":
+                    yield true;
+                }
+                case "openConsole" -> {
                     openConsole = CommonUtils.toBoolean(value);
-                    return true;
-                case "create":
+                    yield true;
+                }
+                case "create" -> {
                     createNewConnection = CommonUtils.toBoolean(value);
-                    return true;
-                default:
-                    return false;
-            }
+                    yield true;
+                }
+                default -> false;
+            };
         }
     }
 }
---
+++ plugins/org.jkiss.dbeaver.ui.editors.base/src/org/jkiss/dbeaver/ui/editors/file/FileTypeHandlerRegistry.java
@@ -0,0 +1,71 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2025 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jkiss.dbeaver.ui.editors.file;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtensionRegistry;
+import org.eclipse.core.runtime.Platform;
+import org.jkiss.code.NotNull;
+import org.jkiss.code.Nullable;
+import org.jkiss.dbeaver.Log;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FileTypeHandlerRegistry {
+    private static final Log log = Log.getLog(FileTypeHandlerRegistry.class);
+
+    public static final String CONFIG_FILE_NAME = "dataformat-profiles.xml"; //$NON-NLS-1$
+
+    private static FileTypeHandlerRegistry instance = null;
+
+    public synchronized static FileTypeHandlerRegistry getInstance() {
+        if (instance == null) {
+            instance = new FileTypeHandlerRegistry(Platform.getExtensionRegistry());
+        }
+        return instance;
+    }
+
+    private final List<FileTypeHandlerDescriptor> handlers = new ArrayList<>();
+    private final Map<String, FileTypeHandlerDescriptor> handlerByExtension = new HashMap<>();
+
+    private FileTypeHandlerRegistry(IExtensionRegistry registry) {
+        {
+            IConfigurationElement[] extElements = registry.getConfigurationElementsFor(FileTypeHandlerDescriptor.EXTENSION_ID);
+            for (IConfigurationElement ext : extElements) {
+                FileTypeHandlerDescriptor formatterDescriptor = new FileTypeHandlerDescriptor(ext);
+                handlers.add(formatterDescriptor);
+                for (String fileExt : formatterDescriptor.getExtensions()) {
+                    handlerByExtension.put(fileExt, formatterDescriptor);
+                }
+            }
+        }
+    }
+
+    @NotNull
+    public List<FileTypeHandlerDescriptor> getHandlers() {
+        return handlers;
+    }
+
+    @Nullable
+    public FileTypeHandlerDescriptor findHandler(String fileExtension) {
+        return handlerByExtension.get(fileExtension);
+    }
+
+}
