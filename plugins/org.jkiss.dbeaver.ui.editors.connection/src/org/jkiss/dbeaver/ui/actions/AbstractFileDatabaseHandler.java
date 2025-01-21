85962fc95da9464e4d2d8551f4358c59e3901ee0
serge-rider <serge@dbeaver.com>
2025-01-21 12:04:16 +0100

    dbeaver/pro#3984 Datasource refactoring. Persist entity editor tabs config

---
+++ plugins/org.jkiss.dbeaver.ui.editors.connection/src/org/jkiss/dbeaver/ui/actions/AbstractFileDatabaseHandler.java
@@ -0,0 +1,126 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2025 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jkiss.dbeaver.ui.actions;
+
+import org.jkiss.api.DriverReference;
+import org.jkiss.code.NotNull;
+import org.jkiss.code.Nullable;
+import org.jkiss.dbeaver.DBException;
+import org.jkiss.dbeaver.Log;
+import org.jkiss.dbeaver.model.DBPDataSource;
+import org.jkiss.dbeaver.model.DBPDataSourceContainer;
+import org.jkiss.dbeaver.model.app.DBPProject;
+import org.jkiss.dbeaver.model.connection.DBPConnectionConfiguration;
+import org.jkiss.dbeaver.model.connection.DBPDriver;
+import org.jkiss.dbeaver.model.navigator.DBNDatabaseNode;
+import org.jkiss.dbeaver.model.navigator.DBNUtils;
+import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
+import org.jkiss.dbeaver.model.struct.DBSEntity;
+import org.jkiss.dbeaver.model.struct.DBSObject;
+import org.jkiss.dbeaver.model.struct.DBSObjectContainer;
+import org.jkiss.dbeaver.runtime.DBWorkbench;
+import org.jkiss.dbeaver.ui.UIUtils;
+import org.jkiss.dbeaver.ui.editors.file.IFileTypeHandler;
+import org.jkiss.dbeaver.ui.navigator.NavigatorUtils;
+
+import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Database file handler
+ */
+public abstract class AbstractFileDatabaseHandler implements IFileTypeHandler {
+
+    private static final Log log = Log.getLog(AbstractFileDatabaseHandler.class);
+
+    @Override
+    public void openFiles(
+        @NotNull List<Path> fileList,
+        @NotNull Map<String, String> parameters,
+        @Nullable DBPDataSourceContainer providedDataSource
+    ) {
+        DBPProject project = DBWorkbench.getPlatform().getWorkspace().getActiveProject();
+        if (project == null) {
+            log.error("No active project - cannot open file");
+            return;
+        }
+        DBPDriver driver = DBWorkbench.getPlatform().getDataSourceProviderRegistry().findDriver(getDriverReference());
+        if (driver == null) {
+            log.error("Driver '" + getDriverReference() + "' not found");
+            return;
+        }
+        DBPConnectionConfiguration configuration = new DBPConnectionConfiguration();
+        String databaseName = createDatabaseName(fileList);
+        configuration.setDatabaseName(databaseName);
+        DBPDataSourceContainer dsContainer = project.getDataSourceRegistry().createDataSource(driver, configuration);
+        dsContainer.setName(databaseName);
+        dsContainer.setTemporary(true);
+
+        try {
+            project.getDataSourceRegistry().addDataSource(dsContainer);
+        } catch (DBException e) {
+            log.error(e);
+            return;
+        }
+
+        try {
+            UIUtils.runInProgressService(monitor -> {
+                try {
+                    if (dsContainer.connect(monitor, true, true)) {
+                        DBPDataSource dataSource = dsContainer.getDataSource();
+                        if (dataSource instanceof DBSObjectContainer container) {
+                            openEntitiesFrom(monitor, container);
+                        }
+                    }
+                } catch (DBException e) {
+                    throw new InvocationTargetException(e);
+                }
+            });
+        } catch (InvocationTargetException e) {
+            DBWorkbench.getPlatformUI().showError(
+                "Connecting to " + getDatabaseTerm() + " datasource",
+                "Error connecting to " + getDatabaseTerm() + " datasource",
+                e.getTargetException());
+        } catch (InterruptedException ignore) {
+
+        }
+    }
+
+    private void openEntitiesFrom(DBRProgressMonitor monitor, DBSObjectContainer container) throws DBException {
+        for (DBSObject child : container.getChildren(monitor)) {
+            if (child instanceof DBSEntity) {
+                DBNDatabaseNode node = DBNUtils.getNodeByObject(monitor, child, true);
+                if (node != null) {
+                    UIUtils.asyncExec(() -> {
+                        NavigatorUtils.openNavigatorNode(node, UIUtils.getActiveWorkbenchWindow());
+                    });
+                }
+            } else if (child instanceof DBSObjectContainer oc) {
+                openEntitiesFrom(monitor, oc);
+            }
+        }
+    }
+
+    protected abstract String getDatabaseTerm();
+
+    protected abstract String createDatabaseName(@NotNull List<Path> fileList);
+
+    protected abstract DriverReference getDriverReference();
+
+}
