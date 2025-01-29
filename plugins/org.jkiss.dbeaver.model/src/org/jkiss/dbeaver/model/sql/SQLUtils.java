3d88aa7734aa7462484eb01456c279b36e38e1bc
Elizabeth <e1izabeth.k@outlook.com>
2025-01-29 13:08:47 +0300

    Revert "dbeaver/pro#3140 Move check for single expression in parenthesis to utils for reuse"

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/sql/SQLUtils.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/sql/SQLUtils.java
@@ -16,8 +16,6 @@
  */
 package org.jkiss.dbeaver.model.sql;
 
-import net.sf.jsqlparser.expression.Expression;
-import net.sf.jsqlparser.expression.operators.relational.ParenthesedExpressionList;
 import org.eclipse.core.runtime.Platform;
 import org.jkiss.code.NotNull;
 import org.jkiss.code.Nullable;
@@ -1176,12 +1174,5 @@ public final class SQLUtils {
         }
         actualIdentifierString = forceUnquotted ? unquottedIdentifier : dialect.getQuotedIdentifier(unquottedIdentifier, true, false);
         return actualIdentifierString;
-    }
-
-    /**
-     * Checks if provided expression is of type ParenthesedExpressionList and has only one expression inside
-     */
-    public static boolean isSingleExpressionInParenthesis(@NotNull Expression expr) {
-        return expr instanceof ParenthesedExpressionList<?> parenthesis && parenthesis.size() == 1;
     }
 }
