bba9ceedf40a9aa8b726050c874111f0d07345c9
serge-rider <serge@dbeaver.com>
2025-01-29 09:19:00 +0100

    dbeaver/pro#4035 SQL script tasks

--- plugins/org.jkiss.dbeaver.tasks.ui.view/src/org/jkiss/dbeaver/tasks/ui/view/DatabaseTasksView.java
+++ plugins/org.jkiss.dbeaver.tasks.ui.view/src/org/jkiss/dbeaver/tasks/ui/view/DatabaseTasksView.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2024 DBeaver Corp and others
+ * Copyright (C) 2010-2025 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -355,6 +355,7 @@ public class DatabaseTasksView extends ViewPart implements DBTTaskListener {
                     }
                 }
                 case TASK_EXECUTE -> refresh();
+                case TASK_ACTIVATE -> tasksTree.getViewer().setSelection(new StructuredSelection(task), true);
             }
         });
     }
