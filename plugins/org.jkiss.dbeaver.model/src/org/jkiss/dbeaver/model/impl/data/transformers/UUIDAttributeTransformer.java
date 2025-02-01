7783dcee98b7b2ab87404066155ff3ab178dc227
eusebe-cda <152634664+eusebe-cda@users.noreply.github.com>
2025-02-01 10:40:15 +0100

    dbeaver/dbeaver#35139-sqlite-guid-convesion-to-uuid

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/impl/data/transformers/UUIDAttributeTransformer.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/impl/data/transformers/UUIDAttributeTransformer.java
@@ -85,6 +85,8 @@ public class UUIDAttributeTransformer implements DBDAttributeTransformer {
                 Object cachedValue = ((DBDContentCached) value).getCachedValue();
                 if (cachedValue instanceof byte[]) {
                     bytes = (byte[]) cachedValue;
+                } else if (cachedValue instanceof String) {
+                    bytes = ((String) cachedValue).getBytes();
                 }
             }
             if (bytes != null) {
