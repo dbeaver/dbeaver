437ea930a5a0e3c177734a64bae05f3881bd6aef
ShadelessFox <yanuari.channel@gmail.com>
2025-02-03 13:42:32 +0100

    dbeaver/pro#4042 Fix DND not working in tasks view

--- plugins/org.jkiss.dbeaver.tasks.ui.view/src/org/jkiss/dbeaver/tasks/ui/view/DatabaseTasksView.java
+++ plugins/org.jkiss.dbeaver.tasks.ui.view/src/org/jkiss/dbeaver/tasks/ui/view/DatabaseTasksView.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2024 DBeaver Corp and others
+ * Copyright (C) 2010-2025 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -148,6 +148,8 @@ public class DatabaseTasksView extends ViewPart implements DBTTaskListener {
             }
         });
         tasksTree.getViewer().addSelectionChangedListener(event -> UIUtils.asyncExec(() -> loadTaskRuns(false)));
+
+        DatabaseTasksTree.addDragAndDropSourceSupport(tasksTree.getViewer());
     }
 
     private void createTaskRunTable(Composite parent) {
