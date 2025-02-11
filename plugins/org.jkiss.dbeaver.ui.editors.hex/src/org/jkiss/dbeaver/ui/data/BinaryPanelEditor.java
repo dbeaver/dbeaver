9445da17ac1dc4f1eb073444b6503719f4ef0be0
ShadelessFox <yanuari.channel@gmail.com>
2025-02-11 17:18:44 +0100

    dbeaver/pro#4164 Use monospace font for all content editors

--- plugins/org.jkiss.dbeaver.ui.editors.hex/src/org/jkiss/dbeaver/ui/data/BinaryPanelEditor.java
+++ plugins/org.jkiss.dbeaver.ui.editors.hex/src/org/jkiss/dbeaver/ui/data/BinaryPanelEditor.java
@@ -156,4 +156,5 @@ public class BinaryPanelEditor implements IStreamValueEditor<HexEditControl> {
     public void contributeSettings(@NotNull IContributionManager manager, @NotNull HexEditControl control) throws DBCException {
 
     }
+
 }
