de979f1d9c5117b96f3c82ea5f46e16221c00b87
serge-rider <serge@dbeaver.com>
2025-02-11 15:39:49 +0100

    dbeaver/pro#4150 Timestamp with time zone parser

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/impl/data/formatters/DateTimeDataFormatter.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/impl/data/formatters/DateTimeDataFormatter.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2024 DBeaver Corp and others
+ * Copyright (C) 2010-2025 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -129,7 +129,11 @@ public class DateTimeDataFormatter implements DBDDataFormatter {
                 }
             }
         }
-        return dateFormat.parse(value);
+        try {
+            return OffsetDateTime.parse(value, dateTimeFormatter);
+        } catch (Exception e) {
+            return dateFormat.parse(value);
+        }
     }
 
 }
