ea681c44432a1fe0d895926674ee4a984185afd7
serge-rider <serge@dbeaver.com>
2025-02-12 17:04:57 +0100

    dbeaver.pro#4152 UI deadlock fix

--- plugins/org.jkiss.dbeaver.model.rcp/src/org/jkiss/dbeaver/model/rcp/DesktopWorkspaceImpl.java
+++ plugins/org.jkiss.dbeaver.model.rcp/src/org/jkiss/dbeaver/model/rcp/DesktopWorkspaceImpl.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2024 DBeaver Corp and others
+ * Copyright (C) 2010-2025 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -274,12 +274,14 @@ public class DesktopWorkspaceImpl extends EclipseWorkspaceImpl implements DBPWor
                         if (project.exists()) {
                             continue;
                         }
+/*
                         try {
                             monitor.subTask("Adding project '" + projectName + "'");
                             project.create(monitor.getNestedMonitor());
                         } catch (CoreException e) {
                             log.error("Error adding project '" + projectName + "' to workspace");
                         }
+*/
                     }
                 }
             }
