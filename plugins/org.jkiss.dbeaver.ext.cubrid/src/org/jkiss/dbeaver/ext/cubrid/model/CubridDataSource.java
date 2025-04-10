0c28a0ecf7cf4e901d5e475d3db8ce07fbfe1f2f
Longha Seng <senglongha.tech@gmail.com>
2025-04-10 16:03:57 +0700

    #37762 Enhance partition functionality

--- plugins/org.jkiss.dbeaver.ext.cubrid/src/org/jkiss/dbeaver/ext/cubrid/model/CubridDataSource.java
+++ plugins/org.jkiss.dbeaver.ext.cubrid/src/org/jkiss/dbeaver/ext/cubrid/model/CubridDataSource.java
@@ -46,6 +46,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 
@@ -256,6 +257,12 @@ public class CubridDataSource extends GenericDataSource
     @Override
     public boolean splitProceduresAndFunctions() {
         return true;
+    }
+
+    @Nullable
+    @Override
+    public GenericSchema getSchema(String name) {
+        return super.getSchema(name == null ? null : name.toUpperCase(Locale.ENGLISH));
     }
 
     private void setTracking(@NotNull DBRProgressMonitor monitor) throws DBException {
