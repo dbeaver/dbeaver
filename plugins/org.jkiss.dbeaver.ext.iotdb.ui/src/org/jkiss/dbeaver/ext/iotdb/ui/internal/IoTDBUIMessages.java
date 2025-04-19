1eea8f252fcc4c70447c2cf0d6d563d7ea70b560
LimJiaWenBrenda <limjiawenbrenda221@gmail.com>
2025-04-19 18:14:11 +0800

    Issues solved 5/5: extended GenericConnectionPage + removed redundant messages

--- plugins/org.jkiss.dbeaver.ext.iotdb.ui/src/org/jkiss/dbeaver/ext/iotdb/ui/internal/IoTDBUIMessages.java
+++ plugins/org.jkiss.dbeaver.ext.iotdb.ui/src/org/jkiss/dbeaver/ext/iotdb/ui/internal/IoTDBUIMessages.java
@@ -26,27 +26,6 @@ public class IoTDBUIMessages extends NLS {
         NLS.initializeMessages(BUNDLE_NAME, IoTDBUIMessages.class);
     }
 
-    public static String dialog_connection_advanced_tab;
-    public static String dialog_connection_advanced_tab_tooltip;
-    public static String dialog_connection_browse_button;
-    public static String dialog_connection_browse_button_tip;
-    public static String dialog_connection_create_button;
-    public static String dialog_connection_create_button_tip;
-    public static String dialog_connection_database_schema_label;
-    public static String dialog_connection_db_file_chooser_text;
-    public static String dialog_connection_db_folder_chooser_message;
-    public static String dialog_connection_db_folder_chooser_text;
-    public static String dialog_connection_general_tab;
-    public static String dialog_connection_general_tab_tooltip;
-    public static String dialog_connection_host_label;
-    public static String dialog_connection_jdbc_url_;
-    public static String dialog_connection_password_label;
-    public static String dialog_connection_path_label;
-    public static String dialog_connection_port_label;
-    public static String dialog_connection_server_label;
-    public static String dialog_connection_test_connection_button;
-    public static String dialog_connection_user_name_label;
-
     public static String controls_privilege_table_column_privilege_grant;
     public static String controls_privilege_table_column_privilege_grant_tip;
     public static String controls_privilege_table_column_privilege_name;
@@ -55,10 +34,6 @@ public class IoTDBUIMessages extends NLS {
     public static String controls_privilege_table_column_privilege_status_tip;
     public static String controls_privilege_table_push_button_check_all;
     public static String controls_privilege_table_push_button_clear_all;
-    public static String edit_command_grant_privilege_action_grant_privilege;
-    public static String edit_command_grant_privilege_action_revoke_privilege;
-    public static String edit_command_grant_privilege_name_grant_privilege;
-    public static String edit_command_grant_privilege_name_revoke_privilege;
     public static String editors_user_editor_abstract_load_grants;
     public static String editors_user_editor_general_control_dba_privileges;
     public static String editors_user_editor_general_group_login;
