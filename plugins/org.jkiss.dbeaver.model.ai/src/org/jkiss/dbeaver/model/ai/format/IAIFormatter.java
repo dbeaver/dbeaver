7d686f48a4fe39f7077299c089a95d102c04a627
Ivan Gagarkin <gagarkin.iiu@gmail.com>
2025-04-27 21:42:59 +0200

    dbeaver/pro#5487 Send sample table data to AI if preference is enabled

--- plugins/org.jkiss.dbeaver.model.ai/src/org/jkiss/dbeaver/model/ai/format/IAIFormatter.java
+++ plugins/org.jkiss.dbeaver.model.ai/src/org/jkiss/dbeaver/model/ai/format/IAIFormatter.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2024 DBeaver Corp and others
+ * Copyright (C) 2010-2025 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -58,5 +58,12 @@ public interface IAIFormatter {
         @NotNull DBSEntityAttribute attribute,
         @NotNull DBRProgressMonitor monitor
     );
+
+    void addDataSample(
+        @NotNull DBRProgressMonitor monitor,
+        @NotNull DBSEntity object,
+        @NotNull DBCExecutionContext executionContext,
+        @NotNull StringBuilder description
+    ) throws DBException;
 }
 
