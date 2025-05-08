b95c450038db0a5b5211c4998ef800e2a6323fa4
Georgii Gvinepadze <georgii.gvinepadze@dbeaver.com>
2025-05-08 13:16:44 +0400

    dbeaver/pro#4020 stage for stats

--- plugins/org.jkiss.dbeaver.ui.statistics/src/org/jkiss/dbeaver/ui/statistics/StatisticsTransmitter.java
+++ plugins/org.jkiss.dbeaver.ui.statistics/src/org/jkiss/dbeaver/ui/statistics/StatisticsTransmitter.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2024 DBeaver Corp and others
+ * Copyright (C) 2010-2025 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -56,7 +56,7 @@ public class StatisticsTransmitter {
         this.workspaceId = workspaceId;
 
         if (System.getenv("LM_STAGE_MODE") != null) {
-            endpoint = "https://stats.stage.dbeaver.infra/send-statistics";
+            endpoint = "http://stats.stage.dbeaver.infra/send-statistics";
         } else {
             endpoint = "https://stats.dbeaver.com/send-statistics";
         }
