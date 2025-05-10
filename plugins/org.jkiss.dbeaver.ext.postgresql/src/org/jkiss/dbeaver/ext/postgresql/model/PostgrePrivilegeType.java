29c59c50c4c7e961a31f7680bcfb21b96467afd3
publicvoid <aglicheev.ao@gmail.com>
2025-05-10 20:54:59 +0300

    #37882 add maintain privilege

--- plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/model/PostgrePrivilegeType.java
+++ plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/model/PostgrePrivilegeType.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2024 DBeaver Corp and others
+ * Copyright (C) 2010-2025 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -40,6 +40,7 @@ public enum PostgrePrivilegeType implements DBAPrivilegeType {
     TEMPORARY('T', true, PostgreDatabase.class),
     EXECUTE('X', true, PostgreProcedure.class),
     USAGE('U', true, PostgreSequence.class, PostgreDataType.class, PostgreSchema.class),
+    MAINTAIN('m', true, PostgreTable.class, PostgreMaterializedView.class),
 
     // Redshift-specific types
     RULE('R', true, PostgreTableReal.class),
