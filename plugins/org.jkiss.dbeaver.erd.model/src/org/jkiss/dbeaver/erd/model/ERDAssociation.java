293255e223692e4e1c514e3171f8b65731a91808
serge-rider <serge@dbeaver.com>
2025-05-21 07:12:56 +0200

    dbeaver/pro#4336 Remove auto-detect of associations on diagram edit

--- plugins/org.jkiss.dbeaver.erd.model/src/org/jkiss/dbeaver/erd/model/ERDAssociation.java
+++ plugins/org.jkiss.dbeaver.erd.model/src/org/jkiss/dbeaver/erd/model/ERDAssociation.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2024 DBeaver Corp and others
+ * Copyright (C) 2010-2025 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -157,7 +157,12 @@ public class ERDAssociation extends ERDObject<DBSEntityAssociation> {
 		this.sourceEntity = sourceForeignKey;
 	}
 
-	@NotNull
+    protected void resetConnections() {
+        this.sourceAttributes = null;
+        this.targetAttributes = null;
+    }
+
+    @NotNull
     public List<ERDEntityAttribute> getSourceAttributes() {
         return sourceAttributes == null ? Collections.emptyList() : sourceAttributes;
     }
