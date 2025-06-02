25fdb671d2939ea356cb3b813a48b67a2548224e
Elizabeth <28875055+E1izabeth@users.noreply.github.com>
2025-06-02 23:10:21 +0300

    dbeaver/pro#5891 Fix command execution for AI suggestion handler

--- plugins/org.jkiss.dbeaver.ui.ai/src/org/jkiss/dbeaver/ui/ai/internal/AITranslateHandler.java
+++ plugins/org.jkiss.dbeaver.ui.ai/src/org/jkiss/dbeaver/ui/ai/internal/AITranslateHandler.java
@@ -28,10 +28,10 @@ public class AITranslateHandler extends AbstractHandler {
 
     @Override
     public Object execute(ExecutionEvent event) {
-        Command command = ActionUtils.findCommand(SQLEditorCommands.CMD_AI_CHAT_TOGGLE);
+        Command command = ActionUtils.findCommand(SQLEditorCommands.CMD_AI_SUGGESTION);
         if (command != null && command.isEnabled() && command.getHandler() != null) {
             // PRO products, sorry
-            ActionUtils.runCommand(SQLEditorCommands.CMD_AI_CHAT_TOGGLE, HandlerUtil.getActiveWorkbenchWindow(event));
+            ActionUtils.runCommand(SQLEditorCommands.CMD_AI_SUGGESTION, HandlerUtil.getActiveWorkbenchWindow(event));
             return null;
         }
 
