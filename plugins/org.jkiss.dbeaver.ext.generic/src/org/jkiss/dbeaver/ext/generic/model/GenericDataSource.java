f82bacb404a988a08bb82dda41d664bcfccd98dd
Elizabeth <e1izabeth.k@outlook.com>
2025-06-06 12:46:09 +0300

    #38316 Remove error logging if driver fails to read types

--- plugins/org.jkiss.dbeaver.ext.generic/src/org/jkiss/dbeaver/ext/generic/model/GenericDataSource.java
+++ plugins/org.jkiss.dbeaver.ext.generic/src/org/jkiss/dbeaver/ext/generic/model/GenericDataSource.java
@@ -472,7 +472,7 @@ public class GenericDataSource extends JDBCDataSource implements DBPTermProvider
             try {
                 dataTypeCache.getAllObjects(monitor, this);
             } catch (Exception e) {
-                log.error("Can't fetch database data types: " + e.getMessage(), e);
+                log.debug("Can't fetch database data types: " + e.getMessage());
             }
             if (CommonUtils.isEmpty(dataTypeCache.getCachedObjects())) {
                 // Use basic data types
