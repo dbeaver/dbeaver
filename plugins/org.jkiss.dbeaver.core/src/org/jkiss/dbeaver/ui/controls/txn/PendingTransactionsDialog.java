569390ac8ac2e65df93d9b3f5377180925043a13
serge-rider <serge@dbeaver.com>
2025-06-15 07:49:24 +0200

    dbeaver/pro#5983 Code style, dead code remove

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/controls/txn/PendingTransactionsDialog.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/controls/txn/PendingTransactionsDialog.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2024 DBeaver Corp and others
+ * Copyright (C) 2010-2025 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,6 +43,7 @@ import java.util.List;
 public class PendingTransactionsDialog extends TransactionInfoDialog {
 
     private static final String DIALOG_ID = "DBeaver.PendingTransactionsDialog";//$NON-NLS-1$
+
     private Tree contextTree;
     private DBCExecutionContext selectedContext;
     private Button commitButton;
