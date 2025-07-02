c91b5327b05d7b7806116342d508515be898fa7d
Serge Rider <serge@dbeaver.com>
2025-07-02 07:33:06 +0300

    dbeaver/pro#4137 Rename ERD bundles

---
+++ plugins/org.jkiss.dbeaver.ui.editors.erd/src/org/jkiss/dbeaver/ui/editors/erd/model/ERDDecorator.java
@@ -0,0 +1,60 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2025 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jkiss.dbeaver.ui.editors.erd.model;
+
+import org.eclipse.draw2d.geometry.Dimension;
+import org.eclipse.draw2d.geometry.Insets;
+import org.eclipse.gef.EditPartFactory;
+import org.eclipse.gef.palette.PaletteRoot;
+import org.jkiss.code.NotNull;
+import org.jkiss.code.Nullable;
+import org.jkiss.dbeaver.ui.editors.erd.editor.ERDViewStyle;
+
+/**
+ * ERD object adapter
+ */
+public interface ERDDecorator {
+
+    boolean showCheckboxes();
+
+    /**
+     * Checks whether the given {@code style} is supported by the decorator.
+     */
+    boolean supportsAttributeStyle(@NotNull ERDViewStyle style);
+
+    boolean supportsAttributeVisibility();
+
+    boolean supportsStructureEdit();
+
+    /**
+     * Margin around entity figure. This affects diagram connections layout
+     */
+    @NotNull
+    Insets getDefaultEntityInsets();
+
+    /**
+     * Snap size for entity figures. Rounds entity position to this snap size.
+     */
+    @Nullable
+    Dimension getEntitySnapSize();
+
+    @NotNull
+    EditPartFactory createPartFactory();
+
+    void fillPalette(@NotNull PaletteRoot paletteRoot, boolean readOnly);
+
+}
