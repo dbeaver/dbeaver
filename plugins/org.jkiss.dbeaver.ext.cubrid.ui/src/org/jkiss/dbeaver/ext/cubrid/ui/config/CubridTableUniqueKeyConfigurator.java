bf33f3f7e93a08c5411a72110067c62730054c2d
Longha Seng <senglongha.tech@gmail.com>
2025-08-20 15:14:17 +0700

    Fix primary key window not popup

--- plugins/org.jkiss.dbeaver.ext.cubrid.ui/src/org/jkiss/dbeaver/ext/cubrid/ui/config/CubridTableUniqueKeyConfigurator.java
+++ plugins/org.jkiss.dbeaver.ext.cubrid.ui/src/org/jkiss/dbeaver/ext/cubrid/ui/config/CubridTableUniqueKeyConfigurator.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jkiss.dbeaver.ext.cubrid.ui.views;
+package org.jkiss.dbeaver.ext.cubrid.ui.config;
 
 import java.lang.reflect.Field;
 import java.util.Map;
