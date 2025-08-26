0b8a3c683f4a9347211d13cafa39de719d99c6f7
Dmitrii Barnukov <dmitrii.barnukov@dbeaver.com>
2025-08-26 15:40:15 +0200

    Update plugins/org.jkiss.dbeaver.model.ai/src/org/jkiss/dbeaver/model/ai/impl/AIAssistantImpl.java
    Co-authored-by: ShadelessFox <35821147+ShadelessFox@users.noreply.github.com>

--- plugins/org.jkiss.dbeaver.model.ai/src/org/jkiss/dbeaver/model/ai/impl/AIAssistantImpl.java
+++ plugins/org.jkiss.dbeaver.model.ai/src/org/jkiss/dbeaver/model/ai/impl/AIAssistantImpl.java
@@ -172,9 +172,9 @@ public class AIAssistantImpl implements AIAssistant {
         );
 
         AIEngineResponse completionResponse = requestCompletion(engine, monitor, completionRequest);
-        String responseVariant = completionResponse.variants().stream().findFirst().orElseThrow(
-            () -> new DBException("Empty AI response when executing command")
-        );
+        String responseVariant = completionResponse.variants().stream()
+            .findFirst()
+            .orElseThrow(() -> new DBException("Empty AI response when executing command"));
         MessageChunk[] messageChunks = processAndSplitCompletion(
             monitor,
             request.context(),
