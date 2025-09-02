53c84e96d8fe4f96c4e1a9978e65cbae62d116c0
Tsimafei Labanovich <timofeilobanovich@gmail.com>
2025-09-02 13:14:50 +0200

    dbeaver/pro#6777 Enhance combo box value setting logic with type-specific handling and null checks

--- plugins/org.jkiss.dbeaver.data.transfer.ui/src/org/jkiss/dbeaver/tools/transfer/ui/pages/database/DatabaseConsumerPageMapping.java
+++ plugins/org.jkiss.dbeaver.data.transfer.ui/src/org/jkiss/dbeaver/tools/transfer/ui/pages/database/DatabaseConsumerPageMapping.java
@@ -893,8 +893,19 @@ public class DatabaseConsumerPageMapping extends DataTransferPageNodeSettings {
 
             @Override
             protected void doSetValue(Object value) {
-                if (combo != null && !combo.isDisposed()) {
-                    combo.setText(CommonUtils.toString(value));
+                if (combo == null || combo.isDisposed()) {
+                    return;
+                }
+                if (value == null) {
+                    combo.setText("");
+                } else {
+                    if (value instanceof DBPNamedObject dbpNamedObject) {
+                        combo.setText(dbpNamedObject.getName());
+                    } else if (value instanceof Enum anEnum) {
+                        combo.setText(anEnum.name());
+                    } else {
+                        combo.setText(CommonUtils.toString(value));
+                    }
                 }
             }
         };
