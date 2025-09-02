0a0413df3ec576802b92e266cd20348d63d4f140
Tsimafei Labanovich <timofeilobanovich@gmail.com>
2025-09-02 12:43:58 +0200

    dbeaver/dbeaver#38034 Add isPGObject method to Kingbase integration and refactor Postgre interval value handling

--- plugins/org.jkiss.dbeaver.ext.kingbase/src/org/jkiss/dbeaver/ext/kingbase/model/PostgreServerKingbase.java
+++ plugins/org.jkiss.dbeaver.ext.kingbase/src/org/jkiss/dbeaver/ext/kingbase/model/PostgreServerKingbase.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2024 DBeaver Corp and others
+ * Copyright (C) 2010-2025 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +17,8 @@
 
 package org.jkiss.dbeaver.ext.kingbase.model;
 
+import org.jkiss.code.NotNull;
+import org.jkiss.dbeaver.ext.kingbase.KingbaseConstants;
 import org.jkiss.dbeaver.ext.postgresql.model.PostgreDataSource;
 import org.jkiss.dbeaver.ext.postgresql.model.PostgreDatabase;
 import org.jkiss.dbeaver.ext.postgresql.model.impls.PostgreServerExtensionBase;
@@ -61,5 +63,12 @@ public class PostgreServerKingbase extends PostgreServerExtensionBase {
     @Override
     public PostgreDatabase.SchemaCache createSchemaCache(PostgreDatabase database) {
         return new KingbaseSchemaCache();
+    }
+
+    @Override
+    public boolean isPGObject(@NotNull Object object) {
+        String className = object.getClass().getName();
+        return className.equals(KingbaseConstants.KB_OBJECT_CLASS)
+            || className.equals(KingbaseConstants.KB_INTERVAL_CLASS);
     }
 }
