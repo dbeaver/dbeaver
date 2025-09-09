94fa006352604f8528cffafaf0981508a1f871ce
Elizabeth <e1izabeth.k@outlook.com>
2025-09-09 18:41:21 +0300

    dbeaver/pro#6422 Fix empty result set on query execution for ai chat with opened editor

--- plugins/org.jkiss.dbeaver.ui.editors.sql/src/org/jkiss/dbeaver/ui/editors/sql/SQLEditor.java
+++ plugins/org.jkiss.dbeaver.ui.editors.sql/src/org/jkiss/dbeaver/ui/editors/sql/SQLEditor.java
@@ -3473,6 +3473,14 @@ public class SQLEditor extends SQLEditorBase implements
         }
     }
 
+    public boolean hasOnlyEmptyResultViews() {
+        return this.queryProcessors.isEmpty() || (
+            this.queryProcessors.stream().allMatch(qp -> qp.resultContainers.isEmpty() || (
+                qp.resultContainers.stream().allMatch(rc -> !rc.viewer.hasData())
+            ))
+        );
+    }
+
     private boolean isContextChanged(DBPEvent event) {
         DBPEvent.Action eventAction = event.getAction();
         DBSObject eventObject = event.getObject();
