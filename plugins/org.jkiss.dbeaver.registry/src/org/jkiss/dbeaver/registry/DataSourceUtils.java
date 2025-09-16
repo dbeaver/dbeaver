b5a23011814d3fce77ef2e611a6166ebd7aef108
Viktor Kirst <viktor.kirst@dbeaver.com>
2025-09-16 16:34:09 +0400

    dbeaver/dbeaver#38696 Support advanced properties via 'advProp.' prefix

--- plugins/org.jkiss.dbeaver.registry/src/org/jkiss/dbeaver/registry/DataSourceUtils.java
+++ plugins/org.jkiss.dbeaver.registry/src/org/jkiss/dbeaver/registry/DataSourceUtils.java
@@ -70,6 +70,7 @@ public class DataSourceUtils {
     private static final String PREFIX_HANDLER = "handler.";
     private static final String PREFIX_PROP = "prop.";
     private static final String PREFIX_AUTH_PROP = "authProp.";
+    private static final String PREFIX_AVANCED_PROP = "advProp.";
 
     private static final Log log = Log.getLog(DataSourceUtils.class);
 
@@ -205,6 +206,10 @@ public class DataSourceUtils {
                         paramName = paramName.substring(divPos + 1);
                         Map<String, String> handlerPopMap = handlerProps.computeIfAbsent(handlerId, k -> new HashMap<>());
                         handlerPopMap.put(paramName, paramValue);
+                        handled = true;
+                    } else if (paramName.length() > PREFIX_AVANCED_PROP.length() && paramName.startsWith(PREFIX_AVANCED_PROP)) {
+                        paramName = paramName.substring(PREFIX_AVANCED_PROP.length());
+                        DBWorkbench.getPlatform().getPreferenceStore().setValue(paramName, paramValue);
                         handled = true;
                     } else if (parameterHandler != null) {
                         handled = parameterHandler.setParameter(paramName, paramValue);
