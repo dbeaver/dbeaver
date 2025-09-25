280ef6a9ebf5f3bf554743256c4262dfc97664b3
serge-rider <serge@dbeaver.com>
2025-09-25 05:33:06 +0200

    dbeaver/pro#6802 Internal DB init logging fix

--- plugins/org.jkiss.dbeaver.model.sql.jdbc/src/org/jkiss/dbeaver/model/sql/schema/ClassLoaderScriptSource.java
+++ plugins/org.jkiss.dbeaver.model.sql.jdbc/src/org/jkiss/dbeaver/model/sql/schema/ClassLoaderScriptSource.java
@@ -60,7 +60,7 @@ public class ClassLoaderScriptSource implements SQLSchemaScriptSource {
         if (resource == null) {
             throw new IOException("Resource '" + createScriptPath + "' not found in " + this.classLoader.getClass().getName());
         }
-        log.info("Reading migration file: '" + createScriptPath + "'");
+        log.debug("Reading migration file: '" + createScriptPath + "'");
         return new InputStreamReader(resource);
     }
 
@@ -75,14 +75,14 @@ public class ClassLoaderScriptSource implements SQLSchemaScriptSource {
         String migrationFileNameWithSpecificPrefix = updateScriptPrefix + versionNumber + "_" + specificPrefix + ".sql";
         InputStream resource = classLoader.getResourceAsStream(migrationFileNameWithSpecificPrefix);
         if (resource != null) {
-            log.info("Reading migration file: '" + migrationFileNameWithSpecificPrefix + "'");
+            log.debug("Reading migration file: '" + migrationFileNameWithSpecificPrefix + "'");
             return new InputStreamReader(resource);
         }
 
         String migrationFileName = updateScriptPrefix + versionNumber + ".sql";
         resource = classLoader.getResourceAsStream(migrationFileName);
         if (resource != null) {
-            log.info("Reading migration file: '" + migrationFileName + "'");
+            log.debug("Reading migration file: '" + migrationFileName + "'");
             return new InputStreamReader(resource);
         }
         return null;
