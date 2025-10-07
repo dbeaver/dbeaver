c83b81b9a1a1f16347d88371c9b520c984d7250b
Solovev Anton <solovev.anton93@gmail.com>
2025-10-07 11:18:24 +0200

    dbeaver/dbeaver#38331: add tests

--- test/org.jkiss.dbeaver.model.navigator.test/src/org/jkiss/dbeaver/model/navigator/DBNUtilsTest.java
+++ test/org.jkiss.dbeaver.model.navigator.test/src/org/jkiss/dbeaver/model/navigator/DBNUtilsTest.java
@@ -16,13 +16,143 @@
  */
 package org.jkiss.dbeaver.model.navigator;
 
+import org.jkiss.dbeaver.ModelPreferences;
+import org.jkiss.dbeaver.runtime.DBWorkbench;
 import org.jkiss.junit.DBeaverUnitTest;
+import org.junit.After;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class DBNUtilsTest extends DBeaverUnitTest {
 
-    @Test
-    public void test(){
+    private final List<String> changedProperties = new ArrayList<>();
 
+    @After
+    public void tearDown() throws Exception {
+        var prefStore = DBWorkbench.getPlatform().getPreferenceStore();
+        changedProperties.forEach(prefStore::setToDefault);
+        changedProperties.clear();
+    }
+
+
+    @Test
+    public void sortChildrenWithSortAlphabeticallyAndNoFoldersFirstPropertyShouldIgnoreCase() {
+        // given
+        addProperty(ModelPreferences.NAVIGATOR_SORT_ALPHABETICALLY, true);
+        addProperty(ModelPreferences.NAVIGATOR_SORT_FOLDERS_FIRST, false);
+        List<String> givenNames = List.of("b", "a", "A", "C");
+        List<String> expectedNames = List.of("a", "A", "b", "C");
+        // when
+        var result = DBNUtils.filterNavigableChildren(getNamedNodes(givenNames), true);
+        // then
+        assertEquals(expectedNames, Arrays.stream(result).map(DBNNode::getNodeDisplayName).toList());
+    }
+
+    @Test
+    public void sortChildrenWithSortAlphabeticallyAndNoFoldersFirstPropertyShouldIgnoreCaseNumbersCase() {
+        // given
+        addProperty(ModelPreferences.NAVIGATOR_SORT_ALPHABETICALLY, true);
+        addProperty(ModelPreferences.NAVIGATOR_SORT_FOLDERS_FIRST, false);
+        List<String> givenNames = List.of("s2", "s1", "s10", "s03");
+        List<String> expectedNames = List.of("s1", "s2", "s03", "s10");
+        // when
+        var result = DBNUtils.filterNavigableChildren(getNamedNodes(givenNames), true);
+        // then
+        assertEquals(expectedNames, Arrays.stream(result).map(DBNNode::getNodeDisplayName).toList());
+    }
+
+    @Test
+    public void sortChildrenWithSortAlphabeticallyAndNoFoldersFirstPropertyShouldIgnoreCaseBigNumbersCase() {
+        // given
+        addProperty(ModelPreferences.NAVIGATOR_SORT_ALPHABETICALLY, true);
+        addProperty(ModelPreferences.NAVIGATOR_SORT_FOLDERS_FIRST, false);
+        List<String> givenNames = List.of("s2123456789123456789", "s1123456789123456789");
+        List<String> expectedNames = List.of("s1123456789123456789", "s2123456789123456789");
+        // when
+        var result = DBNUtils.filterNavigableChildren(getNamedNodes(givenNames), true);
+        // then
+        assertEquals(expectedNames, Arrays.stream(result).map(DBNNode::getNodeDisplayName).toList());
+    }
+
+
+    @Test
+    public void sortChildrenWithSortAlphabeticallyAndWithFoldersFirstPropertyShouldIgnoreCase() {
+        // given
+        addProperty(ModelPreferences.NAVIGATOR_SORT_ALPHABETICALLY, true);
+        addProperty(ModelPreferences.NAVIGATOR_SORT_FOLDERS_FIRST, true);
+        List<String> givenNames = List.of("b", "a", "A", "C");
+        List<String> expectedNames = List.of("a", "A", "b", "C");
+        // when
+        var result = DBNUtils.filterNavigableChildren(getNamedNodes(givenNames), true);
+        // then
+        assertEquals(expectedNames, Arrays.stream(result).map(DBNNode::getNodeDisplayName).toList());
+    }
+
+    @Test
+    public void sortChildrenWithSortAlphabeticallyAndWithFoldersFirstPropertyShouldIgnoreCaseNumbersCase() {
+        // given
+        addProperty(ModelPreferences.NAVIGATOR_SORT_ALPHABETICALLY, true);
+        addProperty(ModelPreferences.NAVIGATOR_SORT_FOLDERS_FIRST, true);
+        List<String> givenNames = List.of("s2", "s1", "s10", "s03");
+        List<String> expectedNames = List.of("s1", "s2", "s03", "s10");
+        // when
+        var result = DBNUtils.filterNavigableChildren(getNamedNodes(givenNames), true);
+        // then
+        assertEquals(expectedNames, Arrays.stream(result).map(DBNNode::getNodeDisplayName).toList());
+    }
+
+    @Test
+    public void sortChildrenWithSortAlphabeticallyAndWithFoldersFirstPropertyShouldIgnoreCaseBigNumbersCase() {
+        // given
+        addProperty(ModelPreferences.NAVIGATOR_SORT_ALPHABETICALLY, true);
+        addProperty(ModelPreferences.NAVIGATOR_SORT_FOLDERS_FIRST, true);
+        List<String> givenNames = List.of("s2123456789123456789", "s1123456789123456789");
+        List<String> expectedNames = List.of("s1123456789123456789", "s2123456789123456789");
+        // when
+        var result = DBNUtils.filterNavigableChildren(getNamedNodes(givenNames), true);
+        // then
+        assertEquals(expectedNames, Arrays.stream(result).map(DBNNode::getNodeDisplayName).toList());
+    }
+
+    @Test
+    public void sortChildrenWithSortAlphabeticallyAndWithFoldersFirstPropertyShouldNotBeSorted() {
+        // given
+        addProperty(ModelPreferences.NAVIGATOR_SORT_ALPHABETICALLY, false);
+        addProperty(ModelPreferences.NAVIGATOR_SORT_FOLDERS_FIRST, true);
+        List<String> givenNames = List.of("b", "a", "A", "C");
+        List<String> expectedNames = List.of("b", "a", "A", "C");
+        // when
+        var result = DBNUtils.filterNavigableChildren(getNamedNodes(givenNames), true);
+        // then
+        assertEquals(expectedNames, Arrays.stream(result).map(DBNNode::getNodeDisplayName).toList());
+    }
+
+
+    private void addProperty(String key, boolean value) {
+        var prefStore = DBWorkbench.getPlatform().getPreferenceStore();
+        if (!prefStore.contains(key)) {
+            throw new IllegalArgumentException("No such property: " + key);
+        }
+        changedProperties.add(key);
+        prefStore.setValue(key, value);
+    }
+
+    private DBNNode[] getNamedNodes(List<String> names) {
+        return names
+            .stream()
+            .map(this::createMockNamedNode)
+            .toArray(DBNNode[]::new);
+    }
+
+    private DBNNode createMockNamedNode(String name) {
+        DBNNode node = mock(DBNNode.class, RETURNS_DEEP_STUBS);
+        when(node.getNodeDisplayName()).thenReturn(name);
+        return node;
     }
 }
