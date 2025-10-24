c240af06ba2f970f29c282628445de7de79b2a76
Tsimafei Labanovich <timofeilobanovich@gmail.com>
2025-10-24 03:04:40 +0200

    dbeaver/pro#6682 Fix i18n

--- plugins/org.jkiss.dbeaver.ui.ai/src/org/jkiss/dbeaver/ui/ai/format/DefaultFormattingConfigurator.java
+++ plugins/org.jkiss.dbeaver.ui.ai/src/org/jkiss/dbeaver/ui/ai/format/DefaultFormattingConfigurator.java
@@ -121,23 +121,23 @@ public class DefaultFormattingConfigurator implements IObjectPropertyConfigurato
     }
 
     protected void createQueryExecutionSettings(@NotNull Composite chatSettingsGroup) {
-        confirmSQLCombo = createConfirmQueryCombo(chatSettingsGroup, "Select", "Confirm regular SQL select queries");
+        confirmSQLCombo = createConfirmQueryCombo(chatSettingsGroup, AIUIMessages.gpt_preference_page_ai_query_confirm_sql_label, AIUIMessages.gpt_preference_page_ai_query_confirm_sql_tip);
         confirmSQLCombo.add(AIUIMessages.gpt_preference_page_ai_query_confirm_rule_execute);
         confirmSQLCombo.add(AIUIMessages.gpt_preference_page_ai_query_confirm_rule_confirm);
         confirmSQLCombo.select(0);
 
-        confirmDMLCombo = createConfirmQueryCombo(chatSettingsGroup, "Modify", "Confirm INSERT/DELETE/UPDATE queries");
+        confirmDMLCombo = createConfirmQueryCombo(chatSettingsGroup, AIUIMessages.gpt_preference_page_ai_query_confirm_dml_label, AIUIMessages.gpt_preference_page_ai_query_confirm_dml_tip);
         confirmDMLCombo.add(AIUIMessages.gpt_preference_page_ai_query_confirm_rule_execute);
         confirmDMLCombo.add(AIUIMessages.gpt_preference_page_ai_query_confirm_rule_confirm);
         confirmDMLCombo.add(AIUIMessages.gpt_preference_page_ai_query_confirm_rule_disable_autocommit);
         confirmDMLCombo.select(1);
 
-        confirmDDLCombo = createConfirmQueryCombo(chatSettingsGroup, "Schema", "Confirm schema modification queries (CREATE, DROP, ALTER, etc)");
+        confirmDDLCombo = createConfirmQueryCombo(chatSettingsGroup, AIUIMessages.gpt_preference_page_ai_query_confirm_ddl_label, AIUIMessages.gpt_preference_page_ai_query_confirm_ddl_tip);
         confirmDDLCombo.add(AIUIMessages.gpt_preference_page_ai_query_confirm_rule_execute);
         confirmDDLCombo.add(AIUIMessages.gpt_preference_page_ai_query_confirm_rule_confirm);
         confirmDDLCombo.select(1);
 
-        confirmOtherCombo = createConfirmQueryCombo(chatSettingsGroup, "Other", "Confirm unknown query types (including NoSQL)");
+        confirmOtherCombo = createConfirmQueryCombo(chatSettingsGroup, AIUIMessages.gpt_preference_page_ai_query_confirm_other_label, AIUIMessages.gpt_preference_page_ai_query_confirm_other_tip);
         confirmOtherCombo.add(AIUIMessages.gpt_preference_page_ai_query_confirm_rule_execute);
         confirmOtherCombo.add(AIUIMessages.gpt_preference_page_ai_query_confirm_rule_confirm);
         confirmOtherCombo.select(1);
