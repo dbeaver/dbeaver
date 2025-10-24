bd6d87e896c13987192086463bd4de9e4c864251
Tsimafei Labanovich <timofeilobanovich@gmail.com>
2025-10-24 13:08:10 +0200

    dbeaver/pro#6973 Add unit tests

--- test/org.jkiss.dbeaver.test.platform/src/org/jkiss/dbeaver/model/SQLUtilsTest.java
+++ test/org.jkiss.dbeaver.test.platform/src/org/jkiss/dbeaver/model/SQLUtilsTest.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2024 DBeaver Corp and others
+ * Copyright (C) 2010-2025 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -57,5 +57,52 @@ public class SQLUtilsTest extends DBeaverUnitTest {
     public void makeGlobFromSqlLikePattern_whenWithSpecialSymbols_thenSuccess(){
 
         Assert.assertEquals("key?*\\?*\\", SQLUtils.makeGlobFromSqlLikePattern("key_%?*\\"));
+    }
+
+    @Test
+    public void extractTypesOnly_whenNullOrEmpty_thenParens() {
+        Assert.assertEquals("()", SQLUtils.extractTypesOnly(null));
+        Assert.assertEquals("()", SQLUtils.extractTypesOnly(""));
+        Assert.assertEquals("()", SQLUtils.extractTypesOnly("   "));
+        Assert.assertEquals("()", SQLUtils.extractTypesOnly("()"));
+        Assert.assertEquals("()", SQLUtils.extractTypesOnly("(   )"));
+    }
+
+    @Test
+    public void extractTypesOnly_whenNamesPresent_thenRemoved() {
+        Assert.assertEquals("(NUMBER(38,0), VARCHAR)",
+            SQLUtils.extractTypesOnly("(a NUMBER(38,0), b VARCHAR)"));
+        Assert.assertEquals("(ARRAY, OBJECT)",
+            SQLUtils.extractTypesOnly("(x ARRAY, y OBJECT)"));
+    }
+
+    @Test
+    public void extractTypesOnly_whenNestedTypes_thenKeepInnerParens() {
+        Assert.assertEquals("(DECIMAL(10,2), ARRAY(VARCHAR))",
+            SQLUtils.extractTypesOnly("(price DECIMAL(10,2), tags ARRAY(VARCHAR))"));
+        Assert.assertEquals("(ARRAY(OBJECT), VARIANT)",
+            SQLUtils.extractTypesOnly("(p ARRAY(OBJECT), v VARIANT)"));
+    }
+
+    @Test
+    public void extractTypesOnly_whenNoOuterParens_thenSingleType() {
+        Assert.assertEquals("(NUMBER)",
+            SQLUtils.extractTypesOnly("id NUMBER"));
+        Assert.assertEquals("(ARRAY(VARCHAR))",
+            SQLUtils.extractTypesOnly("arr ARRAY(VARCHAR)"));
+    }
+
+    @Test
+    public void extractTypesOnly_whenWhitespaceAndCase_thenCanonicalUpper() {
+        Assert.assertEquals("(NUMBER, VARCHAR)",
+            SQLUtils.extractTypesOnly("(  a   number  ,   b   varchar  )"));
+        Assert.assertEquals("(ARRAY, OBJECT, VARIANT)",
+            SQLUtils.extractTypesOnly("( arr  array , obj   object , v   variant )"));
+    }
+
+    @Test
+    public void extractTypesOnly_whenComplexSignature_thenCorrectSplit() {
+        Assert.assertEquals("(DECIMAL(38,0), ARRAY(DECIMAL(10,2)), OBJECT)",
+            SQLUtils.extractTypesOnly("(a DECIMAL(38,0), b ARRAY(DECIMAL(10,2)), c OBJECT)"));
     }
 }
