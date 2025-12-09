88c6dd9cf7c664ced85886a77f320c22fa8a7c1e
Dmitrii Barnukov <dmitrii.barnukov@dbeaver.com>
2025-12-09 09:43:02 +0100

    dbeaver/pro#7618 updated test datasource setup

--- test/org.jkiss.dbeaver.model.lsp.test/src/org/jkiss/dbeaver/model/lsp/test/DBLTextDocumentServiceDataSourceTest.java
+++ test/org.jkiss.dbeaver.model.lsp.test/src/org/jkiss/dbeaver/model/lsp/test/DBLTextDocumentServiceDataSourceTest.java
@@ -18,41 +18,46 @@ package org.jkiss.dbeaver.model.lsp.test;
 
 import org.eclipse.lsp4j.*;
 import org.jkiss.code.Nullable;
-import org.jkiss.dbeaver.ext.postgresql.model.*;
+import org.jkiss.dbeaver.DBException;
+import org.jkiss.dbeaver.ext.postgresql.model.PostgreDataSource;
+import org.jkiss.dbeaver.ext.postgresql.model.PostgreDataType;
 import org.jkiss.dbeaver.model.DBPDataSourceContainer;
-import org.jkiss.dbeaver.model.app.DBPProject;
+import org.jkiss.dbeaver.model.connection.DBPConnectionConfiguration;
 import org.jkiss.dbeaver.model.lsp.DBLTextDocumentService;
+import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
 import org.jkiss.dbeaver.model.runtime.VoidProgressMonitor;
+import org.jkiss.dbeaver.registry.DataSourceDescriptor;
 import org.jkiss.dbeaver.runtime.DBWorkbench;
 import org.jkiss.junit.DBeaverUnitTest;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 public class DBLTextDocumentServiceDataSourceTest extends DBeaverUnitTest {
+    private final DBRProgressMonitor monitor = new VoidProgressMonitor();
+    private DBPDataSourceContainer dataSourceContainer;
+
     private final DBLTextDocumentService service = new DBLTextDocumentService();
 
-    private PostgreDataSource testDataSource;
-
-    @Mock
-    DBPDataSourceContainer mockDataSourceContainer;
-
     @Before
-    public void setUp() throws Exception {
-        DBPProject project = DBWorkbench.getPlatform().getWorkspace().getProjects().getFirst();
-        Mockito.when(mockDataSourceContainer.getProject()).thenReturn(project);
-        Mockito.when(mockDataSourceContainer.getPreferenceStore()).thenReturn(DBWorkbench.getPlatform().getPreferenceStore());
-        Mockito.when(mockDataSourceContainer.getId()).thenReturn("test_data_source");
-        Mockito.when(mockDataSourceContainer.getDataSource()).thenReturn(testDataSource);
-        Mockito.when(mockDataSourceContainer.getDriver()).thenReturn(DBWorkbench.getPlatform().getDataSourceProviderRegistry().findDriver("postgresql"));
+    public void setUp() throws DBException, NoSuchFieldException, IllegalAccessException {
+        var driver = DBWorkbench.getPlatform().getDataSourceProviderRegistry().findDriver("postgresql");
+        var configuration = new DBPConnectionConfiguration();
+        dataSourceContainer = new DataSourceDescriptor(
+            DBWorkbench.getPlatform().getWorkspace().getActiveProject().getDataSourceRegistry(),
+            DataSourceDescriptor.generateNewId(driver),
+            driver,
+            configuration
+        );
+        dataSourceContainer.setName("Test DB");
+        dataSourceContainer.setTemporary(true);
 
-        testDataSource = new PostgreDataSource(mockDataSourceContainer, "PG Test", "postgres") {
+        var testDataSource = new PostgreDataSource(dataSourceContainer, "PG Test", "postgres") {
             @Override
             public boolean isServerVersionAtLeast(int major, int minor) {
                 return major <= 10;
@@ -65,33 +70,36 @@ public class DBLTextDocumentServiceDataSourceTest extends DBeaverUnitTest {
             }
         };
 
-        PostgreRole testUser = new PostgreRole(null, "tester", "test", true);
-        PostgreDatabase testDatabase = testDataSource.createDatabaseImpl(
-            new VoidProgressMonitor(), "testdb", testUser, null, null, null
-        );
-        PostgreSchema testSchema = new PostgreSchema(testDatabase, "test_schema", testUser);
+        Field dataSourceField = dataSourceContainer.getClass().getDeclaredField("dataSource");
+        dataSourceField.setAccessible(true);
+        dataSourceField.set(dataSourceContainer, testDataSource);
 
-        PostgreTableRegular testTable = new PostgreTableRegular(testSchema);
-        testTable.setName("test_table");
-        testTable.setPartition(false);
-        testTable.setPersisted(true);
+        var project = DBWorkbench.getPlatform().getWorkspace().getProjects().getFirst();
+        project.getDataSourceRegistry().addDataSource(dataSourceContainer);
 
-        PostgreTableColumn column = new PostgreTableColumn(testTable);
-        column.setName("column1");
-        column.setTypeName("int4");
-        column.setOrdinalPosition(1);
-        List<PostgreTableColumn> cachedAttributes = (List<PostgreTableColumn>) testTable.getCachedAttributes();
-        cachedAttributes.add(column);
-
-        PostgreView testView = new PostgreView(testSchema);
-        testView.setName("testView");
-        testView.setPersisted(true);
+//        PostgreRole testUser = new PostgreRole(null, "tester", "test", true);
+//        PostgreDatabase testDatabase = testDataSource.createDatabaseImpl(
+//            monitor, "testdb", testUser, null, null, null
+//        );
+//        PostgreSchema testSchema = new PostgreSchema(testDatabase, "test_schema", testUser);
+//
+//        PostgreTableRegular testTable = new PostgreTableRegular(testSchema);
+//        testTable.setName("test_table");
+//        testTable.setPartition(false);
+//        testTable.setPersisted(true);
+//
+//        PostgreTableColumn column = new PostgreTableColumn(testTable);
+//        column.setName("column1");
+//        column.setTypeName("int4");
+//        column.setOrdinalPosition(1);
+//        List<PostgreTableColumn> cachedAttributes = (List<PostgreTableColumn>) testTable.getCachedAttributes();
+//        cachedAttributes.add(column);
     }
 
     @Test
     public void shouldFormatPostgresSqlQuery() throws ExecutionException, InterruptedException {
         String query = """
-            DO $$ BEGIN CREATE TABLE logs (id serial PRIMARY KEY,message text,created_at timestamptz DEFAULT now());ELSE RAISE NOTICE 'Table "logs" already exists.';END IF;END $$;
+            INSERT INTO users (id, profile) VALUES (1,'{"name": "JohnDoe"}'::jsonb) ON CONFLICT (id) DO UPDATE SET profile = users.profile || EXCLUDED.profile RETURNING id, profile->>'name' AS name;
             """.trim();
         TextDocumentItem textDocument = new TextDocumentItem();
         textDocument.setText(query);
@@ -100,23 +108,25 @@ public class DBLTextDocumentServiceDataSourceTest extends DBeaverUnitTest {
         DocumentFormattingParams formattingParams = new DocumentFormattingParams();
         formattingParams.setTextDocument(new TextDocumentIdentifier(DocumentServiceUtils.BASIC_SQL_URI));
         FormattingOptions formattingOptions = new FormattingOptions();
-        DBPDataSourceContainer container = testDataSource.getContainer();
-        formattingOptions.putString(DBLTextDocumentService.PROJECT_ID_OPTION, container.getProject().getId());
-        formattingOptions.putString(DBLTextDocumentService.DATA_SOURCE_ID_OPTION, container.getId());
+        formattingOptions.putString(DBLTextDocumentService.PROJECT_ID_OPTION, dataSourceContainer.getProject().getId());
+        formattingOptions.putString(DBLTextDocumentService.DATA_SOURCE_ID_OPTION, dataSourceContainer.getId());
         formattingParams.setOptions(formattingOptions);
 
         CompletableFuture<List<? extends TextEdit>> future = service.formatting(formattingParams);
 
         TextEdit textEdit = future.get().getFirst();
         String expectedQuery = """
-            DO $$
-            BEGIN
-                CREATE TABLE logs (id serial PRIMARY KEY,
-                message text,
-                created_at timestamptz DEFAULT now());
-            ELSE RAISE NOTICE 'Table "logs" already exists.';
-            END IF;
-            END $$;
+            insert
+                into
+                users (id,
+                profile)
+            values (1,
+            '{"name": "JohnDoe"}'::jsonb) on
+            CONFLICT (id) DO
+            update
+            set
+                profile = users.profile || EXCLUDED.profile RETURNING id,
+                profile->>'name' as name;
             """.trim();
 
         Assert.assertEquals(expectedQuery.trim(), textEdit.getNewText());
