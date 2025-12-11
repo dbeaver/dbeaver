9de4390dc62ff04d181645f0f3e495576ed43a80
Tsimafei Labanovich <timofeilobanovich@gmail.com>
2025-12-11 19:00:13 +0100

    dbeaver/pro#7286 Fix git comments

--- plugins/org.jkiss.dbeaver.ui.config.migration/src/org/jkiss/dbeaver/ui/config/migration/wizards/pgadmin/ConfigImportWizardPagePgAdminSettings.java
+++ plugins/org.jkiss.dbeaver.ui.config.migration/src/org/jkiss/dbeaver/ui/config/migration/wizards/pgadmin/ConfigImportWizardPagePgAdminSettings.java
@@ -22,6 +22,7 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.jkiss.code.NotNull;
 import org.jkiss.dbeaver.ui.UIUtils;
 import org.jkiss.dbeaver.ui.config.migration.ImportConfigMessages;
 import org.jkiss.dbeaver.ui.controls.TextWithOpenFile;
@@ -40,7 +41,7 @@ public class ConfigImportWizardPagePgAdminSettings extends WizardPage {
     }
 
     @Override
-    public void createControl(Composite parent) {
+    public void createControl(@NotNull Composite parent) {
         Composite placeholder = new Composite(parent, SWT.NONE);
         placeholder.setLayout(new GridLayout(1, true));
 
@@ -63,6 +64,7 @@ public class ConfigImportWizardPagePgAdminSettings extends WizardPage {
         return inputFile != null && inputFile.exists();
     }
 
+    @NotNull
     public File getInputFile() {
         return inputFile;
     }
