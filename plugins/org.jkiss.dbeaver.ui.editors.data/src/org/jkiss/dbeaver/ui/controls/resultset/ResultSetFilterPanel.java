0552350ae4b670416f3d25434c42d4107b07d727
Tsimafei Labanovich <timofeilobanovich@gmail.com>
2025-12-16 14:41:23 +0100

    dbeaver/dbeaver#38770 Revert

--- plugins/org.jkiss.dbeaver.ui.editors.data/src/org/jkiss/dbeaver/ui/controls/resultset/ResultSetFilterPanel.java
+++ plugins/org.jkiss.dbeaver.ui.editors.data/src/org/jkiss/dbeaver/ui/controls/resultset/ResultSetFilterPanel.java
@@ -218,11 +218,21 @@ class ResultSetFilterPanel extends Composite implements IContentProposalProvider
                     e.detail = SWT.TRAVERSE_NONE;
                 }
             });
+
+            this.filtersText.addVerifyKeyListener(e -> {
+                if (e.keyCode == SWT.CR || e.keyCode == SWT.LF || e.character == SWT.CR) {
+                    if (filterExpanded && (e.stateMask & SWT.CTRL) == 0) {
+                        return;
+                    }
+                    // Suppress Enter handling if filter is not expanded
+                    e.doit = false;
+                }
+            });
             this.filtersText.addKeyListener(new KeyAdapter() {
                 @Override
                 public void keyPressed(KeyEvent e) {
                     if (e.keyCode == SWT.ARROW_DOWN) {
-                        if (filterExpanded && (e.stateMask & SWT.CTRL) ==0) {
+                        if (filterExpanded && (e.stateMask & SWT.CTRL) == 0) {
                             return;
                         }
                         historyPanel.showFilterHistoryPopup();
