d3c29c293d3bce7e1db2940e3b36f142a4a63e6c
masaaya <156692516+mkanetsuna@users.noreply.github.com>
2026-01-02 02:36:07 +0900

    Remove unused log field from TimescaleSchema

--- plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/model/impls/timescale/TimescaleSchema.java
+++ plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/model/impls/timescale/TimescaleSchema.java
@@ -2,20 +2,16 @@ package org.jkiss.dbeaver.ext.postgresql.model.impls.timescale;
 
 import org.jkiss.code.NotNull;
 import org.jkiss.dbeaver.DBException;
-import org.jkiss.dbeaver.Log;
 import org.jkiss.dbeaver.ext.postgresql.model.PostgreDatabase;
 import org.jkiss.dbeaver.ext.postgresql.model.PostgreSchema;
 import org.jkiss.dbeaver.ext.postgresql.model.PostgreTableBase;
 import org.jkiss.dbeaver.ext.postgresql.model.PostgreTableReal;
-import org.jkiss.dbeaver.model.exec.DBCException;
 import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class TimescaleSchema extends PostgreSchema {
-
-    private static final Log log = Log.getLog(TimescaleSchema.class);
 
     public TimescaleSchema(PostgreDatabase owner, String name, ResultSet dbResult) throws SQLException {
         super(owner, name, dbResult);
