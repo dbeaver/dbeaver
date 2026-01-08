d2ba30243ff3b8d3b224fad2677408c74ea1294b
ShadelessFox <yanuari.channel@gmail.com>
2026-01-08 17:16:36 +0100

    dbeaver/pro#7486 Don't pre-open results panel

--- plugins/org.jkiss.dbeaver.ui.editors.sql/src/org/jkiss/dbeaver/ui/editors/sql/SQLEditor.java
+++ plugins/org.jkiss.dbeaver.ui.editors.sql/src/org/jkiss/dbeaver/ui/editors/sql/SQLEditor.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2025 DBeaver Corp and others
+ * Copyright (C) 2010-2026 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -1460,8 +1460,6 @@ public class SQLEditor extends SQLEditorBase implements
         // Extra views
         createExtraViewControls();
 
-        // Create results tab
-        createQueryProcessor(true, true, true);
         if (isHideQueryText()) {
             resultsSash.setMaximizedControl(resultTabs);
         } else {
