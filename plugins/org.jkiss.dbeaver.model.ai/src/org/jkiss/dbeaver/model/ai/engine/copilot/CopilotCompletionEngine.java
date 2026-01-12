ddadde4b8b3825cd09cf92e1d79e2002afc08a07
Georgii Gvinepadze <georgii.gvinepadze@dbeaver.com>
2026-01-12 16:42:49 +0400

    dbeaver/dbeaver#39442 auth endpoint configuration
    Signed-off-by: Georgii Gvinepadze <georgii.gvinepadze@dbeaver.com>

--- plugins/org.jkiss.dbeaver.model.ai/src/org/jkiss/dbeaver/model/ai/engine/copilot/CopilotCompletionEngine.java
+++ plugins/org.jkiss.dbeaver.model.ai/src/org/jkiss/dbeaver/model/ai/engine/copilot/CopilotCompletionEngine.java
@@ -38,7 +38,7 @@ public class CopilotCompletionEngine<P extends CopilotProperties> extends BaseCo
         @NotNull
         @Override
         protected CopilotClient initialize() throws DBException {
-            return createClient();
+            return createClient(getProperties().getBaseAuthUrl());
         }
 
         @Override
@@ -152,12 +152,12 @@ public class CopilotCompletionEngine<P extends CopilotProperties> extends BaseCo
         );
     }
 
-    protected CopilotClient createClient() throws DBException {
+    protected CopilotClient createClient(@NotNull String baseAuthUrl) throws DBException {
         String token = properties.getToken();
         if (token == null || token.isEmpty()) {
             throw new DBException("Copilot API token is not set");
         }
 
-        return new CopilotClient();
+        return new CopilotClient(baseAuthUrl);
     }
 }
