e9d7578f6178d8a174bd88bae663ff7b4279c91a
Solovev Anton <solovev.anton93@gmail.com>
2026-01-12 10:47:27 +0100

    dbeaver/pro#7009: add manual filter support

--- plugins/org.jkiss.dbeaver.ui.editors.data/src/org/jkiss/dbeaver/ui/controls/resultset/panel/grouping/GroupingResultsContainer.java
+++ plugins/org.jkiss.dbeaver.ui.editors.data/src/org/jkiss/dbeaver/ui/controls/resultset/panel/grouping/GroupingResultsContainer.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2025 DBeaver Corp and others
+ * Copyright (C) 2010-2026 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -327,7 +327,9 @@ public class GroupingResultsContainer implements IResultSetContainer {
                 : ga.getDisplayName())
             .map(dataFilter::getConstraint)
             .filter(Objects::nonNull).toList();
-        return new DBDDataFilter(attributeConstraints);
+        DBDDataFilter newFilter = new DBDDataFilter(attributeConstraints);
+        newFilter.setWhere(dataFilter.getWhere());
+        return newFilter;
     }
 
     private void resetDataFilters() {
