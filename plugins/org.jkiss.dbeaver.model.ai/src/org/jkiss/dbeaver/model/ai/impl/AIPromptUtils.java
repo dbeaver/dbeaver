d1711220bcddbf47de42703b5bdba0e09c8e6f1a
Dmitrii Barnukov <dmitrii.barnukov@dbeaver.com>
2026-01-13 14:01:44 +0100

    dbeaver/pro#7978 fix import

--- plugins/org.jkiss.dbeaver.model.ai/src/org/jkiss/dbeaver/model/ai/impl/AIPromptUtils.java
+++ plugins/org.jkiss.dbeaver.model.ai/src/org/jkiss/dbeaver/model/ai/impl/AIPromptUtils.java
@@ -16,6 +16,7 @@
  */
 package org.jkiss.dbeaver.model.ai.impl;
 
+import org.jkiss.code.NotNull;
 import org.jkiss.code.Nullable;
 import org.jkiss.dbeaver.model.DBPDataSource;
 import org.jkiss.dbeaver.model.DBPDataSourceInfo;
