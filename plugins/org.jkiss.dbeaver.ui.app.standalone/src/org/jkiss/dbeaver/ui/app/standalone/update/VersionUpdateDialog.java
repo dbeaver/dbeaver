451553cfeb4ab36097aab294dccf85652fb882a3
serge-rider <serge@dbeaver.com>
2026-01-14 08:32:58 +0100

    dbeaver/pro#7991 Version and license dialogs

--- plugins/org.jkiss.dbeaver.ui.app.standalone/src/org/jkiss/dbeaver/ui/app/standalone/update/VersionUpdateDialog.java
+++ plugins/org.jkiss.dbeaver.ui.app.standalone/src/org/jkiss/dbeaver/ui/app/standalone/update/VersionUpdateDialog.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2025 DBeaver Corp and others
+ * Copyright (C) 2010-2026 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -115,7 +115,12 @@ public class VersionUpdateDialog extends Dialog {
         Composite composite = new Composite(parent, SWT.NONE);
         composite.setLayoutData(new GridData(GridData.FILL_BOTH));
         composite.setLayout(new GridLayout(1, false));
-        Composite propGroup = UIUtils.createControlGroup(composite, CoreMessages.dialog_version_update_title, 2, GridData.FILL_BOTH, 0);
+        Composite propGroup = UIUtils.createTitledComposite(
+            composite,
+            CoreMessages.dialog_version_update_title,
+            2,
+            GridData.FILL_BOTH
+        );
 
         createTopArea(composite);
 
