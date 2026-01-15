46b53cdabd7a544e2aa40845c9c65f1e212dc1e3
ShadelessFox <yanuari.channel@gmail.com>
2026-01-15 11:56:43 +0100

    dbeaver/pro#7982 Windows AArch64 download url

--- plugins/org.jkiss.dbeaver.ui.app.standalone/src/org/jkiss/dbeaver/ui/app/standalone/update/VersionUpdateDialog.java
+++ plugins/org.jkiss.dbeaver.ui.app.standalone/src/org/jkiss/dbeaver/ui/app/standalone/update/VersionUpdateDialog.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2025 DBeaver Corp and others
+ * Copyright (C) 2010-2026 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -344,14 +344,11 @@ public class VersionUpdateDialog extends Dialog {
 
     @Nullable
     private PlatformInstaller getPlatformInstaller() {
-        switch (Platform.getOS()) {
-            case Platform.OS_WIN32:
-                return new WindowsInstaller();
-            case Platform.OS_MACOSX:
-                return new MacintoshInstaller();
-            default:
-                return null;
-        }
+        return switch (Platform.getOS()) {
+            case Platform.OS_WIN32 -> new WindowsInstaller();
+            case Platform.OS_MACOSX -> new MacintoshInstaller();
+            default -> null;
+        };
     }
 
     @NotNull
@@ -371,7 +368,7 @@ public class VersionUpdateDialog extends Dialog {
             os = OS_LINUX;
         }
         String dist = System.getProperty(PROP_DISTRIBUTION_TYPE);
-        if (RuntimeUtils.isLinux() && CommonUtils.isEmpty(dist)) {
+        if (CommonUtils.isEmpty(dist) && RuntimeUtils.isLinux()) {
             // If distribution type was not set explicitly, then let's attempt a dumb guess.
             try {
                 RuntimeUtils.executeProcess("/usr/bin/apt-get", "--version");
@@ -407,9 +404,9 @@ public class VersionUpdateDialog extends Dialog {
         @Override
         public String getExecutableName(@NotNull ApplicationDescriptor application) {
             if ("zip".equals(System.getProperty(PROP_DISTRIBUTION_TYPE))) {
-                return application.getId() + "-latest-win32.win32.x86_64.zip";
+                return application.getId() + "-latest-win32.win32." + Platform.getOSArch() + ".zip";
             } else {
-                return application.getId() + "-latest-x86_64-setup.exe";
+                return application.getId() + "-latest-" + Platform.getOSArch() + "-setup.exe";
             }
         }
     }
