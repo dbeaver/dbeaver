d679b9e0a5421c65b9449aa7281044ccc0cfc7e0
ShadelessFox <yanuari.channel@gmail.com>
2026-01-19 16:33:45 +0100

    dbeaver/pro#7970 Execution context entity name init fix

--- plugins/org.jkiss.dbeaver.ext.generic/src/org/jkiss/dbeaver/ext/generic/model/GenericExecutionContext.java
+++ plugins/org.jkiss.dbeaver.ext.generic/src/org/jkiss/dbeaver/ext/generic/model/GenericExecutionContext.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2025 DBeaver Corp and others
+ * Copyright (C) 2010-2026 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -181,7 +181,6 @@ public class GenericExecutionContext extends JDBCExecutionContext implements DBC
                         dbStat.execute();
                     }
                 }
-                selectedEntityName = entityName;
             } catch (SQLException e) {
                 throw new DBCException(e, this);
             } finally {
@@ -190,6 +189,10 @@ public class GenericExecutionContext extends JDBCExecutionContext implements DBC
                 }
             }
         }
+        if (entityName == null) {
+            entityName = context.selectedEntityName;
+        }
+        selectedEntityName = entityName;
     }
 
     @Override
