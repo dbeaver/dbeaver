eb19a13d2da508461917a6029944e10d7b824d10
IvanGorshechnikov <ivan.gorshechnikov1@gmail.com>
2026-01-26 12:22:31 +0100

    dbeaver/dbeaver#39947 Fix Athena

--- plugins/org.jkiss.dbeaver.ext.athena/src/org/jkiss/dbeaver/ext/athena/model/AthenaDataSource.java
+++ plugins/org.jkiss.dbeaver.ext.athena/src/org/jkiss/dbeaver/ext/athena/model/AthenaDataSource.java
@@ -67,10 +67,14 @@ public class AthenaDataSource extends GenericDataSource {
         }
 
         // Hack to fix update from v2 -> v3 driver version https://github.com/dbeaver/dbeaver/issues/39947
+        // https://docs.aws.amazon.com/athena/latest/ug/jdbc-v3-driver-aws-configuration-profile-credentials.html
         if (!CommonUtils.isEmpty(connectionInfo.getProperties()) && connectionInfo.getProperties()
             .get(AthenaConstants.PROP_AWS_CREDENTIALS_PROVIDER_CLASS)
             .equals(AthenaConstants.PROP_OLD_VALUE_AWS_CREDENTIALS_PROVIDER_CLASS)) {
-            connectionInfo.getProperties().remove(AthenaConstants.PROP_AWS_CREDENTIALS_PROVIDER_CLASS);
+            connectionInfo.getProperties().put(
+                AthenaConstants.PROP_AWS_CREDENTIALS_PROVIDER_CLASS,
+                "ProfileCredentials"
+            );
         }
 
         return props;
