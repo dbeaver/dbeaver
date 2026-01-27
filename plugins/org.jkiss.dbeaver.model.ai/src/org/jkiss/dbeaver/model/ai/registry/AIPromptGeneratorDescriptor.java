85b36bb438a242f21149abfa17774419922e3d25
IvanGorshechnikov <122968941+IvanGorshechnikov@users.noreply.github.com>
2026-01-27 15:01:46 +0100

    Update plugins/org.jkiss.dbeaver.model.ai/src/org/jkiss/dbeaver/model/ai/registry/AIPromptGeneratorDescriptor.java
    Co-authored-by: ShadelessFox <35821147+ShadelessFox@users.noreply.github.com>

--- plugins/org.jkiss.dbeaver.model.ai/src/org/jkiss/dbeaver/model/ai/registry/AIPromptGeneratorDescriptor.java
+++ plugins/org.jkiss.dbeaver.model.ai/src/org/jkiss/dbeaver/model/ai/registry/AIPromptGeneratorDescriptor.java
@@ -47,10 +47,9 @@ public class AIPromptGeneratorDescriptor extends AbstractDescriptor {
         this.id = config.getAttribute(RegistryConstants.ATTR_ID);
         this.label = config.getAttribute(RegistryConstants.ATTR_LABEL);
         this.icon = iconToImage(config.getAttribute(RegistryConstants.ATTR_ICON));
-        this.uses = new ArrayList<>();
-        for (IConfigurationElement pe : config.getChildren("uses")) {
-            uses.add(new Uses(pe));
-        }
+        this.uses = Stream.of(config.getChildren("uses"))
+            .map(Uses::new)
+            .toList();
     }
 
     @NotNull
