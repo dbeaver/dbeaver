7df90ff60c0f08c1a643b5644e80d0e5918d518f
Solovev Anton <solovev.anton93@gmail.com>
2026-02-17 15:41:33 +0100

    dbeaver/pro#8273: fix private project TE bug

--- plugins/org.jkiss.dbeaver.model.rcp/src/org/jkiss/dbeaver/model/rcp/EclipseWorkspaceImpl.java
+++ plugins/org.jkiss.dbeaver.model.rcp/src/org/jkiss/dbeaver/model/rcp/EclipseWorkspaceImpl.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2025 DBeaver Corp and others
+ * Copyright (C) 2010-2026 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -214,7 +214,8 @@ public abstract class EclipseWorkspaceImpl extends BaseWorkspaceImpl implements 
                 }
                 this.projects.put(project, projectMetadata);
 
-                if (activeProject == null || (!CommonUtils.isEmpty(activeProjectName) && project.getName().equals(activeProjectName))) {
+                if (activeProject == null || (!CommonUtils.isEmpty(activeProjectName) && projectMetadata.getName()
+                    .equals(activeProjectName))) {
                     activeProject = projectMetadata;
                 }
                 projectMetadata.hideConfigurationFiles();
