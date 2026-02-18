f4daccc5a5b93a528284aee98a9f0c71dfd99848
Ivan Gagarkin <gagarkin.iiu@gmail.com>
2026-02-18 10:34:39 +0100

    dbeaver/pro#8176 Add quotes to formatted timestamp values in SQLite

--- plugins/org.jkiss.dbeaver.ext.sqlite/src/org/jkiss/dbeaver/ext/sqlite/model/data/SQLiteValueHandler.java
+++ plugins/org.jkiss.dbeaver.ext.sqlite/src/org/jkiss/dbeaver/ext/sqlite/model/data/SQLiteValueHandler.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2024 DBeaver Corp and others
+ * Copyright (C) 2010-2026 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -129,7 +129,7 @@ public class SQLiteValueHandler extends JDBCAbstractValueHandler implements DBDV
                 }
             }
 
-            return timestampFormatter.formatValue(value);
+            return "'" + timestampFormatter.formatValue(value) + "'";
         } else if (value instanceof String string && column.getDataKind() == DBPDataKind.DATETIME) {
             if (format == DBDDisplayFormat.NATIVE && !string.startsWith("'") && !string.endsWith("'")) {
                 return "'" + value + "'";
