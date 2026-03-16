19c4f65446b9d672c9856825a959de03c78dc4cd
Georgii Gvinepadze <georgii.gvinepadze@dbeaver.com>
2026-03-16 17:50:53 +0400

    dbeaver/pro#8708 fix test run
    Signed-off-by: Georgii Gvinepadze <georgii.gvinepadze@dbeaver.com>

--- plugins/org.jkiss.dbeaver.osgi.test.runner/src/org/jkiss/junit/osgi/OSGITestRunner.java
+++ plugins/org.jkiss.dbeaver.osgi.test.runner/src/org/jkiss/junit/osgi/OSGITestRunner.java
@@ -357,8 +357,9 @@ public class OSGITestRunner extends BlockJUnit4ClassRunner {
         // Install all bundles from the directory
         for (String bundleFile : ManifestElement.getArrayFromList(props.getProperty("osgi.bundles"))) {
             if (
-                //TODO research why app bundles skipped
-                bundleFile.contains(".app")
+                // Avoid adding app bundles to bundle start list,
+                // because they will be started by application launcher and may have specific start level
+                bundleFile.contains(".app.")
                     && !bundleFile.contains(appBundleName)
                     && !bundleFile.contains("org.eclipse")
                     && forceDependencies.stream().noneMatch(bundleFile::contains)
