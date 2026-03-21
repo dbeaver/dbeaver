fe8d931391340e75602ad86a6da321793092499f
Kartikey Tripathi <kartikeytripathiietkhandari@gmail.com>
2026-03-21 16:50:13 +0530

    Add Manticore Search driver support

---
+++ plugins/org.jkiss.dbeaver.ext.generic/src/org/jkiss/dbeaver/ext/generic/model/ManticoreSQLDialect.java
@@ -0,0 +1,12 @@
+﻿package org.jkiss.dbeaver.ext.generic.model;
+
+public class ManticoreSQLDialect extends GenericSQLDialect {
+    public ManticoreSQLDialect() {
+        super();
+    }
+
+    @Override
+    public boolean supportsAliasInSelect() {
+        return false;
+    }
+}
