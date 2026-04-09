1fa152d005f8a28ae81bad95b15dad963583255a
Serge Rider <serge@dbeaver.com>
2026-04-09 14:46:08 +0200

    dbeaver/pro#8755 Explain plan refactoring

--- plugins/org.jkiss.dbeaver.ui.editors.sql/src/org/jkiss/dbeaver/ui/editors/sql/plan/registry/SQLPlanViewRegistry.java
+++ plugins/org.jkiss.dbeaver.ui.editors.sql/src/org/jkiss/dbeaver/ui/editors/sql/plan/registry/SQLPlanViewRegistry.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2024 DBeaver Corp and others
+ * Copyright (C) 2010-2026 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -45,7 +45,7 @@ public class SQLPlanViewRegistry {
         return instance;
     }
 
-    private List<SQLPlanViewDescriptor> planViewDescriptors = new ArrayList<>();
+    private final List<SQLPlanViewDescriptor> planViewDescriptors = new ArrayList<>();
 
     private SQLPlanViewRegistry(IExtensionRegistry registry)
     {
