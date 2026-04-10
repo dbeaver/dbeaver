63dedaa05ea5dbb9909d0d7c84a7c687699c8a01
ShadelessFox <yanuari.channel@gmail.com>
2026-04-10 16:16:07 +0200

    dbeaver/pro#8513 Add action utils

--- plugins/org.jkiss.dbeaver.ui/src/org/jkiss/dbeaver/ui/ActionUtils.java
+++ plugins/org.jkiss.dbeaver.ui/src/org/jkiss/dbeaver/ui/ActionUtils.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2025 DBeaver Corp and others
+ * Copyright (C) 2010-2026 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,10 +24,7 @@ import org.eclipse.core.commands.ParameterizedCommand;
 import org.eclipse.core.commands.common.NotDefinedException;
 import org.eclipse.core.expressions.EvaluationContext;
 import org.eclipse.core.expressions.IEvaluationContext;
-import org.eclipse.jface.action.Action;
-import org.eclipse.jface.action.ActionContributionItem;
-import org.eclipse.jface.action.ContributionItem;
-import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.action.*;
 import org.eclipse.jface.bindings.Binding;
 import org.eclipse.jface.bindings.TriggerSequence;
 import org.eclipse.jface.commands.ToggleState;
@@ -52,13 +49,14 @@ import org.eclipse.ui.services.IServiceLocator;
 import org.jkiss.code.NotNull;
 import org.jkiss.code.Nullable;
 import org.jkiss.dbeaver.Log;
+import org.jkiss.dbeaver.model.DBIcon;
 import org.jkiss.dbeaver.model.DBPImage;
 import org.jkiss.dbeaver.runtime.DBWorkbench;
 import org.jkiss.utils.ArrayUtils;
 import org.jkiss.utils.CommonUtils;
 
-import java.util.List;
 import java.util.*;
+import java.util.List;
 
 /**
  * Action utils
@@ -179,6 +177,18 @@ public class ActionUtils {
             contributionParameters.mode = CommandContributionItem.MODE_FORCE_TEXT;
         }
         return new CommandContributionItem(contributionParameters);
+    }
+
+    @NotNull
+    public static IContributionItem makeContribution(
+        @NotNull String text,
+        @NotNull String toolTipText,
+        @NotNull DBIcon icon,
+        @NotNull Runnable callback
+    ) {
+        var item = new ActionContributionItem(makeAction(text, toolTipText, icon, callback));
+        item.setMode(ActionContributionItem.MODE_FORCE_TEXT);
+        return item;
     }
 
     public static boolean isCommandEnabled(String commandId, IServiceLocator site) {
@@ -424,6 +434,25 @@ public class ActionUtils {
         }
 
         return actionImpl;
+    }
+
+    @NotNull
+    public static IAction makeAction(
+        @NotNull String text,
+        @NotNull String toolTipText,
+        @NotNull DBIcon icon,
+        @NotNull Runnable callback
+    ) {
+        Action action = new Action() {
+            @Override
+            public void run() {
+                callback.run();
+            }
+        };
+        action.setText(text);
+        action.setToolTipText(toolTipText);
+        action.setImageDescriptor(DBeaverIcons.getImageDescriptor(icon));
+        return action;
     }
 
     public static void evaluatePropertyState(String propertyName) {
