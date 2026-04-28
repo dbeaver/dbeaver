f7fde475ca7d575b2a6896e0f47e8c8f4cffa3f0
Dmitrii <dmitrii.barnukov@dbeaver.com>
2026-04-28 10:28:53 +0200

    dbeaver/pro#5090 Make AWS fs id case-insensitive

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/fs/AbstractFileSystemProvider.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/fs/AbstractFileSystemProvider.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2024 DBeaver Corp and others
+ * Copyright (C) 2010-2026 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -45,7 +45,7 @@ public abstract class AbstractFileSystemProvider implements DBFFileSystemProvide
             fileSystem = fileSystems.length == 0 ? null : fileSystems[0];
         } else {
             for (DBFVirtualFileSystem fs : fileSystems) {
-                if (fs.getId().equals(fsId)) {
+                if (fs.getId().equalsIgnoreCase(fsId)) {
                     fileSystem = fs;
                     break;
                 }
