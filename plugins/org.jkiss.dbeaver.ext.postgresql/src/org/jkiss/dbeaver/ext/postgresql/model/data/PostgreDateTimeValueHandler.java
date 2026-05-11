45292e22dc354a07b48da00a96f43da9c72e9bbc
Elizabeth <e1izabeth.k@outlook.com>
2026-05-11 10:42:09 +0300

    #40757 Fix time end of day handling for postgresql

--- plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/model/data/PostgreDateTimeValueHandler.java
+++ plugins/org.jkiss.dbeaver.ext.postgresql/src/org/jkiss/dbeaver/ext/postgresql/model/data/PostgreDateTimeValueHandler.java
@@ -90,19 +90,37 @@ public class PostgreDateTimeValueHandler extends JDBCDateTimeValueHandler {
         if (resultSet instanceof JDBCResultSet jdbc) {
             if (type.getTypeID() == Types.TIME && !formatSettings.isUseNativeDateTimeFormat()) {
                 try {
+                    //
+                    // psql driver carries all the values in a byte arrays inside the PgResultSet::thisRow::data field,
+                    // which is being parsed by get-methods without reloading, so it is ok to access one multiple times
+                    // (previous approach WAS doing that always anyway)
                     String rawString = jdbc.getString(index + 1);
-                    if (TIME_END_OF_DAY_STRING.equals(rawString)) {
-                        return PostgreEndOfDay.withoutOffset();
-                    }
-                    if (rawString != null) {
+                    if (rawString == null) {
+                        return null;
+                    } else {
+                        // org.postgresql.jdbc.TypeInfoCache has hardcoded Types.TIME for both TIMETZ and TIME, so it itself recognizes them
+                        // but DOES NOT EXPOSE this information for us. While should have been returning Types.TIME_WITH_TIMEZONE for TIMETZ
+                        // jdbc.getObject<OffsetTime> on a LocalTime value throws conversion exception and vice versa, we don't want that!
                         if (rawString.contains("+")) {
-                            if (rawString.startsWith(TIME_END_OF_DAY_STRING + "+")) {
+                            // psql driver does the same startsWith(..) then custom string parsing manually, see
+                            // org.postgresql.jdbc.TimestampUtils::toOffsetTime called from PgResultSet::getOffsetTime
+                            // let's do the same
+                            if (rawString.startsWith(TIME_END_OF_DAY_STRING)) {
+                                // but instead of OffsetTime.MAX which is '23:59:59.999999999-18:00', we want zone-specific END_OF_DAY
                                 return new PostgreOffsetEndOfDay(ZoneOffset.of(rawString.substring(TIME_END_OF_DAY_STRING.length())));
                             } else {
                                 return jdbc.getObject(index + 1, OffsetTime.class);
                             }
                         } else {
-                            return jdbc.getObject(index + 1, LocalTime.class);
+                            // psql driver does literally the same, see
+                            // org.postgresql.jdbc.TimestampUtils::toLocalTime called from PgResultSet::getLocalTime
+                            // let's do the same as postgresql jdbc driver
+                            if (TIME_END_OF_DAY_STRING.equals(rawString)) {
+                                // but instead of LocalTime.MAX which is '23:59:59.999999999' in-the-day, we want zone-less END_OF_DAY
+                                return PostgreEndOfDay.withoutOffset();
+                            } else {
+                                return jdbc.getObject(index + 1, LocalTime.class);
+                            }
                         }
                     }
                 } catch (SQLException e) {
