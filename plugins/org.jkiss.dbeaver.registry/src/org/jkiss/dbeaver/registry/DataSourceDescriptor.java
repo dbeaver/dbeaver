3ea478cb4ee18f409dfd102211e415b4feee9f59
Ainur <ainur.iagudin@dbeaver.com>
2026-05-20 17:03:01 +0200

    dbeaver/pro#9296 add comment

--- plugins/org.jkiss.dbeaver.registry/src/org/jkiss/dbeaver/registry/DataSourceDescriptor.java
+++ plugins/org.jkiss.dbeaver.registry/src/org/jkiss/dbeaver/registry/DataSourceDescriptor.java
@@ -533,6 +533,8 @@ public class DataSourceDescriptor
         this.secretsContainsDatabaseCreds = false;
         this.availableSharedCredentials = null;
         this.selectedSharedCredentials = null;
+        // we need to reset username and password for connection that are not connected to database
+        // in some cases, when connection is established, username is needed (e.g. retrieving current user info in Cubrid)
         if (sharedCredentials && !isConnected()) {
             // For shared credentials reset cache also
             connectionInfo.setUserName(null);
