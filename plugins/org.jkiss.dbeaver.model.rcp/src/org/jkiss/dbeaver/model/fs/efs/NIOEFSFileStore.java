1db4771d0660c99df173ef91d8fe6b0465fcfe92
Solovev Anton <solovev.anton93@gmail.com>
2026-06-09 18:38:51 +0200

    dbeaver/pro#8037: file store impl

--- plugins/org.jkiss.dbeaver.model.rcp/src/org/jkiss/dbeaver/model/fs/efs/NIOEFSFileStore.java
+++ plugins/org.jkiss.dbeaver.model.rcp/src/org/jkiss/dbeaver/model/fs/efs/NIOEFSFileStore.java
@@ -16,42 +16,32 @@
  */
 package org.jkiss.dbeaver.model.fs.efs;
 
+import org.eclipse.core.filesystem.IFileStore;
+import org.jkiss.code.NotNull;
+import org.jkiss.code.Nullable;
+
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.nio.file.FileStore;
 import java.nio.file.attribute.FileAttributeView;
 import java.nio.file.attribute.FileStoreAttributeView;
 
 public class NIOEFSFileStore extends FileStore {
 
+    private final IFileStore efsFileStore;
 
-    @Override
-    public String name() {
-        return "";
-    }
-
-    @Override
-    public String type() {
-        return "";
-    }
-
-    @Override
-    public boolean isReadOnly() {
-        return false;
-    }
-
-    @Override
-    public long getTotalSpace() throws IOException {
-        return 0;
+    public NIOEFSFileStore(@NotNull IFileStore efsFileStore) {
+        this.efsFileStore = efsFileStore;
     }
 
     @Override
     public long getUsableSpace() throws IOException {
-        return 0;
+        throw new UnsupportedEncodingException();
     }
 
     @Override
     public long getUnallocatedSpace() throws IOException {
-        return 0;
+        throw new UnsupportedEncodingException();
     }
 
     @Override
@@ -65,12 +55,35 @@ public class NIOEFSFileStore extends FileStore {
     }
 
     @Override
+    @Nullable
     public <V extends FileStoreAttributeView> V getFileStoreAttributeView(Class<V> type) {
         return null;
     }
 
     @Override
     public Object getAttribute(String attribute) throws IOException {
-        return null;
+        throw new UnsupportedOperationException("Does not support the given attribute: " + attribute);
+    }
+
+    @Override
+    @NotNull
+    public String name() {
+        return efsFileStore.getName();
+    }
+
+    @Override
+    @NotNull
+    public String type() {
+        return efsFileStore.getFileSystem().getScheme();
+    }
+
+    @Override
+    public boolean isReadOnly() {
+        return false;
+    }
+
+    @Override
+    public long getTotalSpace() throws IOException {
+        return 0;
     }
 }
