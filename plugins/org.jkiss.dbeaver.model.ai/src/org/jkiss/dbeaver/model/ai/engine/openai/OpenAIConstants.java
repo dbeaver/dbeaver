9dabb7d8902b12b8ea5fbc9fc16585b7def8d17c
Serge Rider <serge@dbeaver.com>
2026-07-14 09:13:19 +0200

    dbeaver/pro#9839 Gpt-5 is the default openai model now

--- plugins/org.jkiss.dbeaver.model.ai/src/org/jkiss/dbeaver/model/ai/engine/openai/OpenAIConstants.java
+++ plugins/org.jkiss.dbeaver.model.ai/src/org/jkiss/dbeaver/model/ai/engine/openai/OpenAIConstants.java
@@ -20,7 +20,6 @@ package org.jkiss.dbeaver.model.ai.engine.openai;
  * OpenAI constants
  */
 public class OpenAIConstants {
-    public static final String DEFAULT_MODEL = "gpt-4o";
 
     public static final String OPENAI_ENGINE = "openai";
     public static final String GPT_API_TOKEN = "gpt.token";
