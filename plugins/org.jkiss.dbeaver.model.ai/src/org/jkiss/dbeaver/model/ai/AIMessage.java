776782bc71445fc67ff16c231b21278e688da89e
IvanGorshechnikov <vania.gorshevhnikov@gmail.com>
2026-07-15 12:12:53 +0200

    dbeaver/pro#9878 Fix timeout error message

--- plugins/org.jkiss.dbeaver.model.ai/src/org/jkiss/dbeaver/model/ai/AIMessage.java
+++ plugins/org.jkiss.dbeaver.model.ai/src/org/jkiss/dbeaver/model/ai/AIMessage.java
@@ -18,10 +18,14 @@ package org.jkiss.dbeaver.model.ai;
 
 import org.jkiss.code.NotNull;
 import org.jkiss.code.Nullable;
+import org.jkiss.dbeaver.model.ai.internal.AIMessages;
 import org.jkiss.utils.CommonUtils;
 
+import java.net.SocketTimeoutException;
+import java.net.http.HttpTimeoutException;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.TimeoutException;
 
 /**
  * Represents a single AI message
@@ -135,11 +139,21 @@ public class AIMessage {
     public AIMessage(@NotNull Throwable error) {
         this(
             AIMessageType.ERROR,
-            CommonUtils.getAllExceptionMessages(error),
-            CommonUtils.getAllExceptionMessages(error),
+            getErrorMessage(error),
+            getErrorMessage(error),
             LocalDateTime.now(),
             null,
             error);
+    }
+
+    @NotNull
+    private static String getErrorMessage(@NotNull Throwable error) {
+        for (Throwable t = error; t != null; t = t.getCause()) {
+            if (t instanceof HttpTimeoutException || t instanceof SocketTimeoutException || t instanceof TimeoutException) {
+                return AIMessages.ai_error_request_timed_out;
+            }
+        }
+        return CommonUtils.getAllExceptionMessages(error);
     }
 
     public AIMessage(
