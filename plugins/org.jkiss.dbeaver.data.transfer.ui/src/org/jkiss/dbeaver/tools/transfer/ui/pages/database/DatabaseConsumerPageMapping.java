cb6644b400230c06f880046b5ad818c224c2d253
IvanGorshechnikov <122968941+IvanGorshechnikov@users.noreply.github.com>
2026-08-04 14:19:20 +0200

    dbeaver/pro#10072 Fix bottom buttons in data transfer (#41661)
    * dbeaver/pro#10072 Fix bottom buttons in data transfer
    
    * dbeaver/pro#10072 Fix bottom buttons in data transfer
    
    ---------
    
    Co-authored-by: Matvey16 <82543000+Matvey16@users.noreply.github.com>

--- plugins/org.jkiss.dbeaver.data.transfer.ui/src/org/jkiss/dbeaver/tools/transfer/ui/pages/database/DatabaseConsumerPageMapping.java
+++ plugins/org.jkiss.dbeaver.data.transfer.ui/src/org/jkiss/dbeaver/tools/transfer/ui/pages/database/DatabaseConsumerPageMapping.java
@@ -93,6 +93,7 @@ public class DatabaseConsumerPageMapping extends DataTransferPageNodeSettings {
     private ControlEditor chooseContainerEditor;
     private Button upButton;
     private Button downButton;
+    private Composite bottomBar;
     private Button recreateCheck;
     private Button transformCheck;
     private Combo transformCombo;
@@ -410,7 +411,7 @@ public class DatabaseConsumerPageMapping extends DataTransferPageNodeSettings {
 
         {
             boolean withUpDown = getWizard().getSettings().getDataPipes().size() > 1;
-            Composite bottomBar = UIUtils.createComposite(composite, 5 + (withUpDown ? 2 : 0));
+            bottomBar = UIUtils.createComposite(composite, 5 + (withUpDown ? 2 : 0));
             bottomBar.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 
             Label hintInfo = new Label(bottomBar, SWT.NONE);
@@ -1756,7 +1757,7 @@ public class DatabaseConsumerPageMapping extends DataTransferPageNodeSettings {
         updateRecreateCheck(selectedMapping);
         setControlExcluded(recreateCheck, !isContainer);
         setControlExcluded(transformCheck, !isColumn);
-        setControlExcluded(transformCombo, !isColumn);
+        transformCombo.setVisible(isColumn);
         layoutBottomBar();
         updateTransformControls(selectedMapping);
         updateUpAndDownButtons();
@@ -1784,12 +1785,10 @@ public class DatabaseConsumerPageMapping extends DataTransferPageNodeSettings {
     }
 
     private void layoutBottomBar() {
-        if (recreateCheck != null && !recreateCheck.isDisposed()) {
-            Composite parent = recreateCheck.getParent();
-            if (parent != null && !parent.isDisposed()) {
-                parent.layout(true);
-            }
+        if (bottomBar == null || bottomBar.isDisposed()) {
+            return;
         }
+        bottomBar.layout(true);
     }
 
     protected boolean hasMappings(@Nullable DatabaseMappingObject mapping) {
