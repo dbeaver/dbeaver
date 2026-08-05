f86b58f9aff9ded00602e36853312978a1ce0a6f
Serge Rider <serge@dbeaver.com>
2026-08-05 10:59:52 +0300

    dbeaver/pro#9893 CDS base model

--- plugins/org.jkiss.dbeaver.ui.navigator/src/org/jkiss/dbeaver/ui/controls/ViewerColumnController.java
+++ plugins/org.jkiss.dbeaver.ui.navigator/src/org/jkiss/dbeaver/ui/controls/ViewerColumnController.java
@@ -353,16 +353,16 @@ public class ViewerColumnController<COLUMN, ELEMENT> {
         isPacking = true;
         try {
             int itemCount = 0;
-            if (viewer instanceof TreeViewer) {
-                itemCount = ((TreeViewer) viewer).getTree().getItemCount();
+            if (viewer instanceof TreeViewer treeViewer) {
+                itemCount = treeViewer.getTree().getItemCount();
                 float[] ratios = null;
-                if (((TreeViewer) viewer).getTree().getColumnCount() == 2) {
+                if (treeViewer.getTree().getColumnCount() == 2) {
                     ratios = new float[]{0.6f, 0.4f};
                 }
-                UIUtils.packColumns(((TreeViewer) viewer).getTree(), forceAutoSize, ratios);
-            } else if (viewer instanceof TableViewer) {
-                itemCount = ((TableViewer) viewer).getTable().getItemCount();
-                UIUtils.packColumns(((TableViewer) viewer).getTable(), forceAutoSize);
+                UIUtils.packColumns(treeViewer.getTree(), forceAutoSize, ratios);
+            } else if (viewer instanceof TableViewer tableViewer) {
+                itemCount = tableViewer.getTable().getItemCount();
+                UIUtils.packColumns(tableViewer.getTable(), forceAutoSize);
             }
 
             /*if (itemCount == 0) */{
