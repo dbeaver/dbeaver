246399b44ee3990bf802204182ebf97652c232aa
ShadelessFox <yanuari.channel@gmail.com>
2026-08-14 14:37:30 +0200

    dbeaver/pro#10277 Move language chooser from the button bar to the page

--- plugins/org.jkiss.dbeaver.ui.app.config/src/org/jkiss/dbeaver/ui/app/config/pages/ProductConfigWelcomePage.java
+++ plugins/org.jkiss.dbeaver.ui.app.config/src/org/jkiss/dbeaver/ui/app/config/pages/ProductConfigWelcomePage.java
@@ -16,19 +16,42 @@
  */
 package org.jkiss.dbeaver.ui.app.config.pages;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.jkiss.code.NotNull;
+import org.jkiss.dbeaver.model.app.DBPPlatformDesktop;
+import org.jkiss.dbeaver.model.app.DBPPlatformLanguage;
+import org.jkiss.dbeaver.model.app.DBPPlatformLanguageManager;
+import org.jkiss.dbeaver.registry.language.PlatformLanguageRegistry;
+import org.jkiss.dbeaver.runtime.DBWorkbench;
+import org.jkiss.dbeaver.ui.UIUtils;
 import org.jkiss.dbeaver.ui.app.config.nls.ProductConfigMessages;
-import org.jkiss.dbeaver.ui.forms.UIAlignX;
-import org.jkiss.dbeaver.ui.forms.UIGrowX;
-import org.jkiss.dbeaver.ui.forms.UIPanelBuilder;
-import org.jkiss.dbeaver.ui.forms.UIRowBuilder;
+import org.jkiss.dbeaver.ui.forms.*;
 
 import java.util.function.Consumer;
 
 public class ProductConfigWelcomePage extends ProductConfigWizardPage {
+    private final UIObservable<DBPPlatformLanguage> language;
+    private boolean seenLanguageChangeWarning = false;
+
     public ProductConfigWelcomePage() {
         super(ProductConfigMessages.welcome_title, ProductConfigMessages.welcome_description);
+
+        language = UIObservable.of(
+            DBPPlatformDesktop.getInstance().getPlatformLanguage(),
+            DBPPlatformLanguage.class
+        );
+        language.addChangeListener((o, newLanguage) -> {
+            if (!seenLanguageChangeWarning) {
+                seenLanguageChangeWarning = true;
+                UIUtils.showMessageBox(
+                    getShell(),
+                    "Language change",
+                    "Language change will be applied after restart.",
+                    SWT.ICON_INFORMATION
+                );
+            }
+        });
     }
 
     @Override
@@ -36,10 +59,17 @@ public class ProductConfigWelcomePage extends ProductConfigWizardPage {
         setControl(UIPanelBuilder.build(parent, buildPanel()));
     }
 
+    @Override
+    public void applySettings() {
+        if (DBWorkbench.getPlatform() instanceof DBPPlatformLanguageManager manager) {
+            manager.setPlatformLanguage(language.get());
+        }
+    }
+
     @NotNull
-    private static Consumer<UIPanelBuilder> buildPanel() {
-        return pb -> pb
-            .margins(10, 10)
+    private Consumer<UIPanelBuilder> buildPanel() {
+        return pb -> pb.margins(10, 10)
+            .row(rb -> rb.panel(createLanguageChooser()))
             .row(rb -> rb
                 .label(lb -> lb
                     .text(ProductConfigMessages.welcome_body_text)
@@ -59,5 +89,19 @@ public class ProductConfigWelcomePage extends ProductConfigWizardPage {
                             .hint(TEXT_WIDTH_HINT, TEXT_HEIGHT_HINT)
                             .align(UIAlignX.FILL)
                             .grow(UIGrowX.ALWAYS)))));
+    }
+
+    @NotNull
+    private Consumer<UIPanelBuilder> createLanguageChooser() {
+        if (!(DBWorkbench.getPlatform() instanceof DBPPlatformLanguageManager)) {
+            return UIRowBuilder.identityConsumer();
+        }
+        return pb -> pb.row(rb -> rb
+            .label("Language:")
+            .comboBox(
+                PlatformLanguageRegistry.getInstance().getLanguages(),
+                language,
+                DBPPlatformLanguage::getLabel))
+            .row(rb -> rb.label(""));
     }
 }
