028eeaaed410a1abab78c10b1ec05cfbd18a4e37
serge-rider <serge@dbeaver.com>
2026-08-17 09:28:10 +0200

    dbeaver/pro#10293 Fix main toolbars background

--- plugins/org.jkiss.dbeaver.ui.navigator/src/org/jkiss/dbeaver/ui/actions/datasource/DataSourceToolbarUtils.java
+++ plugins/org.jkiss.dbeaver.ui.navigator/src/org/jkiss/dbeaver/ui/actions/datasource/DataSourceToolbarUtils.java
@@ -37,6 +37,7 @@ import org.jkiss.dbeaver.model.struct.DBStructUtils;
 import org.jkiss.dbeaver.ui.UIUtils;
 import org.jkiss.dbeaver.ui.actions.ConnectionCommands;
 import org.jkiss.dbeaver.ui.editors.EditorUtils;
+import org.jkiss.dbeaver.utils.RuntimeUtils;
 
 public class DataSourceToolbarUtils {
 
@@ -58,8 +59,8 @@ public class DataSourceToolbarUtils {
     }
 
     public static void refreshSelectorToolbar(IWorkbenchWindow window) {
-        if (window instanceof WorkbenchWindow ww && window.getActivePage() != null) {
-            MTrimBar topTrim = ww.getTopTrim();
+        if (window instanceof WorkbenchWindow && window.getActivePage() != null) {
+            MTrimBar topTrim = ((WorkbenchWindow) window).getTopTrim();
             boolean showConnectionSelector = false;
             boolean showSchemaSelector = false;
             IEditorPart activeEditor = window.getActivePage().getActiveEditor();
@@ -87,7 +88,7 @@ public class DataSourceToolbarUtils {
                         if (widget instanceof Composite controlsPanel) {
                             Control[] childControl = controlsPanel.getChildren();
                             for (Control cc : childControl) {
-                                cc.setBackground(bgColor != null ? bgColor : controlsPanel.getBackground());
+                                cc.setBackground(bgColor == null ? controlsPanel.getBackground() : bgColor);
                                 cc.setEnabled(showConnectionSelector && canChangeConn);
                             }
                         }
@@ -111,7 +112,19 @@ public class DataSourceToolbarUtils {
                             }
                         }
                     }
-                    return;
+                } else if (RuntimeUtils.isWindows()) {
+                    // Fix of broken tool items bg color dbeaver/pro#10293
+                    // Set items background to toolbar background
+                    // We have similar fix in ConControlElementHandler
+                    if (element instanceof MElementContainer<?>) {
+                        Object widget = element.getWidget();
+                        if (widget instanceof Composite controlsPanel) {
+                            Color tbBg = controlsPanel.getBackground();
+                            for (Control cc : controlsPanel.getChildren()) {
+                                cc.setBackground(tbBg);
+                            }
+                        }
+                    }
                 }
             }
         }
