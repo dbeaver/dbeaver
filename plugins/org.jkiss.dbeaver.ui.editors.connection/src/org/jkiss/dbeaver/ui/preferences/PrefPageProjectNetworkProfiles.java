3ed1ff50a303a7561c840465bed1043be77f6dc3
Solovev Anton <solovev.anton93@gmail.com>
2026-08-18 11:32:59 +0200

    dbeaver/pro#10008: fix style

--- plugins/org.jkiss.dbeaver.ui.editors.connection/src/org/jkiss/dbeaver/ui/preferences/PrefPageProjectNetworkProfiles.java
+++ plugins/org.jkiss.dbeaver.ui.editors.connection/src/org/jkiss/dbeaver/ui/preferences/PrefPageProjectNetworkProfiles.java
@@ -156,11 +156,9 @@ public class PrefPageProjectNetworkProfiles extends PrefPageNetworkProfiles impl
             UIConnectionMessages.pref_page_network_profiles_tool_delete_confirmation_title,
             NLS.bind(
                 UIConnectionMessages.pref_page_network_profiles_tool_delete_used_confirmation_question,
-                new Object[] {
                     selectedProfile.getProfileName(),
                     usedBy.size(),
                     usedByNames
-                }
             )
         )) {
             return false;
