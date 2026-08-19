864459f94541444372375ed8fbb7a85b1487383b
sergeyteleshev <iamsergeyteleshev@gmail.com>
2026-08-19 10:15:12 +0200

    dbeaver/pro#6605 cleanup

--- plugins/org.jkiss.dbeaver.model.ai/src/org/jkiss/dbeaver/model/ai/engine/openai/OpenAIProperties.java
+++ plugins/org.jkiss.dbeaver.model.ai/src/org/jkiss/dbeaver/model/ai/engine/openai/OpenAIProperties.java
@@ -33,10 +33,10 @@ import org.jkiss.dbeaver.runtime.DBWorkbench;
 import java.util.Map;
 
 public class OpenAIProperties extends BaseAIEngineProperties implements OpenAIBaseProperties {
-    private static final String GPT_BASE_URL = "gpt.base_url";
-    private static final String GPT_TOKEN = "gpt.token";
-    private static final String GPT_MODEL = "gpt.model";
-    private static final String GPT_CONTEXT_WINDOW_SIZE = "gpt.contextWindowSize";
+    protected static final String GPT_BASE_URL = "gpt.base_url";
+    protected static final String GPT_TOKEN = "gpt.token";
+    protected static final String GPT_MODEL = "gpt.model";
+    protected static final String GPT_CONTEXT_WINDOW_SIZE = "gpt.contextWindowSize";
 
     @Nullable
     @SerializedName(GPT_BASE_URL)
@@ -49,11 +49,11 @@ public class OpenAIProperties extends BaseAIEngineProperties implements OpenAIBa
 
     @Nullable
     @SerializedName(GPT_MODEL)
-    private String model;
+    protected String model;
 
     @Nullable
     @SerializedName(GPT_CONTEXT_WINDOW_SIZE)
-    private Integer contextWindowSize;
+    protected Integer contextWindowSize;
 
     public OpenAIProperties() {
     }
