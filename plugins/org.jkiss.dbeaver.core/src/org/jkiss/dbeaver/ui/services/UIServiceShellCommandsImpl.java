b96f227f2bae4002b1580963fc1778445abe0e70
Solovev Anton <solovev.anton93@gmail.com>
2026-08-20 16:43:48 +0200

    dbeaver/pro#10206: refactor prompt UI

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/services/UIServiceShellCommandsImpl.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/services/UIServiceShellCommandsImpl.java
@@ -17,26 +17,20 @@
 package org.jkiss.dbeaver.ui.services;
 
 import org.eclipse.jface.dialogs.IDialogConstants;
-import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.jface.window.Window;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Text;
 import org.jkiss.code.NotNull;
 import org.jkiss.code.Nullable;
 import org.jkiss.dbeaver.DBException;
 import org.jkiss.dbeaver.core.CoreMessages;
-import org.jkiss.dbeaver.model.DBIcon;
 import org.jkiss.dbeaver.model.preferences.ConfirmedShellCommandsStore;
 import org.jkiss.dbeaver.model.runtime.DBRShellCommand;
 import org.jkiss.dbeaver.model.runtime.RunnableWithResult;
 import org.jkiss.dbeaver.runtime.ui.UIServiceShellCommands;
-import org.jkiss.dbeaver.ui.DBeaverIcons;
 import org.jkiss.dbeaver.ui.UIUtils;
 import org.jkiss.dbeaver.ui.dialogs.BaseDialog;
 
@@ -91,9 +85,6 @@ public class UIServiceShellCommandsImpl implements UIServiceShellCommands {
     }
 
     private static class ConfirmShellCommandDialog extends BaseDialog {
-        private static final int MINIMUM_WIDTH = 700;
-        private static final int MAXIMUM_SCRIPT_LINES = 8;
-
         private final Map<String, String> approvalContext;
         private final String command;
 
@@ -111,34 +102,26 @@ public class UIServiceShellCommandsImpl implements UIServiceShellCommands {
         @Override
         protected Composite createDialogArea(@NotNull Composite parent) {
             Composite area = super.createDialogArea(parent);
-            ((GridLayout) area.getLayout()).numColumns = 2;
-
-            Label warningImage = new Label(area, SWT.NONE);
-            warningImage.setImage(DBeaverIcons.getImage(DBIcon.STATUS_WARNING));
-            warningImage.setLayoutData(new GridData(SWT.BEGINNING, SWT.BEGINNING, false, false));
-
-            Composite content = UIUtils.createComposite(area, 1);
+            Composite content = UIUtils.createComposite(area, 2);
             content.setLayoutData(new GridData(GridData.FILL_BOTH));
 
-            Label description = UIUtils.createLabel(content, CoreMessages.shell_cmd_manager_add_command_confirmation_description);
-            description.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-
-            Composite context = UIUtils.createComposite(content, 2);
-            context.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+            UIUtils.createWarningLabel(
+                content,
+                CoreMessages.shell_cmd_manager_add_command_confirmation_description,
+                GridData.FILL_HORIZONTAL,
+                2
+            );
             for (Map.Entry<String, String> entry : approvalContext.entrySet()) {
-                UIUtils.createControlLabel(context, entry.getKey());
-                Label value = UIUtils.createLabel(context, entry.getValue());
-                value.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+                UIUtils.createLabelText(content, entry.getKey(), entry.getValue(), SWT.READ_ONLY);
             }
 
-            UIUtils.createControlLabel(content, CoreMessages.shell_cmd_manager_add_command_script_label);
-            Text script = new Text(content, SWT.BORDER | SWT.MULTI | SWT.READ_ONLY | SWT.WRAP | SWT.V_SCROLL);
-            script.setFont(JFaceResources.getTextFont());
-            script.setText(command);
-            GridData scriptData = new GridData(GridData.FILL_BOTH);
-            int lineCount = Math.min(MAXIMUM_SCRIPT_LINES, Math.max(2, script.getLineCount()));
-            scriptData.heightHint = UIUtils.getFontHeight(script) * lineCount + 10;
-            script.setLayoutData(scriptData);
+            UIUtils.createLabelText(
+                content,
+                CoreMessages.shell_cmd_manager_add_command_script_label,
+                command,
+                SWT.BORDER | SWT.MULTI | SWT.READ_ONLY | SWT.WRAP | SWT.V_SCROLL,
+                new GridData(GridData.FILL_BOTH)
+            );
 
             return area;
         }
