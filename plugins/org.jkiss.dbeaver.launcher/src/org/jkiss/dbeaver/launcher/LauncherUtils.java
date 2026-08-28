d990bb29bf0e7e2308961563969957d3ad9a3dcf
gsastraw <gsastraw@gmail.com>
2026-08-28 23:57:56 +0200

    dbeaver/dbeaver#41639 Util methods to fix UNC path handling on Windows

--- plugins/org.jkiss.dbeaver.launcher/src/org/jkiss/dbeaver/launcher/LauncherUtils.java
+++ plugins/org.jkiss.dbeaver.launcher/src/org/jkiss/dbeaver/launcher/LauncherUtils.java
@@ -49,6 +49,31 @@ class LauncherUtils {
     }
 
 
+    static URL toURL(File file) throws MalformedURLException {
+        return file.toURI().toURL();
+    }
+
+
+    static File toFile(URL url) {
+        try {
+            return new File(url.toURI());
+        } catch (URISyntaxException e) {
+            // Takes into account the edge case where file urls initially created by deprecated File.toURL calls are
+            // persisted locally. Without this check, conversion would break, as File.toURL can create URLs with
+            // characters that are illegal in URIs like unescaped spaces.
+            return new File(url.getFile());
+        } catch (IllegalArgumentException e) {
+            // A file URL with an authority (the legacy UNC form file://host/share/path produced by File.toURL()) cannot
+            // be turned into a File via URI, so we should rebuild the URL to preserve the server name.
+            String host = url.getHost();
+            if ("file".equalsIgnoreCase(url.getProtocol()) && host != null && !host.isEmpty()) {
+                return new File("//" + host + url.getPath());
+            }
+            throw e;
+        }
+    }
+
+
     static URL adjustTrailingSlash(URL url, boolean trailingSlash) throws MalformedURLException {
         String file = url.getFile();
         if (trailingSlash == (file.endsWith("/"))) //$NON-NLS-1$
