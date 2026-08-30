913a1b5b40a3880b2427abffd3ceccce7eb5aed3
JoeyShapiro <57827758+JoeyShapiro@users.noreply.github.com>
2026-08-30 09:47:49 -0500

    use scientific for small numbers (#37842)
    * dbeaver/dbeaver#37841 use scientific for small numbers
    
    * made scientific option. handle zero and negs
    
    * made some edits requested
    
    * simplified code
    
    * added settings for scientific notation
    
    * added try and catch for scientific notation
    
    * properly throw errors
    
    * remove printing error
    
    * remove npe catch
    
    * use locale for default pattern
    
    * throw error if pattern is invalid
    
    * merge in the rest
    
    * cleaner way to get the format
    
    * dbeaver/dbeaver#37841 Fix use science notation
    
    * dbeaver/dbeaver#37841 Reject exponent separator clashing with locale separators
    
    ---------
    
    Co-authored-by: kseniaguzeeva <112612526+kseniaguzeeva@users.noreply.github.com>
    Co-authored-by: Diana <31996417+uslss@users.noreply.github.com>
    Co-authored-by: IvanGorshechnikov <vania.gorshevhnikov@gmail.com>
    Co-authored-by: Matvey16 <82543000+Matvey16@users.noreply.github.com>

--- plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/impl/data/formatters/NumberFormatSample.java
+++ plugins/org.jkiss.dbeaver.model/src/org/jkiss/dbeaver/model/impl/data/formatters/NumberFormatSample.java
@@ -20,6 +20,7 @@ import org.jkiss.code.NotNull;
 import org.jkiss.dbeaver.model.data.DBDDataFormatterSample;
 
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.text.NumberFormat;
 import java.util.HashMap;
 import java.util.Locale;
@@ -36,7 +37,11 @@ public class NumberFormatSample implements DBDDataFormatterSample {
     public static final String PROP_USE_TYPE_SCALE  ="useTypeScale";
     public static final String PROP_ROUNDING_MODE ="roundingMode";
     public static final String PROP_NATIVE_SPECIAL_VALUES = "nativeSpecialValues";
+    public static final String PROP_SCIENTIFIC_SMALL_VALUES = "scientificSmallValues";
+    public static final String PROP_SCIENTIFIC_EXP_SEP = "scientificExponentSeparator";
+    public static final String PROP_SCIENTIFIC_PATTERN = "scientificPattern";
     public static final String PROP_EXCLUDE_ID_COLUMNS = "excludeIdColumns";
+    public static final String DEFAULT_SCIENTIFIC_PATTERN = "0.###E0";
 
     @NotNull
     @Override
@@ -52,6 +57,9 @@ public class NumberFormatSample implements DBDDataFormatterSample {
         props.put(PROP_MIN_FRACT_DIGITS, tmp.getMinimumFractionDigits());
         props.put(PROP_USE_TYPE_SCALE, false);
         props.put(PROP_NATIVE_SPECIAL_VALUES, false);
+        props.put(PROP_SCIENTIFIC_SMALL_VALUES, false);
+        props.put(PROP_SCIENTIFIC_EXP_SEP, DecimalFormatSymbols.getInstance(locale).getExponentSeparator());
+        props.put(PROP_SCIENTIFIC_PATTERN, DEFAULT_SCIENTIFIC_PATTERN);
         props.put(PROP_EXCLUDE_ID_COLUMNS, false);
         // Use UNNECESSARY by default [#6493]
         // FIX: Use default. The problem in rounding was caused by float->double conversions.
