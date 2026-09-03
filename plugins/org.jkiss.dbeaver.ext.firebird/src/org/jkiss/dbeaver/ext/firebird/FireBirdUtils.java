76c2872c17ae1fa453e5f712db13a036e8d2ad37
Sakshi Chitnis <sakshigchitnis27@gmail.com>
2026-09-03 17:06:24 +0100

    #41699 Handle empty Firebird function source

--- plugins/org.jkiss.dbeaver.ext.firebird/src/org/jkiss/dbeaver/ext/firebird/FireBirdUtils.java
+++ plugins/org.jkiss.dbeaver.ext.firebird/src/org/jkiss/dbeaver/ext/firebird/FireBirdUtils.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2024 DBeaver Corp and others
+ * Copyright (C) 2010-2026 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -86,6 +86,10 @@ public class FireBirdUtils {
                     }
                 }
             } else {
+                return null;
+            }
+
+            if (CommonUtils.isEmpty(source)) {
                 return null;
             }
 
