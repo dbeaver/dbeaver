a9d01b9a2e96582ab322dd6c80962615fdeec4e7
Serge Rider <serge@dbeaver.com>
2026-09-09 15:54:53 +0200

    dbeaver/pro#9427 Refresh database navigator on project add

--- plugins/org.jkiss.dbeaver.ui.navigator/src/org/jkiss/dbeaver/ui/navigator/database/DatabaseNavigatorView.java
+++ plugins/org.jkiss.dbeaver.ui.navigator/src/org/jkiss/dbeaver/ui/navigator/database/DatabaseNavigatorView.java
@@ -106,19 +106,26 @@ public class DatabaseNavigatorView extends NavigatorViewBase implements DBPProje
     }
 
     @Override
-    public void handleActiveProjectChange(@NotNull DBPProject oldValue, @NotNull DBPProject newValue) {
-        UIExecutionQueue.queueExec(() -> {
-            DBNNode rootNode;
-            try {
-                rootNode = getRootNode();
-            } catch (Exception e) {
-                log.error(e);
-                rootNode = new DBNEmptyNode();
-            }
-            getNavigatorTree().setInput(rootNode);
-            getSite().getSelectionProvider().setSelection(new StructuredSelection());
-        });
-
+    public void handleProjectAdd(@NotNull DBPProject project) {
+        if (project.equals(DBPPlatformDesktop.getInstance().getWorkspace().getActiveProject())) {
+            UIExecutionQueue.queueExec(this::updateNavigatorRoot);
+        }
     }
 
+    @Override
+    public void handleActiveProjectChange(@NotNull DBPProject oldValue, @NotNull DBPProject newValue) {
+        UIExecutionQueue.queueExec(this::updateNavigatorRoot);
+    }
+
+    private void updateNavigatorRoot() {
+        DBNNode rootNode;
+        try {
+            rootNode = getRootNode();
+        } catch (Exception e) {
+            log.error(e);
+            rootNode = new DBNEmptyNode();
+        }
+        getNavigatorTree().setInput(rootNode);
+        getSite().getSelectionProvider().setSelection(new StructuredSelection());
+    }
 }
