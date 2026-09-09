3219d7f4a3ab6b0eea6546ef0630d942fdda5c1e
Serge Rider <serge@jkiss.org>
2026-09-09 05:55:26 +0200

    dbeaver/pro#10435 Unify OAuth interactions (#42042)
    * dbeaver/pro#10435 Unify OAuth interactions
    
    * dbeaver/pro#10435 Address OAuth review feedback
    
    * dbeaver/pro#10435 Use proper success page
    
    * dbeaver/pro#10435 Use theming for success page
    
    * dbeaver/pro#10435 Patch ClickHouse auth configurator

--- plugins/org.jkiss.dbeaver.ui/src/org/jkiss/dbeaver/ui/oauth/OAuthBrowserAuthDialog.java
+++ plugins/org.jkiss.dbeaver.ui/src/org/jkiss/dbeaver/ui/oauth/OAuthBrowserAuthDialog.java
@@ -14,68 +14,71 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jkiss.dbeaver.ui.ai.engine.openai;
+package org.jkiss.dbeaver.ui.oauth;
 
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.widgets.LabelFactory;
+import org.eclipse.jface.widgets.LinkFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.dnd.TextTransfer;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Link;
 import org.eclipse.swt.widgets.Shell;
 import org.jkiss.code.NotNull;
+import org.jkiss.dbeaver.ui.ShellUtils;
 import org.jkiss.dbeaver.ui.UIUtils;
-import org.jkiss.dbeaver.ui.ai.internal.AIUIMessages;
+import org.jkiss.dbeaver.ui.internal.UIMessages;
 
 import java.net.URI;
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
 
-class OpenAIAccountAuthDialog extends Dialog {
+/**
+ * Modal dialog shown while an OAuth authorization is completed in the browser.
+ */
+public class OAuthBrowserAuthDialog extends Dialog {
     private static final int COPY_LINK_ID = IDialogConstants.CLIENT_ID + 1;
 
-    private final URI authorizationUri;
-    private final CompletableFuture<Void> completion;
+    @NotNull
+    private final URI browserUrl;
+    @NotNull
+    private final Future<Void> future;
 
-    OpenAIAccountAuthDialog(
-        @NotNull Shell parentShell,
-        @NotNull URI authorizationUri,
-        @NotNull CompletableFuture<Void> completion
-    ) {
-        super(parentShell);
-        this.authorizationUri = authorizationUri;
-        this.completion = completion;
+    public OAuthBrowserAuthDialog(@NotNull Shell shell, @NotNull URI browserUrl, @NotNull Future<Void> future) {
+        super(shell);
+        this.browserUrl = browserUrl;
+        this.future = future;
         setShellStyle(SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL);
     }
 
     @Override
     protected Control createDialogArea(Composite parent) {
         Composite composite = (Composite) super.createDialogArea(parent);
-        getShell().setText(AIUIMessages.openai_configurator_sign_in_dialog_title);
+        getShell().setText(UIMessages.dialog_auth_code_title);
 
-        Label label = new Label(composite, SWT.WRAP);
-        label.setText(AIUIMessages.openai_configurator_browser_dialog_message);
-        label.setLayoutData(GridDataFactory.fillDefaults().grab(true, false).create());
-
-        Link authorizationLink = new Link(composite, SWT.NONE);
-        authorizationLink.setText("<a>" + AIUIMessages.openai_configurator_authorization_link + "</a>");
-        authorizationLink.setLayoutData(GridDataFactory.fillDefaults().grab(true, false).hint(420, SWT.DEFAULT).create());
-        authorizationLink.addListener(SWT.Selection, event -> UIUtils.openWebBrowser(authorizationUri.toString()));
+        LabelFactory.newLabel(SWT.WRAP)
+            .text(UIMessages.dialog_oauth_browser_message)
+            .layoutData(GridDataFactory.fillDefaults().grab(true, false).create())
+            .create(composite);
+        LinkFactory.newLink(SWT.NONE)
+            .text("<a>" + UIMessages.dialog_oauth_browser_open_label + "</a>")
+            .layoutData(GridDataFactory.fillDefaults().grab(true, false).hint(420, SWT.DEFAULT).create())
+            .onSelect(event -> ShellUtils.launchProgram(browserUrl.toString()))
+            .create(composite);
         return composite;
     }
 
     @Override
     protected void createButtonsForButtonBar(Composite parent) {
-        createButton(parent, COPY_LINK_ID, AIUIMessages.openai_configurator_copy_link, true);
+        createButton(parent, COPY_LINK_ID, UIMessages.dialog_auth_code_copy_link_label, true);
         createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, false);
     }
 
     @Override
     protected void buttonPressed(int buttonId) {
         if (buttonId == COPY_LINK_ID) {
-            UIUtils.setClipboardContents(getShell().getDisplay(), TextTransfer.getInstance(), authorizationUri.toString());
+            UIUtils.setClipboardContents(getShell().getDisplay(), TextTransfer.getInstance(), browserUrl.toString());
         } else {
             super.buttonPressed(buttonId);
         }
@@ -83,13 +86,13 @@ class OpenAIAccountAuthDialog extends Dialog {
 
     @Override
     protected void cancelPressed() {
-        completion.cancel(false);
+        future.cancel(false);
         super.cancelPressed();
     }
 
     @Override
     protected void handleShellCloseEvent() {
-        completion.cancel(false);
+        future.cancel(false);
         super.handleShellCloseEvent();
     }
 }
