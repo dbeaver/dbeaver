106c8e4256d6b51d46c97f1c2c52cdf1635bf819
Ruslan Musaev <43766501+HocKu7@users.noreply.github.com>
2026-09-11 12:50:41 +0200

    dbeaver/pro#9818 te allow select only in datasets (#42064)
    * dbeaver/pro#9818 te allow select only in datasets
    
    * dbeaver/pro#9818 te allow select only in datasets

--- test/org.jkiss.dbeaver.model.sql.test/src/org/jkiss/dbeaver/model/sql/SQLQueryDangerousDetectionTest.java
+++ test/org.jkiss.dbeaver.model.sql.test/src/org/jkiss/dbeaver/model/sql/SQLQueryDangerousDetectionTest.java
@@ -20,6 +20,8 @@ import org.jkiss.junit.DBeaverUnitTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class SQLQueryDangerousDetectionTest extends DBeaverUnitTest {
 
     @Test
@@ -76,6 +78,56 @@ public class SQLQueryDangerousDetectionTest extends DBeaverUnitTest {
         var query = new SQLQuery(null, "DROP schema users");
         // then
         Assertions.assertTrue(query.isDropDangerous());
+    }
+
+    @Test
+    public void allSelectStatementsShouldHaveSelectType() {
+        for (String queryText : List.of(
+            "SELECT * FROM test",
+            "SELECT id FROM test UNION SELECT id FROM other_test",
+            "(SELECT * FROM test)"
+        )) {
+            var query = new SQLQuery(null, queryText);
+            Assertions.assertEquals(SQLQueryType.SELECT, query.getType(), queryText);
+        }
+    }
+
+    @Test
+    public void readOnlySelectStatementsShouldNotBeMutating() {
+        for (String queryText : List.of(
+            "SELECT * FROM test",
+            "SELECT id FROM test UNION SELECT id FROM other_test",
+            "WITH selected AS (SELECT * FROM test) SELECT * FROM selected",
+            "(SELECT * FROM test)"
+        )) {
+            var query = new SQLQuery(null, queryText);
+            Assertions.assertFalse(query.isMutatingStatement(), queryText);
+            Assertions.assertFalse(query.isModifying(), queryText);
+        }
+    }
+
+    @Test
+    public void modifyingSelectStatementsShouldBeMutating() {
+        for (String queryText : List.of(
+            "SELECT * INTO copy FROM test",
+            "SELECT * FROM (SELECT * INTO copy FROM test) nested",
+            "WITH changed AS (DELETE FROM test RETURNING *) SELECT * FROM changed",
+            "WITH changed AS (INSERT INTO test VALUES (1) RETURNING *) SELECT * FROM changed",
+            "WITH changed AS (UPDATE test SET id = 1 RETURNING *) SELECT * FROM changed",
+            "WITH safe AS (SELECT 1), changed AS (DELETE FROM test RETURNING *) SELECT * FROM safe",
+            "SELECT 1 ORDER BY (WITH changed AS (DELETE FROM test RETURNING *) SELECT count(*) FROM changed)",
+            "SELECT 1 UNION SELECT 2 " +
+                "ORDER BY (WITH changed AS (DELETE FROM test RETURNING *) SELECT count(*) FROM changed)",
+            "SELECT 1 GROUP BY (WITH changed AS (DELETE FROM test RETURNING *) SELECT count(*) FROM changed)"
+        )) {
+            var query = new SQLQuery(null, queryText);
+            Assertions.assertTrue(query.isMutatingStatement(), queryText);
+            Assertions.assertTrue(query.isModifying(), queryText);
+        }
+
+        var lockingQuery = new SQLQuery(null, "SELECT * FROM test FOR UPDATE");
+        Assertions.assertFalse(lockingQuery.isMutatingStatement());
+        Assertions.assertTrue(lockingQuery.isModifying());
     }
 
 }
