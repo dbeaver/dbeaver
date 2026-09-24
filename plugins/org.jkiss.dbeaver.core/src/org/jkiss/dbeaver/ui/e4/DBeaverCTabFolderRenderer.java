8c4fb30e654e887a07176394b6e604aa94ae33a1
Elizabeth <28875055+E1izabeth@users.noreply.github.com>
2026-09-24 22:08:14 +0300

    dbeaver/pro#10554 Add visible tab separators (#42203)
    * dbeaver/pro#10554 Add visible tab separators
    
    * dbeaver/pro#10554 Use tab outline color for separators
    
    * dbeaver/pro#10554 Restore separator color fallback
    
    * dbeaver/pro#10554 Use consistent separator color
    
    * dbeaver/pro#10554 Reuse tab outline theme constant
    
    * dbeaver/pro#10554 Reuse shared UI color constants
    
    ---------
    
    Co-authored-by: Serge Rider <serge@jkiss.org>

--- plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/e4/DBeaverCTabFolderRenderer.java
+++ plugins/org.jkiss.dbeaver.core/src/org/jkiss/dbeaver/ui/e4/DBeaverCTabFolderRenderer.java
@@ -17,7 +17,9 @@
 package org.jkiss.dbeaver.ui.e4;
 
 import org.eclipse.e4.ui.internal.css.swt.ICTabRendering;
+import org.eclipse.e4.ui.internal.workbench.PartStackUtil;
 import org.eclipse.e4.ui.internal.workbench.swt.AbstractPartRenderer;
+import org.eclipse.e4.ui.model.application.ui.MUIElement;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.workbench.renderers.swt.CTabRendering;
 import org.eclipse.swt.SWT;
@@ -34,8 +36,10 @@ import org.jkiss.code.NotNull;
 import org.jkiss.code.Nullable;
 import org.jkiss.dbeaver.Log;
 import org.jkiss.dbeaver.model.DBPDataSourceContainer;
+import org.jkiss.dbeaver.ui.UIColors;
 import org.jkiss.dbeaver.ui.UIStyles;
 import org.jkiss.dbeaver.ui.UIUtils;
+import org.jkiss.dbeaver.ui.css.CSSUtils;
 import org.jkiss.dbeaver.utils.RuntimeUtils;
 
 import java.lang.reflect.Field;
@@ -120,6 +124,7 @@ public final class DBeaverCTabFolderRenderer extends CTabRendering implements IC
                     }
 
                     super.draw(part, state | SWT.HOT, bounds, gc);
+                    drawTabSeparator(state, bounds, gc);
                 } finally {
                     // Restore whatever we have changed back to original values
                     closeRectField.set(item, oldCloseRect);
@@ -144,6 +149,34 @@ public final class DBeaverCTabFolderRenderer extends CTabRendering implements IC
         }
 
         super.draw(part, state, bounds, gc);
+        if (part >= 0 && part < parent.getItemCount()) {
+            drawTabSeparator(state, bounds, gc);
+        }
+    }
+
+    private void drawTabSeparator(int state, @NotNull Rectangle bounds, @NotNull GC gc) {
+        if ((!isEditorStack() && !CSSUtils.isDatabaseColored(parent)) ||
+            (state & SWT.SELECTED) != 0 || bounds.width <= 0 || bounds.height <= 0) {
+            return;
+        }
+
+        Color oldForeground = gc.getForeground();
+        int oldLineWidth = gc.getLineWidth();
+        Color separatorColor = UIUtils.getColorRegistry().get(UIColors.INACTIVE_TAB_OUTLINE_COLOR);
+        gc.setForeground(separatorColor != null
+            ? separatorColor
+            : gc.getDevice().getSystemColor(SWT.COLOR_WIDGET_NORMAL_SHADOW));
+        gc.setLineWidth(1);
+        boolean onBottom = parent.getTabPosition() == SWT.BOTTOM;
+        int x = bounds.x + bounds.width - 1;
+        gc.drawLine(x, bounds.y - (onBottom ? 1 : 0), x, bounds.y + bounds.height - (onBottom ? 1 : 0));
+        gc.setLineWidth(oldLineWidth);
+        gc.setForeground(oldForeground);
+    }
+
+    private boolean isEditorStack() {
+        return parent.getData(AbstractPartRenderer.OWNING_ME) instanceof MUIElement element &&
+            PartStackUtil.isEditorStack(element);
     }
 
     private void updateMinMaxToolBarBackground() {
