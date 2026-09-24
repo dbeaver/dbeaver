88b5cd9eefbe59f50f448b33768d8e5a5917d084
ShadelessFox <35821147+ShadelessFox@users.noreply.github.com>
2026-09-24 21:02:47 +0200

    dbeaver/pro#10661 Add link to global profiles (#42159)

--- plugins/org.jkiss.dbeaver.ui.editors.connection/src/org/jkiss/dbeaver/ui/internal/UIConnectionMessages.java
+++ plugins/org.jkiss.dbeaver.ui.editors.connection/src/org/jkiss/dbeaver/ui/internal/UIConnectionMessages.java
@@ -328,6 +328,7 @@ public class UIConnectionMessages extends NLS {
     public static String pref_page_network_profiles_tool_no_profile_error_information;
     public static String pref_page_network_profiles_global_project_label;
     public static String pref_page_network_profiles_global_project_hint;
+    public static String pref_page_network_profiles_project_global_hint;
     public static String pref_page_network_profiles_global_project_name_used_in_local_label;
     public static String pref_page_network_profiles_global_project_name_used_in_local_question;
     public static String pref_page_network_profiles_local_name_used_in_global_label;
